972f39dab1718949e9f1e5265fa326a2b72450c3
Hazelcast <info@hazelcast.com>
2009-06-02 14:09:15 +0000

    Added JavaDoc
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@337 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- src/com/hazelcast/config/ConfigBuilder.java
+++ src/com/hazelcast/config/ConfigBuilder.java
@@ -1,7 +1,18 @@
 package com.hazelcast.config;
 
+/**
+ * Interface for all config builders.
+ */
+
 public interface ConfigBuilder {
 
+	/**
+	 * Parses the configuration and stores the values in passed Config object.
+	 * 
+	 * @param config the Config object to be filled by ConfigBuilder
+	 * 
+	 * @throws Exception the exception
+	 */
 	public abstract void parse(final Config config) throws Exception;
 
 }
