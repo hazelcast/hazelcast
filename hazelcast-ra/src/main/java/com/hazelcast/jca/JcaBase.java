4b98613303147dd8bf89224574f96ad6067e18e5
Talip Ozturk <talip@hazelcast.com>
2009-07-21 20:47:09 +0000

    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@420 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

---
+++ hazelcast-ra/src/main/java/com/hazelcast/jca/JcaBase.java
@@ -0,0 +1,34 @@
+/* 
+ * Copyright (c) 2007-2008, Hazel Ltd. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.hazelcast.jca;
+
+import com.hazelcast.impl.Build;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JcaBase {
+    protected static Logger logger = Logger.getLogger(JcaBase.class.getName());
+
+    private static final boolean DEBUG = Build.DEBUG;
+
+    public void log(final Object caller, final Object msg) {
+        if (DEBUG)
+            logger.log(Level.FINEST, caller + " : " + msg);
+    }
+}
