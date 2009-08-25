fdb0411ff888217d8accbe07c1b916bbb780bd68
Talip Ozturk <talip@hazelcast.com>
2009-08-25 23:51:06 +0000

    Fixes issue 128. ServerSocket.setReuseAddress(true) so that we can start on the 'shutdown' port again.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@500 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/impl/AddressPicker.java
+++ hazelcast/src/main/java/com/hazelcast/impl/AddressPicker.java
@@ -208,7 +208,16 @@ public class AddressPicker {
             }
             final InetAddress inetAddress = InetAddress.getByName(currentAddress);
             ServerSocket serverSocket = serverSocketChannel.socket();
-            serverSocket.setReuseAddress(false);
+            /**
+             * why setReuseAddress(true)?
+             * when the member is shutdown,
+             * the serversocket port will be in TIME_WAIT state for the next
+             * 2 minutes or so. If you start the member right after shutting it down
+             * you may not able able to bind to the same port because it is in TIME_WAIT
+             * state. if you set reuseaddress=true then TIME_WAIT will be ignored and
+             * you will be able to bind to the same port again.
+             */
+            serverSocket.setReuseAddress(true);
             InetSocketAddress isa;
 
             int port = config.getPort();
@@ -219,7 +228,7 @@ public class AddressPicker {
                     break;
                 } catch (final Exception e) {
                     serverSocket = serverSocketChannel.socket();
-                    serverSocket.setReuseAddress(false);
+                    serverSocket.setReuseAddress(true);
                     port++;
                 }
             }
