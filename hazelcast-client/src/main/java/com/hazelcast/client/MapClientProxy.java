7f14d419a4b9be3cb58896876551009600dec4dc
Fuad Malikov <fuad@hazelcast.com>
2009-09-14 14:29:55 +0000

    Removed unused imports
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@565 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-client/src/main/java/com/hazelcast/client/MapClientProxy.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/MapClientProxy.java
@@ -17,14 +17,7 @@
 
 package com.hazelcast.client;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.IOException;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
