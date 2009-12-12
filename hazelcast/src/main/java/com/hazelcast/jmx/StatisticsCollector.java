cbc288f256463d9534ab35f704718a4b4270c961
Talip Ozturk <talip@hazelcast.com>
2009-12-12 11:52:51 +0000

    License updates.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@798 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/jmx/StatisticsCollector.java
+++ hazelcast/src/main/java/com/hazelcast/jmx/StatisticsCollector.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2007-2009, Hazel Ltd. All Rights Reserved.
+ * Copyright (c) 2008-2009, Hazel Ltd. All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  * limitations under the License.
  *
  */
+
 package com.hazelcast.jmx;
 
 interface StatisticsCollector {
