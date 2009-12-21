9bdd0f2554899066dee37522e3575287ac03f865
Fuad Malikov <fuad@hazelcast.com>
2009-12-21 14:26:12 +0000

    ExecutorServiceClientProxy now  compiles with both java 1.6 and 1.5
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@852 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-client/src/main/java/com/hazelcast/client/ExecutorServiceClientProxy.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/ExecutorServiceClientProxy.java
@@ -88,46 +88,50 @@ public class ExecutorServiceClientProxy implements ClientProxy, ExecutorService{
         return submit(runnable, null);
     }
 
-    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> callables) throws InterruptedException {
-        // copied from ExecutorServiceProxy
-    	if (callables == null)
-    		throw new NullPointerException();
-    	List<Future<T>> futures = new ArrayList<Future<T>>(callables.size());
-    	boolean done = false;
-    	try {
-    		for (Callable<T> command : callables) {
-    	        futures.add(submit(command));
-    		}
-    		for (Future<T> f : futures) {
-    			if (!f.isDone()) {
-    				try {
-    					f.get();
-    				}
-    				catch (CancellationException ignore) {
-    				}
-    				catch (ExecutionException ignore) {
-    				}
-    			}
-    		}
-    		done = true;
-    		return futures;
-    	}
-    	finally {
-    		if (!done)
-    			for (Future<T> f : futures)
-    				f.cancel(true);
-    	}
-    }
+    public List<Future> invokeAll(Collection tasks) throws InterruptedException {
+            // Inspired to JDK7
+            if (tasks == null)
+                throw new NullPointerException();
+            List<Future> futures = new ArrayList<Future>(tasks.size());
+            boolean done = false;
+            try {
 
-    public <T> List<Future<T>> invokeAll(Collection<Callable<T>> callables, long l, TimeUnit timeUnit) throws InterruptedException {
+                for (Object command : tasks) {
+                    futures.add(submit((Callable) command));
+                }
+                for (Future f : futures) {
+                    if (!f.isDone()) {
+                        try {
+                            f.get();
+                        }
+                        catch (CancellationException ignore) {
+                        }
+                        catch (ExecutionException ignore) {
+                        }
+                    }
+                }
+                done = true;
+                return futures;
+            }
+            finally {
+                if (!done)
+                    for (Future f : futures)
+                        f.cancel(true);
+            }
+        }
+    
+
+    public List invokeAll(Collection tasks, long timeout, TimeUnit unit)
+            throws InterruptedException {
         throw new UnsupportedOperationException();
     }
 
-    public <T> T invokeAny(Collection<Callable<T>> callables) throws InterruptedException, ExecutionException {
+    public Object invokeAny(Collection tasks) throws InterruptedException, ExecutionException {
         throw new UnsupportedOperationException();
     }
 
-    public <T> T invokeAny(Collection<Callable<T>> callables, long l, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
+    public Object invokeAny(Collection tasks, long timeout, TimeUnit unit)
+            throws InterruptedException, ExecutionException, TimeoutException {
         throw new UnsupportedOperationException();
     }
 
