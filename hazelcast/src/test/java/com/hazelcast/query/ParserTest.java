4cc6933b40164c2f87a0f10137d9172a0fdb2702
Hazelcast <info@hazelcast.com>
2009-12-29 00:09:19 +0000

    Test clean up, added two failing tests that cause Parser#toPrefix() to fail with ArrayIndexOutOfBoundsException
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@868 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/test/java/com/hazelcast/query/ParserTest.java
+++ hazelcast/src/test/java/com/hazelcast/query/ParserTest.java
@@ -17,8 +17,10 @@
 
 package com.hazelcast.query;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
@@ -26,105 +28,137 @@ import static junit.framework.Assert.*;
 
 public class ParserTest {
 	Parser parser = new Parser();
+	
 	@Test
 	public void parseEmpty(){
-		List l = parser.toPrefix("");
+		List<String> l = parser.toPrefix("");
 		assertEquals(Arrays.asList(), l);
 	}
+	
 	@Test
 	public void parseAEqB(){
 		String s = "a = b";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("a","b","="), list);
 	}
+	
 	@Test 
 	public void parseAeqBandXgrtY(){
 		assertTrue(parser.hasHigherPrecedence("=", "AND"));
 		assertFalse(parser.hasHigherPrecedence("=", ">"));
 
-		List list = parser.toPrefix("a = b AND x > y");
+		List<String> list = parser.toPrefix("a = b AND x > y");
 		assertEquals(Arrays.asList("a","b","=","x","y",">","AND"), list);
 	}
+	
 	@Test
 	public void parseAeqBandOpenBsmlCorDgtEclose(){
 		String s = "A = B AND ( B < C OR D > E )";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("A","B","=","B","C","<","D","E",">","OR","AND"),list);
 	}
+	
 	@Test
 	public void testComplexStatement(){
 		String s = "age > 5 AND ( ( ( active = true ) AND ( age = 23 ) ) OR age > 40 ) AND ( salary > 10 ) OR age = 10";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("age","5",">","active","true","=","age","23","=","AND","age","40",">","OR","AND","salary","10",">","AND","age","10","=","OR"), list);
 	}
+	
 	@Test
 	public void testTwoInnerParanthesis(){
 		String s = "a and b AND ( ( ( a > c AND b > d ) OR ( x = y ) ) ) OR t > u";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("a","b","and","a","c",">","b","d",">","AND","x","y","=","OR","AND","t","u",">","OR"), list);
 	}
 
     @Test
 	public void testBetweenAnd(){
 		String s = "a and b between 10 and 15";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("a","b","10","15","between","and"), list);
 	}
-
 
     @Test
 	public void testBetween(){
 		String s = "b between 10 and 15";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("b","10","15","between"), list);
 	}
-
 
     @Test
 	public void testIn(){
 		String s = "a and b OR c in ( 4, 5, 6 )";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("a","b","and","c","4,5,6","in", "OR"), list);
 	}
 
     @Test
 	public void testNot(){
 		String s = "a and not(b) OR c not in ( 4, 5, 6 )";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("a","b","not", "and", "c","4,5,6","in","not","OR"), list);
 	}
 	
 	@Test
 	public void split1(){
-		List tokens = parser.split("a and b");
+		List<String> tokens = parser.split("a and b");
 		assertEquals(Arrays.asList("a","and","b"), tokens);
 	}
+	
 	@Test
 	public void split2(){
-		List tokens = parser.split("(a and b)");
+		List<String> tokens = parser.split("(a and b)");
 		assertEquals(Arrays.asList("(","a","and","b",")"), tokens);
 	}
+	
 	@Test
 	public void split3(){
-		List tokens = parser.split("((a and b))");
+		List<String> tokens = parser.split("((a and b))");
 		assertEquals(Arrays.asList("(","(","a","and","b",")",")"), tokens);
 	}
+	
 	@Test
 	public void split4(){
-		List tokens = parser.split("a and b AND(((a>c AND b> d) OR (x = y )) ) OR t>u");
+		List<String> tokens = parser.split("a and b AND(((a>c AND b> d) OR (x = y )) ) OR t>u");
 		assertEquals(Arrays.asList("a","and","b","AND","(","(","(","a",">","c","AND","b",">","d",")","OR","(","x","=","y",")",")",")","OR","t",">","u"), tokens);
 	}
 	
 	@Test
 	public void split5(){
-		List tokens = parser.split("a and b AND(((a>=c AND b> d) OR (x <> y )) ) OR t>u");
+		List<String> tokens = parser.split("a and b AND(((a>=c AND b> d) OR (x <> y )) ) OR t>u");
 		assertEquals(Arrays.asList("a","and","b","AND","(","(","(","a",">=","c","AND","b",">","d",")","OR","(","x","<>","y",")",")",")","OR","t",">","u"), tokens);
 	}
 	
 	@Test
 	public void testComplexStatementWithGreaterAndEqueals(){
 		String s = "age>=5 AND ((( active = true ) AND (age = 23 )) OR age > 40) AND( salary>10 ) OR age=10";
-		List list = parser.toPrefix(s);
+		List<String> list = parser.toPrefix(s);
 		assertEquals(Arrays.asList("age","5",">=","active","true","=","age","23","=","AND","age","40",">","OR","AND","salary","10",">","AND","age","10","=","OR"), list);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void parserShouldNotAcceptNull() {
+		parser.toPrefix(null);
+		fail();
+	}
+	
+	@Test
+	public void parserShouldThrowOnInvalidInput() {
+		parser.toPrefix(")");
+		fail();
+	}
+	
+	@Test
+	public void shouldNotThrowOnRandomInput() {
+		Random random = new SecureRandom();
+		StringBuilder stringBuilder = new StringBuilder();
+		for(int i = 0; i < 1000; i++) {
+			stringBuilder.setLength(0);
+			for(int n = 0; n < 1000; n++) {
+				stringBuilder.append((char) (random.nextInt() & 0xFFFF));
+			}
+			parser.toPrefix(stringBuilder.toString());
+		}
 	}
 }
