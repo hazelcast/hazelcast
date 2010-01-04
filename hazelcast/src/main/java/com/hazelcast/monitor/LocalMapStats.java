4a202a45e11e5a929ad08167fc0033cd1a3c06ba
Talip Ozturk <talip@hazelcast.com>
2010-01-04 10:18:40 +0000

    LocalMapStats javadoc
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@887 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/monitor/LocalMapStats.java
+++ hazelcast/src/main/java/com/hazelcast/monitor/LocalMapStats.java
@@ -17,31 +17,113 @@
 
 package com.hazelcast.monitor;
 
+/**
+ * Local map statistics. As everything is partitioned in Hazelcast,
+ * each member owns 1/N (N being the number of members in the cluster)
+ * entries of a distributed map. Each member also holds backup entries
+ * of another member. LocalMapStats tells you the count of owned and backup
+ * entries besides their size in memory.
+ * <p/>
+ * When an entry is removed, it is not erased from the map immediately.
+ * Hazelcast will mark it as removed and erase it couple of seconds later for
+ * correct versioning of backups.
+ */
 public interface LocalMapStats {
 
+    /**
+     * Returns the number of entries owned by this member.
+     *
+     * @return number of entries owned by this member.
+     */
     int getOwnedEntryCount();
 
+    /**
+     * Returns the number of backup entries hold by this member.
+     *
+     * @return number of backup entries hold by this member.
+     */
     int getBackupEntryCount();
 
+    /**
+     * Returns the number of marked as removed entries in this member.
+     * <p/>
+     * When an entry is removed, it is not erased from the map immediately.
+     * Hazelcast will mark it as removed and erase it couple of seconds later for
+     * correct versioning of backups.
+     *
+     * @return number of entries marked as removed.
+     */
     int getMarkedAsRemovedEntryCount();
 
+    /**
+     * Returns memory cost (number of bytes) of owned entries in this member.
+     *
+     * @return memory cost (number of bytes) of owned entries in this member.
+     */
     int getOwnedEntryMemoryCost();
 
+    /**
+     * Returns memory cost (number of bytes) of backup entries in this member.
+     *
+     * @return memory cost (number of bytes) of backup entries in this member.
+     */
     int getBackupEntryMemoryCost();
 
+    /**
+     * Returns memory cost (number of bytes) of marked as
+     * removed entries in this member.
+     *
+     * @return memory cost (number of bytes) of marked as removed entries.
+     */
     int getMarkedAsRemovedMemoryCost();
 
+    /**
+     * Returns the creation time of this map on this member.
+     *
+     * @return creation time of this map on this member.
+     */
     long getCreationTime();
 
+    /**
+     * Returns the last access (read) time of the locally owned entries.
+     *
+     * @return last access time.
+     */
     long getLastAccessTime();
 
+    /**
+     * Returns the last update time of the locally owned entries.
+     *
+     * @return last update time.
+     */
     long getLastUpdateTime();
 
+    /**
+     * Returns the last eviction time of the locally owned entries.
+     *
+     * @return last eviction time.
+     */
     long getLastEvictionTime();
 
+    /**
+     * Returns the number of hits (reads) of the locally owned entries.
+     *
+     * @return number of hits (reads).
+     */
     int getHits();
 
+    /**
+     * Returns the number of currently locked locally owned keys.
+     *
+     * @return number of locked entries.
+     */
     int getLockedEntryCount();
 
+    /**
+     * Returns the number of cluster-wide threads waiting
+     * to acquire locks for the locally owned keys.
+     *
+     * @return number of threads waiting for locks.
+     */
     int getLockWaitCount();
 }
