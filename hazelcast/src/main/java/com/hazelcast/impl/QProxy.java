5c94a10c4aec56bcc316e375af7b7d31761b80ba
Hazelcast <info@hazelcast.com>
2010-01-05 00:17:40 +0000

    Cleanup, all referenced methods are provided by IQueue<E>, no need to duplicate them
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@897 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/impl/QProxy.java
+++ hazelcast/src/main/java/com/hazelcast/impl/QProxy.java
@@ -18,47 +18,6 @@
 package com.hazelcast.impl;
 
 import com.hazelcast.core.IQueue;
-import com.hazelcast.core.ItemListener;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.concurrent.TimeUnit;
-
-interface QProxy extends IQueue {
-
-    boolean offer(Object obj);
-
-    boolean offer(Object obj, long timeout, TimeUnit unit) throws InterruptedException;
-
-    void put(Object obj) throws InterruptedException;
-
-    Object peek();
-
-    Object poll();
-
-    Object poll(long timeout, TimeUnit unit) throws InterruptedException;
-
-    Object take() throws InterruptedException;
-
-    int remainingCapacity();
-
-    Iterator iterator();
-
-    int size();
-
-    void addItemListener(ItemListener listener, boolean includeValue);
-
-    void removeItemListener(ItemListener listener);
-
-    String getName();
-
-    boolean remove(Object obj);
-
-    int drainTo(Collection c);
-
-    int drainTo(Collection c, int maxElements);
-
-    void destroy();
-
-    InstanceType getInstanceType();
+interface QProxy<E> extends IQueue<E> {
 }
