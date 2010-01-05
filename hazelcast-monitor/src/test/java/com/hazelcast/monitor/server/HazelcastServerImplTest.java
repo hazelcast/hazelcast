bc025f04a8a10e0e63d418bb975d4f16daeee8a4
Fuad Malikov <fuad@hazelcast.com>
2010-01-05 09:17:28 +0000

    Cleaned the unnecessary tests
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@898 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-monitor/src/test/java/com/hazelcast/monitor/server/HazelcastServerImplTest.java
+++ hazelcast-monitor/src/test/java/com/hazelcast/monitor/server/HazelcastServerImplTest.java
@@ -17,81 +17,13 @@
 
 package com.hazelcast.monitor.server;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.core.*;
-import com.hazelcast.monitor.client.ClusterView;
-import com.hazelcast.monitor.client.ConnectionExceptoin;
-import com.hazelcast.monitor.client.HazelcastService;
-import com.hazelcast.monitor.client.event.ChangeEvent;
 import org.junit.Test;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 public class HazelcastServerImplTest {
 
     @Test
-    public void connectToClusterGetMap() throws ConnectionExceptoin, InterruptedException {
-        HazelcastService service = new HazelcastServiceImpl();
-        HazelcastInstance h1 = Hazelcast.newHazelcastInstance(null);
-//        HazelcastInstance h2 = Hazelcast.newHazelcastInstance(null);
-        String hostName = h1.getCluster().getLocalMember().getInetSocketAddress().getHostName();
-        int port = h1.getCluster().getLocalMember().getInetSocketAddress().getPort();
+    public void run() {
 
-        ClusterView cv = service.connectCluster("fuad-dev", "dev-pass", hostName + ":" + port);
-        Map map1 = h1.getMap("map1");
-        Thread.sleep(100);
-        List<ChangeEvent> list = service.getChange();
-        System.out.println(list.size());
-        for (Iterator<ChangeEvent> iterator = list.iterator(); iterator.hasNext();) {
-            ChangeEvent changeEvent = iterator.next();
-            System.out.println("ChangeEvent: " + changeEvent);
-        }
-    }
-
-    @Test
-    public void connectToClusterDestroyMap() throws ConnectionExceptoin, InterruptedException {
-        HazelcastService service = new HazelcastServiceImpl();
-        HazelcastInstance h1 = Hazelcast.newHazelcastInstance(null);
-        String hostName = h1.getCluster().getLocalMember().getInetSocketAddress().getHostName();
-        int port = h1.getCluster().getLocalMember().getInetSocketAddress().getPort();
-        service.connectCluster("fuad-dev", "dev-pass", hostName + ":" + port);
-        IMap map1 = h1.getMap("map1");
-        List<ChangeEvent> list = service.getChange();
-        Thread.sleep(100);
-
-        map1.destroy();
-        Thread.sleep(100);
-        list = service.getChange();
-        System.out.println(list.size());
-        for (Iterator<ChangeEvent> iterator = list.iterator(); iterator.hasNext();) {
-            ChangeEvent changeEvent = iterator.next();
-            System.out.println("ChangeEvent: " + changeEvent);
-        }
-    }
-
-    @Test
-    public void listenfromHazelcast() throws ConnectionExceptoin, InterruptedException {
-        HazelcastInstance h1 = Hazelcast.newHazelcastInstance(null);
-        String hostName = h1.getCluster().getLocalMember().getInetSocketAddress().getHostName();
-        int port = h1.getCluster().getLocalMember().getInetSocketAddress().getPort();
-
-        HazelcastClient client = HazelcastClient.newHazelcastClient("fuad-dev", "dev-pass", hostName + ":" + port);
-        client.addInstanceListener(new InstanceListener() {
-
-            public void instanceCreated(InstanceEvent event) {
-                System.out.println("Created:" + event);
-            }
-
-            public void instanceDestroyed(InstanceEvent event) {
-                System.out.println("Destroyed:" + event);
-            }
-
-        });
-        IMap map1 = h1.getMap("map1");
-        map1.destroy();
-        Thread.sleep(10000);
     }
 
 
