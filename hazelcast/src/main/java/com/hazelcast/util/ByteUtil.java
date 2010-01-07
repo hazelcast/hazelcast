8a3d978b1e280753d4be83317b5c89efc4783da8
Hazelcast <info@hazelcast.com>
2010-01-07 22:27:34 +0000

    Removed CheckStyle warnings.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@911 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/util/ByteUtil.java
+++ hazelcast/src/main/java/com/hazelcast/util/ByteUtil.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright (c) 2008-2010, Hazel Ltd. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- * 
+ * You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,28 +22,34 @@ package com.hazelcast.util;
  */
 public final class ByteUtil {
 
-	private final static byte[] POWERS = new byte[] {1, 2, 4, 8, 16, 32, 64, -128};
+    /**
+     * Array of bit positions.
+     */
+    private static final byte[] POWERS = new byte[] {
+        1, 2, 4, 8, 16, 32, 64, -128
+    };
 
-	/**
-	 * All members are static and there should never be an instance of this class.
-	 */
-	private ByteUtil() {
-	}
+    /**
+     * All members are static and there should
+     * never be an instance of this class.
+     */
+    private ByteUtil() {
+    }
 
-	/**
-	 * Sets a bit to 1.
-	 * 
-	 * @param number the original byte value
-	 * @param index the bit to set
-	 * @return the modified byte value
-	 */
+    /**
+     * Sets a bit to 1.
+     *
+     * @param number the original byte value
+     * @param index the bit to set
+     * @return the modified byte value
+     */
     public static byte setTrue(final byte number, final int index) {
         return (byte) (number | POWERS[index]);
     }
 
     /**
      * Clears a bit, by setting it to 0.
-     * 
+     *
      * @param number the original byte value
      * @param index the bit to set
      * @return the modified byte value
@@ -54,7 +60,7 @@ public final class ByteUtil {
 
     /**
      * Checks if the index-th bit of number is set.
-     * 
+     *
      * @param number the byte value
      * @param index the bit to check
      * @return true if the bit is set, false otherwise
@@ -64,8 +70,8 @@ public final class ByteUtil {
     }
 
     /**
-     * Checks if the index-th bit of number is NOT set. 
-     * 
+     * Checks if the index-th bit of number is NOT set.
+     *
      * @param number the byte value
      * @param index the bit to check
      * @return true if the bit is NOT set, false otherwise
