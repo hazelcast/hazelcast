2155f66a2a44fa9057bed99e91e8ccded7a006f4
Fuad Malikov <fuad@hazelcast.com>
2010-03-15 13:41:21 +0000

    Concurrency fix
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1100 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-client/src/main/java/com/hazelcast/client/impl/ListenerManager.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/impl/ListenerManager.java
@@ -99,7 +99,7 @@ public class ListenerManager extends ClientRunnable {
                 entryListeners.put(name, new HashMap<Object, List<EntryListener<?, ?>>>());
             }
             if (!entryListeners.get(name).containsKey(key)) {
-                entryListeners.get(name).put(key, new ArrayList<EntryListener<?, ?>>());
+                entryListeners.get(name).put(key, new CopyOnWriteArrayList<EntryListener<?, ?>>());
             }
             entryListeners.get(name).get(key).add(entryListener);
         }
@@ -207,7 +207,7 @@ public class ListenerManager extends ClientRunnable {
 
         public synchronized void registerMessageListener(String name, MessageListener messageListener) {
             if (!messageListeners.containsKey(name)) {
-                messageListeners.put(name, new ArrayList<MessageListener<Object>>());
+                messageListeners.put(name, new CopyOnWriteArrayList<MessageListener<Object>>());
             }
             messageListeners.get(name).add(messageListener);
         }
