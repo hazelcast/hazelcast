41eb71b347835cde8989dc42c1a4d05519c76781
Fuad Malikov <fuad@hazelcast.com>
2010-07-27 11:11:47 +0000

    Use interfaces instead of implementations whenever possible
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1281 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/monitor/TimedClusterStats.java
+++ hazelcast/src/main/java/com/hazelcast/monitor/TimedClusterStats.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright (c) 2008-2010, Hazel Ltd. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- * 
+ * You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,14 +30,18 @@ import java.util.Map;
 import java.util.Set;
 
 public class TimedClusterStats implements DataSerializable {
-    long time;
-    Map<Member, MemberStatsImpl> memberStats = new HashMap<Member, MemberStatsImpl>();
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = -2924333395156041186L;
+	long time;
+    Map<Member, MemberStats> mapMemberStats = new HashMap<Member, MemberStats>();
 
     public void writeData(DataOutput out) throws IOException {
         out.writeLong(time);
-        out.writeInt(memberStats.size());
-        Set<Map.Entry<Member, MemberStatsImpl>> memberStatEntries = memberStats.entrySet();
-        for (Map.Entry<Member, MemberStatsImpl> memberStatEntry : memberStatEntries) {
+        out.writeInt(mapMemberStats.size());
+        Set<Map.Entry<Member, MemberStats>> memberStatEntries = mapMemberStats.entrySet();
+        for (Map.Entry<Member, MemberStats> memberStatEntry : memberStatEntries) {
             memberStatEntry.getKey().writeData(out);
             memberStatEntry.getValue().writeData(out);
         }
@@ -49,10 +53,26 @@ public class TimedClusterStats implements DataSerializable {
         for (int i = 0; i < memberStatsCount; i++) {
             Member member = new MemberImpl();
             member.readData(in);
-            MemberStatsImpl memberStatsImpl = new MemberStatsImpl();
-            memberStatsImpl.readData(in);
-            memberStats.put(member, memberStatsImpl);
+            MemberStats memberStats = new MemberStatsImpl();
+            memberStats.readData(in);
+            mapMemberStats.put(member, memberStats);
         }
     }
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public MemberStats putMemberStats(Member member, MemberStats mStats){
+		return mapMemberStats.put(member, mStats);
+	}
+
+	public boolean containsKey(Member member) {
+		return mapMemberStats.containsKey(member);
+	}
 }
 
