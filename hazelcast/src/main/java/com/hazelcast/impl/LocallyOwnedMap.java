c892025f65e0f2dc76cd8146f2f166c1ebea4c05
Talip Ozturk <talip@hazelcast.com>
2010-08-20 18:06:26 +0000

    Fixes issue 345.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1344 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/impl/LocallyOwnedMap.java
+++ hazelcast/src/main/java/com/hazelcast/impl/LocallyOwnedMap.java
@@ -68,7 +68,7 @@ class LocallyOwnedMap {
             for (Map.Entry<Object, Record> entry : entries) {
                 Object key = entry.getKey();
                 Record record = entry.getValue();
-                if (!record.isActive() || !record.isValid(now)) {
+                if (record == null || !record.isActive() || !record.isValid(now)) {
                     lsKeysToRemove.add(key);
                 }
             }
