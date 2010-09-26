8d3e47d5e5a1f0bfe886a27355148851b5aa00c0
Mehmet Dogan <mehmet@hazelcast.com>
2010-09-26 18:49:08 +0000

    java 1.5 compiler does not support @Override annotation on methods implementing interface methods
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1389 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-client/src/test/java/com/hazelcast/client/HazelcastClientMapTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/HazelcastClientMapTest.java
@@ -54,19 +54,15 @@ public class HazelcastClientMapTest extends HazelcastClientTestBase {
 
     private static class EntryAdapter<K, V> implements EntryListener<K, V> {
 
-		@Override
 		public void entryAdded(EntryEvent<K, V> event) {
 		}
 
-		@Override
 		public void entryRemoved(EntryEvent<K, V> event) {
 		}
 
-		@Override
 		public void entryUpdated(EntryEvent<K, V> event) {
 		}
 
-		@Override
 		public void entryEvicted(EntryEvent<K, V> event) {
 		}
     	
