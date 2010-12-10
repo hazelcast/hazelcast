272ad9a1dcfe1036e6502f93e97cb273e8f3853e
Hazelcast <info@hazelcast.com>
2010-12-10 12:42:28 +0000

    constants
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1537 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

---
+++ hazelcast-wm/src/main/java/com/hazelcast/web/tomcat/HazelConstants.java
@@ -0,0 +1,19 @@
+package com.hazelcast.web.tomcat;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.IMap;
+
+/**
+ * @author ali
+ *
+ */
+
+public interface HazelConstants {
+	
+	public static final IMap hazelAttributes = Hazelcast.getMap("__hz_ses_attrs");
+	
+	public static final String HAZEL_SESSION_MARK = "__hz_ses_mark";
+	
+	public static final String HAZEL_MARK_EXCEPTION = "'"+HAZEL_SESSION_MARK + "' is a reserved key for Hazelcast!";
+
+}
