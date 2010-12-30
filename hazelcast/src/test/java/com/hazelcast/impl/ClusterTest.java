a74d72d93563fd47ecb3846e594fa5476da6f555
Talip Ozturk <talip@hazelcast.com>
2010-12-30 09:15:57 +0000

    Test fix. (insufficient wait)
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1574 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/test/java/com/hazelcast/impl/ClusterTest.java
+++ hazelcast/src/test/java/com/hazelcast/impl/ClusterTest.java
@@ -1394,7 +1394,7 @@ public class ClusterTest {
         latch.await();
         es.shutdown();
         assertTrue(es.awaitTermination(5, TimeUnit.SECONDS));
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 100; i++) {
             sleep(1000);
             Runtime.getRuntime().gc();
             long usedMemoryEnd = getUsedMemoryAsMB();
@@ -1446,7 +1446,7 @@ public class ClusterTest {
         latch.await();
         es.shutdown();
         assertTrue(es.awaitTermination(5, TimeUnit.SECONDS));
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 100; i++) {
             sleep(1000);
             Runtime.getRuntime().gc();
             long usedMemoryEnd = getUsedMemoryAsMB();
