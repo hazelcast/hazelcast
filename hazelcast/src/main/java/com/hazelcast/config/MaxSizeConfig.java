f1eee57bbdbfb375c48a7cabbb582394bc4d51a7
Talip Ozturk <talip@hazelcast.com>
2011-03-22 10:30:16 +0000

    MapMaxSizePolicy updates
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1740 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/config/MaxSizeConfig.java
+++ hazelcast/src/main/java/com/hazelcast/config/MaxSizeConfig.java
@@ -47,6 +47,9 @@ public class MaxSizeConfig implements DataSerializable {
     }
 
     public MaxSizeConfig setSize(int size) {
+        if (size == 0) {
+            size = Integer.MAX_VALUE;
+        }
         this.size = size;
         return this;
     }
