fe12b9c6fc723156b70ac85dfef97946e849aab9
Mehmet Dogan <mehmet@hazelcast.com>
2011-04-11 15:50:14 +0000

    Added javadoc from hibernate
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1762 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-hibernate/src/main/java/com/hazelcast/hibernate/access/AccessDelegate.java
+++ hazelcast-hibernate/src/main/java/com/hazelcast/hibernate/access/AccessDelegate.java
@@ -26,39 +26,184 @@ import org.hibernate.cache.access.SoftLock;
  * @author Leo Kim (lkim@limewire.com)
  */
 public interface AccessDelegate<T extends HazelcastRegion> {
+	
+	/**
+	 * Get the wrapped cache region
+	 *
+	 * @return The underlying region
+	 */
     T getHazelcastRegion();
-
+    
     IMap getCache();
 
-    boolean afterInsert(Object key, Object value, Object version) throws CacheException;
+    /**
+	 * Attempt to retrieve an object from the cache. Mainly used in attempting
+	 * to resolve entities/collections from the second level cache.
+	 *
+	 * @param key The key of the item to be retrieved.
+	 * @param txTimestamp a timestamp prior to the transaction start time
+	 * @return the cached object or <tt>null</tt>
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    Object get(Object key, long txTimestamp) throws CacheException;
 
+    /**
+	 * Called after an item has been inserted (before the transaction completes),
+	 * instead of calling evict().
+	 * This method is used by "synchronous" concurrency strategies.
+	 *
+	 * @param key The item key
+	 * @param value The item
+	 * @param version The item's version value
+	 * @return Were the contents of the cache actual changed by this operation?
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    boolean insert(Object key, Object value, Object version) throws CacheException;
+    
+    /**
+	 * Called after an item has been inserted (after the transaction completes),
+	 * instead of calling release().
+	 * This method is used by "asynchronous" concurrency strategies.
+	 *
+	 * @param key The item key
+	 * @param value The item
+	 * @param version The item's version value
+	 * @return Were the contents of the cache actual changed by this operation?
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    boolean afterInsert(Object key, Object value, Object version) throws CacheException;
+    
+    /**
+	 * Called after an item has been updated (before the transaction completes),
+	 * instead of calling evict(). This method is used by "synchronous" concurrency
+	 * strategies.
+	 *
+	 * @param key The item key
+	 * @param value The item
+	 * @param currentVersion The item's current version value
+	 * @param previousVersion The item's previous version value
+	 * @return Were the contents of the cache actual changed by this operation?
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    boolean update(Object key, Object value, Object currentVersion, Object previousVersion) throws CacheException;
+
+    /**
+	 * Called after an item has been updated (after the transaction completes),
+	 * instead of calling release().  This method is used by "asynchronous"
+	 * concurrency strategies.
+	 *
+	 * @param key The item key
+	 * @param value The item
+	 * @param currentVersion The item's current version value
+	 * @param previousVersion The item's previous version value
+	 * @param lock The lock previously obtained from {@link #lockItem}
+	 * @return Were the contents of the cache actual changed by this operation?
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     boolean afterUpdate(Object key, Object value, Object currentVersion, Object previousVersion, SoftLock lock)
             throws CacheException;
 
-    void evict(Object key) throws CacheException;
-
-    void evictAll() throws CacheException;
-
-    Object get(Object key, long txTimestamp) throws CacheException;
-
-    boolean insert(Object key, Object value, Object version) throws CacheException;
-
-    SoftLock lockItem(Object key, Object version) throws CacheException;
-
-    SoftLock lockRegion() throws CacheException;
-
+    /**
+	 * Attempt to cache an object, after loading from the database.
+	 *
+	 * @param key The item key
+	 * @param value The item
+	 * @param txTimestamp a timestamp prior to the transaction start time
+	 * @param version the item version number
+	 * @return <tt>true</tt> if the object was successfully cached
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     boolean putFromLoad(Object key, Object value, long txTimestamp, Object version) throws CacheException;
 
+    /**
+	 * Attempt to cache an object, after loading from the database, explicitly
+	 * specifying the minimalPut behavior.
+	 *
+	 * @param key The item key
+	 * @param value The item
+	 * @param txTimestamp a timestamp prior to the transaction start time
+	 * @param version the item version number
+	 * @param minimalPutOverride Explicit minimalPut flag
+	 * @return <tt>true</tt> if the object was successfully cached
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     boolean putFromLoad(Object key, Object value, long txTimestamp, Object version, boolean minimalPutOverride)
             throws CacheException;
 
+    /**
+	 * Called after an item has become stale (before the transaction completes).
+	 * This method is used by "synchronous" concurrency strategies.
+	 *
+	 * @param key The key of the item to remove
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     void remove(Object key) throws CacheException;
 
+    /**
+	 * Called to evict data from the entire region
+	 *
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     void removeAll() throws CacheException;
+    
+    /**
+	 * Forcibly evict an item from the cache immediately without regard for transaction
+	 * isolation.
+	 *
+	 * @param key The key of the item to remove
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    void evict(Object key) throws CacheException;
 
+    /**
+	 * Forcibly evict all items from the cache immediately without regard for transaction
+	 * isolation.
+	 *
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    void evictAll() throws CacheException;
+    
+    /**
+	 * We are going to attempt to update/delete the keyed object. This
+	 * method is used by "asynchronous" concurrency strategies.
+	 * <p/>
+	 * The returned object must be passed back to release(), to release the
+	 * lock. Concurrency strategies which do not support client-visible
+	 * locks may silently return null.
+	 *
+	 * @param key The key of the item to lock
+	 * @param version The item's current version value
+	 * @return A representation of our lock on the item; or null.
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    SoftLock lockItem(Object key, Object version) throws CacheException;
+
+    /**
+	 * Lock the entire region
+	 *
+	 * @return A representation of our lock on the item; or null.
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
+    SoftLock lockRegion() throws CacheException;
+
+    /**
+	 * Called when we have finished the attempted update/delete (which may or
+	 * may not have been successful), after transaction completion.  This method
+	 * is used by "asynchronous" concurrency strategies.
+	 *
+	 * @param key The item key
+	 * @param lock The lock previously obtained from {@link #lockItem}
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     void unlockItem(Object key, SoftLock lock) throws CacheException;
 
+    /**
+	 * Called after we have finished the attempted invalidation of the entire
+	 * region
+	 *
+	 * @param lock The lock previously obtained from {@link #lockRegion}
+	 * @throws CacheException Propogated from underlying {@link org.hibernate.cache.Region}
+	 */
     void unlockRegion(SoftLock lock) throws CacheException;
-
-    boolean update(Object key, Object value, Object currentVersion, Object previousVersion) throws CacheException;
+    
 }
