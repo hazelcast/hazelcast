0b64c9c15f46376af519c35d98fb56e227190b48
Talip Ozturk <talip@hazelcast.com>
2011-05-02 19:30:10 +0000

    Fixes issue 556. Too long config.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1822 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/impl/MulticastService.java
+++ hazelcast/src/main/java/com/hazelcast/impl/MulticastService.java
@@ -51,7 +51,7 @@ public class MulticastService implements Runnable {
         logger = node.getLogger(MulticastService.class.getName());
         Config config = node.getConfig();
         this.multicastSocket = multicastSocket;
-        int bufferSize = 1024 * 1024;
+        int bufferSize = 64 * 1024;
         this.datagramPacketReceive = new DatagramPacket(new byte[bufferSize], bufferSize);
         this.datagramPacketSend = new DatagramPacket(new byte[bufferSize], bufferSize, InetAddress
                 .getByName(config.getNetworkConfig().getJoin().getMulticastConfig().getMulticastGroup()),
@@ -127,7 +127,7 @@ public class MulticastService implements Runnable {
                 joinInfo.writeToPacket(datagramPacketSend);
                 multicastSocket.send(datagramPacketSend);
             } catch (IOException e) {
-                logger.log(Level.WARNING, e.getMessage(), e);
+                logger.log(Level.WARNING, "You probably have too long Hazelcast configuration!", e);
             }
         }
     }
