dcda06199e3b9717152b0300bed0c80980a091cf
Fuad Malikov <fuad@hazelcast.com>
2011-06-28 15:21:48 +0000

    Removed the dependency to aws sdk and implemented our own ec2 over soap.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1887 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

---
+++ src/main/java/com/hazelcast/aws/impl/AWSClient.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2008-2011, Hazel Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.hazelcast.aws.impl;
+
+import java.util.List;
+
+public class AWSClient {
+    private String accessKey;
+    private String secretKey;
+
+    public AWSClient(String accessKey, String secretKey) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
+
+    public List<String> getPrivateDnsNames() throws Exception {
+        List<String> list = new DescribeInstances(accessKey, secretKey).execute();
+        return list;
+    }
+}
