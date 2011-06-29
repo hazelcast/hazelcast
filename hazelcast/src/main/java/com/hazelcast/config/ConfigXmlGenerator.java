c107859092914bf1f6c684ee202f309eb60330e2
Mehmet Dogan <mehmet@hazelcast.com>
2011-06-29 11:51:17 +0000

    fix read-backup-data node value
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@1894 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/config/ConfigXmlGenerator.java
+++ hazelcast/src/main/java/com/hazelcast/config/ConfigXmlGenerator.java
@@ -135,7 +135,7 @@ public class ConfigXmlGenerator {
 			xml.append("<time-to-live-seconds>").append(m.getTimeToLiveSeconds()).append("</time-to-live-seconds>");
 			xml.append("<max-idle-seconds>").append(m.getMaxIdleSeconds()).append("</max-idle-seconds>");
 			xml.append("<cache-value>").append(m.isCacheValue()).append("</cache-value>");
-			xml.append("<read-backup-data>").append(m.isCacheValue()).append("</read-backup-data>");
+			xml.append("<read-backup-data>").append(m.isReadBackupData()).append("</read-backup-data>");
 			xml.append("<merge-policy>").append(m.getMergePolicy()).append("</merge-policy>");
 			
 			if(m.getMapStoreConfig() != null) {
