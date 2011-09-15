60d4c05981bc60105a7f2d6a89ce534384c159d7
Fuad Malikov <fuad@hazelcast.com>
2011-09-15 08:16:54 +0000

    Heartbeat timer should be shutdown on client.shutdown
    git-svn-id: http://hazelcast.googlecode.com/svn/branches/1.9.4@2095 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-client/src/main/java/com/hazelcast/client/ConnectionManager.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/ConnectionManager.java
@@ -50,6 +50,8 @@ public class ConnectionManager implements MembershipListener {
     private volatile boolean running = true;
 
     private final LifecycleServiceClientImpl lifecycleService;
+    Timer heartbeatTimer = new Timer();
+
 
     public ConnectionManager(HazelcastClient client, LifecycleServiceClientImpl lifecycleService, InetSocketAddress[] clusterMembers, boolean shuffle, long timeout) {
         this.TIMEOUT = timeout;
@@ -69,8 +71,7 @@ public class ConnectionManager implements MembershipListener {
     }
 
     void scheduleHeartbeatTimerTask() {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        heartbeatTimer.schedule(new TimerTask() {
             @Override
             public void run() {
                 long diff = client.getInRunnable().lastReceived - System.currentTimeMillis();
@@ -313,5 +314,6 @@ public class ConnectionManager implements MembershipListener {
     public void shutdown() {
         logger.log(Level.INFO, getClass().getSimpleName() + " shutdown");
         running = false;
+        heartbeatTimer.cancel();
     }
 }
