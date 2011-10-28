ea3c5f697b5c54674bda8db59cd4711e99fad6dc
Mehmet Dogan <mehmet@hazelcast.com>
2011-10-28 08:03:50 +0000

    Added license declarations
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@2207 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/config/PermissionConfig.java
+++ hazelcast/src/main/java/com/hazelcast/config/PermissionConfig.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2008-2010, Hazel Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.hazelcast.config;
 
 import java.util.HashSet;
@@ -28,18 +45,20 @@ public class PermissionConfig {
 		EXECUTOR_SERVICE, LISTENER, TRANSACTION, ALL
 	}
 	
-	public void addEndpoint(String endpoint) {
+	public PermissionConfig addEndpoint(String endpoint) {
 		if(endpoints == null) {
 			endpoints = new HashSet<String>();
 		}
 		endpoints.add(endpoint);
+		return this;
 	}
 	
-	public void addAction(String action) {
+	public PermissionConfig addAction(String action) {
 		if(actions == null) {
 			actions = new HashSet<String>();
 		}
 		actions.add(action);
+		return this;
 	}
 
 	public PermissionType getType() {
