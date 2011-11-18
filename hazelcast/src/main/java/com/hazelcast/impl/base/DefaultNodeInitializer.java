bf849f7e13ebc1f9e596e85bcb852a1eeeb222df
Mehmet Dogan <mehmet@hazelcast.com>
2011-11-18 07:59:28 +0000

    Fixes issue 721.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@2232 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/impl/base/DefaultNodeInitializer.java
+++ hazelcast/src/main/java/com/hazelcast/impl/base/DefaultNodeInitializer.java
@@ -61,10 +61,11 @@ public class DefaultNodeInitializer implements NodeInitializer {
         build = System.getProperty("hazelcast.build", "unknown");
         if ("unknown".equals(version) || "unknown".equals(build)) {
             try {
-                InputStream inRuntimeProperties = NodeInitializer.class.getClassLoader().getResourceAsStream("hazelcast-runtime.properties");
+                final InputStream inRuntimeProperties = NodeInitializer.class.getClassLoader().getResourceAsStream("hazelcast-runtime.properties");
                 if (inRuntimeProperties != null) {
                     Properties runtimeProperties = new Properties();
                     runtimeProperties.load(inRuntimeProperties);
+                    inRuntimeProperties.close();
                     version = runtimeProperties.getProperty("hazelcast.version");
                     build = runtimeProperties.getProperty("hazelcast.build");
                 }
@@ -106,5 +107,6 @@ public class DefaultNodeInitializer implements NodeInitializer {
     }
     
     public void destroy() {
+    	logger.log(Level.INFO, "Destroying node initializer.");
     }
 }
