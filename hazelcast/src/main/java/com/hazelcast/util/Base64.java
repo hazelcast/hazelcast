e1ea84eda3f2149cad86a2bbb34c810fa558c835
Peter Veentjer <alarmnummer@gmail.com>
2011-12-28 17:48:21 +0000

    Javadoc improvement
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@2323 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/main/java/com/hazelcast/util/Base64.java
+++ hazelcast/src/main/java/com/hazelcast/util/Base64.java
@@ -294,7 +294,7 @@ public final class  Base64 {
     /**
      * Decodes Base64 data into octects
      *
-     * @param binaryData Byte array containing Base64 data
+     * @param base64Data Byte array containing Base64 data
      * @return Array containind decoded data.
      */
     public static synchronized byte[] decode( byte[] base64Data ) {
