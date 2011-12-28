5856078799d2bfc99513d30334f43da6df8f3885
Peter Veentjer <alarmnummer@gmail.com>
2011-12-28 13:12:22 +0000

    Fixed compilation issue
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@2316 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast-client/src/test/java/com/hazelcast/client/longrunning/DynamicClusterTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/longrunning/DynamicClusterTest.java
@@ -234,7 +234,7 @@ public class DynamicClusterTest {
         final CountDownLatch latch = new CountDownLatch(3);
         final String message = "Hazelcast Rocks!";
         topic.addMessageListener(new MessageListener<String>() {
-            public void onMessage(String msg) {
+            public void onMessage(Message<String> msg) {
                 if (msg.equals(message)) {
                     latch.countDown();
                 }
@@ -1013,7 +1013,7 @@ public class DynamicClusterTest {
         HazelcastInstance h2 = Hazelcast.newHazelcastInstance(c2);
         HazelcastClient client2 = HazelcastClient.newHazelcastClient(c2.getGroupConfig().getName(), c2.getGroupConfig().getPassword(), "127.0.0.1:5702");
         client2.getTopic("def").addMessageListener(new MessageListener<Object>() {
-            public void onMessage(Object message) {
+            public void onMessage(Message message) {
             }
         });
         LifecycleCountingListener l = new LifecycleCountingListener();
