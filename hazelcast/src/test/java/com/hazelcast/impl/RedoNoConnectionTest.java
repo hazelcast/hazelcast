0af154afd196d10f969a1cb9adb01b04cbdba2ad
Talip Ozturk <talip@hazelcast.com>
2012-01-31 09:13:52 +0000

    RedoNoConnectionTest fixes
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@2411 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- hazelcast/src/test/java/com/hazelcast/impl/RedoNoConnectionTest.java
+++ hazelcast/src/test/java/com/hazelcast/impl/RedoNoConnectionTest.java
@@ -100,7 +100,7 @@ public class RedoNoConnectionTest extends RedoTestService {
         config.setProperty(GroupProperties.PROP_HEARTBEAT_INTERVAL_SECONDS, "6");
         final HazelcastInstance h1 = Hazelcast.newHazelcastInstance(config);
         final HazelcastInstance h2 = Hazelcast.newHazelcastInstance(config);
-        BeforeAfterTester t = new BeforeAfterTester(
+        Runnable t = new RunAfterTester(
                 new DisconnectionBehavior(h1, h2),
                 new MultiCallBuilder(h1));
         t.run();
@@ -112,7 +112,7 @@ public class RedoNoConnectionTest extends RedoTestService {
         config.setProperty(GroupProperties.PROP_HEARTBEAT_INTERVAL_SECONDS, "6");
         final HazelcastInstance h1 = Hazelcast.newHazelcastInstance(config);
         final HazelcastInstance h2 = Hazelcast.newHazelcastInstance(config);
-        BeforeAfterTester t = new BeforeAfterTester(
+        Runnable t = new RunAfterTester(
                 new DisconnectionBehavior(h2, h1),
                 new MultiCallBuilder(h2));
         t.run();
@@ -139,7 +139,7 @@ public class RedoNoConnectionTest extends RedoTestService {
         config.setProperty(GroupProperties.PROP_HEARTBEAT_INTERVAL_SECONDS, "6");
         final HazelcastInstance h1 = Hazelcast.newHazelcastInstance(config);
         final HazelcastInstance h2 = Hazelcast.newHazelcastInstance(config);
-        BeforeAfterTester t = new BeforeAfterTester(
+        Runnable t = new RunAfterTester(
                 new DisconnectionBehavior(h2, h1),
                 new QueueCallBuilder(h2));
         t.run();
