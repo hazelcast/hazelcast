a3f1743f5561e746e5cbd62d42add1cf09ed3893
Talip Ozturk <talip@hazelcast.com>
2012-02-12 12:09:46 +0000

    Copyright update. Cleanup.
    git-svn-id: http://hazelcast.googlecode.com/svn/trunk@2451 3f8e66b6-ca9d-11dd-a2b5-e5f827957e07

--- src/main/java/com/hazelcast/aws/security/EC2RequestSigner.java
+++ src/main/java/com/hazelcast/aws/security/EC2RequestSigner.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2012, Hazel Bilisim Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.aws.security;
 
 import com.hazelcast.aws.impl.DescribeInstances;
@@ -5,22 +21,6 @@ import com.hazelcast.aws.utility.AwsURLEncoder;
 
 import java.security.SignatureException;
 import java.util.*;
-/*
-* Copyright (c) 2008-2010, Hazel Ltd. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
 
 public class EC2RequestSigner {
     private static final String HTTP_VERB = "GET\n";
