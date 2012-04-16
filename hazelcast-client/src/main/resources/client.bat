9fcbccc8ca476b1d8fe0f601aaf33d7159339189
Mehmet Dogan <mehmet@hazelcast.com>
2012-04-16 14:57:36 +0300

    Updated maven manifest generation.

--- hazelcast-client/src/main/resources/client.bat
+++ hazelcast-client/src/main/resources/client.bat
@@ -1,3 +1,1 @@
-@ECHO OFF
-
-java -Djava.net.preferIPv4Stack=true -classpath ../lib/hazelcast-${project.version}.jar:../lib/hazelcast-client-${project.version}.jar com.hazelcast.client.examples.TestClientApp
+java -Djava.net.preferIPv4Stack=true -jar ../lib/hazelcast-client-${project.version}.jar
