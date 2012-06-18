5d8ab9b9c4a357b10c5054c893d64eff6c138864
Enes Akar <enesakar@gmail.com>
2012-06-18 15:48:51 +0300

    fiex #183

--- hazelcast-client/src/test/java/com/hazelcast/client/ListenerTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/ListenerTest.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.client;
 
+import com.hazelcast.config.Config;
 import com.hazelcast.core.*;
 import com.hazelcast.core.LifecycleEvent.LifecycleState;
 import org.junit.After;
@@ -27,6 +28,7 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @mdogan 4/24/12
@@ -40,6 +42,60 @@ public class ListenerTest {
     public void cleanup() throws Exception {
         Hazelcast.shutdownAll();
         HazelcastClient.shutdownAll();
+    }
+
+
+
+    /* github issue #183 */
+    @Test
+    public void testKeyBasedListeners() throws InterruptedException {
+        try {
+            Config config = new Config();
+            HazelcastInstance instance = Hazelcast.newHazelcastInstance(config);
+            IMap<String, String> map = instance.getMap("map");
+            map.put("key1", "value1");
+            map.put("key2", "value2");
+
+            ClientConfig clientConfig = new ClientConfig();
+            HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+
+            final AtomicInteger count = new AtomicInteger(0);
+            IMap<String, String> clientMap = client.getMap("map");
+
+            clientMap.addEntryListener(new EntryListener<String, String>() {
+                public void entryAdded(EntryEvent<String, String> entryEvent) {
+                    count.incrementAndGet();
+                }
+                public void entryRemoved(EntryEvent<String, String> entryEvent) {
+                }
+                public void entryUpdated(EntryEvent<String, String> entryEvent) {
+                    count.incrementAndGet();
+                }
+                public void entryEvicted(EntryEvent<String, String> entryEvent) {
+                }
+            },"key1" , true);
+
+            clientMap.addEntryListener(new EntryListener<String, String>() {
+                public void entryAdded(EntryEvent<String, String> entryEvent) {
+                    count.incrementAndGet();
+                }
+                public void entryRemoved(EntryEvent<String, String> entryEvent) {
+                }
+                public void entryUpdated(EntryEvent<String, String> entryEvent) {
+                    System.out.println("event map");
+                    count.incrementAndGet();
+                }
+                public void entryEvicted(EntryEvent<String, String> entryEvent) {
+                }
+            },"key2" , true);
+
+            map.put("key1", "new-value1");
+            Thread.sleep(100);
+            Assert.assertEquals(count.get(), 1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Test
@@ -64,4 +120,6 @@ public class ListenerTest {
         Assert.assertEquals(new LifecycleEvent(LifecycleState.SHUTTING_DOWN), q.poll(3, TimeUnit.SECONDS));
         Assert.assertEquals(new LifecycleEvent(LifecycleState.SHUTDOWN), q.poll(3, TimeUnit.SECONDS));
     }
+
+
 }
