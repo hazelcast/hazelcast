9b96dcfdb36ee7dfc9835b57a118eb8b37192b02
Mehmet Dogan <mehmet@hazelcast.com>
2012-08-31 14:25:51 +0300

    Fix tests.

--- hazelcast/src/test/java/com/hazelcast/impl/ClusterLockTest.java
+++ hazelcast/src/test/java/com/hazelcast/impl/ClusterLockTest.java
@@ -170,7 +170,7 @@ public class ClusterLockTest {
         assertEquals(h3.getCluster().getLocalMember(), h3.getPartitionService().getPartition(1).getOwner());
         assertEquals(1, map1.put(1, 2));
         rec3 = cmap3.getRecord(dKey);
-        Thread.sleep(2000); // scheduled action may be invalid because of backup copy, wait a little.
+        Thread.sleep(3000); // scheduled action may be invalid because of backup copy, wait a little.
         assertEquals(1, rec3.getScheduledActionCount());
         assertTrue(rec3.getScheduledActions().iterator().next().isValid());
         map1.unlock(1);
