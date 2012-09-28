86aac0a44658303ed5ea3eb494ebf8ea64b9a461
Mehmet Dogan <mehmet@hazelcast.com>
2012-09-28 12:04:50 +0300

    Code formatting.

--- hazelcast/src/test/java/com/hazelcast/nio/IOUtilTest.java
+++ hazelcast/src/test/java/com/hazelcast/nio/IOUtilTest.java
@@ -1,31 +1,45 @@
+/*
+ * Copyright (c) 2008-2012, Hazel Bilisim Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.nio;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * @author Tomasz Nurkiewicz
  * @since 25.09.12, 12:03
  */
 @RunWith(com.hazelcast.util.RandomBlockJUnit4ClassRunner.class)
-public class IOUtilTest
-{
+public class IOUtilTest {
 
     private static final byte[] NON_EMPTY_BYTE_ARRAY = new byte[100];
     private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
     private static final int SIZE = 3;
 
     @Test
-    public void shouldReturnMinusOneWhenEmptyByteBufferProvidedAndReadingOneByte() throws Exception
-    {
+    public void shouldReturnMinusOneWhenEmptyByteBufferProvidedAndReadingOneByte() throws Exception {
         //given
         final ByteBuffer empty = ByteBuffer.wrap(EMPTY_BYTE_ARRAY);
         final InputStream inputStream = IOUtil.newInputStream(empty);
@@ -38,8 +52,7 @@ public class IOUtilTest
     }
 
     @Test
-    public void shouldReadWholeByteBuffer() throws Exception
-    {
+    public void shouldReadWholeByteBuffer() throws Exception {
         //given
         final ByteBuffer empty = ByteBuffer.wrap(new byte[SIZE]);
         final InputStream inputStream = IOUtil.newInputStream(empty);
@@ -52,8 +65,7 @@ public class IOUtilTest
     }
 
     @Test
-    public void shouldAllowReadingByteBufferInChunks() throws Exception
-    {
+    public void shouldAllowReadingByteBufferInChunks() throws Exception {
         //given
         final ByteBuffer buffer = ByteBuffer.wrap(new byte[SIZE]);
         final InputStream inputStream = IOUtil.newInputStream(buffer);
@@ -68,8 +80,7 @@ public class IOUtilTest
     }
 
     @Test
-    public void shouldReturnMinusOneWhenNothingRemainingInByteBuffer() throws Exception
-    {
+    public void shouldReturnMinusOneWhenNothingRemainingInByteBuffer() throws Exception {
         //given
         final int SIZE = 3;
         final ByteBuffer buffer = ByteBuffer.wrap(new byte[SIZE]);
@@ -84,8 +95,7 @@ public class IOUtilTest
     }
 
     @Test
-    public void shouldReturnMinusOneWhenEmptyByteBufferProvidedAndReadingSeveralBytes() throws Exception
-    {
+    public void shouldReturnMinusOneWhenEmptyByteBufferProvidedAndReadingSeveralBytes() throws Exception {
         //given
         final ByteBuffer empty = ByteBuffer.wrap(EMPTY_BYTE_ARRAY);
         final InputStream inputStream = IOUtil.newInputStream(empty);
@@ -98,43 +108,36 @@ public class IOUtilTest
     }
 
     @Test
-    public void shouldThrowWhenTryingToReadFullyFromEmptyByteBuffer() throws Exception
-    {
+    public void shouldThrowWhenTryingToReadFullyFromEmptyByteBuffer() throws Exception {
         //given
         final ByteBuffer empty = ByteBuffer.wrap(EMPTY_BYTE_ARRAY);
         final DataInputStream inputStream = new DataInputStream(IOUtil.newInputStream(empty));
 
-        try
-        {
+        try {
             //when
             inputStream.readFully(NON_EMPTY_BYTE_ARRAY);
             fail("EOFException expected");
         }
         //then
-        catch (EOFException e)
-        {
+        catch (EOFException e) {
         }
     }
 
     @Test
-    public void shouldThrowWhenByteBufferExhaustedAndTryingToReadFully() throws Exception
-    {
+    public void shouldThrowWhenByteBufferExhaustedAndTryingToReadFully() throws Exception {
         //given
         final ByteBuffer buffer = ByteBuffer.wrap(new byte[SIZE]);
         final DataInputStream inputStream = new DataInputStream(IOUtil.newInputStream(buffer));
         inputStream.readFully(new byte[SIZE]);
 
-        try
-        {
+        try {
             //when
             inputStream.readFully(NON_EMPTY_BYTE_ARRAY);
             fail("EOFException expected");
         }
         //then
-        catch (EOFException e)
-        {
+        catch (EOFException e) {
         }
     }
-
 
 }
