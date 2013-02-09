0a55bf7586d1c257f41ebd08567aa9b013a97dc2
Norman Ovenseri <novenseri@gmail.com>
2013-02-09 04:56:08 -0600

    Added test for flushing the map through TextCommandService.
    Renamed MemcacheFlushCommandTest. Removed unused values.

--- hazelcast/src/test/java/com/hazelcast/impl/ascii/memcache/FlushCommandTest.java
+++ hazelcast/src/test/java/com/hazelcast/impl/ascii/memcache/FlushCommandTest.java
@@ -7,10 +7,17 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.stubbing.OngoingStubbing;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.impl.FactoryImpl;
+import com.hazelcast.impl.FactoryImpl.HazelcastInstanceProxy;
 import com.hazelcast.impl.ascii.TextCommandConstants;
 import com.hazelcast.impl.ascii.TextCommandConstants.TextCommandType;
 import com.hazelcast.impl.ascii.TextCommandService;
@@ -18,16 +25,12 @@ import com.hazelcast.impl.ascii.TextCommandServiceImpl;
 import com.hazelcast.impl.ascii.rest.HttpCommand;
 import com.hazelcast.impl.ascii.rest.HttpGetCommand;
 import com.hazelcast.impl.ascii.rest.HttpGetCommandProcessor;
+import com.hazelcast.impl.base.FactoryAwareNamedProxy;
 
-public class MemcacheFlushCommandTest {
-	private static final String uri_map_prefix = "/hazelcast/rest/maps/";
-    private static final String uri_map_correct = uri_map_prefix + "testmap/testkey";
-
-    private static final String uri_queue_prefix = "/hazelcast/rest/queues/";
-
+public class FlushCommandTest {
     private final String TEST_MAP = "default";
     private final String TEST_KEY = "testKey";
-    private final String TEST_VALUE = "bs";
+    private final String TEST_VALUE = "test_value";
 
     private static FlushCommand expected_result;
     private static final TextCommandService mockTextCommandService = mock(TextCommandServiceImpl.class);
@@ -35,9 +38,9 @@ public class MemcacheFlushCommandTest {
 
     @BeforeClass
     public static void initialSetup() {
-        componentUnderTest = new FlushCommandProcessor(mockTextCommandService);
-        expected_result = new FlushCommand();
-        expected_result.setResponse(TextCommandConstants.OK);
+    	componentUnderTest = new FlushCommandProcessor(mockTextCommandService);
+    	expected_result = new FlushCommand();
+    	expected_result.setResponse(TextCommandConstants.OK);
     }
     
     @After
@@ -45,16 +48,40 @@ public class MemcacheFlushCommandTest {
         reset(mockTextCommandService);
         
     }
+    
+    @AfterClass
+    public static void init() throws Exception {
+        Hazelcast.shutdownAll();
+    }
 
     @Test
-    public void testFlush() {
+    public void testFlushCmd() {
     	mockTextCommandService.put(TEST_MAP, TEST_KEY, TEST_VALUE, 3600);
-    	OngoingStubbing<Object> yada = when(mockTextCommandService.get(TEST_MAP, TEST_KEY)).thenReturn(TEST_VALUE);
+    	when(mockTextCommandService.get(TEST_MAP, TEST_KEY)).thenReturn(TEST_VALUE);
         FlushCommand command = new FlushCommand();
         componentUnderTest.handle(command);
 
-        //Object blah = verify(mockTextCommandService).get(TEST_MAP, TEST_KEY); // This function fails... I really don't know how to use this
+        // Check if flush called
         assertEquals(expected_result.response, command.response);
+    }
+    
+    @Test
+    public void testFlushMap()
+    {
+    	// Create instance
+        HazelcastInstance defaultInst = Hazelcast.newHazelcastInstance(new Config());
+        
+        // Create new map
+        IMap<Object, Object> testMap = defaultInst.getMap(TEST_MAP);
+        testMap.put(TEST_KEY, TEST_VALUE);
+        
+        // Make sure value has been stored
+    	assertEquals(testMap.size(), 1);
+    	
+    	// Flush and test if empty
+    	TextCommandServiceImpl txtCmd = new TextCommandServiceImpl(((HazelcastInstanceProxy)defaultInst).getFactory().node);
+    	txtCmd.flush(TEST_MAP);
+    	assertEquals(testMap.size(), 0);
     }
 
 }
