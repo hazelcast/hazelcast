6a79591002e380f47554159c6ff6b9b9c0caf8c1
Fuad Malikov <fuad@hazelcast.com>
2013-02-13 16:47:26 -0800

    client router

--- hazelcast-client/src/main/java/com/hazelcast/client/ConnectionPool.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/ConnectionPool.java
@@ -17,40 +17,42 @@
 package com.hazelcast.client;
 
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.util.pool.ObjectPool;
+import com.hazelcast.client.util.pool.QueueBasedObjectPool;
 import com.hazelcast.core.*;
 import com.hazelcast.instance.MemberImpl;
 import com.hazelcast.nio.Address;
-import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.nio.serialization.SerializationService;
-import com.hazelcast.partition.MigrationEvent;
-import com.hazelcast.partition.MigrationListener;
 import com.hazelcast.partition.Partition;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConnectionPool {
     static private final int POOL_SIZE = 2;
-    private Map<Address, BlockingQueue<Connection>> mPool;
+    private Map<Address, ObjectPool<Connection>> mPool;
     private BlockingQueue<Connection> singlePool = new LinkedBlockingQueue<Connection>(POOL_SIZE);
     private final ConnectionManager connectionManager;
     private final SerializationService serializationService;
     public volatile Map<Integer, Member> partitionTable = new ConcurrentHashMap<Integer, Member>(271);
     public final AtomicInteger partitionCount = new AtomicInteger(0);
+    public volatile Router router;
 
     public ConnectionPool(ClientConfig config, final ConnectionManager connectionManager, final SerializationService serializationService) {
         this.connectionManager = connectionManager;
         this.serializationService = serializationService;
         initialConnection(config);
+
     }
 
     public void init(Cluster cluster, final PartitionService partitionService) {
+        router = new RandomRouter(cluster);
         addMembershipListener(cluster);
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -61,7 +63,7 @@ public class ConnectionPool {
         }, 0, 1000);
         partitionCount.set(partitionTable.size());
         Set<Member> members = cluster.getMembers();
-        mPool = new ConcurrentHashMap<Address, BlockingQueue<Connection>>(members.size());
+        mPool = new ConcurrentHashMap<Address, ObjectPool<Connection>>(members.size());
         for (Member _member : members) {
             createPoolForTheMember((MemberImpl) _member);
         }
@@ -74,7 +76,6 @@ public class ConnectionPool {
                 partitionTable.put(p.getPartitionId(), p.getOwner());
         }
     }
-
 
     private void addMembershipListener(final Cluster cluster) {
         cluster.addMembershipListener(new MembershipListener() {
@@ -113,12 +114,12 @@ public class ConnectionPool {
     private void createPoolForTheMember(MemberImpl member) {
         try {
             Address address = member.getAddress();
-            BlockingQueue<Connection> pool = new LinkedBlockingQueue<Connection>(POOL_SIZE);
+            ObjectPool<Connection> pool = new QueueBasedObjectPool<Connection>(POOL_SIZE);
             mPool.put(address, pool);
             while (pool.size() < POOL_SIZE) {
                 Connection connection = new Connection(address, 0, serializationService);
                 connectionManager.bindConnection(connection);
-                pool.offer(connection);
+                pool.add(connection);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -127,7 +128,7 @@ public class ConnectionPool {
 
     public Connection takeConnection(Member member) throws InterruptedException {
         if (member == null) {
-            return singlePool.take();
+            member = router.next();
         }
         return mPool.get(member.getInetSocketAddress()).take();
     }
@@ -137,6 +138,50 @@ public class ConnectionPool {
             singlePool.offer(connection);
             return;
         }
-        mPool.get(connection.getAddress()).offer(connection);
+        mPool.get(connection.getAddress()).release(connection);
+    }
+
+    interface Router {
+        public Member next();
+    }
+
+    class StaticRouter implements Router {
+        final Member member;
+
+        StaticRouter(Member member) {
+            this.member = member;
+        }
+
+        @Override
+        public Member next() {
+            return member;
+        }
+    }
+
+    class RandomRouter implements Router, MembershipListener {
+        List<Member> members = new CopyOnWriteArrayList<Member>();
+        AtomicInteger index = new AtomicInteger(0);
+        Random random = new Random(System.currentTimeMillis());
+
+        RandomRouter(Cluster cluster) {
+            cluster.addMembershipListener(this);
+            members.addAll(cluster.getMembers());
+        }
+
+        @Override
+        public Member next() {
+            int i = random.nextInt(members.size());
+            return members.get(i);
+        }
+
+        @Override
+        public void memberAdded(MembershipEvent membershipEvent) {
+            members.add(membershipEvent.getMember());
+        }
+
+        @Override
+        public void memberRemoved(MembershipEvent membershipEvent) {
+            members.remove(membershipEvent.getMember());
+        }
     }
 }
