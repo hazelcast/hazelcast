545e44282b7bd54aecadc688b1cbd7766a179e59
Ali Gurbuz <ali@hazelcast.com>
2013-02-19 15:05:02 +0200

    DynamicClusterTest

--- hazelcast/src/test/java/com/hazelcast/map/test/DynamicClusterTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/test/DynamicClusterTest.java
@@ -17,13 +17,16 @@
 package com.hazelcast.map.test;
 
 import com.hazelcast.core.IMap;
+import com.hazelcast.core.IQueue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 
 @RunWith(com.hazelcast.util.RandomBlockJUnit4ClassRunner.class)
@@ -75,16 +78,25 @@ public class DynamicClusterTest extends BaseTest {
 
         log("starting");
         final IMap map = getInstance(0).getMap("testMapSize");
-        final int putSize = 50*1000;
-        final int removeSize = putSize * 50;
+        final int putSize = 80*1000;
+        final int removeSize = putSize * 25;
         final AtomicInteger putCount = new AtomicInteger(putSize);
+        final AtomicBoolean putException = new AtomicBoolean(false);
+        final AtomicBoolean removeException = new AtomicBoolean(false);
 
         new Thread() {
 
             public void run() {
-                for (int i = 0; i < putSize; i++) {
-                    map.put("key" + i, "value" + i);
-                    putCount.decrementAndGet();
+                try {
+                    for (int i = 0; i < putSize; i++) {
+                        map.put("key" + i, "value" + i);
+                        putCount.decrementAndGet();
+                    }
+                }
+                catch (Exception e){
+                    putException.set(true);
+                    log("exexex");
+                    log(e.getMessage());
                 }
             }
 
@@ -95,13 +107,20 @@ public class DynamicClusterTest extends BaseTest {
         new Thread() {
 
             public void run() {
-                for (int i = 0; i < removeSize; i++) {
-                    Random ran = new Random(System.currentTimeMillis());
-                    Object o = map.remove("key" + ran.nextInt(putSize));
-                    if (o != null) {
-                        removed.incrementAndGet();
+                try {
+                    for (int i = 0; i < removeSize; i++) {
+                        Random ran = new Random(System.currentTimeMillis());
+                        Object o = map.remove("key" + ran.nextInt(putSize));
+                        if (o != null) {
+                            removed.incrementAndGet();
+                        }
+                        removeCount.decrementAndGet();
                     }
-                    removeCount.decrementAndGet();
+                }
+                catch (Exception e){
+                    removeException.set(true);
+                    log("exexex");
+                    log(e.getMessage());
                 }
             }
 
@@ -122,9 +141,11 @@ public class DynamicClusterTest extends BaseTest {
 
         }
 
-        while (putCount.get() != 0 && removeCount.get() != 0){
+        while (putCount.get() != 0 || removeCount.get() != 0){
             Thread.sleep(1000);
             log("putCount: " + putCount.get() + "  removeCount: " + removeCount.get());
+            assertFalse(putException.get());
+            assertFalse(removeException.get());
         }
 
         assertEquals(putSize - removed.get(), map.size());
@@ -136,6 +157,70 @@ public class DynamicClusterTest extends BaseTest {
 
     @Test
     public void testQueueSize() throws Exception {
+        final IQueue queue = getInstance(0).getQueue("testQueueSize"+rand.nextInt(100));
+        final int pollSize = 150*1000;
+        final int offerSize = 220*1000;
+
+        final AtomicInteger offerCount = new AtomicInteger(offerSize);
+        final AtomicInteger pollCount = new AtomicInteger(pollSize);
+        final AtomicInteger polled = new AtomicInteger();
+        final AtomicInteger offered = new AtomicInteger();
+
+        new Thread(){
+            public void run() {
+                for (int i=0; i<offerSize; i++){
+                    try {
+                        if(queue.offer("item"+i)){
+                            offered.incrementAndGet();
+                        }
+                    }
+                    catch (Exception e){
+                        log("exexex");
+                        log(e.getMessage());
+                    }
+                    offerCount.decrementAndGet();
+                }
+            }
+        }.start();
+
+        new Thread(){
+            public void run() {
+                for (int i=0; i<pollSize; i++){
+                    try {
+                        Object o = queue.poll();
+                        if (o != null){
+                            polled.incrementAndGet();
+                        }
+                    }
+                    catch (Exception e){
+                        log("exexex");
+                        log(e.getMessage());
+                    }
+                    pollCount.decrementAndGet();
+                }
+            }
+        }.start();
+
+        Thread.sleep(500);
+
+        for (int i=0; i<4; i++){
+            log("remove instance");
+            removeInstance(1);
+            Thread.sleep(2000);
+
+            log("new instance");
+            newInstance();
+            Thread.sleep(1000);
+            log("offerCount: " + offerCount.get() + "  pollCount: " + pollCount.get());
+            Thread.sleep(1000);
+        }
+
+        while (offerCount.get() != 0 || pollCount.get() != 0){
+            log("offerCount: " + offerCount.get() + "  pollCount: " + pollCount.get());
+            Thread.sleep(1000);
+        }
+        assertEquals(offered.get()-polled.get(), queue.size());
+        log("offered: " + offered.get() + "  polled: " + polled.get());
 
     }
 
