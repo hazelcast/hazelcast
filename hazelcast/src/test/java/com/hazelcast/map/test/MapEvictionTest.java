3b604528213f377e760d311389adcd132279c947
sancar <sancar@hazelcast.com>
2013-02-19 15:43:01 +0200

    map eviction speed test

--- hazelcast/src/test/java/com/hazelcast/map/test/MapEvictionTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/test/MapEvictionTest.java
@@ -22,9 +22,12 @@ import com.hazelcast.config.MaxSizeConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.hazelcast.instance.StaticNodeFactory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -32,6 +35,85 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(com.hazelcast.util.RandomBlockJUnit4ClassRunner.class)
 public class MapEvictionTest {
+
+    //TODO eviction can not keep up with puts
+    //P.S. when there is 1 milliseconds between puts, test does not fail
+    @Test
+    public void testEvictionSpeedTest() {
+        final int k = 3;
+        final int size = 1700;
+        final CountDownLatch latch = new CountDownLatch(k);
+        final  String mapName = "testEvictionSpeedTest";
+        Config cfg = new Config();
+        MapConfig mc = cfg.getMapConfig(mapName);
+        mc.setEvictionPolicy(MapConfig.EvictionPolicy.LRU);
+        mc.setEvictionPercentage(25);
+        MaxSizeConfig msc = new MaxSizeConfig();
+        msc.setMaxSizePolicy(MaxSizeConfig.MaxSizePolicy.PER_JVM);
+        msc.setSize(size);
+        mc.setMaxSizeConfig(msc);
+
+//        final HazelcastInstance[] instances = StaticNodeFactory.newInstances(cfg, k);
+        Hazelcast.shutdownAll();
+        final HazelcastInstance[] instances = new HazelcastInstance[k];
+        instances[0] = Hazelcast.newHazelcastInstance(cfg);
+        instances[1] = Hazelcast.newHazelcastInstance(cfg);
+        instances[2] = Hazelcast.newHazelcastInstance(cfg);
+
+        new Thread() {
+            final IMap map = instances[0].getMap(mapName);
+
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                while (latch.getCount() != 0) {
+                    try {
+                        System.out.println("Checking");
+                        System.out.println("Actual Size " + map.size());
+                        System.out.println("Max Eviction Size " + size * k);
+                        System.out.println("Fault tolerant Size " + (size * k + size * k * 20 / 100));
+                        assertTrue(map.size() <= (size * k + size * k * 20 / 100));
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        }.start();
+
+        for (int i = 0; i < k; i++) {
+            final IMap map = instances[i].getMap(mapName);
+            new Thread() {
+                public void run() {
+                    for (int j = 0; j < 1000000; j++)   {
+                        map.put(j + k * 1000000, j);
+//                        try {
+//                            Thread.sleep(1);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                    }
+                    System.out.println("done");
+                    latch.countDown();
+                }
+            }.start();
+        }
+
+
+        try {
+            Assert.assertEquals(latch.await(10, TimeUnit.MINUTES), true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+
+
+    }
 
     @Test
     public void testMapWideEviction() throws InterruptedException {
@@ -58,7 +140,6 @@ public class MapEvictionTest {
         Hazelcast.shutdownAll();
     }
 
-
     @Test
     public void testMapRecordEviction() throws InterruptedException {
         Config cfg = new Config();
@@ -74,7 +155,7 @@ public class MapEvictionTest {
         }
         int nsize = 20;
         for (int i = 0; i < nsize; i++) {
-            map.put(size+i, size+i, 1000, TimeUnit.SECONDS);
+            map.put(size + i, size + i, 1000, TimeUnit.SECONDS);
         }
         for (int i = 0; i < nsize; i++) {
             map.put(i, i, 1, TimeUnit.SECONDS);
@@ -108,7 +189,6 @@ public class MapEvictionTest {
         assertEquals(nsize, map.size());
         Hazelcast.shutdownAll();
     }
-
 
 
 }
