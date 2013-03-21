7397c243feb6ed95c63f74e06b2911505f7e9a75
Mehmet Dogan <mehmet@hazelcast.com>
2013-03-21 17:16:58 +0200

    Updated simple map test.

--- hazelcast/src/main/java/com/hazelcast/examples/SimpleMapTest.java
+++ hazelcast/src/main/java/com/hazelcast/examples/SimpleMapTest.java
@@ -122,12 +122,16 @@ public class SimpleMapTest {
 
     private void run(ExecutorService es) {
         final IMap<String, Object> map = instance.getMap(NAMESPACE);
+        final int entryPerThread = entryCount / threadCount + 1;
+
         for (int i = 0; i < threadCount; i++) {
+            final int finalI = i;
             es.execute(new Runnable() {
                 public void run() {
                     try {
+                        final int offset = finalI * entryPerThread;
                         while (true) {
-                            int key = (int) (Math.random() * entryCount);
+                            int key = (int) (Math.random() * entryPerThread) + offset;
                             int operation = ((int) (Math.random() * 100));
                             if (operation < getPercentage) {
                                 map.get(String.valueOf(key));
