bea0c3ea4c2f08a6d67bcf88b2bd02d6f14397b7
enesakar <enesakar@gmail.com>
2013-05-29 14:41:28 +0300

    map test fix

--- hazelcast/src/test/java/com/hazelcast/map/MapStoreTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/MapStoreTest.java
@@ -46,7 +46,7 @@ public class MapStoreTest extends ParallelTestSupport {
     @Test
     @Ignore("TODO: fix test!")
     public void testMapInitialLoad() throws InterruptedException {
-        int size = 10000;
+        int size = 100000;
         StaticNodeFactory nodeFactory = createNodeFactory(3);
 
         Config cfg = new Config();
@@ -59,6 +59,10 @@ public class MapStoreTest extends ParallelTestSupport {
         HazelcastInstance instance2 = nodeFactory.newHazelcastInstance(cfg);
         IMap map = instance1.getMap("testMapInitialLoad");
 
+        for (int i = 0; i < 100; i++) {
+            System.out.println(map.size());
+            Thread.sleep(1000);
+        }
         assertEquals(size, map.size());
 
         for (int i = 0; i < size; i++) {
@@ -263,7 +267,6 @@ public class MapStoreTest extends ParallelTestSupport {
     }
 
     @Test
-//    @Ignore("TODO: fix test!")
     public void testOneMemberWriteBehindWithEvictions() throws Exception {
         TestEventBasedMapStore testMapStore = new TestEventBasedMapStore();
         Config config = newConfig(testMapStore, 2);
