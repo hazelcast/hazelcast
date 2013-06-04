c3953a3e83156f74cc5469ae096e12e3acd1d34c
Mehmet Dogan <mehmet@hazelcast.com>
2013-06-04 16:46:08 +0300

    Fix MapTransactionTest.

--- hazelcast/src/test/java/com/hazelcast/map/MapTransactionTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/MapTransactionTest.java
@@ -17,8 +17,8 @@
 package com.hazelcast.map;
 
 import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceNotActiveException;
 import com.hazelcast.core.IMap;
 import com.hazelcast.core.TransactionalMap;
 import com.hazelcast.test.HazelcastJUnit4ClassRunner;
@@ -29,8 +29,6 @@ import com.hazelcast.transaction.TransactionException;
 import com.hazelcast.transaction.TransactionOptions;
 import com.hazelcast.transaction.TransactionalTask;
 import com.hazelcast.transaction.TransactionalTaskContext;
-import org.junit.After;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -38,22 +36,13 @@ import org.junit.runner.RunWith;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.*;
 
 @RunWith(HazelcastJUnit4ClassRunner.class)
 @Category(ParallelTest.class)
 public class MapTransactionTest extends HazelcastTestSupport {
-
-    @BeforeClass
-    public static void init() {
-        Hazelcast.shutdownAll();
-    }
-
-    @After
-    public void cleanUp() {
-        Hazelcast.shutdownAll();
-    }
 
     @Test
     public void testCommitOrder() throws TransactionException {
@@ -76,7 +65,7 @@ public class MapTransactionTest extends HazelcastTestSupport {
                 return true;
             }
         });
-        assertEquals("value6",h4.getMap("default").get("1"));
+        assertEquals("value6", h4.getMap("default").get("1"));
     }
 
     @Test
@@ -156,7 +145,7 @@ public class MapTransactionTest extends HazelcastTestSupport {
     }
 
     @Test
-    public void testTxnOwnerDies() throws TransactionException {
+    public void testTxnOwnerDies() throws TransactionException, InterruptedException {
         Config config = new Config();
         final TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(3);
         final HazelcastInstance h1 = factory.newHazelcastInstance(config);
@@ -164,6 +153,7 @@ public class MapTransactionTest extends HazelcastTestSupport {
         final HazelcastInstance h3 = factory.newHazelcastInstance(config);
         final IMap map1 = h1.getMap("default");
         final int size = 50;
+        final AtomicBoolean result = new AtomicBoolean(false);
 
         Runnable runnable = new Runnable() {
             public void run() {
@@ -175,7 +165,6 @@ public class MapTransactionTest extends HazelcastTestSupport {
                                 txMap.put(i, i);
                                 try {
                                     Thread.sleep(100);
-                                    System.out.println("turn:" + i);
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
@@ -183,22 +172,23 @@ public class MapTransactionTest extends HazelcastTestSupport {
                             return true;
                         }
                     });
-                    fail();
-                } catch (TransactionException e) {
+                    result.set(b);
+                } catch (HazelcastInstanceNotActiveException ignored) {
+                } catch (TransactionException ignored) {
                 }
             }
         };
-        try {
-            Thread thread = new Thread(runnable);
-            thread.start();
-            Thread.sleep(200);
-            h1.getLifecycleService().shutdown();
-            thread.join();
-            final IMap map2 = h2.getMap("default");
-            for (int i = 0; i < size; i++) {
-                assertNull(map2.get(i));
-            }
-        } catch (InterruptedException e) {
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+        Thread.sleep(200);
+        h1.getLifecycleService().shutdown();
+        thread.join();
+
+        assertFalse(result.get());
+        final IMap map2 = h2.getMap("default");
+        for (int i = 0; i < size; i++) {
+            assertNull(map2.get(i));
         }
     }
 
@@ -382,7 +372,7 @@ public class MapTransactionTest extends HazelcastTestSupport {
         final HazelcastInstance h2 = factory.newHazelcastInstance(config);
         final IMap map2 = h2.getMap("default");
 
-        boolean b = h1.executeTransaction(new TransactionOptions().setTimeout(1, TimeUnit.SECONDS), new TransactionalTask<Boolean>() {
+        boolean b = h1.executeTransaction(new TransactionalTask<Boolean>() {
             public Boolean execute(TransactionalTaskContext context) throws TransactionException {
                 final TransactionalMap<Object, Object> txMap = context.getMap("default");
                 assertNull(txMap.replace("1", "value"));
