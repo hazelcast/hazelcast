85297486390513dfe76018c088dce76324376932
alarmnummer <alarmnummer@gmail.com>
2013-08-08 12:16:40 +0300

    SemaphoreConfig: improved javadoc + validations

---
+++ hazelcast/src/main/java/com/hazelcast/Main.java
@@ -0,0 +1,11 @@
+package com.hazelcast;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alarmnummer
+ * Date: 8/8/13
+ * Time: 12:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
