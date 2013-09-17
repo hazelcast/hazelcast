050d80b31e009dd2274a3a2fefe5ff86d5d7d9e4
alarmnummer <alarmnummer@gmail.com>
2013-09-17 17:31:05 +0300

    Add support for serializting enums

---
+++ hazelcast/src/test/java/com/hazelcast/nio/serialization/EnumTest.java
@@ -0,0 +1,11 @@
+package com.hazelcast.nio.serialization;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alarmnummer
+ * Date: 9/17/13
+ * Time: 5:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnumTest {
+}
