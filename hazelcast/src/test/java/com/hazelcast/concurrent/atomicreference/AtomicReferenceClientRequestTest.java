b20df7b1677e00431ceddb7e90a0e3615a3e9914
Mehmet Dogan <mehmet@hazelcast.com>
2013-11-25 14:02:55 +0200

    Updated copyright headers.

--- hazelcast/src/test/java/com/hazelcast/concurrent/atomicreference/AtomicReferenceClientRequestTest.java
+++ hazelcast/src/test/java/com/hazelcast/concurrent/atomicreference/AtomicReferenceClientRequestTest.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright (c) 2008-2013, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.concurrent.atomicreference;
 
 import com.hazelcast.client.ClientTestSupport;
 import com.hazelcast.client.SimpleClient;
 import com.hazelcast.concurrent.atomicreference.client.*;
 import com.hazelcast.config.Config;
-import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IAtomicReference;
 import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.test.HazelcastJUnit4ClassRunner;
@@ -14,7 +29,8 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(HazelcastJUnit4ClassRunner.class)
 @Category(ParallelTest.class)
