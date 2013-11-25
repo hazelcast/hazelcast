f3e11492c76de73f5e00d73ea574499991c34aaf
alarmnummer <alarmnummer@gmail.com>
2013-11-25 19:41:06 +0200

    CompletionFuture: the andThen can be configured with an executor

--- hazelcast/src/main/java/com/hazelcast/core/CompletionFuture.java
+++ hazelcast/src/main/java/com/hazelcast/core/CompletionFuture.java
@@ -1,7 +1,9 @@
 package com.hazelcast.core;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
 public interface CompletionFuture<V> extends Future<V> {
     void andThen(ExecutionCallback<V> callback);
+    void andThen(ExecutionCallback<V> callback, Executor executor);
 }
