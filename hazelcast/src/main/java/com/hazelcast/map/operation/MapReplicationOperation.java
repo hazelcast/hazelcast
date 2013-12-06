09cce2bc0e34463091c482049914f37399b37204
enesakar <enesakar@gmail.com>
2013-12-06 16:33:37 +0200

    revised fix about initial load

--- hazelcast/src/main/java/com/hazelcast/map/operation/MapReplicationOperation.java
+++ hazelcast/src/main/java/com/hazelcast/map/operation/MapReplicationOperation.java
@@ -65,10 +65,10 @@ public class MapReplicationOperation extends AbstractOperation {
             String name = entry.getKey();
             // adding if initial data is loaded for the only maps that has mapstore behind
             if(mapContainer.getStore() != null) {
-                mapInitialLoadInfo.put(name, recordStore.isLoaded());
+                mapInitialLoadInfo.put(name, replicaIndex > 0 || recordStore.isLoaded());
             }
             // now prepare data to migrate records
-            Set<RecordReplicationInfo> recordSet = new HashSet<RecordReplicationInfo>(recordStore.size());
+            Set<RecordReplicationInfo> recordSet = new HashSet<RecordReplicationInfo>();
             for (Entry<Data, Record> recordEntry : recordStore.getReadonlyRecordMap().entrySet()) {
                 Data key = recordEntry.getKey();
                 Record record = recordEntry.getValue();
