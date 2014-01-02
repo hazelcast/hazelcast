3b699316a29b170b6051d905d38b143bd316e267
Ali Gurbuz <ali@hazelcast.com>
2014-01-03 00:28:58 +0200

    fixes ticket174, spring map.put overrides ttl from conf

--- hazelcast-spring/src/test/java/com/hazelcast/spring/cache/TestCacheManager.java
+++ hazelcast-spring/src/test/java/com/hazelcast/spring/cache/TestCacheManager.java
@@ -65,6 +65,20 @@ public class TestCacheManager {
         }
     }
 
+    @Test
+    public void testTTL() throws InterruptedException {
+        final String name = bean.getNameWithTTL();
+        Assert.assertEquals("ali", name);
+        final String nameFromCache = bean.getNameWithTTL();
+        Assert.assertEquals("ali", nameFromCache);
+
+        Thread.sleep(3000);
+
+        final String nameFromCacheAfterTTL = bean.getNameWithTTL();
+        Assert.assertNull(nameFromCacheAfterTTL);
+
+    }
+
 
     public static class DummyBean implements IDummyBean {
 
@@ -88,6 +102,16 @@ public class TestCacheManager {
                 return null;
             }
             Assert.fail("should not call this method!");
+            return null;
+        }
+
+        final AtomicBoolean firstCall = new AtomicBoolean(false);
+
+        @Cacheable("map-with-ttl")
+        public String getNameWithTTL(){
+            if (firstCall.compareAndSet(false, true)) {
+                return "ali";
+            }
             return null;
         }
     }
