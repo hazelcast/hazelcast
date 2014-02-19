d68a5e6d733127927ec6d0a099c5c8354b43547a
Kenny MacLeod <kmacleod@atlassian.com>
2014-02-19 12:32:40 +1100

    #1795 Use ArrayList instead of HashSet (cherry-picked from 3.x branch)

--- hazelcast/src/main/java/com/hazelcast/map/DefaultRecordStore.java
+++ hazelcast/src/main/java/com/hazelcast/map/DefaultRecordStore.java
@@ -431,7 +431,7 @@ public class DefaultRecordStore implements RecordStore {
         keysToDelete.removeAll(lockedRecords.keySet());
 
         final MapStoreWrapper store = mapContainer.getStore();
-        Set<Object> keysObject = new HashSet<Object>(keysToDelete.size());
+        Collection<Object> keysObject = new ArrayList<Object>();
         for (Data key : keysToDelete) {
             // todo ea have a clear(Keys) method for optimizations
             removeIndex(key);
