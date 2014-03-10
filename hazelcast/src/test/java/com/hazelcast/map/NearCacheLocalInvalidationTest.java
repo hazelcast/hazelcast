2cff3beb34158546382bc232176010f2bf0aa10e
ahmetmircik <ahmet@hazelcast.com>
2014-03-10 12:31:55 +0200

    fixes issue #1951

--- hazelcast/src/test/java/com/hazelcast/map/NearCacheLocalInvalidationTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/NearCacheLocalInvalidationTest.java
@@ -1,6 +1,10 @@
 package com.hazelcast.map;
 
-import com.hazelcast.config.*;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NearCacheConfig;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.test.HazelcastParallelClassRunner;
@@ -13,11 +17,18 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 @RunWith(HazelcastParallelClassRunner.class)
 @Category(QuickTest.class)
@@ -29,7 +40,7 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
 
     private static final TimeUnit timeunit = TimeUnit.MILLISECONDS;
 
-    private static final String mapName = randomString();
+    private static final String mapName = NearCacheLocalInvalidationTest.class.getCanonicalName();
 
     private HazelcastInstance hcInstance;
 
@@ -68,7 +79,7 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
             String value3 = map.get(key); // here we _might_ still see the value
 
             assertNull(value0);
-            assertEquals(value,value1);
+            assertEquals(value, value1);
             assertEquals(value, value2);
             assertNull(value3);
 
@@ -89,7 +100,7 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
             String value3 = map.get(key); // here we _might_ still see the value
             // assert
             assertNull(value0);
-            assertEquals(value,value1);
+            assertEquals(value, value1);
             assertNull(value3);
         }
     }
@@ -253,7 +264,7 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
     public void testEvict() {
         final IMap<String, String> map = hcInstance.getMap(mapName);
         for (int k = 0; k < numIterations; k++) {
-            String key = "remove_" + String.valueOf(k);
+            String key = "evict_" + String.valueOf(k);
             String value = "merhaba-" + key;
             // test
             String value0 = map.put(key, value);
@@ -271,7 +282,7 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
     public void testSet() {
         final IMap<String, String> map = hcInstance.getMap(mapName);
         for (int k = 0; k < numIterations; k++) {
-            String key = "put_" + String.valueOf(k);
+            String key = "set_" + String.valueOf(k);
             String value = "merhaba-" + key;
 
             // test
@@ -290,7 +301,7 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
     public void testReplace() {
         final IMap<String, String> map = hcInstance.getMap(mapName);
         for (int k = 0; k < numIterations; k++) {
-            String key = "put_" + String.valueOf(k);
+            String key = "replace_" + String.valueOf(k);
             String value = "merhaba-" + key;
             String valueNew = "merhaba-new" + key;
 
@@ -302,6 +313,110 @@ public class NearCacheLocalInvalidationTest extends HazelcastTestSupport {
             // assert
             assertNotNull(value0);
             assertEquals(valueNew, value2);
+        }
+    }
+
+    @Test
+    public void testExecuteOnKey() {
+        final IMap<String, Text> map = hcInstance.getMap(mapName);
+        // loop over several keys to make sure we have keys on both instances
+        for (int k = 0; k < numIterations; k++) {
+            String key = "executeOnKey_" + String.valueOf(k);
+            Text value0 = new Text("content" + k);
+            // put and get to make sure entry exists *and* is in local near cache
+            map.put(key, value0);
+            map.get(key);
+            Text value1 = map.get(key);
+            Text value2 = map.get(key);
+            assertTrue("reference in local near cache must be the same (k=" + k + ")" + value1 + " =? " + value2, value1 == value2);
+            // execute reading entry processor (no invalidation required)
+            final HashSet<String> strings = new HashSet<String>();
+            strings.add(key);
+            map.executeOnKey(key, new ReadingEntryProcessor());
+            // execute writing entry processor (this must locally invalidate near cache)
+            map.executeOnKey(key, new WritingEntryProcessor());
+            Text value4 = map.get(key);
+            assertFalse("reference must change by writing (k=" + k + ")", value1 == value4);
+        }
+    }
+
+    @Test
+    public void testExecuteOnKeys() {
+        final IMap<String, Text> map = hcInstance.getMap(mapName);
+        // loop over several keys to make sure we have keys on both instances
+        for (int k = 0; k < numIterations; k++) {
+            String key = "executeOnKeys_" + String.valueOf(k);
+            Text value0 = new Text("content" + k);
+            // put and get to make sure entry exists *and* is in local near cache
+            map.put(key, value0);
+            map.get(key);
+            Text value1 = map.get(key);
+            Text value2 = map.get(key);
+            assertTrue("reference in local near cache must be the same (k=" + k + ")" + value1 + " =? " + value2,
+                    value1 == value2);
+            // execute reading entry processor (no invalidation required)
+            final HashSet<String> keys = new HashSet<String>();
+            keys.add(key);
+            map.executeOnKeys(keys, new ReadingEntryProcessor());
+            // execute writing entry processor (this must locally invalidate near cache)
+            map.executeOnKeys(keys, new WritingEntryProcessor());
+//            map.executeOnKeys(strings, new WritingEntryProcessor());
+            Text value4 = map.get(key);
+            assertFalse("reference must change by writing (k=" + k + ")", value1 == value4);
+        }
+    }
+
+    /**
+     * An entry processor which only reads.
+     */
+    public static class ReadingEntryProcessor extends AbstractEntryProcessor<String, Text> {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public Object process(Map.Entry<String, Text> entry) {
+            return "read";
+        }
+
+    }
+
+    /**
+     * An entry processor which writes (changes the value and calls Entry.setValue()).
+     */
+    public static class WritingEntryProcessor extends AbstractEntryProcessor<String, Text> {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public Object process(Map.Entry<String, Text> entry) {
+            Text text = entry.getValue();
+            text.setContent("new content");
+            entry.setValue(text);
+            return "written";
+        }
+    }
+
+    public static class Text implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private String content = "original content";
+
+        public Text(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return getContent();
         }
     }
 
