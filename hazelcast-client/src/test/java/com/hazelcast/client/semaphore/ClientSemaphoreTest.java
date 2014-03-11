7da8d6db065ff58c4ea02f52ed196f48f92b101b
danny <danny@hazelcast.com>
2014-03-11 16:39:51 +0200

    concurrent semaphore test

--- hazelcast-client/src/test/java/com/hazelcast/client/semaphore/ClientSemaphoreTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/semaphore/ClientSemaphoreTest.java
@@ -19,18 +19,20 @@ package com.hazelcast.client.semaphore;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.*;
 import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.*;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static com.hazelcast.test.HazelcastTestSupport.randomString;
+import static org.junit.Assert.*;
 
 /**
  * @author ali 5/24/13
@@ -40,7 +42,7 @@ import static org.junit.Assert.assertTrue;
 public class ClientSemaphoreTest {
 
     static final String name = "test1";
-    static HazelcastInstance hz;
+    static HazelcastInstance client;
     static HazelcastInstance server;
     static HazelcastInstance second;
     static ISemaphore s;
@@ -48,13 +50,13 @@ public class ClientSemaphoreTest {
     @BeforeClass
     public static void init(){
         server = Hazelcast.newHazelcastInstance();
-        hz = HazelcastClient.newHazelcastClient(null);
-        s = hz.getSemaphore(name);
+        client = HazelcastClient.newHazelcastClient(null);
+        s = client.getSemaphore(name);
     }
 
     @AfterClass
     public static void destroy() {
-        hz.shutdown();
+        client.shutdown();
         Hazelcast.shutdownAll();
     }
 
@@ -115,5 +117,107 @@ public class ClientSemaphoreTest {
         assertTrue(latch.await(10, TimeUnit.SECONDS));
         assertEquals(0, s.availablePermits());
 
+    }
+
+
+    @Test
+    public void trySemaphoreTest() {
+        trySemaphoreTest(false);
+    }
+
+    @Test
+    public void trySemaphoreWithTimeOutTest() {
+        trySemaphoreTest(true);
+    }
+
+    public void trySemaphoreTest(boolean tryWithTimeOut) {
+        final ISemaphore semaphore = client.getSemaphore(randomString());
+        semaphore.init(1);
+
+        final AtomicInteger upTotal = new AtomicInteger(0);
+        final AtomicInteger downTotal = new AtomicInteger(0);
+
+        SemaphoreTestThread threads[] = new SemaphoreTestThread[8];
+        for(int i=0; i<threads.length; i++){
+
+            SemaphoreTestThread t;
+
+            if(tryWithTimeOut){
+                t = new TrySemaphoreTimeOutThread(semaphore, upTotal, downTotal);
+            }
+            else{
+                t = new TrySemaphoreThread(semaphore, upTotal, downTotal);
+            }
+
+            t.start();
+            threads[i] = t;
+        }
+        HazelcastTestSupport.assertJoinable(threads);
+
+        for(SemaphoreTestThread t : threads){
+            assertNull("thread "+ t +" has error "+t.error, t.error);
+        }
+
+        assertTrue("concurrent access to locked code caused wrong total", upTotal.get() + downTotal.get() == 0);
+    }
+
+    static abstract class SemaphoreTestThread extends Thread{
+        static private final Random random = new Random();
+        protected ISemaphore semaphore ;
+        protected AtomicInteger upTotal;
+        protected AtomicInteger downTotal;
+        public Throwable error =null;
+
+        public SemaphoreTestThread(ISemaphore semaphore, AtomicInteger upTotal, AtomicInteger downTotal){
+            this.semaphore = semaphore;
+            this.upTotal = upTotal;
+            this.downTotal = downTotal;
+        }
+
+        public void run(){
+            try{
+                doRun();
+            }catch (Throwable e){
+                error = e;
+            }
+        }
+
+        abstract void doRun() throws Exception;
+
+        protected void work(){
+            int delta = random.nextInt(1000);
+            upTotal.addAndGet(delta);
+            downTotal.addAndGet(-delta);
+        }
+    }
+
+    static class TrySemaphoreThread extends SemaphoreTestThread{
+        public TrySemaphoreThread(ISemaphore semaphore, AtomicInteger upTotal, AtomicInteger downTotal){
+            super(semaphore, upTotal, downTotal);
+        }
+
+        public void doRun() throws Exception{
+            for ( int i=0; i<1000*10; i++ ) {
+                if(semaphore.tryAcquire()){
+                    work();
+                    semaphore.release();
+                }
+            }
+        }
+    }
+
+    static class TrySemaphoreTimeOutThread extends SemaphoreTestThread{
+        public TrySemaphoreTimeOutThread(ISemaphore semaphore, AtomicInteger upTotal, AtomicInteger downTotal){
+            super(semaphore, upTotal, downTotal);
+        }
+
+        public void doRun() throws Exception{
+            for ( int i=0; i<1000*10; i++ ) {
+                if(semaphore.tryAcquire(10, TimeUnit.MILLISECONDS )){
+                    work();
+                    semaphore.release();
+                }
+            }
+        }
     }
 }
