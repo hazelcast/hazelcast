c202c2aaf87e6f61acbfce96af5cb4076687cf7d
danny <danny@hazelcast.com>
2014-03-27 14:24:27 +0200

    auto merge able

--- hazelcast-client/src/test/java/com/hazelcast/client/queue/ClientQueueTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/queue/ClientQueueTest.java
@@ -31,7 +31,7 @@ import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import static com.hazelcast.test.HazelcastTestSupport.assertTrueEventually;
+import static com.hazelcast.test.HazelcastTestSupport.interruptCurrentThread;
 import static com.hazelcast.test.HazelcastTestSupport.randomString;
 import static org.junit.Assert.*;
 
