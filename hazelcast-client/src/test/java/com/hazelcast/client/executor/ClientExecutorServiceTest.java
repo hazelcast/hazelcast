ed50d227ea4730a1028d1cfd8f08f5841c494733
danny <danny@hazelcast.com>
2014-04-02 10:04:00 +0300

    auto merge able

--- hazelcast-client/src/test/java/com/hazelcast/client/executor/ClientExecutorServiceTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/executor/ClientExecutorServiceTest.java
@@ -100,7 +100,7 @@ public class ClientExecutorServiceTest {
     public void testAwaitTermination() throws InterruptedException, ExecutionException, TimeoutException {
         final IExecutorService service = client.getExecutorService(randomString());
         final boolean result = service.awaitTermination(9999, TimeUnit.DAYS);
-        fail("marked problematic at the method awaitTermination really just returns false and in fact dose nothing !");
+        fail("marked problematic at the method awaitTermination really just returns false and in fact dose nothing !!");
     }
 
     @Test(expected = TimeoutException.class)
