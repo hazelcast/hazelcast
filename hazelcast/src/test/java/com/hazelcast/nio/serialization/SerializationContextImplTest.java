96dd24db390cf25ef944a7a96213ae87e91b622c
Peter Veentjer <alarmnummer@gmail.com>
2014-04-10 08:37:42 +0300

    Minor testing cleanup

--- hazelcast/src/test/java/com/hazelcast/nio/serialization/SerializationContextImplTest.java
+++ hazelcast/src/test/java/com/hazelcast/nio/serialization/SerializationContextImplTest.java
@@ -1,18 +1,24 @@
 package com.hazelcast.nio.serialization;
 
+import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(HazelcastSerialClassRunner.class)
+@Category(QuickTest.class)
 public class SerializationContextImplTest extends HazelcastTestSupport {
 
     @Test
     public void testCompression() throws IOException {
-        String s = generateString(100000);
+        String s = HazelcastTestSupport.generateRandomString(100000);
 
         byte[] bytes = s.getBytes();
         ByteArrayObjectDataOutput compressedDataOutput = new ByteArrayObjectDataOutput(s.length() * 5, null);
@@ -25,13 +31,4 @@ public class SerializationContextImplTest extends HazelcastTestSupport {
         assertEquals(s, found);
     }
 
-    private String generateString(int i) {
-        StringBuffer sb = new StringBuffer();
-        Random random = new Random();
-        for (int k = 0; k < i; k++) {
-            char c = (char) (random.nextInt(26) + 'a');
-            sb.append(c);
-        }
-        return sb.toString();
-    }
 }
