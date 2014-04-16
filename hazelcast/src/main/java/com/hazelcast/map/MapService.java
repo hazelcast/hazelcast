adb55e4806f0f4df52f9fff937ca9785b79228bb
Emin Demirci <emindemirci@gmail.com>
2014-04-16 16:34:30 +0300

    fix overflow on integer multiplication when scheduling TTL eviction

--- hazelcast/src/main/java/com/hazelcast/map/MapService.java
+++ hazelcast/src/main/java/com/hazelcast/map/MapService.java
@@ -412,12 +412,12 @@ public class MapService implements ManagedService, MigrationAwareService,
         if (shouldSchedule) {
             // if ttl is 0 then no eviction. if ttl is -1 then default configured eviction is applied
             if (ttl < 0 && mapContainer.getMapConfig().getTimeToLiveSeconds() > 0) {
-                scheduleTtlEviction(name, record, mapContainer.getMapConfig().getTimeToLiveSeconds() * 1000);
+                scheduleTtlEviction(name, record, mapContainer.getMapConfig().getTimeToLiveSeconds() * 1000L);
             } else if (ttl > 0) {
                 scheduleTtlEviction(name, record, ttl);
             }
             if (mapContainer.getMapConfig().getMaxIdleSeconds() > 0) {
-                scheduleIdleEviction(name, dataKey, mapContainer.getMapConfig().getMaxIdleSeconds() * 1000);
+                scheduleIdleEviction(name, dataKey, mapContainer.getMapConfig().getMaxIdleSeconds() * 1000L);
             }
         }
         return record;
