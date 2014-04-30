b5c4f999a17bd8c5822721d1d69897c9557c7038
Mesut Celik <mesut@hazelcast.com>
2014-04-30 16:54:06 +0300

    hazelcast-wm checkstyle fixes

--- hazelcast-wm/src/main/java/com/hazelcast/web/SessionAttributePredicate.java
+++ hazelcast-wm/src/main/java/com/hazelcast/web/SessionAttributePredicate.java
@@ -18,7 +18,6 @@ package com.hazelcast.web;
 
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.DataSerializable;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.query.Predicate;
 
