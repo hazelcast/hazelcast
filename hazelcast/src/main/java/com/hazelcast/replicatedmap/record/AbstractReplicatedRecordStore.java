ba6a99b050d0a4c082cf270b3b86311e51266d55
noctarius <me@noctarius.com>
2014-05-09 11:24:46 +0200

    Probably fixed the failing ttl unittest

--- hazelcast/src/main/java/com/hazelcast/replicatedmap/record/AbstractReplicatedRecordStore.java
+++ hazelcast/src/main/java/com/hazelcast/replicatedmap/record/AbstractReplicatedRecordStore.java
@@ -93,7 +93,7 @@ public abstract class AbstractReplicatedRecordStore<K, V>
 
         // Force return null on ttl expiration (but before cleanup thread run)
         long ttlMillis = replicatedRecord == null ? 0 : replicatedRecord.getTtlMillis();
-        if (ttlMillis > 0 && System.currentTimeMillis() - replicatedRecord.getUpdateTime() > ttlMillis) {
+        if (ttlMillis > 0 && System.currentTimeMillis() - replicatedRecord.getUpdateTime() >= ttlMillis) {
             replicatedRecord = null;
         }
 
