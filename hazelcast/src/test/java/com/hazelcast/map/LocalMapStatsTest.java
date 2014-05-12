34756bc2b65d92669541f565d531b682a651a8d0
ahmetmircik <ahmet@hazelcast.com>
2014-05-12 12:12:18 +0300

    local map stats test fix attempt

--- hazelcast/src/test/java/com/hazelcast/map/LocalMapStatsTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/LocalMapStatsTest.java
@@ -1,18 +1,17 @@
 package com.hazelcast.map;
 
-import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
-import com.hazelcast.monitor.LocalMapStats;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
-import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(HazelcastParallelClassRunner.class)
 @Category(QuickTest.class)
@@ -22,7 +21,8 @@ public class LocalMapStatsTest extends HazelcastTestSupport {
 
     @Test
     public void testLastAccessTime() throws InterruptedException {
-        long startTime = System.currentTimeMillis();
+        final TimeUnit timeUnit = TimeUnit.MILLISECONDS;
+        final long startTime = timeUnit.toMillis(System.nanoTime());
 
         HazelcastInstance h1 = createHazelcastInstance();
         IMap<String, String> map1 = h1.getMap(name);
