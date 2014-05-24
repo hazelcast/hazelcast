658e18ec903e126e7e79b6a023b8a6c87f58b910
ahmetmircik <ahmet@hazelcast.com>
2014-05-24 21:46:36 +0300

    size estimator calculation for replica puts bug fix.

--- hazelcast/src/test/java/com/hazelcast/map/SizeEstimatorTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/SizeEstimatorTest.java
@@ -53,6 +53,42 @@ public class SizeEstimatorTest extends HazelcastTestSupport {
         assertEquals(164, map.getLocalMapStats().getHeapCost());
     }
 
+
+    @Test
+    public void testHowUpdatesAffectHeapCostWithMultipleBackupNodes() throws InterruptedException {
+        // constants.
+        final String mapName = randomMapName("testUpdatesWithMultipleNodes");
+        final int nodeCount = 3;
+        final int backupCount = nodeCount - 1;
+        final int expectedReplicaCount = nodeCount;
+        final long putCount = 1L;
+        final long expectedPerEntryHeapCost = 168L;
+        // config.
+        final Config config = new Config();
+        config.getMapConfig(mapName).setBackupCount(backupCount);
+        // nodes.
+        final TestHazelcastInstanceFactory instanceFactory = new TestHazelcastInstanceFactory(nodeCount);
+        final HazelcastInstance[] nodes = instanceFactory.newInstances(config);
+        // map.
+        final IMap<Long, Long> map = nodes[0].getMap(mapName);
+        // put.
+        for (long i = 0; i < putCount; i++) {
+            map.put(i, i);
+        }
+        // update.
+        for (long i = 0; i < putCount; i++) {
+            map.put(i, i);
+        }
+        // heap cost.
+        long heapCost = 0L;
+        for (int i = 0; i < nodeCount; i++) {
+            heapCost += nodes[i].getMap(mapName).getLocalMapStats().getHeapCost();
+        }
+        // assert heap cost.
+        assertEquals("Map heap cost calculation is wrong!",
+                expectedPerEntryHeapCost * putCount * expectedReplicaCount, heapCost);
+    }
+
     @Test
     public void testPutRemoveWithTwoNodeOwnerAndBackup() throws InterruptedException {
         final String mapName = "default";
