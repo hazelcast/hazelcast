be899b33073a5c96f920da816a513e2cd56e3adb
noctarius <me@noctarius.com>
2014-05-26 08:25:48 +0200

    Fixed expected values for unittest

--- hazelcast/src/test/java/com/hazelcast/mapreduce/MapReduceTest.java
+++ hazelcast/src/test/java/com/hazelcast/mapreduce/MapReduceTest.java
@@ -750,6 +750,7 @@ public class MapReduceTest
         for (int i = 0; i < 100; i++) {
             expectedResult += i;
         }
+        expectedResult = (int) ((double) expectedResult / 100);
 
         assertEquals(expectedResult, (int) future.get());
     }
