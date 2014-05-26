a5634065b600601ddc56ec2efb9ed4bc6c9e0cba
noctarius <me@noctarius.com>
2014-05-26 07:56:52 +0200

    Added try/catch to collate operation because it is not running on caller thread and exception might get lost otherwise

--- hazelcast/src/main/java/com/hazelcast/mapreduce/impl/task/TrackableJobFuture.java
+++ hazelcast/src/main/java/com/hazelcast/mapreduce/impl/task/TrackableJobFuture.java
@@ -82,7 +82,12 @@ public class TrackableJobFuture<V>
         Object finalResult = result;
         // If collator is available we need to execute it now
         if (collator != null) {
-            finalResult = collator.collate(((Map) finalResult).entrySet());
+            try {
+                finalResult = collator.collate(((Map) finalResult).entrySet());
+            } catch (Exception e) {
+                // Possible exception while collating
+                finalResult = e;
+            }
         }
         if (finalResult instanceof Throwable && !(finalResult instanceof CancellationException)) {
             finalResult = new ExecutionException((Throwable) finalResult);
