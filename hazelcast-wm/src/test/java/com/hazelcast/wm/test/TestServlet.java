845ccdb864dadf7dbb0631d55bd9bf4a3a065cf1
Mesut Celik <mesut@hazelcast.com>
2014-05-28 15:15:32 +0300

    fixes #2434

--- hazelcast-wm/src/test/java/com/hazelcast/wm/test/TestServlet.java
+++ hazelcast-wm/src/test/java/com/hazelcast/wm/test/TestServlet.java
@@ -22,6 +22,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class TestServlet extends HttpServlet {
 
@@ -48,6 +50,12 @@ public class TestServlet extends HttpServlet {
         } else if (req.getRequestURI().endsWith("update")) {
             session.setAttribute("key", "value-updated");
             resp.getWriter().write("true");
+
+        } else if (req.getRequestURI().endsWith("names")) {
+            List<String> names = Collections.list(session.getAttributeNames());
+            String nameList = names.toString();
+            //return comma seperated list of attribute names
+            resp.getWriter().write(nameList.substring(1, nameList.length() - 1).replace(", ", ","));
 
         } else if (req.getRequestURI().endsWith("reload")) {
             session.invalidate();
