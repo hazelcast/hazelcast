02f9e73aef9265546376af0dc1f18c500934f730
Emin Demirci <emindemirci@gmail.com>
2014-05-30 12:21:14 +0300

    cleanup hosted management center leftovers

--- hazelcast/src/main/java/com/hazelcast/config/ManagementCenterConfig.java
+++ hazelcast/src/main/java/com/hazelcast/config/ManagementCenterConfig.java
@@ -16,9 +16,6 @@
 
 package com.hazelcast.config;
 
-/**
- *
- */
 public class ManagementCenterConfig {
 
     private boolean enabled = false;
@@ -26,10 +23,6 @@ public class ManagementCenterConfig {
     private String url;
     
     private int updateInterval = 5;
-
-    private String clusterId;
-
-    private String securityToken;
 
     public ManagementCenterConfig() {
     }
@@ -66,30 +59,11 @@ public class ManagementCenterConfig {
         return this;
     }
 
-    public String getClusterId() {
-        return clusterId;
-    }
-
-    public void setClusterId(String clusterId) {
-        this.clusterId = clusterId;
-    }
-
-    public String getSecurityToken() {
-        return securityToken;
-    }
-
-    public void setSecurityToken(String securityToken) {
-        this.securityToken = securityToken;
-    }
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("ManagementCenterConfig");
         sb.append("{enabled=").append(enabled);
-        sb.append(", url='").append(url).append('\'');
-        sb.append(", clusterId='").append(clusterId).append('\'');
-        sb.append(", securityToken='").append(securityToken).append('\'');
         sb.append(", url='").append(url).append('\'');
         sb.append(", updateInterval=").append(updateInterval);
         sb.append('}');
--- hazelcast/src/main/java/com/hazelcast/instance/GroupProperties.java
+++ hazelcast/src/main/java/com/hazelcast/instance/GroupProperties.java
@@ -25,8 +25,6 @@ import com.hazelcast.util.HealthMonitorLevel;
  */
 public class GroupProperties {
 
-    public static final String PROP_HOSTED_MANAGEMENT_ENABLED = "hazelcast.hosted.management.enabled";
-    public static final String PROP_HOSTED_MANAGEMENT_URL = "hazelcast.hosted.management.url";
     public static final String PROP_HEALTH_MONITORING_LEVEL = "hazelcast.health.monitoring.level";
     public static final String PROP_HEALTH_MONITORING_DELAY_SECONDS = "hazelcast.health.monitoring.delay.seconds";
     public static final String PROP_VERSION_CHECK_ENABLED = "hazelcast.version.check.enabled";
@@ -98,13 +96,6 @@ public class GroupProperties {
     public static final String PROP_ELASTIC_MEMORY_SHARED_STORAGE = "hazelcast.elastic.memory.shared.storage";
     public static final String PROP_ELASTIC_MEMORY_UNSAFE_ENABLED = "hazelcast.elastic.memory.unsafe.enabled";
     public static final String PROP_ENTERPRISE_LICENSE_KEY = "hazelcast.enterprise.license.key";
-
-    /**
-     * This property will only be used temporary until we have exposed the hosted management center to the public.
-     * So it will be disabled by default.
-     */
-    public final GroupProperty HOSTED_MANAGEMENT_ENABLED;
-    public final GroupProperty HOSTED_MANAGEMENT_URL;
 
     public final GroupProperty PARTITION_OPERATION_THREAD_COUNT;
     public final GroupProperty GENERIC_OPERATION_THREAD_COUNT;
@@ -238,12 +229,6 @@ public class GroupProperties {
      * @param config
      */
     public GroupProperties(Config config) {
-        HOSTED_MANAGEMENT_ENABLED = new GroupProperty(config, PROP_HOSTED_MANAGEMENT_ENABLED, "false");
-
-        //todo: we need to pull out the version.
-        HOSTED_MANAGEMENT_URL
-                = new GroupProperty(config, PROP_HOSTED_MANAGEMENT_URL, "http://manage.hazelcast.com/3.2");
-
         HEALTH_MONITORING_LEVEL
                 = new GroupProperty(config, PROP_HEALTH_MONITORING_LEVEL, HealthMonitorLevel.SILENT.toString());
         HEALTH_MONITORING_DELAY_SECONDS = new GroupProperty(config, PROP_HEALTH_MONITORING_DELAY_SECONDS, "30");
