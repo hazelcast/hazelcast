32e14f3a7ddde795094fdaa4861ad2a05badd6ce
sgalland <stephane.galland@utbm.fr>
2014-05-31 12:46:30 +0200

    Fixing the unit test of the multicast loopback mode.

--- hazelcast/src/test/java/com/hazelcast/instance/MulticastLoopbackModeTest.java
+++ hazelcast/src/test/java/com/hazelcast/instance/MulticastLoopbackModeTest.java
@@ -19,6 +19,12 @@ package com.hazelcast.instance;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 import org.junit.After;
 import org.junit.Before;
@@ -30,29 +36,87 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.MulticastConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ISet;
+import com.hazelcast.core.Member;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestEnvironment;
 import com.hazelcast.test.annotation.QuickTest;
 
-/**
+/** Test the multicast loopback mode when there is no other
+ * network interface than 127.0.0.1.
+ *  
  * @author St&amp;eacute;phane Galland <galland@arakhne.org>
  */
 @RunWith(HazelcastParallelClassRunner.class)
 @Category(QuickTest.class)
 public class MulticastLoopbackModeTest extends HazelcastTestSupport {
 
-	private String useNetwork;
-	private String multicastGroup;
-	
-	@Before
-	public void setUpTests() {
-		this.useNetwork = System.setProperty(TestEnvironment.HAZELCAST_TEST_USE_NETWORK, Boolean.TRUE.toString());
-        this.multicastGroup = System.clearProperty("hazelcast.multicast.group");
+	/** Replies if a network interface was properly configured.
+	 * 
+	 * @return <code>true</code> if there is at least one configured interface;
+	 * <code>false</code> otherwise.
+	 */
+	protected static boolean hasConfiguredNetworkInterface() {
+		try {
+			Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
+			while (e.hasMoreElements()) {
+				NetworkInterface i = e.nextElement();
+				Enumeration<InetAddress> as = i.getInetAddresses();
+				while (as.hasMoreElements()) {
+					InetAddress a = as.nextElement();
+					if (a instanceof Inet4Address && !a.isLoopbackAddress()
+							&& !a.isMulticastAddress()) {
+						return true;
+					}
+				}
+			}
+		}
+		catch (Exception _) {
+			// Silently cast the exceptions
+		}
+		return false;
 	}
 	
+	/** Wait for the real termination of the Hazelcast instance.
+	 * 
+	 * @param hz
+	 */
+	protected static void waitForTermination(HazelcastInstance hz) {
+		TestUtil.terminateInstance(hz);
+		hz.shutdown(); // Only to be sure
+		boolean hasMember = true;
+		do {
+			try {
+				Member m = hz.getCluster().getLocalMember();
+				hasMember = m!=null;
+			}
+			catch(Throwable _) {
+				hasMember = false;
+			}
+		}
+		while (hasMember);
+	}
+
+	private String useNetwork;
+	private String multicastGroup;
+	private HazelcastInstance hz1;
+	private HazelcastInstance hz2;
+
+	@Before
+	public void setUpTests() {
+		assumeFalse(
+				"This test can be processed only if your host has no configured network interface.",
+				hasConfiguredNetworkInterface());
+		this.hz1 = this.hz2 = null;
+		this.useNetwork = System.setProperty(TestEnvironment.HAZELCAST_TEST_USE_NETWORK, Boolean.TRUE.toString());
+		this.multicastGroup = System.clearProperty("hazelcast.multicast.group");
+	}
+
 	@After
 	public void tearDownTests() {
+		if (this.hz2!=null) waitForTermination(this.hz2);
+		if (this.hz1!=null) waitForTermination(this.hz1);
+		this.hz1 = this.hz2 = null;
 		if (this.useNetwork==null)
 			System.clearProperty(TestEnvironment.HAZELCAST_TEST_USE_NETWORK);
 		else
@@ -61,96 +125,67 @@ public class MulticastLoopbackModeTest extends HazelcastTestSupport {
 			System.clearProperty("hazelcast.multicast.group");
 		else
 			System.setProperty("hazelcast.multicast.group", this.multicastGroup);
+	}	
+
+	private void createTestEnvironment(boolean loopbackMode) throws Exception {
+		Config config1 = new Config();
+		{
+			config1.setProperty("hazelcast.local.localAddress", "127.0.0.1"); //$NON-NLS-1$
+			MulticastConfig multicastConfig = config1.getNetworkConfig().getJoin().getMulticastConfig();
+			multicastConfig.setEnabled(true);
+			multicastConfig.setLoopbackModeEnabled(loopbackMode);
+		}
+
+		Config config2 = new Config();
+		{
+			config2.setProperty("hazelcast.local.localAddress", "127.0.0.1"); //$NON-NLS-1$
+			MulticastConfig multicastConfig = config2.getNetworkConfig().getJoin().getMulticastConfig();
+			multicastConfig.setEnabled(true);
+			multicastConfig.setLoopbackModeEnabled(loopbackMode);
+		}
+
+		this.hz1 = HazelcastInstanceFactory.newHazelcastInstance(config1);
+		assertNotNull("cannot create the first hazelcastInstance",hz1);
+
+		this.hz2 = HazelcastInstanceFactory.newHazelcastInstance(config2);
+		assertNotNull("cannot create the first hazelcastInstance",hz2);
 	}
-	
-    @Test
-    public void testEnabledMode(){
-        Config config1 = new Config();
-		{
-			config1.setProperty("hazelcast.local.localAddress", "127.0.0.1"); //$NON-NLS-1$
-			MulticastConfig multicastConfig = config1.getNetworkConfig().getJoin().getMulticastConfig();
-			multicastConfig.setEnabled(true);
-			multicastConfig.setLoopbackModeEnabled(true);
-		}
 
-        Config config2 = new Config();
-		{
-			config2.setProperty("hazelcast.local.localAddress", "127.0.0.1"); //$NON-NLS-1$
-			MulticastConfig multicastConfig = config2.getNetworkConfig().getJoin().getMulticastConfig();
-			multicastConfig.setEnabled(true);
-			multicastConfig.setLoopbackModeEnabled(true);
-		}
 
+	@Test
+	public void testEnabledMode() throws Exception {
 		String dataName = randomMapName(MulticastLoopbackModeTest.class.getName());
 		String myID = randomString();
-		
-        HazelcastInstance hz1 = HazelcastInstanceFactory.newHazelcastInstance(config1);
-        assertNotNull("cannot create the first hazelcastInstance",hz1);
-        try {
-            ISet<String> theMap = hz1.getSet(dataName);
-            theMap.add(myID);
-            
-            HazelcastInstance hz2 = HazelcastInstanceFactory.newHazelcastInstance(config2);
-            assertNotNull("cannot create the first hazelcastInstance",hz2);
-            try {
-                ISet<String> remoteMap = hz2.getSet(dataName);
-                
-                sleepSeconds(1);
-                
-                assertTrue(remoteMap.contains(myID));
-            }
-            finally {
-            	if (hz2!=null) TestUtil.terminateInstance(hz2);
-            }
-        }
-        finally {
-            if (hz1!=null) TestUtil.terminateInstance(hz1);
-        }
-    }
 
-    @Test
-    public void testDisabledMode(){
-        Config config1 = new Config();
-		{
-			config1.setProperty("hazelcast.local.localAddress", "127.0.0.1"); //$NON-NLS-1$
-			MulticastConfig multicastConfig = config1.getNetworkConfig().getJoin().getMulticastConfig();
-			multicastConfig.setEnabled(true);
-			multicastConfig.setLoopbackModeEnabled(false);
-		}
+		createTestEnvironment(true);
 
-        Config config2 = new Config();
-		{
-			config2.setProperty("hazelcast.local.localAddress", "127.0.0.1"); //$NON-NLS-1$
-			MulticastConfig multicastConfig = config2.getNetworkConfig().getJoin().getMulticastConfig();
-			multicastConfig.setEnabled(true);
-			multicastConfig.setLoopbackModeEnabled(false);
-		}
+		ISet<String> theMap = this.hz1.getSet(dataName);
+		theMap.add(myID);
 
+
+		ISet<String> remoteMap = this.hz2.getSet(dataName);
+
+		sleepSeconds(1);
+
+		assertTrue(remoteMap.contains(myID));
+	}
+
+	@Test
+	public void testDisabledMode() throws Exception {
 		String dataName = randomMapName(MulticastLoopbackModeTest.class.getName());
 		String myID = randomString();
-		
-		HazelcastInstance hz1 = HazelcastInstanceFactory.newHazelcastInstance(config1);
-        assertNotNull("cannot create the first hazelcastInstance",hz1);
-        try {
-            ISet<String> theMap = hz1.getSet(dataName);
-            theMap.add(myID);
-            
-            HazelcastInstance hz2 = HazelcastInstanceFactory.newHazelcastInstance(config2);
-            assertNotNull("cannot create the first hazelcastInstance",hz2);
-            try {
-                ISet<String> remoteMap = hz2.getSet(dataName);
-                
-                sleepSeconds(1);
-                
-                assertFalse(remoteMap.contains(myID));
-            }
-            finally {
-            	if (hz2!=null) TestUtil.terminateInstance(hz2);
-            }
-        }
-        finally {
-        	if (hz1!=null) TestUtil.terminateInstance(hz1);
-        }
-    }
+
+		createTestEnvironment(false);
+
+		ISet<String> theMap = this.hz1.getSet(dataName);
+		theMap.add(myID);
+
+
+		ISet<String> remoteMap = this.hz2.getSet(dataName);
+
+		sleepSeconds(1);
+
+		assertFalse(remoteMap.contains(myID));
+	}
 
 }
