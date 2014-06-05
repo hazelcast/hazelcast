37d7224334e940fff9d521eaf6850ef23bfd7da6
danny <danny@hazelcast.com>
2014-06-05 16:36:59 +0300

    revert fix from this pr as its with alli's one

--- hazelcast/src/main/java/com/hazelcast/map/record/ObjectRecordFactory.java
+++ hazelcast/src/main/java/com/hazelcast/map/record/ObjectRecordFactory.java
@@ -42,7 +42,7 @@ public class ObjectRecordFactory implements RecordFactory<Object> {
         if (value instanceof Data) {
             v = serializationService.toObject(value);
         }
-        return statisticsEnabled ? new ObjectRecordWithStats(key, v) : new ObjectRecord(key, v);
+        return statisticsEnabled ? new ObjectRecordWithStats(key, value) : new ObjectRecord(key, v);
     }
 
     @Override
