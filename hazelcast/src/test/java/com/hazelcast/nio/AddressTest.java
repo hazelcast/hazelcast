688bae6d789678b9ae7386af2f8fd3a2416211ed
Peter Veentjer <alarmnummer@gmail.com>
2014-06-11 09:58:27 +0300

    Added runner/category on test

--- hazelcast/src/test/java/com/hazelcast/nio/AddressTest.java
+++ hazelcast/src/test/java/com/hazelcast/nio/AddressTest.java
@@ -1,10 +1,16 @@
 package com.hazelcast.nio;
 
+import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
+@RunWith(HazelcastSerialClassRunner.class)
+@Category(QuickTest.class)
 public class AddressTest {
 
     @Test(expected = IllegalArgumentException.class)
