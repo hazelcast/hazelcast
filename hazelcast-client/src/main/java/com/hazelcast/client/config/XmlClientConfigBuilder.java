adecd9b794c5232fc5730a70d7df455cc9a7e12f
Peter Veentjer <alarmnummer@gmail.com>
2014-06-13 11:43:36 +0300

    Pulled out xml config location mechanism to reduce size of xmlconfigbuilder; did the same for the client to make it consistent

--- hazelcast-client/src/main/java/com/hazelcast/client/config/XmlClientConfigBuilder.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/config/XmlClientConfigBuilder.java
@@ -19,7 +19,6 @@ package com.hazelcast.client.config;
 import com.hazelcast.client.util.RandomLB;
 import com.hazelcast.client.util.RoundRobinLB;
 import com.hazelcast.config.AbstractXmlConfigHelper;
-import com.hazelcast.config.Config;
 import com.hazelcast.config.ConfigLoader;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.ListenerConfig;
@@ -27,7 +26,6 @@ import com.hazelcast.config.NearCacheConfig;
 import com.hazelcast.config.SSLConfig;
 import com.hazelcast.config.SerializationConfig;
 import com.hazelcast.config.SocketInterceptorConfig;
-import com.hazelcast.core.HazelcastException;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.hazelcast.nio.IOUtil;
@@ -41,7 +39,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -50,8 +47,6 @@ import java.net.URL;
  * Loads the {@link com.hazelcast.client.config.ClientConfig} using XML.
  */
 public class XmlClientConfigBuilder extends AbstractXmlConfigHelper {
-
-    private static final ILogger LOGGER = Logger.getLogger(XmlClientConfigBuilder.class);
 
     private static final int DEFAULT_VALUE = 5;
 
@@ -95,117 +90,9 @@ public class XmlClientConfigBuilder extends AbstractXmlConfigHelper {
      * </ol>
      */
     public XmlClientConfigBuilder() {
-        try {
-            if (loadFromSystemProperty()) {
-                return;
-            }
-
-            if (loadFromWorkingDirectory()) {
-                return;
-            }
-
-            if (loadClientHazelcastXmlFromClasspath()) {
-                return;
-            }
-
-            loadDefaultConfigurationFromClasspath();
-        } catch (final RuntimeException e) {
-            throw new HazelcastException("Failed to load ClientConfig", e);
-        }
+        XmlClientConfigLocator locator = new XmlClientConfigLocator();
+        this.in = locator.getIn();
     }
-
-    private void loadDefaultConfigurationFromClasspath() {
-        LOGGER.info("Loading 'hazelcast-client-default.xml' from classpath.");
-
-        in = Config.class.getClassLoader().getResourceAsStream("hazelcast-client-default.xml");
-        if (in == null) {
-            throw new HazelcastException("Could not load 'hazelcast-client-default.xml' from classpath");
-        }
-    }
-
-    private boolean loadClientHazelcastXmlFromClasspath() {
-        URL url = Config.class.getClassLoader().getResource("hazelcast-client.xml");
-        if (url == null) {
-            LOGGER.finest("Could not find 'hazelcast-client.xml' in classpath.");
-            return false;
-        }
-
-        LOGGER.info("Loading 'hazelcast-client.xml' from classpath.");
-
-        in = Config.class.getClassLoader().getResourceAsStream("hazelcast-client.xml");
-        if (in == null) {
-            throw new HazelcastException("Could not load 'hazelcast-client.xml' from classpath");
-        }
-        return true;
-    }
-
-    private boolean loadFromWorkingDirectory() {
-        File file = new File("hazelcast-client.xml");
-        if (!file.exists()) {
-            LOGGER.finest("Could not find 'hazelcast-client.xml' in working directory.");
-            return false;
-        }
-
-        LOGGER.info("Loading 'hazelcast-client.xml' from working directory.");
-        try {
-            in = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            throw new HazelcastException("Failed to open file: " + file.getAbsolutePath(), e);
-        }
-        return true;
-    }
-
-    private boolean loadFromSystemProperty() {
-        String configSystemProperty = System.getProperty("hazelcast.client.config");
-
-        if (configSystemProperty == null) {
-            LOGGER.finest("Could not 'hazelcast.client.config' System property");
-            return false;
-        }
-
-        LOGGER.info("Loading configuration " + configSystemProperty + " from System property 'hazelcast.client.config'");
-
-        if (configSystemProperty.startsWith("classpath:")) {
-            loadSystemPropertyClassPathResource(configSystemProperty);
-        } else {
-            loadSystemPropertyFileResource(configSystemProperty);
-        }
-        return true;
-    }
-
-    private void loadSystemPropertyFileResource(String configSystemProperty) {
-        //it is a file.
-        File configurationFile = new File(configSystemProperty);
-        LOGGER.info("Using configuration file at " + configurationFile.getAbsolutePath());
-
-        if (!configurationFile.exists()) {
-            String msg = "Config file at '" + configurationFile.getAbsolutePath() + "' doesn't exist.";
-            throw new HazelcastException(msg);
-        }
-
-        try {
-            in = new FileInputStream(configurationFile);
-        } catch (FileNotFoundException e) {
-            throw new HazelcastException("Failed to open file: " + configurationFile.getAbsolutePath(), e);
-        }
-    }
-
-    private void loadSystemPropertyClassPathResource(String configSystemProperty) {
-        //it is a explicit configured classpath resource.
-        String resource = configSystemProperty.substring("classpath:".length());
-
-        LOGGER.info("Using classpath resource at " + resource);
-
-        if (resource.isEmpty()) {
-            throw new HazelcastException("classpath resource can't be empty");
-        }
-
-        in = Config.class.getClassLoader().getResourceAsStream(resource);
-        if (in == null) {
-            throw new HazelcastException("Could not load classpath resource: " + resource);
-        }
-    }
-
 
     public ClientConfig build() {
         return build(Thread.currentThread().getContextClassLoader());
@@ -341,7 +228,8 @@ public class XmlClientConfigBuilder extends AbstractXmlConfigHelper {
             if ("enabled".equalsIgnoreCase(att.getNodeName())) {
                 clientAwsConfig.setEnabled(checkTrue(value));
             } else if (att.getNodeName().equals("connection-timeout-seconds")) {
-                clientAwsConfig.setConnectionTimeoutSeconds(getIntegerValue("connection-timeout-seconds", value, DEFAULT_VALUE));
+                int timeout = getIntegerValue("connection-timeout-seconds", value, DEFAULT_VALUE);
+                clientAwsConfig.setConnectionTimeoutSeconds(timeout);
             }
         }
         for (Node n : new IterableNodeList(node.getChildNodes())) {
