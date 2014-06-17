9e6ddd9289be06dfd86743ec349eff3acafef05e
Emin Demirci <emin@hazelcast.com>
2014-06-17 15:30:01 +0300

    improper word

--- hazelcast/src/main/java/com/hazelcast/management/JsonSerializable.java
+++ hazelcast/src/main/java/com/hazelcast/management/JsonSerializable.java
@@ -3,7 +3,7 @@ package com.hazelcast.management;
 import com.eclipsesource.json.JsonObject;
 
 /**
- * JsonSerializable is a serialization method that serializes/de-serializes to/from JSON.
+ * JsonSerializable is a serialization interface that serializes/de-serializes to/from JSON.
  */
 public interface JsonSerializable {
 
