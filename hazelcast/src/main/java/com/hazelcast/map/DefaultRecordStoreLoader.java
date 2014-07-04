d0d65aa1c8bf54b797318ab87e83b64159181d35
ahmetmircik <ahmet@hazelcast.com>
2014-07-04 03:51:11 +0300

    review fixes, init with size and use load batch size method

--- hazelcast/src/main/java/com/hazelcast/map/DefaultRecordStoreLoader.java
+++ hazelcast/src/main/java/com/hazelcast/map/DefaultRecordStoreLoader.java
@@ -168,7 +168,7 @@ public class DefaultRecordStoreLoader implements RecordStoreLoader {
         if (entries == null || entries.isEmpty()) {
             return Collections.emptyList();
         }
-        final List<Data> keyValueSequence = new ArrayList<Data>();
+        final List<Data> keyValueSequence = new ArrayList<Data>(entries.size());
         for (final Map.Entry<Object, Object> entry : entries.entrySet()) {
             final Object key = entry.getKey();
             final Object value = entry.getValue();
@@ -268,7 +268,7 @@ public class DefaultRecordStoreLoader implements RecordStoreLoader {
     }
 
     private void doChunkedLoad(Map<Data, Object> loadedKeys, NodeEngine nodeEngine) {
-        final int mapLoadChunkSize = nodeEngine.getGroupProperties().MAP_LOAD_CHUNK_SIZE.getInteger();
+        final int mapLoadChunkSize = getLoadBatchSize();
         final Queue<Map> chunks = new LinkedList<Map>();
         Map<Data, Object> partitionKeys = new HashMap<Data, Object>();
         Iterator<Map.Entry<Data, Object>> iterator = loadedKeys.entrySet().iterator();
