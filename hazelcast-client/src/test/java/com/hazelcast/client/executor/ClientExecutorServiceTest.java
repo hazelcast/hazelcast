1ef1c74d90ba0a8eaa5cefa07a88a5042abf77b7
Donnerbart <github@thunderphreak.de>
2014-07-08 23:27:00 +0200

    Fixed import wildcard.

--- hazelcast-client/src/test/java/com/hazelcast/client/executor/ClientExecutorServiceTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/executor/ClientExecutorServiceTest.java
@@ -34,7 +34,12 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static com.hazelcast.test.HazelcastTestSupport.assertTrueEventually;
 import static com.hazelcast.test.HazelcastTestSupport.randomString;
