edc952839409ff5c63201046db26110496f589a3
Emin Demirci <emindemirci@gmail.com>
2014-07-08 22:26:51 +0300

    QueueIterator::remove throws UnsupportedOperationException

--- hazelcast-client/src/test/java/com/hazelcast/client/queue/ClientQueueTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/queue/ClientQueueTest.java
@@ -276,21 +276,13 @@ public class ClientQueueTest {
         }
     }
 
-    @Test
+    @Test(expected = UnsupportedOperationException.class)
     public void testQueueRemoveFromIterator() {
         IQueue<String> queue = client.getQueue(randomString());
         queue.add("one");
-        queue.add("two");
-        queue.add("three");
-        queue.add("four");
-
         Iterator<String> iterator = queue.iterator();
-        while(iterator.hasNext()) {
-            iterator.next();
-            iterator.remove();
-        }
-        assertEquals(0, queue.size());
-
+        iterator.next();
+        iterator.remove();
     }
 
 
