34d56337b02b41f7803b2cb0d91809fc34736da2
Emin Demirci <emindemirci@gmail.com>
2014-07-10 15:25:56 +0300

    test improvements

--- hazelcast/src/test/java/com/hazelcast/map/EntryProcessorBouncingNodesTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/EntryProcessorBouncingNodesTest.java
@@ -14,24 +14,23 @@
  *  limitations under the License.
  */
 
-package com.hazelcast.partition;
+package com.hazelcast.map;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.MapConfig;
-import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.core.PartitionService;
-import com.hazelcast.map.AbstractEntryProcessor;
 import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.QuickTest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import org.junit.After;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -40,36 +39,22 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
-public class RebalancingPartitionTest {
+public class EntryProcessorBouncingNodesTest extends HazelcastTestSupport {
 
     private static final int ENTRIES = 10;
     private static final long ITERATIONS = 50;
     private static String MAP_NAME = "test-map";
-
-    static {
-        System.setProperty("hazelcast.mancenter.enabled", "false");
-        System.setProperty("hazelcast.local.localAddress", "127.0.0.1");
-        System.setProperty("java.net.preferIPv4Stack", "true");
-    }
-
-    @After
-    public void teardown() {
-        Hazelcast.shutdownAll();
-    }
+    private TestHazelcastInstanceFactory instanceFactory = createHazelcastInstanceFactory(50);
 
     @Test
     public void testRepartitioningCluster() throws InterruptedException {
         CountDownLatch startLatch = new CountDownLatch(1);
-        CountDownLatch stopLatch = new CountDownLatch(1);
-
-        System.out.println("Starting 3 Hazelcast instances");
+        AtomicBoolean isRunning = new AtomicBoolean(true);
 
         // start up three instances
         HazelcastInstance instance = newInstance();
         HazelcastInstance instance2 = newInstance();
         HazelcastInstance instance3 = newInstance();
-
-        System.out.println("Stable instance: " + instance.getCluster().getLocalMember().getUuid());
 
         // Create a map that we'll use to test data consistency while nodes are joining and leaving the cluster
         // The basic idea is pretty simple. In a loop, for each key in the IMap, we'll add a number to a list.
@@ -87,17 +72,14 @@ public class RebalancingPartitionTest {
         assertEquals(ENTRIES, map.size());
 
         // spin up the threads that stop/start the instance2 and instance3, leaving one instance always running
-        Thread bounceThread1 = new Thread(new RestartNodeRunnable(instance2, startLatch, stopLatch));
-        Thread bounceThread2 = new Thread(new RestartNodeRunnable(instance3, startLatch, stopLatch));
+        Thread bounceThread1 = new Thread(new RestartNodeRunnable(instance2, startLatch, isRunning));
+        Thread bounceThread2 = new Thread(new RestartNodeRunnable(instance3, startLatch, isRunning));
         bounceThread1.start();
         bounceThread2.start();
 
         // now, with nodes joining and leaving the cluster concurrently, start adding numbers to the lists
         int iteration = 0;
         while (iteration < ITERATIONS) {
-            if (iteration % 10 == 0) {
-                System.out.println("iteration " + iteration);
-            }
             if (iteration == 30) {
                 // let the bounce threads start bouncing
                 startLatch.countDown();
@@ -112,72 +94,57 @@ public class RebalancingPartitionTest {
         }
 
         // signal the bounce threads that we're done
-        stopLatch.countDown();
+        isRunning.set(false);
 
-        System.out.println("waiting for bounceThreads to complete");
-
-//        // wait for the instance bounces to complete
+        // wait for the instance bounces to complete
         bounceThread1.join();
         bounceThread2.join();
 
-        System.out.println("starting verification");
         PartitionService partitionService = instance.getPartitionService();
-
         List<String> errors = new ArrayList<String>();
         // validate map contents for partitions owned by instance - these have never been rebalanced
         for (int i = 0; i < ENTRIES; ++i) {
-            System.out.print("verifying entry " + i);
             List<Integer> list = map.get(i);
             if (expected.size() <= list.size()) {
                 // there can be one or two more entries that are inserted because of retries.
-                System.out.println(" - pass");
                 continue;
             }
 
             String owner = partitionService.getPartition(i).getOwner().getUuid();
             String error = owner + "[" + i + "] doesn't match! Expected/actual length: " + expected.size() + "/" + list.size() +
                     "\nExpected : " + expected.toString() + "\nActual   : " + list + "\n";
-            System.out.println(" - fail");
-            System.err.println(error);
             errors.add(error);
         }
-
         assertEquals(Collections.emptyList(), errors);
     }
 
-    private static HazelcastInstance newInstance() {
+    private  HazelcastInstance newInstance() {
         final Config config = new Config();
         final MapConfig mapConfig = new MapConfig(MAP_NAME);
         mapConfig.setBackupCount(2);
         config.addMapConfig(mapConfig);
-        return Hazelcast.newHazelcastInstance(config);
+        return instanceFactory.newHazelcastInstance(config);
     }
 
-    private static class RestartNodeRunnable implements Runnable {
+    private class RestartNodeRunnable implements Runnable {
         private final CountDownLatch start;
-        private final CountDownLatch stop;
+        private final AtomicBoolean isRunning;
         private HazelcastInstance instance;
 
-        private RestartNodeRunnable(HazelcastInstance instance, CountDownLatch startLatch, CountDownLatch stopLatch) {
+        private RestartNodeRunnable(HazelcastInstance instance, CountDownLatch startLatch, AtomicBoolean isRunning) {
             this.instance = instance;
             this.start = startLatch;
-            this.stop = stopLatch;
+            this.isRunning = isRunning;
         }
 
         @Override
         public void run() {
             try {
                 start.await();
-                while (!stop.await(0, TimeUnit.MILLISECONDS)) {
-                    long start = System.currentTimeMillis();
-                    String name = instance.getName() + "(" + instance.getCluster().getLocalMember().getUuid() + ")";
+                while (isRunning.get()) {
                     instance.shutdown();
-                    System.out.println(name + ": shutdown in " + (System.currentTimeMillis() - start) + " millis");
                     Thread.sleep(10l);
-                    start = System.currentTimeMillis();
                     instance = newInstance();
-                    String newName = instance.getName() + "(" + instance.getCluster().getLocalMember().getUuid() + ")";
-                    System.out.println(name + ": restarted as " + newName + " in " + (System.currentTimeMillis() - start) + " millis");
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -201,7 +168,6 @@ public class RebalancingPartitionTest {
 
             list.add(nextVal);
             entry.setValue(list);
-
             return null;
         }
     }
