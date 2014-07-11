f69347217831687f116ab885eb6912e7bcf2e776
Donnerbart <github@thunderphreak.de>
2014-07-11 15:42:12 +0200

    Cleanup of com.hazelcast.client package oome.

--- hazelcast-client/src/test/java/com/hazelcast/client/oome/OomeOnClientAuthenticationMain.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/oome/OomeOnClientAuthenticationMain.java
@@ -19,7 +19,6 @@ package com.hazelcast.client.oome;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
 
 /**
  * A test application that starts clients with bad credentials. There was a  bug in the system that eventually
@@ -32,7 +31,7 @@ public class OomeOnClientAuthenticationMain {
     }
 
     public static void main(String[] args) {
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        Hazelcast.newHazelcastInstance();
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getGroupConfig().setPassword("foo");
         clientConfig.getNetworkConfig().setConnectionAttemptLimit(0);
@@ -41,8 +40,7 @@ public class OomeOnClientAuthenticationMain {
             System.out.println("At:" + k);
             try {
                 HazelcastClient.newHazelcastClient(clientConfig);
-            } catch (IllegalStateException e) {
-
+            } catch (IllegalStateException ignored) {
             }
         }
     }
