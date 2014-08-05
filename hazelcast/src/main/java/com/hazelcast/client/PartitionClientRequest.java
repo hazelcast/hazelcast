cea53e7d22e27f987f15be441b48120606079438
sancar <sancar@hazelcast.com>
2014-08-05 09:58:10 +0300

    javadoc update for PartitionClientRequest

--- hazelcast/src/main/java/com/hazelcast/client/PartitionClientRequest.java
+++ hazelcast/src/main/java/com/hazelcast/client/PartitionClientRequest.java
@@ -24,12 +24,21 @@ public abstract class PartitionClientRequest extends ClientRequest {
 
     private static final int TRY_COUNT = 100;
 
+    /**
+     * Called on node side, before starting any operation.
+     */
     protected void beforeProcess() {
     }
 
+    /**
+     * Called on node side, after process is run and right before sending the response to the client.
+     */
     protected void beforeResponse() {
     }
 
+    /**
+     * Called on node side, after sending the response to the client.
+     */
     protected void afterResponse() {
     }
 
