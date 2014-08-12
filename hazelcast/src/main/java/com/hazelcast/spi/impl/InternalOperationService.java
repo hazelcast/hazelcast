cdaafe9fe03e381a10fcf49ab77076fea476d58e
Peter Veentjer <alarmnummer@gmail.com>
2014-08-12 19:49:01 +0300

    Minor checkstyle related fixes

--- hazelcast/src/main/java/com/hazelcast/spi/impl/InternalOperationService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/InternalOperationService.java
@@ -17,7 +17,6 @@
 package com.hazelcast.spi.impl;
 
 import com.hazelcast.instance.MemberImpl;
-import com.hazelcast.nio.Packet;
 import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.OperationService;
 
@@ -30,7 +29,7 @@ import com.hazelcast.spi.OperationService;
  */
 public interface InternalOperationService extends OperationService {
 
-     void onMemberLeft(MemberImpl member);
+    void onMemberLeft(MemberImpl member);
 
     boolean isCallTimedOut(Operation op);
 
