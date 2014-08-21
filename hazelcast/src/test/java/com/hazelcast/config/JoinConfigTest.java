78f77bccbde8c95222a73f77cef4a029e36fb77a
Peter Veentjer <alarmnummer@gmail.com>
2014-08-21 09:00:59 +0300

    Added correct test annotations on the JoinConfigTest

--- hazelcast/src/test/java/com/hazelcast/config/JoinConfigTest.java
+++ hazelcast/src/test/java/com/hazelcast/config/JoinConfigTest.java
@@ -1,9 +1,15 @@
 package com.hazelcast.config;
 
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.fail;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category(QuickTest.class)
 public class JoinConfigTest {
 
     @Test
