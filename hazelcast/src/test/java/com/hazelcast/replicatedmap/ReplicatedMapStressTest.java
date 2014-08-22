e191e60debcb3d9dbbe80de51f9870946c441e64
Peter Veentjer <alarmnummer@gmail.com>
2014-08-22 13:06:07 +0300

    ReplicatedMapStressTest now running with 25 instead of 50 threads*

--- hazelcast/src/test/java/com/hazelcast/replicatedmap/ReplicatedMapStressTest.java
+++ hazelcast/src/test/java/com/hazelcast/replicatedmap/ReplicatedMapStressTest.java
@@ -1,24 +1,25 @@
 package com.hazelcast.replicatedmap;
 
+import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.NightlyTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 
-
+@RunWith(HazelcastSerialClassRunner.class)
 @Category(NightlyTest.class)
 public class ReplicatedMapStressTest extends HazelcastTestSupport {
     @Test
     public void stressTestRemove() throws Exception {
-        int noOfThreads = 50;
+        int noOfThreads = 25;
         final CountDownLatch latch = new CountDownLatch(noOfThreads);
         final AtomicInteger noOfFailures = new AtomicInteger();
-
 
         for (int i = 0; i < noOfThreads; i++) {
             new Thread() {
