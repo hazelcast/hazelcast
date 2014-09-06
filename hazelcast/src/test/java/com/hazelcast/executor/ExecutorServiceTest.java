c509e1948bf9cff72fb5bb4475d4513350eedcfb
ahmetmircik <ahmet@hazelcast.com>
2014-09-06 13:10:09 +0300

    use assertOpenEventually for latch in testSubmitToKeyOwnerRunnable

--- hazelcast/src/test/java/com/hazelcast/executor/ExecutorServiceTest.java
+++ hazelcast/src/test/java/com/hazelcast/executor/ExecutorServiceTest.java
@@ -232,7 +232,7 @@ public class ExecutorServiceTest extends HazelcastTestSupport {
             }
             service.submitToKeyOwner(new ScriptRunnable(script, map), key, callback);
         }
-        latch.await(10, TimeUnit.SECONDS);
+        assertOpenEventually(latch);
         assertEquals(0, instances[0].getAtomicLong("testSubmitToKeyOwnerRunnable").get());
         assertEquals(k, count.get());
     }
@@ -264,7 +264,7 @@ public class ExecutorServiceTest extends HazelcastTestSupport {
             map.put("member", instance.getCluster().getLocalMember());
             service.submitToMember(new ScriptRunnable(script, map), instance.getCluster().getLocalMember(), callback);
         }
-        latch.await(10, TimeUnit.SECONDS);
+        assertOpenEventually(latch);
         assertEquals(0, instances[0].getAtomicLong("testSubmitToMemberRunnable").get());
         assertEquals(k, count.get());
     }
