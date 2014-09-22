ea595bcf6038931a7e00b89f77695fc02050ce41
serkanozal <serkan@hazelcast.com>
2014-09-22 19:21:15 +0300

    checkstyle fix for issue #3483

--- hazelcast/src/main/java/com/hazelcast/nio/UTFEncoderDecoder.java
+++ hazelcast/src/main/java/com/hazelcast/nio/UTFEncoderDecoder.java
@@ -152,14 +152,14 @@ public final class UTFEncoderDecoder {
             implements StringValueArrayProviderFactory {
 
         private static long stringValueFieldOffset = -1;
-        private static sun.misc.Unsafe UNSAFE;
+        private static sun.misc.Unsafe unsafe;
 
         static {
             if (UnsafeHelper.UNSAFE_AVAILABLE) {
-                UNSAFE = UnsafeHelper.UNSAFE;
+                unsafe = UnsafeHelper.UNSAFE;
                 try {
                     stringValueFieldOffset =
-                            UNSAFE.objectFieldOffset(String.class.getDeclaredField("value"));
+                            unsafe.objectFieldOffset(String.class.getDeclaredField("value"));
                 } catch (Throwable t) {
 
                 }
@@ -168,12 +168,12 @@ public final class UTFEncoderDecoder {
 
         @Override
         public StringValueArrayProvider create(String str) {
-            return new UnsafeBasedStringCharProvider(UNSAFE, stringValueFieldOffset, str);
+            return new UnsafeBasedStringCharProvider(unsafe, stringValueFieldOffset, str);
         }
 
         @Override
         public boolean isAvailable() {
-            return UNSAFE != null && stringValueFieldOffset != -1;
+            return unsafe != null && stringValueFieldOffset != -1;
         }
 
     }
