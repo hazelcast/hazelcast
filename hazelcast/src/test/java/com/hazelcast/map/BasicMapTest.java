962fcf79a08492d899dec25ceee96a97b6ab96f4
ahmetmircik <ahmet@hazelcast.com>
2014-09-22 14:07:48 +0300

    test fix testMapLoaderLoadUpdatingIndex, assertTrueEventually added

--- hazelcast/src/test/java/com/hazelcast/map/BasicMapTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/BasicMapTest.java
@@ -31,6 +31,7 @@ import com.hazelcast.core.MapLoader;
 import com.hazelcast.core.MapStoreFactory;
 import com.hazelcast.query.Predicate;
 import com.hazelcast.query.SqlPredicate;
+import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
@@ -1135,8 +1136,9 @@ public class BasicMapTest extends HazelcastTestSupport {
             map.put(i, new SampleIndexableObject("My-" + i, i));
         }
 
-        SqlPredicate predicate = new SqlPredicate("name='My-5'");
+        final SqlPredicate predicate = new SqlPredicate("name='My-5'");
         Set<Entry<Integer, SampleIndexableObject>> result = map.entrySet(predicate);
+
         assertEquals(1, result.size());
         assertEquals(5, (int) result.iterator().next().getValue().value);
 
@@ -1145,10 +1147,16 @@ public class BasicMapTest extends HazelcastTestSupport {
         map = getInstance().getMap("testMapLoaderLoadUpdatingIndex");
         assertFalse(map.isEmpty());
 
-        predicate = new SqlPredicate("name='My-5'");
-        result = map.entrySet(predicate);
-        assertEquals(1, result.size());
-        assertEquals(5, (int) result.iterator().next().getValue().value);
+        final IMap<Integer, SampleIndexableObject> mapFinal = map;
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                Set<Entry<Integer, SampleIndexableObject>> result = mapFinal.entrySet(predicate);
+                assertEquals(1, result.size());
+                assertEquals(5, (int) result.iterator().next().getValue().value);
+            }
+        });
+
     }
 
     @Test
