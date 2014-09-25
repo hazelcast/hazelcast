1090742923b9d8a34d71024f4a982867d0a7ffd4
Mehmet Dogan <mehmet@hazelcast.com>
2014-09-25 10:16:16 +0300

    Test compilation fix.

--- hazelcast/src/test/java/com/hazelcast/map/mapstore/MapStoreEvictionTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/mapstore/MapStoreEvictionTest.java
@@ -1,15 +1,8 @@
 package com.hazelcast.map.mapstore;
 
-import static java.lang.Math.max;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.GroupConfig;
-import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MapStoreConfig;
 import com.hazelcast.config.MaxSizeConfig;
 import com.hazelcast.core.HazelcastInstance;
@@ -19,6 +12,12 @@ import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static java.lang.Math.max;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(HazelcastParallelClassRunner.class)
 @Category(QuickTest.class)
@@ -59,7 +58,7 @@ public class MapStoreEvictionTest extends HazelcastTestSupport {
         .setMapStoreConfig(mapStoreConfig)
         .setMaxSizeConfig(maxSizeConfig)
         .setEvictionPercentage(10)
-        .setEvictionPolicy(MapConfig.EvictionPolicy.LRU)
+        .setEvictionPolicy(EvictionPolicy.LRU)
         .setMergePolicy(LatestUpdateMapMergePolicy.class.getName());
 
         return cfg;
