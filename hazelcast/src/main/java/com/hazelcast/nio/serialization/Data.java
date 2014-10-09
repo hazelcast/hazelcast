cc5df41779b501443688ebb3a2b979d00ecf61d9
Mehmet Dogan <mehmet@hazelcast.com>
2014-10-09 17:33:47 +0300

    Added Data javadoc.

--- hazelcast/src/main/java/com/hazelcast/nio/serialization/Data.java
+++ hazelcast/src/main/java/com/hazelcast/nio/serialization/Data.java
@@ -18,29 +18,99 @@ package com.hazelcast.nio.serialization;
 
 import java.nio.ByteOrder;
 
+/**
+ * Data is basic unit of serialization. It stores binary form of an object serialized
+ * by {@link com.hazelcast.nio.serialization.SerializationService#toData(Object)}.
+ *
+ */
 public interface Data {
 
+    /**
+     * Returns byte array representation of internal binary format.
+     *
+     * @return binary data
+     */
     byte[] getData();
 
+    /**
+     * Returns serialization type of binary form. It's defined by
+     * {@link Serializer#getTypeId()}
+     *
+     * @return serializer type id
+     */
     int getType();
 
+    /**
+     * Returns size of binary data
+     *
+     * @return data size
+     */
     int dataSize();
 
+    /**
+     * Returns approximate heap cost of this Data object in bytes.
+     *
+     * @return approximate heap cost
+     */
     int getHeapCost();
 
+    /**
+     * Returns partition hash calculated for serialized object.
+     * Partition hash is used to determine partition of a Data and is calculated using
+     * {@link com.hazelcast.core.PartitioningStrategy} during serialization.
+     * <p/>
+     * If partition hash is not set then standard <tt>hashCode()</tt> is used.
+     *
+     * @return partition hash
+     * @see com.hazelcast.core.PartitionAware
+     * @see com.hazelcast.core.PartitioningStrategy
+     * @see com.hazelcast.nio.serialization.SerializationService#toData(Object, com.hazelcast.core.PartitioningStrategy)
+     */
     int getPartitionHash();
 
+    /**
+     * Returns true if Data has partition hash, false otherwise.
+     * @return true if Data has partition hash, false otherwise.
+     */
     boolean hasPartitionHash();
 
+    /**
+     * Returns 64-bit hash code for this Data object.
+     * @return 64-bit hash code
+     */
     long hash64();
 
+    /**
+     * Returns true if this Data is created from a {@link com.hazelcast.nio.serialization.Portable} object,
+     * false otherwise.
+     *
+     * @return true if source object is <tt>Portable</tt>, false otherwise.
+     */
     boolean isPortable();
 
+    /**
+     * Returns byte array representation of header. Header is used to store <tt>Portable</tt> metadata
+     * during serialization and consists of <tt>factoryId</tt>, <tt>classId</tt> and <tt>version</tt> for each
+     * <tt>Portable</tt> field that source object contains.
+     *
+     * @return header
+     */
     byte[] getHeader();
 
+    /**
+     * Returns size of header.
+     *
+     * @return size of header
+     */
     int headerSize();
 
+    /**
+     * Reads an integer header from given offset using given <tt>ByteOrder</tt>.
+     *
+     * @param offset offset of integer header
+     * @param order byte order
+     *
+     * @return integer header
+     */
     int readIntHeader(int offset, ByteOrder order);
-
-    long readLongHeader(int offset, ByteOrder order);
 }
