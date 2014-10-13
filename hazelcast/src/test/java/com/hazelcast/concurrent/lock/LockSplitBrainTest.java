7a7396331e5891939c4e99f9241119dfec874c02
hasancelik <hcelik18@gmail.com>
2014-10-13 10:57:27 +0300

    split brain tests for lock and semaphore

---
+++ hazelcast/src/test/java/com/hazelcast/concurrent/lock/LockSplitBrainTest.java
@@ -0,0 +1,126 @@
+package com.hazelcast.concurrent.lock;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ILock;
+import com.hazelcast.core.LifecycleEvent;
+import com.hazelcast.core.LifecycleListener;
+import com.hazelcast.core.MemberAttributeEvent;
+import com.hazelcast.core.MembershipEvent;
+import com.hazelcast.core.MembershipListener;
+import com.hazelcast.instance.GroupProperties;
+import com.hazelcast.instance.HazelcastInstanceFactory;
+import com.hazelcast.instance.Node;
+import com.hazelcast.instance.TestUtil;
+import com.hazelcast.test.HazelcastTestSupport;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.assertTrue;
+
+public class LockSplitBrainTest extends HazelcastTestSupport {
+
+
+    @Before
+    @After
+    public void killAllHazelcastInstances() throws IOException {
+        HazelcastInstanceFactory.shutdownAll();
+    }
+
+    @Test
+    public void testLockSplitBrain_acquireSameLock() throws InterruptedException {
+        Config config = newConfig();
+        HazelcastInstance h1 = Hazelcast.newHazelcastInstance(config);
+        HazelcastInstance h2 = Hazelcast.newHazelcastInstance(config);
+        HazelcastInstance h3 = Hazelcast.newHazelcastInstance(config);
+        final String key = generateKeyOwnedBy(h3);
+        ILock lock = h3.getLock(key);
+        lock.lock();
+
+        TestMemberShipListener memberShipListener = new TestMemberShipListener(2);
+        h3.getCluster().addMembershipListener(memberShipListener);
+        TestLifeCycleListener lifeCycleListener = new TestLifeCycleListener(1);
+        h3.getLifecycleService().addLifecycleListener(lifeCycleListener);
+
+        closeConnectionBetween(h1,h3);
+        closeConnectionBetween(h2,h3);
+
+        assertOpenEventually(memberShipListener.latch);
+        assertClusterSizeEventually(2, h1);
+        assertClusterSizeEventually(2, h2);
+        assertClusterSizeEventually(1, h3);
+
+        ILock h1Lock = h1.getLock(key);
+        h1Lock.lock();
+
+        lock.forceUnlock();
+
+        assertOpenEventually(lifeCycleListener.latch);
+        assertClusterSizeEventually(3, h1);
+        assertClusterSizeEventually(3, h2);
+        assertClusterSizeEventually(3, h3);
+
+        ILock testLock = h3.getLock(key);
+        assertTrue(testLock.isLocked());
+    }
+
+    private void closeConnectionBetween(HazelcastInstance h1, HazelcastInstance h2) {
+        if (h1 == null || h2 == null) return;
+        final Node n1 = TestUtil.getNode(h1);
+        final Node n2 = TestUtil.getNode(h2);
+        n1.clusterService.removeAddress(n2.address);
+        n2.clusterService.removeAddress(n1.address);
+    }
+
+    private Config newConfig() {
+        Config config = new Config();
+        config.setProperty(GroupProperties.PROP_MERGE_FIRST_RUN_DELAY_SECONDS, "30");
+        config.setProperty(GroupProperties.PROP_MERGE_NEXT_RUN_DELAY_SECONDS, "3");
+        return config;
+    }
+
+    private class TestLifeCycleListener implements LifecycleListener {
+
+        CountDownLatch latch;
+
+        TestLifeCycleListener(int countdown) {
+            latch = new CountDownLatch(countdown);
+        }
+
+        @Override
+        public void stateChanged(LifecycleEvent event) {
+            if (event.getState() == LifecycleEvent.LifecycleState.MERGED) {
+                latch.countDown();
+            }
+        }
+    }
+
+    private class TestMemberShipListener implements MembershipListener {
+
+        final CountDownLatch latch;
+
+        TestMemberShipListener(int countdown) {
+            latch = new CountDownLatch(countdown);
+        }
+
+        @Override
+        public void memberAdded(MembershipEvent membershipEvent) {
+
+        }
+
+        @Override
+        public void memberRemoved(MembershipEvent membershipEvent) {
+            latch.countDown();
+        }
+
+        @Override
+        public void memberAttributeChanged(MemberAttributeEvent memberAttributeEvent) {
+
+        }
+    }
+}
---
+++ hazelcast/src/test/java/com/hazelcast/concurrent/semaphore/SemaphoreSplitBrainTest.java
@@ -0,0 +1,127 @@
+package com.hazelcast.concurrent.semaphore;
+
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ISemaphore;
+import com.hazelcast.core.LifecycleEvent;
+import com.hazelcast.core.LifecycleListener;
+import com.hazelcast.core.MemberAttributeEvent;
+import com.hazelcast.core.MembershipEvent;
+import com.hazelcast.core.MembershipListener;
+import com.hazelcast.instance.GroupProperties;
+import com.hazelcast.instance.HazelcastInstanceFactory;
+import com.hazelcast.instance.Node;
+import com.hazelcast.instance.TestUtil;
+import com.hazelcast.test.HazelcastTestSupport;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SemaphoreSplitBrainTest extends HazelcastTestSupport {
+
+    @Before
+    @After
+    public void killAllHazelcastInstances() throws IOException {
+        HazelcastInstanceFactory.shutdownAll();
+    }
+
+    @Test
+    public void testSemaphoreSplitBrain() throws InterruptedException {
+        Config config = newConfig();
+        HazelcastInstance h1 = Hazelcast.newHazelcastInstance(config);
+        HazelcastInstance h2 = Hazelcast.newHazelcastInstance(config);
+        HazelcastInstance h3 = Hazelcast.newHazelcastInstance(config);
+        final String key = generateKeyOwnedBy(h3);
+        ISemaphore semaphore = h3.getSemaphore(key);
+        semaphore.init(5);
+        semaphore.acquire(3);
+
+        TestMemberShipListener memberShipListener = new TestMemberShipListener(2);
+        h3.getCluster().addMembershipListener(memberShipListener);
+        TestLifeCycleListener lifeCycleListener = new TestLifeCycleListener(1);
+        h3.getLifecycleService().addLifecycleListener(lifeCycleListener);
+
+        closeConnectionBetween(h1, h3);
+        closeConnectionBetween(h2, h3);
+
+        assertOpenEventually(memberShipListener.latch);
+        assertClusterSizeEventually(2, h1);
+        assertClusterSizeEventually(2, h2);
+        assertClusterSizeEventually(1, h3);
+
+        ISemaphore semaphore1 = h1.getSemaphore(key);
+        //when member is down, permits are released.
+        assertEquals(5, semaphore1.availablePermits());
+        semaphore1.acquire(4);
+
+        assertOpenEventually(lifeCycleListener.latch);
+        assertClusterSizeEventually(3, h1);
+        assertClusterSizeEventually(3, h2);
+        assertClusterSizeEventually(3, h3);
+
+        ISemaphore testSemaphore = h3.getSemaphore(key);
+        assertEquals(1, testSemaphore.availablePermits());
+    }
+
+    private void closeConnectionBetween(HazelcastInstance h1, HazelcastInstance h2) {
+        if (h1 == null || h2 == null) return;
+        final Node n1 = TestUtil.getNode(h1);
+        final Node n2 = TestUtil.getNode(h2);
+        n1.clusterService.removeAddress(n2.address);
+        n2.clusterService.removeAddress(n1.address);
+    }
+
+    private Config newConfig() {
+        Config config = new Config();
+        config.setProperty(GroupProperties.PROP_MERGE_FIRST_RUN_DELAY_SECONDS, "30");
+        config.setProperty(GroupProperties.PROP_MERGE_NEXT_RUN_DELAY_SECONDS, "3");
+        return config;
+    }
+
+    private class TestLifeCycleListener implements LifecycleListener {
+
+        CountDownLatch latch;
+
+        TestLifeCycleListener(int countdown) {
+            latch = new CountDownLatch(countdown);
+        }
+
+        @Override
+        public void stateChanged(LifecycleEvent event) {
+            if (event.getState() == LifecycleEvent.LifecycleState.MERGED) {
+                latch.countDown();
+            }
+        }
+    }
+
+    private class TestMemberShipListener implements MembershipListener {
+
+        final CountDownLatch latch;
+
+        TestMemberShipListener(int countdown) {
+            latch = new CountDownLatch(countdown);
+        }
+
+        @Override
+        public void memberAdded(MembershipEvent membershipEvent) {
+
+        }
+
+        @Override
+        public void memberRemoved(MembershipEvent membershipEvent) {
+            latch.countDown();
+        }
+
+        @Override
+        public void memberAttributeChanged(MemberAttributeEvent memberAttributeEvent) {
+
+        }
+    }
+}
