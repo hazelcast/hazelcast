3f8b5633b1066bc1fb7b98c5452749dc8da84346
Ali Gurbuz <ali@hazelcast.com>
2014-10-15 12:33:54 +0300

    fixes #3858 serialization of value moved from abstract record to data/object records, added serializtion test for cache

--- hazelcast/src/main/java/com/hazelcast/cache/impl/record/CacheDataRecord.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/record/CacheDataRecord.java
@@ -16,16 +16,46 @@
 
 package com.hazelcast.cache.impl.record;
 
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.Data;
+
+import java.io.IOException;
 
 class CacheDataRecord
         extends AbstractCacheRecord<Data> {
+
+    private Data value;
 
     CacheDataRecord() {
         super();
     }
 
     CacheDataRecord(Data key, Data value, long expiryTime) {
-        super(key, value, expiryTime);
+        super(key, expiryTime);
+        this.value = value;
+    }
+
+    @Override
+    public Data getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(Data value) {
+        this.value = value;
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+        super.writeData(out);
+        out.writeData(value);
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+        super.readData(in);
+        value = in.readData();
+
     }
 }
