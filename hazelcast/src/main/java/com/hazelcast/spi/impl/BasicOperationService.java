2ebe6cb336f067c20b38d6bae2d859481d5887cc
Jaromir Hamala <jaromir.hamala@gmail.com>
2014-10-16 14:52:57 +0100

    Exponential Back-Off Policy added

--- hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
@@ -31,6 +31,7 @@ import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.partition.InternalPartition;
 import com.hazelcast.partition.InternalPartitionService;
 import com.hazelcast.partition.ReplicaErrorLogger;
+import com.hazelcast.spi.BackoffPolicy;
 import com.hazelcast.spi.BackupAwareOperation;
 import com.hazelcast.spi.ExecutionService;
 import com.hazelcast.spi.ExecutionTracingService;
@@ -137,6 +138,8 @@ final class BasicOperationService implements InternalOperationService {
     private final BackupTimeoutHandlerThread backupTimeoutHandlerThread;
     private volatile boolean shutdown;
 
+    private final BackoffPolicy backoffPolicy;
+
     BasicOperationService(NodeEngineImpl nodeEngine) {
         this.nodeEngine = nodeEngine;
         this.node = nodeEngine.getNode();
@@ -159,7 +162,9 @@ final class BasicOperationService implements InternalOperationService {
         this.operationPacketHandler = new OperationPacketHandler();
         this.responsePacketHandler = new ResponsePacketHandler();
 
-        this.asyncExecutor = executionService.register(ExecutionService.ASYNC_EXECUTOR, coreSize,
+        this.backoffPolicy = new ExponentialBackoffPolicy();
+
+        executionService.register(ExecutionService.ASYNC_EXECUTOR, coreSize,
                 ASYNC_QUEUE_CAPACITY, ExecutorType.CONCRETE);
 
         this.backupTimeoutHandlerThread = new BackupTimeoutHandlerThread();
@@ -351,24 +356,21 @@ final class BasicOperationService implements InternalOperationService {
             packet.setHeader(Packet.HEADER_URGENT);
         }
         Connection connection = node.getConnectionManager().getOrConnect(target);
-        WriteResult sent;
-        int attempts = 10;
-        do {
+
+        WriteResult sent = nodeEngine.send(packet, connection);
+        if (sent != WriteResult.FULL) {
+            return sent == WriteResult.SUCCESS;
+        }
+
+        int maxAttempts = 15;
+        int state = BackoffPolicy.EMPTY_STATE;
+        for (int i = 0; i < maxAttempts; i++) {
+            state = backoffPolicy.apply(state, i);
             sent = nodeEngine.send(packet, connection);
-            if (sent == WriteResult.SUCCESS) {
-                return true;
+            if (sent != WriteResult.FULL) {
+                return sent == WriteResult.SUCCESS;
             }
-            if (sent == WriteResult.FAILURE) {
-                return true;
-            }
-            System.out.println("Packet Send for operation "+op+" has not been successful. Is back-pressure being applied? Still have "+attempts+" attempt to try.");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while (--attempts > 0);
-        System.out.println("Failed to send an operation");
+        }
         return false;
     }
 
