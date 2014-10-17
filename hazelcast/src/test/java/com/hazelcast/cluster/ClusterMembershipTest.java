e1d89198fa413a027d0d97ce2b8564a55e819215
Emin Demirci <emindemirci@gmail.com>
2014-10-17 10:58:33 +0300

    move joiner lock to the NodeRegistry to make all joiners to synchronize on the same object

--- hazelcast/src/test/java/com/hazelcast/cluster/ClusterMembershipTest.java
+++ hazelcast/src/test/java/com/hazelcast/cluster/ClusterMembershipTest.java
@@ -93,6 +93,7 @@ public class ClusterMembershipTest extends HazelcastTestSupport {
                 public void run() {
                     final HazelcastInstance hz = nodeFactory.newHazelcastInstance(config);
                     hz.getMap(mapName);
+                    assertClusterSizeEventually(instanceCount, hz, 20);
                     latch.countDown();
                 }
             });
