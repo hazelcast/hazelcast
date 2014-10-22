8cf9e1b9be43987d4181226a73f25c23b908916d
noctarius <me@noctarius.com>
2014-10-22 17:06:10 +0400

    Deserialization constructor readded

--- hazelcast/src/main/java/com/hazelcast/cache/impl/record/CacheDataRecord.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/record/CacheDataRecord.java
@@ -23,11 +23,16 @@ import com.hazelcast.nio.serialization.Data;
 import java.io.IOException;
 
 /**
- * Implementation of {@link com.hazelcast.cache.impl.record.CacheRecord} where value has an internal serialized format.
+ * Implementation of {@link com.hazelcast.cache.impl.record.CacheRecord} where value has internal serialized format
  */
 class CacheDataRecord extends AbstractCacheRecord<Data> {
 
     private Data value;
+
+    // Deserialization constructor
+    CacheDataRecord() {
+        super();
+    }
 
     CacheDataRecord(Data key, Data value, long expiryTime) {
         super(key, expiryTime);
