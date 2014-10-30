d8b7e3baa80cc0152bd93ad9f7aef283b5df72f3
Peter Veentjer <alarmnummer@gmail.com>
2014-10-30 08:06:20 +0200

    Added some  logging to determine of async back pressure has kicked in

--- hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
@@ -329,7 +329,7 @@ final class BasicOperationService implements InternalOperationService {
 
         // todo: a hack to prevent urgent backup operations be kicked out of this method.
         if (op instanceof BackupOperation && !op.isUrgent()) {
-            System.out.println("Unexpected operation: "+op);
+            System.out.println("Unexpected operation: " + op);
             //throw new IllegalArgumentException("Can't send a Backup operation using the regular send method, op: " + op);
         }
 
@@ -1006,6 +1006,12 @@ final class BasicOperationService implements InternalOperationService {
 
                 if (result == WriteResult.FULL) {
                     fullConnectionEncountered = true;
+
+                    if (lastFullTimeMs + 10000 > System.currentTimeMillis()) {
+                        lastFullTimeMs = System.currentTimeMillis();
+
+                        logger.severe("Back pressure applied on async backup calls");
+                    }
                 }
 
                 if (isSyncBackup) {
@@ -1023,6 +1029,8 @@ final class BasicOperationService implements InternalOperationService {
 
             return syncBackups;
         }
+
+        private volatile long lastFullTimeMs = System.currentTimeMillis();
 
         private Backup newBackup(BackupAwareOperation backupAwareOp, long[] replicaVersions,
                                  int replicaIndex, boolean isSyncBackup) {
