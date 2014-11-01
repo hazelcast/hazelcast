e3df012a4e7b86197d1911eae2ecce6e5c895fd3
Peter Veentjer <alarmnummer@gmail.com>
2014-11-01 11:45:42 +0200

    Improved debug logging regarding sync calls

--- hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
@@ -987,6 +987,7 @@ final class BasicOperationService implements InternalOperationService {
 
         private final AtomicLong backupCounter = new AtomicLong();
         private final AtomicLong fullCOunter = new AtomicLong();
+        private final AtomicLong notFullCounter = new AtomicLong();
 
         /**
          * Makes the actual backup.
@@ -1043,10 +1044,12 @@ final class BasicOperationService implements InternalOperationService {
             if (fullConnectionEncountered) {
                 syncBackups += asyncBackups;
                 fullCOunter.incrementAndGet();
+            }else{
+                notFullCounter.incrementAndGet();
             }
 
             if ((backupCounter.incrementAndGet() % 20000) == 0) {
-                logger.info(backupCounter.get() + "  Backups sync = " + syncBackups + " fullCounter: " + fullCOunter);
+                logger.info(backupCounter.get() + "  Backups sync = " + syncBackups + " fullCounter: " + fullCOunter+" notFullCounter: "+notFullCounter);
             }
 
             return syncBackups;
