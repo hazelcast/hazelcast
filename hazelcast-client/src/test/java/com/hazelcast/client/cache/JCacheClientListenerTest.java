f3810c4f5b5061b0c74a23ef0517aae1429d75fb
Mehmet Dogan <mehmet@hazelcast.com>
2014-11-03 16:13:04 +0200

    Fixed jcache sync listener test

--- hazelcast-client/src/test/java/com/hazelcast/client/cache/JCacheClientListenerTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/cache/JCacheClientListenerTest.java
@@ -16,144 +16,43 @@
 
 package com.hazelcast.client.cache;
 
+import com.hazelcast.cache.JCacheListenerTest;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.cache.impl.HazelcastClientCachingProvider;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.instance.HazelcastInstanceFactory;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import javax.cache.Cache;
-import javax.cache.CacheManager;
-import javax.cache.configuration.CompleteConfiguration;
-import javax.cache.configuration.FactoryBuilder;
-import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
-import javax.cache.configuration.MutableConfiguration;
-import javax.cache.event.CacheEntryCreatedListener;
-import javax.cache.event.CacheEntryEvent;
-import javax.cache.event.CacheEntryExpiredListener;
-import javax.cache.event.CacheEntryListenerException;
-import javax.cache.event.CacheEntryRemovedListener;
-import javax.cache.event.CacheEntryUpdatedListener;
-import java.io.Serializable;
-import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
+import javax.cache.spi.CachingProvider;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
-public class JCacheClientListenerTest {
+public class JCacheClientListenerTest extends JCacheListenerTest {
 
-    @Test
-    public void testSyncListenerFromClient() throws Exception {
-        try {
-            HazelcastInstance hz1 = Hazelcast.newHazelcastInstance();
-            HazelcastInstance hz2 = Hazelcast.newHazelcastInstance();
-
-            ClientConfig clientConfig = new ClientConfig();
-            clientConfig.getNetworkConfig().addAddress("127.0.0.1");
-
-            HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-
-            HazelcastClientCachingProvider cachingProvider = HazelcastClientCachingProvider.createCachingProvider(client);
-            CacheManager cacheManager = cachingProvider.getCacheManager();
-
-            AtomicInteger counter = new AtomicInteger();
-
-            CompleteConfiguration<String, String> config = new MutableConfiguration<String, String>()
-                    .setTypes(String.class, String.class)
-                    .addCacheEntryListenerConfiguration(
-                            new MutableCacheEntryListenerConfiguration<String, String>(
-                                    FactoryBuilder.factoryOf(new TestListener(counter)), null, true, true
-                            )
-                    );
-
-            final Cache<String, String> cache = cacheManager.createCache("test", config);
-
-            final CountDownLatch[] latches = new CountDownLatch[10];
-            for (int o = 0; o < latches.length; o++) {
-                final int index = o;
-                latches[o] = new CountDownLatch(1);
-                new Thread() {
-                    public void run() {
-                        for (int i = 0; i < 100000; i++) {
-                            String key = String.valueOf(i % 10);
-                            String value = UUID.randomUUID().toString();
-                            System.out.println("key=" + key + ", value=" + value);
-                            cache.put(key, value);
-                        }
-                        latches[index].countDown();
-                    }
-                }.start();
-            }
-
-            for (int o = 0; o < latches.length; o++) {
-                latches[o].await();
-            }
-
-            System.out.println("Listener invoked locally: " + counter.get());
-
-        } finally {
-            HazelcastClient.shutdownAll();
-            Hazelcast.shutdownAll();
-        }
+    @Before
+    @After
+    public void cleanup() {
+        HazelcastClient.shutdownAll();
+        HazelcastInstanceFactory.terminateAll();
     }
 
-    public static class TestListener
-            implements CacheEntryCreatedListener<String, String>,
-                       CacheEntryUpdatedListener<String, String>,
-                       CacheEntryRemovedListener<String, String>,
-                       CacheEntryExpiredListener<String, String>,
-                       Serializable {
+    @Override
+    protected CachingProvider getCachingProvider() {
+        Hazelcast.newHazelcastInstance();
+        Hazelcast.newHazelcastInstance();
 
-        private final AtomicInteger counter;
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().addAddress("127.0.0.1");
 
-        public TestListener(AtomicInteger counter) {
-            this.counter = counter;
-        }
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
 
-        @Override
-        public void onCreated(Iterable<CacheEntryEvent<? extends String, ? extends String>> cacheEntryEvents)
-                throws CacheEntryListenerException {
-
-            for (CacheEntryEvent cacheEntryEvent : cacheEntryEvents) {
-                System.out.println(cacheEntryEvent);
-                counter.incrementAndGet();
-            }
-        }
-
-        @Override
-        public void onExpired(Iterable<CacheEntryEvent<? extends String, ? extends String>> cacheEntryEvents)
-                throws CacheEntryListenerException {
-
-            for (CacheEntryEvent cacheEntryEvent : cacheEntryEvents) {
-                System.out.println(cacheEntryEvent);
-                counter.incrementAndGet();
-            }
-        }
-
-        @Override
-        public void onRemoved(Iterable<CacheEntryEvent<? extends String, ? extends String>> cacheEntryEvents)
-                throws CacheEntryListenerException {
-
-            for (CacheEntryEvent cacheEntryEvent : cacheEntryEvents) {
-                System.out.println(cacheEntryEvent);
-                counter.incrementAndGet();
-            }
-        }
-
-        @Override
-        public void onUpdated(Iterable<CacheEntryEvent<? extends String, ? extends String>> cacheEntryEvents)
-                throws CacheEntryListenerException {
-
-            for (CacheEntryEvent cacheEntryEvent : cacheEntryEvents) {
-                System.out.println(cacheEntryEvent);
-                counter.incrementAndGet();
-            }
-        }
+        return HazelcastClientCachingProvider.createCachingProvider(client);
     }
 }
