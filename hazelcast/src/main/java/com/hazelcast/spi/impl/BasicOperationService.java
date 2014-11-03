a2788aeb9db0ca46c68855d9a99e4b37e001fee3
Peter Veentjer <alarmnummer@gmail.com>
2014-11-03 15:25:48 +0200

    More logging

--- hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
@@ -1030,6 +1030,8 @@ final class BasicOperationService implements InternalOperationService {
                 boolean syncBackup = replicaIndex <= desiredSyncBackups;
                 if (!syncBackup) {
                     Connection connection = node.getConnectionManager().getOrConnect(target);
+                    logger.severe("connection.isFull:"+connection.isFull());
+
                     if(connection.isFull()){
                         syncBackup = true;
                         fullConnectionEncountered = true;
