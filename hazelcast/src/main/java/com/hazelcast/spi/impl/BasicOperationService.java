9b4d9cee730b95a188585564184c931759bf9d87
Peter Veentjer <alarmnummer@gmail.com>
2014-11-03 14:57:11 +0200

    isSyncBackup=true is put in comment

--- hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/BasicOperationService.java
@@ -1039,7 +1039,7 @@ final class BasicOperationService implements InternalOperationService {
                 if (!isSyncBackup) {
                     Connection connection = node.getConnectionManager().getOrConnect(target);
                     if(connection.isFull()){
-                        isSyncBackup = true;
+                        //isSyncBackup = true;
                         fullConnectionEncountered = true;
                     }
                 }
@@ -1068,9 +1068,9 @@ final class BasicOperationService implements InternalOperationService {
                 notFullCounter.incrementAndGet();
             }
 
-           // if ((backupCounter.incrementAndGet() % 20) == 0) {
+            if ((backupCounter.incrementAndGet() % 20) == 0) {
                 logger.info(backupCounter.get() + "  Backups sync = " + syncBackups + " fullCounter: " + fullCOunter + " notFullCounter: " + notFullCounter);
-           // }
+            }
 
             logger.severe("syncBackup:" + syncBackups);
 
