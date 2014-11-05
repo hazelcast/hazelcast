527d8ee3f796b3fa9e59b4109cecde128b785248
Ahmet MIRCIK <ahmet@hazelcast.com>
2014-11-05 15:54:20 +0200

    fix eviction max size policy test

--- hazelcast/src/test/java/com/hazelcast/map/EvictionMaxSizePolicyTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/EvictionMaxSizePolicyTest.java
@@ -10,6 +10,7 @@ import com.hazelcast.map.proxy.MapProxyImpl;
 import com.hazelcast.nio.Address;
 import com.hazelcast.partition.InternalPartitionService;
 import com.hazelcast.spi.NodeEngine;
+import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.QuickTest;
@@ -28,7 +29,6 @@ import static org.junit.Assert.assertTrue;
 public class EvictionMaxSizePolicyTest extends HazelcastTestSupport {
 
     private static final int PARTITION_COUNT = 271;
-
 
     @Test
     public void testPerNodePolicy() {
@@ -209,42 +209,65 @@ public class EvictionMaxSizePolicyTest extends HazelcastTestSupport {
     }
 
 
-    private void assertPerNodePolicyWorks(Collection<IMap> maps, int perNodeMaxSize, int nodeCount) {
+    private void assertPerNodePolicyWorks(Collection<IMap> maps, final int perNodeMaxSize, final int nodeCount) {
         final int mapSize = getSize(maps);
         final String message = String.format("map size is %d and it should be smaller "
                         + "than perNodeMaxSize * nodeCount which is %d",
                 mapSize, perNodeMaxSize * nodeCount);
 
-        assertTrue(message, mapSize <= perNodeMaxSize * nodeCount);
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertTrue(message, mapSize <= perNodeMaxSize * nodeCount);
+            }
+        });
+
     }
 
 
-    private void assertPerPartitionPolicyWorks(Collection<IMap> maps, int perPartitionMaxSize) {
+    private void assertPerPartitionPolicyWorks(Collection<IMap> maps, final int perPartitionMaxSize) {
         final int mapSize = getSize(maps);
         final String message = String.format("map size is %d and it should be smaller "
                         + "than perPartitionMaxSize * PARTITION_COUNT which is %d",
                 mapSize, perPartitionMaxSize * PARTITION_COUNT);
 
-        assertTrue(message, mapSize <= perPartitionMaxSize * PARTITION_COUNT);
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertTrue(message, mapSize <= perPartitionMaxSize * PARTITION_COUNT);
+            }
+        });
+
     }
 
-    private void assertUsedHeapSizePolicyWorks(Collection<IMap> maps, int maxSizeInMegaBytes) {
+    private void assertUsedHeapSizePolicyWorks(Collection<IMap> maps, final int maxSizeInMegaBytes) {
         final long heapCost = getHeapCost(maps);
         final String message = String.format("heap cost is %d and it should be smaller "
                         + "than allowed max heap size %d in bytes",
                 heapCost, convertMegaBytesToBytes(maxSizeInMegaBytes));
 
-        assertTrue(message, heapCost <= convertMegaBytesToBytes(maxSizeInMegaBytes));
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertTrue(message, heapCost <= convertMegaBytesToBytes(maxSizeInMegaBytes));
+            }
+        });
     }
 
-    private void assertUsedHeapPercentagePolicyTriggersEviction(Collection<IMap> maps, int putCount) {
+    private void assertUsedHeapPercentagePolicyTriggersEviction(Collection<IMap> maps, final int putCount) {
         final int size = getSize(maps);
         final long heapCost = getHeapCost(maps);
         final String message = String.format("map size is %d, heap cost is %d in "
                         + "bytes but total memory is %d in bytes",
                 size, heapCost, Runtime.getRuntime().totalMemory());
 
-        assertTrue(message, size < putCount);
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertTrue(message, size < putCount);
+            }
+        });
+
     }
 
 
