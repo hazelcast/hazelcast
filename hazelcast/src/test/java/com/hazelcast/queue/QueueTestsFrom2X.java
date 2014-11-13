68622e76c1ef9772a82f38eff799035c89182d66
Ahmet MIRCIK <ahmet@hazelcast.com>
2014-11-13 10:26:22 +0200

    java 8 fix

--- hazelcast/src/test/java/com/hazelcast/queue/QueueTestsFrom2X.java
+++ hazelcast/src/test/java/com/hazelcast/queue/QueueTestsFrom2X.java
@@ -57,7 +57,7 @@ public class QueueTestsFrom2X extends HazelcastTestSupport {
         final CountDownLatch latch = new CountDownLatch(8);
         final String value = "hello";
         final HazelcastInstance instance = createHazelcastInstance();
-        IQueue<String> queue = instance.getQueue("testQueueItemListener");
+        IQueue queue = instance.getQueue("testQueueItemListener");
         queue.addItemListener(new ItemListener<String>() {
             public void itemAdded(ItemEvent<String> itemEvent) {
                 assertEquals(value, itemEvent.getItem());
@@ -237,7 +237,7 @@ public class QueueTestsFrom2X extends HazelcastTestSupport {
 
     @Test
     public void issue427QOfferIncorrectWithinTransaction() {
-         Config config = new Config();
+        Config config = new Config();
         config.getQueueConfig("default").setMaxSize(100);
         HazelcastInstance h = createHazelcastInstance(config);
         final TransactionContext transactionContext = h.newTransactionContext();
