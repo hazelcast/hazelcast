253a1f3e5ca802d97fdd1f5fdd5971e90244641c
Emin Demirci <emindemirci@gmail.com>
2014-11-18 12:21:09 +0200

    added missing return to prevent node tries to connect itself when network partition happens

--- hazelcast/src/main/java/com/hazelcast/cluster/ClusterServiceImpl.java
+++ hazelcast/src/main/java/com/hazelcast/cluster/ClusterServiceImpl.java
@@ -719,6 +719,7 @@ public final class ClusterServiceImpl implements ClusterService, ConnectionListe
         if (node.getThisAddress().equals(masterAddress)) {
             logger.finest("Ignoring master response: " + masterAddress + " from: " + callerAddress
                     + ". This node is already master...");
+            return;
         }
 
         lock.lock();
