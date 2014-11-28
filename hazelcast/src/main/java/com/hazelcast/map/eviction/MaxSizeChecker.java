b96e4af66a2c140164bd62b7599eff0ba055c1d3
ajermakovics <ajermakovics@users.noreply.github.com>
2014-11-28 10:20:25 +0000

    Adding LRU policy to eviction test. Removing isLoaded check during map eviction. #4066

--- hazelcast/src/main/java/com/hazelcast/map/eviction/MaxSizeChecker.java
+++ hazelcast/src/main/java/com/hazelcast/map/eviction/MaxSizeChecker.java
@@ -173,7 +173,7 @@ public class MaxSizeChecker {
     /**
      * used when deciding evictable or not.
      */
-    private int getApproximateMaxSize(int maxSizeFromConfig) {
+    public static int getApproximateMaxSize(int maxSizeFromConfig) {
         // because not to exceed the max size much we start eviction early.
         // so decrease the max size with ratio .95 below
         return maxSizeFromConfig * EVICTION_START_THRESHOLD_PERCENTAGE / ONE_HUNDRED_PERCENT;
