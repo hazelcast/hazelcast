7b2fcdf2d9dc4f36d8fed5a9fbb0f451e594b7db
ajermakovics <ajermakovics@users.noreply.github.com>
2014-11-28 10:20:25 +0000

    Adding LRU policy to eviction test. Removing isLoaded check during map eviction. #4066

--- hazelcast/src/test/java/com/hazelcast/map/mapstore/MapStoreEvictionTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/mapstore/MapStoreEvictionTest.java
@@ -1,10 +1,8 @@
 package com.hazelcast.map.mapstore;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.GroupConfig;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MapStoreConfig;
@@ -20,6 +18,12 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static com.hazelcast.test.TimeConstants.MINUTE;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -28,40 +32,42 @@ import org.junit.runner.RunWith;
 @Category(QuickTest.class)
 public class MapStoreEvictionTest extends HazelcastTestSupport {
 
-    private int mapStoreEntryCount = 10 * 1000;
+    private int mapStoreEntryCount = 1000;
     private int nodes = 2;
     private int maxSizePerNode = mapStoreEntryCount / 4;
     private int maxSizePerCluster = maxSizePerNode * nodes;
     private AtomicInteger loadedValueCount = new AtomicInteger();
     private TestHazelcastInstanceFactory nodeFactory = createHazelcastInstanceFactory(nodes);
 
-    @Test(timeout = 120000)
+    @Test(timeout = 2 * MINUTE)
     public void testLoadsAll_whenEvictionDisabled() throws Exception {
         Config cfg = newConfig("map", false);
 
-        HazelcastInstance instances[] = nodeFactory.newInstances(cfg);
-        assertClusterSizeEventually(nodes, instances[0]);
-        IMap<Object, Object> map = instances[0].getMap("map");
+        HazelcastInstance hz = nodeFactory.newInstances(cfg)[0];
+        assertClusterSizeEventually(nodes, hz);
+        IMap<Object, Object> map = hz.getMap("map");
+        waitClusterForSafeState(hz);
 
-        assertEquals(mapStoreEntryCount, loadedValueCount.get());
         assertEquals(mapStoreEntryCount, map.size());
+        assertEquals(mapStoreEntryCount, loadedValueCount.get());
     }
 
-    @Test(timeout = 120000)
+    @Test(timeout = 2 * MINUTE)
     public void testLoadsLessThanMaxSize_whenEvictionEnabled() throws Exception {
         Config cfg = newConfig("map", true);
 
-        HazelcastInstance instances[] = nodeFactory.newInstances(cfg);
-        assertClusterSizeEventually(nodes, instances[0]);
-        IMap<Object, Object> map = instances[0].getMap("map");
+        HazelcastInstance hz = nodeFactory.newInstances(cfg)[0];
+        assertClusterSizeEventually(nodes, hz);
+        IMap<Object, Object> map = hz.getMap("map");
+        waitAllForSafeState();
 
-        assertEquals(maxSizePerCluster, loadedValueCount.get());
+        assertFalse(map.isEmpty());
         assertTrue(maxSizePerCluster >= map.size());
+        assertTrue(maxSizePerCluster >= loadedValueCount.get());
     }
 
     private Config newConfig(String mapName, boolean sizeLimited) {
         Config cfg = new Config();
-
         cfg.setGroupConfig(new GroupConfig(getClass().getSimpleName()));
 
         MapStoreConfig mapStoreConfig = new MapStoreConfig()
@@ -73,6 +79,7 @@ public class MapStoreEvictionTest extends HazelcastTestSupport {
         if( sizeLimited ) {
             MaxSizeConfig maxSizeConfig = new MaxSizeConfig(maxSizePerNode, MaxSizeConfig.MaxSizePolicy.PER_NODE);
             mapConfig.setMaxSizeConfig(maxSizeConfig);
+            mapConfig.setEvictionPolicy(EvictionPolicy.LRU);
         }
 
         return cfg;
