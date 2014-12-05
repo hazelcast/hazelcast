f26ef33badcbdba6a8c49ad073c7322cea7a03d2
serkanozal <serkan@hazelcast.com>
2014-12-05 21:15:59 +0200

    "EntryCountCacheMaxSizeChecker" updated as our estimated max partition size based formula by using std-dev

--- hazelcast/src/main/java/com/hazelcast/cache/impl/maxsize/impl/EntryCountCacheMaxSizeChecker.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/maxsize/impl/EntryCountCacheMaxSizeChecker.java
@@ -10,38 +10,25 @@ import com.hazelcast.config.CacheMaxSizeConfig;
  */
 public class EntryCountCacheMaxSizeChecker implements CacheMaxSizeChecker {
 
-    private static final float ENTRY_COUNT_FACTOR = 1.0F;
-
-    private final int maxEntryCount;
     private final CacheRecordMap cacheRecordMap;
-    private final int partitionCount;
+    private final int maxPartitionSize;
 
     public EntryCountCacheMaxSizeChecker(CacheMaxSizeConfig maxSizeConfig,
             CacheRecordMap cacheRecordMap, int partitionCount) {
-        this.maxEntryCount = maxSizeConfig.getSize();
         this.cacheRecordMap = cacheRecordMap;
-        this.partitionCount = partitionCount;
+        this.maxPartitionSize = calculateMaxPartitionSize(maxSizeConfig.getSize(), partitionCount);
+    }
+
+    protected int calculateMaxPartitionSize(int maxEntryCount, int partitionCount) {
+        final double balancedPartitionSize = (double) maxEntryCount / (double) partitionCount;
+        final double approximatedStdDev = Math.sqrt(balancedPartitionSize);
+        final int stdDevMultiplier = maxEntryCount <= 4000 ? 5 : 3;
+        return (int) ((approximatedStdDev * stdDevMultiplier) + balancedPartitionSize);
     }
 
     @Override
     public boolean isReachedToMaxSize() {
-        /**
-         *  Estimated entry count can be calculated dynamically as "e = (s x p) + sqrt(s)"
-         *  where
-         *      e : entry count
-         *      s : size of current record store
-         *      p : partition count
-         *
-         * Like this:
-         *      final int s = cacheRecordMap.size();
-         *      final int p = partitionCount;
-         *      final int estimatedSize = (int) ((s * p) + Math.sqrt(s));
-         *
-         * See discussion at "https://hazelcast.atlassian.net/wiki/display/EN/JCache+Eviction".
-         */
-
-        final int estimatedSize = (int) ((cacheRecordMap.size() * partitionCount) * ENTRY_COUNT_FACTOR);
-        return estimatedSize >= maxEntryCount;
+        return cacheRecordMap.size() >= maxPartitionSize;
     }
 
 }
