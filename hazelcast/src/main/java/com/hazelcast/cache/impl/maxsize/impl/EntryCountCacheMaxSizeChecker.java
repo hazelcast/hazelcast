2ec946452ae1269118ed20464176b5844fe2eb5b
serkanozal <serkan@hazelcast.com>
2014-12-10 11:05:10 +0200

    "EntryCountCacheMaxSizeChecker" updated and refactored as std. dev. multiplier usage for maximum 1.000.000 entry count

--- hazelcast/src/main/java/com/hazelcast/cache/impl/maxsize/impl/EntryCountCacheMaxSizeChecker.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/maxsize/impl/EntryCountCacheMaxSizeChecker.java
@@ -9,25 +9,43 @@ import com.hazelcast.cache.impl.record.CacheRecordMap;
  */
 public class EntryCountCacheMaxSizeChecker implements CacheMaxSizeChecker {
 
+    private static final int MAX_ENTRY_COUNT_FOR_THRESHOLD_USAGE = 1000000;
+    private static final int STD_DEV_OF_5_THRESHOLD = 4000;
+    private static final int STD_DEV_MULTIPLIER_5 = 5;
+    private static final int STD_DEV_MULTIPLIER_3 = 3;
+
     private final CacheRecordMap cacheRecordMap;
     private final int maxPartitionSize;
 
     public EntryCountCacheMaxSizeChecker(final int size,
-            final CacheRecordMap cacheRecordMap, int partitionCount) {
+            final CacheRecordMap cacheRecordMap, final int partitionCount) {
         this.cacheRecordMap = cacheRecordMap;
         this.maxPartitionSize = calculateMaxPartitionSize(size, partitionCount);
     }
 
     // Defined as "public static" since external tests can test the logic without knowing the logic
     // for calculating the estimated max size.
-    //CHECKSTYLE:OFF
     public static int calculateMaxPartitionSize(int maxEntryCount, int partitionCount) {
         final double balancedPartitionSize = (double) maxEntryCount / (double) partitionCount;
         final double approximatedStdDev = Math.sqrt(balancedPartitionSize);
-        final int stdDevMultiplier = maxEntryCount <= 4000 ? 5 : 3;
+        int stdDevMultiplier;
+
+        if (maxEntryCount <= STD_DEV_OF_5_THRESHOLD) {
+            // Below 4.000 entry count, multiplying standard deviation with 5 gives better estimations
+            // as our experiments and test results.
+            stdDevMultiplier = STD_DEV_MULTIPLIER_5;
+        } else if (maxEntryCount > STD_DEV_OF_5_THRESHOLD
+                && maxEntryCount <= MAX_ENTRY_COUNT_FOR_THRESHOLD_USAGE) {
+            // Above 4.000 entry count but below 1.000.000 entry count,
+            // multiplying standard deviation with 3 gives better estimations
+            // as our experiments and test results.
+            stdDevMultiplier = STD_DEV_MULTIPLIER_3;
+        } else {
+            // Over 1.000.000 entries, there is no need for using standard deviation
+            stdDevMultiplier = 0;
+        }
         return (int) ((approximatedStdDev * stdDevMultiplier) + balancedPartitionSize);
     }
-    //CHECKSTYLE:ON
 
     @Override
     public boolean isReachedToMaxSize() {
