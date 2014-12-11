a0e5421d53a675bd1f643c2e629f65b2c4eef9d8
David Strom <david.strom@hazelcast.com>
2014-12-10 18:41:42 -0800

    copyedit

--- hazelcast/src/main/java/com/hazelcast/core/IQueue.java
+++ hazelcast/src/main/java/com/hazelcast/core/IQueue.java
@@ -24,8 +24,8 @@ import java.util.concurrent.TimeUnit;
 /**
  * Concurrent, blocking, distributed, observable queue.
  *
- * The IQueue is not a partitioned data-structure. So all the content of the IQueue is stored in a single machine (and
- * in the backup). So the IQueue will not scale by adding more members in the cluster.
+ * The IQueue is not a partitioned data-structure. All the content of the IQueue is stored in a single machine (and
+ * in the backup). The IQueue will not scale by adding more members in the cluster.
  *
  * @see BaseQueue
  * @see java.util.Queue
@@ -40,7 +40,7 @@ public interface IQueue<E> extends BlockingQueue<E>, BaseQueue<E>, ICollection<E
      * methods here to prevent wrong method return type issue when
      * compiled with java 8.
      *
-     * For additional details see;
+     * For additional details see:
      *
      * http://mail.openjdk.java.net/pipermail/compiler-dev/2014-November/009139.html
      * https://bugs.openjdk.java.net/browse/JDK-8064803
