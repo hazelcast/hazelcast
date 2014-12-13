39be4831414e797e98bfc1159baa3ecbf73f729e
David Strom <david.strom@hazelcast.com>
2014-12-12 17:04:21 -0800

    copyedit

--- hazelcast/src/main/java/com/hazelcast/core/PartitionService.java
+++ hazelcast/src/main/java/com/hazelcast/core/PartitionService.java
@@ -20,7 +20,7 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
- * PartitionService allows to query {@link Partition}s and attach/detach {@link MigrationListener}s to listen to partition
+ * PartitionService allows you to query {@link Partition}s and attach/detach {@link MigrationListener}s to listen to partition
  * migration events.
  *
  * The methods on the PartitionService are thread-safe.
@@ -33,22 +33,22 @@ public interface PartitionService {
     /**
      * Returns a set containing all the {@link Partition}s in the cluster.
      *
-     * @return all partitions
+     * @return all partitions in the cluster
      */
     Set<Partition> getPartitions();
 
     /**
-     * Returns partition which given key belongs to.
+     * Returns the partition that the given key belongs to.
      *
-     * @param key key
-     * @return partition which given key belongs to
+     * @param key the given key
+     * @return the partition that the given key belongs to
      * todo: what about null.
      */
     Partition getPartition(Object key);
 
     /**
      * Generates a random partition key. This is useful if you want to partition data in the same partition,
-     * but don't care which partition it is going to be.
+     * but you do not care which partition it is going to be.
      * <p/>
      * The returned value will never be null.
      *
@@ -64,8 +64,8 @@ public interface PartitionService {
      * <p/>
      * There is no check for duplicate registrations, so if you register the listener twice, it will get events twice.
      *
-     * @param migrationListener listener
-     * @return returns registration id.
+     * @param migrationListener the added MigrationListener
+     * @return returns the registration id for the MigrationListener.
      * @throws java.lang.NullPointerException if migrationListener is null.
      * @throws UnsupportedOperationException  if this operation isn't supported. For example on the client side it isn't possible
      *                                        to add a MigrationListener.
@@ -80,46 +80,48 @@ public interface PartitionService {
      * <p/>
      * This method can safely be called multiple times for the same registration-id; every subsequent call is just ignored.
      *
-     * @param registrationId Id of listener registration.
-     * @return true if listener is removed, false otherwise.
+     * @param registrationId the registration id of the listener to remove.
+     * @return true if the listener is removed, false otherwise.
      * @throws java.lang.NullPointerException if registrationId is null.
-     * @throws UnsupportedOperationException  if this operation isn't supported. For example on the client side it isn't possible
+     * @throws UnsupportedOperationException  if this operation isn't supported. For example, on the client side it isn't possible
      *                                        to add/remove a MigrationListener.
      * @see #addMigrationListener(MigrationListener)
      */
     boolean removeMigrationListener(String registrationId);
 
     /**
-     * Checks whether the cluster is in a safe state. When in a safe state
+     * Checks whether the cluster is in a safe state. When in a safe state,
      * it is permissible to shut down a server instance.
      *
-     * @return <code>true</code> if there are no partitions being migrated, and there are sufficient backups
-     * for each partition per the configuration, otherwise <code>false</code>.
+     * @return true if there are no partitions being migrated, and there are sufficient backups
+     * for each partition per the configuration; false otherwise.
      * @since 3.3
      */
     boolean isClusterSafe();
 
     /**
-     * Check if the given member is safe to shutdown, means check if at least one backup of the partitions
-     * those owned by given member are in sync with primary.
+     * Check if the given member is safe to shutdown, meaning check if at least one backup of the partitions
+     * owned by the given member are in sync with primary.
      *
-     * @param member Cluster member to query.
-     * @return <code>true</code> if member in a safe state, other wise <code>false</code>.
+     * @param member the cluster member to query.
+     * @return true if the member is in a safe state, false otherwise.
      * @since 3.3
      */
     boolean isMemberSafe(Member member);
 
     /**
-     * Check if local member is safe to shutdown, means check if at least one backup of the partitions
-     * those owned by local member are in sync with primary.
+     * Check if the local member is safe to shutdown, meaning check if at least one backup of the partitions
+     * owned by the local member are in sync with primary.
      *
      * @since 3.3
      */
     boolean isLocalMemberSafe();
 
     /**
-     * Force local member to be safe by checking and syncing partitions those owned by local member
+     * Force the local member to be safe by checking and syncing partitions owned by the local member
      * with at least one of the backups.
+     * @param timeout the time limit for checking/syncing with the backup
+     * @param unit the unit of time for timeout
      *
      * @since 3.3
      */
