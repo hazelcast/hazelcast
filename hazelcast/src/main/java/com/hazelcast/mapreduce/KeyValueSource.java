f6c94edc45a65baae4a3ca4afbaff1f3ed10bd6f
David Strom <david.strom@hazelcast.com>
2014-12-19 12:13:26 -0800

    copyedit

--- hazelcast/src/main/java/com/hazelcast/mapreduce/KeyValueSource.java
+++ hazelcast/src/main/java/com/hazelcast/mapreduce/KeyValueSource.java
@@ -36,7 +36,7 @@ import java.util.Map;
  * The abstract KeyValueSource class is used to implement custom data sources for mapreduce algorithms.<br/>
  * Default shipped implementations contains KeyValueSources for Hazelcast data structures like
  * {@link com.hazelcast.core.IMap} and {@link com.hazelcast.core.MultiMap}. Custom implementations could
- * be external files, URLs or any other data source can be visualized as key-value pairs.
+ * be external files, URLs or any other data source that can be visualized as key-value pairs.
  *
  * @param <K> key type
  * @param <V> value type
@@ -47,58 +47,59 @@ public abstract class KeyValueSource<K, V>
         implements Closeable {
 
     /**
-     * This method is called before accessing the key-value pairs of this KeyValueSource
+     * This method is called before accessing the key-value pairs of this KeyValueSource.
      *
      * @param nodeEngine nodeEngine of this cluster node
-     * @return true if operation succeed otherwise false
+     * @return true if the operation succeeded, false otherwise
      */
     public abstract boolean open(NodeEngine nodeEngine);
 
     /**
      * Called to request if at least one more key-value pair is available from this
-     * data source. If so this method returns true otherwise it false.
+     * data source. If so, this method returns true, otherwise it returns false.
      *
-     * @return true if at least one more value is available otherwise false
+     * @return true if at least one more key-value pair is available from this
+     * data source, false otherwise
      */
     public abstract boolean hasNext();
 
     /**
-     * Returns the current index' key for {@link KeyPredicate} analysis. This is called
+     * Returns the current index key for {@link KeyPredicate} analysis. This is called
      * to prevent a possible deserialization of unneeded values because the key is not
      * interesting for the running mapreduce algorithm.
      *
-     * @return current index' key
+     * @return the current index key for {@link KeyPredicate} analysis
      */
     public abstract K key();
 
     /**
-     * Returns the current index' element
+     * Returns the current index element
      *
-     * @return current index' element
+     * @return the current index element
      */
     public abstract Map.Entry<K, V> element();
 
     /**
-     * This method need to reset all internal state as it would be a new instance at all.
+     * This method resets all internal state to be a new instance.
      * The same instance of the KeyValueSource may be used multiple times in a row depending
      * on the internal implementation, especially when the KeyValueSource implements
      * {@link com.hazelcast.mapreduce.PartitionIdAware}.<br/>
-     * If the instance is reused a sequence of {@link #reset()}, {@link #open(com.hazelcast.spi.NodeEngine)}
+     * If the instance is reused, a sequence of reset(), {@link #open(com.hazelcast.spi.NodeEngine)}
      * and {@link #close()} is called multiple times with the other methods between open(...) and close().
      *
-     * @return true if reset was successful otherwise false
+     * @return true if reset was successful, false otherwise
      */
     public abstract boolean reset();
 
     /**
      * <p>
-     * If {@link #isAllKeysSupported()} returns true a call to this method has to return
-     * all clusterwide available keys. If there is no chance to precollect all keys do to
-     * partitioning of the data {@link #isAllKeysSupported()} must return false.<br/>
+     * If {@link #isAllKeysSupported()} returns true, a call to this method returns
+     * all clusterwide available keys. If there is no chance to precollect all keys due to
+     * partitioning of the data {@link #isAllKeysSupported()}, this method returns false.<br/>
      * </p>
      * <p>
      * If this functionality is not available and {@link Job#onKeys(Object[])},
-     * {@link Job#onKeys(Iterable)} or {@link Job#keyPredicate(KeyPredicate)} is used a
+     * {@link Job#onKeys(Iterable)}, or {@link Job#keyPredicate(KeyPredicate)} is used, a
      * preselection of the interesting partitions / nodes is not available and the
      * overall processing speed my be degraded.
      * </p>
@@ -119,26 +120,26 @@ public abstract class KeyValueSource<K, V>
     /**
      * <p>
      * If it is possible to collect all clusterwide available keys for this KeyValueSource
-     * implementation then this method should return true.<br/>
-     * If true is returned a call to {@link #getAllKeys()} must return all available keys
+     * implementation then this method returns true.<br/>
+     * If true is returned, a call to {@link #getAllKeys()} must return all available keys
      * to execute a preselection of interesting partitions / nodes based on returns keys.
      * </p>
      * <p>
      * If this functionality is not available and {@link Job#onKeys(Object[])},
-     * {@link Job#onKeys(Iterable)} or {@link Job#keyPredicate(KeyPredicate)} is used a
+     * {@link Job#onKeys(Iterable)}, or {@link Job#keyPredicate(KeyPredicate)} is used, a
      * preselection of the interesting partitions / nodes is not available and the
      * overall processing speed my be degraded.
      * </p>
      *
-     * @return true if collecting clusterwide keys is available otherwide false
+     * @return true if collecting clusterwide keys is available, false otherwise
      */
     public boolean isAllKeysSupported() {
         return false;
     }
 
     /**
-     * This method is meant for overriding to implement collecting of all clusterwide available keys
-     * and returning them from {@link #getAllKeys()}.
+     * This method is meant to be overridden to implement collecting of all clusterwide available keys
+     * and return them from {@link #getAllKeys()}.
      *
      * @return a collection of all clusterwide available keys
      */
@@ -149,7 +150,7 @@ public abstract class KeyValueSource<K, V>
     /**
      * A helper method to build a KeyValueSource implementation based on the specified {@link IMap}
      *
-     * @param map map to build a KeyValueSource implementation with
+     * @param map map to build a KeyValueSource implementation
      * @param <K> key type of the map
      * @param <V> value type of the map
      * @return KeyValueSource implementation based on the specified map
@@ -161,7 +162,7 @@ public abstract class KeyValueSource<K, V>
     /**
      * A helper method to build a KeyValueSource implementation based on the specified {@link MultiMap}
      *
-     * @param multiMap multiMap to build a KeyValueSource implementation with
+     * @param multiMap multiMap to build a KeyValueSource implementation
      * @param <K>      key type of the multiMap
      * @param <V>      value type of the multiMap
      * @return KeyValueSource implementation based on the specified multiMap
@@ -173,10 +174,10 @@ public abstract class KeyValueSource<K, V>
     /**
      * A helper method to build a KeyValueSource implementation based on the specified {@link IList}.<br/>
      * The key returned by this KeyValueSource implementation is <b>ALWAYS</b> the name of the list itself,
-     * whereas the value are the entries of list one by one. So this implementation behaves like a MultiMap
+     * whereas the value are the entries of the list, one by one. This implementation behaves like a MultiMap
      * with a single key but multiple values.
      *
-     * @param list list to build a KeyValueSource implementation with
+     * @param list list to build a KeyValueSource implementation
      * @param <V>  value type of the list
      * @return KeyValueSource implementation based on the specified list
      */
@@ -187,10 +188,10 @@ public abstract class KeyValueSource<K, V>
     /**
      * A helper method to build a KeyValueSource implementation based on the specified {@link ISet}.<br/>
      * The key returned by this KeyValueSource implementation is <b>ALWAYS</b> the name of the set itself,
-     * whereas the value are the entries of set one by one. So this implementation behaves like a MultiMap
+     * whereas the value are the entries of the set, one by one. This implementation behaves like a MultiMap
      * with a single key but multiple values.
      *
-     * @param set set to build a KeyValueSource implementation with
+     * @param set set to build a KeyValueSource implementation
      * @param <V> value type of the set
      * @return KeyValueSource implementation based on the specified set
      */
