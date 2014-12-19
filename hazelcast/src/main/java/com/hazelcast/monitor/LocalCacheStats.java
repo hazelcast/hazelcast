ad719056a9d48b03526b71af43ecdb879b5dd5c1
David Strom <david.strom@hazelcast.com>
2014-12-18 16:08:26 -0800

    copyedit

--- hazelcast/src/main/java/com/hazelcast/monitor/LocalCacheStats.java
+++ hazelcast/src/main/java/com/hazelcast/monitor/LocalCacheStats.java
@@ -7,71 +7,79 @@ package com.hazelcast.monitor;
 public interface LocalCacheStats extends LocalInstanceStats {
 
     /**
+     * Returns the number of hits (successful get operations) on the cache.
      *
-     * @return the number of hits
+     * @return the number of hits (successful get operations) on the cache
      */
     long getCacheHits();
 
     /**
+     * Returns the percentage of hits (successful get operations) on the cache.
      *
-     * @return the percentage of hits (successful get operations)
+     * @return the percentage of hits (successful get operations) on the cache
      */
     float getCacheHitPercentage();
 
     /**
+     * Returns the number of missed cache accesses on the cache.
      *
-     * @return the number misses
+     * @return the number of missed cache accesses on the cache
      */
     long getCacheMisses();
 
     /**
+     * Returns the percentage of missed cache accesses on the cache.
      *
-     * @return the percentage of missed cache accesses
+     * @return the percentage of missed cache accesses on the cache
      */
     float getCacheMissPercentage();
 
     /**
+     * Returns the number of gets on the cache.
      *
-     * @return the number of gets
+     * @return the number of gets on the cache
      */
     long getCacheGets();
 
     /**
+     * Returns the number of puts to the queue.
      *
-     * @return the number of puts
+     * @return the number of puts to the queue
      */
     long getCachePuts();
 
     /**
+     * Returns the number of removals from the queue.
      *
-     * @return the number of removals
+     * @return the number of removals from the queue
      */
     long getCacheRemovals();
 
     /**
+     * Returns the number of evictions on the cache.
      *
-     * @return the number of evictions
+     * @return the number of evictions on the cache
      */
     long getCacheEvictions();
 
     /**
-     * The mean time to execute gets
+     * Returns the mean time to execute gets on the cache.
      *
-     * @return the time in µs
+     * @return the mean time in µs to execute gets on the cache
      */
     float getAverageGetTime();
 
     /**
-     * The mean time to execute puts
+     * Returns the mean time to execute puts on the cache.
      *
-     * @return the time in µs
+     * @return the mean time in µs to execute puts on the cache
      */
     float getAveragePutTime();
 
     /**
-     * The mean time to execute removes
+     * Returns the mean time to execute removes on the cache.
      *
-     * @return the time in µs
+     * @return the mean time in µs to execute removes on the cache
      */
     float getAverageRemoveTime();
 
