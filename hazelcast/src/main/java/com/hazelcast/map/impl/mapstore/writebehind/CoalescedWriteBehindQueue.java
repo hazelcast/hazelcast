90a9b8c6b898876325f06313ddf91033259485f3
Ahmet MIRCIK <ahmet@hazelcast.com>
2014-12-22 15:26:33 +0200

    update old value in coalesced write-behind-queue

--- hazelcast/src/main/java/com/hazelcast/map/impl/mapstore/writebehind/CoalescedWriteBehindQueue.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/mapstore/writebehind/CoalescedWriteBehindQueue.java
@@ -31,19 +31,23 @@ class CoalescedWriteBehindQueue implements WriteBehindQueue<DelayedEntry> {
         if (delayedEntry == null) {
             return false;
         }
+        calculateStoreTime(delayedEntry);
         final Data key = (Data) delayedEntry.getKey();
-        // if key exists in this queue before, use previous store time;
-        // this is because we do not want to shift store time of any key.
-        final DelayedEntry existingDelayedEntry = queue.get(key);
-        if (existingDelayedEntry != null) {
-            final long storeTime = existingDelayedEntry.getStoreTime();
-            delayedEntry.setStoreTime(storeTime);
-            queue.replace(key, delayedEntry);
-        } else {
-            queue.put(key, delayedEntry);
-        }
-
+        queue.put(key, delayedEntry);
         return true;
+    }
+
+    /**
+     * If this is an existing key in this queue, use previously set store time;
+     * since we do not want to shift store time of an existing key on every update.
+     */
+    private void calculateStoreTime(DelayedEntry delayedEntry) {
+        Data key = (Data) delayedEntry.getKey();
+        DelayedEntry currentEntry = queue.get(key);
+        if (currentEntry != null) {
+            long currentStoreTime = currentEntry.getStoreTime();
+            delayedEntry.setStoreTime(currentStoreTime);
+        }
     }
 
     @Override
