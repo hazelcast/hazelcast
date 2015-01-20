b193ac05b31a4407659de395492c7c3ce66106ab
sancar <sancar@hazelcast.com>
2015-01-20 10:24:20 +0200

    unresolved addresses should be retried when trying to connect. fixes #4349.

---
+++ hazelcast-client/src/test/java/com/hazelcast/client/ClientConnectionTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.client;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.Client;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(HazelcastSerialClassRunner.class)
+@Category(QuickTest.class)
+public class ClientConnectionTest extends HazelcastTestSupport {
+
+    @After
+    @Before
+    public void cleanup() {
+        HazelcastClient.shutdownAll();
+        Hazelcast.shutdownAll();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testWithIllegalAddress() {
+        String illegalAddress = randomString();
+
+        Hazelcast.newHazelcastInstance();
+        ClientConfig config = new ClientConfig();
+        config.getNetworkConfig().addAddress(illegalAddress);
+        HazelcastClient.newHazelcastClient(config);
+    }
+
+    @Test
+    public void testWithLegalAndIllegalAddressTogether() {
+        String illegalAddress = randomString();
+
+        HazelcastInstance server = Hazelcast.newHazelcastInstance();
+        ClientConfig config = new ClientConfig();
+        config.getNetworkConfig().addAddress(illegalAddress).addAddress("localhost");
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
+
+        Collection<Client> connectedClients = server.getClientService().getConnectedClients();
+        assertEquals(connectedClients.size(), 1);
+
+        Client serverSideClientInfo = connectedClients.iterator().next();
+        assertEquals(serverSideClientInfo.getUuid(), client.getLocalEndpoint().getUuid());
+    }
+}
