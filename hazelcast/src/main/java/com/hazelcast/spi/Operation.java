385477f6ae3c7f292ef0f860c410d14e3c2d0407
Peter Veentjer <alarmnummer@gmail.com>
2015-01-30 08:45:38 +0200

    Included the partition-id into the operation.toString as well

--- hazelcast/src/main/java/com/hazelcast/spi/Operation.java
+++ hazelcast/src/main/java/com/hazelcast/spi/Operation.java
@@ -419,6 +419,7 @@ public abstract class Operation implements DataSerializable {
     public String toString() {
         final StringBuilder sb = new StringBuilder(getClass().getName()).append('{');
         sb.append("serviceName='").append(serviceName).append('\'');
+        sb.append(", partitionId=").append(partitionId);
         sb.append(", callId=").append(callId);
         sb.append(", invocationTime=").append(invocationTime);
         sb.append(", waitTimeout=").append(waitTimeout);
