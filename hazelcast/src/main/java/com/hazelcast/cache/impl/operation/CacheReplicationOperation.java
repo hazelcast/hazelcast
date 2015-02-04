d9c72eadecbf3507c6188e9edae3fb8512597ade
Mehmet Dogan <mehmet@hazelcast.com>
2015-02-04 10:40:40 +0200

    Small fixes related to latest Data impl change.

--- hazelcast/src/main/java/com/hazelcast/cache/impl/operation/CacheReplicationOperation.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/operation/CacheReplicationOperation.java
@@ -24,7 +24,6 @@ import com.hazelcast.config.CacheConfig;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.Data;
-import com.hazelcast.nio.serialization.DefaultData;
 import com.hazelcast.spi.AbstractOperation;
 import com.hazelcast.util.Clock;
 
@@ -141,7 +140,7 @@ public class CacheReplicationOperation extends AbstractOperation {
             // expired entries were found while serializing, since the
             // real subCount will then be different from the one written
             // before
-            out.writeData(new DefaultData());
+            out.writeData(null);
         }
     }
 
@@ -167,7 +166,7 @@ public class CacheReplicationOperation extends AbstractOperation {
                 // Empty data received so reading can be stopped here since
                 // since the real object subCount might be different from
                 // the number on the stream due to found expired entries
-                if (key.dataSize() == 0) {
+                if (key == null) {
                     break;
                 }
                 CacheRecord record = in.readObject();
