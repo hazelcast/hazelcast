dfc03bd29e3039ab25f9b053f16029ae6f270c2f
Ali Gurbuz <ali@hazelcast.com>
2015-02-04 16:29:14 +0200

    added missing beta annotation

--- hazelcast/src/main/java/com/hazelcast/spi/StatisticsAwareService.java
+++ hazelcast/src/main/java/com/hazelcast/spi/StatisticsAwareService.java
@@ -1,6 +1,7 @@
 package com.hazelcast.spi;
 
 import com.hazelcast.monitor.LocalInstanceStats;
+import com.hazelcast.spi.annotation.Beta;
 
 import java.util.Map;
 
@@ -9,6 +10,7 @@ import java.util.Map;
  * This interface is in BETA stage and is subject to change in upcoming releases.
  *
  */
+@Beta
 public interface StatisticsAwareService {
 
     <T extends LocalInstanceStats> Map<String, T> getStats();
