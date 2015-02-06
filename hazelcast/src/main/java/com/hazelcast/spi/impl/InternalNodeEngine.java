4e7d640b6983312a8c327578a94b04a860610ebb
Peter Veentjer <alarmnummer@gmail.com>
2015-02-06 12:27:10 +0200

    Improved javadoc

--- hazelcast/src/main/java/com/hazelcast/spi/impl/InternalNodeEngine.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/InternalNodeEngine.java
@@ -3,10 +3,15 @@ package com.hazelcast.spi.impl;
 import com.hazelcast.spi.NodeEngine;
 
 /**
- * The InternalNodeEngine extends the NodeEngine and exposes all kinds of other internal services that
+ * The InternalNodeEngine extends the {@link NodeEngine} and exposes all kinds of other internal services that
  * are not exposed to the regular SPI user.
  */
 public interface InternalNodeEngine extends NodeEngine {
 
+    /**
+     * Gets the ServiceManager
+     *
+     * @return the ServiceManager.
+     */
     ServiceManager getServiceManager();
 }
