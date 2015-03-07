94c9472d58b1acf1612f1f74827b53103c31d958
Peter Veentjer <alarmnummer@gmail.com>
2015-03-07 14:48:52 +0200

    WIP

--- hazelcast/src/test/java/com/hazelcast/spi/impl/operationexecutor/progressive/PartitionQueue_parkTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/impl/operationexecutor/progressive/PartitionQueue_parkTest.java
@@ -1,4 +1,4 @@
-package com.hazelcast.spi.impl.operationexecutor.progressive;
+ package com.hazelcast.spi.impl.operationexecutor.progressive;
 
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
@@ -7,18 +7,14 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import static com.hazelcast.spi.impl.operationexecutor.progressive.Node.EXECUTING;
-import static com.hazelcast.spi.impl.operationexecutor.progressive.Node.EXECUTING_PRIORITY;
 import static com.hazelcast.spi.impl.operationexecutor.progressive.Node.PARKED;
 import static com.hazelcast.spi.impl.operationexecutor.progressive.Node.STOLEN;
 import static com.hazelcast.spi.impl.operationexecutor.progressive.Node.STOLEN_UNPARKED;
 import static com.hazelcast.spi.impl.operationexecutor.progressive.Node.UNPARKED;
 import static com.hazelcast.spi.impl.operationexecutor.progressive.PartitionQueueState.Executing;
-import static com.hazelcast.spi.impl.operationexecutor.progressive.PartitionQueueState.ExecutingPriority;
 import static com.hazelcast.spi.impl.operationexecutor.progressive.PartitionQueueState.UnparkedPriority;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
@@ -57,13 +53,9 @@ public class PartitionQueue_parkTest extends PartitionQueueAbstractTest {
     private void whenIllegalState_thenIllegalStateException(Node node) {
         partitionQueue.head.set(node);
 
-        try {
-            partitionQueue.park();
-            fail();
-        } catch (IllegalStateException expected) {
+        boolean result = partitionQueue.park();
 
-        }
-
+        assertTrue(result);
         assertHead(node);
         assertNoNewUnparks();
         assertBuffersUnchanged();
@@ -99,7 +91,7 @@ public class PartitionQueue_parkTest extends PartitionQueueAbstractTest {
         assertBuffersUnchanged();
     }
 
-     @Test
+    @Test
     public void whenExecuting_andPendingNormalWork_thenParkFails() {
         Node node = new Node();
         node.init("", Executing, null);
