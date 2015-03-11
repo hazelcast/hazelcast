ac4a91eac95ac033f0100ef6a59b7370342ace11
cherednichenko <sergei_cherednichenko@yahoo.com>
2015-03-11 21:54:39 +0200

    Added a couple unit tests for MapConfig, QueueConfig, CacheConfig.

--- hazelcast/src/test/java/com/hazelcast/cache/CacheConfigTest.java
+++ hazelcast/src/test/java/com/hazelcast/cache/CacheConfigTest.java
@@ -202,6 +202,42 @@ public class CacheConfigTest extends HazelcastTestSupport {
         assertNull(testCache);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void setAsyncBackupCount_whenTooSmall(){
+        CacheConfig config = new CacheConfig();
+        config.setAsyncBackupCount(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setAsyncBackupCount_whenItsZero (){
+        CacheConfig config = new CacheConfig();
+        config.setAsyncBackupCount(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setAsyncBackupCount_whenTooLarge (){
+        CacheConfig config = new CacheConfig();
+        config.setAsyncBackupCount(200); //max allowed is 6.. I need to look up the constant
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setBackupCount_whenTooSmall(){
+        CacheConfig config = new CacheConfig();
+        config.setBackupCount(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setBackupCount_whenItsZero(){
+        CacheConfig config = new CacheConfig();
+        config.setBackupCount(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setBackupCount_whenTooLarge(){
+        CacheConfig config = new CacheConfig();
+        config.setBackupCount(200); //max allowed is 6.. I need to look up the constant
+    }
+
     @Test
     public void testGetPreConfiguredCache() {
         Config config = new Config();
