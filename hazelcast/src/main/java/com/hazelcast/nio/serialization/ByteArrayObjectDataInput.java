b148b622cb3674119b13dddf2ad9425f87689170
Michael Bischoff <jpaw@online.de>
2015-03-22 21:12:41 +0100

    fix checkstyle advice

--- hazelcast/src/main/java/com/hazelcast/nio/serialization/ByteArrayObjectDataInput.java
+++ hazelcast/src/main/java/com/hazelcast/nio/serialization/ByteArrayObjectDataInput.java
@@ -393,7 +393,7 @@ class ByteArrayObjectDataInput extends InputStream implements BufferObjectDataIn
      * @see java.io.FilterInputStream#in
      */
     public int readUnsignedByte() throws IOException {
-        return 0xff & (int)readByte();
+        return 0xff & (int) readByte();
     }
 
     /**
@@ -410,7 +410,7 @@ class ByteArrayObjectDataInput extends InputStream implements BufferObjectDataIn
      * @see java.io.FilterInputStream#in
      */
     public int readUnsignedShort() throws IOException {
-        return 0xffff & (int)readShort();
+        return 0xffff & (int) readShort();
     }
 
     /**
