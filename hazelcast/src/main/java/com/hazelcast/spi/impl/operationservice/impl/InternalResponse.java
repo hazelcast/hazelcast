4b4954038fb4cb67f29f9e9b0858bf9ce571d101
Peter Veentjer <alarmnummer@gmail.com>
2015-03-25 06:17:54 +0200

    Removed the InternalResponse.RETRY_RESPONSE since not used anymore

--- hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/InternalResponse.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/InternalResponse.java
@@ -6,25 +6,24 @@ final class InternalResponse {
      * A response indicating the 'null' value.
      */
     static final Object NULL_RESPONSE = new InternalResponse("Invocation::NULL_RESPONSE");
-    /**
-     * A response indicating that the operation should be executed again. E.g. because an operation
-     * was send to the wrong machine.
-     */
-    static final Object RETRY_RESPONSE = new InternalResponse("Invocation::RETRY_RESPONSE");
-    /**
+
+     /**
      * Indicating that there currently is no 'result' available. An example is some kind of blocking
      * operation like ILock.lock. If this lock isn't available at the moment, the wait response
      * is returned.
      */
     static final Object WAIT_RESPONSE = new InternalResponse("Invocation::WAIT_RESPONSE");
+
     /**
      * A response indicating that a timeout has happened.
      */
     static final Object TIMEOUT_RESPONSE = new InternalResponse("Invocation::TIMEOUT_RESPONSE");
+
     /**
      * A response indicating that the operation execution was interrupted
      */
     static final Object INTERRUPTED_RESPONSE = new InternalResponse("Invocation::INTERRUPTED_RESPONSE");
+
     private String toString;
 
     InternalResponse(String toString) {
