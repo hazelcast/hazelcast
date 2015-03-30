9a81513caad0373a16468d6748d842432843e2bc
Mehmet Dogan <mehmet@hazelcast.com>
2015-03-30 14:55:44 +0300

    InternalPartitionService should not process a partition state which is sent from a non-master member, as it's doing for non-member right know.

--- hazelcast/src/main/java/com/hazelcast/partition/impl/InternalPartitionServiceImpl.java
+++ hazelcast/src/main/java/com/hazelcast/partition/impl/InternalPartitionServiceImpl.java
@@ -579,6 +579,7 @@ public class InternalPartitionServiceImpl implements InternalPartitionService, M
                         logger.warning("Received a ClusterRuntimeState, but its sender doesn't seem to be master!"
                                 + " => Sender: " + sender + ", Master: " + master + "! "
                                 + "(Ignore if master node has changed recently.)");
+                        return;
                     }
                 }
             }
