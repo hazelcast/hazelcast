8e746fdf0b69e7aa01ccdd89a82ccf02a23d91bb
Ensar Basri Kahveci <ebkahveci@gmail.com>
2015-04-02 11:49:51 +0300

    Temporarily ignore new-client tests

--- hazelcast-client-new/src/test/java/com/hazelcast/client/partitionservice/ClientPartitionLostListenerTest.java
+++ hazelcast-client-new/src/test/java/com/hazelcast/client/partitionservice/ClientPartitionLostListenerTest.java
@@ -14,6 +14,7 @@ import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -31,6 +32,7 @@ import static org.mockito.Mockito.mock;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
+@Ignore
 public class ClientPartitionLostListenerTest {
 
     @After
