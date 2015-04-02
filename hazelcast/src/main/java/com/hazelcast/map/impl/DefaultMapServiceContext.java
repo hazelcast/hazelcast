74bd208f4a97e2cc7c2de19c9fdf1f473c5d8d75
Mehmet Dogan <mehmet@hazelcast.com>
2015-04-02 14:50:31 +0300

    Revert DefaultMapServiceContext change, since it breaks enterprise repo.

--- hazelcast/src/main/java/com/hazelcast/map/impl/DefaultMapServiceContext.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/DefaultMapServiceContext.java
@@ -66,8 +66,12 @@ class DefaultMapServiceContext extends AbstractMapServiceContextSupport {
         this.nearCacheProvider = new NearCacheProvider(this, nodeEngine);
         this.localMapStatsProvider = new LocalMapStatsProvider(this, nodeEngine);
         this.mergePolicyProvider = new MergePolicyProvider(nodeEngine);
-        this.mapEventPublisher = new MapEventPublisherSupport(this);
+        this.mapEventPublisher = createMapEventPublisherSupport();
         this.mapContextQuerySupport = new BasicMapContextQuerySupport(this);
+    }
+
+    MapEventPublisherSupport createMapEventPublisherSupport() {
+        return new MapEventPublisherSupport(this);
     }
 
     @Override
