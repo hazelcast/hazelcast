b0273c7b8bfb373ab336baae7c981f4a79fe3032
Peter Veentjer <alarmnummer@gmail.com>
2015-04-02 18:08:32 +0300

    Fix logging wrong usage in the InvocaitonFuture. Logging on info even though it is debug message + the logging check didn't match

--- hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/InvocationFuture.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/InvocationFuture.java
@@ -136,7 +136,7 @@ final class InvocationFuture<E> implements InternalCompletableFuture<E> {
                 // this is no good; no logging while holding a lock
                 ILogger logger = invocation.logger;
                 if (logger.isFinestEnabled()) {
-                    logger.info("Future response is already set! Current response: "
+                    logger.finest("Future response is already set! Current response: "
                             + response + ", Offered response: " + offeredResponse + ", Invocation: " + invocation);
                 }
                 return;
