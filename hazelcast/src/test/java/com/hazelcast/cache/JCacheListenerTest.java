444267df3a190fcb177ff053c3a9282b3437e5e8
Peter Veentjer <alarmnummer@gmail.com>
2015-04-06 14:28:27 +0300

    Speed up JCacheListenerTest by making it parallel instead of serial

--- hazelcast/src/test/java/com/hazelcast/cache/JCacheListenerTest.java
+++ hazelcast/src/test/java/com/hazelcast/cache/JCacheListenerTest.java
@@ -18,6 +18,7 @@ package com.hazelcast.cache;
 
 import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
@@ -45,7 +46,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
-@RunWith(HazelcastSerialClassRunner.class)
+@RunWith(HazelcastParallelClassRunner.class)
 @Category(QuickTest.class)
 public class JCacheListenerTest extends HazelcastTestSupport {
 
