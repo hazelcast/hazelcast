7b85ff159bbd7f596113afede1f9879583bd40de
David Strom <david.strom@hazelcast.com>
2015-04-08 16:20:14 -0700

    minor copyedits
    minor copyedits

--- hazelcast/src/main/java/com/hazelcast/config/InvalidConfigurationException.java
+++ hazelcast/src/main/java/com/hazelcast/config/InvalidConfigurationException.java
@@ -29,7 +29,7 @@ public class InvalidConfigurationException extends RuntimeException {
     /**
      * Creates a InvalidConfigurationException with the given message.
      *
-     * @param message the message.
+     * @param message The message for the exception.
      */
     public InvalidConfigurationException(String message) {
         super(message);
@@ -38,8 +38,8 @@ public class InvalidConfigurationException extends RuntimeException {
     /**
      * ** Constructs a new runtime exception with the specified detail message and
      * cause.
-     * @param message the message.
-     * @param cause the cause.
+     * @param message The message for the runtime exception.
+     * @param cause The cause of the runtime exception.
      */
     public InvalidConfigurationException(final String message, final Throwable cause) {
         super(message, cause);
