cdb4f1c695cf6dc1435582a522d18de4bcb34233
Donnerbart <github@thunderphreak.de>
2015-04-15 04:49:29 +0200

    Added newline to class header and fixed affected classes.

--- hazelcast/src/main/java/com/hazelcast/client/impl/client/package-info.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/client/package-info.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * <p>This contains the Requests and other DTO's exchanged between client/server</p>
  */
--- hazelcast/src/main/java/com/hazelcast/util/scheduler/CoalescingDelayedTrigger.java
+++ hazelcast/src/main/java/com/hazelcast/util/scheduler/CoalescingDelayedTrigger.java
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hazelcast.util.scheduler;
 
+package com.hazelcast.util.scheduler;
 
 import com.hazelcast.spi.ExecutionService;
 import com.hazelcast.util.Clock;
