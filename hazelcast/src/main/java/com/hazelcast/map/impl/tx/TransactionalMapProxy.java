bdcf0b96d469e986714006ea4e48fc0b0bf082d0
Peter Veentjer <alarmnummer@gmail.com>
2015-04-24 12:32:28 +0300

    TransactionalMapProxy throws NPE instead of IllegalArgumentException in case of type violation

--- hazelcast/src/main/java/com/hazelcast/map/impl/tx/TransactionalMapProxy.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/tx/TransactionalMapProxy.java
@@ -320,9 +320,7 @@ public class TransactionalMapProxy extends TransactionalMapProxySupport implemen
     public Set keySet(Predicate predicate) {
         checkTransactionState();
         checkNotNull(predicate, "Predicate should not be null!");
-        if (predicate instanceof PagingPredicate) {
-            throw new NullPointerException("Paging is not supported for Transactional queries!");
-        }
+        checkNotInstanceOf(PagingPredicate.class, predicate, "Paging is not supported for Transactional queries!");
 
         MapService service = getService();
         MapServiceContext mapServiceContext = service.getMapServiceContext();
