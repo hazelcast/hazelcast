f2b335747e9d5414b4b3effb58a447760a882fe7
Peter Veentjer <alarmnummer@gmail.com>
2015-05-20 13:31:20 +0300

    Minor javadoc improvement in CallerNotMemberException

--- hazelcast/src/main/java/com/hazelcast/spi/exception/CallerNotMemberException.java
+++ hazelcast/src/main/java/com/hazelcast/spi/exception/CallerNotMemberException.java
@@ -19,7 +19,7 @@ package com.hazelcast.spi.exception;
 import com.hazelcast.nio.Address;
 
 /**
- * A {@link RetryableHazelcastException} that indicates that a operation was send by a machine which isn't member
+ * A {@link RetryableHazelcastException} that indicates that an operation was send by a machine which isn't member
  * in the cluster when the request is operation.
  */
 public class CallerNotMemberException extends RetryableHazelcastException {
