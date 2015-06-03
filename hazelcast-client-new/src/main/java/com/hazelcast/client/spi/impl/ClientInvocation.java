885e72ee6b6e7f4a052b6703a12b7bdf0e690b9a
Alexander Toktarev <toktarev@gmail.com>
2015-06-03 18:15:15 +0300

    Bug 5422: Retry on AuthenticationException

--- hazelcast-client-new/src/main/java/com/hazelcast/client/spi/impl/ClientInvocation.java
+++ hazelcast-client-new/src/main/java/com/hazelcast/client/spi/impl/ClientInvocation.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.client.spi.impl;
 
+import com.hazelcast.client.AuthenticationException;
 import com.hazelcast.client.HazelcastClientNotActiveException;
 import com.hazelcast.client.config.ClientProperties;
 import com.hazelcast.client.connection.nio.ClientConnection;
@@ -196,7 +197,9 @@ public class ClientInvocation implements Runnable {
             return;
         }
 
-        if (exception instanceof IOException || exception instanceof HazelcastInstanceNotActiveException) {
+        if (exception instanceof IOException
+                || exception instanceof HazelcastInstanceNotActiveException
+                || exception instanceof AuthenticationException) {
             if (handleRetry()) {
                 return;
             }
