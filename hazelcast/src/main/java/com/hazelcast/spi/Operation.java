19f38acf10ee562ba9d16f5a2a38f401c3b8ac39
Mehmet Dogan <mehmet@hazelcast.com>
2015-07-13 12:19:31 +0300

    Added Operation#onSetCallId() javadoc and explained its purpose and usage.

--- hazelcast/src/main/java/com/hazelcast/spi/Operation.java
+++ hazelcast/src/main/java/com/hazelcast/spi/Operation.java
@@ -186,6 +186,18 @@ public abstract class Operation implements DataSerializable {
         return this;
     }
 
+    /**
+     * This method is called every time a new <tt>callId</tt> is set to the operation.
+     * A new <tt>callId</tt> is set to an operation before initial invocation
+     * and before every invocation retry.
+     * <p/>
+     * By default this is a no-op method. Operation implementations which are willing to
+     * get notified on <tt>callId</tt> changes can override this method. New <tt>callId</tt>
+     * can be accessed by calling {@link #getCallId()}.
+     * <p/>
+     * For example an operation can distinguish the first invocation and invocation retries by keeping
+     * the initial <tt>callId</tt>.
+     */
     protected void onSetCallId() {
     }
 
