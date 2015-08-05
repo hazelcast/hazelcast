bb63b43736a18fb3cb3b141d0b491dc6f84a7d83
Jaromir Hamala <jaromir.hamala@gmail.com>
2015-08-05 19:52:33 +0300

    Tests moved into Parallel Runner

--- hazelcast/src/test/java/com/hazelcast/map/query/QueryIndexMigrationTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/query/QueryIndexMigrationTest.java
@@ -26,9 +26,10 @@ import com.hazelcast.query.SampleObjects.Employee;
 import com.hazelcast.query.SampleObjects.Value;
 import com.hazelcast.query.SqlPredicate;
 import com.hazelcast.test.AssertTask;
-import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
+import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import com.hazelcast.util.Clock;
 import com.hazelcast.util.IterableUtil;
@@ -60,8 +61,8 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-@RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class QueryIndexMigrationTest extends HazelcastTestSupport {
 
     private TestHazelcastInstanceFactory nodeFactory;
