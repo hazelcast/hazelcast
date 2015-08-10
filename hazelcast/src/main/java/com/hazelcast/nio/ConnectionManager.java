ba3b1319f6d3e977df7379ce1a352c20308c3307
Serdar Ozmen <serdar@hazelcast.com>
2015-08-10 10:31:58 +0300

    Minor copyedits.

--- hazelcast/src/main/java/com/hazelcast/nio/ConnectionManager.java
+++ hazelcast/src/main/java/com/hazelcast/nio/ConnectionManager.java
@@ -22,7 +22,7 @@ package com.hazelcast.nio;
 public interface ConnectionManager {
 
     /**
-     * Gets the number of clients connections.
+     * Gets the number of client connections.
      *
      * @return number of client connections.
      */
@@ -50,7 +50,7 @@ public interface ConnectionManager {
     int getActiveConnectionCount();
 
     /**
-     * Gets the connection for a given address. If the connection doesn't exists, null is returned.
+     * Gets the connection for a given address. If the connection does not exist, it returns null.
      *
      * @param address the remote side of the connection.
      * @return the found Connection, or none if one doesn't exist.
@@ -67,8 +67,8 @@ public interface ConnectionManager {
     Connection getOrConnect(Address address);
 
     /**
-     * Gets the existing connection for a given address. If it doesn't exist, the system will try to make the connection
-     * asynchronously. In this case null is returned.
+     * Gets the existing connection for a given address. If it does not exist, the system will try to connect
+     * asynchronously. In this case, it returns null.
      *
      * When the connection is established at some point in time, it can be retrieved using the
      * {@link #getConnection(Address)}.
@@ -86,7 +86,7 @@ public interface ConnectionManager {
      *
      * If connection is null, the call is ignored.
      *
-     * If the connection already is destroyed, the call is ignored.
+     * If the connection is already destroyed, the call is ignored.
      *
      * @param connection the Connection to destroy.
      */
@@ -103,30 +103,31 @@ public interface ConnectionManager {
     void addConnectionListener(ConnectionListener listener);
 
     /**
-     * Starts ConnectionManager, initializes its resources, starts threads etc. After start ConnectionManager becomes fully
-     * operational.
+     * Starts ConnectionManager, initializes its resources, starts threads, etc. After start, ConnectionManager
+     * becomes fully operational.
      * <p/>
-     * If it's already started, then this method has no effect.
+     * If it is already started, then this method has no effect.
      *
      * @throws IllegalStateException if ConnectionManager is shutdown
      */
     void start();
 
     /**
-     * Stops ConnectionManager, releases its resources, stops threads etc. When stopped ConnectionManager can be started again
-     * using {@link #start()}.
+     * Stops ConnectionManager, releases its resources, stops threads, etc. When stopped, ConnectionManager
+     * can be started again using {@link #start()}.
      * <p/>
-     * This method has no effect if it's already stopped or shutdown.
+     * This method has no effect if it is already stopped or shutdown.
      * <p/>
-     * Currently <tt>stop</tt> is called during merge process to detach node from current cluster. After node becomes ready to
-     * join to the new cluster, <tt>start</tt> is called to re-initialize the ConnectionManager.
+     * Currently <tt>stop</tt> is called during the merge process to detach node from the current cluster. After
+     * node becomes ready to join to the new cluster, <tt>start</tt> is called to re-initialize the ConnectionManager.
      */
     void stop();
 
     /**
-     * Shutdowns ConnectionManager completely. ConnectionManager won't be operational anymore and cannot be restarted.
+     * Shutdowns ConnectionManager completely. ConnectionManager will not be operational anymore and cannot
+     * be restarted.
      * <p/>
-     * This method has no effect if it's already shutdown.
+     * This method has no effect if it is already shutdown.
      */
     void shutdown();
 }
