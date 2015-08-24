90d45af7ef2ea733e4f0cd1d31a100ba08ca4192
Jaromir Hamala <jaromir.hamala@gmail.com>
2015-08-24 19:03:29 +0300

    Query results duplicates are filtered out on a member side.
    Duplicates are only possible when the same partition is queried multiple-times.
    This change ignores results from a member if it contains at least one partition
    we have already collected. Chances are it'll throw away even some non-duplicates,
    but they will be collected later in the `collectResultsFromMissingPartitions()`
    method anyway so it's not a big deal.

---
+++ hazelcast/src/main/java/com/hazelcast/util/BitSetUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.util;
+
+import java.util.BitSet;
+
+/**
+ * Convenient method for BitSet manipulations
+ *
+ */
+public final class BitSetUtils {
+
+    private BitSetUtils() {
+
+    }
+
+    /**
+     * Returns true if at least one bit on a given position is set
+     *
+     * @param bitSet
+     * @param indexes
+     * @return {@code true} is BitSet contains at least one bit at any position from index which is set, otherwise
+     *         return {@code false}
+     */
+    public static boolean hasAtLeastOneBitSet(BitSet bitSet, Iterable<Integer> indexes) {
+        for (Integer index : indexes) {
+            if (bitSet.get(index)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Set all bits on a given positions
+     *
+     * @param bitSet
+     * @param indexes
+     */
+    public static void setBits(BitSet bitSet, Iterable<Integer> indexes) {
+        for (Integer index : indexes) {
+            bitSet.set(index);
+        }
+    }
+
+
+}
