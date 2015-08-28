2252120e58e9933c4a46f80c23acf838d3378c20
Jaromir Hamala <jaromir.hamala@gmail.com>
2015-08-28 18:46:08 +0300

    MapReduce and Aggregation tests re-enabled. (cherry picked from commit f79e8d3)

--- hazelcast-client-new/src/test/java/com/hazelcast/client/mapreduce/aggregation/SumAggregationTest.java
+++ hazelcast-client-new/src/test/java/com/hazelcast/client/mapreduce/aggregation/SumAggregationTest.java
@@ -22,7 +22,6 @@ import com.hazelcast.mapreduce.aggregation.Aggregations;
 import com.hazelcast.mapreduce.aggregation.Supplier;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -35,7 +34,6 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(HazelcastParallelClassRunner.class)
 @Category(QuickTest.class)
-@Ignore //https://github.com/hazelcast/hazelcast/issues/5096
 public class SumAggregationTest
         extends AbstractAggregationTest {
 
--- hazelcast/src/test/java/com/hazelcast/mapreduce/impl/task/ReducerTaskSchedulerTest.java
+++ hazelcast/src/test/java/com/hazelcast/mapreduce/impl/task/ReducerTaskSchedulerTest.java
@@ -19,7 +19,6 @@ package com.hazelcast.mapreduce.impl.task;
 import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
-import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
 import org.junit.Before;
@@ -35,7 +34,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(HazelcastParallelClassRunner.class)
-@Category({QuickTest.class, ParallelTest.class})
+@Category(QuickTest.class)
 public class ReducerTaskSchedulerTest extends HazelcastTestSupport {
 
     private ExecutorService executorService;
