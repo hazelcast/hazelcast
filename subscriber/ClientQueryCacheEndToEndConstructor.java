e0629f8fb13740a0947ffc81632fd3bf2db96c21
alarmnummer <peter.veentjer@hazelcast.com>
2015-09-30 16:28:48 +0300

    WIP

--- subscriber/ClientQueryCacheEndToEndConstructor.java
+++ subscriber/ClientQueryCacheEndToEndConstructor.java
@@ -3,18 +3,15 @@ package com.hazelcast.client.impl.querycache.subscriber;
 import com.hazelcast.core.EntryEventType;
 import com.hazelcast.map.impl.client.MadePublishableRequest;
 import com.hazelcast.map.impl.client.PublisherCreateRequest;
-import com.hazelcast.map.impl.query.QueryResultSet;
-import com.hazelcast.map.impl.querycache.accumulator.AccumulatorInfo;
+import com.hazelcast.map.impl.query.QueryResult;
+import com.hazelcast.map.impl.query.QueryResultRow;
 import com.hazelcast.map.impl.querycache.InvokerWrapper;
+import com.hazelcast.map.impl.querycache.accumulator.AccumulatorInfo;
 import com.hazelcast.map.impl.querycache.subscriber.AbstractQueryCacheEndToEndConstructor;
 import com.hazelcast.map.impl.querycache.subscriber.InternalQueryCache;
 import com.hazelcast.map.impl.querycache.subscriber.QueryCacheEndToEndConstructor;
 import com.hazelcast.map.impl.querycache.subscriber.QueryCacheRequest;
-import com.hazelcast.nio.serialization.Data;
 
-
-import java.util.AbstractMap;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -32,9 +29,9 @@ public class ClientQueryCacheEndToEndConstructor extends AbstractQueryCacheEndTo
     public void createPublisherAccumulator(AccumulatorInfo info) throws Exception {
         PublisherCreateRequest publisherCreateRequest = new PublisherCreateRequest(info);
         InvokerWrapper invokerWrapper = context.getInvokerWrapper();
-        List<QueryResultSet> queryResultSets
-                = (List<QueryResultSet>) invokerWrapper.invoke(publisherCreateRequest);
-        setResults(queryCache, queryResultSets);
+        List<QueryResult> queryResults
+                = (List<QueryResult>) invokerWrapper.invoke(publisherCreateRequest);
+        setResults(queryCache, queryResults);
         if (info.isPopulate()) {
             madePublishable(info.getMapName(), info.getCacheName());
         }
@@ -44,40 +41,34 @@ public class ClientQueryCacheEndToEndConstructor extends AbstractQueryCacheEndTo
         context.getInvokerWrapper().invokeOnAllPartitions(new MadePublishableRequest(mapName, cacheName));
     }
 
-    private void setResults(InternalQueryCache queryCache, List<QueryResultSet> queryResultSets) {
+    private void setResults(InternalQueryCache queryCache, List<QueryResult> queryResults) {
+        // afaik we can get
         if (includeValue) {
-            populateWithValues(queryCache, queryResultSets);
+            populateWithValues(queryCache, queryResults);
         } else {
-            populateWithoutValues(queryCache, queryResultSets);
+            populateWithoutValues(queryCache, queryResults);
         }
     }
 
-    private void populateWithValues(InternalQueryCache queryCache, List<QueryResultSet> queryResultSets) {
-        for (QueryResultSet resultSet : queryResultSets) {
-            if (resultSet == null || resultSet.isEmpty()) {
+    private void populateWithValues(InternalQueryCache queryCache, List<QueryResult> queryResults) {
+        for (QueryResult queryResult : queryResults) {
+            if (queryResult == null || queryResult.isEmpty()) {
                 continue;
             }
-            Iterator iterator = resultSet.iterator();
-            while (iterator.hasNext()) {
-                AbstractMap.SimpleImmutableEntry entry = (AbstractMap.SimpleImmutableEntry) iterator.next();
-                Data dataKey = (Data) entry.getKey();
-                Data valueData = (Data) entry.getValue();
-                queryCache.setInternal(dataKey, valueData, false, EntryEventType.ADDED);
+            for (QueryResultRow row : queryResult) {
+                queryCache.setInternal(row.getKey(), row.getValue(), false, EntryEventType.ADDED);
             }
         }
     }
 
-    private void populateWithoutValues(InternalQueryCache queryCache, List<QueryResultSet> queryResultSets) {
-        for (QueryResultSet resultSet : queryResultSets) {
-            if (resultSet == null || resultSet.isEmpty()) {
+    private void populateWithoutValues(InternalQueryCache queryCache, List<QueryResult> queryResultSets) {
+        for (QueryResult queryResult : queryResultSets) {
+            if (queryResult == null || queryResult.isEmpty()) {
                 continue;
             }
-            Iterator iterator = resultSet.iterator();
-            while (iterator.hasNext()) {
-                Data dataKey = (Data) iterator.next();
-                queryCache.setInternal(dataKey, null, false, EntryEventType.ADDED);
+            for (QueryResultRow row : queryResult) {
+                queryCache.setInternal(row.getKey(), null, false, EntryEventType.ADDED);
             }
         }
     }
-
 }
