69784cf2a3da39cfd74a0feaecc2f35202fbd9d9
mustafaiman <mustafa.iman@hazelcast.com>
2015-10-02 17:17:30 +0300

    HazelcastAccessor of Hibernate deprecated and cleanup in hibernate

--- hazelcast-hibernate/hazelcast-hibernate3/src/test/java/com/hazelcast/hibernate/CustomPropertiesTest.java
+++ hazelcast-hibernate/hazelcast-hibernate3/src/test/java/com/hazelcast/hibernate/CustomPropertiesTest.java
@@ -30,15 +30,18 @@ import com.hazelcast.test.annotation.SlowTest;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.hibernate.cache.CacheException;
 
 import java.util.Date;
 import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(HazelcastSerialClassRunner.class)
@@ -92,18 +95,49 @@ public class CustomPropertiesTest extends HibernateTestSupport {
         Config config = new Config();
         config.setInstanceName("hibernate");
         HazelcastInstance hz = factory.newHazelcastInstance(config);
-        Properties props = getDefaultProperties();
+        Properties props = new Properties();
         props.setProperty(Environment.CACHE_REGION_FACTORY, HazelcastCacheRegionFactory.class.getName());
         props.put(CacheEnvironment.HAZELCAST_INSTANCE_NAME, "hibernate");
         props.put(CacheEnvironment.SHUTDOWN_ON_STOP, "false");
-        HazelcastMockInstanceLoader instanceLoader = new HazelcastMockInstanceLoader();
-        instanceLoader.configure(props);
-        instanceLoader.setInstanceFactory(factory);
-        final SessionFactory sf = createSessionFactory(props, instanceLoader);
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+
+        Configuration configuration = new Configuration();
+        configuration.addProperties(props);
+
+        SessionFactory sf = configuration.buildSessionFactory();
         assertTrue(hz.equals(HazelcastAccessor.getHazelcastInstance(sf)));
         sf.close();
         assertTrue(hz.getLifecycleService().isRunning());
         factory.shutdownAll();
+    }
+
+    @Test
+    public void testHazelcastAccessorReturnsNullIfSecondLevelCacheIsNotHazelcast() {
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+
+        Configuration configuration = new Configuration();
+        configuration.addProperties(props);
+        SessionFactory sf = configuration.buildSessionFactory();
+
+        assertNull(HazelcastAccessor.getHazelcastInstance(sf));
+
+        sf.close();
+    }
+
+    @Test(expected = CacheException.class)
+    public void testWrongHazelcastConfigurationFilePathShouldThrow() {
+        Properties props = new Properties();
+        props.setProperty(Environment.CACHE_REGION_FACTORY, HazelcastCacheRegionFactory.class.getName());
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+
+        //we give a non-exist config file address, should fail fast
+        props.setProperty("hibernate.cache.hazelcast.configuration_file_path", "non-exist.xml");
+
+        Configuration configuration = new Configuration();
+        configuration.addProperties(props);
+        SessionFactory sf = configuration.buildSessionFactory();
+        sf.close();
     }
 
     private Properties getDefaultProperties() {
