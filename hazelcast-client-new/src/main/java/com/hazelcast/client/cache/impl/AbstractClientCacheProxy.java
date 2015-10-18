0ab2704c7ba19c8a41c97910a6adf537c9f38044
Serkan OZAL <serkan@hazelcast.com>
2015-10-18 12:53:28 +0300

    `CachePutAllCodec.encodeRequest` fixed by passing `completionId` instead of `partitionId` for putAll operation on new-client

--- hazelcast-client-new/src/main/java/com/hazelcast/client/cache/impl/AbstractClientCacheProxy.java
+++ hazelcast-client-new/src/main/java/com/hazelcast/client/cache/impl/AbstractClientCacheProxy.java
@@ -398,7 +398,7 @@ abstract class AbstractClientCacheProxy<K, V>
                 int completionId = nextCompletionId();
                 // TODO If there is a single entry, we could make use of a put operation since that is a bit cheaper
                 ClientMessage request =
-                        CachePutAllCodec.encodeRequest(nameWithPrefix, entries, expiryPolicyData, partitionId);
+                        CachePutAllCodec.encodeRequest(nameWithPrefix, entries, expiryPolicyData, completionId);
                 Future f = invoke(request, partitionId, completionId);
                 futureEntriesTuples.add(new FutureEntriesTuple(f, entries));
                 if (nearCache != null) {
