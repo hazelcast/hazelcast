982f70008f407bd105ca5e81f8b101d308c59919
Serkan OZAL <serkan@hazelcast.com>
2015-10-19 17:08:56 +0300

    Use specified instance if instance name is given regardless from URI is specified or not

--- hazelcast/src/main/java/com/hazelcast/cache/impl/HazelcastServerCachingProvider.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/HazelcastServerCachingProvider.java
@@ -58,42 +58,31 @@ public final class HazelcastServerCachingProvider
     @Override
     protected HazelcastServerCacheManager createHazelcastCacheManager(URI uri, ClassLoader classLoader,
                                                                       Properties properties) {
+        final boolean isDefaultURI = (uri == null || uri.equals(getDefaultURI()));
         final HazelcastInstance instance;
-        //uri is null or default or a non hazelcast one, then we use the internal shared instance
-        if (uri == null || uri.equals(getDefaultURI())) {
-            if (hazelcastInstance == null) {
-                try {
-                    hazelcastInstance = instanceFromProperties(classLoader, properties, true);
-                } catch (Exception e) {
-                    throw ExceptionUtil.rethrow(e);
-                }
+        try {
+            instance = getOrCreateInstance(classLoader, properties, isDefaultURI);
+            if (instance == null) {
+                throw new IllegalArgumentException(INVALID_HZ_INSTANCE_SPECIFICATION_MESSAGE);
             }
-            instance = hazelcastInstance;
-        } else {
-            try {
-                instance = instanceFromProperties(classLoader, properties, false);
-                if (instance == null) {
-                    throw new IllegalArgumentException(INVALID_HZ_INSTANCE_SPECIFICATION_MESSAGE);
-                }
-            } catch (Exception e) {
-                throw ExceptionUtil.rethrow(e);
-            }
+        } catch (Exception e) {
+            throw ExceptionUtil.rethrow(e);
         }
         return new HazelcastServerCacheManager(this, instance, uri, classLoader, properties);
     }
 
-    private HazelcastInstance instanceFromProperties(ClassLoader classLoader, Properties properties, boolean isDefault)
+    private HazelcastInstance getOrCreateInstance(ClassLoader classLoader, Properties properties, boolean isDefaultURI)
             throws URISyntaxException, IOException {
-        ClassLoader theClassLoader = classLoader == null ? getDefaultClassLoader() : classLoader;
-        HazelcastInstance instance = null;
         String location = properties.getProperty(HazelcastCachingProvider.HAZELCAST_CONFIG_LOCATION);
+        // If config location is specified, get instance through it.
         if (location != null) {
             URI uri = new URI(location);
             String scheme = uri.getScheme();
             if (scheme == null) {
-                //it is a place holder
+                // It is a place holder
                 uri = new URI(System.getProperty(uri.getRawSchemeSpecificPart()));
             }
+            ClassLoader theClassLoader = classLoader == null ? getDefaultClassLoader() : classLoader;
             final URL configURL;
             if ("classpath".equals(scheme)) {
                 configURL = theClassLoader.getResource(uri.getRawSchemeSpecificPart());
@@ -105,23 +94,32 @@ public final class HazelcastServerCachingProvider
             try {
                 Config config = new XmlConfigBuilder(configURL).build();
                 config.setClassLoader(theClassLoader);
-                //TODO should we assign an instance name
                 config.setInstanceName(configURL.toString());
-                instance = HazelcastInstanceFactory.getOrCreateHazelcastInstance(config);
+                return HazelcastInstanceFactory.getOrCreateHazelcastInstance(config);
             } catch (Exception e) {
                 throw ExceptionUtil.rethrow(e);
             }
         }
+
+        // If config location is specified, get instance with its name.
         String instanceName = properties.getProperty(HazelcastCachingProvider.HAZELCAST_INSTANCE_NAME);
         if (instanceName != null) {
-            instance = Hazelcast.getHazelcastInstanceByName(instanceName);
+            return Hazelcast.getHazelcastInstanceByName(instanceName);
         }
-        if (isDefault) {
-            Config config = new XmlConfigBuilder().build();
-            if (config.getInstanceName() == null) {
-                config.setInstanceName("JCacheSharedInstance");
+
+        HazelcastInstance instance = null;
+        // No instance specified with name of config location,
+        // so we are going on with the default one if the URI is the default.
+        if (isDefaultURI) {
+            if (hazelcastInstance == null) {
+                // If there is no default instance in use (not created yet and not specified), create a new one.
+                instance = Hazelcast.newHazelcastInstance();
+                // Since there is no default instance in use, set new instance as default one.
+                hazelcastInstance = instance;
+            } else {
+                // Use the existing default instance.
+                instance = hazelcastInstance;
             }
-            instance = HazelcastInstanceFactory.getOrCreateHazelcastInstance(config);
         }
         return instance;
     }
---
+++ hazelcast/src/test/java/com/hazelcast/cache/CachingProviderTest.java
@@ -0,0 +1,114 @@
+package com.hazelcast.cache;
+
+import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.TestHazelcastInstanceFactory;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import javax.cache.CacheException;
+import javax.cache.spi.CachingProvider;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class CachingProviderTest extends HazelcastTestSupport {
+
+    protected static final int INSTANCE_COUNT = 2;
+
+    protected TestHazelcastInstanceFactory instanceFactory;
+
+    @Before
+    public void setup() {
+        instanceFactory = createTestHazelcastInstanceFactory(INSTANCE_COUNT);
+    }
+
+    protected TestHazelcastInstanceFactory createTestHazelcastInstanceFactory(int count) {
+        return createHazelcastInstanceFactory(count);
+    }
+
+    protected HazelcastInstance createCacheInstance() {
+        return instanceFactory.newHazelcastInstance();
+    }
+
+    protected CachingProvider createCachingProvider(HazelcastInstance defaultInstance) {
+        return HazelcastServerCachingProvider.createCachingProvider(defaultInstance);
+    }
+
+    protected enum InstanceNameUsageType {
+
+        VALID_INSTANCE_NAME,
+        INVALID_INSTANCE_NAME,
+        NO_INSTANCE_NAME
+    }
+
+    protected void checkUsedInstanceInCachingProvider(boolean uriSpecified,
+                                                      InstanceNameUsageType instanceNameUsageType) throws URISyntaxException {
+        HazelcastInstance instance1 = createCacheInstance();
+        HazelcastInstance instance2 = createCacheInstance();
+        CachingProvider cachingProvider = createCachingProvider(instance1);
+        Properties properties = new Properties();
+        if (InstanceNameUsageType.VALID_INSTANCE_NAME == instanceNameUsageType) {
+            properties.setProperty(HazelcastCachingProvider.HAZELCAST_INSTANCE_NAME, instance2.getName());
+        } else if (InstanceNameUsageType.INVALID_INSTANCE_NAME == instanceNameUsageType) {
+            properties.setProperty(HazelcastCachingProvider.HAZELCAST_INSTANCE_NAME, instance2.getName() + "-Invalid");
+        }
+        HazelcastCacheManager cacheManager =
+                (HazelcastCacheManager) cachingProvider.getCacheManager(
+                        uriSpecified ? new URI("MyURI") : null, null, properties);
+
+        assertNotNull(cacheManager);
+
+        HazelcastInstance instance = cacheManager.getHazelcastInstance();
+
+        if (InstanceNameUsageType.NO_INSTANCE_NAME == instanceNameUsageType) {
+            assertEquals(instance1, instance);
+        } else {
+            assertEquals(instance2, instance);
+        }
+    }
+
+    @Test
+    public void specifiedInstanceShouldBeUsedWhenInstanceNameIsGivenWithDefaultURI() throws URISyntaxException {
+        checkUsedInstanceInCachingProvider(false, InstanceNameUsageType.VALID_INSTANCE_NAME);
+    }
+
+    @Test
+    public void specifiedInstanceShouldBeUsedWhenInstanceNameIsGivenWithSpecifiedURI() throws URISyntaxException {
+        checkUsedInstanceInCachingProvider(true, InstanceNameUsageType.VALID_INSTANCE_NAME);
+    }
+
+    @Test(expected = CacheException.class)
+    public void cacheManagerShouldNotBeAbleToCreatedWhenGivenInstanceNameIsInvalidWithDefaultURI() throws URISyntaxException {
+        checkUsedInstanceInCachingProvider(false, InstanceNameUsageType.INVALID_INSTANCE_NAME);
+    }
+
+    @Test(expected = CacheException.class)
+    public void cacheManagerShouldNotBeAbleToCreatedWhenGivenInstanceNameIsInvalidWithSpecifiedURI() throws URISyntaxException {
+        checkUsedInstanceInCachingProvider(true, InstanceNameUsageType.INVALID_INSTANCE_NAME);
+    }
+
+    @Test
+    public void defaultInstanceShouldBeUsedWhenInstanceNameIsNotGivenWithDefaultURI() throws URISyntaxException {
+        checkUsedInstanceInCachingProvider(false, InstanceNameUsageType.NO_INSTANCE_NAME);
+    }
+
+    @Test(expected = CacheException.class)
+    public void cacheManagerShouldNotBeAbleToCreatedWhenInstanceNameIsNotGivenWithSpecifiedURI() throws URISyntaxException {
+        checkUsedInstanceInCachingProvider(true, InstanceNameUsageType.NO_INSTANCE_NAME);
+    }
+
+}
