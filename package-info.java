09704115f506f2220ae62506af5bf448b61d5623
sancar <sancar@hazelcast.com>
2015-10-21 09:53:02 +0300

    rename old client to legacy

--- package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains query cache internal implementations for client side.
- */
-package com.hazelcast.client.impl.querycache;
--- subscriber/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains Hazelcast Enterprise version client-side query cache
- * subscriber implementation classes.
- */
-package com.hazelcast.client.impl.querycache.subscriber;
