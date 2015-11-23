79bb6f686409644ca32052cb0aa33d4749be6619
Mehmet Dogan <mehmet@dogan.io>
2015-11-23 10:38:13 +0200

    Increase transaction timeout in AdvancedClusterStateTest
    Increased transaction timeout in AdvancedClusterStateTest in remaining
    test. See PR #6763.
    
    Fixes #6383

--- hazelcast/src/test/java/com/hazelcast/cluster/impl/AdvancedClusterStateTest.java
+++ hazelcast/src/test/java/com/hazelcast/cluster/impl/AdvancedClusterStateTest.java
@@ -222,7 +222,7 @@ public class AdvancedClusterStateTest extends HazelcastTestSupport {
         });
     }
 
-    @Test
+    @Test(timeout = 60000)
     public void changeClusterState_shouldFail_withoutBackup_whenInitiatorDies_afterPrepare() throws Exception {
         final TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(3);
         final HazelcastInstance[] instances = factory.newInstances();
@@ -230,7 +230,7 @@ public class AdvancedClusterStateTest extends HazelcastTestSupport {
         final HazelcastInstance hz = instances[instances.length - 1];
         TransactionManagerServiceImpl transactionManagerService = spyTransactionManagerService(hz);
 
-        TransactionOptions options = new TransactionOptions().setDurability(0).setTimeout(10, TimeUnit.SECONDS);
+        TransactionOptions options = new TransactionOptions().setDurability(0).setTimeout(30, TimeUnit.SECONDS);
         when(transactionManagerService.newAllowedDuringPassiveStateTransaction(options)).thenAnswer(new TransactionAnswer() {
             @Override
             protected void afterPrepare() {
