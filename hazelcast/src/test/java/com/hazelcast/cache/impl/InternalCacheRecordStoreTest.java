be3967efab5a7987d695dd4bfc3d72c61babc966
Donnerbart <github@thunderphreak.de>
2015-11-26 14:54:25 +0100

    Cleanup of InternalCacheRecordStoreTest.

--- hazelcast/src/test/java/com/hazelcast/cache/impl/InternalCacheRecordStoreTest.java
+++ hazelcast/src/test/java/com/hazelcast/cache/impl/InternalCacheRecordStoreTest.java
@@ -5,7 +5,7 @@ import com.hazelcast.cache.CacheTestSupport;
 import com.hazelcast.cache.HazelcastCacheManager;
 import com.hazelcast.config.CacheConfig;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.instance.TestUtil;
+import com.hazelcast.instance.Node;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
 import org.junit.Test;
 
@@ -16,17 +16,19 @@ import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class InternalCacheRecorStoreTest extends CacheTestSupport {
+public class InternalCacheRecordStoreTest extends CacheTestSupport {
 
-    TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory();
+    private TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory();
 
-    //https://github.com/hazelcast/hazelcast/issues/6618
+    /**
+     * Test for issue: https://github.com/hazelcast/hazelcast/issues/6618
+     */
     @Test
     public void testBatchEventMapShouldBeCleanedAfterRemoveAll() {
         String cacheName = randomString();
 
         CacheConfig<Integer, String> config = createCacheConfig();
-        final CacheFromDifferentNodesTest.SimpleEntryListener<Integer, String> listener =
+        CacheFromDifferentNodesTest.SimpleEntryListener<Integer, String> listener =
                 new CacheFromDifferentNodesTest.SimpleEntryListener<Integer, String>();
         MutableCacheEntryListenerConfiguration<Integer, String> listenerConfiguration =
                 new MutableCacheEntryListenerConfiguration<Integer, String>(
@@ -46,8 +48,12 @@ public class InternalCacheRecorStoreTest extends CacheTestSupport {
 
         cache.removeAll();
 
-        ICacheService cacheService = TestUtil.getNode(instance).getNodeEngine().getService(ICacheService.SERVICE_NAME);
-        AbstractCacheRecordStore recordStore = (AbstractCacheRecordStore) cacheService.getRecordStore("/hz/" + cacheName, partitionId);
+        Node node = getNode(instance);
+        assertNotNull(node);
+
+        ICacheService cacheService = node.getNodeEngine().getService(ICacheService.SERVICE_NAME);
+        AbstractCacheRecordStore recordStore = (AbstractCacheRecordStore) cacheService
+                .getRecordStore("/hz/" + cacheName, partitionId);
         assertEquals(0, recordStore.batchEvent.size());
     }
 
