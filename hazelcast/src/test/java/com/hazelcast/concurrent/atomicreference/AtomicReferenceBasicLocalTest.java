c32ad40f85061b84724dd9b5b8479eebea8675a0
Donnerbart <github@thunderphreak.de>
2015-11-26 15:38:12 +0100

    Fixed names of AtomicReference tests, so that class names containing abstract are not executable and class names containing basic are executable test classes (e.g. @RunWith).

--- hazelcast/src/test/java/com/hazelcast/concurrent/atomicreference/AtomicReferenceBasicLocalTest.java
+++ hazelcast/src/test/java/com/hazelcast/concurrent/atomicreference/AtomicReferenceBasicLocalTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(HazelcastParallelClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
-public class AtomicReferenceBasicLocalTest extends AtomicReferenceBasicTest {
+public class AtomicReferenceBasicLocalTest extends AtomicReferenceAbstractTest {
 
     @Override
     protected HazelcastInstance[] newInstances() {
