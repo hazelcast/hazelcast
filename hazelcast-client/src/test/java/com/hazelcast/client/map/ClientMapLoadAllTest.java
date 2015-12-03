73150ae73b4ed21eb9393dc211b21d556a955907
hasancelik <hasan@hazelcast.com>
2015-12-03 12:12:58 +0200

    ignored testLoadAll_givenKeys

--- hazelcast-client/src/test/java/com/hazelcast/client/map/ClientMapLoadAllTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/map/ClientMapLoadAllTest.java
@@ -10,6 +10,7 @@ import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -62,6 +63,7 @@ public class ClientMapLoadAllTest extends AbstractMapStoreTest {
     }
 
     @Test
+    @Ignore
     public void testLoadAll_givenKeys() throws Exception {
         final String mapName = randomMapName();
         final Config config = createNewConfig(mapName);
