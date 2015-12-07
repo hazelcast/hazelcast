b90d2ea5c62151482c63fd33180046bbebec5cc3
Ángel Luis López Monterroso <angel9484@gmail.com>
2015-12-07 14:10:05 +0100

    Fixed tests.

--- hazelcast/src/test/java/com/hazelcast/map/impl/DefaultRecordStoreTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/DefaultRecordStoreTest.java
@@ -1,15 +1,9 @@
 package com.hazelcast.map.impl;
 
-import com.hazelcast.logging.ILogger;
-import com.hazelcast.map.impl.mapstore.MapStoreContext;
-import com.hazelcast.map.impl.mapstore.MapStoreManager;
 import com.hazelcast.map.impl.record.Record;
-import com.hazelcast.map.impl.record.RecordFactory;
+import com.hazelcast.map.impl.recordstore.DefaultRecordStore;
 import com.hazelcast.nio.serialization.Data;
-import com.hazelcast.nio.serialization.SerializationService;
-import com.hazelcast.spi.NodeEngine;
 import org.junit.Test;
-import org.mockito.stubbing.OngoingStubbing;
 
 import java.util.Date;
 
@@ -24,7 +18,6 @@ public class DefaultRecordStoreTest {
     @Test
     public void testReadBackupWhenReturnIsNull() throws Exception {
         DefaultRecordStore defaultRecordStore = mock(DefaultRecordStore.class);
-        when(defaultRecordStore.getNow()).thenReturn(new Date(9999).getTime());
         when(defaultRecordStore.getRecord(mock(Data.class))).thenReturn(null);
         Object o = defaultRecordStore.readBackup(mock(Data.class));
         assertNull(o);
@@ -34,7 +27,6 @@ public class DefaultRecordStoreTest {
     public void testReadBackupWhenReturnIsExpired() throws Exception {
         DefaultRecordStore defaultRecordStore = mock(DefaultRecordStore.class);
         long now = new Date(9999).getTime();
-        when(defaultRecordStore.getNow()).thenReturn(now);
         Record record = mock(Record.class);
         doReturn(record).when(defaultRecordStore).getRecord(mock(Data.class));
         doReturn(true).when(defaultRecordStore).isExpired(null, now, false);
