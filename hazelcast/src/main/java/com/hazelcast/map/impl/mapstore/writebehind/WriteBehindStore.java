1495461a2b5cf0829737a8ed00595f0064814f43
Ahmet MIRCIK <ahmet@hazelcast.com>
2015-12-12 23:36:42 +0200

    Removes extra serialization when coalescing mode is on

--- hazelcast/src/main/java/com/hazelcast/map/impl/mapstore/writebehind/WriteBehindStore.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/mapstore/writebehind/WriteBehindStore.java
@@ -33,6 +33,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static com.hazelcast.config.InMemoryFormat.OBJECT;
+
 /**
  * Write behind map data store implementation.
  * Created per every record-store. Only called from one thread.
@@ -58,6 +60,11 @@ public class WriteBehindStore extends AbstractMapDataStore<Data, Object> {
     private final AtomicInteger flushCounter;
 
     private final InMemoryFormat inMemoryFormat;
+
+    /**
+     * @see {@link com.hazelcast.config.MapStoreConfig#setWriteCoalescing(boolean)}
+     */
+    private final boolean coalesce;
 
     private WriteBehindQueue<DelayedEntry> writeBehindQueue;
 
@@ -85,27 +92,32 @@ public class WriteBehindStore extends AbstractMapDataStore<Data, Object> {
     private final ConcurrentMap<Data, DelayedEntry> stagingArea;
 
     public WriteBehindStore(MapStoreWrapper store, SerializationService serializationService,
-                            long writeDelayTime, int partitionId, InMemoryFormat inMemoryFormat) {
+                            long writeDelayTime, int partitionId, InMemoryFormat inMemoryFormat, boolean coalesce) {
         super(store, serializationService);
         this.writeDelayTime = writeDelayTime;
         this.partitionId = partitionId;
         this.stagingArea = new ConcurrentHashMap<Data, DelayedEntry>();
         this.flushCounter = new AtomicInteger(0);
         this.inMemoryFormat = inMemoryFormat;
+        this.coalesce = coalesce;
     }
 
 
     @Override
     public Object add(Data key, Object value, long now) {
-
-        // we will be in this `if` only in case of an entry modification via entry-processor.
-        // otherwise this extra serialization of value should not be happen.
-        if (InMemoryFormat.OBJECT.equals(inMemoryFormat)) {
+        // This note describes the problem when we want to persist all states of an entry (means write-coalescing is off)
+        // by using both EntryProcessor + OBJECT in-memory-format:
+        //
+        // If in-memory-format is OBJECT, there is a possibility that a previous state of an entry can be overwritten
+        // by a subsequent write operation while both are waiting in the write-behind-queue, this is because they are referencing
+        // to the same entry-value. To prevent such a problem, we are taking snapshot of the value by serializing it,
+        // this means an extra serialization and additional latency for operations like map#put but it is needed,
+        // otherwise we can lost a state.
+        if (!coalesce && OBJECT == inMemoryFormat) {
             value = toData(value);
         }
 
-        long writeDelay = this.writeDelayTime;
-        long storeTime = now + writeDelay;
+        long storeTime = now + writeDelayTime;
         DelayedEntry<Data, Object> delayedEntry
                 = DelayedEntries.createDefault(key, value, storeTime, partitionId);
 
@@ -131,8 +143,7 @@ public class WriteBehindStore extends AbstractMapDataStore<Data, Object> {
 
     @Override
     public void remove(Data key, long now) {
-        long writeDelay = this.writeDelayTime;
-        long storeTime = now + writeDelay;
+        long storeTime = now + writeDelayTime;
         DelayedEntry<Data, Object> delayedEntry
                 = DelayedEntries.createWithoutValue(key, storeTime, partitionId);
 
