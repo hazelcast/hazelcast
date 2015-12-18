0955482ca200c79f6f9b4f3da40f51f9ccbfbae0
Can Gencer <cgencer@gmail.com>
2015-12-18 11:46:06 +0200

    Increase timeouts for client stress tests

--- hazelcast-client-new/src/test/java/com/hazelcast/client/stress/AtomicLongUpdateStressTest.java
+++ hazelcast-client-new/src/test/java/com/hazelcast/client/stress/AtomicLongUpdateStressTest.java
@@ -65,7 +65,7 @@ public class AtomicLongUpdateStressTest extends StressTestSupport {
         test(true);
     }
 
-    @Test
+    @Test(timeout=600000)
     public void testFixedCluster() {
         test(false);
     }
