a49c6325c04ef76804d49ecc9644097f5be9dfc6
muratayan <murat@hazelcast.com>
2015-12-23 14:50:46 +0200

    Removed listeners from JMX Beans

--- hazelcast/src/main/java/com/hazelcast/jmx/ListMBean.java
+++ hazelcast/src/main/java/com/hazelcast/jmx/ListMBean.java
@@ -17,39 +17,15 @@
 package com.hazelcast.jmx;
 
 import com.hazelcast.core.IList;
-import com.hazelcast.core.ItemEvent;
-import com.hazelcast.core.ItemListener;
-import java.util.concurrent.atomic.AtomicLong;
-
-import static com.hazelcast.util.EmptyStatement.ignore;
 
 /**
  * Management bean for {@link com.hazelcast.core.IList}
  */
 @ManagedDescription("IList")
 public class ListMBean extends HazelcastMBean<IList<?>> {
-
-    private AtomicLong totalAddedItemCount = new AtomicLong();
-    private AtomicLong totalRemovedItemCount = new AtomicLong();
-    private final String registrationId;
-
     protected ListMBean(IList<?> managedObject, ManagementService service) {
         super(managedObject, service);
         objectName = service.createObjectName("IList", managedObject.getName());
-
-        //todo: using the event system to register number of adds/remove is an very expensive price to pay.
-        ItemListener itemListener = new ItemListener() {
-            @Override
-            public void itemAdded(ItemEvent item) {
-                totalAddedItemCount.incrementAndGet();
-            }
-
-            @Override
-            public void itemRemoved(ItemEvent item) {
-                totalRemovedItemCount.incrementAndGet();
-            }
-        };
-        registrationId = managedObject.addItemListener(itemListener, false);
     }
 
     @ManagedAnnotation(value = "clear", operation = true)
@@ -62,24 +38,5 @@ public class ListMBean extends HazelcastMBean<IList<?>> {
     @ManagedDescription("Name of the DistributedObject")
     public String getName() {
         return managedObject.getName();
-    }
-
-    @ManagedAnnotation("totalAddedItemCount")
-    public long getTotalAddedItemCount() {
-        return totalAddedItemCount.get();
-    }
-
-    @ManagedAnnotation("totalRemovedItemCount")
-    public long getTotalRemovedItemCount() {
-        return totalRemovedItemCount.get();
-    }
-
-    public void preDeregister() throws Exception {
-        super.preDeregister();
-        try {
-            managedObject.removeItemListener(registrationId);
-        } catch (Exception ignored) {
-            ignore(ignored);
-        }
     }
 }
