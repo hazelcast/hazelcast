a7a38e02dcc0f88adf8106b5dbdf5494f9524427
LoneRifle <LoneRifle@users.noreply.github.com>
2015-12-29 18:44:27 +0800

    Revert "Test fixes"
    This reverts commit d06bd10f65462193e9d42d781fa3fe1f3c05b5ef.

--- hazelcast-client-legacy/src/test/java/com/hazelcast/client/map/ClientMapBasicTest.java
+++ hazelcast-client-legacy/src/test/java/com/hazelcast/client/map/ClientMapBasicTest.java
@@ -23,12 +23,10 @@ import com.hazelcast.core.EntryView;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.core.MapEvent;
-import com.hazelcast.map.listener.EntryEvictedListener;
 import com.hazelcast.monitor.LocalMapStats;
 import com.hazelcast.query.Predicate;
 import com.hazelcast.query.SqlPredicate;
 import com.hazelcast.test.HazelcastParallelClassRunner;
-import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
@@ -42,13 +40,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import static com.hazelcast.test.HazelcastTestSupport.randomString;
 import static com.hazelcast.test.HazelcastTestSupport.sleepSeconds;
-import static com.hazelcast.test.HazelcastTestSupport.assertOpenEventually;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -363,16 +359,9 @@ public class ClientMapBasicTest {
         String key = "Key";
         String value = "Val";
 
-        final CountDownLatch latch = new CountDownLatch(1);
-        map.addEntryListener(new EntryEvictedListener<String, String>() {
-            public void entryEvicted(EntryEvent<String, String> event) {
-                latch.countDown();
-            }
-        }, true);
-
         Future<Void> result = map.setAsync(key, value, 1, TimeUnit.SECONDS);
+        sleepSeconds(2);
         result.get();
-        assertOpenEventually(latch);
         assertEquals(null, map.get(key));
     }
 
@@ -383,17 +372,10 @@ public class ClientMapBasicTest {
         String oldValue = "oldValue";
         String newValue = "Val";
 
-        final CountDownLatch latch = new CountDownLatch(1);
-        map.addEntryListener(new EntryEvictedListener<String, String>() {
-            public void entryEvicted(EntryEvent<String, String> event) {
-                latch.countDown();
-            }
-        }, true);
-
         map.set(key, oldValue);
         Future<Void> result = map.setAsync(key, newValue, 1, TimeUnit.SECONDS);
+        sleepSeconds(2);
         result.get();
-        assertOpenEventually(latch);
         assertEquals(null, map.get(key));
     }
 
--- hazelcast-client-legacy/src/test/java/com/hazelcast/client/map/ClientMapTest.java
+++ hazelcast-client-legacy/src/test/java/com/hazelcast/client/map/ClientMapTest.java
@@ -298,7 +298,7 @@ public class ClientMapTest {
         future.get();
         assertEquals("value1", map.get("key"));
 
-        assertOpenEventually(latch);
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
         assertNull(map.get("key"));
     }
 
