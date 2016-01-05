dc050d4adc218769b52b57d7bb1263200c945ccf
Serkan ÖZAL <serkan@hazelcast.com>
2016-01-05 13:50:22 +0200

    Typo fixed in `CacheBasicAbstractTest::expiryTimeShouldNotBeChangedOnUpdateWhenCreatedExpiryPolicyIsUsed` and expiry time has been decreased

--- hazelcast/src/test/java/com/hazelcast/cache/CacheBasicAbstractTest.java
+++ hazelcast/src/test/java/com/hazelcast/cache/CacheBasicAbstractTest.java
@@ -821,9 +821,9 @@ public abstract class CacheBasicAbstractTest extends CacheTestSupport {
     // https://github.com/hazelcast/hazelcast/issues/7236
     @Test
     public void expiryTimeShouldNotBeChangedOnUpdateWhenCreatedExpiryPolicyIsUsed() {
-        final int CRETAED_EXPIRY_TIME_IN_MSEC = 1000;
+        final int CREATED_EXPIRY_TIME_IN_MSEC = 100;
 
-        Duration duration = new Duration(TimeUnit.MILLISECONDS, CRETAED_EXPIRY_TIME_IN_MSEC);
+        Duration duration = new Duration(TimeUnit.MILLISECONDS, CREATED_EXPIRY_TIME_IN_MSEC);
         CacheConfig<Integer, String> cacheConfig = new CacheConfig<Integer, String>();
         cacheConfig.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(duration));
 
@@ -831,7 +831,7 @@ public abstract class CacheBasicAbstractTest extends CacheTestSupport {
         cache.put(1, "value");
         cache.put(1, "value");
 
-        sleepAtLeastMillis(CRETAED_EXPIRY_TIME_IN_MSEC + 1);
+        sleepAtLeastMillis(CREATED_EXPIRY_TIME_IN_MSEC + 1);
 
         assertNull(cache.get(1));
     }
