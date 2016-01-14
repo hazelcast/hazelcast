29e0037e6e8ac9b3686221a5975f018d44eded82
Ali Gurbuz <ali@hazelcast.com>
2016-01-14 09:20:23 +0200

    Fixed race in IdGenerator, moved logic to IdGeneratorImpl to reuse from client-side

--- hazelcast/src/test/java/com/hazelcast/concurrent/idgen/IdGeneratorStressTest.java
+++ hazelcast/src/test/java/com/hazelcast/concurrent/idgen/IdGeneratorStressTest.java
@@ -1,22 +1,108 @@
 package com.hazelcast.concurrent.idgen;
 
-import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IdGenerator;
+import com.hazelcast.test.HazelcastTestRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.TestHazelcastInstanceFactory;
+import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
+import com.hazelcast.test.annotation.RunParallel;
+import com.hazelcast.util.RandomPicker;
+import com.hazelcast.util.collection.LongHashSet;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-@RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
-public class IdGeneratorStressTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-    @Test
-    public void testMultipleThreadsInSameHazelcastInstance(){
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+@RunParallel
+@RunWith(HazelcastTestRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
+public class IdGeneratorStressTest extends HazelcastTestSupport {
+
+    private static final int THREAD_COUNT = 32;
+
+    private static final int NUMBER_OF_IDS_PER_THREAD = 400001;
+
+    private static final int TOTAL_ID_GENERATED = THREAD_COUNT * NUMBER_OF_IDS_PER_THREAD;
+
+    @Parameterized.Parameter(0)
+    public int clusterSize;
+
+    @Parameterized.Parameters(name = "clusterSize:{0}")
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(new Object[][]{
+                {1},
+                {6},
+        });
+    }
+
+    String name;
+    HazelcastInstance[] instances;
+
+    @Before
+    public void setup() {
+        TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(clusterSize);
+        instances = factory.newInstances();
+        name = randomString();
     }
 
     @Test
-    public void testMultipleThreadsInDifferentInstances(){
+    public void testMultipleThreads() throws ExecutionException, InterruptedException {
+        pickIdGenerator().init(13013);
 
+        List<Future> futureList = new ArrayList<Future>(THREAD_COUNT);
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            IdGenerator idGenerator = pickIdGenerator();
+            IdGeneratorCallable callable = new IdGeneratorCallable(idGenerator);
+            Future<long[]> future = spawn(callable);
+            futureList.add(future);
+        }
+
+        LongHashSet totalGeneratedIds = new LongHashSet(TOTAL_ID_GENERATED, -1);
+        for (Future<long[]> future : futureList) {
+            long[] generatedIds = future.get();
+            for (long generatedId : generatedIds) {
+                assertTrue("ID: " + generatedId, totalGeneratedIds.add(generatedId));
+            }
+        }
+
+        assertEquals(TOTAL_ID_GENERATED, totalGeneratedIds.size());
     }
+
+    private IdGenerator pickIdGenerator() {
+        int index = RandomPicker.getInt(instances.length);
+        return instances[index].getIdGenerator(name);
+    }
+
+    private static class IdGeneratorCallable implements Callable<long[]> {
+
+        IdGenerator idGenerator;
+
+        public IdGeneratorCallable(IdGenerator idGenerator) {
+            this.idGenerator = idGenerator;
+        }
+
+        @Override
+        public long[] call() throws Exception {
+            long[] generatedIds = new long[NUMBER_OF_IDS_PER_THREAD];
+            for (int j = 0; j < NUMBER_OF_IDS_PER_THREAD; j++) {
+                generatedIds[j] = idGenerator.newId();
+            }
+            return generatedIds;
+        }
+    }
+
 }
