eb5c6480640f2287864f99da78db4947b0498fa6
Mehmet Dogan <mehmet@dogan.io>
2016-01-15 15:21:40 +0200

    Invalid partition id should not crash partition threads
    When an operation, a partition-specific-runnable or a packet with an
    invalid partition id (greater than max partition id) is submitted to
    operation executor, it causes partition operation thread to crash with
    ArrayIndexOutOfBoundsException. After that point, no task can be executed
    on any that thread and also thread's task queue can lead to oome.
    
    Even this can be considered a security failure, one can send a corrupted
    client message to a server and server becomes stuck.
    
    (cherry picked from commit 472e8d965f3558f99608cc1b59909770d14a7959)

--- hazelcast/src/test/java/com/hazelcast/spi/impl/operationexecutor/classic/OperationThreadTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/impl/operationexecutor/classic/OperationThreadTest.java
@@ -4,6 +4,7 @@ import com.hazelcast.instance.OutOfMemoryErrorDispatcher;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.nio.Packet;
 import com.hazelcast.spi.Operation;
+import com.hazelcast.spi.impl.PartitionSpecificRunnable;
 import com.hazelcast.spi.impl.operationexecutor.OperationRunner;
 import com.hazelcast.spi.impl.operationexecutor.OperationRunnerFactory;
 import com.hazelcast.test.AssertTask;
@@ -15,6 +16,7 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -71,6 +73,70 @@ public class OperationThreadTest extends AbstractClassicOperationExecutorTest {
 
         int normalSize = operationThread.normalPendingCount();
         assertEquals(Integer.MAX_VALUE, normalSize);
+    }
+
+    @Test
+    public void executeOperation_withInvalid_partitionId() {
+        final int partitionId = Integer.MAX_VALUE;
+        Operation operation = new DummyPartitionOperation(partitionId);
+        testExecute_withInvalid_partitionId(operation);
+    }
+
+    @Test
+    public void executePartitionSpecificRunnable_withInvalid_partitionId() {
+        final int partitionId = Integer.MAX_VALUE;
+        testExecute_withInvalid_partitionId(new PartitionSpecificRunnable() {
+            @Override
+            public int getPartitionId() {
+                return partitionId;
+            }
+            @Override
+            public void run() {
+            }
+        });
+    }
+
+    @Test
+    public void executePacket_withInvalid_partitionId() {
+        final int partitionId = Integer.MAX_VALUE;
+        Operation operation = new DummyPartitionOperation(partitionId);
+        Packet packet = new Packet(serializationService.toBytes(operation), operation.getPartitionId());
+        packet.setHeader(Packet.HEADER_OP);
+
+        testExecute_withInvalid_partitionId(packet);
+    }
+
+    private void testExecute_withInvalid_partitionId(Object task) {
+        handlerFactory = mock(OperationRunnerFactory.class);
+        OperationRunner handler = mock(OperationRunner.class);
+        when(handlerFactory.createGenericRunner()).thenReturn(handler);
+        when(handlerFactory.createPartitionRunner(anyInt())).thenReturn(handler);
+
+        initExecutor();
+
+        if (task instanceof Operation) {
+            executor.execute((Operation) task);
+        } else if (task instanceof PartitionSpecificRunnable) {
+            executor.execute((PartitionSpecificRunnable) task);
+        } else if (task instanceof Packet) {
+            executor.execute((Packet) task);
+        } else {
+            fail("invalid task!");
+        }
+
+        final Runnable emptyRunnable = new Runnable() {
+            @Override
+            public void run() {
+            }
+        };
+        executor.runOnAllPartitionThreads(emptyRunnable);
+
+        assertTrueEventually(new AssertTask() {
+            @Override
+            public void run() throws Exception {
+                assertEquals(0, executor.getPriorityOperationExecutorQueueSize());
+            }
+        });
     }
 
     private PartitionOperationThread createNewOperationThread(ScheduleQueue mockScheduleQueue) {
