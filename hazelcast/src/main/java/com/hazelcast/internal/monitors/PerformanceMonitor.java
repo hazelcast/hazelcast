8295af1d68b8d0e8fbc728ee271cb862b86de804
alarmnummer <peter.veentjer@hazelcast.com>
2016-03-20 14:36:44 +0200

    Minor javadoc fix in PerformanceMonitor

--- hazelcast/src/main/java/com/hazelcast/internal/monitors/PerformanceMonitor.java
+++ hazelcast/src/main/java/com/hazelcast/internal/monitors/PerformanceMonitor.java
@@ -84,7 +84,7 @@ public class PerformanceMonitor {
     }
 
     /**
-     * Registers a PerformanceMonitorPlugin to it will be scheduled.
+     * Registers a PerformanceMonitorPlugin.
      *
      * This method is threadsafe.
      *
