9d8d99c7543d11305f7df9aa69fd722e2a8f8cff
Ahmet MIRCIK <ahmet@hazelcast.com>
2016-03-30 17:41:10 +0300

    Fixes index creation in wrong map-container problem. Upon subsequent destroy and creation of imap, there is a possibility that there can be more than one map-container referenced by record-stores at the same time. Due to that indexes can be created in an unexpected map-container and this can lead to return less than expected number of results after querying imap

--- hazelcast/src/main/java/com/hazelcast/map/impl/PartitionContainer.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/PartitionContainer.java
@@ -125,7 +125,8 @@ public class PartitionContainer {
         return maps.get(mapName);
     }
 
-    public void destroyMap(String name) {
+    public void destroyMap(MapContainer mapContainer) {
+        String name = mapContainer.getName();
         RecordStore recordStore = maps.remove(name);
         if (recordStore != null) {
             recordStore.destroy();
@@ -136,6 +137,9 @@ public class PartitionContainer {
             // this IMap partition.
             clearLockStore(name);
         }
+
+        MapServiceContext mapServiceContext = mapService.getMapServiceContext();
+        mapServiceContext.removeMapContainer(mapContainer);
     }
 
     private void clearLockStore(String name) {
