702da924db840b4711131b7727c3c4765db16454
Torkel Dominique <torkel@twilio.com>
2016-04-12 22:36:39 +0800

    Port torkeld@0b15cc1 from @torkeld to fix #1156 and #1719
    * If the PartitionIteratingOperation has a
    PartitionWideEntryWithPredicateOperationFactory, try to query the
    indices associated with the map that the factory is scoped for.
    * In the factory itself, if the index service returns keys given a
    predicate query, retain them and issue a MultipleEntryOperation
    containing those keys instead of a
    PartitionWideEntryWithPredicateOperation.

--- hazelcast/src/main/java/com/hazelcast/map/impl/operation/PartitionWideEntryWithPredicateOperationFactory.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/operation/PartitionWideEntryWithPredicateOperationFactory.java
@@ -17,18 +17,26 @@
 package com.hazelcast.map.impl.operation;
 
 import com.hazelcast.map.EntryProcessor;
+import com.hazelcast.map.impl.MapService;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.query.Predicate;
+import com.hazelcast.query.impl.Indexes;
+import com.hazelcast.query.impl.QueryableEntry;
 import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.OperationFactory;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PartitionWideEntryWithPredicateOperationFactory implements OperationFactory {
     private String name;
     private EntryProcessor entryProcessor;
     private Predicate predicate;
+    private boolean hasIndex;
+    private Set<Data> keySet;
 
     public PartitionWideEntryWithPredicateOperationFactory() {
     }
@@ -41,7 +49,23 @@ public class PartitionWideEntryWithPredicateOperationFactory implements Operatio
 
     @Override
     public Operation createOperation() {
+        if (hasIndex) {
+            return new MultipleEntryOperation(name, keySet, entryProcessor);
+        }
         return new PartitionWideEntryWithPredicateOperation(name, entryProcessor, predicate);
+    }
+
+    public void queryIndex(MapService mapService) {
+        Indexes indexService = mapService.getMapServiceContext().getMapContainer(name).getIndexes();
+        Set<QueryableEntry> querySet = indexService.query(predicate);
+        if (querySet != null) {
+            Set<Data> keys = new HashSet<Data>();
+            for (QueryableEntry e: querySet) {
+                keys.add(e.getKeyData());
+            }
+            hasIndex = true;
+            keySet = keys;
+        }
     }
 
     @Override
