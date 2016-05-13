6b804627dad4f5ba55073fc596dc10513d7bbdf2
Tom Bujok <tom@hazelcast.com>
2016-05-13 13:57:02 +0200

    Fixing findbugs issue in PortableUtils

--- hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/PortableUtils.java
+++ hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/PortableUtils.java
@@ -44,9 +44,9 @@ final class PortableUtils {
     static int validateAndGetArrayQuantifierFromCurrentToken(String token, String fullPath) {
         String quantifier = extractArgumentsFromAttributeName(token);
         if (quantifier == null) {
-            throw new IllegalArgumentException("Malformed quantifier " + quantifier + " in " + fullPath);
+            throw new IllegalArgumentException("Malformed quantifier in " + fullPath);
         }
-        int index = Integer.valueOf(quantifier);
+        int index = Integer.parseInt(quantifier);
         if (index < 0) {
             throw new IllegalArgumentException("Array index " + index + " cannot be negative in " + fullPath);
         }
