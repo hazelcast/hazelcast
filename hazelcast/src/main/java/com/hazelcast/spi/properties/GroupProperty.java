93c6312f91808f1582d548af2883b0d1d6e8999c
pveentjer <alarmnummer@gmail.com>
2016-06-10 07:15:54 +0300

    Disabled the hazelcast.application.validation.token
    This token was added for a potential customer some years ago, but I doubt it is being used.
    So lets deprecate is so we can get rid of it in 3.8.
    
    If people complain, we can always remove the deprecation.

--- hazelcast/src/main/java/com/hazelcast/spi/properties/GroupProperty.java
+++ hazelcast/src/main/java/com/hazelcast/spi/properties/GroupProperty.java
@@ -50,6 +50,7 @@ public final class GroupProperty {
      * <p/>
      * This validation-token will be checked before member join the cluster.
      */
+    @Deprecated
     public static final HazelcastProperty APPLICATION_VALIDATION_TOKEN
             = new HazelcastProperty("hazelcast.application.validation.token");
 
