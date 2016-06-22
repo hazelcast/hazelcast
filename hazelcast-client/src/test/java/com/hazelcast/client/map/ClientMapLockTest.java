ea36259d22c518ae70f5eb951123c74351be48b6
Mehmet Dogan <mehmet@dogan.io>
2016-06-22 10:51:53 +0300

    Increase `map.tryPut()` timeout in ClientMapLockTest
    Current timeout is close to lock lease-time and may cause `tryPut()`
    to fail occasionally.
    
    Fixes #8284

--- hazelcast-client/src/test/java/com/hazelcast/client/map/ClientMapLockTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/map/ClientMapLockTest.java
@@ -436,12 +436,12 @@ public class ClientMapLockTest {
         final String newValue = "NEW_VALUE";
 
         map.put(key, oldValue);
-        map.lock(key, 4, TimeUnit.SECONDS);
+        map.lock(key, 1, TimeUnit.SECONDS);
 
         final CountDownLatch tryPutReturned = new CountDownLatch(1);
         new Thread() {
             public void run() {
-                map.tryPut(key, newValue, 8, TimeUnit.SECONDS);
+                map.tryPut(key, newValue, 60, TimeUnit.SECONDS);
                 tryPutReturned.countDown();
             }
         }.start();
@@ -456,12 +456,12 @@ public class ClientMapLockTest {
         final String key = "key";
         final String value = "value";
 
-        map.lock(key, 4, TimeUnit.SECONDS);
+        map.lock(key, 1, TimeUnit.SECONDS);
 
         final CountDownLatch tryPutReturned = new CountDownLatch(1);
         new Thread() {
             public void run() {
-                map.tryPut(key, value, 8, TimeUnit.SECONDS);
+                map.tryPut(key, value, 60, TimeUnit.SECONDS);
                 tryPutReturned.countDown();
             }
         }.start();
