9b0ed8427a81e8a2f5592b1b4665c085032cc4a1
Mahmoud Rahbar Azad <mahmoud.azad@acando.de>
2016-09-11 21:59:36 +0200

    renamed field

--- src/main/java/com/noctarius/hazelcast/kubernetes/KubernetesProperties.java
+++ src/main/java/com/noctarius/hazelcast/kubernetes/KubernetesProperties.java
@@ -78,7 +78,7 @@ public final class KubernetesProperties {
     public static final PropertyDefinition NAMESPACE = property("namespace", STRING);
 
 	public static final PropertyDefinition KUBERNETES_MASTER_URL = property("kubernetes-master", STRING);
-	public static final PropertyDefinition KUBERNETES_API_TOKEN = property("kubernetes-api-token", STRING);
+	public static final PropertyDefinition KUBERNETES_API_TOKEN = property("api-token", STRING);
 
     // Prevent instantiation
     private KubernetesProperties() {
