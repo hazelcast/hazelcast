9e3cad6310cfdff251e657d82cf370233db0303b
Donnerbart <github@thunderphreak.de>
2016-09-20 14:02:38 +0200

    Removed ParallelTest from SecondsBasedEntryTaskSchedulerTest since it contains timing sensitive tests.

--- hazelcast/src/test/java/com/hazelcast/util/scheduler/SecondsBasedEntryTaskSchedulerTest.java
+++ hazelcast/src/test/java/com/hazelcast/util/scheduler/SecondsBasedEntryTaskSchedulerTest.java
@@ -2,7 +2,6 @@ package com.hazelcast.util.scheduler;
 
 import com.hazelcast.spi.TaskScheduler;
 import com.hazelcast.test.HazelcastParallelClassRunner;
-import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,8 +25,11 @@ import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * Has timing sensitive tests, do not make a {@link com.hazelcast.test.annotation.ParallelTest}.
+ */
 @RunWith(HazelcastParallelClassRunner.class)
-@Category({QuickTest.class, ParallelTest.class})
+@Category(QuickTest.class)
 public class SecondsBasedEntryTaskSchedulerTest {
 
     @Mock
