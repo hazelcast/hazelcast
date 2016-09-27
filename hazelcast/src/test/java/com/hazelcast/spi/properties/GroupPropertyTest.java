3e54cc9cf5240077f4c49d777a7d85b6349a8e65
Donnerbart <github@thunderphreak.de>
2016-09-27 20:17:23 +0200

    Added missing annotations to GroupPropertyTest.

--- hazelcast/src/test/java/com/hazelcast/spi/properties/GroupPropertyTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/properties/GroupPropertyTest.java
@@ -1,12 +1,19 @@
 package com.hazelcast.spi.properties;
 
+import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class GroupPropertyTest extends HazelcastTestSupport {
 
     @Test
-    public void testConstructor() throws Exception {
+    public void testConstructor() {
         assertUtilityConstructor(GroupProperty.class);
     }
 }
