98d4473cd7822e2eaac295c78ccf1e1fd139f12d
Donnerbart <github@thunderphreak.de>
2016-09-27 20:18:37 +0200

    Added missing annotations to PartitionGroupMetaDataTest.

--- hazelcast/src/test/java/com/hazelcast/spi/partitiongroup/PartitionGroupMetaDataTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/partitiongroup/PartitionGroupMetaDataTest.java
@@ -1,8 +1,15 @@
 package com.hazelcast.spi.partitiongroup;
 
+import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class PartitionGroupMetaDataTest extends HazelcastTestSupport {
 
     @Test
