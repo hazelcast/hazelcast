ac4eda9c6b5dd67c6fa89e1034480d802fea3eae
Donnerbart <github@thunderphreak.de>
2016-09-27 20:18:06 +0200

    Added missing annotations to WrongTargetExceptionTest.

--- hazelcast/src/test/java/com/hazelcast/spi/exception/WrongTargetExceptionTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/exception/WrongTargetExceptionTest.java
@@ -1,10 +1,17 @@
 package com.hazelcast.spi.exception;
 
 import com.hazelcast.nio.Address;
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class WrongTargetExceptionTest {
 
     @Test
