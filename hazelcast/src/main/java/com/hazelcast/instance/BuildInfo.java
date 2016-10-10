ca61ead3bcb7dcad3a47c0f842d21b44182ec801
ihsan demir <ihsan@hazelcast.com>
2016-10-10 16:37:58 +0300

    Eliminate boxing/unboxing. Fix due to findbugs.

--- hazelcast/src/main/java/com/hazelcast/instance/BuildInfo.java
+++ hazelcast/src/main/java/com/hazelcast/instance/BuildInfo.java
@@ -80,14 +80,14 @@ public class BuildInfo {
         Matcher matcher = VERSION_PATTERN.matcher(version);
         if (matcher.matches()) {
             try {
-                int calculatedVersion = MAJOR_VERSION_MULTIPLIER * Integer.valueOf(matcher.group(1))
-                        + MINOR_VERSION_MULTIPLIER * Integer.valueOf(matcher.group(2));
+                int calculatedVersion = MAJOR_VERSION_MULTIPLIER * Integer.parseInt(matcher.group(1))
+                        + MINOR_VERSION_MULTIPLIER * Integer.parseInt(matcher.group(2));
 
                 int groupCount = matcher.groupCount();
                 if (groupCount >= PATCH_GROUP_COUNT) {
                     String patchVersionString = matcher.group(PATCH_GROUP_COUNT);
                     if (null != patchVersionString && !patchVersionString.startsWith("-")) {
-                        calculatedVersion += Integer.valueOf(patchVersionString);
+                        calculatedVersion += Integer.parseInt(patchVersionString);
                     }
                 }
                 return calculatedVersion;
