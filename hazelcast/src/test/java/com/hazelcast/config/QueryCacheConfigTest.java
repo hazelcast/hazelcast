0a54e38ee1e5011faeda506dd63aef1440d61cc7
Donnerbart <github@thunderphreak.de>
2016-10-11 17:31:06 +0200

    Increased code coverage of QueryCacheConfig

--- hazelcast/src/test/java/com/hazelcast/config/QueryCacheConfigTest.java
+++ hazelcast/src/test/java/com/hazelcast/config/QueryCacheConfigTest.java
@@ -8,49 +8,87 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(HazelcastParallelClassRunner.class)
 @Category({QuickTest.class, ParallelTest.class})
 public class QueryCacheConfigTest extends HazelcastTestSupport {
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSetName_throwsException_whenNameNull() throws Exception {
+    public void testSetName_throwsException_whenNameNull() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setName(null);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testSetPredicate_throwsException_whenPredicateNull() throws Exception {
+    public void testSetPredicate_throwsException_whenPredicateNull() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setPredicateConfig(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSetBatchSize_throwsException_whenNotPositive() throws Exception {
+    public void testSetBatchSize_throwsException_whenNotPositive() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setBatchSize(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSetBufferSize_throwsException_whenNotPositive() throws Exception {
+    public void testSetBufferSize_throwsException_whenNotPositive() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setBufferSize(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSetDelaySeconds_throwsException_whenNegative() throws Exception {
+    public void testSetDelaySeconds_throwsException_whenNegative() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setDelaySeconds(-1);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testSetInMemoryFormat_throwsException_whenNull() throws Exception {
+    public void testSetInMemoryFormat_throwsException_whenNull() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setInMemoryFormat(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSetInMemoryFormat_throwsException_whenNative() throws Exception {
+    public void testSetInMemoryFormat_throwsException_whenNative() {
         QueryCacheConfig config = new QueryCacheConfig();
         config.setInMemoryFormat(InMemoryFormat.NATIVE);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetEvictionConfig_throwsException_whenNull() {
+        QueryCacheConfig config = new QueryCacheConfig();
+        config.setEvictionConfig(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddEntryListenerConfig_throwsException_whenNull() {
+        QueryCacheConfig config = new QueryCacheConfig();
+        config.addEntryListenerConfig(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetEntryListenerConfigs_throwsException_whenNull() {
+        QueryCacheConfig config = new QueryCacheConfig();
+        config.setEntryListenerConfigs(null);
+    }
+
+    @Test
+    public void testSetIndexConfigs_withNull() {
+        QueryCacheConfig config = new QueryCacheConfig();
+        config.setIndexConfigs(null);
+
+        assertNotNull(config.getIndexConfigs());
+        assertTrue(config.getIndexConfigs().isEmpty());
+    }
+
+    @Test
+    public void testToString() {
+        QueryCacheConfig config = new QueryCacheConfig();
+
+        assertNotNull(config.toString());
+        assertTrue(config.toString().contains("QueryCacheConfig"));
     }
 }
