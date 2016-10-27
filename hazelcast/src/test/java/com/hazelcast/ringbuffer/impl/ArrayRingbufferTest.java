a4c304bfad4a5cc832ba1fa6d691014a4f9f16cc
Matko Medenjak <matko@hazelcast.com>
2016-10-27 15:06:06 +0200

    increased code coverage for arrayringbuffer

--- hazelcast/src/test/java/com/hazelcast/ringbuffer/impl/ArrayRingbufferTest.java
+++ hazelcast/src/test/java/com/hazelcast/ringbuffer/impl/ArrayRingbufferTest.java
@@ -15,21 +15,32 @@ import static org.junit.Assert.assertTrue;
 @Category({QuickTest.class, ParallelTest.class})
 public class ArrayRingbufferTest {
     @Test(expected = StaleSequenceException.class)
-    public void testStaleSequenceThrowsException() {
-        final ArrayRingbuffer rb = new ArrayRingbuffer(5);
-        for (int i = 0; i < rb.getCapacity(); i++) {
-            rb.add("");
-        }
+    public void testReadStaleSequenceThrowsException() {
+        final ArrayRingbuffer rb = fullRingbuffer();
         rb.read(rb.headSequence() - 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testFutureSequenceThrowsException() {
-        final ArrayRingbuffer rb = new ArrayRingbuffer(5);
-        for (int i = 0; i < rb.getCapacity(); i++) {
-            rb.add("");
-        }
+    public void testReadFutureSequenceThrowsException() {
+        final ArrayRingbuffer rb = fullRingbuffer();
         rb.read(rb.tailSequence() + 1);
+    }
+
+    @Test(expected = StaleSequenceException.class)
+    public void testBlockableReadStaleSequenceThrowsException() {
+        final ArrayRingbuffer rb = fullRingbuffer();
+        rb.checkBlockableReadSequence(rb.headSequence() - 1);
+    }
+
+    public void testBlockableReadFutureSequenceOk() {
+        final ArrayRingbuffer rb = fullRingbuffer();
+        rb.checkBlockableReadSequence(rb.tailSequence() + 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBlockableReadFutureSequenceThrowsException() {
+        final ArrayRingbuffer rb = fullRingbuffer();
+        rb.checkBlockableReadSequence(rb.tailSequence() + 2);
     }
 
     @Test
@@ -38,5 +49,13 @@ public class ArrayRingbufferTest {
         assertTrue(rb.isEmpty());
         rb.add("");
         assertFalse(rb.isEmpty());
+    }
+
+    private static ArrayRingbuffer fullRingbuffer() {
+        final ArrayRingbuffer rb = new ArrayRingbuffer(5);
+        for (int i = 0; i < rb.getCapacity(); i++) {
+            rb.add("");
+        }
+        return rb;
     }
 }
