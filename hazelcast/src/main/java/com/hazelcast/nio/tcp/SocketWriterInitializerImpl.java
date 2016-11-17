b222b667eb286ceab79bbbca6c90639d5c4794b2
ihsan demir <ihsan@hazelcast.com>
2016-11-17 15:42:22 +0200

    The client protocol is incorrectly printed as Cluster protocol. The log is: (#9266)
    SocketWriter is not set, creating WriteHandler with CLUSTER protocol
    
     It is also printed as warning but it may not be a warning, it may just as well be normal case as info to the user. The PR fixes this problem.

--- hazelcast/src/main/java/com/hazelcast/nio/tcp/SocketWriterInitializerImpl.java
+++ hazelcast/src/main/java/com/hazelcast/nio/tcp/SocketWriterInitializerImpl.java
@@ -21,11 +21,11 @@ import com.hazelcast.internal.networking.SocketWriterInitializer;
 import com.hazelcast.internal.networking.WriteHandler;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.nio.IOService;
+import com.hazelcast.nio.Protocols;
 import com.hazelcast.nio.ascii.TextWriteHandler;
 
 import java.net.SocketException;
 import java.nio.ByteBuffer;
-import java.util.logging.Level;
 
 import static com.hazelcast.nio.IOService.KILO_BYTE;
 import static com.hazelcast.nio.IOUtil.newByteBuffer;
@@ -43,7 +43,7 @@ public class SocketWriterInitializerImpl implements SocketWriterInitializer<TcpI
 
     @Override
     public void init(TcpIpConnection connection, SocketWriter writer, String protocol) {
-        logger.log(Level.WARNING, "SocketWriter is not set, creating WriteHandler with CLUSTER protocol!");
+        logger.info("Initializing SocketWriter WriteHandler with " + Protocols.toUserFriendlyString(protocol));
 
         initHandler(connection, writer, protocol);
         initOutputBuffer(connection, writer, protocol);
