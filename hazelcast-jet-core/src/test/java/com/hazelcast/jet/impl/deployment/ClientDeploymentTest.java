bcd0ef53337be34f5c6436d15b7af7fb156962fa
Marko Topolnik <marko@hazelcast.com>
2016-12-15 11:49:48 +0100

    Add Timeout rule to ClientDeploymentTest
    Build has been getting stuck due to hanging within @After

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/deployment/ClientDeploymentTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/impl/deployment/ClientDeploymentTest.java
@@ -6,12 +6,17 @@ import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
+import org.junit.Rule;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 
 @Category(QuickTest.class)
 @RunWith(HazelcastParallelClassRunner.class)
 public class ClientDeploymentTest extends AbstractDeploymentTest {
+
+    @Rule
+    public final Timeout timeoutRule = Timeout.seconds(360);
 
     private TestHazelcastFactory factory = new TestHazelcastFactory();
 
