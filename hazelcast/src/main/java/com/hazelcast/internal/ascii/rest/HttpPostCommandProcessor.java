f5fde4ac4d7cfc6ddc8b9c28de07714b52ab9d2f
Matko Medenjak <matko@hazelcast.com>
2016-12-20 11:40:07 +0100

    rest test refactoring and increase code coverage

--- hazelcast/src/main/java/com/hazelcast/internal/ascii/rest/HttpPostCommandProcessor.java
+++ hazelcast/src/main/java/com/hazelcast/internal/ascii/rest/HttpPostCommandProcessor.java
@@ -76,10 +76,10 @@ public class HttpPostCommandProcessor extends HttpCommandProcessor<HttpPostComma
                 return;
             } else if (uri.startsWith(URI_FORCESTART_CLUSTER_URL)) {
                 handleForceStart(command);
-            } else if (uri.startsWith(URI_HOT_RESTART_BACKUP_CLUSTER_URL)) {
-                handleHotRestartBackup(command);
             } else if (uri.startsWith(URI_HOT_RESTART_BACKUP_INTERRUPT_CLUSTER_URL)) {
                 handleHotRestartBackupInterrupt(command);
+            } else if (uri.startsWith(URI_HOT_RESTART_BACKUP_CLUSTER_URL)) {
+                handleHotRestartBackup(command);
             } else if (uri.startsWith(URI_PARTIALSTART_CLUSTER_URL)) {
                 handlePartialStart(command);
             } else if (uri.startsWith(URI_CLUSTER_NODES_URL)) {
