hazelcast/hazelcast

commit 080297503f32e20623e65120625e09ac7f9a1f96

Hazelcast listeners are lost during cluster disconnects (#8848)
* Fixes the client listener registration if the client owner connection is lost and reconnected (e.g. due to heartbeat or network problems).

Authentication response returns a list of members at which the client is unregistered so that the listener service acts upon this list. The smart listener service re-registers the listeners to these unregistered members. The non-smart listener service re-registers the listeners only if the reconnected member is not the same as the previous member or the previous member client resources were cleaned up.

The ClientDisconnectionOperation also acts smart so that it only cleans client resources if the dead member uuid is the current owner member uuid of the client and there is no alive connection endpoint to the client.

* Forward port of fixes at https://github.com/hazelcast/hazelcast/pull/9183

hazelcast/src/main/java/com/hazelcast/client/impl/ClientEndpointManagerImpl.java
@@ -55,6 +55,8 @@ public class ClientEndpointManagerImpl implements ClientEndpointManager {
     @Probe(name = "count", level = MANDATORY)
     private final ConcurrentMap<Connection, ClientEndpoint> endpoints =
             new ConcurrentHashMap<Connection, ClientEndpoint>();
+    private final ConcurrentMap<String, ClientEndpoint> clientEndpoints =
+            new ConcurrentHashMap<String, ClientEndpoint>();
 
     @Probe(name = "totalRegistrations", level = MANDATORY)
     private MwCounter totalRegistrations = newMwCounter();
@@ -89,14 +91,27 @@ public class ClientEndpointManagerImpl implements ClientEndpointManager {
     }
 
     @Override
+    public ClientEndpoint getEndpoint(String clientUuid) {
+        return clientEndpoints.get(clientUuid);
+    }
+
+    @Override
     public void registerEndpoint(ClientEndpoint endpoint) {
         checkNotNull(endpoint, "endpoint can't be null");
 
         final Connection conn = endpoint.getConnection();
-        if (endpoints.putIfAbsent(conn, endpoint) != null) {
-            logger.severe("An endpoint already exists for connection:" + conn);
+        ClientEndpoint existingEndpoint = endpoints.put(conn, endpoint);
+        clientEndpoints.put(endpoint.getUuid(), endpoint);
+        if (existingEndpoint != null && endpoint != existingEndpoint) {
+            if (existingEndpoint.isFirstConnection()) {
+                logger.severe("An endpoint (first connection) already exists for connection:" + conn);
+            } else {
+                logger.info("Changed " + conn + " as the first connection for " + endpoint);
+            }
         } else {
-            totalRegistrations.inc();
+            if (endpoint != existingEndpoint) {
+                totalRegistrations.inc();
+            }
         }
     }
 
@@ -112,6 +127,7 @@ public class ClientEndpointManagerImpl implements ClientEndpointManager {
         ClientEndpointImpl endpoint = (ClientEndpointImpl) ce;
 
         endpoints.remove(endpoint.getConnection());
+        clientEndpoints.remove(endpoint.getUuid());
         logger.info("Destroying " + endpoint);
         try {
             endpoint.destroy();

hazelcast/src/main/java/com/hazelcast/client/impl/ClientEngineImpl.java
@@ -98,7 +98,8 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
      */
     public static final String SERVICE_NAME = "hz:core:clientEngine";
 
-    private static final int ENDPOINT_REMOVE_DELAY_SECONDS = 10;
+    public static final int ENDPOINT_REMOVE_DELAY_SECONDS = 10;
+
     private static final int EXECUTOR_QUEUE_CAPACITY_PER_CORE = 100000;
     private static final int THREADS_PER_CORE = 20;
 
@@ -296,7 +297,6 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
         try {
             nodeEngine.getExecutionService().schedule(new DestroyEndpointTask(deadMemberUuid),
                     ENDPOINT_REMOVE_DELAY_SECONDS, TimeUnit.SECONDS);
-
         } catch (RejectedExecutionException e) {
             if (logger.isFinestEnabled()) {
                 logger.finest(e);
@@ -311,7 +311,7 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
     public Collection<Client> getClients() {
         final HashSet<Client> clients = new HashSet<Client>();
         for (ClientEndpoint endpoint : endpointManager.getEndpoints()) {
-            clients.add((Client) endpoint);
+            clients.add(endpoint);
         }
         return clients;
     }
@@ -347,12 +347,12 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
         ownershipMappings.clear();
     }
 
-    public void addOwnershipMapping(String clientUuid, String ownerUuid) {
-        ownershipMappings.put(clientUuid, ownerUuid);
+    public String addOwnershipMapping(String clientUuid, String ownerUuid) {
+        return ownershipMappings.put(clientUuid, ownerUuid);
     }
 
-    public void removeOwnershipMapping(String clientUuid) {
-        ownershipMappings.remove(clientUuid);
+    public boolean removeOwnershipMapping(String clientUuid, String memberUuid) {
+        return ownershipMappings.remove(clientUuid, memberUuid);
     }
 
     public TransactionManagerService getTransactionManagerService() {
@@ -371,19 +371,32 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
         @Override
         public void connectionRemoved(Connection connection) {
             if (connection.isClient() && nodeEngine.isRunning()) {
-                ClientEndpointImpl endpoint = (ClientEndpointImpl) endpointManager.getEndpoint(connection);
+                final ClientEndpointImpl endpoint = (ClientEndpointImpl) endpointManager.getEndpoint(connection);
                 if (endpoint == null) {
+                    logger.finest("connectionRemoved: No endpoint for connection:" + connection);
                     return;
                 }
 
                 if (!endpoint.isFirstConnection()) {
+                    logger.finest("connectionRemoved: Not the owner conn:" + connection + " for endpoint " + endpoint);
                     return;
                 }
 
                 String localMemberUuid = node.getThisUuid();
                 String ownerUuid = endpoint.getPrincipal().getOwnerUuid();
                 if (localMemberUuid.equals(ownerUuid)) {
-                    callDisconnectionOperation(endpoint);
+                    try {
+                        nodeEngine.getExecutionService().schedule(new Runnable() {
+                            @Override
+                            public void run() {
+                                callDisconnectionOperation(endpoint);
+                            }
+                        }, ENDPOINT_REMOVE_DELAY_SECONDS, TimeUnit.SECONDS);
+                    } catch (RejectedExecutionException e) {
+                        if (logger.isFinestEnabled()) {
+                            logger.finest(e);
+                        }
+                    }
                 }
             }
         }
@@ -391,20 +404,29 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
         private void callDisconnectionOperation(ClientEndpointImpl endpoint) {
             Collection<Member> memberList = nodeEngine.getClusterService().getMembers();
             OperationService operationService = nodeEngine.getOperationService();
-            ClientDisconnectionOperation op = createClientDisconnectionOperation(endpoint.getUuid());
+            String memberUuid = getLocalMember().getUuid();
+            String clientUuid = endpoint.getUuid();
+
+            String ownerMember = ownershipMappings.get(clientUuid);
+            if (!memberUuid.equals(ownerMember)) {
+                // do nothing if the owner already changed (double checked locking)
+                return;
+            }
+
+            ClientDisconnectionOperation op = createClientDisconnectionOperation(clientUuid, memberUuid);
             operationService.run(op);
 
             for (Member member : memberList) {
                 if (!member.localMember()) {
-                    op = createClientDisconnectionOperation(endpoint.getUuid());
+                    op = createClientDisconnectionOperation(clientUuid, memberUuid);
                     operationService.send(op, member.getAddress());
                 }
             }
         }
     }
 
-    private ClientDisconnectionOperation createClientDisconnectionOperation(String clientUuid) {
-        ClientDisconnectionOperation op = new ClientDisconnectionOperation(clientUuid);
+    private ClientDisconnectionOperation createClientDisconnectionOperation(String clientUuid, String memberUuid) {
+        ClientDisconnectionOperation op = new ClientDisconnectionOperation(clientUuid, memberUuid);
         op.setNodeEngine(nodeEngine)
                 .setServiceName(SERVICE_NAME)
                 .setService(this)
@@ -433,7 +455,7 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
                 String memberUuid = entry.getValue();
                 if (deadMemberUuid.equals(memberUuid)) {
                     iterator.remove();
-                    ClientDisconnectionOperation op = createClientDisconnectionOperation(clientUuid);
+                    ClientDisconnectionOperation op = createClientDisconnectionOperation(clientUuid, deadMemberUuid);
                     nodeEngine.getOperationService().run(op);
                 }
             }

hazelcast-client/src/main/java/com/hazelcast/client/connection/nio/ClientConnection.java
@@ -19,6 +19,7 @@ package com.hazelcast.client.connection.nio;
 import com.hazelcast.client.connection.ClientConnectionManager;
 import com.hazelcast.client.impl.HazelcastClientInstanceImpl;
 import com.hazelcast.core.LifecycleService;
+import com.hazelcast.core.Member;
 import com.hazelcast.instance.BuildInfo;
 import com.hazelcast.internal.metrics.DiscardableMetricsProvider;
 import com.hazelcast.internal.metrics.MetricsRegistry;
@@ -43,6 +44,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -81,6 +83,13 @@ public class ClientConnection implements SocketConnection, DiscardableMetricsPro
     private volatile String closeReason;
     private int connectedServerVersion = BuildInfo.UNKNOWN_HAZELCAST_VERSION;
     private String connectedServerVersionString;
+
+    /**
+     * The list of members from which the client resources are deleted at the time of connection authentication. The client may
+     * use this list to re-register the client resources such the listeners to these members.
+     */
+    private List<Member> clientUnregisteredMembers;
+
 
     public ClientConnection(HazelcastClientInstanceImpl client,
                             IOThreadingModel ioThreadingModel,
@@ -387,5 +396,13 @@ public class ClientConnection implements SocketConnection, DiscardableMetricsPro
 
     public String getConnectedServerVersionString() {
         return connectedServerVersionString;
+    }
+
+    public List<Member> getClientUnregisteredMembers() {
+        return clientUnregisteredMembers;
+    }
+
+    public void setClientUnregisteredMembers(List<Member> clientUnregisteredMembers) {
+        this.clientUnregisteredMembers = clientUnregisteredMembers;
     }
 }

hazelcast-client/src/main/java/com/hazelcast/client/connection/nio/ClientConnectionManagerImpl.java
@@ -403,6 +403,10 @@ public class ClientConnectionManagerImpl implements ClientConnectionManager {
             }
             final long now = Clock.currentTimeMillis();
             for (final ClientConnection connection : connections.values()) {
+                if (!connection.isAlive()) {
+                    continue;
+                }
+
                 if (now - connection.lastReadTimeMillis() > heartbeatTimeout) {
                     if (connection.isHeartBeating()) {
                         logger.warning("Heartbeat failed to connection : " + connection);
@@ -412,18 +416,22 @@ public class ClientConnectionManagerImpl implements ClientConnectionManager {
                 }
                 if (now - connection.lastReadTimeMillis() > heartbeatInterval) {
                     ClientMessage request = ClientPingCodec.encodeRequest();
-                    ClientInvocation clientInvocation = new ClientInvocation(client, request, connection);
+                    final ClientInvocation clientInvocation = new ClientInvocation(client, request, connection);
                     clientInvocation.setBypassHeartbeatCheck(true);
                     connection.onHeartbeatRequested();
                     clientInvocation.invokeUrgent().andThen(new ExecutionCallback<ClientMessage>() {
                         @Override
                         public void onResponse(ClientMessage response) {
-                            connection.onHeartbeatReceived();
+                            if (connection.isAlive()) {
+                                connection.onHeartbeatReceived();
+                            }
                         }
 
                         @Override
                         public void onFailure(Throwable t) {
-                            logger.warning("Error receiving heartbeat for connection: " + connection, t);
+                            if (connection.isAlive()) {
+                                logger.warning("Error receiving heartbeat for connection: " + connection, t);
+                            }
                         }
                     });
                 } else {
@@ -475,17 +483,7 @@ public class ClientConnectionManagerImpl implements ClientConnectionManager {
             ownerUuid = principal.getOwnerUuid();
         }
 
-        ClientMessage clientMessage;
-        if (credentials.getClass().equals(UsernamePasswordCredentials.class)) {
-            UsernamePasswordCredentials cr = (UsernamePasswordCredentials) credentials;
-            clientMessage = ClientAuthenticationCodec
-                    .encodeRequest(cr.getUsername(), cr.getPassword(), uuid, ownerUuid, asOwner, ClientTypes.JAVA,
-                            serializationVersion, BuildInfoProvider.getBuildInfo().getVersion());
-        } else {
-            Data data = ss.toData(credentials);
-            clientMessage = ClientAuthenticationCustomCodec.encodeRequest(data, uuid, ownerUuid,
-                    asOwner, ClientTypes.JAVA, serializationVersion, BuildInfoProvider.getBuildInfo().getVersion());
-        }
+        ClientMessage clientMessage = encodeAuthenticationRequest(asOwner, ss, serializationVersion, uuid, ownerUuid);
         ClientInvocation clientInvocation = new ClientInvocation(client, clientMessage, connection);
         ClientInvocationFuture future = clientInvocation.invokeUrgent();
         future.andThen(new ExecutionCallback<ClientMessage>() {
@@ -501,6 +499,7 @@ public class ClientConnectionManagerImpl implements ClientConnectionManager {
                             clusterService.setPrincipal(new ClientPrincipal(result.uuid, result.ownerUuid));
                         }
                         connection.setConnectedServerVersion(result.serverHazelcastVersion);
+                        connection.setClientUnregisteredMembers(result.clientUnregisteredMembers);
                         authenticated(target, connection);
                         callback.onSuccess(connection, asOwner);
                         break;
@@ -524,6 +523,22 @@ public class ClientConnectionManagerImpl implements ClientConnectionManager {
                 callback.onFailure(t);
             }
         }, executionService.getInternalExecutor());
+    }
+
+    private ClientMessage encodeAuthenticationRequest(boolean asOwner, SerializationService ss, byte serializationVersion,
+                                                      String uuid, String ownerUuid) {
+        ClientMessage clientMessage;
+        if (credentials.getClass().equals(UsernamePasswordCredentials.class)) {
+            UsernamePasswordCredentials cr = (UsernamePasswordCredentials) credentials;
+            clientMessage = ClientAuthenticationCodec
+                    .encodeRequest(cr.getUsername(), cr.getPassword(), uuid, ownerUuid, asOwner, ClientTypes.JAVA,
+                            serializationVersion, BuildInfoProvider.getBuildInfo().getVersion());
+        } else {
+            Data data = ss.toData(credentials);
+            clientMessage = ClientAuthenticationCustomCodec.encodeRequest(data, uuid, ownerUuid,
+                    asOwner, ClientTypes.JAVA, serializationVersion, BuildInfoProvider.getBuildInfo().getVersion());
+        }
+        return clientMessage;
     }
 
     private class InitConnectionTask implements Runnable {

hazelcast-client/src/main/java/com/hazelcast/client/spi/ClientListenerService.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.client.spi;
 
+import com.hazelcast.client.connection.nio.ClientConnection;
 import com.hazelcast.client.spi.impl.ListenerMessageCodec;
 
 /**
@@ -29,4 +30,10 @@ public interface ClientListenerService {
     String registerListener(ListenerMessageCodec listenerMessageCodec, EventHandler handler);
 
     boolean deregisterListener(String registrationId);
+
+    /**
+     * Do the appropriate action based on connection to the cluster
+     * @param clientConnection The connection that the client is using for cluster connection.
+     */
+    void onClusterConnect(ClientConnection clientConnection);
 }

hazelcast-client/src/main/java/com/hazelcast/client/spi/impl/ClusterListenerSupport.java
@@ -187,20 +187,25 @@ public abstract class ClusterListenerSupport implements ConnectionListener, Conn
                 }
                 break;
             }
+            Connection connection = null;
             try {
                 triedAddresses.add(inetSocketAddress);
                 Address address = new Address(inetSocketAddress);
                 if (logger.isFinestEnabled()) {
                     logger.finest("Trying to connect to " + address);
                 }
-                Connection connection = connectionManager.getOrConnect(address, true);
+                connection = connectionManager.getOrConnect(address, true);
                 ownerConnectionAddress = connection.getEndPoint();
                 clientMembershipListener.listenMembershipEvents(ownerConnectionAddress);
+                client.getListenerService().onClusterConnect((ClientConnection) connection);
                 fireConnectionEvent(LifecycleEvent.LifecycleState.CLIENT_CONNECTED);
                 return true;
             } catch (Exception e) {
                 Level level = e instanceof AuthenticationException ? Level.WARNING : Level.FINEST;
                 logger.log(level, "Exception during initial connection to " + inetSocketAddress, e);
+                if (null != connection) {
+                    connection.close("Could not connect to " + inetSocketAddress + " as owner", e);
+                }
             }
         }
         return false;

hazelcast-client/src/main/java/com/hazelcast/client/spi/impl/listener/ClientEventRegistration.java
@@ -17,7 +17,7 @@
 package com.hazelcast.client.spi.impl.listener;
 
 import com.hazelcast.client.spi.impl.ListenerMessageCodec;
-import com.hazelcast.nio.Address;
+import com.hazelcast.core.Member;
 
 import static com.hazelcast.util.Preconditions.isNotNull;
 
@@ -26,13 +26,13 @@ import static com.hazelcast.util.Preconditions.isNotNull;
  */
 public class ClientEventRegistration {
 
-    private Address subscriber;
+    private Member subscriber;
     private final String serverRegistrationId;
     private final long callId;
     private final ListenerMessageCodec codec;
 
     public ClientEventRegistration(String serverRegistrationId,
-                                   long callId, Address subscriber, ListenerMessageCodec codec) {
+                                   long callId, Member subscriber, ListenerMessageCodec codec) {
         isNotNull(serverRegistrationId, "serverRegistrationId");
         this.serverRegistrationId = serverRegistrationId;
         this.callId = callId;
@@ -59,7 +59,7 @@ public class ClientEventRegistration {
      *
      * @return subscriber
      */
-    public Address getSubscriber() {
+    public Member getSubscriber() {
         return subscriber;
     }
 

hazelcast-client/src/main/java/com/hazelcast/client/spi/impl/listener/ClientListenerServiceImpl.java
@@ -114,10 +114,6 @@ public abstract class ClientListenerServiceImpl implements ClientListenerService
         ClientExecutionServiceImpl.shutdownExecutor("registrationExecutor", registrationExecutor, logger);
     }
 
-    public StripedExecutor getEventExecutor() {
-        return eventExecutor;
-    }
-
     public void start() {
     }
 

commit 572d542bccd1e201b3a498580baa80dc3f7a0ea5

Fixes regression caused by #8848
Since the logic in ClientDisconnectionOperation change so that
cleanup can only done by that operation, cleanups made elsewhere
should have been removed but forgotten.

fixes #6991

hazelcast/src/main/java/com/hazelcast/client/impl/ClientEndpointManagerImpl.java
@@ -31,7 +31,6 @@ import com.hazelcast.spi.impl.NodeEngineImpl;
 import javax.security.auth.login.LoginException;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -144,18 +143,6 @@ public class ClientEndpointManagerImpl implements ClientEndpointManager {
                 endpoint.getSocketAddress(),
                 endpoint.getClientType());
         clientEngine.sendClientEvent(event);
-    }
-
-    public void removeEndpoints(String memberUuid) {
-        Iterator<ClientEndpoint> iterator = endpoints.values().iterator();
-        while (iterator.hasNext()) {
-            ClientEndpoint endpoint = iterator.next();
-            String ownerUuid = endpoint.getPrincipal().getOwnerUuid();
-            if (memberUuid.equals(ownerUuid)) {
-                iterator.remove();
-                removeEndpoint(endpoint, true, "Cleanup of disconnected client resources");
-            }
-        }
     }
 
     @Override

hazelcast/src/main/java/com/hazelcast/client/impl/ClientEngineImpl.java
@@ -75,7 +75,6 @@ import javax.security.auth.login.LoginException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
@@ -442,24 +441,16 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PostJoinAwar
     private class DestroyEndpointTask implements Runnable {
         private final String deadMemberUuid;
 
-        public DestroyEndpointTask(String deadMemberUuid) {
+        DestroyEndpointTask(String deadMemberUuid) {
             this.deadMemberUuid = deadMemberUuid;
         }
 
         @Override
         public void run() {
-            endpointManager.removeEndpoints(deadMemberUuid);
-            removeMappings();
-        }
-
-        void removeMappings() {
-            Iterator<Map.Entry<String, String>> iterator = ownershipMappings.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<String, String> entry = iterator.next();
+            for (Map.Entry<String, String> entry : ownershipMappings.entrySet()) {
                 String clientUuid = entry.getKey();
                 String memberUuid = entry.getValue();
                 if (deadMemberUuid.equals(memberUuid)) {
-                    iterator.remove();
                     ClientDisconnectionOperation op = createClientDisconnectionOperation(clientUuid, deadMemberUuid);
                     nodeEngine.getOperationService().run(op);
                 }