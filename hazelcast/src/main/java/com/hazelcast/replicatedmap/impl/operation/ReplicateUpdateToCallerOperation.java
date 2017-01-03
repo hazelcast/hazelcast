fbba54773fb822ef148e3fa131e81b31d4316fa5
Ensar Basri Kahveci <ebkahveci@gmail.com>
2017-01-03 10:36:17 +0300

    Clean up replicated map logs
    Backport of #9554

--- hazelcast/src/main/java/com/hazelcast/replicatedmap/impl/operation/ReplicateUpdateToCallerOperation.java
+++ hazelcast/src/main/java/com/hazelcast/replicatedmap/impl/operation/ReplicateUpdateToCallerOperation.java
@@ -68,9 +68,11 @@ public class ReplicateUpdateToCallerOperation extends AbstractOperation implemen
         long currentVersion = store.getVersion();
         long updateVersion = response.getVersion();
         if (currentVersion >= updateVersion) {
-            logger.finest("Stale update received for replicated map -> " + name + ",  partitionId -> "
-                    + getPartitionId() + " , current version -> " + currentVersion + ", update version -> "
-                    + updateVersion + ", rejecting update!");
+            if (logger.isFineEnabled()) {
+                logger.fine("Rejecting stale update received for replicated map: " + name + "  partitionId="
+                        + getPartitionId() + " current version: " + currentVersion + " update version: " + updateVersion);
+            }
+
             return;
         }
         Object key = store.marshall(dataKey);
