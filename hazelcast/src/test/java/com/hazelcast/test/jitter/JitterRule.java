c9ad7725addcec7cc895437ae0eb62be1cd338c6
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-01-04 13:39:09 +0200

    JitterRule is auto-enabled when running on Jenkins
    It can be explicitly disabled/disabled via
    the `hazelcast.jitterMonitor.mode` property.

--- hazelcast/src/test/java/com/hazelcast/test/jitter/JitterRule.java
+++ hazelcast/src/test/java/com/hazelcast/test/jitter/JitterRule.java
@@ -1,15 +1,11 @@
 package com.hazelcast.test.jitter;
 
-import com.hazelcast.util.StringUtil;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.List;
 
 import static com.hazelcast.util.QuickMath.nextPowerOfTwo;
 import static com.hazelcast.util.StringUtil.LINE_SEPARATOR;
@@ -17,14 +13,13 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
- * JUnit rule for detecting JVM/OS hiccups. It's meant to give you some environmental insight
- * in the case test failure.
+ * JUnit rule for detecting JVM/OS hiccups. It's meant to give you an insight into your environment
+ * in the case of a test failure. This is useful for troubleshooting of spuriously failing tests.
  */
 public class JitterRule implements TestRule {
-
     /**
-     * Time interval aggregated into a single bucket. Smaller interval allow
-     * a better picture about hiccups in time, too small intervals use too
+     * Time interval aggregated into a single bucket. Smaller interval provides
+     * a clearer picture about hiccups in time, too small intervals may use too
      * much of memory and can also generate overwhelming amount of data.
      */
     public static final long AGGREGATION_INTERVAL_MILLIS = SECONDS.toMillis(5);
@@ -46,19 +41,43 @@ public class JitterRule implements TestRule {
     public static final long RESOLUTION_NANOS = MILLISECONDS.toNanos(10);
 
     /**
-     * Hiccups over this threshold will counted separately. This is useful for counting
+     * Hiccups over this threshold will be counted separately. This is useful for counting
      * serious hiccups.
      *
      */
     public static final long LONG_HICCUP_THRESHOLD = SECONDS.toNanos(1);
 
+    private static final String MODE_PROPERTY_NAME = "hazelcast.jitterMonitor.mode";
+    private static final Mode DEFAULT_MODE = Mode.JENKINS;
+    private static final boolean ENABLED = isEnabled();
 
     static {
-        JitterMonitor.ensureRunning();
+        if (ENABLED) {
+            JitterMonitor.ensureRunning();
+        }
+    }
+
+    private static boolean isEnabled() {
+        String modePropertyValue = System.getProperty(MODE_PROPERTY_NAME, DEFAULT_MODE.name());
+        Mode mode = Mode.valueOf(modePropertyValue);
+        switch (mode) {
+            case DISABLED:
+                return false;
+            case ENABLED:
+                return true;
+            case JENKINS:
+                return JenkinsDetector.isOnJenkins();
+            default:
+                throw new IllegalArgumentException("Unknown mode: " + mode);
+        }
     }
 
     @Override
     public Statement apply(final Statement base, final Description description) {
+        if (!ENABLED) {
+            return base;
+        }
+
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
