a756afd60beecb08c3949ef6f77345b2bb480230
Mehmet Dogan <mehmet@dogan.io>
2017-01-04 10:57:54 +0300

    Add missing @Category to ProxySplitBrainTest
    See #8752

--- hazelcast/src/test/java/com/hazelcast/core/ProxySplitBrainTest.java
+++ hazelcast/src/test/java/com/hazelcast/core/ProxySplitBrainTest.java
@@ -1,10 +1,17 @@
 package com.hazelcast.core;
 
 import com.hazelcast.test.AssertTask;
+import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.SplitBrainTestSupport;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class ProxySplitBrainTest extends SplitBrainTestSupport {
 
     @Override
