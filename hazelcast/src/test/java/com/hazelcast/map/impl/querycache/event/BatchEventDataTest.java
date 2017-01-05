46bb534204dfb6727a2202933a1fb6c6cf0e7255
Donnerbart <github@thunderphreak.de>
2017-01-05 12:03:25 +0100

    Added missing annotations to BatchEventDataTest.

--- hazelcast/src/test/java/com/hazelcast/map/impl/querycache/event/BatchEventDataTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/querycache/event/BatchEventDataTest.java
@@ -1,7 +1,12 @@
 package com.hazelcast.map.impl.querycache.event;
 
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,6 +18,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class BatchEventDataTest {
 
     private QueryCacheEventData eventData;
