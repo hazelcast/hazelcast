adc024e081d86a2d3bf5a09f56293ce68ce76725
Donnerbart <github@thunderphreak.de>
2017-01-12 20:56:25 +0100

    Added forced GC to AbstractClockTest to counterfeit PermGen OOME.

--- hazelcast/src/test/java/com/hazelcast/util/AbstractClockTest.java
+++ hazelcast/src/test/java/com/hazelcast/util/AbstractClockTest.java
@@ -82,6 +82,7 @@ public abstract class AbstractClockTest extends HazelcastTestSupport {
             Method method = instanceClass.getMethod("shutdown");
             method.invoke(isolatedNode);
             isolatedNode = null;
+            System.gc();
         } catch (Exception e) {
             throw new RuntimeException("Could not start shutdown Hazelcast instance", e);
         }
