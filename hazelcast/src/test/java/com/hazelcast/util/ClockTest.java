1c947dfb48bcf79cc0f95c515ee8dd0c6271acfe
Donnerbart <github@thunderphreak.de>
2017-01-16 11:05:47 +0100

    Split ClockTest into a QuickTest and SlowTest
    ClockTest seems to crash our CI builds quite often. The test method
    test_whenConfiguringClockOffset_thenSystemOffsetClockIsCreated() takes
    over 3 seconds to complete. The SystemClockChangeTest which uses the
    same classloader magic takes up to 3 minutes (it's a NightlyTest).
    
    This commit splits up the ClockTest into a really quick unit test of all
    single components and skips the classloading magic for the singleton
    part of the Clock implementation. This should fix the immediate issue
    with our builds, but needs further investigation of the root cause.

--- hazelcast/src/test/java/com/hazelcast/util/ClockTest.java
+++ hazelcast/src/test/java/com/hazelcast/util/ClockTest.java
@@ -19,24 +19,19 @@ package com.hazelcast.util;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
+import static java.lang.String.format;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
 public class ClockTest extends AbstractClockTest {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
     @After
     public void tearDown() {
-        shutdownIsolatedNode();
         resetClock();
     }
 
@@ -51,28 +46,45 @@ public class ClockTest extends AbstractClockTest {
     }
 
     @Test
-    public void test_whenConfiguringClockOffset_thenSystemOffsetClockIsCreated() {
-        System.setProperty(ClockProperties.HAZELCAST_CLOCK_OFFSET, "15");
-        startIsolatedNode();
+    public void testCreateClock_withDefaults() {
+        Clock.ClockImpl clock = Clock.createClock();
 
-        assertTrue(getClusterTime(isolatedNode) > 0);
+        assertInstanceOf(Clock.SystemClock.class, clock);
     }
 
     @Test
-    public void test_whenConfiguringInvalidClockOffset_thenExceptionIsThrown() {
-        System.setProperty(ClockProperties.HAZELCAST_CLOCK_OFFSET, "InvalidNumber");
+    public void testCreateClock_withClockImpl() {
+        setJumpingClock(30);
 
-        expectedException.expectCause(new RootCauseMatcher(NumberFormatException.class));
+        Clock.ClockImpl clock = Clock.createClock();
 
-        startIsolatedNode();
+        assertInstanceOf(JumpingSystemClock.class, clock);
     }
 
     @Test
-    public void test_whenConfiguringNonExistingClockImpl_thenExceptionIsThrown() {
-        System.setProperty(ClockProperties.HAZELCAST_CLOCK_IMPL, "NonExistingClockImpl");
+    public void testCreateClock_withClockOffset() {
+        setClockOffset(30);
 
-        expectedException.expectCause(new RootCauseMatcher(ClassNotFoundException.class));
+        Clock.ClockImpl clock = Clock.createClock();
 
-        startIsolatedNode();
+        assertInstanceOf(Clock.SystemOffsetClock.class, clock);
+    }
+
+    @Test
+    public void testSystemClock() {
+        Clock.SystemClock clock = new Clock.SystemClock();
+
+        assertTrue(clock.currentTimeMillis() > 0);
+    }
+
+    @Test
+    public void testSystemOffsetClock() {
+        Clock.SystemOffsetClock clock = new Clock.SystemOffsetClock(-999999999);
+
+        long systemMillis = System.currentTimeMillis();
+        sleepSeconds(1);
+        long offsetMillis = clock.currentTimeMillis();
+        assertTrue(format("SystemOffsetClock should be far behind the normal clock! %d < %d", offsetMillis, systemMillis),
+                offsetMillis < systemMillis);
     }
 }
