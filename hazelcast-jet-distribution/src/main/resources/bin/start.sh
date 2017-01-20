39fcf78caf346f2491bd69fcdb55dc3a0c4e5315
Can Gencer <cgencer@gmail.com>
2017-01-20 10:50:54 +0100

    Include example config in package, and fix output in start.sh

--- hazelcast-jet-distribution/src/main/resources/bin/start.sh
+++ hazelcast-jet-distribution/src/main/resources/bin/start.sh
@@ -54,6 +54,6 @@ if [ -z "${PID}" ]; then
     $RUN_JAVA -server $JAVA_OPTS com.hazelcast.jet.server.StartServer &
     echo $! > ${PID_FILE}
 else
-    echo "Another Hazelcast Jet instance is already started in this folder. To start a new instance, please unzip ${project.version}.zip/tar.gz in a new folder."
+    echo "Another Hazelcast Jet instance is already started in this folder. To start a new instance, please unzip hazelcast-jet-${project.version}.zip in a new folder."
     exit 0
 fi
