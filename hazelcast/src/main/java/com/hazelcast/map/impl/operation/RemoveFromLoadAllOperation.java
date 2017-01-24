02a7306647c16aaf668f2eb4cc2aedd5fa9b0a22
Tomek Bujok <tom.bujok@gmail.com>
2017-01-24 15:02:34 +0100

    Deletes keys from LoadAll on partition operation. (#9730)

---
+++ hazelcast/src/main/java/com/hazelcast/map/impl/operation/RemoveFromLoadAllOperation.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.map.impl.operation;
+
+import com.hazelcast.map.impl.MapDataSerializerHook;
+import com.hazelcast.map.impl.recordstore.Storage;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.Data;
+import com.hazelcast.spi.PartitionAwareOperation;
+import com.hazelcast.spi.impl.MutatingOperation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Removes data from Record store on LoadAllOperation that replaces existing values
+ */
+public class RemoveFromLoadAllOperation extends MapOperation implements PartitionAwareOperation, MutatingOperation {
+
+    private List<Data> keys;
+
+    public RemoveFromLoadAllOperation() {
+        keys = Collections.emptyList();
+    }
+
+    public RemoveFromLoadAllOperation(String name, List<Data> keys) {
+        super(name);
+        this.keys = keys;
+    }
+
+    @Override
+    public void run() throws Exception {
+        removeExistingKeys(keys);
+    }
+
+    private void removeExistingKeys(Collection<Data> keys) {
+        if (keys == null || keys.isEmpty()) {
+            return;
+        }
+        Storage storage = recordStore.getStorage();
+        Iterator<Data> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            Data key = iterator.next();
+            if (storage.containsKey(key)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    @Override
+    protected void writeInternal(ObjectDataOutput out) throws IOException {
+        super.writeInternal(out);
+        final int size = keys.size();
+        out.writeInt(size);
+        for (Data key : keys) {
+            out.writeData(key);
+        }
+    }
+
+    @Override
+    protected void readInternal(ObjectDataInput in) throws IOException {
+        super.readInternal(in);
+        final int size = in.readInt();
+        if (size > 0) {
+            keys = new ArrayList<Data>(size);
+        }
+        for (int i = 0; i < size; i++) {
+            Data data = in.readData();
+            keys.add(data);
+        }
+    }
+
+    @Override
+    public int getId() {
+        return MapDataSerializerHook.REMOVE_FROM_LOAD_ALL;
+    }
+}
