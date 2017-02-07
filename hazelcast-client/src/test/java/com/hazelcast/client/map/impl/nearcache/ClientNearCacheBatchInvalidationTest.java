1b3e87306d7e1c585871da4aadba9f6f4087d275
Ahmet Mırçık <ahmetmircik@gmail.com>
2017-02-07 23:31:50 +0300

    Made newClientConfig method `protected`

--- hazelcast-client/src/test/java/com/hazelcast/client/map/impl/nearcache/ClientNearCacheBatchInvalidationTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/map/impl/nearcache/ClientNearCacheBatchInvalidationTest.java
@@ -191,7 +191,7 @@ public class ClientNearCacheBatchInvalidationTest extends ClientTestSupport {
         assertNearCacheSizeEventually(clientMap, 0);
     }
 
-    private ClientConfig newClientConfig(String mapName) {
+    protected ClientConfig newClientConfig(String mapName) {
         NearCacheConfig nearCacheConfig = new NearCacheConfig();
         nearCacheConfig.setInMemoryFormat(getNearCacheInMemoryFormat());
         nearCacheConfig.setName(mapName);
