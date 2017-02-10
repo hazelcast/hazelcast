8761ec94e4fbf809740226a0de77857da701ce64
emre <emre@hazelcast.com>
2017-02-10 16:44:35 +0300

    start.bat prevents starting two instances from the same directory Fixed stop.bat Fix #9820 Fix #9822

--- hazelcast/src/main/resources/start.bat
+++ hazelcast/src/main/resources/start.bat
@@ -23,7 +23,14 @@ if NOT "%MAX_HEAP_SIZE%" == "" (
 	set JAVA_OPTS=%JAVA_OPTS% -Xmx%MAX_HEAP_SIZE%
 )
 
-set CLASSPATH=%~dp0..\lib\hazelcast-all-${project.version}.jar
+set "CLASSPATH=%~dp0..\lib\hazelcast-all-${project.version}.jar"
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`tasklist /FI "WINDOWTITLE eq hazelcast %CLASSPATH%" 2^>NUL ^| find /I /C "java"`) DO (
+SET COUNT=%%F
+)
+IF NOT "%COUNT%"=="0" (
+    goto alreadyrunning
+)
 
 ECHO ########################################
 ECHO # RUN_JAVA=%RUN_JAVA%
@@ -31,13 +38,16 @@ ECHO # JAVA_OPTS=%JAVA_OPTS%
 ECHO # starting now...."
 ECHO ########################################
 
-start "hazelcast" "%RUN_JAVA%" %JAVA_OPTS% -cp "%CLASSPATH%" "com.hazelcast.core.server.StartServer"
+start "hazelcast %CLASSPATH%" "%RUN_JAVA%" %JAVA_OPTS% -cp "%CLASSPATH%" "com.hazelcast.core.server.StartServer"
 goto endofscript
 
 :error
 ECHO JAVA_HOME environment variable must be set!
 pause
 
+:alreadyrunning
+ECHO Another Hazelcast instance is already started in this folder. To start a new instance, please unzip the zip file in a new folder.
+pause
 
 :endofscript
 
