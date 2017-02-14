e76b0ea275e821fc6a2f646c3a4c21acc723c565
Mehmet Dogan <mehmet@hazelcast.com>
2017-02-14 10:51:10 +0300

    Update DefaultNodeExtension.java

--- hazelcast/src/main/java/com/hazelcast/instance/DefaultNodeExtension.java
+++ hazelcast/src/main/java/com/hazelcast/instance/DefaultNodeExtension.java
@@ -104,7 +104,7 @@ public class DefaultNodeExtension implements NodeExtension {
         }
         systemLogger.info("Hazelcast " + buildInfo.getVersion()
                 + " (" + build + ") starting at " + node.getThisAddress());
-        systemLogger.info("Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.");
+        systemLogger.info("Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.");
         systemLogger.info("Configured Hazelcast Serialization version : " + buildInfo.getSerializationVersion());
     }
 
