3fae6bb81faf0a80ef93ae2495618c862d14f64d
Marko Topolnik <marko@hazelcast.com>
2017-02-15 11:10:40 +0100

    Update legal notices regarding Agrona code

--- hazelcast/src/main/java/com/hazelcast/internal/util/concurrent/package-info.java
+++ hazelcast/src/main/java/com/hazelcast/internal/util/concurrent/package-info.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2008, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ * Portions Copyright 2016 Real Logic Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
