d35cd27be19b516e958e87d70698922dfda446ba
Can Gencer <cgencer@gmail.com>
2017-02-15 09:31:59 +0100

    Update MR deprecation message

--- hazelcast/src/main/java/com/hazelcast/mapreduce/JobPartitionState.java
+++ hazelcast/src/main/java/com/hazelcast/mapreduce/JobPartitionState.java
@@ -24,7 +24,9 @@ import com.hazelcast.nio.serialization.impl.BinaryInterface;
  * the status of an process piece while operation is executing.
  *
  * @since 3.2
- * @deprecated This API is superseded by Hazelcast JET (http://jet.hazelcast.org) and will be remove in 3.9
+ * @deprecated MapReduce is deprecated and will be removed in 4.0.
+ * For map aggregations, you can use {@link com.hazelcast.aggregation.Aggregator} on IMap.
+ * For general data processing, it is superseded by <a href="http://jet.hazelcast.org">Hazelcast Jet</a>.
  */
 @Deprecated
 @BinaryInterface
