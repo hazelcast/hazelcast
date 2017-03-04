36f5f3b85a171120532e0673ae8a23deb187bcd8
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-03-04 18:38:15 +0200

    CacheManagementTest parallelized
    It still uses HazelcastSerialClassRunner because tests register
    the same MBean, but it can run parallelized on JVM-level.

--- hazelcast/src/test/java/com/hazelcast/cache/CacheManagementTest.java
+++ hazelcast/src/test/java/com/hazelcast/cache/CacheManagementTest.java
@@ -22,6 +22,7 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
+import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -34,7 +35,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class CacheManagementTest extends CacheTestSupport {
 
     private TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(2);
