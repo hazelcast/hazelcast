69f13ca96c2332a31ff17edb85b7e96e070ff8f6
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-03-06 13:06:38 +0200

    Some TCP tests moved into SlowTest category
    They belong to I/O models disabled by default and they are not all that quick

--- hazelcast/src/test/java/com/hazelcast/internal/networking/spinning/Spinning_TcpIpConnectionManager_ConnectMemberTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/networking/spinning/Spinning_TcpIpConnectionManager_ConnectMemberTest.java
@@ -2,13 +2,13 @@ package com.hazelcast.internal.networking.spinning;
 
 import com.hazelcast.nio.tcp.TcpIpConnectionManager_ConnectMemberBaseTest;
 import com.hazelcast.test.HazelcastSerialClassRunner;
-import com.hazelcast.test.annotation.QuickTest;
+import com.hazelcast.test.annotation.SlowTest;
 import org.junit.Before;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
+@Category(SlowTest.class)
 public class Spinning_TcpIpConnectionManager_ConnectMemberTest extends TcpIpConnectionManager_ConnectMemberBaseTest {
 
     @Before
