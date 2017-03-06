a85e4afe554de5aacc383deffc2dd09058402b0d
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-03-06 14:14:22 +0200

    Spinning_TcpIpConnection_BasicTest moved into SlowTest

--- hazelcast/src/test/java/com/hazelcast/internal/networking/spinning/Spinning_TcpIpConnection_BasicTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/networking/spinning/Spinning_TcpIpConnection_BasicTest.java
@@ -3,12 +3,13 @@ package com.hazelcast.internal.networking.spinning;
 import com.hazelcast.nio.tcp.TcpIpConnection_BaseTest;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
+import com.hazelcast.test.annotation.SlowTest;
 import org.junit.Before;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
+@Category(SlowTest.class)
 public class Spinning_TcpIpConnection_BasicTest extends TcpIpConnection_BaseTest {
 
     @Before
