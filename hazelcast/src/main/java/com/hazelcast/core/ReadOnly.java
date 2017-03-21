51353e56761d36933a950957d1273a248f6a2bf0
Tomek Bujok <tom.bujok@gmail.com>
2017-03-21 17:11:06 +0100

    Adding specific exception for ReadOnly modifications (#10116)

--- hazelcast/src/main/java/com/hazelcast/core/ReadOnly.java
+++ hazelcast/src/main/java/com/hazelcast/core/ReadOnly.java
@@ -19,19 +19,19 @@ package com.hazelcast.core;
 import com.hazelcast.map.EntryProcessor;
 
 /**
- *
  * Allows notifying Hazelcast that the processing unit implementing this interface will not do any modifications.
  * This marker interface allows optimizing the processing to gain more performance.
  *
+ * If the processing processing unit implementing this interface does a modification an exception will be thrown.
+ *
  * Currently supported in:
  * <ul>
- *     <li>{@link EntryProcessor} passed to {@link IMap#executeOnKey(Object, EntryProcessor)}</li>
- *     <li>{@link EntryProcessor} passed to {@link IMap#submitToKey(Object, EntryProcessor)} </li>
- *     <li>{@link EntryProcessor} passed to {@link IMap#submitToKey(Object, EntryProcessor, ExecutionCallback)} </li>
+ * <li>{@link EntryProcessor} passed to {@link IMap#executeOnKey(Object, EntryProcessor)}</li>
+ * <li>{@link EntryProcessor} passed to {@link IMap#submitToKey(Object, EntryProcessor)} </li>
+ * <li>{@link EntryProcessor} passed to {@link IMap#submitToKey(Object, EntryProcessor, ExecutionCallback)} </li>
  * </ul>
  *
  * @see Offloadable
- *
  */
 public interface ReadOnly {
 }
