dc078b90a44274453eb29971d801f6b5dbaf914e
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-03-22 18:27:57 +0200

    Reading build info from a class instead of properties
    Kudus to Vassilis for the Maven magic!

---
+++ hazelcast/src/main/template/com/hazelcast/instance/BuildProperties.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.instance;
+
+public final class BuildProperties {
+    public static final String VERSION = "${project.version}";
+    public static final String BUILD = "${timestamp}";
+    public static final String REVISION = "${git.commit.id.abbrev}";
+    public static final String DISTRIBUTION = "${hazelcast.distribution}";
+    public static final String SERIALIZATION_VERSION = "${hazelcast.serialization.version}";
+
+    private BuildProperties() {
+    }
+}
