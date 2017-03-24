c16e494022d5b04b15279fe77bdaa00ff1f142dd
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-03-24 11:26:57 +0200

    Add the Generated- prefix to BuildProperties
    Also use the EnterpriseBuildProperties only if they were
    defined by the same classloader as your own classes.

--- hazelcast/src/main/template/com/hazelcast/instance/GeneratedBuildProperties.java
+++ hazelcast/src/main/template/com/hazelcast/instance/GeneratedBuildProperties.java
@@ -16,13 +16,28 @@
 
 package com.hazelcast.instance;
 
-public final class BuildProperties {
+/**
+ * This class is generated in a build-time from a template stored at
+ * src/main/template/com/hazelcast/instance/GeneratedBuildProperties.
+ *
+ * Do not edit by hand as the changes will be overwritten in the next build.
+ *
+ * We used to have the version info as property file, but this caused issues
+ * in on environments with a complicated classloading model. Having the info
+ * as a Java class provide a better control when you have multiple version of
+ * Hazelcast deployed.
+ *
+ * WARNING: DO NOT CHANGE FIELD NAMES IN THE TEMPLATE.
+ * The fields are read via reflection at {@link com.hazelcast.instance.BuildInfoProvider}
+ *
+ */
+public final class GeneratedBuildProperties {
     public static final String VERSION = "${project.version}";
     public static final String BUILD = "${timestamp}";
     public static final String REVISION = "${git.commit.id.abbrev}";
     public static final String DISTRIBUTION = "${hazelcast.distribution}";
     public static final String SERIALIZATION_VERSION = "${hazelcast.serialization.version}";
 
-    private BuildProperties() {
+    private GeneratedBuildProperties() {
     }
 }
