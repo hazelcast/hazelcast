33f2fbcbd254eb201e9aa4a753b40d49a8d12922
Ahmet Mırçık <ahmetmircik@gmail.com>
2017-06-20 17:45:33 +0300

    Fixed test: updates_on_same_key_prevents_expiration_on_backups

--- hazelcast/src/test/java/com/hazelcast/map/BackupExpirationTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/BackupExpirationTest.java
@@ -20,27 +20,29 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.core.IMap;
 import com.hazelcast.internal.nearcache.impl.invalidation.InvalidationQueue;
 import com.hazelcast.map.impl.MapService;
 import com.hazelcast.map.impl.MapServiceContext;
-import com.hazelcast.map.impl.record.Record;
 import com.hazelcast.map.impl.recordstore.RecordStore;
 import com.hazelcast.monitor.LocalMapStats;
+import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.spi.impl.NodeEngineImpl;
+import com.hazelcast.spi.serialization.SerializationService;
 import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastParametersRunnerFactory;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
-import com.hazelcast.util.Clock;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Collection;
+import java.util.Map;
 
 import static com.hazelcast.config.InMemoryFormat.BINARY;
 import static com.hazelcast.config.InMemoryFormat.OBJECT;
@@ -138,19 +140,41 @@ public class BackupExpirationTest extends HazelcastTestSupport {
     public void dont_collect_expired_keys_if_expiration_reason_is_TTL() throws Exception {
         configureAndStartNodes(30, 1, 5);
 
-        NodeEngineImpl nodeEngineImpl = getNodeEngineImpl(nodes[0]);
-        MapService mapService = nodeEngineImpl.getService(MapService.SERVICE_NAME);
-        MapServiceContext mapServiceContext = mapService.getMapServiceContext();
-        RecordStore recordStore = mapServiceContext.getRecordStore(0, MAP_NAME);
+        IMap map = nodes[0].getMap(MAP_NAME);
+        Object expirationQueueLength = map.executeOnKey("1", new BackupExpirationQueueLengthFinder());
 
-        long now = Clock.currentTimeMillis();
-        Record record = recordStore.createRecord("value", 100, now);
-        sleepSeconds(1);
-        recordStore.doPostEvictionOperations(record, false);
+        assertEquals(0, ((Integer) expirationQueueLength).intValue());
+    }
 
-        InvalidationQueue expiredKeys = recordStore.getExpiredKeys();
+    // This EP mimics TTL expiration process by creating a record
+    // which expires after 100 millis. TTL expired key should not be added to the expiration queue
+    // after `recordStore.get`.
+    public static final class BackupExpirationQueueLengthFinder
+            extends AbstractEntryProcessor implements HazelcastInstanceAware {
 
-        assertEquals(0, expiredKeys.size());
+        private transient HazelcastInstance node;
+
+        @Override
+        public Object process(Map.Entry entry) {
+            NodeEngineImpl nodeEngineImpl = getNodeEngineImpl(node);
+            SerializationService ss = nodeEngineImpl.getSerializationService();
+            MapService mapService = nodeEngineImpl.getService(MapService.SERVICE_NAME);
+            MapServiceContext mapServiceContext = mapService.getMapServiceContext();
+            RecordStore recordStore = mapServiceContext.getRecordStore(0, MAP_NAME);
+
+            Data dataKey = ss.toData(1);
+            recordStore.put(dataKey, "value", 100);
+            sleepSeconds(1);
+            recordStore.get(dataKey, false);
+
+            InvalidationQueue expiredKeys = recordStore.getExpiredKeys();
+            return expiredKeys.size();
+        }
+
+        @Override
+        public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+            this.node = hazelcastInstance;
+        }
     }
 
     public static long getTotalEntryCount(IMap map) {
