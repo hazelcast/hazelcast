b8521660b0d82611788bf9c6b9156c6ef63fb37c
Thomas Kountis <tkountis@gmail.com>
2017-06-20 21:47:23 +0100

    Spring XML support for Management Center mutual-auth (#10791)
    * Spring XML support for Management Center mutual-auth

--- hazelcast-spring/src/main/java/com/hazelcast/spring/HazelcastConfigBeanDefinitionParser.java
+++ hazelcast-spring/src/main/java/com/hazelcast/spring/HazelcastConfigBeanDefinitionParser.java
@@ -45,6 +45,7 @@ import com.hazelcast.config.ListConfig;
 import com.hazelcast.config.ListenerConfig;
 import com.hazelcast.config.LockConfig;
 import com.hazelcast.config.LoginModuleConfig;
+import com.hazelcast.config.MCMutualAuthConfig;
 import com.hazelcast.config.ManagementCenterConfig;
 import com.hazelcast.config.MapAttributeConfig;
 import com.hazelcast.config.MapConfig;
@@ -108,6 +109,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -139,7 +141,7 @@ import static com.hazelcast.util.StringUtil.upperCaseInternal;
  * &lt;/hz:config&gt;
  * </pre>
  */
-@SuppressWarnings("checkstyle:executablestatementcount")
+@SuppressWarnings({"checkstyle:executablestatementcount"})
 public class HazelcastConfigBeanDefinitionParser extends AbstractHazelcastBeanDefinitionParser {
 
     protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
@@ -436,24 +438,6 @@ public class HazelcastConfigBeanDefinitionParser extends AbstractHazelcastBeanDe
             }
             configBuilder.addPropertyValue("networkConfig", beanDefinition);
         }
-
-/*
-        protected void handleViaReflection(org.w3c.dom.Node child) {
-            final String methodName = xmlToJavaName("handle-" + cleanNodeName(child));
-            final Method method;
-            try {
-                method = getClass().getMethod(methodName, new Class[]{org.w3c.dom.Node.class});
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-                return;
-            }
-            try {
-                method.invoke(this, new Object[]{child});
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-*/
 
         public void handleGroup(Node node) {
             createAndFillBeanBuilder(node, GroupConfig.class, "groupConfig", configBuilder);
@@ -1055,7 +1039,43 @@ public class HazelcastConfigBeanDefinitionParser extends AbstractHazelcastBeanDe
         }
 
         private void handleManagementCenter(final Node node) {
-            createAndFillBeanBuilder(node, ManagementCenterConfig.class, "managementCenterConfig", configBuilder);
+            final BeanDefinitionBuilder managementCenterConfigBuilder = createBeanBuilder(ManagementCenterConfig.class);
+            fillAttributeValues(node, managementCenterConfigBuilder);
+            // < 3.9 - Backwards compatibility
+            boolean isComplexType = false;
+            List<String> complexTypeElements = Arrays.asList("url", "mutual-auth");
+            for (Node c : childElements(node)) {
+                if (complexTypeElements.contains(cleanNodeName(c))) {
+                    isComplexType = true;
+                    break;
+                }
+            }
+            if (isComplexType) {
+                for (Node child : childElements(node)) {
+                    if ("url".equals(cleanNodeName(child))) {
+                        String url = getTextContent(child);
+                        managementCenterConfigBuilder.addPropertyValue("url", url);
+                    } else if ("mutual-auth".equals(cleanNodeName(child))) {
+                        managementCenterConfigBuilder.addPropertyValue("mutualAuthConfig",
+                                handleMcMutualAuthConfig(child).getBeanDefinition());
+                    }
+                }
+            }
+            configBuilder.addPropertyValue("managementCenterConfig", managementCenterConfigBuilder.getBeanDefinition());
+        }
+
+        private BeanDefinitionBuilder handleMcMutualAuthConfig(Node node) {
+            final BeanDefinitionBuilder mcMutualAuthConfigBuilder = createBeanBuilder(MCMutualAuthConfig.class);
+            fillAttributeValues(node, mcMutualAuthConfigBuilder);
+            for (Node n : childElements(node)) {
+                String nodeName = cleanNodeName(n);
+                if ("factory-class-name".equals(nodeName)) {
+                    mcMutualAuthConfigBuilder.addPropertyValue("factoryClassName", getTextContent(n).trim());
+                } else if ("properties".equals(nodeName)) {
+                    handleProperties(n, mcMutualAuthConfigBuilder);
+                }
+            }
+            return mcMutualAuthConfigBuilder;
         }
 
         public void handleNearCacheConfig(Node node, BeanDefinitionBuilder configBuilder) {
