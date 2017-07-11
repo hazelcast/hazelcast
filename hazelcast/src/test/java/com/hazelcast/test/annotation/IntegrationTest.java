9fc498e3867ef01aa338c237112260b55e59dcc2
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-07-11 15:25:11 +0300

    Add integration tests for checking hard dependencies
    The test is meant to prevent a dependency to accidentally leak into
    Import-Package in MANIFEST.MF
    
    This changeset introduces a new test category: IntegrationTest
    Tests belonging to this category are executed after packaging - with
    a final JAR (including shading and all that) on a classpath.
    
    Fixes

---
+++ hazelcast/src/test/java/com/hazelcast/test/annotation/IntegrationTest.java
@@ -0,0 +1,10 @@
+package com.hazelcast.test.annotation;
+
+/**
+ * Tests which should be executed after a final JAR is packaged.
+ * These tests are executed by the Fail-Safe Maven plugin.
+ * (other tests are executed by the Surefire plugin)
+ *
+ */
+public final class IntegrationTest {
+}
