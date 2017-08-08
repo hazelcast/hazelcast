abc5a79bb06274f43b0b14681b923ea52aa0c27f
ihsan demir <ihsan@hazelcast.com>
2017-08-08 14:23:17 +0300

    Remove the unneded duplicate interface in legacy cache config classes.
    Extend the TypedDataSerializable test to test the behaviour where the reconstructed object should be of the type that was provided by the TypedDataSerializable interface.

--- hazelcast/src/test/java/com/hazelcast/internal/serialization/impl/AbstractSerializationServiceTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/serialization/impl/AbstractSerializationServiceTest.java
@@ -240,6 +240,11 @@ public class AbstractSerializationServiceTest {
 
         TypedBaseClass typedBaseObject = new TypedBaseClass(baseObject);
         Data typedData = abstractSerializationService.toData(typedBaseObject);
+
+        deserializedObject = abstractSerializationService.toObject(typedData);
+        assertEquals(BaseClass.class, deserializedObject.getClass());
+        assertEquals(baseObject, deserializedObject);
+
         deserializedObject = abstractSerializationService.toObject(typedData, TypedBaseClass.class);
         assertEquals(typedBaseObject, deserializedObject);
     }
@@ -263,12 +268,14 @@ public class AbstractSerializationServiceTest {
         @Override
         public void writeData(ObjectDataOutput out)
                 throws IOException {
+            innerObj.writeData(out);
             out.writeInt(innerObj.intField);
         }
 
         @Override
         public void readData(ObjectDataInput in)
                 throws IOException {
+            innerObj.readData(in);
             innerObj.intField = in.readInt();
         }
 
