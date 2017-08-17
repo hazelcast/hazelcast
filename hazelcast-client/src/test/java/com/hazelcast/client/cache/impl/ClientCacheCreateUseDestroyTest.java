eb69ccbc9a6a0f526635e1475b002f9b5a98f901
Vassilis Bekiaris <vbekiaris@gmail.com>
2017-08-17 15:21:59 +0300

    Cache create/destroy tests
    (cherry picked from commit 1c113c0)

---
+++ hazelcast-client/src/test/java/com/hazelcast/client/cache/impl/ClientCacheCreateUseDestroyTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.client.cache.impl;
+
+import com.hazelcast.cache.HazelcastCachingProvider;
+import com.hazelcast.cache.impl.CacheCreateUseDestroyTest;
+import com.hazelcast.cache.impl.ICacheService;
+import com.hazelcast.client.test.TestHazelcastFactory;
+import com.hazelcast.core.HazelcastInstance;
+import org.junit.After;
+import org.junit.Before;
+
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+
+public class ClientCacheCreateUseDestroyTest extends CacheCreateUseDestroyTest {
+
+    private TestHazelcastFactory factory;
+
+    @Override
+    @Before
+    public void setup() {
+        assumptions();
+        factory = new TestHazelcastFactory();
+        HazelcastInstance member = factory.newHazelcastInstance(getConfig());
+        CachingProvider provider = Caching.getCachingProvider();
+        defaultCacheManager = provider.getCacheManager(null, null,
+                HazelcastCachingProvider.propertiesByInstanceItself(factory.newHazelcastClient()));
+        cacheService = getNode(member).getNodeEngine().getService(ICacheService.SERVICE_NAME);
+        CacheEntryListenerFactory.listener = null;
+    }
+
+    @After
+    public void cleanup() {
+        if (factory != null) {
+            factory.terminateAll();
+        }
+    }
+
+
+}
