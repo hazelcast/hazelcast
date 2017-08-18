ae0f6a2d3bbf4d06ae5d9dcaf15882bd0c084131
donnerbart <github@thunderphreak.de>
2017-08-18 17:52:04 +0200

    Made SecurityWithoutEnterpriseTest a ParallelTest (no real network used)

--- hazelcast/src/test/java/com/hazelcast/security/SecurityWithoutEnterpriseTest.java
+++ hazelcast/src/test/java/com/hazelcast/security/SecurityWithoutEnterpriseTest.java
@@ -4,21 +4,24 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.SecurityConfig;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
-
 @RunWith(HazelcastSerialClassRunner.class)
-@Category(QuickTest.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class SecurityWithoutEnterpriseTest extends HazelcastTestSupport {
 
     @Test(expected = IllegalStateException.class)
-    public void test() throws IOException {
+    public void test() {
+        SecurityConfig securityConfig = new SecurityConfig()
+                .setEnabled(true);
+
         Config config = new Config()
-                .setSecurityConfig(new SecurityConfig().setEnabled(true));
+                .setSecurityConfig(securityConfig);
+
         createHazelcastInstance(config);
     }
 }
