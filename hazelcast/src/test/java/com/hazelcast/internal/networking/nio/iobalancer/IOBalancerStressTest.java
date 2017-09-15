bb0cbcd599bbb0d34f694c804ebf8ad33b06713f
donnerbart <github@thunderphreak.de>
2017-09-15 14:44:34 +0200

    Small cleanup of IOBalancerStressTest
    * fixed JavaDoc
    * re-ordered methods according to usage
    * replaced StringBuffer with StringBuilder

--- hazelcast/src/test/java/com/hazelcast/internal/networking/nio/iobalancer/IOBalancerStressTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/networking/nio/iobalancer/IOBalancerStressTest.java
@@ -39,7 +39,6 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -54,15 +53,15 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
 
     @Before
     @After
-    public void killAllHazelcastInstances() throws IOException {
+    public void killAllHazelcastInstances() {
         HazelcastInstanceFactory.terminateAll();
     }
 
     @Test
     public void testEachConnectionUseDifferentSelectorEventually() {
-        Config config = new Config();
-        config.setProperty(GroupProperty.IO_BALANCER_INTERVAL_SECONDS.getName(), "1");
-        config.setProperty(GroupProperty.IO_THREAD_COUNT.getName(), "2");
+        Config config = new Config()
+                .setProperty(GroupProperty.IO_BALANCER_INTERVAL_SECONDS.getName(), "1")
+                .setProperty(GroupProperty.IO_THREAD_COUNT.getName(), "2");
 
         HazelcastInstance instance1 = Hazelcast.newHazelcastInstance(config);
         HazelcastInstance instance2 = Hazelcast.newHazelcastInstance(config);
@@ -72,7 +71,6 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
         instance2 = Hazelcast.newHazelcastInstance(config);
 
         IMap<Integer, Integer> map = instance1.getMap(randomMapName());
-
         for (int i = 0; i < 10000; i++) {
             map.put(i, i);
         }
@@ -94,40 +92,10 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
                 assertBalanced(selector, handlers);
             }
         } catch (AssertionError e) {
-            // if something fails, we want to see the debug
+            // if something fails, we want to see the debug output
             System.out.println(debug(connectionManager));
             throw e;
         }
-    }
-
-    public String debug(TcpIpConnectionManager connectionManager) {
-        NioEventLoopGroup threadingModel = (NioEventLoopGroup) connectionManager.getEventLoopGroup();
-
-        StringBuffer sb = new StringBuffer();
-        sb.append("in selectors\n");
-        for (NioThread in : threadingModel.getInputThreads()) {
-            sb.append(in + ": " + in.getEventCount() + "\n");
-
-            for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
-                NioChannelReader socketReader = ((NioChannel) connection.getChannel()).getReader();
-                if (socketReader.getOwner() == in) {
-                    sb.append("\t" + socketReader + " eventCount:" + socketReader.getLoad() + "\n");
-                }
-            }
-        }
-        sb.append("out selectors\n");
-        for (NioThread in : threadingModel.getOutputThreads()) {
-            sb.append(in + ": " + in.getEventCount() + "\n");
-
-            for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
-                NioChannelWriter socketWriter = ((NioChannel) connection.getChannel()).getWriter();
-                if (socketWriter.getOwner() == in) {
-                    sb.append("\t" + socketWriter + " eventCount:" + socketWriter.getLoad() + "\n");
-                }
-            }
-        }
-
-        return sb.toString();
     }
 
     private Map<NioThread, Set<MigratableHandler>> getHandlersPerSelector(TcpIpConnectionManager connectionManager) {
@@ -140,15 +108,23 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
         return handlersPerSelector;
     }
 
+    private void add(Map<NioThread, Set<MigratableHandler>> handlersPerSelector, MigratableHandler handler) {
+        Set<MigratableHandler> handlers = handlersPerSelector.get(handler.getOwner());
+        if (handlers == null) {
+            handlers = new HashSet<MigratableHandler>();
+            handlersPerSelector.put(handler.getOwner(), handlers);
+        }
+        handlers.add(handler);
+    }
+
     /**
      * A selector is balanced if:
-     * - it has 1 active handler (so a high event count)
-     * - potentially 1 dead handler (duplicate connection). So event count should be low.
-     *
-     * @param selector
-     * @param handlers
+     * <ul>
+     * <li>it has 1 active handler (so a high event count)</li>
+     * <li>potentially 1 dead handler (duplicate connection), so event count should be low</li>
+     * </ul>
      */
-    public void assertBalanced(NioThread selector, Set<MigratableHandler> handlers) {
+    private void assertBalanced(NioThread selector, Set<MigratableHandler> handlers) {
         assertTrue("no handlers were found for selector:" + selector, handlers.size() > 0);
         assertTrue("too many handlers were found for selector:" + selector, handlers.size() <= 2);
 
@@ -172,12 +148,33 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
                 activeHandler.getLoad() > 1000);
     }
 
-    private void add(Map<NioThread, Set<MigratableHandler>> handlersPerSelector, MigratableHandler handler) {
-        Set<MigratableHandler> handlers = handlersPerSelector.get(handler.getOwner());
-        if (handlers == null) {
-            handlers = new HashSet<MigratableHandler>();
-            handlersPerSelector.put(handler.getOwner(), handlers);
+    private String debug(TcpIpConnectionManager connectionManager) {
+        NioEventLoopGroup threadingModel = (NioEventLoopGroup) connectionManager.getEventLoopGroup();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("in selectors\n");
+        for (NioThread in : threadingModel.getInputThreads()) {
+            sb.append(in).append(": ").append(in.getEventCount()).append("\n");
+
+            for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
+                NioChannelReader socketReader = ((NioChannel) connection.getChannel()).getReader();
+                if (socketReader.getOwner() == in) {
+                    sb.append("\t").append(socketReader).append(" eventCount:").append(socketReader.getLoad()).append("\n");
+                }
+            }
         }
-        handlers.add(handler);
+        sb.append("out selectors\n");
+        for (NioThread in : threadingModel.getOutputThreads()) {
+            sb.append(in).append(": ").append(in.getEventCount()).append("\n");
+
+            for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
+                NioChannelWriter socketWriter = ((NioChannel) connection.getChannel()).getWriter();
+                if (socketWriter.getOwner() == in) {
+                    sb.append("\t").append(socketWriter).append(" eventCount:").append(socketWriter.getLoad()).append("\n");
+                }
+            }
+        }
+
+        return sb.toString();
     }
 }
