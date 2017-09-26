0862c483877c7ba750cda91700b3d4e47bf941b9
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-09-26 17:44:55 +0200

    Increase hazelcast.wait.seconds.before.join in a test
    Fixes #11085
    
    The hazelcast.wait.seconds.before.join is set to 1s in our tests by default.
    This is insufficient to the MulticastDiscovery strategy as the 2nd member may
    start before receiving the multicast message from the 1st member.

--- hazelcast/src/test/java/com/hazelcast/spi/discovery/multicast/MemberToMemberDiscoveryTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/discovery/multicast/MemberToMemberDiscoveryTest.java
@@ -23,20 +23,27 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.OverridePropertyRule;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.io.InputStream;
 
+import static com.hazelcast.test.OverridePropertyRule.set;
+
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
 public class MemberToMemberDiscoveryTest extends HazelcastTestSupport {
 
     private Config config;
+
+    @Rule
+    public final OverridePropertyRule overridePropertyRule = set("hazelcast.wait.seconds.before.join", "10");
 
     @Before
     public void setUp() {
