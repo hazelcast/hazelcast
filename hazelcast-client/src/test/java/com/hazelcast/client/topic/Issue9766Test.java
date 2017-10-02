fe02c690d8b711ed7872f72c7fb4e98b4cfbf93e
sancar <sancar@hazelcast.com>
2017-10-02 15:43:38 +0300

    fixes Issue9766Test.serverRestartWhenReliableTopicListenerRegistered
    Test was failing because client can not connect to cluster.
    Test is fixed via setting connection attemp limit to Integer.MAX
    
    fixes #11487

--- hazelcast-client/src/test/java/com/hazelcast/client/topic/Issue9766Test.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/topic/Issue9766Test.java
@@ -15,7 +15,7 @@
  */
 
 package com.hazelcast.client.topic;/*
- * Copyright (c) 2008, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +30,7 @@ package com.hazelcast.client.topic;/*
  * limitations under the License.
  */
 
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.test.TestHazelcastFactory;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ITopic;
@@ -67,8 +68,10 @@ public class Issue9766Test {
     public void serverRestartWhenReliableTopicListenerRegistered() {
         HazelcastInstance server = hazelcastFactory.newHazelcastInstance();
 
-        HazelcastInstance hazelcastClient = hazelcastFactory.newHazelcastClient();
-        HazelcastInstance hazelcastClient2 = hazelcastFactory.newHazelcastClient();
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().setConnectionAttemptLimit(Integer.MAX_VALUE);
+        HazelcastInstance hazelcastClient = hazelcastFactory.newHazelcastClient(clientConfig);
+        HazelcastInstance hazelcastClient2 = hazelcastFactory.newHazelcastClient(clientConfig);
         ITopic<Integer> topic = hazelcastClient.getReliableTopic(topicName);
         final ITopic<Integer> topic2 = hazelcastClient2.getReliableTopic(topicName);
 
