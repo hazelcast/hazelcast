133787502db4414e6a8d0f736d3f4ac1528b81e3
ihsan demir <ihsan@hazelcast.com>
2017-10-05 16:46:14 +0300

    Opened the ignored test cases.

--- hazelcast-client/src/test/java/com/hazelcast/client/statistics/ClientStatisticsTest.java
+++ hazelcast-client/src/test/java/com/hazelcast/client/statistics/ClientStatisticsTest.java
@@ -38,7 +38,6 @@ import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -173,7 +172,6 @@ public class ClientStatisticsTest extends ClientTestSupport {
     }
 
     @Test
-    @Ignore
     public void testStatisticsClusterReconnect() {
         HazelcastInstance hazelcastInstance = hazelcastFactory.newHazelcastInstance();
         HazelcastClientInstanceImpl client = createHazelcastClient();
@@ -202,7 +200,6 @@ public class ClientStatisticsTest extends ClientTestSupport {
     }
 
     @Test
-    @Ignore
     public void testStatisticsTwoClients() {
         HazelcastInstance hazelcastInstance = hazelcastFactory.newHazelcastInstance();
         HazelcastClientInstanceImpl client1 = createHazelcastClient();
