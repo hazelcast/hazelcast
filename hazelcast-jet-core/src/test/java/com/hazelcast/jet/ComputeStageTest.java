3b53ceb412b9047fdad6a238641e9b925d747593
Marko Topolnik <marko@hazelcast.com>
2017-10-06 15:55:36 +0200

    ComputeStageTest in cluster

--- hazelcast-jet-core/src/test/java/com/hazelcast/jet/ComputeStageTest.java
+++ hazelcast-jet-core/src/test/java/com/hazelcast/jet/ComputeStageTest.java
@@ -17,27 +17,26 @@
 package com.hazelcast.jet;
 
 import com.hazelcast.core.IList;
+import com.hazelcast.core.IMap;
 import com.hazelcast.jet.accumulator.LongAccumulator;
 import com.hazelcast.jet.aggregate.AggregateOperation;
-import com.hazelcast.jet.core.JetTestSupport;
 import com.hazelcast.jet.core.processor.Processors;
 import com.hazelcast.jet.datamodel.ItemsByTag;
 import com.hazelcast.jet.datamodel.Tag;
 import com.hazelcast.jet.datamodel.Tuple2;
 import com.hazelcast.jet.datamodel.Tuple3;
-import com.hazelcast.test.HazelcastParallelClassRunner;
-import com.hazelcast.test.annotation.QuickTest;
-import org.junit.After;
+import com.hazelcast.jet.function.DistributedFunction;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static com.hazelcast.jet.JoinClause.joinMapEntries;
 import static com.hazelcast.jet.Traversers.traverseIterable;
@@ -46,41 +45,35 @@ import static com.hazelcast.jet.aggregate.AggregateOperations.counting;
 import static com.hazelcast.jet.datamodel.ItemsByTag.itemsByTag;
 import static com.hazelcast.jet.datamodel.Tuple2.tuple2;
 import static com.hazelcast.jet.datamodel.Tuple3.tuple3;
+import static com.hazelcast.jet.function.DistributedFunctions.alwaysTrue;
 import static com.hazelcast.jet.function.DistributedFunctions.wholeItem;
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@Category(QuickTest.class)
-@RunWith(HazelcastParallelClassRunner.class)
-public class ComputeStageTest extends JetTestSupport {
+public class ComputeStageTest extends TestInClusterSupport {
 
+    private static final char ITEM_COUNT = 1000;
     private Pipeline pipeline;
     private ComputeStage<Integer> srcStage;
     private Sink<Object> sink;
 
-    private JetTestInstanceFactory factory;
-    private JetInstance jet;
-    private IList<Integer> srcList;
+    private IMap<String, Integer> srcMap;
     private IList<Object> sinkList;
 
     @Before
     public void before() {
         pipeline = Pipeline.create();
-        srcStage = pipeline.drawFrom(Sources.readList("src"));
-        sink = Sinks.writeList("sink");
 
-        factory = new JetTestInstanceFactory();
-        jet = factory.newMember();
-        srcList = jet.getList("src");
-        sinkList = jet.getList("sink");
+        String srcName = randomName();
+        srcStage = pipeline.drawFrom(readMapValues(srcName));
+        srcMap = jet().getMap(srcName);
+
+        String sinkName = randomName();
+        sink = Sinks.writeList(sinkName);
+        sinkList = jet().getList(sinkName);
     }
-
-    @After
-    public void tearDown() {
-        factory.terminateAll();
-    }
-
 
     @Test(expected = IllegalArgumentException.class)
     public void when_emptyPipelineToDag_then_exceptionInIterator() {
@@ -105,26 +98,33 @@ public class ComputeStageTest extends JetTestSupport {
         mapped.drainTo(sink);
 
         // When
-        srcList.add(1);
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
         execute();
 
         // Then
-        assertEquals(bag("1"), toBag(sinkList));
+        List<String> expected = input.stream()
+                                     .map(String::valueOf)
+                                     .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void filter() {
         // Given
-        ComputeStage<Integer> filtered = srcStage.filter(o -> o.equals(2));
+        ComputeStage<Integer> filtered = srcStage.filter(i -> i % 2 == 1);
         filtered.drainTo(sink);
 
         // When
-        srcList.add(1);
-        srcList.add(2);
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
         execute();
 
         // Then
-        assertEquals(bag(2), toBag(sinkList));
+        List<Integer> expected = input.stream()
+                                      .filter(i -> i % 2 == 1)
+                                      .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
@@ -134,11 +134,15 @@ public class ComputeStageTest extends JetTestSupport {
         flatMapped.drainTo(sink);
 
         // When
-        srcList.add(1);
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
         execute();
 
         // Then
-        assertEquals(bag("1A", "1B"), toBag(sinkList));
+        List<String> expected = input.stream()
+                                     .flatMap(o -> Stream.of(o + "A", o + "B"))
+                                     .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
@@ -148,35 +152,49 @@ public class ComputeStageTest extends JetTestSupport {
         grouped.drainTo(sink);
 
         // When
-        srcList.addAll(asList(1, 2, 3, 3, 2, 3));
+        List<Integer> input = IntStream.range(1, 100).boxed()
+                                       .flatMap(i -> Collections.nCopies(i, i).stream())
+                                       .collect(toList());
+        putToSrcMap(input);
         execute();
 
         // Then
-        assertEquals(bag(entry(1, 1L), entry(2, 2L), entry(3, 3L)), toBag(sinkList));
+        List<Entry<Integer, Long>> expected = IntStream.range(1, 100)
+                                                       .mapToObj(i -> entry(i, (long) i))
+                                                       .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void hashJoinTwo() {
         // Given
-        ComputeStage<Map.Entry<Integer, String>> enrichingStage = pipeline.drawFrom(Sources.readList("enrich"));
+        String enrichingName = randomName();
+        ComputeStage<Entry<Integer, String>> enrichingStage = pipeline.drawFrom(Sources.readMap(enrichingName));
 
         ComputeStage<Tuple2<Integer, String>> joined = srcStage.hashJoin(enrichingStage, joinMapEntries(wholeItem()));
         joined.drainTo(sink);
 
         // When
-        srcList.addAll(asList(1, 2, 1));
-        jet.getList("enrich").addAll(asList(entry(1, "one"), entry(2, "two")));
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
+        IMap<Integer, String> enriching = jet().getMap(enrichingName);
+        input.forEach(i -> enriching.put(i, i + "A"));
         execute();
 
         // Then
-        assertEquals(bag(tuple2(1, "one"), tuple2(2, "two"), tuple2(1, "one")), toBag(sinkList));
+        List<Tuple2<Integer, String>> expected = input.stream()
+                                                      .map(i -> tuple2(i, i + "A"))
+                                                      .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void hashJoinThree() {
         // Given
-        ComputeStage<Map.Entry<Integer, String>> enrichingStage1 = pipeline.drawFrom(Sources.readList("enrich1"));
-        ComputeStage<Map.Entry<Integer, String>> enrichingStage2 = pipeline.drawFrom(Sources.readList("enrich2"));
+        String enriching1Name = randomName();
+        String enriching2Name = randomName();
+        ComputeStage<Entry<Integer, String>> enrichingStage1 = pipeline.drawFrom(Sources.readMap(enriching1Name));
+        ComputeStage<Entry<Integer, String>> enrichingStage2 = pipeline.drawFrom(Sources.readMap(enriching2Name));
         ComputeStage<Tuple3<Integer, String, String>> joined = srcStage.hashJoin(
                 enrichingStage1, joinMapEntries(wholeItem()),
                 enrichingStage2, joinMapEntries(wholeItem())
@@ -184,42 +202,56 @@ public class ComputeStageTest extends JetTestSupport {
         joined.drainTo(sink);
 
         // When
-        srcList.addAll(asList(1, 2, 1));
-        jet.getList("enrich1").addAll(asList(entry(1, "one"), entry(2, "two")));
-        jet.getList("enrich2").addAll(asList(entry(1, "uno"), entry(2, "due")));
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
+        IMap<Integer, String> enriching1 = jet().getMap(enriching1Name);
+        IMap<Integer, String> enriching2 = jet().getMap(enriching2Name);
+        input.forEach(i -> enriching1.put(i, i + "A"));
+        input.forEach(i -> enriching2.put(i, i + "B"));
         execute();
 
         // Then
-        assertEquals(bag(tuple3(1, "one", "uno"), tuple3(2, "two", "due"), tuple3(1, "one", "uno")), toBag(sinkList));
+        List<Tuple3<Integer, String, String>> expected = input.stream()
+                                                              .map(i -> tuple3(i, i + "A", i + "B"))
+                                                              .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void hashJoinBuilder() {
         // Given
-        ComputeStage<Map.Entry<Integer, String>> enrichingStage1 = pipeline.drawFrom(Sources.readList("enrich1"));
-        ComputeStage<Map.Entry<Integer, String>> enrichingStage2 = pipeline.drawFrom(Sources.readList("enrich2"));
+        String enriching1Name = randomName();
+        String enriching2Name = randomName();
+        ComputeStage<Entry<Integer, String>> enrichingStage1 = pipeline.drawFrom(Sources.readMap(enriching1Name));
+        ComputeStage<Entry<Integer, String>> enrichingStage2 = pipeline.drawFrom(Sources.readMap(enriching2Name));
         HashJoinBuilder<Integer> b = srcStage.hashJoinBuilder();
-        Tag<String> english = b.add(enrichingStage1, joinMapEntries(wholeItem()));
-        Tag<String> italian = b.add(enrichingStage2, joinMapEntries(wholeItem()));
+        Tag<String> tagA = b.add(enrichingStage1, joinMapEntries(wholeItem()));
+        Tag<String> tagB = b.add(enrichingStage2, joinMapEntries(wholeItem()));
         ComputeStage<Tuple2<Integer, ItemsByTag>> joined = b.build();
         joined.drainTo(sink);
 
         // When
-        srcList.addAll(asList(1, 2, 1));
-        jet.getList("enrich1").addAll(asList(entry(1, "one"), entry(2, "two")));
-        jet.getList("enrich2").addAll(asList(entry(1, "uno"), entry(2, "due")));
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
+        IMap<Integer, String> enriching1 = jet().getMap(enriching1Name);
+        IMap<Integer, String> enriching2 = jet().getMap(enriching2Name);
+        input.forEach(i -> enriching1.put(i, i + "A"));
+        input.forEach(i -> enriching2.put(i, i + "B"));
         execute();
 
         // Then
-        Tuple2<Integer, ItemsByTag> one = tuple2(1, itemsByTag(english, "one", italian, "uno"));
-        Tuple2<Integer, ItemsByTag> two = tuple2(2, itemsByTag(english, "two", italian, "due"));
-        assertEquals(bag(one, two, one), toBag(sinkList));
+        List<Tuple2<Integer, ItemsByTag>> expected = input
+                .stream()
+                .map(i -> tuple2(i, itemsByTag(tagA, i + "A", tagB, i + "B")))
+                .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void coGroupTwo() {
         //Given
-        ComputeStage<Integer> src1 = pipeline.drawFrom(Sources.readList("src1"));
+        String src1Name = randomName();
+        ComputeStage<Integer> src1 = pipeline.drawFrom(readMapValues(src1Name));
 
         ComputeStage<Entry<Integer, Long>> coGrouped = srcStage.coGroup(wholeItem(), src1, wholeItem(),
                 AggregateOperation
@@ -231,20 +263,27 @@ public class ComputeStageTest extends JetTestSupport {
         coGrouped.drainTo(sink);
 
         // When
-        List<Integer> oneTwoThree = asList(1, 2, 2, 3, 3, 3);
-        srcList.addAll(oneTwoThree);
-        jet.getList("src1").addAll(oneTwoThree);
+        List<Integer> input = IntStream.range(1, 100).boxed()
+                                       .flatMap(i -> Collections.nCopies(i, i).stream())
+                                       .collect(toList());
+        putToSrcMap(input);
+        putToMap(jet().getMap(src1Name), input);
         execute();
 
         // Then
-        assertEquals(bag(entry(1, 11L), entry(2, 22L), entry(3, 33L)), toBag(sinkList));
+        List<Entry<Integer, Long>> expected = IntStream.range(1, 100)
+                                                       .mapToObj(i -> entry(i, 11L * i))
+                                                       .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void coGroupThree() {
         //Given
-        ComputeStage<Integer> src1 = pipeline.drawFrom(Sources.readList("src1"));
-        ComputeStage<Integer> src2 = pipeline.drawFrom(Sources.readList("src2"));
+        String src1Name = randomName();
+        String src2Name = randomName();
+        ComputeStage<Integer> src1 = pipeline.drawFrom(readMapValues(src1Name));
+        ComputeStage<Integer> src2 = pipeline.drawFrom(readMapValues(src2Name));
 
         ComputeStage<Entry<Integer, Long>> coGrouped = srcStage.coGroup(wholeItem(),
                 src1, wholeItem(),
@@ -259,21 +298,28 @@ public class ComputeStageTest extends JetTestSupport {
         coGrouped.drainTo(sink);
 
         // When
-        List<Integer> oneTwoThree = asList(1, 2, 2, 3, 3, 3);
-        srcList.addAll(oneTwoThree);
-        jet.getList("src1").addAll(oneTwoThree);
-        jet.getList("src2").addAll(oneTwoThree);
+        List<Integer> input = IntStream.range(1, 100).boxed()
+                                       .flatMap(i -> Collections.nCopies(i, i).stream())
+                                       .collect(toList());
+        putToSrcMap(input);
+        putToMap(jet().getMap(src1Name), input);
+        putToMap(jet().getMap(src2Name), input);
         execute();
 
         // Then
-        assertEquals(bag(entry(1, 111L), entry(2, 222L), entry(3, 333L)), toBag(sinkList));
+        List<Entry<Integer, Long>> expected = IntStream.range(1, 100)
+                                                       .mapToObj(i -> entry(i, 111L * i))
+                                                       .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
     public void coGroupBuilder() {
         //Given
-        ComputeStage<Integer> src1 = pipeline.drawFrom(Sources.readList("src1"));
-        ComputeStage<Integer> src2 = pipeline.drawFrom(Sources.readList("src2"));
+        String src1Name = randomName();
+        String src2Name = randomName();
+        ComputeStage<Integer> src1 = pipeline.drawFrom(readMapValues(src1Name));
+        ComputeStage<Integer> src2 = pipeline.drawFrom(readMapValues(src2Name));
         CoGroupBuilder<Integer, Integer> b = srcStage.coGroupBuilder(wholeItem());
         Tag<Integer> tag0 = b.tag0();
         Tag<Integer> tag1 = b.add(src1, wholeItem());
@@ -288,14 +334,19 @@ public class ComputeStageTest extends JetTestSupport {
         coGrouped.drainTo(sink);
 
         // When
-        List<Integer> oneTwoThree = asList(1, 2, 2, 3, 3, 3);
-        srcList.addAll(oneTwoThree);
-        jet.getList("src1").addAll(oneTwoThree);
-        jet.getList("src2").addAll(oneTwoThree);
+        List<Integer> input = IntStream.range(1, 100).boxed()
+                                       .flatMap(i -> Collections.nCopies(i, i).stream())
+                                       .collect(toList());
+        putToSrcMap(input);
+        putToMap(jet().getMap(src1Name), input);
+        putToMap(jet().getMap(src2Name), input);
         execute();
 
         // Then
-        assertEquals(bag(entry(1, 111L), entry(2, 222L), entry(3, 333L)), toBag(sinkList));
+        List<Entry<Integer, Long>> expected = IntStream.range(1, 100)
+                                                       .mapToObj(i -> entry(i, 111L * i))
+                                                       .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
     }
 
     @Test
@@ -305,20 +356,41 @@ public class ComputeStageTest extends JetTestSupport {
         custom.drainTo(sink);
 
         // When
-        srcList.add(1);
+        List<Integer> input = sequence(ITEM_COUNT);
+        putToSrcMap(input);
         execute();
 
         // Then
-        assertEquals(bag("1"), toBag(sinkList));
+        List<String> expected = input.stream()
+                                     .map(String::valueOf)
+                                     .collect(toList());
+        assertEquals(toBag(expected), sinkToBag());
+    }
+
+    private void putToSrcMap(List<Integer> data) {
+        putToMap(srcMap, data);
+    }
+
+    private static void putToMap(Map<String, Integer> dest, List<Integer> data) {
+        int[] key = {0};
+        data.forEach(i -> dest.put(String.valueOf(key[0]++), i));
+    }
+
+    private JetInstance jet() {
+        return testMode.getJet();
     }
 
     private void execute() {
-        jet.newJob(pipeline).join();
+        jet().newJob(pipeline).join();
     }
 
-    @SafeVarargs
-    private static <T> Map<T, Integer> bag(T... elems) {
-        return toBag(asList(elems));
+    private Map<Object, Integer> sinkToBag() {
+        return toBag(this.sinkList);
+    }
+
+    private static Source<Integer> readMapValues(String srcName) {
+        return Sources.readMap(srcName, alwaysTrue(),
+                (DistributedFunction<Entry<Integer, Integer>, Integer>) Entry::getValue);
     }
 
     private static <T> Map<T, Integer> toBag(Collection<T> coll) {
@@ -327,5 +399,9 @@ public class ComputeStageTest extends JetTestSupport {
             bag.merge(t, 1, (count, x) -> count + 1);
         }
         return bag;
+    }
+
+    private static List<Integer> sequence(int itemCount) {
+        return IntStream.range(0, itemCount).boxed().collect(toList());
     }
 }
