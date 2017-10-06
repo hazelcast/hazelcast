f10a486fc9786a4b724d69e2c888c9c123f613e2
Josef Cacek <josef.cacek@gmail.com>
2017-10-06 14:00:39 +0200

    Fix the unzip instruction in start.sh

--- hazelcast/src/main/resources/start.sh
+++ hazelcast/src/main/resources/start.sh
@@ -54,6 +54,6 @@ if [ -z "${PID}" ]; then
     $RUN_JAVA -server $JAVA_OPTS com.hazelcast.core.server.StartServer &
     echo $! > ${PID_FILE}
 else
-    echo "Another hazelcast instance is already started in this folder. To start a new instance, please unzip ${project.version}.zip/tar.gz in a new folder."
+    echo "Another hazelcast instance is already started in this folder. To start a new instance, please unzip hazelcast-${project.version}.zip/tar.gz in a new folder."
     exit 0
 fi
