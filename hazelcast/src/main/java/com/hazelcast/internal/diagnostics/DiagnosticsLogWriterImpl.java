bf2975b37015a967a0a0c76028e78245905df614
donnerbart <github@thunderphreak.de>
2017-10-09 14:17:31 +0200

    Fixed visibility of DiagnosticsLogWriterImpl for EE plugins

--- hazelcast/src/main/java/com/hazelcast/internal/diagnostics/DiagnosticsLogWriterImpl.java
+++ hazelcast/src/main/java/com/hazelcast/internal/diagnostics/DiagnosticsLogWriterImpl.java
@@ -34,7 +34,7 @@ import static java.util.Calendar.YEAR;
 /**
  * A writer like structure dedicated for the {@link DiagnosticsPlugin} rendering.
  */
-class DiagnosticsLogWriterImpl implements DiagnosticsLogWriter {
+public class DiagnosticsLogWriterImpl implements DiagnosticsLogWriter {
 
     private static final String STR_LONG_MIN_VALUE = String.format(LOCALE_INTERNAL, "%,d", Long.MIN_VALUE);
 
@@ -67,11 +67,11 @@ class DiagnosticsLogWriterImpl implements DiagnosticsLogWriter {
     // used to write primitives without causing litter
     private StringBuilder stringBuilder = new StringBuilder();
 
-    DiagnosticsLogWriterImpl() {
+    public DiagnosticsLogWriterImpl() {
         this(false);
     }
 
-    DiagnosticsLogWriterImpl(boolean includeEpochTime) {
+    public DiagnosticsLogWriterImpl(boolean includeEpochTime) {
         this.includeEpochTime = includeEpochTime;
     }
 
