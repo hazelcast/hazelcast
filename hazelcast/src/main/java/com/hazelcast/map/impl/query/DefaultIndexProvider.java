593f37e82158278556592eeb962ae1ddf589dc93
Tom Bujok <tom@hazelcast.com>
2017-10-31 16:26:34 +0100

    Adding a tunnable trade-off for index query result copying. Wiring-up the Hazelcast property...

--- hazelcast/src/main/java/com/hazelcast/map/impl/query/DefaultIndexProvider.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/query/DefaultIndexProvider.java
@@ -18,12 +18,14 @@ package com.hazelcast.map.impl.query;
 
 import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.query.impl.Index;
+import com.hazelcast.query.impl.IndexCopyBehavior;
 import com.hazelcast.query.impl.IndexImpl;
 import com.hazelcast.query.impl.getters.Extractors;
 
 public class DefaultIndexProvider implements IndexProvider {
     @Override
-    public Index createIndex(String attributeName, boolean ordered, Extractors extractors, InternalSerializationService ss) {
-        return new IndexImpl(attributeName, ordered, ss, extractors);
+    public Index createIndex(String attributeName, boolean ordered, Extractors extractors,
+                             InternalSerializationService ss, IndexCopyBehavior copyBehavior) {
+        return new IndexImpl(attributeName, ordered, ss, extractors, copyBehavior);
     }
 }
