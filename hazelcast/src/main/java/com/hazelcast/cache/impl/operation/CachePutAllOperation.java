258b7e3df958ad108143cedaab07f7f1163203a5
Vassilis Bekiaris <vbekiaris@gmail.com>
2017-11-03 11:32:25 +0200

    Avoid NPE on backup execution of CachePutAllOperation
    The backupRecord may be null (for example if expired on put).

--- hazelcast/src/main/java/com/hazelcast/cache/impl/operation/CachePutAllOperation.java
+++ hazelcast/src/main/java/com/hazelcast/cache/impl/operation/CachePutAllOperation.java
@@ -83,7 +83,10 @@ public class CachePutAllOperation
             Data key = entry.getKey();
             Data value = entry.getValue();
             CacheRecord backupRecord = cache.put(key, value, expiryPolicy, callerUuid, completionId);
-            backupRecords.put(key, backupRecord);
+            // backupRecord may be null (eg expired on put)
+            if (backupRecord != null) {
+                backupRecords.put(key, backupRecord);
+            }
         }
     }
 
