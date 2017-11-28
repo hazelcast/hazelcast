hazelcast/hazelcast

commit c92b228e54e45bc3dcf1b723d0035192e7fe4f54

Fix member missing attributes issue after lite member promotion
Local member and the member instance from the cluster member-list
should have the same attributes after lite member promotion.

hazelcast/src/main/java/com/hazelcast/instance/MemberImpl.java
@@ -20,6 +20,7 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.core.Member;
 import com.hazelcast.internal.cluster.impl.ClusterDataSerializerHook;
+import com.hazelcast.internal.cluster.impl.ClusterServiceImpl;
 import com.hazelcast.internal.cluster.impl.operations.MemberAttributeChangedOp;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.nio.Address;
@@ -219,7 +220,7 @@ public final class MemberImpl extends AbstractMember implements Member, Hazelcas
         try {
             for (Member member : nodeEngine.getClusterService().getMembers()) {
                 if (!member.localMember()) {
-                    os.send(operation, member.getAddress());
+                    os.invokeOnTarget(ClusterServiceImpl.SERVICE_NAME, operation, member.getAddress());
                 } else {
                     os.execute(operation);
                 }

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/ClusterServiceImpl.java
@@ -621,15 +621,11 @@ public class ClusterServiceImpl implements ClusterService, ConnectionListener, M
     public void updateMemberAttribute(String uuid, MemberAttributeOperationType operationType, String key, Object value) {
         lock.lock();
         try {
-            for (MemberImpl member : membershipManager.getMembers()) {
-                if (member.getUuid().equals(uuid)) {
-                    if (!member.equals(getLocalMember())) {
-                        member.updateAttribute(operationType, key, value);
-                    }
-                    sendMemberAttributeEvent(member, operationType, key, value);
-                    break;
-                }
+            MemberImpl member = membershipManager.getMember(uuid);
+            if (!member.equals(getLocalMember())) {
+                member.updateAttribute(operationType, key, value);
             }
+            sendMemberAttributeEvent(member, operationType, key, value);
         } finally {
             lock.unlock();
         }
@@ -1134,12 +1130,20 @@ public class ClusterServiceImpl implements ClusterService, ConnectionListener, M
                 throw new IllegalStateException("Cannot promote to data member! Previous master was: " + master.getAddress()
                     + ", Current master is: " + getMasterAddress());
             }
-
-            localMember = new MemberImpl(member.getAddress(), member.getVersion(), true, member.getUuid(),
-                    member.getAttributes(), false, node.hazelcastInstance);
         } finally {
             lock.unlock();
         }
+    }
+
+    MemberImpl promoteAndGetLocalMember() {
+        MemberImpl member = getLocalMember();
+        assert member.isLiteMember() : "Local member is not lite member!";
+        assert lock.isHeldByCurrentThread() : "Called without holding cluster service lock!";
+
+        localMember = new MemberImpl(member.getAddress(), member.getVersion(), true, member.getUuid(),
+                member.getAttributes(), false, node.hazelcastInstance);
+        node.loggingService.setThisMember(localMember);
+        return localMember;
     }
 
     @Override

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/MembershipManager.java
@@ -250,11 +250,7 @@ public class MembershipManager {
             MemberImpl member = currentMemberMap.getMember(address);
 
             if (member != null && member.getUuid().equals(memberInfo.getUuid())) {
-                if (member.isLiteMember() && !memberInfo.isLiteMember()) {
-                    // lite member promoted
-                    logger.info(member + " is promoted to normal member.");
-                    member = createMember(memberInfo);
-                }
+                member = createNewMemberImplIfChanged(memberInfo, member);
                 members[memberIndex++] = member;
                 continue;
             }
@@ -267,7 +263,7 @@ public class MembershipManager {
                 removedMembers.add(member);
             }
 
-            member = createMember(memberInfo);
+            member = createMember(memberInfo, memberInfo.getAttributes());
             addedMembers.add(member);
 
             long now = clusterService.getClusterTime();
@@ -300,15 +296,28 @@ public class MembershipManager {
         clusterService.printMemberList();
     }
 
-    private MemberImpl createMember(MemberInfo memberInfo) {
+    private MemberImpl createNewMemberImplIfChanged(MemberInfo newMemberInfo, MemberImpl member) {
+        if (member.isLiteMember() && !newMemberInfo.isLiteMember()) {
+            // lite member promoted
+            logger.info(member + " is promoted to normal member.");
+            if (member.localMember()) {
+                member = clusterService.promoteAndGetLocalMember();
+            } else {
+                member = createMember(newMemberInfo, member.getAttributes());
+            }
+        }
+        return member;
+    }
+
+    private MemberImpl createMember(MemberInfo memberInfo, Map<String, Object> attributes) {
         Address address = memberInfo.getAddress();
         Address thisAddress = node.getThisAddress();
         String ipV6ScopeId = thisAddress.getScopeId();
         address.setScopeId(ipV6ScopeId);
         boolean localMember = thisAddress.equals(address);
 
-        return new MemberImpl(address, memberInfo.getVersion(), localMember, memberInfo.getUuid(),
-                memberInfo.getAttributes(), memberInfo.isLiteMember(), node.hazelcastInstance);
+        return new MemberImpl(address, memberInfo.getVersion(), localMember, memberInfo.getUuid(), attributes,
+                memberInfo.isLiteMember(), node.hazelcastInstance);
     }
 
     void setMembers(MemberMap memberMap) {
@@ -864,8 +873,12 @@ public class MembershipManager {
             MemberImpl[] members = memberMap.getMembers().toArray(new MemberImpl[0]);
             for (int i = 0; i < members.length; i++) {
                 if (member.equals(members[i])) {
-                    member = new MemberImpl(member.getAddress(), member.getVersion(), member.localMember(),
-                            member.getUuid(), member.getAttributes(), false, node.hazelcastInstance);
+                    if (member.localMember()) {
+                        member = clusterService.promoteAndGetLocalMember();
+                    } else {
+                        member = new MemberImpl(member.getAddress(), member.getVersion(), member.localMember(), member.getUuid(),
+                                member.getAttributes(), false, node.hazelcastInstance);
+                    }
                     members[i] = member;
                     break;
                 }

hazelcast/src/test/java/com/hazelcast/internal/cluster/impl/PromoteLiteMemberTest.java
@@ -60,6 +60,7 @@ import static com.hazelcast.test.PacketFiltersUtil.dropOperationsFrom;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
@@ -327,13 +328,61 @@ public class PromoteLiteMemberTest extends HazelcastTestSupport {
 
     @Test(expected = UnsupportedOperationException.class)
     // RU_COMPAT_WITH_3_8
-    public void liteMemberPromotion_notSupported_beforeV39()  {
+    public void liteMemberPromotion_notSupported_beforeV39() {
         System.setProperty(BuildInfoProvider.HAZELCAST_INTERNAL_OVERRIDE_VERSION, Version.of(3, 8).toString());
         TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory();
 
         HazelcastInstance hz = factory.newHazelcastInstance(new Config().setLiteMember(true));
 
         hz.getCluster().promoteLocalLiteMember();
+    }
+
+    @Test
+    public void masterMemberAttributes_arePreserved_afterPromotion() throws Exception {
+        memberAttributes_arePreserved_afterPromotion(true);
+    }
+
+    @Test
+    public void normalMemberAttributes_arePreserved_afterPromotion() throws Exception {
+        memberAttributes_arePreserved_afterPromotion(false);
+    }
+
+    private void memberAttributes_arePreserved_afterPromotion(boolean isMaster) throws Exception {
+        final String attribute1 = "attr1";
+        final String attribute2 = "attr2";
+        final String attributeValue = "value";
+
+        TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory();
+        HazelcastInstance[] instances = new HazelcastInstance[2];
+        instances[0] = factory.newHazelcastInstance(new Config().setLiteMember(isMaster));
+        instances[1] = factory.newHazelcastInstance(new Config().setLiteMember(!isMaster));
+
+        HazelcastInstance hz = instances[isMaster ? 0 : 1];
+
+        // Get local member and SET attribute BEFORE promotion
+        Member localMember = hz.getCluster().getLocalMember();
+        localMember.setStringAttribute(attribute1, attributeValue);
+        assertEquals(attributeValue, localMember.getStringAttribute(attribute1));
+
+        // Promote local Lite member
+        hz.getCluster().promoteLocalLiteMember();
+
+        // Get local member and SET attribute AFTER promotion
+        localMember = hz.getCluster().getLocalMember();
+        localMember.setStringAttribute(attribute2, attributeValue);
+
+        // Check attributes from localMember
+        assertEquals(attributeValue, localMember.getStringAttribute(attribute1));
+        assertEquals(attributeValue, localMember.getStringAttribute(attribute2));
+
+        // Check attributes from member list
+        for (Member member : hz.getCluster().getMembers()) {
+            if (member.localMember()) {
+                assertEquals(attributeValue, member.getStringAttribute(attribute1));
+                assertEquals(attributeValue, member.getStringAttribute(attribute2));
+                break;
+            }
+        }
     }
 
     private void assertPromotionInvocationStarted(HazelcastInstance instance) {

commit 8b9abb66ac72306361e4b38571dbb237d51a6e8f

Fix member attribute change invocations
The same operation instance cannot be used in multiple invocations
concurrently.

This bug is introduced by https://github.com/hazelcast/hazelcast/pull/11850

hazelcast/src/main/java/com/hazelcast/instance/MemberImpl.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.instance;
 
+import com.hazelcast.cluster.MemberAttributeOperationType;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.core.Member;
@@ -30,6 +31,7 @@ import com.hazelcast.spi.OperationService;
 import com.hazelcast.spi.annotation.PrivateApi;
 import com.hazelcast.spi.impl.NodeEngineImpl;
 import com.hazelcast.util.ExceptionUtil;
+import com.hazelcast.util.function.Supplier;
 import com.hazelcast.version.MemberVersion;
 
 import java.util.Map;
@@ -194,8 +196,7 @@ public final class MemberImpl extends AbstractMember implements Member, Hazelcas
         }
 
         if (instance != null) {
-            MemberAttributeChangedOp op = new MemberAttributeChangedOp(REMOVE, key, null);
-            invokeOnAllMembers(op);
+            invokeOnAllMembers(new MemberAttributeOperationSupplier(REMOVE, key, null));
         }
     }
 
@@ -224,22 +225,19 @@ public final class MemberImpl extends AbstractMember implements Member, Hazelcas
         }
 
         if (instance != null) {
-            MemberAttributeChangedOp op = new MemberAttributeChangedOp(PUT, key, value);
-            invokeOnAllMembers(op);
+            invokeOnAllMembers(new MemberAttributeOperationSupplier(PUT, key, value));
         }
     }
 
-    private void invokeOnAllMembers(Operation operation) {
+    private void invokeOnAllMembers(Supplier<Operation> operationSupplier) {
         NodeEngineImpl nodeEngine = instance.node.nodeEngine;
         OperationService os = nodeEngine.getOperationService();
-        String uuid = nodeEngine.getLocalMember().getUuid();
-        operation.setCallerUuid(uuid).setNodeEngine(nodeEngine);
         try {
             for (Member member : nodeEngine.getClusterService().getMembers()) {
                 if (!member.localMember()) {
-                    os.invokeOnTarget(ClusterServiceImpl.SERVICE_NAME, operation, member.getAddress());
+                    os.invokeOnTarget(ClusterServiceImpl.SERVICE_NAME, operationSupplier.get(), member.getAddress());
                 } else {
-                    os.execute(operation);
+                    os.execute(operationSupplier.get());
                 }
             }
         } catch (Throwable t) {
@@ -254,5 +252,25 @@ public final class MemberImpl extends AbstractMember implements Member, Hazelcas
     @Override
     public int getId() {
         return ClusterDataSerializerHook.MEMBER;
+    }
+
+    private class MemberAttributeOperationSupplier implements Supplier<Operation> {
+        private final MemberAttributeOperationType operationType;
+        private final String key;
+        private final Object value;
+
+        MemberAttributeOperationSupplier(MemberAttributeOperationType operationType, String key, Object value) {
+            this.operationType = operationType;
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public Operation get() {
+            NodeEngineImpl nodeEngine = instance.node.nodeEngine;
+            String uuid = nodeEngine.getLocalMember().getUuid();
+            return new MemberAttributeChangedOp(operationType, key, value)
+                    .setCallerUuid(uuid).setNodeEngine(nodeEngine);
+        }
     }
 }