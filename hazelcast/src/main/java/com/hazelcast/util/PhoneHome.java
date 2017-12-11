0aecf8fcfc61f98411d6eca530f9182b1eed21ed
hasancelik <hasan@hazelcast.com>
2017-12-11 11:28:24 +0300

    fixed url typo

--- hazelcast/src/main/java/com/hazelcast/util/PhoneHome.java
+++ hazelcast/src/main/java/com/hazelcast/util/PhoneHome.java
@@ -68,7 +68,7 @@ public final class PhoneHome {
     private static final int H_INTERVAL = 300;
     private static final int J_INTERVAL = 600;
 
-    private static final String BASE_PHONE_HOME_URL = "http://127.0.0.1:8080/ping";
+    private static final String BASE_PHONE_HOME_URL = "http://phonehome.hazelcast.com/ping";
     private static final int CONNECTION_TIMEOUT_MILLIS = 3000;
     private static final String FALSE = "false";
 
