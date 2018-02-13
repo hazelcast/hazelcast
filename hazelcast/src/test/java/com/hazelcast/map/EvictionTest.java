24edd18fc63644a494a75811337307b7f78635c3
Sergey Sitnikov <serge.sitnikov@gmail.com>
2018-02-13 23:48:46 +0700

    Remove unused import

--- hazelcast/src/test/java/com/hazelcast/map/EvictionTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/EvictionTest.java
@@ -51,7 +51,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
