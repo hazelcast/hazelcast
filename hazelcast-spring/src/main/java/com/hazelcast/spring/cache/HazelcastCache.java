131015e0c294fbe41f31322812496daf284039d4
Hasan Çelik <hasan@hazelcast.com>
2018-02-15 11:16:59 +0300

    return narrow return type at spring HazelcastCache#getNativeCache (#12342)

--- hazelcast-spring/src/main/java/com/hazelcast/spring/cache/HazelcastCache.java
+++ hazelcast-spring/src/main/java/com/hazelcast/spring/cache/HazelcastCache.java
@@ -56,7 +56,7 @@ public class HazelcastCache implements Cache {
     }
 
     @Override
-    public Object getNativeCache() {
+    public IMap<Object, Object> getNativeCache() {
         return map;
     }
 
