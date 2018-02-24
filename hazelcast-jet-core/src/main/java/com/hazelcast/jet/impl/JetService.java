c8a078ee312ada2cecd24b310e03c06f5c36098a
Viliam Durina <viliam-durina@users.noreply.github.com>
2018-02-24 14:25:40 +0100

    Update copyright to 2018 (#739)

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/JetService.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/JetService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -114,7 +114,7 @@ public class JetService
                 "\to---o o---o   o   o-o   |     o     o---o o---o   |          | o-o     |  \n" +
                 "\t|   | |   |  /    |     |     |     |   |     |   |      \\   | |       |  \n" +
                 "\to   o o   o o---o o---o o---o o---o o   o o---o   o       o--o o---o   o   ");
-        logger.info("Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.");
+        logger.info("Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.");
     }
 
     @Override
--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/TopologicalSorter.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/TopologicalSorter.java
@@ -1,6 +1,5 @@
 /*
- * Original work Copyright (c) 2016 The Apache Software Foundation
- * Modified work Copyright (c) 2008-2017, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
