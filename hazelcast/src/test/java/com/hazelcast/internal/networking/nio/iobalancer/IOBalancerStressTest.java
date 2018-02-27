849ad81821ab37c694926374070f109a7e0abe9e
pveentjer <alarmnummer@gmail.com>
2018-02-27 12:13:18 +0200

    Renaming in IOBalancer
    The balancer it talking in selectors, handlers, events etc.
    This is old naming that didn't get updated.
    
    But these days it is owner, pipelines and load.

--- hazelcast/src/test/java/com/hazelcast/internal/networking/nio/iobalancer/IOBalancerStressTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/networking/nio/iobalancer/IOBalancerStressTest.java
@@ -58,7 +58,7 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
     }
 
     @Test
-    public void testEachConnectionUseDifferentSelectorEventually() {
+    public void testEachConnectionUseDifferentOwnerEventually() {
         Config config = new Config()
                 .setProperty(GroupProperty.IO_BALANCER_INTERVAL_SECONDS.getName(), "1")
                 .setProperty(GroupProperty.IO_THREAD_COUNT.getName(), "2");
@@ -83,13 +83,13 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
     private void assertBalanced(HazelcastInstance hz) {
         TcpIpConnectionManager connectionManager = (TcpIpConnectionManager) getConnectionManager(hz);
 
-        Map<NioThread, Set<MigratablePipeline>> handlersPerSelector = getHandlersPerSelector(connectionManager);
+        Map<NioThread, Set<MigratablePipeline>> pipelinesPerOwner = getPipelinesPerOwner(connectionManager);
 
         try {
-            for (Map.Entry<NioThread, Set<MigratablePipeline>> entry : handlersPerSelector.entrySet()) {
-                NioThread selector = entry.getKey();
-                Set<MigratablePipeline> handlers = entry.getValue();
-                assertBalanced(selector, handlers);
+            for (Map.Entry<NioThread, Set<MigratablePipeline>> entry : pipelinesPerOwner.entrySet()) {
+                NioThread owner = entry.getKey();
+                Set<MigratablePipeline> pipelines = entry.getValue();
+                assertBalanced(owner, pipelines);
             }
         } catch (AssertionError e) {
             // if something fails, we want to see the debug output
@@ -98,79 +98,79 @@ public class IOBalancerStressTest extends HazelcastTestSupport {
         }
     }
 
-    private Map<NioThread, Set<MigratablePipeline>> getHandlersPerSelector(TcpIpConnectionManager connectionManager) {
-        Map<NioThread, Set<MigratablePipeline>> handlersPerSelector = new HashMap<NioThread, Set<MigratablePipeline>>();
+    private Map<NioThread, Set<MigratablePipeline>> getPipelinesPerOwner(TcpIpConnectionManager connectionManager) {
+        Map<NioThread, Set<MigratablePipeline>> pipelinesPerOwner = new HashMap<NioThread, Set<MigratablePipeline>>();
         for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
             NioChannel channel = (NioChannel) connection.getChannel();
-            add(handlersPerSelector, channel.inboundPipeline());
-            add(handlersPerSelector, channel.outboundPipeline());
+            add(pipelinesPerOwner, channel.inboundPipeline());
+            add(pipelinesPerOwner, channel.outboundPipeline());
         }
-        return handlersPerSelector;
+        return pipelinesPerOwner;
     }
 
-    private void add(Map<NioThread, Set<MigratablePipeline>> handlersPerSelector, MigratablePipeline handler) {
-        Set<MigratablePipeline> handlers = handlersPerSelector.get(handler.owner());
-        if (handlers == null) {
-            handlers = new HashSet<MigratablePipeline>();
-            handlersPerSelector.put(handler.owner(), handlers);
+    private void add(Map<NioThread, Set<MigratablePipeline>> pipelinesPerOwner, MigratablePipeline owner) {
+        Set<MigratablePipeline> pipelines = pipelinesPerOwner.get(owner.owner());
+        if (pipelines == null) {
+            pipelines = new HashSet<MigratablePipeline>();
+            pipelinesPerOwner.put(owner.owner(), pipelines);
         }
-        handlers.add(handler);
+        pipelines.add(owner);
     }
 
     /**
-     * A selector is balanced if:
+     * A owner is balanced if:
      * <ul>
      * <li>it has 1 active handler (so a high event count)</li>
      * <li>potentially 1 dead handler (duplicate connection), so event count should be low</li>
      * </ul>
      */
-    private void assertBalanced(NioThread selector, Set<MigratablePipeline> handlers) {
-        assertTrue("no handlers were found for selector:" + selector, handlers.size() > 0);
-        assertTrue("too many handlers were found for selector:" + selector, handlers.size() <= 2);
+    private void assertBalanced(NioThread owner, Set<MigratablePipeline> pipelines) {
+        assertTrue("no pipelines were found for owner:" + owner, pipelines.size() > 0);
+        assertTrue("too many pipelines were found for owner:" + owner, pipelines.size() <= 2);
 
-        Iterator<MigratablePipeline> iterator = handlers.iterator();
-        MigratablePipeline activeHandler = iterator.next();
-        if (handlers.size() == 2) {
-            MigratablePipeline deadHandler = iterator.next();
-            if (activeHandler.load() < deadHandler.load()) {
-                MigratablePipeline tmp = deadHandler;
-                deadHandler = activeHandler;
-                activeHandler = tmp;
+        Iterator<MigratablePipeline> iterator = pipelines.iterator();
+        MigratablePipeline activePipeline = iterator.next();
+        if (pipelines.size() == 2) {
+            MigratablePipeline deadPipeline = iterator.next();
+            if (activePipeline.load() < deadPipeline.load()) {
+                MigratablePipeline tmp = deadPipeline;
+                deadPipeline = activePipeline;
+                activePipeline = tmp;
             }
 
             // the maximum number of events seen on the dead connection is 3. 10 should be save to assume the
             // connection is dead.
             assertTrue("at most 10 event should have been received, number of events received:"
-                    + deadHandler.load(), deadHandler.load() < 10);
+                    + deadPipeline.load(), deadPipeline.load() < 10);
         }
 
-        assertTrue("activeHandlerEvent count should be at least 1000, but was:" + activeHandler.load(),
-                activeHandler.load() > 1000);
+        assertTrue("activeHandlerEvent count should be at least 1000, but was:" + activePipeline.load(),
+                activePipeline.load() > 1000);
     }
 
     private String debug(TcpIpConnectionManager connectionManager) {
         NioEventLoopGroup threadingModel = (NioEventLoopGroup) connectionManager.getEventLoopGroup();
 
         StringBuilder sb = new StringBuilder();
-        sb.append("in selectors\n");
+        sb.append("in owners\n");
         for (NioThread in : threadingModel.getInputThreads()) {
             sb.append(in).append(": ").append(in.getEventCount()).append("\n");
 
             for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
                 NioInboundPipeline socketReader = ((NioChannel) connection.getChannel()).inboundPipeline();
                 if (socketReader.owner() == in) {
-                    sb.append("\t").append(socketReader).append(" eventCount:").append(socketReader.load()).append("\n");
+                    sb.append("\t").append(socketReader).append(" load:").append(socketReader.load()).append("\n");
                 }
             }
         }
-        sb.append("out selectors\n");
+        sb.append("out owners\n");
         for (NioThread in : threadingModel.getOutputThreads()) {
             sb.append(in).append(": ").append(in.getEventCount()).append("\n");
 
             for (TcpIpConnection connection : connectionManager.getActiveConnections()) {
                 NioOutboundPipeline socketWriter = ((NioChannel) connection.getChannel()).outboundPipeline();
                 if (socketWriter.owner() == in) {
-                    sb.append("\t").append(socketWriter).append(" eventCount:").append(socketWriter.load()).append("\n");
+                    sb.append("\t").append(socketWriter).append(" load:").append(socketWriter.load()).append("\n");
                 }
             }
         }
