1f1b6cdf1f3bb1c6049c13fd44d4739387628a3d
Zoltan Baranyi <blazember@gmail.com>
2018-03-27 22:48:24 +0200

    Make EvictionTest resilient against hiccups
    Eviction tests assert that after some time an entry added with TTL is still available in the map. If these tests experience a longer hiccup, an oversleep may happen causing that the entry is no longer available in the map, which fails the test. This commit introduces `HazelcastTestSupport#sleepAtMostSeconds`, which addresses hiding these hiccups.
    
    Fixes #12376

--- hazelcast/src/test/java/com/hazelcast/test/HazelcastTestSupport.java
+++ hazelcast/src/test/java/com/hazelcast/test/HazelcastTestSupport.java
@@ -37,6 +37,8 @@ import com.hazelcast.internal.partition.InternalPartition;
 import com.hazelcast.internal.partition.InternalPartitionService;
 import com.hazelcast.internal.partition.impl.PartitionServiceState;
 import com.hazelcast.internal.serialization.InternalSerializationService;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
 import com.hazelcast.map.impl.operation.DefaultMapOperationProvider;
 import com.hazelcast.map.impl.operation.MapOperation;
 import com.hazelcast.map.impl.operation.MapOperationProvider;
@@ -360,6 +362,42 @@ public abstract class HazelcastTestSupport {
             TimeUnit.SECONDS.sleep(seconds);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Sleeps for time that is left until referenceTime + seconds. If no
+     * time is left until that time, a warning is logged and no sleep
+     * will happen.
+     * <p>
+     * Opposed to the language provided sleep constructs, this method
+     * does not guarantee minimum sleep time as it assumes that no time
+     * elapsed since {@code referenceTime} which is never true.
+     * <p>
+     * This method can be useful in hiding occasional hiccups of the
+     * execution environment in tests which are sensitive to
+     * oversleeping. Tests like the ones verify TTL, lease time behavior
+     * are typical examples for that.
+     *
+     * @param referenceTime the time in milliseconds since which the
+     *                      sleep end time should be calculated
+     * @param seconds       desired sleep duration in seconds
+     */
+    public static void sleepAtMostSeconds(long referenceTime, int seconds) {
+        long now = System.currentTimeMillis();
+        long sleepEnd = referenceTime + SECONDS.toMillis(seconds);
+        long sleepTime = sleepEnd - now;
+
+        if (sleepTime > 0) {
+            try {
+                MILLISECONDS.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        } else {
+            ILogger logger = Logger.getLogger(HazelcastTestSupport.class);
+            long absSleepTime = Math.abs(sleepTime);
+            logger.warning("There is no time left to sleep. We are beyond the desired end of sleep by " + absSleepTime + "ms");
         }
     }
 
