79546494b1c8df9323552a057aa94afbec1f89aa
Sergey Sitnikov <serge.sitnikov@gmail.com>
2018-04-12 19:26:00 +0700

    Fix a race condition while destroying proxies
    Before this change there was a race condition while destroying proxies
    in a cluster-wide way. Multiple threads were performing destruction of
    the same proxy instance simultaneously and sometimes none of them
    were able to succeed at the local resources cleanup.
    
    The idea of the fix is to unify the proxy destruction logic in
    ProxyManager to make sure the cluster-wide destruction for a certain
    proxy instance is done only once.
    
    Fixes: https://github.com/hazelcast/hazelcast/issues/12679

--- hazelcast-client/src/main/java/com/hazelcast/client/spi/ProxyManager.java
+++ hazelcast-client/src/main/java/com/hazelcast/client/spi/ProxyManager.java
@@ -324,14 +324,52 @@ public final class ProxyManager {
     }
 
     /**
+     * Destroys the given proxy in a cluster-wide way.
+     * <p>
+     * Upon successful completion the proxy is unregistered in this proxy
+     * manager, all local resources associated with the proxy are released and
+     * a distributed object destruction operation is issued to the cluster.
+     * <p>
+     * If the given proxy instance is not registered in this proxy manager, the
+     * proxy instance is considered stale. In this case, this stale instance is
+     * a subject to a local-only destruction and its registered counterpart, if
+     * there is any, is a subject to a cluster-wide destruction.
+     *
+     * @param proxy the proxy to destroy.
+     */
+    public void destroyProxy(ClientProxy proxy) {
+        ObjectNamespace objectNamespace = new DistributedObjectNamespace(proxy.getServiceName(),
+                proxy.getDistributedObjectName());
+        ClientProxyFuture registeredProxyFuture = proxies.remove(objectNamespace);
+        ClientProxy registeredProxy = registeredProxyFuture == null ? null : registeredProxyFuture.get();
+
+        try {
+            if (registeredProxy != null) {
+                try {
+                    registeredProxy.destroyLocally();
+                } finally {
+                    registeredProxy.destroyRemotely();
+                }
+            }
+        } finally {
+            if (proxy != registeredProxy) {
+                // The given proxy is stale and was already destroyed, but the caller
+                // may have allocated local resources in the context of this stale proxy
+                // instance after it was destroyed, so we have to cleanup it locally one
+                // more time to make sure there are no leaking local resources.
+                proxy.destroyLocally();
+            }
+        }
+    }
+
+    /**
      * Locally destroys the proxy identified by the given service and object ID.
      * <p>
      * Upon successful completion the proxy is unregistered in this proxy
      * manager and all local resources associated with the proxy are released.
-     * See {@link ClientProxy#destroyLocally} for more details.
      *
      * @param service the service associated with the proxy.
-     * @param id      the ID of the object.
+     * @param id      the ID of the object to destroy the proxy of.
      */
     public void destroyProxyLocally(String service, String id) {
         ObjectNamespace objectNamespace = new DistributedObjectNamespace(service, id);
@@ -348,11 +386,6 @@ public final class ProxyManager {
         }
         return factory.create(id)
                 .setContext(context);
-    }
-
-    public void removeProxy(String service, String id) {
-        final ObjectNamespace ns = new DistributedObjectNamespace(service, id);
-        proxies.remove(ns);
     }
 
     private void initializeWithRetry(ClientProxy clientProxy) throws Exception {
