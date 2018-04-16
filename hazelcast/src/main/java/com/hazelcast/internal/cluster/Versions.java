3c6baf91e736128709dd185642f7e3b0d8e760c3
donnerbart <github@thunderphreak.de>
2018-04-16 15:04:33 +0200

    Added 3.11 to Versions
     * added V3_11 to Versions (which fixes AbstractDistributedObjectTest)
     * fixed VersionedInCurrentVersionTest to ignore download errors

--- hazelcast/src/main/java/com/hazelcast/internal/cluster/Versions.java
+++ hazelcast/src/main/java/com/hazelcast/internal/cluster/Versions.java
@@ -35,8 +35,13 @@ public final class Versions {
      */
     public static final Version V3_10 = Version.of(3, 10);
 
-    public static final Version PREVIOUS_CLUSTER_VERSION = V3_9;
-    public static final Version CURRENT_CLUSTER_VERSION = V3_10;
+    /**
+     * Represents cluster version 3.11
+     */
+    public static final Version V3_11 = Version.of(3, 11);
+
+    public static final Version PREVIOUS_CLUSTER_VERSION = V3_10;
+    public static final Version CURRENT_CLUSTER_VERSION = V3_11;
 
     private Versions() {
     }
