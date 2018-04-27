56dd06f989e32e753c3d98a3b47faae259eec1c6
Jiří Holuša <jiri@hazelcast.com>
2018-04-27 09:10:54 +0200

    Fix XMLConfigBuilderTest.testConfigurationWithFileName test on Zing JDK6

--- hazelcast/src/test/java/com/hazelcast/config/XMLConfigBuilderTest.java
+++ hazelcast/src/test/java/com/hazelcast/config/XMLConfigBuilderTest.java
@@ -88,6 +88,8 @@ public class XMLConfigBuilderTest extends HazelcastTestSupport {
 
     @Test
     public void testConfigurationWithFileName() throws Exception {
+        assumeThatNotZingJDK6(); // https://github.com/hazelcast/hazelcast/issues/9044
+
         File file = createTempFile("foo", "bar");
         file.deleteOnExit();
 
