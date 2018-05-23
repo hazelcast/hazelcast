a395ea8fbeae526107a666436efb9957b3679dc2
Josef Cacek <josef.cacek@gmail.com>
2018-05-23 21:58:58 +0200

    Update XML schema version to 3.11

--- hazelcast/src/main/java/com/hazelcast/config/ConfigXmlGenerator.java
+++ hazelcast/src/main/java/com/hazelcast/config/ConfigXmlGenerator.java
@@ -112,7 +112,7 @@ public class ConfigXmlGenerator {
                 .append("xmlns=\"http://www.hazelcast.com/schema/config\"\n")
                 .append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n")
                 .append("xsi:schemaLocation=\"http://www.hazelcast.com/schema/config ")
-                .append("http://www.hazelcast.com/schema/config/hazelcast-config-3.10.xsd\">");
+                .append("http://www.hazelcast.com/schema/config/hazelcast-config-3.11.xsd\">");
         gen.open("group")
                 .node("name", config.getGroupConfig().getName())
                 .node("password", getOrMaskValue(config.getGroupConfig().getPassword()))
