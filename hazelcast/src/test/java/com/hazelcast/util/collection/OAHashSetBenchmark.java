a71b4dd4eb47869b0c051ffe5de2a9ce0d9524db
Zoltan Baranyi <blazember@gmail.com>
2018-06-13 11:08:00 +0200

    Add open-addressing hash set implementation
    See the javadoc of OAHashSet for the details.
    This Set implementation is based on Int2ObjectHashMap with some tailoring.

---
+++ hazelcast/src/test/java/com/hazelcast/util/collection/OAHashSetBenchmark.java
@@ -0,0 +1,300 @@
+/*
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.util.collection;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Benchmark for OAHashSet using Java HashSet implementation as a baseline
+ * <p>
+ * This benchmark puts the heap under pressure and addition benchmarks
+ * may produce different amount of garbage, depending on the performance
+ * of OAHashSet and Java HashSet. This makes comparing with the baseline
+ * pointless, since the more frequent GC executions distort the results.
+ * Due to this, we want to keep GC away from the measurements by either
+ * <ul>
+ * <li> increasing the heap size big enough to avoid GC
+ * <li> or make JMH to run GC between iterations via {@link OptionsBuilder#shouldDoGC}
+ * at the cost of significantly increased benchmark execution time.
+ * See {@link #main(String[])}
+ * </ul>
+ * The GC-free benchmark execution can be verified by adding {@link GCProfiler}
+ * in {@link #main(String[])}
+ */
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(value = 5, jvmArgsAppend = {"-Xms8G", "-Xmx8G"})
+@State(Scope.Benchmark)
+public class OAHashSetBenchmark {
+    // cost factors for simulating expensive equals() and hashcode() methods
+    // higher cost factors make the impact of the equals() and hashCode() methods more significant to the
+    // benchmark results and highlight the effectiveness of the cached hashes in the OAHash implementation
+    // on the other hand, higher cost factors make the test biased in favor of the OAHashSet implementation
+    private static final int EQUALS_COST_FACTOR = 10;
+    private static final int HASHCODE_COST_FACTOR = 10;
+
+    private static final int HUGE_PRIME = 982455757;
+
+    private int anInt = HUGE_PRIME;
+
+    @Benchmark
+    public boolean add_java_HashSet(JavaHashSetContext context) {
+        return context.set.add(getAnEntry());
+    }
+
+    @Benchmark
+    public boolean add_hz_OAHashSet(OAHashSetContext context) {
+        return context.set.add(getAnEntry());
+    }
+
+    @Benchmark
+    public boolean add_hz_OAHashSet_withHash(OAHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry();
+        return context.set.add(anEntry.getHash(), anEntry);
+    }
+
+    @Benchmark
+    public boolean contains_java_HashSet(PreFilledJavaHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry(context.size);
+        return context.set.contains(anEntry);
+    }
+
+    @Benchmark
+    public boolean contains_hz_OAHashSet(PreFilledOAHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry(context.size);
+        return context.set.contains(anEntry);
+    }
+
+    @Benchmark
+    public boolean contains_hz_OAHashSet_withHash(PreFilledOAHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry(context.size);
+        return context.set.contains(anEntry.getHash(), anEntry);
+    }
+
+    @Benchmark
+    public boolean remove_java_HashSet(PreFilledJavaHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry(context.size);
+        return context.set.remove(anEntry);
+    }
+
+    @Benchmark
+    public boolean remove_hz_OAHashSet(PreFilledOAHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry(context.size);
+        return context.set.remove(anEntry);
+    }
+
+    @Benchmark
+    public boolean remove_hz_OAHashSet_withHash(PreFilledOAHashSetContext context) {
+        ObjectWithExpensiveHashCodeAndEquals anEntry = getAnEntry(context.size);
+        return context.set.remove(anEntry.getHash(), anEntry);
+    }
+
+    private int getAnInt() {
+        anInt += HUGE_PRIME;
+        return anInt;
+    }
+
+    private int getAnInt(int max) {
+        return getAnInt() % max;
+    }
+
+    private ObjectWithExpensiveHashCodeAndEquals getAnEntry() {
+        return new ObjectWithExpensiveHashCodeAndEquals(getAnInt(), EQUALS_COST_FACTOR, HASHCODE_COST_FACTOR);
+    }
+
+    private ObjectWithExpensiveHashCodeAndEquals getAnEntry(int maxInt) {
+        return new ObjectWithExpensiveHashCodeAndEquals(getAnInt(maxInt), EQUALS_COST_FACTOR, HASHCODE_COST_FACTOR);
+    }
+
+    public static abstract class BenchmarkContext {
+        @Param({"10", "100", "1000", "10000", "100000", "1000000"})
+        protected int size;
+
+        protected int getValue(int base) {
+            return base * HUGE_PRIME % size;
+        }
+    }
+
+    @State(Scope.Benchmark)
+    public static class OAHashSetContext extends BenchmarkContext {
+        protected OAHashSet<ObjectWithExpensiveHashCodeAndEquals> set;
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            set = new OAHashSet<ObjectWithExpensiveHashCodeAndEquals>(size);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() {
+            //            System.out.println("OAHashSet.size: " + set.size());
+            //            System.out.println("OAHashSet.capacity: " + set.capacity());
+            //            System.out.println("OAHashSet.footprint: " + set.footprint());
+        }
+
+    }
+
+    @State(Scope.Benchmark)
+    public static class PreFilledOAHashSetContext extends OAHashSetContext {
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            super.setUp();
+            for (int i = 0; set.size() < size * 0.8F; i++) {
+                int value = getValue(i);
+                set.add(value, new ObjectWithExpensiveHashCodeAndEquals(value, EQUALS_COST_FACTOR, HASHCODE_COST_FACTOR));
+            }
+            //            System.out.println("Prefilled to size " + set.size() + " for initial capacity " + size);
+        }
+    }
+
+    @State(Scope.Benchmark)
+    public static class JavaHashSetContext extends BenchmarkContext {
+        protected HashSet<ObjectWithExpensiveHashCodeAndEquals> set;
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            set = new HashSet<ObjectWithExpensiveHashCodeAndEquals>(size);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() {
+            //            System.out.println("HashSet.size: " + set.size());
+        }
+    }
+
+    @State(Scope.Benchmark)
+    public static class PreFilledJavaHashSetContext extends JavaHashSetContext {
+
+        @Setup(Level.Trial)
+        public void setUp() {
+            super.setUp();
+            for (int i = 0; set.size() < size * 0.8F; i++) {
+                int value = getValue(i);
+                set.add(new ObjectWithExpensiveHashCodeAndEquals(value, EQUALS_COST_FACTOR, HASHCODE_COST_FACTOR));
+            }
+            //            System.out.println("Prefilled to size " + set.size() + " for initial capacity " + size);
+        }
+    }
+
+    private static class ObjectWithExpensiveHashCodeAndEquals {
+        private final int hash;
+        private final Integer value;
+        private final String valueAsStr;
+        private final String sqrtAsStr;
+        private final int equalsCostFactor;
+        private final int hashCodeCostFactor;
+
+        private ObjectWithExpensiveHashCodeAndEquals(int value, int equalsCostFactor, int hashCodeCostFactor) {
+            this.value = value;
+            this.valueAsStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit" + value;
+            this.sqrtAsStr = Double.toString(Math.sqrt(value));
+            this.equalsCostFactor = equalsCostFactor;
+            this.hashCodeCostFactor = hashCodeCostFactor;
+            this.hash = hashCodeInternal();
+        }
+
+        private int getHash() {
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            boolean equals = false;
+
+            for (int i = 0; i < equalsCostFactor; i++) {
+                equals = equalsInternal(o);
+            }
+
+            return equals;
+        }
+
+        private boolean equalsInternal(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            ObjectWithExpensiveHashCodeAndEquals that = (ObjectWithExpensiveHashCodeAndEquals) o;
+
+            if (value != null ? !value.equals(that.value) : that.value != null) {
+                return false;
+            }
+            if (valueAsStr != null ? !valueAsStr.equals(that.valueAsStr) : that.valueAsStr != null) {
+                return false;
+            }
+            return sqrtAsStr != null ? sqrtAsStr.equals(that.sqrtAsStr) : that.sqrtAsStr == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int hashCode = 0;
+
+            for (int i = 0; i < hashCodeCostFactor; i++) {
+                hashCode = hashCodeInternal();
+            }
+
+            return hashCode;
+        }
+
+        private int hashCodeInternal() {
+            int result = value != null ? value.hashCode() : 0;
+            result = 31 * result + (valueAsStr != null ? valueAsStr.hashCode() : 0);
+            result = 31 * result + (sqrtAsStr != null ? sqrtAsStr.hashCode() : 0);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(OAHashSetBenchmark.class.getSimpleName())
+                .resultFormat(ResultFormatType.JSON)
+                .addProfiler(GCProfiler.class)
+                //                .addProfiler(LinuxPerfProfiler.class)
+                //                .addProfiler(HotspotMemoryProfiler.class)
+                //                .shouldDoGC(true)
+                //                .verbosity(VerboseMode.SILENT)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
