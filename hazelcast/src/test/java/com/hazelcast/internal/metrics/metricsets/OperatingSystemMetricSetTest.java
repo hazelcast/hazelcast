91604323f07e5cfe373884f41757582046dbb085
Josef Cacek <josef.cacek@gmail.com>
2018-08-08 20:10:56 +0200

    Fix OS metrics on IBM Java 8 and Java 9+.

--- hazelcast/src/test/java/com/hazelcast/internal/metrics/metricsets/OperatingSystemMetricSetTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/metrics/metricsets/OperatingSystemMetricSetTest.java
@@ -23,6 +23,8 @@ import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
+
+import org.junit.AssumptionViolatedException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -62,8 +64,16 @@ public class OperatingSystemMetricSetTest extends HazelcastTestSupport {
     }
 
     @Test
-    public void testComSunManagementUnixOperatingSystem() {
-        assumeOperatingSystemMXBean("com.sun.management.UnixOperatingSystem");
+    public void testComSunManagementUnixOperatingSystemMXBean() {
+        assumeOperatingSystemMXBeanType("com.sun.management.UnixOperatingSystemMXBean");
+        assertContainsSensor("os.maxFileDescriptorCount");
+        assertContainsSensor("os.openFileDescriptorCount");
+
+    }
+
+    @Test
+    public void testComSunManagementOperatingSystemMXBean() {
+        assumeOperatingSystemMXBeanType("com.sun.management.OperatingSystemMXBean");
 
         assertContainsSensor("os.committedVirtualMemorySize");
         assertContainsSensor("os.freePhysicalMemorySize");
@@ -71,8 +81,6 @@ public class OperatingSystemMetricSetTest extends HazelcastTestSupport {
         assertContainsSensor("os.processCpuTime");
         assertContainsSensor("os.totalPhysicalMemorySize");
         assertContainsSensor("os.totalSwapSpaceSize");
-        assertContainsSensor("os.maxFileDescriptorCount");
-        assertContainsSensor("os.openFileDescriptorCount");
 
         assumeThatNoJDK6();
         // only available in JDK 7+
@@ -80,35 +88,19 @@ public class OperatingSystemMetricSetTest extends HazelcastTestSupport {
         assertContainsSensor("os.systemCpuLoad");
     }
 
-    @Test
-    public void testSunManagementOperatingSystemImpl() {
-        assumeOperatingSystemMXBean("sun.management.OperatingSystemImpl");
-
-        assertContainsSensor("os.committedVirtualMemorySize");
-        assertContainsSensor("os.freePhysicalMemorySize");
-        assertContainsSensor("os.freeSwapSpaceSize");
-        assertContainsSensor("os.processCpuTime");
-        assertContainsSensor("os.totalPhysicalMemorySize");
-        assertContainsSensor("os.totalSwapSpaceSize");
-        assertContainsSensor("os.processCpuLoad");
-        assertContainsSensor("os.systemCpuLoad");
-
-        assumeThatNoWindowsOS();
-        // not implemented on Windows
-        assertContainsSensor("os.maxFileDescriptorCount");
-        assertContainsSensor("os.openFileDescriptorCount");
-    }
-
     private void assertContainsSensor(String parameter) {
         boolean contains = metricsRegistry.getNames().contains(parameter);
         assertTrue("sensor: " + parameter + " is not found", contains);
     }
 
-    private void assumeOperatingSystemMXBean(String expected) {
+    private void assumeOperatingSystemMXBeanType(String expected) {
         OperatingSystemMXBean bean = ManagementFactory.getOperatingSystemMXBean();
-        String foundClass = bean.getClass().getName();
-
-        assumeTrue(foundClass + " is not usable", expected.equals(foundClass));
+        try {
+            Class<?> expectedInterface = Class.forName(expected);
+            assumeTrue(expectedInterface.isAssignableFrom(bean.getClass()));
+        } catch (ClassNotFoundException e) {
+            throw new AssumptionViolatedException("MXBean interface " + expected + " was not found");
+        }
     }
 
     @Test
@@ -142,13 +134,13 @@ public class OperatingSystemMetricSetTest extends HazelcastTestSupport {
         assertFalse(parameterExist);
     }
 
-    public class FakeOperatingSystemBean {
+    public static class FakeOperatingSystemBean {
 
-        double doubleMethod() {
+        public double doubleMethod() {
             return 10;
         }
 
-        long longMethod() {
+        public long longMethod() {
             return 10;
         }
     }
