a115775e8f6453e9b2c0d4a859bf1ff4b2e26440
Thomas Kountis <tkountis@gmail.com>
2018-08-23 14:33:41 +0100

    Fix broken unit test for LatestUpdate MergePolicy

--- hazelcast/src/test/java/com/hazelcast/map/MergePolicyTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/MergePolicyTest.java
@@ -84,11 +84,11 @@ public class MergePolicyTest extends HazelcastTestSupport {
         IMap<Object, Object> map2 = h2.getMap(mapName);
         map1.put("key1", "value");
         // prevent updating at the same time
-        sleepAtLeastMillis(1);
+        sleepAtLeastMillis(1000);
         map2.put("key1", "LatestUpdatedValue");
         map2.put("key2", "value2");
         // prevent updating at the same time
-        sleepAtLeastMillis(1);
+        sleepAtLeastMillis(1000);
         map1.put("key2", "LatestUpdatedValue2");
 
         // allow merge process to continue
