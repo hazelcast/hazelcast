b5b1ef72f95dfefcf6c893d6eca30dbeb9ffeb16
Zoltan Baranyi <blazember@gmail.com>
2018-08-24 14:28:35 +0200

    Add unit test for serialization

--- hazelcast/src/main/java/com/hazelcast/map/impl/operation/BaseRemoveOperation.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/operation/BaseRemoveOperation.java
@@ -17,10 +17,15 @@
 package com.hazelcast.map.impl.operation;
 
 import com.hazelcast.core.EntryEventType;
+import com.hazelcast.internal.cluster.Versions;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.Data;
 import com.hazelcast.spi.BackupAwareOperation;
 import com.hazelcast.spi.Operation;
 import com.hazelcast.spi.impl.MutatingOperation;
+
+import java.io.IOException;
 
 public abstract class BaseRemoveOperation extends LockAwareOperation
         implements BackupAwareOperation, MutatingOperation {
@@ -40,23 +45,6 @@ public abstract class BaseRemoveOperation extends LockAwareOperation
     }
 
     public BaseRemoveOperation() {
-    }
-
-    @Override
-    public void innerBeforeRun() throws Exception {
-        super.innerBeforeRun();
-        // RU_COMPAT_3_10
-        //
-        // we restore both the disableWanReplicationEvent and the ttl flags
-        // before the operation is getting executed
-        //
-        // this may happen if the operation was created by a 3.10.5+ member
-        // which carries over the disableWanReplicationEvent flag's value
-        // in the TTL field for wire format compatibility reasons
-        if ((ttl & BITMASK_TTL_DISABLE_WAN) == 0) {
-            disableWanReplicationEvent = true;
-            ttl ^= BITMASK_TTL_DISABLE_WAN;
-        }
     }
 
     @Override
@@ -96,5 +84,46 @@ public abstract class BaseRemoveOperation extends LockAwareOperation
     @Override
     public void onWaitExpire() {
         sendResponse(null);
+    }
+
+    @Override
+    protected void writeInternal(ObjectDataOutput out) throws IOException {
+        // RU_COMPAT_3_10
+        if (disableWanReplicationEvent && out.getVersion().isEqualTo(Versions.V3_10)) {
+
+            // disableWanReplicationEvent flag is not serialized in 3.10, which may
+            // lead to publishing remove WAN events by error, if the operation
+            // executes on a remote node. This may lead to republishing remove
+            // events to clusters that have already processed it, possibly causing
+            // data loss, if the removed entry has been added back since then.
+            //
+            // Serializing the field would break the compatibility, hence
+            // we encode its value into the TTL field, which is serialized
+            // but not used for remove operations.
+            //
+            // Note that this serialization has the side effect that the
+            // value of TTL changes, but it is acceptable since the field
+            // is not in use.
+            // This value change is done during serialization to keep
+            // clusters already on 3.11+ unaffected from this compatibility trick.
+            this.ttl ^= BITMASK_TTL_DISABLE_WAN;
+        }
+
+        super.writeInternal(out);
+    }
+
+    @Override
+    protected void readInternal(ObjectDataInput in) throws IOException {
+        super.readInternal(in);
+
+        // RU_COMPAT_3_10
+        if (in.getVersion().isEqualTo(Versions.V3_10)) {
+            // restore disableWanReplicationEvent flag
+            //
+            // this may happen if the operation was created by a 3.10.5+ member
+            // which carries over the disableWanReplicationEvent flag's value
+            // in the TTL field for wire format compatibility reasons
+            disableWanReplicationEvent |= (ttl & BITMASK_TTL_DISABLE_WAN) == 0;
+        }
     }
 }
