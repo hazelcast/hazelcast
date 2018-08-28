6cfa72f0e5e81e588757142529703898ba5f943f
Viliam Durina <viliam-durina@users.noreply.github.com>
2018-08-28 11:31:41 +0200

    Remove taking partitions from factory
    Will make some tests to fail, but should resolve after #13527 is merged

--- hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/operations/PartitionIteratingOperation.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/operations/PartitionIteratingOperation.java
@@ -178,15 +178,9 @@ public final class PartitionIteratingOperation extends Operation implements Iden
 
         private void executeOperations(PartitionAwareOperationFactory givenFactory) {
             final NodeEngine nodeEngine = getNodeEngine();
-
             final PartitionAwareOperationFactory factory = givenFactory.createFactoryOnRunner(nodeEngine);
-
-            int[] operationFactoryPartitions = factory.getPartitions();
-            partitions = operationFactoryPartitions == null ? partitions : operationFactoryPartitions;
-
             final OperationResponseHandler responseHandler = new OperationResponseHandlerImpl(partitions);
             final Object service = getServiceName() == null ? null : getService();
-
 
             PartitionTaskFactory f = new PartitionTaskFactory() {
                 @Override
