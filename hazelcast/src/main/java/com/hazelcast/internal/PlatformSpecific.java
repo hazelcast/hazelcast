cd462a10c8e2e8ba11f9f8816b98f47e3fbeb709
Vassilis Bekiaris <vbekiaris@gmail.com>
2019-02-13 10:49:07 +0200

    Adds doc for providing alternative JDK6/8 impls in PlatformSpecific

--- hazelcast/src/main/java/com/hazelcast/internal/PlatformSpecific.java
+++ hazelcast/src/main/java/com/hazelcast/internal/PlatformSpecific.java
@@ -18,6 +18,45 @@ package com.hazelcast.internal;
 
 /**
  * Utility class to construct instances depending on the runtime platform.
+ * Steps to introduce Java 6 / 8 alternative implementations:
+ * <ol>
+ *     <li>Define and use an interface in Hazelcast codebase</li>
+ *     <li>Implement the interface for Java 6</li>
+ *     <li>Implement the interface for Java 8+. Annotate this implementation with
+ *     {@link RequiresJdk8}: animal-sniffer will not scan the annotated class or methods
+ *     for Java 6 API compatibility, so use this annotation at the smallest possible scope
+ *     </li>
+ *     <li>Add a {@code createXYZ} public static method in {@code PlatformSpecific} utility
+ *     class</li>
+ *     <li>Use {@code PlatformSpecific.createXYZ} whenever you need a new instance</li>
+ * </ol>
+ *
+ * The implementation of a constructor utility method would look something like this example:
+ * <pre>
+ * {@code
+ * public static <S> MethodProbe createLongMethodProbe(Method method, Probe probe, int type) {
+ *   if (JavaVersion.isAtLeast(JAVA_1_8)) {
+ *     return new LongMethodProbeJdk8<S>(method, probe, type);
+ *   } else {
+ *     return new MethodProbe.LongMethodProbe<S>(method, probe, type);
+ *   }
+ * }
+ * }
+ * </pre>
+ *
+ * This should be tested in two separate tests, to ensure the proper instance is created
+ * depending on Java runtime version, like this:
+ * <pre>
+ *     {@code
+ *     @Test
+ *     public void testJdk8Probe() {
+ *       // ensure this test only runs on JDK 8+
+ *       assumeJdk8OrNewer();
+ *       MethodProbe jdk8Probe = PlatformSpecific.createLongMethodProbe(...);
+ *       assertTrue(jdk8Probe instanceof LongMethodProbeJdk8.class);
+ *     }
+ *     }
+ * </pre>
  */
 public final class PlatformSpecific {
 
