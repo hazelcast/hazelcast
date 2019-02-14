1c91811a175c15892333931f17b9bac2da8d942c
Emin Demirci <emin@hazelcast.com>
2019-02-14 14:26:28 +0300

    Added license key property to be injected by enterprise docker image (#1248)
    * Added license key property to be injected by enterprise docker image
    
    * Fixed indentation

--- hazelcast-jet-distribution/src/main/resources/bin/common.sh
+++ hazelcast-jet-distribution/src/main/resources/bin/common.sh
@@ -3,9 +3,9 @@ JET_HOME=`cd "$SCRIPT_DIR/.." >/dev/null; pwd`
 PID_FILE=$JET_HOME/bin/jet_instance.pid
 
 if [ $JAVA_HOME ]; then
-	JAVA=$JAVA_HOME/bin/java
+    JAVA=$JAVA_HOME/bin/java
 else
-	echo "JAVA_HOME environment variable not available."
+    echo "JAVA_HOME environment variable not available."
     JAVA=`which java 2>/dev/null`
 fi
 
@@ -22,20 +22,28 @@ fi
 # MAX_HEAP_SIZE=1G
 
 if [ "x$MIN_HEAP_SIZE" != "x" ]; then
-	JAVA_OPTS="$JAVA_OPTS -Xms${MIN_HEAP_SIZE}"
+    JAVA_OPTS="$JAVA_OPTS -Xms${MIN_HEAP_SIZE}"
 fi
 
 if [ "x$MAX_HEAP_SIZE" != "x" ]; then
-	JAVA_OPTS="$JAVA_OPTS -Xmx${MAX_HEAP_SIZE}"
+    JAVA_OPTS="$JAVA_OPTS -Xmx${MAX_HEAP_SIZE}"
 fi
 
-## add classpath and java opts entries comes from docker image if defined
+#### add classpath and java opts entries comes from docker image if defined
 if [ "x$CLASSPATH_DEFAULT" != "x" ]; then
     CLASSPATH="${CLASSPATH_DEFAULT}:${CLASSPATH}"
 fi
 
 if [ "x$JAVA_OPTS_DEFAULT" != "x" ]; then
     JAVA_OPTS="${JAVA_OPTS_DEFAULT} ${JAVA_OPTS}"
+fi
+
+if [ "x$JET_LICENSE_KEY" != "x" ]; then
+    JAVA_OPTS="$JAVA_OPTS -Dhazelcast.enterprise.license.key=${JET_LICENSE_KEY}"
+fi
+
+if [ "x$JET_LICENCE_KEY" != "x" ]; then
+    JAVA_OPTS="$JAVA_OPTS -Dhazelcast.enterprise.license.key=${JET_LICENCE_KEY}"
 fi
 
 CLASSPATH="$JET_HOME/lib/${hazelcast.jet.artifact}-${project.version}.jar:$CLASSPATH"
