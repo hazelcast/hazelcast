d538eee9e5ce8c1ab3d2b065f4121c7d15d247a1
Matko Medenjak <matko@hazelcast.com>
2019-02-19 11:37:52 +0100

    Checkstyle

--- hazelcast/src/test/java/com/hazelcast/query/impl/getters/JsonGetterContextCacheTest.java
+++ hazelcast/src/test/java/com/hazelcast/query/impl/getters/JsonGetterContextCacheTest.java
@@ -24,7 +24,6 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
