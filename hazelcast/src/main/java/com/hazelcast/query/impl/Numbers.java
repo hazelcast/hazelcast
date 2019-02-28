7af4ac5262aad13f66d670e6d902b14faa22dd12
Sergey Sitnikov <serge.sitnikov@gmail.com>
2019-02-28 15:59:31 +0700

    drop support for BigInteger and BigDecimal

--- hazelcast/src/main/java/com/hazelcast/query/impl/Numbers.java
+++ hazelcast/src/main/java/com/hazelcast/query/impl/Numbers.java
@@ -16,9 +16,6 @@
 
 package com.hazelcast.query.impl;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 public final class Numbers {
 
     private Numbers() {
@@ -40,10 +37,6 @@ public final class Numbers {
                 return lhs.longValue() == rhs.longValue();
             } else if (rhsClass == Byte.class) {
                 return lhs.longValue() == rhs.longValue();
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhs.longValue()).equals(rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhs.longValue()).equals(rhs);
             }
         } else if (lhsClass == Double.class) {
             if (rhsClass == Long.class) {
@@ -56,10 +49,6 @@ public final class Numbers {
                 return lhs.doubleValue() == rhs.doubleValue();
             } else if (rhsClass == Byte.class) {
                 return lhs.doubleValue() == rhs.doubleValue();
-            } else if (rhs instanceof BigInteger) {
-                return new BigDecimal(lhs.doubleValue()).equals(new BigDecimal((BigInteger) rhs));
-            } else if (rhs instanceof BigDecimal) {
-                return new BigDecimal(lhs.doubleValue()).equals(rhs);
             }
         } else if (lhsClass == Integer.class) {
             if (rhsClass == Long.class) {
@@ -72,10 +61,6 @@ public final class Numbers {
                 return lhs.intValue() == rhs.intValue();
             } else if (rhsClass == Byte.class) {
                 return lhs.intValue() == rhs.intValue();
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhs.intValue()).equals(rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhs.intValue()).equals(rhs);
             }
         } else if (lhsClass == Float.class) {
             if (rhsClass == Long.class) {
@@ -88,10 +73,6 @@ public final class Numbers {
                 return lhs.floatValue() == rhs.floatValue();
             } else if (rhsClass == Byte.class) {
                 return lhs.floatValue() == rhs.floatValue();
-            } else if (rhs instanceof BigInteger) {
-                return new BigDecimal(lhs.floatValue()).equals(new BigDecimal((BigInteger) rhs));
-            } else if (rhs instanceof BigDecimal) {
-                return new BigDecimal(lhs.floatValue()).equals(rhs);
             }
         } else if (lhsClass == Short.class) {
             if (rhsClass == Long.class) {
@@ -104,10 +85,6 @@ public final class Numbers {
                 return lhs.floatValue() == rhs.floatValue();
             } else if (rhsClass == Byte.class) {
                 return lhs.shortValue() == rhs.shortValue();
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhs.shortValue()).equals(rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhs.shortValue()).equals(rhs);
             }
         } else if (lhsClass == Byte.class) {
             if (rhsClass == Long.class) {
@@ -120,42 +97,6 @@ public final class Numbers {
                 return lhs.floatValue() == rhs.floatValue();
             } else if (rhsClass == Short.class) {
                 return lhs.shortValue() == rhs.shortValue();
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhs.shortValue()).equals(rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhs.shortValue()).equals(rhs);
-            }
-        } else if (lhs instanceof BigInteger) {
-            if (rhsClass == Long.class) {
-                return lhs.equals(BigInteger.valueOf(rhs.longValue()));
-            } else if (rhsClass == Double.class) {
-                return new BigDecimal((BigInteger) lhs).equals(new BigDecimal(rhs.doubleValue()));
-            } else if (rhsClass == Integer.class) {
-                return lhs.equals(BigInteger.valueOf(rhs.intValue()));
-            } else if (rhsClass == Float.class) {
-                return new BigDecimal((BigInteger) lhs).equals(new BigDecimal(rhs.floatValue()));
-            } else if (rhsClass == Short.class) {
-                return new BigDecimal((BigInteger) lhs).equals(BigDecimal.valueOf(rhs.shortValue()));
-            } else if (rhsClass == Byte.class) {
-                return new BigDecimal((BigInteger) lhs).equals(BigDecimal.valueOf(rhs.byteValue()));
-            } else if (rhs instanceof BigDecimal) {
-                return new BigDecimal((BigInteger) lhs).equals(rhs);
-            }
-        } else if (lhs instanceof BigDecimal) {
-            if (rhsClass == Long.class) {
-                return lhs.equals(BigDecimal.valueOf(rhs.longValue()));
-            } else if (rhsClass == Double.class) {
-                return lhs.equals(new BigDecimal(rhs.doubleValue()));
-            } else if (rhsClass == Integer.class) {
-                return lhs.equals(BigDecimal.valueOf(rhs.intValue()));
-            } else if (rhsClass == Float.class) {
-                return lhs.equals(new BigDecimal(rhs.floatValue()));
-            } else if (rhsClass == Short.class) {
-                return lhs.equals(BigDecimal.valueOf(rhs.shortValue()));
-            } else if (rhsClass == Byte.class) {
-                return lhs.equals(BigDecimal.valueOf(rhs.byteValue()));
-            } else if (rhs instanceof BigInteger) {
-                return lhs.equals(new BigDecimal((BigInteger) rhs));
             }
         }
 
@@ -184,10 +125,6 @@ public final class Numbers {
                 return compare(lhsNumber.longValue(), rhsNumber.longValue());
             } else if (rhsClass == Byte.class) {
                 return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhsNumber.longValue()).compareTo((BigInteger) rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhsNumber.longValue()).compareTo((BigDecimal) rhs);
             }
         } else if (lhsClass == Double.class) {
             if (rhsClass == Long.class) {
@@ -200,10 +137,6 @@ public final class Numbers {
                 return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
             } else if (rhsClass == Byte.class) {
                 return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhs instanceof BigInteger) {
-                return new BigDecimal(lhsNumber.doubleValue()).compareTo(new BigDecimal((BigInteger) rhs));
-            } else if (rhs instanceof BigDecimal) {
-                return new BigDecimal(lhsNumber.doubleValue()).compareTo((BigDecimal) rhs);
             }
         } else if (lhsClass == Integer.class) {
             if (rhsClass == Long.class) {
@@ -216,10 +149,6 @@ public final class Numbers {
                 return compare(lhsNumber.intValue(), rhsNumber.intValue());
             } else if (rhsClass == Byte.class) {
                 return compare(lhsNumber.intValue(), rhsNumber.intValue());
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhsNumber.intValue()).compareTo((BigInteger) rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhsNumber.intValue()).compareTo((BigDecimal) rhs);
             }
         } else if (lhsClass == Float.class) {
             if (rhsClass == Long.class) {
@@ -232,10 +161,6 @@ public final class Numbers {
                 return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
             } else if (rhsClass == Byte.class) {
                 return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhs instanceof BigInteger) {
-                return new BigDecimal(lhsNumber.floatValue()).compareTo(new BigDecimal((BigInteger) rhs));
-            } else if (rhs instanceof BigDecimal) {
-                return new BigDecimal(lhsNumber.floatValue()).compareTo((BigDecimal) rhs);
             }
         } else if (lhsClass == Short.class) {
             if (rhsClass == Long.class) {
@@ -248,10 +173,6 @@ public final class Numbers {
                 return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
             } else if (rhsClass == Byte.class) {
                 return compare(lhsNumber.shortValue(), rhsNumber.shortValue());
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhsNumber.shortValue()).compareTo((BigInteger) rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhsNumber.shortValue()).compareTo((BigDecimal) rhs);
             }
         } else if (lhsClass == Byte.class) {
             if (rhsClass == Long.class) {
@@ -264,42 +185,6 @@ public final class Numbers {
                 return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
             } else if (rhsClass == Short.class) {
                 return compare(lhsNumber.shortValue(), rhsNumber.shortValue());
-            } else if (rhs instanceof BigInteger) {
-                return BigInteger.valueOf(lhsNumber.shortValue()).compareTo((BigInteger) rhs);
-            } else if (rhs instanceof BigDecimal) {
-                return BigDecimal.valueOf(lhsNumber.shortValue()).compareTo((BigDecimal) rhs);
-            }
-        } else if (lhs instanceof BigInteger) {
-            if (rhsClass == Long.class) {
-                return lhs.compareTo(BigInteger.valueOf(rhsNumber.longValue()));
-            } else if (rhsClass == Double.class) {
-                return new BigDecimal((BigInteger) lhs).compareTo(new BigDecimal(rhsNumber.doubleValue()));
-            } else if (rhsClass == Integer.class) {
-                return lhs.compareTo(BigInteger.valueOf(rhsNumber.intValue()));
-            } else if (rhsClass == Float.class) {
-                return new BigDecimal((BigInteger) lhs).compareTo(new BigDecimal(rhsNumber.floatValue()));
-            } else if (rhsClass == Short.class) {
-                return new BigDecimal((BigInteger) lhs).compareTo(BigDecimal.valueOf(rhsNumber.shortValue()));
-            } else if (rhsClass == Byte.class) {
-                return new BigDecimal((BigInteger) lhs).compareTo(BigDecimal.valueOf(rhsNumber.byteValue()));
-            } else if (rhs instanceof BigDecimal) {
-                return new BigDecimal((BigInteger) lhs).compareTo((BigDecimal) rhs);
-            }
-        } else if (lhs instanceof BigDecimal) {
-            if (rhsClass == Long.class) {
-                return lhs.compareTo(BigDecimal.valueOf(rhsNumber.longValue()));
-            } else if (rhsClass == Double.class) {
-                return lhs.compareTo(new BigDecimal(rhsNumber.doubleValue()));
-            } else if (rhsClass == Integer.class) {
-                return lhs.compareTo(BigDecimal.valueOf(rhsNumber.intValue()));
-            } else if (rhsClass == Float.class) {
-                return lhs.compareTo(new BigDecimal(rhsNumber.floatValue()));
-            } else if (rhsClass == Short.class) {
-                return lhs.compareTo(BigDecimal.valueOf(rhsNumber.shortValue()));
-            } else if (rhsClass == Byte.class) {
-                return lhs.compareTo(BigDecimal.valueOf(rhsNumber.byteValue()));
-            } else if (rhs instanceof BigInteger) {
-                return lhs.compareTo(new BigDecimal((BigInteger) rhs));
             }
         }
 
--- hazelcast/src/main/java/com/hazelcast/query/impl/TypeConverters.java
+++ hazelcast/src/main/java/com/hazelcast/query/impl/TypeConverters.java
@@ -29,8 +29,6 @@ import static com.hazelcast.query.impl.AbstractIndex.NULL;
 
 public final class TypeConverters {
 
-    // FIXME: BigDecimal/Integer conversions
-
     public static final TypeConverter BIG_INTEGER_CONVERTER = new BigIntegerConverter();
     public static final TypeConverter BIG_DECIMAL_CONVERTER = new BigDecimalConverter();
     public static final TypeConverter DOUBLE_CONVERTER = new DoubleConverter();
