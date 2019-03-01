8853100d448f9039ab18a4c21f7f95a6d2fe00de
Sergey Sitnikov <serge.sitnikov@gmail.com>
2019-03-01 13:14:21 +0700

    simplify comparison logic

--- hazelcast/src/main/java/com/hazelcast/query/impl/Numbers.java
+++ hazelcast/src/main/java/com/hazelcast/query/impl/Numbers.java
@@ -58,93 +58,23 @@ public final class Numbers {
         Number lhsNumber = (Number) lhs;
         Number rhsNumber = (Number) rhs;
 
-        if (lhsClass == Long.class) {
-            if (rhsClass == Double.class) {
+        if (isDoubleRepresentable(lhsClass)) {
+            if (isDoubleRepresentable(rhsClass)) {
                 return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Integer.class) {
-                return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            } else if (rhsClass == Float.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Short.class) {
-                return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            } else if (rhsClass == Byte.class) {
-                return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            }
-        } else if (lhsClass == Double.class) {
-            if (rhsClass == Long.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Integer.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Float.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Short.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Byte.class) {
+            } else if (isLongRepresentable(rhsClass)) {
+                // TODO invent a better method of comparing longs and doubles?
                 return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
             }
-        } else if (lhsClass == Integer.class) {
-            if (rhsClass == Long.class) {
+        } else if (isLongRepresentable(lhsClass)) {
+            if (isDoubleRepresentable(rhsClass)) {
+                // TODO invent a better method of comparing longs and doubles?
+                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
+            } else if (isLongRepresentable(rhsClass)) {
                 return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            } else if (rhsClass == Double.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Float.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Short.class) {
-                return compare(lhsNumber.intValue(), rhsNumber.intValue());
-            } else if (rhsClass == Byte.class) {
-                return compare(lhsNumber.intValue(), rhsNumber.intValue());
-            }
-        } else if (lhsClass == Float.class) {
-            if (rhsClass == Long.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Double.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Integer.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Short.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Byte.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            }
-        } else if (lhsClass == Short.class) {
-            if (rhsClass == Long.class) {
-                return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            } else if (rhsClass == Double.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Integer.class) {
-                return compare(lhsNumber.intValue(), rhsNumber.intValue());
-            } else if (rhsClass == Float.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Byte.class) {
-                return compare(lhsNumber.shortValue(), rhsNumber.shortValue());
-            }
-        } else if (lhsClass == Byte.class) {
-            if (rhsClass == Long.class) {
-                return compare(lhsNumber.longValue(), rhsNumber.longValue());
-            } else if (rhsClass == Double.class) {
-                return Double.compare(lhsNumber.doubleValue(), rhsNumber.doubleValue());
-            } else if (rhsClass == Integer.class) {
-                return compare(lhsNumber.intValue(), rhsNumber.intValue());
-            } else if (rhsClass == Float.class) {
-                return Float.compare(lhsNumber.floatValue(), rhsNumber.floatValue());
-            } else if (rhsClass == Short.class) {
-                return compare(lhsNumber.shortValue(), rhsNumber.shortValue());
             }
         }
 
         return lhs.compareTo(rhs);
-    }
-
-    private static int compare(long x, long y) {
-        return x < y ? -1 : (x == y ? 0 : 1);
-    }
-
-    private static int compare(int x, int y) {
-        return x < y ? -1 : (x == y ? 0 : 1);
-    }
-
-    private static int compare(short x, short y) {
-        return x < y ? -1 : (x == y ? 0 : 1);
     }
 
     private static boolean isDoubleRepresentable(Class clazz) {
@@ -153,6 +83,10 @@ public final class Numbers {
 
     private static boolean isLongRepresentable(Class clazz) {
         return clazz == Long.class || clazz == Integer.class || clazz == Short.class || clazz == Byte.class;
+    }
+
+    private static int compare(long x, long y) {
+        return x < y ? -1 : (x == y ? 0 : 1);
     }
 
 }
