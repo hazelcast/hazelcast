602b01bb0e97823ff36191a01c99e3a36c280803
Can Gencer <cgencer@gmail.com>
2019-03-13 16:18:56 +0100

    Make configureJetService synchronized (#1322)

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/Jet.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/Jet.java
@@ -119,7 +119,7 @@ public final class Jet {
         return new JetClientInstanceImpl(((HazelcastClientProxy) client).client);
     }
 
-    private static void configureJetService(JetConfig jetConfig) {
+    private static synchronized void configureJetService(JetConfig jetConfig) {
         Config hzConfig = jetConfig.getHazelcastConfig();
         if (!(hzConfig.getConfigPatternMatcher() instanceof MatchingPointConfigPatternMatcher)) {
             throw new UnsupportedOperationException("Custom config pattern matcher is not supported in Jet");
