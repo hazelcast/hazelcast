feed4ac674ec3098600e7e078ed90886ba5a13c5
Sergey Sitnikov <serge.sitnikov@gmail.com>
2019-03-13 19:22:36 +0700

    document numbers and comparables

--- hazelcast/src/main/java/com/hazelcast/query/impl/Comparables.java
+++ hazelcast/src/main/java/com/hazelcast/query/impl/Comparables.java
@@ -18,8 +18,16 @@ package com.hazelcast.query.impl;
 
 import java.util.Comparator;
 
+/**
+ * Provides utilities which compare and canonicalize {@link Comparable}
+ * instances.
+ */
 public final class Comparables {
 
+    /**
+     * Provides the same comparision logic as {@link #compare} does, but in a
+     * form of a {@link Comparator}.
+     */
     public static final Comparator<Comparable> COMPARATOR = new Comparator<Comparable>() {
 
         @Override
@@ -32,6 +40,18 @@ public final class Comparables {
     private Comparables() {
     }
 
+    /**
+     * Checks the provided {@link Comparable} instances for equality.
+     * <p>
+     * Special numeric comparison logic is used for {@link Double}, {@link Long},
+     * {@link Float}, {@link Integer}, {@link Short} and {@link Byte}. See
+     * {@link Numbers#equal(Number, Number)} for more details.
+     *
+     * @param lhs the left-hand side {@link Comparable}. Can't be {@code null}.
+     * @param rhs the right-hand side {@link Comparable}. May be {@code null}.
+     * @return {@code true} if the provided comparables are equal, {@code false}
+     * otherwise.
+     */
     public static boolean equal(Comparable lhs, Comparable rhs) {
         assert lhs != null;
 
@@ -50,6 +70,19 @@ public final class Comparables {
         return lhs.equals(rhs);
     }
 
+    /**
+     * Compares the provided {@link Comparable} instances.
+     * <p>
+     * Special numeric comparison logic is used for {@link Double}, {@link Long},
+     * {@link Float}, {@link Integer}, {@link Short} and {@link Byte}. See
+     * {@link Numbers#compare(Comparable, Comparable)} for more details.
+     *
+     * @param lhs the left-hand side {@link Comparable}. Can't be {@code null}.
+     * @param rhs the right-hand side {@link Comparable}. Can't be {@code null}.
+     * @return a negative integer, zero, or a positive integer as the left-hand
+     * side {@link Comparable} is less than, equal to, or greater than the
+     * right-hand side {@link Comparable}.
+     */
     @SuppressWarnings("unchecked")
     public static int compare(Comparable lhs, Comparable rhs) {
         assert lhs != null;
@@ -66,6 +99,20 @@ public final class Comparables {
         return lhs.compareTo(rhs);
     }
 
+    /**
+     * Canonicalizes the given {@link Comparable} value for the purpose of a
+     * hash-based lookup.
+     * <p>
+     * Special numeric canonicalization logic is used for {@link Double},
+     * {@link Long}, {@link Float}, {@link Integer}, {@link Short} and
+     * {@link Byte}. See {@link Numbers#canonicalizeForHashLookup} for more
+     * details.
+     *
+     * @param value the {@link Comparable} to canonicalize.
+     * @return a canonical representation of the given {@link Comparable} or
+     * the original {@link Comparable} if there is no special canonical
+     * representation for it.
+     */
     public static Comparable canonicalizeForHashLookup(Comparable value) {
         if (value instanceof Number) {
             return Numbers.canonicalizeForHashLookup(value);
