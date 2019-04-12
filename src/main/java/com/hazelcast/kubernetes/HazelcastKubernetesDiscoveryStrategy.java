b2ddacdd2dfcded2804a4df372bf195d81992840
Rafał Leszko <rafal@hazelcast.com>
2019-04-12 12:00:39 +0200

    Add validation for input properties, refactor, add unit tests (#137)

--- src/main/java/com/hazelcast/kubernetes/HazelcastKubernetesDiscoveryStrategy.java
+++ src/main/java/com/hazelcast/kubernetes/HazelcastKubernetesDiscoveryStrategy.java
@@ -16,48 +16,20 @@
 
 package com.hazelcast.kubernetes;
 
-import com.hazelcast.config.properties.PropertyDefinition;
+import com.hazelcast.kubernetes.KubernetesConfig.DiscoveryMode;
 import com.hazelcast.logging.ILogger;
-import com.hazelcast.nio.IOUtil;
 import com.hazelcast.spi.discovery.AbstractDiscoveryStrategy;
 import com.hazelcast.spi.discovery.DiscoveryNode;
 import com.hazelcast.spi.partitiongroup.PartitionGroupMetaData;
-import com.hazelcast.util.StringUtil;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_API_RETIRES;
-import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_API_TOKEN;
-import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_CA_CERTIFICATE;
-import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_MASTER_URL;
-import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_SYSTEM_PREFIX;
-import static com.hazelcast.kubernetes.KubernetesProperties.NAMESPACE;
-import static com.hazelcast.kubernetes.KubernetesProperties.RESOLVE_NOT_READY_ADDRESSES;
-import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_DNS;
-import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_DNS_TIMEOUT;
-import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_LABEL_NAME;
-import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_LABEL_VALUE;
-import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_NAME;
-import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_PORT;
-
 final class HazelcastKubernetesDiscoveryStrategy
         extends AbstractDiscoveryStrategy {
-
-    private static final String DEFAULT_MASTER_URL = "https://kubernetes.default.svc";
-    private static final int DEFAULT_SERVICE_DNS_TIMEOUT_SECONDS = 5;
-    private static final int DEFAULT_KUBERNETES_API_RETRIES = 3;
-
-    private final String namespace;
-
     private final KubernetesClient client;
     private final EndpointResolver endpointResolver;
 
@@ -66,68 +38,25 @@ final class HazelcastKubernetesDiscoveryStrategy
     HazelcastKubernetesDiscoveryStrategy(ILogger logger, Map<String, Comparable> properties) {
         super(logger, properties);
 
-        String serviceDns = getOrNull(properties, KUBERNETES_SYSTEM_PREFIX, SERVICE_DNS);
-        int serviceDnsTimeout
-                = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, SERVICE_DNS_TIMEOUT, DEFAULT_SERVICE_DNS_TIMEOUT_SECONDS);
-        String serviceName = getOrNull(properties, KUBERNETES_SYSTEM_PREFIX, SERVICE_NAME);
-        int port = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, SERVICE_PORT, 0);
-        String serviceLabel = getOrNull(properties, KUBERNETES_SYSTEM_PREFIX, SERVICE_LABEL_NAME);
-        String serviceLabelValue = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, SERVICE_LABEL_VALUE, "true");
-        namespace = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, NAMESPACE, getNamespaceOrDefault());
-        Boolean resolveNotReadyAddresses = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, RESOLVE_NOT_READY_ADDRESSES, false);
-        int kubernetesApiRetries
-                = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, KUBERNETES_API_RETIRES, DEFAULT_KUBERNETES_API_RETRIES);
-        String kubernetesMaster = getOrDefault(properties, KUBERNETES_SYSTEM_PREFIX, KUBERNETES_MASTER_URL, DEFAULT_MASTER_URL);
-        String apiToken = getApiToken(properties);
-        String caCertificate = caCertificate(properties);
+        KubernetesConfig config = new KubernetesConfig(properties);
+        logger.info(config.toString());
 
-        logger.info("Kubernetes Discovery properties: { "
-                + "service-dns: " + serviceDns + ", "
-                + "service-dns-timeout: " + serviceDnsTimeout + ", "
-                + "service-name: " + serviceName + ", "
-                + "service-port: " + port + ", "
-                + "service-label: " + serviceLabel + ", "
-                + "service-label-value: " + serviceLabelValue + ", "
-                + "namespace: " + namespace + ", "
-                + "resolve-not-ready-addresses: " + resolveNotReadyAddresses + ", "
-                + "kubernetes-api-retries: " + kubernetesApiRetries + ", "
-                + "kubernetes-master: " + kubernetesMaster + "}");
+        client = buildKubernetesClient(config);
 
-        client = buildKubernetesClient(namespace, kubernetesMaster, apiToken, caCertificate, kubernetesApiRetries);
-        if (serviceDns != null) {
-            endpointResolver = new DnsEndpointResolver(logger, serviceDns, port, serviceDnsTimeout);
+        if (DiscoveryMode.DNS_LOOKUP.equals(config.getMode())) {
+            endpointResolver = new DnsEndpointResolver(logger, config.getServiceDns(), config.getServicePort(),
+                    config.getServiceDnsTimeout());
         } else {
-            endpointResolver = new KubernetesApiEndpointResolver(logger, serviceName, port, serviceLabel, serviceLabelValue,
-                    resolveNotReadyAddresses, client);
+            endpointResolver = new KubernetesApiEndpointResolver(logger, config.getServiceName(), config.getServicePort(),
+                    config.getServiceLabelName(), config.getServiceLabelValue(), config.isResolveNotReadyAddresses(), client);
         }
-        logger.info("Kubernetes Discovery activated resolver: " + endpointResolver.getClass().getSimpleName());
+
+        logger.info("Kubernetes Discovery activated with mode: " + config.getMode().name());
     }
 
-    private String getApiToken(Map<String, Comparable> properties) {
-        String apiToken = getOrNull(properties, KUBERNETES_SYSTEM_PREFIX, KUBERNETES_API_TOKEN);
-        if (apiToken == null) {
-            apiToken = readAccountToken();
-        }
-        return apiToken;
-    }
-
-    private String caCertificate(Map<String, Comparable> properties) {
-        String caCertificate = getOrNull(properties, KUBERNETES_SYSTEM_PREFIX, KUBERNETES_CA_CERTIFICATE);
-        if (caCertificate == null) {
-            caCertificate = readCaCertificate();
-        }
-        return caCertificate;
-    }
-
-    private String getNamespaceOrDefault() {
-        String namespace = System.getenv("KUBERNETES_NAMESPACE");
-        if (namespace == null) {
-            namespace = System.getenv("OPENSHIFT_BUILD_NAMESPACE");
-            if (namespace == null) {
-                namespace = "default";
-            }
-        }
-        return namespace;
+    private static KubernetesClient buildKubernetesClient(KubernetesConfig config) {
+        return new KubernetesClient(config.getNamespace(), config.getKubernetesMasterUrl(), config.getKubernetesApiToken(),
+                config.getKubernetesCaCertificate(), config.getKubernetesApiRetries());
     }
 
     public void start() {
@@ -172,60 +101,6 @@ final class HazelcastKubernetesDiscoveryStrategy
         endpointResolver.destroy();
     }
 
-    protected <T extends Comparable> T getOrNull(Map<String, Comparable> properties, String prefix, PropertyDefinition property) {
-        return getOrDefault(properties, prefix, property, null);
-    }
-
-    protected <T extends Comparable> T getOrDefault(Map<String, Comparable> properties, String prefix,
-                                                    PropertyDefinition property, T defaultValue) {
-        if (property == null) {
-            return defaultValue;
-        }
-
-        Comparable value = readProperty(prefix, property);
-        if (value == null) {
-            value = properties.get(property.key());
-        }
-
-        if (value == null) {
-            return defaultValue;
-        }
-
-        return (T) value;
-    }
-
-    private Comparable readProperty(String prefix, PropertyDefinition property) {
-        if (prefix != null) {
-            String p = getProperty(prefix, property);
-            String v = System.getProperty(p);
-            if (StringUtil.isNullOrEmpty(v)) {
-                v = System.getenv(p);
-                if (StringUtil.isNullOrEmpty(v)) {
-                    v = System.getenv(cIdentifierLike(p));
-                }
-            }
-
-            if (!StringUtil.isNullOrEmpty(v)) {
-                return property.typeConverter().convert(v);
-            }
-        }
-        return null;
-    }
-
-    private String cIdentifierLike(String property) {
-        property = property.toUpperCase();
-        property = property.replace(".", "_");
-        return property.replace("-", "_");
-    }
-
-    private String getProperty(String prefix, PropertyDefinition property) {
-        StringBuilder sb = new StringBuilder(prefix);
-        if (prefix.charAt(prefix.length() - 1) != '.') {
-            sb.append('.');
-        }
-        return sb.append(property.key()).toString();
-    }
-
     abstract static class EndpointResolver {
         protected final ILogger logger;
 
@@ -251,36 +126,6 @@ final class HazelcastKubernetesDiscoveryStrategy
                 logger.warning("Address '" + address + "' could not be resolved");
             }
             return null;
-        }
-    }
-
-    private KubernetesClient buildKubernetesClient(String namespace, String kubernetesMaster, String accessToken,
-                                                   String caCertificate, int retries) {
-        return new KubernetesClient(namespace, kubernetesMaster, accessToken, caCertificate, retries);
-    }
-
-    @SuppressFBWarnings("DMI_HARDCODED_ABSOLUTE_FILENAME")
-    private static String readAccountToken() {
-        return readFileContents("/var/run/secrets/kubernetes.io/serviceaccount/token");
-    }
-
-    @SuppressFBWarnings("DMI_HARDCODED_ABSOLUTE_FILENAME")
-    private static String readCaCertificate() {
-        return readFileContents("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt");
-    }
-
-    protected static String readFileContents(String tokenFile) {
-        InputStream is = null;
-        try {
-            File file = new File(tokenFile);
-            byte[] data = new byte[(int) file.length()];
-            is = new FileInputStream(file);
-            is.read(data);
-            return new String(data, "UTF-8");
-        } catch (IOException e) {
-            throw new RuntimeException("Could not get token file", e);
-        } finally {
-            IOUtil.closeResource(is);
         }
     }
 }
---
+++ src/test/java/com/hazelcast/kubernetes/KubernetesConfigTest.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.kubernetes;
+
+import com.hazelcast.config.InvalidConfigurationException;
+import com.hazelcast.nio.IOUtil;
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.hazelcast.kubernetes.KubernetesConfig.DiscoveryMode;
+import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_API_RETIRES;
+import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_API_TOKEN;
+import static com.hazelcast.kubernetes.KubernetesProperties.KUBERNETES_CA_CERTIFICATE;
+import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_DNS;
+import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_DNS_TIMEOUT;
+import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_LABEL_NAME;
+import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_LABEL_VALUE;
+import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_NAME;
+import static com.hazelcast.kubernetes.KubernetesProperties.SERVICE_PORT;
+import static org.junit.Assert.assertEquals;
+
+public class KubernetesConfigTest {
+    private static final String TEST_API_TOKEN = "api-token";
+    private static final String TEST_CA_CERTIFICATE = "ca-certificate";
+
+    @Test
+    public void dnsLookupMode() {
+        // given
+        String serviceDns = "hazelcast.default.svc.cluster.local";
+        int serviceDnsTimeout = 10;
+        int servicePort = 5703;
+
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_DNS.key(), serviceDns);
+        properties.put(SERVICE_DNS_TIMEOUT.key(), serviceDnsTimeout);
+        properties.put(SERVICE_PORT.key(), servicePort);
+
+        // when
+        KubernetesConfig config = new KubernetesConfig(properties);
+
+        // then
+        assertEquals(DiscoveryMode.DNS_LOOKUP, config.getMode());
+        assertEquals(serviceDns, config.getServiceDns());
+        assertEquals(serviceDnsTimeout, config.getServiceDnsTimeout());
+        assertEquals(servicePort, config.getServicePort());
+    }
+
+    @Test
+    public void kubernetesApiModeDefault() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+
+        // when
+        KubernetesConfig config = new KubernetesConfig(properties);
+
+        // then
+        assertEquals(DiscoveryMode.KUBERNETES_API, config.getMode());
+        assertEquals("default", config.getNamespace());
+        assertEquals(false, config.isResolveNotReadyAddresses());
+        assertEquals(TEST_API_TOKEN, config.getKubernetesApiToken());
+        assertEquals(TEST_CA_CERTIFICATE, config.getKubernetesCaCertificate());
+    }
+
+    @Test
+    public void kubernetesApiModeServiceName() {
+        // given
+        String serviceName = "service-name";
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_NAME.key(), serviceName);
+
+        // when
+        KubernetesConfig config = new KubernetesConfig(properties);
+
+        // then
+        assertEquals(DiscoveryMode.KUBERNETES_API, config.getMode());
+        assertEquals(serviceName, config.getServiceName());
+    }
+
+    @Test
+    public void kubernetesApiModeServiceLabel() {
+        // given
+        String serviceLabelName = "service-label-name";
+        String serviceLabelValue = "service-label-value";
+        Map<String, Comparable> properties = createProperties();
+        properties.put(KubernetesProperties.SERVICE_LABEL_NAME.key(), serviceLabelName);
+        properties.put(SERVICE_LABEL_VALUE.key(), serviceLabelValue);
+
+        // when
+        KubernetesConfig config = new KubernetesConfig(properties);
+
+        // then
+        assertEquals(DiscoveryMode.KUBERNETES_API, config.getMode());
+        assertEquals(serviceLabelName, config.getServiceLabelName());
+        assertEquals(serviceLabelValue, config.getServiceLabelValue());
+    }
+
+    @Test(expected = InvalidConfigurationException.class)
+    public void invalidConfigurationBothModesConfigured() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_NAME.key(), "service-name");
+        properties.put(SERVICE_DNS.key(), "service-dns");
+
+        // when
+        new KubernetesConfig(properties);
+
+        // then
+        // throws exception
+    }
+
+    @Test(expected = InvalidConfigurationException.class)
+    public void invalidConfigurationBothModesConfiguredServiceLabel() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_LABEL_NAME.key(), "service-label-name");
+        properties.put(SERVICE_LABEL_VALUE.key(), "service-label-value");
+        properties.put(SERVICE_DNS.key(), "service-dns");
+
+        // when
+        new KubernetesConfig(properties);
+
+        // then
+        // throws exception
+    }
+
+    @Test(expected = InvalidConfigurationException.class)
+    public void invalidConfigurationBothServiceNameAndLabel() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_NAME.key(), "service-name");
+        properties.put(SERVICE_LABEL_NAME.key(), "service-label-name");
+        properties.put(SERVICE_LABEL_VALUE.key(), "service-label-value");
+
+        // when
+        new KubernetesConfig(properties);
+
+        // then
+        // throws exception
+    }
+
+    @Test(expected = InvalidConfigurationException.class)
+    public void invalidServiceDnsTimeout() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_DNS.key(), "service-dns");
+        properties.put(SERVICE_DNS_TIMEOUT.key(), -1);
+
+        // when
+        new KubernetesConfig(properties);
+
+        // then
+        // throws exception
+    }
+
+    @Test(expected = InvalidConfigurationException.class)
+    public void invalidKubernetesApiRetries() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+        properties.put(KUBERNETES_API_RETIRES.key(), -1);
+
+        // when
+        new KubernetesConfig(properties);
+
+        // then
+        // throws exception
+    }
+
+    @Test(expected = InvalidConfigurationException.class)
+    public void invalidServicePort() {
+        // given
+        Map<String, Comparable> properties = createProperties();
+        properties.put(SERVICE_PORT.key(), -1);
+
+        // when
+        new KubernetesConfig(properties);
+
+        // then
+        // throws exception
+    }
+
+    private static Map<String, Comparable> createProperties() {
+        Map<String, Comparable> properties = new HashMap<String, Comparable>();
+        // Predefined test properties
+        properties.put(KUBERNETES_API_TOKEN.key(), TEST_API_TOKEN);
+        properties.put(KUBERNETES_CA_CERTIFICATE.key(), TEST_CA_CERTIFICATE);
+        return properties;
+    }
+
+    @Test
+    public void readFileContents()
+            throws IOException {
+        String expectedContents = "Hello, world!\nThis is a test with Unicode ✓.";
+        String testFile = createTestFile(expectedContents);
+        String actualContents = KubernetesConfig.readFileContents(testFile);
+        assertEquals(expectedContents, actualContents);
+    }
+
+    private static String createTestFile(String expectedContents)
+            throws IOException {
+        File temp = File.createTempFile("test", ".tmp");
+        temp.deleteOnExit();
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(temp), Charset.forName("UTF-8")));
+            bufferedWriter.write(expectedContents);
+        } finally {
+            IOUtil.closeResource(bufferedWriter);
+        }
+        return temp.getAbsolutePath();
+    }
+}
