1c71b1c05f104dc4c8a43ff1c224e92160dc44f0
Carsten Hammer <carsten.hammer@t-online.de>
2019-04-18 17:57:17 +0200

    Change formatting to hazelcast style (spaces instead tabs)
    See pull request #13278
    Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

--- hazelcast/src/test/java/com/hazelcast/map/impl/eviction/ExpirationManagerTimeoutTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/eviction/ExpirationManagerTimeoutTest.java
@@ -16,10 +16,19 @@
 
 package com.hazelcast.map.impl.eviction;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
 import com.hazelcast.config.Config;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MaxSizeConfig;
-import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.test.HazelcastSerialClassRunner;
@@ -27,95 +36,80 @@ import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-
-import java.util.concurrent.TimeUnit;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 @RunWith(HazelcastSerialClassRunner.class)
 @Category({ QuickTest.class, ParallelTest.class })
 public class ExpirationManagerTimeoutTest extends HazelcastTestSupport {
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Ignore("https://github.com/hazelcast/hazelcast/issues/13272")
+    @Test
+    public void afterShortExpirationEntryShouldBeAway() throws InterruptedException {
+        final String KEY = "key";
 
-	@Ignore("https://github.com/hazelcast/hazelcast/issues/13272")
-	@Test
-	public void afterShortExpirationEntryShouldBeAway() throws InterruptedException {
-		final String KEY = "key";
+        Config hConfig = new Config().setInstanceName("instance")
+                .addMapConfig(new MapConfig().setName("test")
+                        .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                        .setTimeToLiveSeconds(20));
+        final HazelcastInstance node = createHazelcastInstance(hConfig);
+        try {
+            IMap<String, String> map = node.getMap("test");
+            /**
+             * after 1 second entry should be evicted
+             */
+            map.put(KEY, "value", 1, TimeUnit.SECONDS);
+            /**
+             * short time after adding it to the map, all ok
+             */
+            map.lock(KEY);
+            Object object = map.get(KEY);
+            map.unlock(KEY);
+            assertNotNull(object);
 
-		Config hConfig = new Config().setInstanceName("instance")
-				.addMapConfig(new MapConfig().setName("test")
-						.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-						.setTimeToLiveSeconds(20));
-		final HazelcastInstance node = createHazelcastInstance(hConfig);
-		try {
-			IMap map = node.getMap("test");
-			/**
-			 * after 1 second entry should be evicted
-			 */
-			map.put(KEY, "value", 1, TimeUnit.SECONDS);
-			/**
-			 * short time after adding it to the map, all ok
-			 */
-			map.lock(KEY);
-			Object object = map.get(KEY);
-			map.unlock(KEY);
-			assertNotNull(object);
+            Thread.sleep(1200);
+            /**
+             * More than one second after adding it, now it should be away
+             */
+            map.lock(KEY);
+            object = map.get(KEY);
+            map.unlock(KEY);
+            assertNull(object);
+        } finally {
+            node.shutdown();
+        }
+    }
 
-			Thread.sleep(1200);
-			/**
-			 * More than one second after adding it, now it should be away
-			 */
-			map.lock(KEY);
-			object = map.get(KEY);
-			map.unlock(KEY);
-			assertNull(object);
-		} finally {
-			node.shutdown();
-		}
-	}
+    @Ignore("https://github.com/hazelcast/hazelcast/issues/13272")
+    @Test
+    public void afterLongerExpirationEntryShouldBeAway() throws InterruptedException {
+        final String KEY = "key";
 
-	@Ignore("https://github.com/hazelcast/hazelcast/issues/13272")
-	@Test
-	public void afterLongerExpirationEntryShouldBeAway() throws InterruptedException {
-		final String KEY = "key";
-
-		Config hConfig = new Config().setInstanceName("instance")
-				.addMapConfig(new MapConfig().setName("test")
-						.setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-						.setTimeToLiveSeconds(20));
-		final HazelcastInstance node = createHazelcastInstance(hConfig);
-		try {
-			IMap map = node.getMap("test");
-			/**
-			 * after 1 second entry should be evicted
-			 */
-			map.put(KEY, "value", 1, TimeUnit.SECONDS);
-			/**
-			 * short time after adding it to the map, all ok
-			 */
-			map.lock(KEY);
-			Object object = map.get(KEY);
-			map.unlock(KEY);
-			assertNotNull(object);
-
-			Thread.sleep(3600);
-			/**
-			 * More than one second after adding it, now it should be away
-			 */
-			map.lock(KEY);
-			object = map.get(KEY);
-			map.unlock(KEY);
-			assertNull(object);
-		} finally {
-			node.shutdown();
-		}
-	}
+        Config hConfig = new Config().setInstanceName("instance")
+                .addMapConfig(new MapConfig().setName("test")
+                        .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                        .setTimeToLiveSeconds(20));
+        final HazelcastInstance node = createHazelcastInstance(hConfig);
+        try {
+            IMap<String, String> map = node.getMap("test");
+            /**
+             * after 1 second entry should be evicted
+             */
+            map.put(KEY, "value", 1, TimeUnit.SECONDS);
+            /**
+             * short time after adding it to the map, all ok
+             */
+            map.lock(KEY);
+            Object object = map.get(KEY);
+            map.unlock(KEY);
+            assertNotNull(object);
+            Thread.sleep(3600);
+            /**
+             * More than one second after adding it, now it should be away
+             */
+            map.lock(KEY);
+            object = map.get(KEY);
+            map.unlock(KEY);
+            assertNull(object);
+        } finally {
+            node.shutdown();
+        }
+    }
 }
