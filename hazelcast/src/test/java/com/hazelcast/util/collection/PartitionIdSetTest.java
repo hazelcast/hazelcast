0dee47c8fbd2aea969090928dd4495d9eaff07f3
Vassilis Bekiaris <vbekiaris@gmail.com>
2019-04-24 13:25:17 +0300

    Add runner and category to PartitionIdSetTest

--- hazelcast/src/test/java/com/hazelcast/util/collection/PartitionIdSetTest.java
+++ hazelcast/src/test/java/com/hazelcast/util/collection/PartitionIdSetTest.java
@@ -16,8 +16,13 @@
 
 package com.hazelcast.util.collection;
 
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -30,6 +35,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelTest.class})
 public class PartitionIdSetTest {
 
     private PartitionIdSet partitionIdSet;
