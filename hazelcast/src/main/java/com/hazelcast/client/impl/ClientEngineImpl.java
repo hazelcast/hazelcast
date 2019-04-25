60b57542d4393a28c3e7577d8451f99dcb0e9870
Josef Cacek <josef.cacek@gmail.com>
2019-04-25 19:00:45 +0200

    Move Client authentication to blockingExecutor

--- hazelcast/src/main/java/com/hazelcast/client/impl/ClientEngineImpl.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/ClientEngineImpl.java
@@ -23,8 +23,6 @@ import com.hazelcast.client.impl.operations.OnJoinClientOperation;
 import com.hazelcast.client.impl.protocol.ClientExceptions;
 import com.hazelcast.client.impl.protocol.ClientMessage;
 import com.hazelcast.client.impl.protocol.MessageTaskFactory;
-import com.hazelcast.client.impl.protocol.task.AuthenticationCustomCredentialsMessageTask;
-import com.hazelcast.client.impl.protocol.task.AuthenticationMessageTask;
 import com.hazelcast.client.impl.protocol.task.BlockingMessageTask;
 import com.hazelcast.client.impl.protocol.task.GetPartitionsMessageTask;
 import com.hazelcast.client.impl.protocol.task.ListenerMessageTask;
@@ -264,8 +262,7 @@ public class ClientEngineImpl implements ClientEngine, CoreService, PreJoinAware
         Class clazz = messageTask.getClass();
         return clazz == PingMessageTask.class
                 || clazz == GetPartitionsMessageTask.class
-                || clazz == AuthenticationMessageTask.class
-                || clazz == AuthenticationCustomCredentialsMessageTask.class;
+                ;
     }
 
     private boolean isQuery(MessageTask messageTask) {
