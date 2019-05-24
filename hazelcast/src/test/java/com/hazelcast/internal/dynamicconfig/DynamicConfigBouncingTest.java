36c3b46e071bc1a1df282a983a98174b44145930
Sergey Sitnikov <serge.sitnikov@gmail.com>
2019-05-24 19:28:47 +0700

    add test to verify the fix

--- hazelcast/src/test/java/com/hazelcast/internal/dynamicconfig/DynamicConfigBouncingTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/dynamicconfig/DynamicConfigBouncingTest.java
@@ -60,7 +60,7 @@ public class DynamicConfigBouncingTest extends HazelcastTestSupport {
     public BounceMemberRule bounceMemberRule = BounceMemberRule.with(getConfig())
             .clusterSize(4)
             .driverCount(1)
-            .useTerminate()
+            .useTerminate(true)
             .build();
 
     public Config getConfig() {
