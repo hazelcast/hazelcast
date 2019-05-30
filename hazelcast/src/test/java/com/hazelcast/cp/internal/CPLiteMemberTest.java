cc537428028584e1f89935398fa4a72a3b03b671
tezc <ozan.vx@gmail.com>
2019-05-30 14:26:42 +0300

    Fix ParallelTest annotation for CPLiteMemberTest

--- hazelcast/src/test/java/com/hazelcast/cp/internal/CPLiteMemberTest.java
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/CPLiteMemberTest.java
@@ -21,7 +21,7 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.cp.CPMember;
 import com.hazelcast.nio.Address;
 import com.hazelcast.test.HazelcastParallelClassRunner;
-import com.hazelcast.test.annotation.ParallelTest;
+import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -42,7 +42,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 @RunWith(HazelcastParallelClassRunner.class)
-@Category({QuickTest.class, ParallelTest.class})
+@Category({QuickTest.class, ParallelJVMTest.class})
 public class CPLiteMemberTest extends HazelcastRaftTestSupport {
 
     @Test
