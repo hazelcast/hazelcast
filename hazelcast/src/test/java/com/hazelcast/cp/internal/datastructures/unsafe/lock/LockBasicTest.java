52b06b68d296f0f04f372ae340131ee19799dc19
Matko Medenjak <matko@hazelcast.com>
2019-06-19 18:29:23 +0200

    Checkstyle

--- hazelcast/src/test/java/com/hazelcast/cp/internal/datastructures/unsafe/lock/LockBasicTest.java
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/datastructures/unsafe/lock/LockBasicTest.java
@@ -22,7 +22,6 @@ import com.hazelcast.cp.lock.ILock;
 import com.hazelcast.internal.partition.InternalPartitionService;
 import com.hazelcast.internal.partition.impl.InternalPartitionImpl;
 import com.hazelcast.spi.exception.DistributedObjectDestroyedException;
-import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.ParallelJVMTest;
