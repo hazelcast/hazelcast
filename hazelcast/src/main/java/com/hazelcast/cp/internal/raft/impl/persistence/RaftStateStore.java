22a2c25e1efcbdc76efc1daff4ef6eee6f87ebe8
Marko Topolnik <marko@hazelcast.com>
2019-07-17 13:12:07 +0200

    Improve Javadoc

--- hazelcast/src/main/java/com/hazelcast/cp/internal/raft/impl/persistence/RaftStateStore.java
+++ hazelcast/src/main/java/com/hazelcast/cp/internal/raft/impl/persistence/RaftStateStore.java
@@ -26,28 +26,29 @@ import java.io.IOException;
 import java.util.Collection;
 
 /**
- * Defines the contract that must be implemented by the
- * storage implementation.
+ * Defines the contract of the Raft storage.
  */
 public interface RaftStateStore extends Closeable {
 
     /**
-     * Initializes the store before starting to persist Raft state.
-     * This method is called before any other method in this interface.
-     * The state store must be ready to persist Raft state once this method
-     * returns.
+     * Initializes the store before starting to persist Raft state. Hazelcast
+     * calls this method before any other method in this interface. After this
+     * method returns, the state store must be ready to accept all other method
+     * calls.
      */
     void open() throws IOException;
 
     /**
      * Persists the given local Raft endpoint and initial Raft group members.
-     * This method returns after the given arguments are written to disk.
+     * When this method returns, all the provided data has been stored to the
+     * durable storage.
      */
     void persistInitialMembers(RaftEndpoint localMember, Collection<RaftEndpoint> initialMembers) throws IOException;
 
     /**
-     * Persists the term and the Raft endpoint that is voted in the given term.
-     * This method returns after the given arguments are written to disk.
+     * Persists the term and the Raft endpoint that has been elected for the
+     * given term. When this method returns, all the provided data has become
+     * durable.
      */
     void persistTerm(int term, RaftEndpoint votedFor) throws IOException;
 
@@ -57,21 +58,24 @@ public interface RaftStateStore extends Closeable {
      * Log entries are appended to the Raft log with sequential log indices.
      * The first log index is 1.
      * <p>
-     * There will be no gaps in persisted log indices before the very first
-     * {@link #persistSnapshot(SnapshotEntry)} call and after each
-     * {@link #persistSnapshot(SnapshotEntry)}.
+     * A block of consecutive log entries has no gaps in the indices, but a
+     * gap can appear between the snapshot entry and its preceding regular
+     * entry. This happens in an edge case where a follower has fallen so far
+     * behind that the missing entries are no longer available from the leader.
+     * In that case the leader will send its snapshot entry instead.
      * <p>
-     * Persisted entries can be truncated from tail via
-     * {@link #truncateEntriesFrom(long)} in some rare failure scenarios.
-     * Consider the following case where 4 log entries are persisted and then
-     * a truncation call is made:
-     * - persistEntry(1)
-     * - persistEntry(2)
-     * - persistEntry(3)
-     * - persistEntry(4)
-     * - truncatedEntriesFrom(3)
-     * After this call sequence, log indices will remain sequential
-     * and the next persistEntry() call will be for index=3.
+     * In a rare failure scenario Raft must delete a range of the newest
+     * entries, rolling back the index of the next persisted entry. Consider
+     * the following case where Raft persists three log entries and then
+     * deletes entries from index 2:
+     * <ul>
+     *     <li> persistEntry(1)
+     *     <li> persistEntry(2)
+     *     <li> persistEntry(3)
+     *     <li> deleteEntriesFrom(2)
+     * </ul>
+     * After this call sequence log indices will remain sequential
+     * and the next persistEntry() call will be for index=2.
      *
      * @see #flushLogs()
      * @see #persistSnapshot(SnapshotEntry)
@@ -83,33 +87,33 @@ public interface RaftStateStore extends Closeable {
     /**
      * Persists the given snapshot entry.
      * <p>
-     * After a snapshot is persisted at index=i and {@link #flushLogs()} is
-     * called, the log entries before index=i (inclusive) are not needed
-     * anymore and can be truncated from the storage. If truncation is not
-     * done, it will not cause a consistency problem but it will increase
-     * the recovery duration. Therefore, truncation can be done in a background
-     * task.
+     * After a snapshot is persisted at <em>index=i</em> and {@link #flushLogs()}
+     * is called, the log entry at <em>index=i</em> and all the preceding
+     * entries are no longer needed and can be evicted from storage. Failing to
+     * evict stale entries will not cause a consistency problem, but it will
+     * increase the time to recover after a restart. Therefore eviction can be
+     * done in a background task.
      * <p>
-     * Snapshots are taken at certain indices, which are determined via
-     * {@link RaftAlgorithmConfig#getCommitIndexAdvanceCountToSnapshot()}.
-     * For instance, if it is 100, snapshots are taken at indices 100, 200,
-     * 300, 400 and so on.
+     * Raft takes snapshots at a predetermined interval, controlled by {@link
+     * RaftAlgorithmConfig#getCommitIndexAdvanceCountToSnapshot()
+     * commitIndexAdvanceCountToSnapshot}. For instance, if it is 100, snapshots
+     * will occur at indices 100, 200, 300, and so on.
      * <p>
-     * The snapshot index correlates to the highest persisted log index
-     * in 2 different ways:
-     * - It can be smaller than or equal to the highest persisted log index
-     * if a Raft node takes a snapshot locally. The difference between
-     * the snapshot index and the highest persisted log index cannot be greater
-     * than {@link RaftAlgorithmConfig#getCommitIndexAdvanceCountToSnapshot()}.
-     * For instance, if
-     * {@link RaftAlgorithmConfig#getCommitIndexAdvanceCountToSnapshot()} is 50
-     * and {@link RaftAlgorithmConfig#getUncommittedEntryCountToRejectNewAppends()}
-     * is 10, and a persistSnapshot() call is made for index=100, the highest
-     * index given to a previous persistEntry() call can be 110.
-     * - The snapshot index can be greater than the highest log entry log index
-     * if a follower Raft node installs a snapshot sent by the leader.
-     * For the same example above, if the highest persisted log index is 25,
-     * the next call can be a snapshot with index=100.
+     * The snapshot index can lag behind the index of the newest log entry that
+     * was already persisted, but there is an upper bound to this difference,
+     * controlled by {@link RaftAlgorithmConfig#getUncommittedEntryCountToRejectNewAppends()
+     * uncommittedEntryCountToRejectNewAppends}. For instance, if {@code
+     * uncommittedEntryCountToRejectNewAppends} is 10, and a {@code
+     * persistSnapshot()} call is made with snapshotIndex=100, the index of the
+     * preceding {@code persistEntry()} call can be at most 110.
+     * <p>
+     * On the other hand, the snapshot index can also be ahead of the newest log
+     * entry. This can happen when a Raft follower has fallen so far behind the
+     * leader that the leader no longer holds the missing entries. In that case
+     * the follower receives a snapshot from the leader. There is no upper bound
+     * on the gap between the newest log entry and the index of the received
+     * snapshot.
+     * </ul>
      *
      * @see #flushLogs()
      * @see #persistEntry(LogEntry)
@@ -118,17 +122,15 @@ public interface RaftStateStore extends Closeable {
     void persistSnapshot(SnapshotEntry entry) throws IOException;
 
     /**
-     * Truncates persisted log entries after the given start index (inclusive).
-     * A truncated log entry is no longer valid and must not be restored
-     * (or must be certainly ignored during the restore process).
+     * Rolls back the log by deleting all entries starting with the given index.
+     * A deleted log entry is no longer valid and must not be restored (or at
+     * least must be ignored during the restore process).
      * <p>
-     * The truncation index can be smaller than or equal to the index of
-     * the highest persisted log entry index. There is a bound on the number
-     * of entries that can be truncated, which is specified by
-     * {@link RaftAlgorithmConfig#getUncommittedEntryCountToRejectNewAppends()}.
-     * Say that it is 5 and the highest persisted log entry index is 20. Then,
-     * at most 5 entries can be truncated from the tail, hence truncation can
-     * start from index=16 (but cannot precede it).
+     * There is a bound on the number of entries that can be deleted, specified
+     * by {@link RaftAlgorithmConfig#getUncommittedEntryCountToRejectNewAppends()
+     * uncommittedEntryCountToRejectNewAppends}. Say that it is 5 and the
+     * highest persisted log entry index is 20. At most 5 newest entries can be
+     * deleted, hence deletion can start at index=16 or higher.
      *
      * @see #flushLogs()
      * @see #persistEntry(LogEntry)
