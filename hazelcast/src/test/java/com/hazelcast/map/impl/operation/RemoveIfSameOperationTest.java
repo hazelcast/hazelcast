4d0fe1b9986afde0220ad75c2bc3b102b0a35f8e
Ahmet Mircik <ahmetmircik@gmail.com>
2019-07-23 10:12:24 +0300

    Fix possible NPE during remove-if-same operation

---
+++ hazelcast/src/test/java/com/hazelcast/map/impl/operation/RemoveIfSameOperationTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.map.impl.operation;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapStoreConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.MapStoreAdapter;
+import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(HazelcastSerialClassRunner.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class RemoveIfSameOperationTest extends HazelcastTestSupport {
+
+    @Test
+    public void removes_key_if_it_is_mapped_to_given_value() {
+        int key = 1;
+        String value = "value";
+
+        ConcurrentMap<Integer, String> store = new ConcurrentHashMap<>();
+        store.put(key, value);
+
+        String mapName = "default";
+        MapStoreConfig mapStoreConfig = new MapStoreConfig();
+        mapStoreConfig.setEnabled(true);
+        mapStoreConfig.setImplementation(new MapStoreAdapter<Integer, String>() {
+
+            @Override
+            public String load(Integer key) {
+                return store.get(key);
+            }
+
+            @Override
+            public void delete(Integer key) {
+                store.remove(key);
+            }
+        });
+
+        Config config = getConfig();
+        config.getMapConfig(mapName).setMapStoreConfig(mapStoreConfig);
+
+        HazelcastInstance node = createHazelcastInstance(config);
+
+        node.getMap(mapName).remove(key, value);
+
+        assertEquals(0, store.size());
+    }
+}
