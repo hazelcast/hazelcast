656ec3e1d0314935b2f07e9601fd8879ec704673
Marko Topolnik <marko@hazelcast.com>
2019-07-25 10:47:34 +0200

    Make LogValidationException an IOException

--- hazelcast/src/main/java/com/hazelcast/cp/internal/raft/exception/LogValidationException.java
+++ hazelcast/src/main/java/com/hazelcast/cp/internal/raft/exception/LogValidationException.java
@@ -1,8 +1,8 @@
 package com.hazelcast.cp.internal.raft.exception;
 
-import com.hazelcast.core.HazelcastException;
+import java.io.IOException;
 
-public class LogValidationException extends HazelcastException {
+public class LogValidationException extends IOException {
     public LogValidationException(String message) {
         super(message);
     }
