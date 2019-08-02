528717c8e7abb7c8173f6279bb954a499b9eac72
ihsan demir <ihsandemir@gmail.com>
2019-08-02 12:25:35 +0300

    Add Java Collections default serializers (#15371)
    * Added built-in serialization support for a number of collections.
    
    * Updated the serialization binary compatibility file to reflect the changes in this PR.

--- hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/SerializationServiceV1.java
+++ hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/SerializationServiceV1.java
@@ -42,14 +42,32 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 import static com.hazelcast.internal.serialization.impl.ConstantSerializers.BooleanArraySerializer;
 import static com.hazelcast.internal.serialization.impl.ConstantSerializers.CharArraySerializer;
@@ -174,13 +192,34 @@ public class SerializationServiceV1 extends AbstractSerializationService {
 
     private void registerJavaTypeSerializers() {
         //Java extensions: more serializers
+        registerConstant(Class.class, new ClassSerializer());
         registerConstant(Date.class, new DateSerializer());
         registerConstant(BigInteger.class, new BigIntegerSerializer());
         registerConstant(BigDecimal.class, new BigDecimalSerializer());
-        registerConstant(Class.class, new ClassSerializer());
         registerConstant(Enum.class, new EnumSerializer());
+
         registerConstant(ArrayList.class, new ArrayListStreamSerializer());
         registerConstant(LinkedList.class, new LinkedListStreamSerializer());
+        registerConstant(CopyOnWriteArrayList.class, new CopyOnWriteArrayListStreamSerializer());
+
+        registerConstant(HashMap.class, new HashMapStreamSerializer());
+        registerConstant(ConcurrentSkipListMap.class, new ConcurrentSkipListMapStreamSerializer());
+        registerConstant(ConcurrentHashMap.class, new ConcurrentHashMapStreamSerializer());
+        registerConstant(LinkedHashMap.class, new LinkedHashMapStreamSerializer());
+        registerConstant(TreeMap.class, new TreeMapStreamSerializer());
+
+        registerConstant(HashSet.class, new HashSetStreamSerializer());
+        registerConstant(TreeSet.class, new TreeSetStreamSerializer());
+        registerConstant(LinkedHashSet.class, new LinkedHashSetStreamSerializer());
+        registerConstant(CopyOnWriteArraySet.class, new CopyOnWriteArraySetStreamSerializer());
+        registerConstant(ConcurrentSkipListSet.class, new ConcurrentSkipListSetStreamSerializer());
+        registerConstant(ArrayDeque.class, new ArrayDequeStreamSerializer());
+        registerConstant(LinkedBlockingQueue.class, new LinkedBlockingQueueStreamSerializer());
+        registerConstant(ArrayBlockingQueue.class, new ArrayBlockingQueueStreamSerializer());
+        registerConstant(PriorityBlockingQueue.class, new PriorityBlockingQueueStreamSerializer());
+        registerConstant(DelayQueue.class, new DelayQueueStreamSerializer());
+        registerConstant(SynchronousQueue.class, new SynchronousQueueStreamSerializer());
+        registerConstant(LinkedTransferQueue.class, new LinkedTransferQueueStreamSerializer());
 
         safeRegister(Serializable.class, javaSerializerAdapter);
         safeRegister(Externalizable.class, javaExternalizableAdapter);
--- hazelcast/src/test/java/com/hazelcast/nio/serialization/SerializationTest.java
+++ hazelcast/src/test/java/com/hazelcast/nio/serialization/SerializationTest.java
@@ -32,7 +32,6 @@ import com.hazelcast.nio.Address;
 import com.hazelcast.nio.IOUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.SerializationConcurrencyTest.Person;
 import com.hazelcast.spi.serialization.SerializationService;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
@@ -61,11 +60,11 @@ import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Properties;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.Arrays.asList;
@@ -285,41 +284,14 @@ public class SerializationTest extends HazelcastTestSupport {
     }
 
     @Test
-    public void testLinkedListSerialization() {
+    public void testSynchronousQueueSerialization() {
         SerializationService ss = new DefaultSerializationServiceBuilder().build();
-        LinkedList<Person> linkedList = new LinkedList<Person>();
-        linkedList.add(new Person(35, 180, 100, "Orhan", null));
-        linkedList.add(new Person(12, 120, 60, "Osman", null));
-        Data data = ss.toData(linkedList);
-        LinkedList deserialized = ss.toObject(data);
-        assertTrue("Objects are not identical!", linkedList.equals(deserialized));
-    }
-
-    @Test
-    public void testUTF8() {
-        SerializationService ss = new DefaultSerializationServiceBuilder().build();
-        String name = "Orhan";
-        Data data = ss.toData(name);
-        assertEquals(name, ss.toObject(data));
-    }
-
-    @Test
-    public void testDataSerializableObjectSerialization() {
-        SerializationService ss = new DefaultSerializationServiceBuilder().build();
-        Person person = new Person(35, 180, 100, "Orhan", null);
-        Data data = ss.toData(person);
-        assertEquals(person, ss.toObject(data));
-    }
-
-    @Test
-    public void testArrayListSerialization() {
-        SerializationService ss = new DefaultSerializationServiceBuilder().build();
-        ArrayList<Person> arrayList = new ArrayList<Person>();
-        arrayList.add(new Person(35, 180, 100, "Orhan", null));
-        arrayList.add(new Person(12, 120, 60, "Osman", null));
-        Data data = ss.toData(arrayList);
-        ArrayList deserialized = ss.toObject(data);
-        assertTrue("Objects are not identical!", arrayList.equals(deserialized));
+        SynchronousQueue q = new SynchronousQueue();
+        Data data = ss.toData(q);
+        SynchronousQueue deserialized = ss.toObject(data);
+        assertTrue("Collections are not identical!", q.containsAll(deserialized));
+        assertTrue("Collections are not identical!", deserialized.containsAll(q));
+        assertEquals("Collection classes are not identical!", q.getClass(), deserialized.getClass());
     }
 
     @Test
