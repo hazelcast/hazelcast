4a6825bb471cedfd692ee970412fda34ab66c0d3
Andrey Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2019-10-07 16:42:56 +0300

    Remove EnumSet litter from MetricsCollectionCycle (#15690)
    * Remove EnumSet litter from MetricsCollectionCycle
    * Improve internal note
    * Include empty array handling into MetricTarget#asSet

---
+++ hazelcast/src/test/java/com/hazelcast/internal/metrics/MetricTargetTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.internal.metrics;
+
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static com.hazelcast.internal.metrics.MetricTarget.DIAGNOSTICS;
+import static com.hazelcast.internal.metrics.MetricTarget.JMX;
+import static com.hazelcast.internal.metrics.MetricTarget.MANAGEMENT_CENTER;
+import static com.hazelcast.test.HazelcastTestSupport.assertContainsAll;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertSame;
+
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class MetricTargetTest {
+
+    @Test
+    public void testAsSet_returnsSameObjects() {
+        assertSame(
+                MetricTarget.asSet(new MetricTarget[]{MANAGEMENT_CENTER}),
+                MetricTarget.asSet(new MetricTarget[]{MANAGEMENT_CENTER})
+        );
+    }
+
+    @Test
+    public void testAsSet_ignoresPermutations() {
+        assertSame(
+                MetricTarget.asSet(new MetricTarget[]{MANAGEMENT_CENTER}),
+                MetricTarget.asSet(new MetricTarget[]{MANAGEMENT_CENTER, MANAGEMENT_CENTER})
+        );
+        assertSame(
+                MetricTarget.asSet(new MetricTarget[]{JMX, MANAGEMENT_CENTER}),
+                MetricTarget.asSet(new MetricTarget[]{MANAGEMENT_CENTER, JMX})
+        );
+    }
+
+    @Test
+    public void testAsSet_supportsAllCombinations() {
+        assertAsSetContainsAll();
+        assertAsSetContainsAll(MANAGEMENT_CENTER);
+        assertAsSetContainsAll(JMX);
+        assertAsSetContainsAll(DIAGNOSTICS);
+        assertAsSetContainsAll(DIAGNOSTICS, JMX);
+        assertAsSetContainsAll(DIAGNOSTICS, MANAGEMENT_CENTER);
+        assertAsSetContainsAll(MANAGEMENT_CENTER, JMX);
+        assertAsSetContainsAll(MANAGEMENT_CENTER, JMX, DIAGNOSTICS);
+    }
+
+    private void assertAsSetContainsAll(MetricTarget... targets) {
+        assertContainsAll(MetricTarget.asSet(targets), asList(targets));
+    }
+
+}
