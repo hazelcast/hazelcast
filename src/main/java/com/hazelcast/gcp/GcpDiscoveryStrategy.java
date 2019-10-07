48231bd584906f073d2fd2188bc399a7ae642820
hasancelik <hasan@hazelcast.com>
2019-10-07 09:58:31 +0300

    convert nio.Address to cluster.Address

--- src/main/java/com/hazelcast/gcp/GcpDiscoveryStrategy.java
+++ src/main/java/com/hazelcast/gcp/GcpDiscoveryStrategy.java
@@ -19,7 +19,7 @@ package com.hazelcast.gcp;
 import com.hazelcast.config.InvalidConfigurationException;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
-import com.hazelcast.nio.Address;
+import com.hazelcast.cluster.Address;
 import com.hazelcast.spi.discovery.AbstractDiscoveryStrategy;
 import com.hazelcast.spi.discovery.DiscoveryNode;
 import com.hazelcast.spi.discovery.DiscoveryStrategy;
