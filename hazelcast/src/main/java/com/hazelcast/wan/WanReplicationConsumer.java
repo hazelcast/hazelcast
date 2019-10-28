0eb4805ee5135ec99aef2ebeed835618a2c33873
Matko Medenjak <medenjak.matko@gmail.com>
2019-10-28 12:27:08 +0100

    Fix WAN queue migration for structures with backupCount>1 (#15873)
    Fix WAN queue migration for structures with backupCount>1
    
    Currently, on migration commit/rollback, we were clearing all partition
    queues if the new replica index was greater than 1. This is fine if the
    backup count for a map/cache is 1 but if the backup count is higher, we
    clear the queues even though we shouldn't.
    We now clear queues depending on the new replica index and the queue
    backup count, exactly how map service does it.
    As a side-effect, we simplify the WAN publisher SPI since we can now
    remove two methods from the SPI - one which was even a leaking
    abstraction mentioning "queues".
    
    Also, fixed NPE issues in CacheMergeOperation when backupCount is 0 and
    renamed "WanEventQueueMigrationListener" which is part of the publisher
    SPI to remove mentioning of "queues".
    
    Fixes: https://github.com/hazelcast/hazelcast-enterprise/issues/2091

---
+++ hazelcast/src/main/java/com/hazelcast/wan/WanReplicationConsumer.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2008-2019, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.wan;
+
+import com.hazelcast.config.WanConsumerConfig;
+import com.hazelcast.config.WanReplicationConfig;
+import com.hazelcast.instance.impl.Node;
+
+/**
+ * Interface to be implemented by custom WAN event consumers. Wan replication
+ * consumers are typically used in conjunction with a custom
+ * {@link WanReplicationPublisher}. The publisher will then publish events
+ * in a custom fashion which the consumer expects and processes accordingly.
+ * This way, you can provide custom publication and consumption mechanisms
+ * and protocols for WAN replication. The default implementation of the WAN
+ * publisher ignores any configured custom consumers and the WAN events will
+ * be processed as if there was no custom consumer implementation.
+ * Can be registered by programmatically or by XML using {@link WanConsumerConfig}.
+ *
+ * @see WanReplicationPublisher
+ */
+public interface WanReplicationConsumer {
+
+    /**
+     * Initialize the WAN consumer. The method is invoked once the node has
+     * started.
+     * The WAN consumer is responsible for registering itself for WAN event
+     * consumption. Typically this means that you would either use the
+     * {@link com.hazelcast.spi.impl.executionservice.ExecutionService}
+     * to schedule the consumer to run periodically or continually by having an
+     * implementation which uses blocking or spinning locks to check for new
+     * events. The implementation is free however to choose another approach.
+     *
+     * @param node               this node
+     * @param wanReplicationName the name of the {@link WanReplicationConfig}
+     * @param config             the WAN consumer config
+     */
+    void init(Node node, String wanReplicationName, WanConsumerConfig config);
+
+    /**
+     * Callback method to shutdown the WAN replication consumer. This is called
+     * on node shutdown.
+     */
+    void shutdown();
+}
