29fe0245e4f6e939ad2a99931aef1245b8f8b514
Bence Erős <ebence88@gmail.com>
2019-11-11 15:52:28 +0100

    fixing tests to cover both with-data and no-data cases

--- hazelcast/src/test/java/com/hazelcast/map/impl/LocalMapStatsProviderTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/LocalMapStatsProviderTest.java
@@ -13,6 +13,7 @@ import com.hazelcast.internal.partition.impl.DummyInternalPartition;
 import com.hazelcast.internal.serialization.SerializationService;
 import com.hazelcast.map.LocalMapStats;
 import com.hazelcast.map.impl.nearcache.MapNearCacheManager;
+import com.hazelcast.map.impl.recordstore.DefaultRecordStore;
 import com.hazelcast.map.impl.recordstore.RecordStore;
 import com.hazelcast.partition.PartitionService;
 import com.hazelcast.query.impl.Indexes;
@@ -28,6 +29,7 @@ import java.util.Properties;
 
 import static com.hazelcast.config.InMemoryFormat.NATIVE;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -37,7 +39,7 @@ import static org.mockito.Mockito.mock;
 public class LocalMapStatsProviderTest {
 
     @Test
-    public void nearCacheUsesNativeMemory() {
+    public void nearCache_withoutStats_UsesNativeMemory() {
         NodeEngine nodeEngine = mock(NodeEngine.class);
         Properties props = new Properties();
         props.put(GroupProperty.MAP_LOAD_CHUNK_SIZE, 22);
@@ -45,20 +47,34 @@ public class LocalMapStatsProviderTest {
         MapServiceContext serviceContext = mock(MapServiceContext.class);
         doReturn(nodeEngine).when(serviceContext).getNodeEngine();
         ProxyService proxyService = mock(ProxyService.class);
-        doReturn(emptyList()).when(proxyService).getDistributedObjectNames("hz:impl:serviceContext");
         doReturn(proxyService).when(nodeEngine).getProxyService();
         doReturn(mock(ClusterService.class)).when(nodeEngine).getClusterService();
 
         MapService mapService = mock(MapService.class);
         doReturn(serviceContext).when(mapService).getMapServiceContext();
         MapContainer mapContainer = mock(MapContainer.class);
+
         MapConfig mapConfig = new MapConfig();
         NearCacheConfig nearCacheConfig = new NearCacheConfig();
         nearCacheConfig.setInMemoryFormat(NATIVE);
         mapConfig.setNearCacheConfig(nearCacheConfig);
+
         doReturn(mapConfig).when(mapContainer).getMapConfig();
         String mapName = "myMap";
         doReturn(mapContainer).when(serviceContext).getMapContainer(mapName);
+        doReturn(new PartitionContainer[]{}).when(serviceContext).getPartitionContainers();
+        doReturn(singletonList(mapName)).when(proxyService).getDistributedObjectNames("hz:impl:mapService");
+
+        doReturn(mock(MapNearCacheManager.class)).when(serviceContext).getMapNearCacheManager();
+
+        LocalMapStatsProvider provider = new LocalMapStatsProvider(serviceContext);
+        Map<String, LocalMapStats> actual = provider.createAllLocalMapStats();
+
+        assertTrue(actual.get(mapName).getNearCacheStats().isNativeMemoryUsed());
+    }
+
+    private void setupPartitionContainer(NodeEngine nodeEngine, MapServiceContext serviceContext, MapService mapService,
+                                         MapContainer mapContainer, String mapName) {
         Indexes indexes = mock(Indexes.class);
         doReturn(indexes).when(mapContainer).createIndexes(false);
         doReturn(new PartitionIndexesStats()).when(indexes).getIndexesStats();
@@ -77,18 +93,10 @@ public class LocalMapStatsProviderTest {
         PartitionContainer partitionContainer = new PartitionContainer(mapService, 0);
         partitionContainer.getRecordStore(mapName, false);
         doReturn(new PartitionContainer[]{partitionContainer}).when(serviceContext).getPartitionContainers();
-
-
-        doReturn(mock(MapNearCacheManager.class)).when(serviceContext).getMapNearCacheManager();
-
-        LocalMapStatsProvider provider = new LocalMapStatsProvider(serviceContext);
-        Map<String, LocalMapStats> actual = provider.createAllLocalMapStats();
-
-        assertTrue(actual.get(mapName).getNearCacheStats().isNativeMemoryUsed());
     }
 
     @Test
-    public void mapOfNearCacheWithStats_usesNativeMemory() {
+    public void nearCache_WithStats_usesNativeMemory() {
         String mapName = "myMap";
         NodeEngine nodeEngine = mock(NodeEngine.class);
         Properties props = new Properties();
@@ -97,7 +105,6 @@ public class LocalMapStatsProviderTest {
         MapServiceContext serviceContext = mock(MapServiceContext.class);
         doReturn(nodeEngine).when(serviceContext).getNodeEngine();
         ProxyService proxyService = mock(ProxyService.class);
-        doReturn(emptyList()).when(proxyService).getDistributedObjectNames("hz:impl:serviceContext");
         doReturn(proxyService).when(nodeEngine).getProxyService();
         doReturn(mock(ClusterService.class)).when(nodeEngine).getClusterService();
 
@@ -121,24 +128,7 @@ public class LocalMapStatsProviderTest {
         doReturn(nearCache).when(nearCacheManager).getNearCache(mapName);
 
         doReturn(mapContainer).when(serviceContext).getMapContainer(mapName);
-        Indexes indexes = mock(Indexes.class);
-        doReturn(indexes).when(mapContainer).createIndexes(false);
-        doReturn(new PartitionIndexesStats()).when(indexes).getIndexesStats();
-        doReturn(new InternalIndex[]{}).when(indexes).getIndexes();
-
-        RecordStore mockRecordStore = mock(RecordStore.class);
-        doReturn(mapName).when(mockRecordStore).getName();
-        doReturn(mapContainer).when(mockRecordStore).getMapContainer();
-        doReturn(mockRecordStore).when(serviceContext).createRecordStore(eq(mapContainer), eq(0), any());
-
-        IPartitionService partitionService = mock(IPartitionService.class);
-        doReturn(partitionService).when(nodeEngine).getPartitionService();
-        doReturn(new DummyInternalPartition(new PartitionReplica[]{}, 0)).when(partitionService).getPartition(0, false);
-        doReturn(new DummyInternalPartition(new PartitionReplica[]{}, 0)).when(partitionService).getPartition(0);
-
-        PartitionContainer partitionContainer = new PartitionContainer(mapService, 0);
-        partitionContainer.getRecordStore(mapName, false);
-        doReturn(new PartitionContainer[]{partitionContainer}).when(serviceContext).getPartitionContainers();
+        setupPartitionContainer(nodeEngine, serviceContext, mapService, mapContainer, mapName);
 
         LocalMapStatsProvider provider = new LocalMapStatsProvider(serviceContext);
         provider.getLocalMapStatsImpl(mapName);
