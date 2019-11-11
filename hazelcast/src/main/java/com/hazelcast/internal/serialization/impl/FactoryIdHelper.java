090b938bb50b7597e1db46bfdc20271147793a02
Matko Medenjak <matko@hazelcast.com>
2019-11-11 13:43:29 +0100

    Remove unused constants

--- hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/FactoryIdHelper.java
+++ hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/FactoryIdHelper.java
@@ -142,56 +142,14 @@ public final class FactoryIdHelper {
     public static final String PARTITION_PORTABLE_FACTORY = "hazelcast.serialization.portable.partition";
     public static final int PARTITION_PORTABLE_FACTORY_ID = -2;
 
-    public static final String CLIENT_PORTABLE_FACTORY = "hazelcast.serialization.portable.client";
-    public static final int CLIENT_PORTABLE_FACTORY_ID = -3;
-
     public static final String MAP_PORTABLE_FACTORY = "hazelcast.serialization.portable.map";
-    public static final int MAP_PORTABLE_FACTORY_ID = -10;
-
-    public static final String QUEUE_PORTABLE_FACTORY = "hazelcast.serialization.portable.queue";
-    public static final int QUEUE_PORTABLE_FACTORY_ID = -11;
-
-    public static final String MULTIMAP_PORTABLE_FACTORY = "hazelcast.serialization.portable.multimap";
-    public static final int MULTIMAP_PORTABLE_FACTORY_ID = -12;
-
-    public static final String EXECUTOR_PORTABLE_FACTORY = "hazelcast.serialization.portable.executor";
-    public static final int EXECUTOR_PORTABLE_FACTORY_ID = -13;
-
-    public static final String CDL_PORTABLE_FACTORY = "hazelcast.serialization.portable.cdl";
-    public static final int CDL_PORTABLE_FACTORY_ID = -14;
-
-    public static final String LOCK_PORTABLE_FACTORY = "hazelcast.serialization.portable.lock";
-    public static final int LOCK_PORTABLE_FACTORY_ID = -15;
-
-    public static final String SEMAPHORE_PORTABLE_FACTORY = "hazelcast.serialization.portable.semaphore";
-    public static final int SEMAPHORE_PORTABLE_FACTORY_ID = -16;
-
-    public static final String ATOMIC_LONG_PORTABLE_FACTORY = "hazelcast.serialization.portable.atomic_long";
-    public static final int ATOMIC_LONG_PORTABLE_FACTORY_ID = -17;
-
-    public static final String TOPIC_PORTABLE_FACTORY = "hazelcast.serialization.portable.topic";
-    public static final int TOPIC_PORTABLE_FACTORY_ID = -18;
-
-    public static final String CLIENT_TXN_PORTABLE_FACTORY = "hazelcast.serialization.portable.client.txn";
-    public static final int CLIENT_TXN_PORTABLE_FACTORY_ID = -19;
-
-    public static final String COLLECTION_PORTABLE_FACTORY = "hazelcast.serialization.portable.collection";
-    public static final int COLLECTION_PORTABLE_FACTORY_ID = -20;
-
-    public static final String ATOMIC_REFERENCE_PORTABLE_FACTORY = "hazelcast.serialization.portable.atomic_reference";
-    public static final int ATOMIC_REFERENCE_PORTABLE_FACTORY_ID = -21;
+    public static final int MAP_PORTABLE_FACTORY_ID = -3;
 
     public static final String REPLICATED_PORTABLE_FACTORY = "hazelcast.serialization.portable.replicated_map";
-    public static final int REPLICATED_PORTABLE_FACTORY_ID = -22;
-
-    public static final String CACHE_PORTABLE_FACTORY = "hazelcast.serialization.portable.cache";
-    public static final int CACHE_PORTABLE_FACTORY_ID = -24;
+    public static final int REPLICATED_PORTABLE_FACTORY_ID = -4;
 
     public static final String RINGBUFFER_PORTABLE_FACTORY = "hazelcast.serialization.portable.ringbuffer";
-    public static final int RINGBUFFER_PORTABLE_FACTORY_ID = -29;
-
-    public static final String ENTERPRISE_MAP_PORTABLE_FACTORY = "hazelcast.serialization.portable.enterprise.map";
-    public static final int ENTERPRISE_MAP_PORTABLE_FACTORY_ID = -30;
+    public static final int RINGBUFFER_PORTABLE_FACTORY_ID = -5;
 
     // factory ID 0 is reserved for Cluster objects (Data, Address, Member etc)...
 
