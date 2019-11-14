da5a18c3be2140482bfbc6c3d86afd291d395db3
Can Gencer <cgencer@gmail.com>
2019-11-14 11:26:41 +0100

    Auto import modules in JDK9+ in start script (#1737)

--- hazelcast-jet-distribution/src/bin-regular/common.sh
+++ hazelcast-jet-distribution/src/bin-regular/common.sh
@@ -12,10 +12,23 @@ if [ -z "$JAVA" ]; then
     exit 1
 fi
 
-CLASSPATH="$JET_HOME/lib/*:$CLASSPATH:$CLASSPATH_DEFAULT"
+# 1 -> Java 8 or earlier (1.8..)
+# 9, 10, 11 -> JDK9, JDK10, JDK11 etc.
+JAVA_VERSION=$(${JAVA} -version 2>&1 | sed -En 's/.* version "([0-9]+).*$/\1/p')
+if [ "$JAVA_VERSION" -ge "9" ]; then
+    JDK_OPTS="\
+        --add-modules java.se \
+        --add-exports java.base/jdk.internal.ref=ALL-UNNAMED \
+        --add-opens java.base/java.lang=ALL-UNNAMED \
+        --add-opens java.base/java.nio=ALL-UNNAMED \
+        --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+        --add-opens java.management/sun.management=ALL-UNNAMED \
+        --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
+    "
+fi
 
 JAVA_OPTS_ARRAY=(\
-$JAVA_OPTS \
+$JDK_OPTS \
 $JAVA_OPTS_DEFAULT \
 "-Dhazelcast.logging.type=log4j" \
 "-Dlog4j.configuration=file:$JET_HOME/config/log4j.properties" \
@@ -23,8 +36,12 @@ $JAVA_OPTS_DEFAULT \
 "-Dhazelcast.config=$JET_HOME/config/hazelcast.yaml" \
 "-Dhazelcast.client.config=$JET_HOME/config/hazelcast-client.yaml" \
 "-Dhazelcast.jet.config=$JET_HOME/config/hazelcast-jet.yaml" \
+$JAVA_OPTS \
 )
+
 
 if [ "$JET_LICENSE_KEY" ]; then
   JAVA_OPTS_ARRAY+=("-Dhazelcast.enterprise.license.key=${JET_LICENSE_KEY}")
 fi
+
+CLASSPATH="$JET_HOME/lib/*:$CLASSPATH:$CLASSPATH_DEFAULT"
