hazelcast/hazelcast

commit fbca5a3bedcf8d0bb4b13bd5ae690ac794ee538d

Remove map eviction policy
Instead we will use EvictionConfig class to introduce custom eviction policies.
Changes in this commit:
 - Removed MapEvictionPolicy --> added MapEvictionPolicyComparator
 - Converted EvictionPolicyComparator, MapEvictionPolicyComparator and
   CacheEvictionPolicyComparator to an interface type.
 - Moved internal api EvictionPolicyComparator and EvictableEntryView to 
   spi.eviction package since they are publicly consumed.
 - Relevant client protocol changes
 - Renamed accessHits --> hits

hazelcast/src/main/java/com/hazelcast/internal/eviction/impl/comparator/LFUEvictionPolicyComparator.java
@@ -16,35 +16,30 @@
 
 package com.hazelcast.internal.eviction.impl.comparator;
 
-import com.hazelcast.internal.eviction.EvictableEntryView;
-import com.hazelcast.internal.eviction.EvictionPolicyComparator;
+import com.hazelcast.spi.eviction.EvictableEntryView;
+import com.hazelcast.spi.eviction.EvictionPolicyComparator;
 import com.hazelcast.internal.serialization.SerializableByConvention;
 
 /**
- * {@link com.hazelcast.config.EvictionPolicy#LFU} policy based {@link EvictionPolicyComparator}.
+ * {@link com.hazelcast.config.EvictionPolicy#LFU}
+ * policy based {@link EvictionPolicyComparator}.
  */
 @SerializableByConvention
-public class LFUEvictionPolicyComparator extends EvictionPolicyComparator {
+public class LFUEvictionPolicyComparator
+        implements EvictionPolicyComparator<Object, Object, EvictableEntryView<Object, Object>> {
+
+    public static final LFUEvictionPolicyComparator INSTANCE
+            = new LFUEvictionPolicyComparator();
 
     @Override
     public int compare(EvictableEntryView e1, EvictableEntryView e2) {
-        long hits1 = e1.getAccessHit();
-        long hits2 = e2.getAccessHit();
-        if (hits2 < hits1) {
-            return SECOND_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-        } else if (hits1 < hits2) {
-            return FIRST_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-        } else {
-            long creationTime1 = e1.getCreationTime();
-            long creationTime2 = e2.getCreationTime();
-            // if hits are same, we select the oldest entry to evict
-            if (creationTime2 < creationTime1) {
-                return SECOND_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-            } else if (creationTime2 > creationTime1) {
-                return FIRST_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-            } else {
-                return BOTH_OF_ENTRIES_HAVE_SAME_PRIORITY_TO_BE_EVICTED;
-            }
-        }
+        int result = Long.compare(e1.getHits(), e2.getHits());
+        // if hits are same, we try to select oldest entry to evict
+        return result == 0 ? Long.compare(e1.getCreationTime(), e2.getCreationTime()) : result;
+    }
+
+    @Override
+    public String toString() {
+        return "LFUEvictionPolicyComparator{" + super.toString() + "} ";
     }
 }

hazelcast/src/main/java/com/hazelcast/internal/eviction/impl/comparator/LRUEvictionPolicyComparator.java
@@ -16,35 +16,30 @@
 
 package com.hazelcast.internal.eviction.impl.comparator;
 
-import com.hazelcast.internal.eviction.EvictableEntryView;
-import com.hazelcast.internal.eviction.EvictionPolicyComparator;
+import com.hazelcast.spi.eviction.EvictableEntryView;
+import com.hazelcast.spi.eviction.EvictionPolicyComparator;
 import com.hazelcast.internal.serialization.SerializableByConvention;
 
 /**
- * {@link com.hazelcast.config.EvictionPolicy#LRU} policy based {@link EvictionPolicyComparator}.
+ * {@link com.hazelcast.config.EvictionPolicy#LRU}
+ * policy based {@link EvictionPolicyComparator}.
  */
 @SerializableByConvention
-public class LRUEvictionPolicyComparator extends EvictionPolicyComparator {
+public class LRUEvictionPolicyComparator
+        implements EvictionPolicyComparator<Object, Object, EvictableEntryView<Object, Object>> {
+
+    public static final LRUEvictionPolicyComparator INSTANCE
+            = new LRUEvictionPolicyComparator();
 
     @Override
     public int compare(EvictableEntryView e1, EvictableEntryView e2) {
-        long accessTime1 = e1.getLastAccessTime();
-        long accessTime2 = e2.getLastAccessTime();
-        if (accessTime2 < accessTime1) {
-            return SECOND_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-        } else if (accessTime1 < accessTime2) {
-            return FIRST_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-        } else {
-            long creationTime1 = e1.getCreationTime();
-            long creationTime2 = e2.getCreationTime();
-            // if access times are same, we select the oldest entry to evict
-            if (creationTime2 < creationTime1) {
-                return SECOND_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-            } else if (creationTime2 > creationTime1) {
-                return FIRST_ENTRY_HAS_HIGHER_PRIORITY_TO_BE_EVICTED;
-            } else {
-                return BOTH_OF_ENTRIES_HAVE_SAME_PRIORITY_TO_BE_EVICTED;
-            }
-        }
+        int result = Long.compare(e1.getLastAccessTime(), e2.getLastAccessTime());
+        // if access times are same, we try to select oldest entry to evict
+        return result == 0 ? Long.compare(e1.getCreationTime(), e2.getCreationTime()) : result;
+    }
+
+    @Override
+    public String toString() {
+        return "LRUEvictionPolicyComparator{" + super.toString() + "} ";
     }
 }

hazelcast/src/main/java/com/hazelcast/internal/eviction/impl/comparator/RandomEvictionPolicyComparator.java
@@ -16,19 +16,29 @@
 
 package com.hazelcast.internal.eviction.impl.comparator;
 
-import com.hazelcast.internal.eviction.EvictableEntryView;
-import com.hazelcast.internal.eviction.EvictionPolicyComparator;
+import com.hazelcast.spi.eviction.EvictableEntryView;
+import com.hazelcast.spi.eviction.EvictionPolicyComparator;
 import com.hazelcast.internal.serialization.SerializableByConvention;
 
 /**
- * {@link com.hazelcast.config.EvictionPolicy#RANDOM} policy based {@link EvictionPolicyComparator}.
+ * {@link com.hazelcast.config.EvictionPolicy#RANDOM}
+ * policy based {@link EvictionPolicyComparator}.
  */
 @SerializableByConvention
-public class RandomEvictionPolicyComparator extends EvictionPolicyComparator {
+public class RandomEvictionPolicyComparator
+        implements EvictionPolicyComparator<Object, Object, EvictableEntryView<Object, Object>> {
+
+    public static final RandomEvictionPolicyComparator INSTANCE
+            = new RandomEvictionPolicyComparator();
 
     @Override
     @SuppressWarnings("ComparatorMethodParameterNotUsed")
     public int compare(EvictableEntryView e1, EvictableEntryView e2) {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "RandomEvictionPolicyComparator{" + super.toString() + "} ";
     }
 }

hazelcast/src/main/java/com/hazelcast/map/MapEvictionPolicyComparator.java
@@ -14,25 +14,26 @@
  * limitations under the License.
  */
 
-package com.hazelcast.cache;
+package com.hazelcast.map;
 
+import com.hazelcast.core.EntryView;
 import com.hazelcast.spi.eviction.EvictionPolicyComparator;
 
 /**
- * {@link ICache} specific {@link EvictionPolicyComparator}
- * for comparing {@link CacheEntryView}s to be evicted.
+ * {@link IMap} specific {@link EvictionPolicyComparator} for
+ * comparing {@link com.hazelcast.core.EntryView}s to be evicted.
  *
  * @param <K> type of the key
  * @param <V> type of the value
  * @see EvictionPolicyComparator
- * @see CacheEntryView
+ * @see EntryView
  */
 @FunctionalInterface
-public interface CacheEvictionPolicyComparator<K, V>
-        extends EvictionPolicyComparator<K, V, CacheEntryView<K, V>> {
+public interface MapEvictionPolicyComparator<K, V>
+        extends EvictionPolicyComparator<K, V, EntryView<K, V>> {
 
     /**
-     * Compares the given {@link CacheEntryView} instances and
+     * Compares the given {@link EntryView} instances and
      * returns the result. The result should be one of
      * <ul>
      *   <li>-1: first entry has higher priority to be evicted</li>
@@ -40,10 +41,10 @@ public interface CacheEvictionPolicyComparator<K, V>
      *   <li> 0: both entries have same priority</li>
      * </ul>
      *
-     * @param o1 the first {@link CacheEntryView} instance to be compared
-     * @param o2 the second {@link CacheEntryView} instance to be compared
+     * @param o1 the first {@link EntryView} instance to be compared
+     * @param o2 the second {@link EntryView} instance to be compared
      * @return the result of comparison
      */
     @Override
-    int compare(CacheEntryView<K, V> o1, CacheEntryView<K, V> o2);
+    int compare(EntryView<K, V> o1, EntryView<K, V> o2);
 }

commit 8e0fed7b70584e776a8141e3795d44d7bdb74378

Fixed DynamicConfigBouncingTest
Fixed a regression caused by
https://github.com/hazelcast/hazelcast/pull/15939 so that
MapEvictionPolicyComparator has no proper equals/hasCode methods
implementation. As a result, EvictionConfig#equals returns wrong result.

Fixes: https://github.com/hazelcast/hazelcast/issues/16035

hazelcast/src/main/java/com/hazelcast/cache/CacheEvictionPolicyComparator.java
@@ -22,6 +22,9 @@ import com.hazelcast.spi.eviction.EvictionPolicyComparator;
  * {@link ICache} specific {@link EvictionPolicyComparator}
  * for comparing {@link CacheEntryView}s to be evicted.
  *
+ * Implementors of the comparator have to implement {@code equals} and {@code hashCode} methods
+ * to support correct config comparison.
+ *
  * @param <K> type of the key
  * @param <V> type of the value
  * @see EvictionPolicyComparator

hazelcast/src/main/java/com/hazelcast/internal/eviction/impl/comparator/LFUEvictionPolicyComparator.java
@@ -42,4 +42,17 @@ public class LFUEvictionPolicyComparator
     public String toString() {
         return "LFUEvictionPolicyComparator{" + super.toString() + "} ";
     }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        return getClass().equals(o.getClass());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
 }

hazelcast/src/main/java/com/hazelcast/internal/eviction/impl/comparator/LRUEvictionPolicyComparator.java
@@ -16,9 +16,9 @@
 
 package com.hazelcast.internal.eviction.impl.comparator;
 
+import com.hazelcast.internal.serialization.SerializableByConvention;
 import com.hazelcast.spi.eviction.EvictableEntryView;
 import com.hazelcast.spi.eviction.EvictionPolicyComparator;
-import com.hazelcast.internal.serialization.SerializableByConvention;
 
 /**
  * {@link com.hazelcast.config.EvictionPolicy#LRU}
@@ -41,5 +41,18 @@ public class LRUEvictionPolicyComparator
     @Override
     public String toString() {
         return "LRUEvictionPolicyComparator{" + super.toString() + "} ";
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        return getClass().equals(o.getClass());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
     }
 }

hazelcast/src/main/java/com/hazelcast/internal/eviction/impl/comparator/RandomEvictionPolicyComparator.java
@@ -41,4 +41,17 @@ public class RandomEvictionPolicyComparator
     public String toString() {
         return "RandomEvictionPolicyComparator{" + super.toString() + "} ";
     }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        return getClass().equals(o.getClass());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
 }

hazelcast/src/main/java/com/hazelcast/map/MapEvictionPolicyComparator.java
@@ -23,6 +23,9 @@ import com.hazelcast.spi.eviction.EvictionPolicyComparator;
  * {@link IMap} specific {@link EvictionPolicyComparator} for
  * comparing {@link com.hazelcast.core.EntryView}s to be evicted.
  *
+ * Implementors of the comparator have to implement {@code equals} and {@code hashCode} methods
+ * to support correct config comparison.
+ *
  * @param <K> type of the key
  * @param <V> type of the value
  * @see EvictionPolicyComparator