d93d7937da2ef8e9c3f1e218b7677d99471cd5eb
Ahmet Mircik <ahmetmircik@gmail.com>
2019-12-20 12:40:42 +0300

    Address review comments of Ihsan 4

--- hazelcast/src/main/java/com/hazelcast/client/cache/impl/NearCachedClientCacheProxy.java
+++ hazelcast/src/main/java/com/hazelcast/client/cache/impl/NearCachedClientCacheProxy.java
@@ -809,7 +809,7 @@ public class NearCachedClientCacheProxy<K, V> extends ClientCacheProxy<K, V> {
                     ? nearCache.tryReserveForCacheOnUpdate(nearCacheKey, keyData) : NOT_RESERVED;
             T response = remoteCallSupplier.get();
             if (reservationId != NOT_RESERVED
-                    && (calledByBooleanMethod && response instanceof Boolean ? ((Boolean) response) : true)) {
+                    && calledByBooleanMethod && (Boolean) response) {
                 Object nearCacheValue = toNearCacheValue(value, valueData);
                 tryPublishReserved(nearCacheKey, nearCacheValue, reservationId, false);
             } else {
@@ -865,7 +865,7 @@ public class NearCachedClientCacheProxy<K, V> extends ClientCacheProxy<K, V> {
 
                     if (throwable != null) {
                         invalidateNearCache(nearCacheKey);
-                    } else if ((calledByBooleanMethod && response instanceof Boolean ? ((Boolean) response) : true)) {
+                    } else if (calledByBooleanMethod && (Boolean) response) {
                         Object nearCacheValue = toNearCacheValue(value, valueData);
                         tryPublishReserved(nearCacheKey, nearCacheValue, reservationId, false);
                     } else {
