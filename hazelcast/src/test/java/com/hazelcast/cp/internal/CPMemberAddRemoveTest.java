844c50c1127b14b544e3590dbe128eb706c27dbb
Mehmet Dogan <mehmet@dogan.io>
2020-01-07 15:08:14 +0300

    Fix METADATA group snapshot issues
    - CPGroupMembershipChange was not serializing groupId (by mistake).
    If snapshot is produced during a membership change, this was causing failure
    during restore. Since we cannot change serialization format in
    patch release, we hardcoded METADATA log capacity to a huge size
    to practically prevent METADATA snapshots.
    
    - CPGroupInfos should be deep copied while creating snapshot,
    since it's a mutable class. But snapshots must be immutable.

--- hazelcast/src/test/java/com/hazelcast/cp/internal/CPMemberAddRemoveTest.java
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/CPMemberAddRemoveTest.java
@@ -41,6 +41,7 @@ import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -652,6 +653,7 @@ public class CPMemberAddRemoveTest extends HazelcastRaftTestSupport {
     }
 
     @Test
+    @Ignore("METADATA snapshot serialization is disabled.")
     public void when_newCPMemberIsAddedToTheMetadataGroupAfterSnapshot_newMemberInstallsSnapshot() throws ExecutionException, InterruptedException {
         int nodeCount = 3;
         final int commitIndexAdvanceCountToSnapshot = 50;
@@ -822,6 +824,7 @@ public class CPMemberAddRemoveTest extends HazelcastRaftTestSupport {
     }
 
     @Test
+    @Ignore("METADATA snapshot serialization is disabled.")
     public void when_newCPMemberIsAddedToTheMetadataGroupAfterRestartAndSnapshot_newMemberInstallsSnapshot() throws ExecutionException, InterruptedException {
         int nodeCount = 3;
         final int commitIndexAdvanceCountToSnapshot = 50;
