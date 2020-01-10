2a82cc392a8ce418d64797c7d0a7c77b334d8da3
Andrey Pechkurov <apechkurov@gmail.com>
2020-01-10 20:51:35 +0300

    Remove obsolete hazelcast.diagnostics.metric.level sys prop from tests
    Also removes redundant diagnostics configuration from AdvancedNetworkStatsIntegrationTest

--- hazelcast/src/test/java/com/hazelcast/internal/diagnostics/DiagnosticsLogFileTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/diagnostics/DiagnosticsLogFileTest.java
@@ -17,7 +17,6 @@
 package com.hazelcast.internal.diagnostics;
 
 import com.hazelcast.config.Config;
-import com.hazelcast.test.AssertTask;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.ParallelJVMTest;
@@ -31,6 +30,7 @@ import org.junit.runner.RunWith;
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(HazelcastParallelClassRunner.class)
@@ -49,7 +49,6 @@ public class DiagnosticsLogFileTest extends HazelcastTestSupport {
         File diagnosticsFolder = new File(parentFolder, "newdir");
 
         config.setProperty("hazelcast.diagnostics.enabled", "true");
-        config.setProperty("hazelcast.diagnostics.metric.level", "DEBUG");
         config.setProperty("hazelcast.diagnostics.directory", diagnosticsFolder.getAbsolutePath());
 
         createHazelcastInstance(config);
@@ -62,7 +61,6 @@ public class DiagnosticsLogFileTest extends HazelcastTestSupport {
         File diagnosticsFolder = folder.newFolder();
 
         config.setProperty("hazelcast.diagnostics.enabled", "true");
-        config.setProperty("hazelcast.diagnostics.metric.level", "DEBUG");
         config.setProperty("hazelcast.diagnostics.directory", diagnosticsFolder.getAbsolutePath());
 
         createHazelcastInstance(config);
@@ -71,15 +69,13 @@ public class DiagnosticsLogFileTest extends HazelcastTestSupport {
     }
 
     private void assertContainsFileEventually(final File dir) {
-        assertTrueEventually(new AssertTask() {
-            @Override
-            public void run() {
-                assertTrue(dir.exists());
-                assertTrue(dir.isDirectory());
+        assertTrueEventually(() -> {
+            assertTrue(dir.exists());
+            assertTrue(dir.isDirectory());
 
-                File[] files = dir.listFiles();
-                assertTrue(files.length > 0);
-            }
+            File[] files = dir.listFiles();
+            assertNotNull(files);
+            assertTrue(files.length > 0);
         });
     }
 }
