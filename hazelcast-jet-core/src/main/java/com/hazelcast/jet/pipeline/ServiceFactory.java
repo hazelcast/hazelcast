a61a0c2c9219a6c9ca8b5b5436768f3668b5c32f
Marko Topolnik <marko@hazelcast.com>
2020-01-24 15:48:42 +0100

    Support attaching files to ServiceFactory
    Also rewrite the class to use clone-and-update instead of a massive
    constructor.

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/ServiceFactory.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/pipeline/ServiceFactory.java
@@ -20,15 +20,21 @@ import com.hazelcast.function.BiFunctionEx;
 import com.hazelcast.function.ConsumerEx;
 import com.hazelcast.function.FunctionEx;
 import com.hazelcast.function.SupplierEx;
+import com.hazelcast.jet.JetException;
 import com.hazelcast.jet.core.Processor;
 import com.hazelcast.jet.core.ProcessorSupplier;
 import com.hazelcast.jet.core.ProcessorSupplier.Context;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.hazelcast.internal.util.Preconditions.checkPositive;
 import static com.hazelcast.jet.impl.util.Util.checkSerializable;
+import static java.util.Collections.emptyMap;
 
 /**
  * A holder of functions needed to create and destroy a service object used in
@@ -85,7 +91,7 @@ import static com.hazelcast.jet.impl.util.Util.checkSerializable;
  *
  * @since 4.0
  */
-public final class ServiceFactory<C, S> implements Serializable {
+public final class ServiceFactory<C, S> implements Serializable, Cloneable {
 
     /**
      * Default value for {@link #maxPendingCallsPerProcessor}.
@@ -102,37 +108,31 @@ public final class ServiceFactory<C, S> implements Serializable {
      */
     public static final boolean ORDERED_ASYNC_RESPONSES_DEFAULT = true;
 
-    private final boolean isCooperative;
+    private boolean isCooperative = COOPERATIVE_DEFAULT;
 
     // options for async
-    private final int maxPendingCallsPerProcessor;
-    private final boolean orderedAsyncResponses;
+    private int maxPendingCallsPerProcessor = MAX_PENDING_CALLS_DEFAULT;
+    private boolean orderedAsyncResponses = ORDERED_ASYNC_RESPONSES_DEFAULT;
 
     @Nonnull
-    private final FunctionEx<? super Context, ? extends C> createContextFn;
-    @Nonnull
-    private final BiFunctionEx<? super Processor.Context, ? super C, ? extends S> createServiceFn;
-    @Nonnull
-    private final ConsumerEx<? super S> destroyServiceFn;
-    @Nonnull
-    private final ConsumerEx<? super C> destroyContextFn;
+    private FunctionEx<? super Context, ? extends C> createContextFn;
 
-    private ServiceFactory(
-            @Nonnull FunctionEx<? super ProcessorSupplier.Context, ? extends C> createContextFn,
-            @Nonnull BiFunctionEx<? super Processor.Context, ? super C, ? extends S> createServiceFn,
-            @Nonnull ConsumerEx<? super S> destroyServiceFn,
-            @Nonnull ConsumerEx<? super C> destroyContextFn,
-            boolean isCooperative,
-            int maxPendingCallsPerProcessor,
-            boolean orderedAsyncResponses
-    ) {
+    @Nonnull
+    private BiFunctionEx<? super Processor.Context, ? super C, ? extends S> createServiceFn = (ctx, svcContext) -> {
+        throw new IllegalStateException("This ServiceFactory is missing a createServiceFn");
+    };
+
+    @Nonnull
+    private ConsumerEx<? super S> destroyServiceFn = ConsumerEx.noop();
+
+    @Nonnull
+    private ConsumerEx<? super C> destroyContextFn = ConsumerEx.noop();
+
+    @Nonnull
+    private Map<String, File> attachedFiles = emptyMap();
+
+    private ServiceFactory(@Nonnull FunctionEx<? super ProcessorSupplier.Context, ? extends C> createContextFn) {
         this.createContextFn = createContextFn;
-        this.createServiceFn = createServiceFn;
-        this.destroyServiceFn = destroyServiceFn;
-        this.destroyContextFn = destroyContextFn;
-        this.isCooperative = isCooperative;
-        this.maxPendingCallsPerProcessor = maxPendingCallsPerProcessor;
-        this.orderedAsyncResponses = orderedAsyncResponses;
     }
 
     /**
@@ -157,15 +157,7 @@ public final class ServiceFactory<C, S> implements Serializable {
             @Nonnull FunctionEx<? super ProcessorSupplier.Context, ? extends C> createContextFn
     ) {
         checkSerializable(createContextFn, "createContextFn");
-        return new ServiceFactory<>(
-                createContextFn,
-                (ctx, svcContext) -> {
-                    throw new IllegalStateException("This ServiceFactory is missing a createServiceFn");
-                },
-                ConsumerEx.noop(),
-                ConsumerEx.noop(),
-                COOPERATIVE_DEFAULT, MAX_PENDING_CALLS_DEFAULT, ORDERED_ASYNC_RESPONSES_DEFAULT
-        );
+        return new ServiceFactory<>(createContextFn);
     }
 
     /**
@@ -181,8 +173,9 @@ public final class ServiceFactory<C, S> implements Serializable {
     @Nonnull
     public ServiceFactory<C, S> withDestroyContextFn(@Nonnull ConsumerEx<? super C> destroyContextFn) {
         checkSerializable(destroyContextFn, "destroyContextFn");
-        return new ServiceFactory<>(createContextFn, createServiceFn, destroyServiceFn, destroyContextFn,
-                isCooperative, maxPendingCallsPerProcessor, orderedAsyncResponses);
+        ServiceFactory<C, S> copy = clone();
+        copy.destroyContextFn = destroyContextFn;
+        return copy;
     }
 
     /**
@@ -209,9 +202,11 @@ public final class ServiceFactory<C, S> implements Serializable {
     public <S_NEW> ServiceFactory<C, S_NEW> withCreateServiceFn(
             @Nonnull BiFunctionEx<? super Processor.Context, ? super C, ? extends S_NEW> createServiceFn
     ) {
-        checkSerializable(createServiceFn, "initFn");
-        return new ServiceFactory<>(createContextFn, createServiceFn, ConsumerEx.noop(), destroyContextFn,
-                isCooperative, maxPendingCallsPerProcessor, orderedAsyncResponses);
+        checkSerializable(createServiceFn, "createServiceFn");
+        @SuppressWarnings("unchecked")
+        ServiceFactory<C, S_NEW> copy = (ServiceFactory<C, S_NEW>) clone();
+        copy.createServiceFn = createServiceFn;
+        return copy;
     }
 
     /**
@@ -228,8 +223,9 @@ public final class ServiceFactory<C, S> implements Serializable {
     @Nonnull
     public ServiceFactory<C, S> withDestroyServiceFn(@Nonnull ConsumerEx<? super S> destroyServiceFn) {
         checkSerializable(destroyServiceFn, "destroyServiceFn");
-        return new ServiceFactory<>(createContextFn, createServiceFn, destroyServiceFn, destroyContextFn,
-                isCooperative, maxPendingCallsPerProcessor, orderedAsyncResponses);
+        ServiceFactory<C, S> copy = clone();
+        copy.destroyServiceFn = destroyServiceFn;
+        return copy;
     }
 
     /**
@@ -246,10 +242,9 @@ public final class ServiceFactory<C, S> implements Serializable {
      */
     @Nonnull
     public ServiceFactory<C, S> toNonCooperative() {
-        return new ServiceFactory<>(
-                createContextFn, createServiceFn, destroyServiceFn, destroyContextFn,
-                false, maxPendingCallsPerProcessor, orderedAsyncResponses
-        );
+        ServiceFactory<C, S> copy = clone();
+        copy.isCooperative = false;
+        return copy;
     }
 
     /**
@@ -273,10 +268,10 @@ public final class ServiceFactory<C, S> implements Serializable {
     @Nonnull
     public ServiceFactory<C, S> withMaxPendingCallsPerProcessor(int maxPendingCallsPerProcessor) {
         checkPositive(maxPendingCallsPerProcessor, "maxPendingCallsPerProcessor must be >= 1");
-        return new ServiceFactory<>(
-                createContextFn, createServiceFn, destroyServiceFn, destroyContextFn,
-                isCooperative, maxPendingCallsPerProcessor, orderedAsyncResponses
-        );
+        ServiceFactory<C, S> copy = clone();
+        copy.maxPendingCallsPerProcessor = maxPendingCallsPerProcessor;
+        return copy;
+
     }
 
     /**
@@ -315,10 +310,66 @@ public final class ServiceFactory<C, S> implements Serializable {
      */
     @Nonnull
     public ServiceFactory<C, S> withUnorderedAsyncResponses() {
-        return new ServiceFactory<>(
-                createContextFn, createServiceFn, destroyServiceFn, destroyContextFn,
-                isCooperative, maxPendingCallsPerProcessor, false
-        );
+        ServiceFactory<C, S> copy = clone();
+        copy.orderedAsyncResponses = false;
+        return copy;
+
+    }
+
+    /**
+     * Attaches a file to this service factory under the given ID. It will
+     * become a part of the Jet job and available to {@link #createContextFn()}
+     * as {@link ProcessorSupplier.Context#attachedFile
+     * procSupplierContext.attachedFile(id)}.
+     *
+     * @return a copy of this factory with the file attached
+     *
+     * @since 4.0
+     */
+    @Nonnull
+    public ServiceFactory<C, S> withAttachedFile(@Nonnull String id, @Nonnull File file) {
+        if (!file.isFile() || !file.canRead()) {
+            throw new IllegalArgumentException("Not an existing, readable file: " + file);
+        }
+        return attachFileOrDir(id, file);
+    }
+
+    /**
+     * Attaches a directory to this service factory under the given ID. It will
+     * become a part of the Jet job and available to {@link #createContextFn()}
+     * as {@link ProcessorSupplier.Context#attachedDirectory
+     * procSupplierContext.attachedDirectory(id)}.
+     *
+     * @return a copy of this factory with the directory attached
+     *
+     * @since 4.0
+     */
+    @Nonnull
+    public ServiceFactory<C, S> withAttachedDirectory(@Nonnull String id, @Nonnull File directory) {
+        if (!directory.isDirectory() || !directory.canRead()) {
+            throw new IllegalArgumentException("Not an existing, readable directory: " + directory);
+        }
+        return attachFileOrDir(id, directory);
+    }
+
+    @Nonnull
+    private ServiceFactory<C, S> attachFileOrDir(String id, @Nonnull File file) {
+        ServiceFactory<C, S> copy = clone();
+        copy.attachedFiles.put(id, file);
+        return copy;
+    }
+
+    /**
+     * Returns a copy of this {@link ServiceFactory} with any attached files
+     * removed.
+     *
+     * @since 4.0
+     */
+    @Nonnull
+    public ServiceFactory<C, S> withoutAttachedFiles() {
+        ServiceFactory<C, S> copy = clone();
+        copy.attachedFiles = emptyMap();
+        return copy;
     }
 
     /**
@@ -389,5 +440,32 @@ public final class ServiceFactory<C, S> implements Serializable {
      */
     public boolean hasOrderedAsyncResponses() {
         return orderedAsyncResponses;
+    }
+
+    /**
+     * Returns the files and directories attached to this service factory. They
+     * will become a part of the Jet job and available to {@link
+     * #createContextFn()} as {@link ProcessorSupplier.Context#attachedFile
+     * procSupplierContext.attachedFile(file.toString())} or
+     * {@link ProcessorSupplier.Context#attachedDirectory
+     * procSupplierContext.attachedDirectory(directory.toString())}.
+     *
+     * @since 4.0
+     */
+    @Nonnull
+    public Map<String, File> attachedFiles() {
+        return Collections.unmodifiableMap(attachedFiles);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected ServiceFactory<C, S> clone() {
+        try {
+            ServiceFactory<C, S> copy = (ServiceFactory<C, S>) super.clone();
+            copy.attachedFiles = new HashMap<>(attachedFiles);
+            return copy;
+        } catch (CloneNotSupportedException e) {
+            throw new JetException(getClass() + " is not cloneable", e);
+        }
     }
 }
