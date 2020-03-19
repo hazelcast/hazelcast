c6b64606a093f8a60b70f126449097f3d9989aaa
devozerov <ppozerov@gmail.com>
2020-03-19 20:36:20 +0300

    Fixed spotbugs.

--- hazelcast/src/main/java/com/hazelcast/sql/impl/row/HeapRow.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/row/HeapRow.java
@@ -20,6 +20,7 @@ import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.sql.impl.SqlDataSerializerHook;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -39,6 +40,7 @@ public class HeapRow implements Row, IdentifiedDataSerializable {
         this.values = new Object[length];
     }
 
+    @SuppressFBWarnings("EI_EXPOSE_REP2")
     public HeapRow(Object[] values) {
         this.values = values;
     }
