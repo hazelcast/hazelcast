hazelcast/hazelcast

commit 03b05ba56fc7f5020fd2d6dd31247d498f23aeae

Cleanup: Remove the look up to member list on client invocation
In 3.12, invocations were opening connections. To avoid opening
a connection to a non-member, we had a member-list check.
In 4.0, it is not needed anymore since connections will not be opened
by invocations.

Related TargetNotMemberException check is also not needed on retry.

(cherry picked from commit 0f5ba7276b8506d5ff4c84f25f2483cc095eb523)

hazelcast/src/main/java/com/hazelcast/client/impl/spi/impl/ClientInvocation.java
@@ -29,7 +29,6 @@ import com.hazelcast.internal.nio.Connection;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.spi.exception.RetryableException;
 import com.hazelcast.spi.exception.TargetDisconnectedException;
-import com.hazelcast.spi.exception.TargetNotMemberException;
 import com.hazelcast.spi.impl.executionservice.TaskScheduler;
 import com.hazelcast.spi.impl.operationservice.impl.BaseInvocation;
 import com.hazelcast.spi.impl.sequence.CallIdSequence;
@@ -237,11 +236,6 @@ public class ClientInvocation extends BaseInvocation implements Runnable {
             return;
         }
 
-        if (isNotAllowedToRetryOnSelection(exception)) {
-            completeExceptionally(exception);
-            return;
-        }
-
         if (!shouldRetry(exception)) {
             completeExceptionally(exception);
             return;
@@ -295,23 +289,6 @@ public class ClientInvocation extends BaseInvocation implements Runnable {
         }
     }
 
-    private boolean isNotAllowedToRetryOnSelection(Throwable exception) {
-        if (isBindToSingleConnection()
-                && (exception instanceof IOException || exception instanceof TargetDisconnectedException)) {
-            return true;
-        }
-
-        if (uuid != null
-                && exception instanceof TargetNotMemberException
-                && clientClusterService.getMember(uuid) == null) {
-            //when invocation send over address
-            //if exception is target not member and
-            //address is not available in member list , don't retry
-            return true;
-        }
-        return false;
-    }
-
     private boolean isBindToSingleConnection() {
         return connection != null;
     }
@@ -340,8 +317,11 @@ public class ClientInvocation extends BaseInvocation implements Runnable {
     }
 
     private boolean shouldRetry(Throwable t) {
-        if (t instanceof IOException || t instanceof HazelcastInstanceNotActiveException
-                || t instanceof RetryableException) {
+        if (isBindToSingleConnection() && (t instanceof IOException || t instanceof TargetDisconnectedException)) {
+            return false;
+        }
+
+        if (t instanceof IOException || t instanceof HazelcastInstanceNotActiveException || t instanceof RetryableException) {
             return true;
         }
         if (t instanceof TargetDisconnectedException) {

hazelcast/src/main/java/com/hazelcast/client/impl/spi/impl/SmartClientInvocationService.java
@@ -22,7 +22,6 @@ import com.hazelcast.client.impl.protocol.ClientMessage;
 import com.hazelcast.client.impl.protocol.codec.ClientLocalBackupListenerCodec;
 import com.hazelcast.client.impl.spi.ClientListenerService;
 import com.hazelcast.client.impl.spi.EventHandler;
-import com.hazelcast.cluster.Member;
 import com.hazelcast.internal.nio.Connection;
 
 import java.io.IOException;
@@ -107,24 +106,15 @@ public class SmartClientInvocationService extends AbstractClientInvocationServic
     @Override
     public void invokeOnTarget(ClientInvocation invocation, UUID uuid) throws IOException {
         assert (uuid != null);
-        Member member = client.getClientClusterService().getMember(uuid);
-        if (member == null) {
+        Connection connection = connectionManager.getConnection(uuid);
+        if (connection == null) {
             if (invocationLogger.isFinestEnabled()) {
-                invocationLogger.finest("Target : " + uuid + " is not in the member list, Retrying on random target");
+                invocationLogger.finest("Client is not connected to target : " + uuid + ", Retrying on random target");
             }
             invokeOnRandomTarget(invocation);
             return;
         }
-        Connection connection = getConnection(member.getUuid());
         invokeOnConnection(invocation, (ClientConnection) connection);
-    }
-
-    private Connection getConnection(UUID target) throws IOException {
-        Connection connection = connectionManager.getConnection(target);
-        if (connection == null) {
-            throw new IOException("No available connection to member " + target);
-        }
-        return connection;
     }
 
     @Override

hazelcast/src/test/java/com/hazelcast/client/heartbeat/ClientHeartbeatTest.java
@@ -104,24 +104,19 @@ public class ClientHeartbeatTest extends ClientTestSupport {
     }
 
     @Test
-    public void testInvocation_whenHeartbeatStopped() throws InterruptedException {
-        hazelcastFactory.newHazelcastInstance();
-        final HazelcastInstance client = hazelcastFactory.newHazelcastClient(getClientConfig());
-        final HazelcastInstance instance2 = hazelcastFactory.newHazelcastInstance();
+    public void testInvocation_whenHeartbeatStopped() {
+        HazelcastInstance instance = hazelcastFactory.newHazelcastInstance();
+        HazelcastInstance client = hazelcastFactory.newHazelcastClient(getClientConfig());
 
-        // make sure client is connected to instance2
-        String keyOwnedByInstance2 = generateKeyOwnedBy(instance2);
+        warmUpPartitions(instance, client);
 
-        // Verify that the client received partition update for instance2
-        waitClientPartitionUpdateForKeyOwner(client, instance2, keyOwnedByInstance2);
+        IMap<String, String> map = client.getMap("test");
+        map.put("foo", "bar");
 
-        IMap<String, String> map = client.getMap(randomString());
-        map.put(keyOwnedByInstance2, randomString());
-
-        blockMessagesFromInstance(instance2, client);
+        blockMessagesFromInstance(instance, client);
 
         expectedException.expect(TargetDisconnectedException.class);
-        map.put(keyOwnedByInstance2, randomString());
+        map.put("for", "bar2");
     }
 
     @Test

commit d3b20d7f35458167afc6cbd922b6eae654965c10

Don't retry member based invocations if TargetNotMember
regression caused by changes in #16603

We were not retrying an invocation if it is send to a specific member
and exception is TargetNotMemberException .
We have removed this check since we are not throwing
TargetNotMemberException on the client side anymore. In this failure
exception comes from the server. It seems this check is still needed.
This pr puts it back.

fixes https://github.com/hazelcast/hazelcast/issues/16836

hazelcast/src/main/java/com/hazelcast/client/impl/spi/impl/ClientInvocation.java
@@ -28,6 +28,7 @@ import com.hazelcast.internal.nio.Connection;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.spi.exception.RetryableException;
 import com.hazelcast.spi.exception.TargetDisconnectedException;
+import com.hazelcast.spi.exception.TargetNotMemberException;
 import com.hazelcast.spi.impl.executionservice.TaskScheduler;
 import com.hazelcast.spi.impl.operationservice.impl.BaseInvocation;
 import com.hazelcast.spi.impl.sequence.CallIdSequence;
@@ -37,7 +38,6 @@ import java.util.UUID;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
-import java.util.function.BiFunction;
 
 import static com.hazelcast.internal.util.Clock.currentTimeMillis;
 import static com.hazelcast.internal.util.StringUtil.timeToString;
@@ -74,7 +74,6 @@ public class ClientInvocation extends BaseInvocation implements Runnable {
     private volatile long invocationTimeoutMillis;
     private boolean urgent;
     private boolean allowRetryOnRandom = true;
-    private BiFunction<ClientInvocation, Object, Boolean> invocationMethod;
 
     protected ClientInvocation(HazelcastClientInstanceImpl client,
                                ClientMessage clientMessage,
@@ -344,6 +343,13 @@ public class ClientInvocation extends BaseInvocation implements Runnable {
 
     private boolean shouldRetry(Throwable t) {
         if (isBindToSingleConnection() && (t instanceof IOException || t instanceof TargetDisconnectedException)) {
+            return false;
+        }
+
+        if (uuid != null && t instanceof TargetNotMemberException) {
+            //when invocation send to a specific member
+            //if target is not longer a member, we should not retry
+            //note that this exception could come from the server
             return false;
         }
 

hazelcast/src/test/java/com/hazelcast/client/pncounter/ClientPNCounterTest.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.client.pncounter;
+
+import com.hazelcast.client.test.TestHazelcastFactory;
+import com.hazelcast.core.ConsistencyLostException;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.crdt.pncounter.PNCounter;
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class ClientPNCounterTest {
+
+    private final TestHazelcastFactory hazelcastFactory = new TestHazelcastFactory();
+
+    @After
+    public void tearDown() {
+        hazelcastFactory.terminateAll();
+    }
+
+    @Test(expected = ConsistencyLostException.class)
+    public void testClusterRestart() {
+        HazelcastInstance instance = hazelcastFactory.newHazelcastInstance();
+        HazelcastInstance client = hazelcastFactory.newHazelcastClient();
+
+        PNCounter pnCounter = client.getPNCounter("test");
+
+        pnCounter.incrementAndGet();
+
+        instance.shutdown();
+        hazelcastFactory.newHazelcastInstance();
+
+        pnCounter.incrementAndGet();
+    }
+
+}