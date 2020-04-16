3abbf931ed61570a2d4b63a63bc0355d044fe6e0
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-04-16 15:29:45 +0200

    Fixes around temporal conversions
    And one random typo

--- hazelcast/src/test/java/usercodedeployment/CapitalizingFirstNameExtractor.java
+++ hazelcast/src/test/java/usercodedeployment/CapitalizingFirstNameExtractor.java
@@ -19,7 +19,7 @@ package usercodedeployment;
 import com.hazelcast.query.extractor.ValueCollector;
 import com.hazelcast.query.extractor.ValueExtractor;
 
-public class CapitalizatingFirstnameExtractor implements ValueExtractor<Person, Object> {
+public class CapitalizingFirstNameExtractor implements ValueExtractor<Person, Object> {
     @Override
     public void extract(Person target, Object argument, ValueCollector collector) {
         collector.addObject(target.getName().toUpperCase());
