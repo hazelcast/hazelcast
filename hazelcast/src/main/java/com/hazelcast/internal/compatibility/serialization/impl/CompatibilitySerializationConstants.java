e4053066139452da25f1170e443326bae898308f
Matko Medenjak <medenjak.matko@gmail.com>
2020-05-12 17:24:16 +0200

    Add support for processing WAN replication messages from 3.x members
    Adds support on PASSIVE (target) cluster to be able to deserialize and
    process WAN replication events from a 3.x ACTIVE (source) cluster.

--- hazelcast/src/main/java/com/hazelcast/internal/compatibility/serialization/impl/CompatibilitySerializationConstants.java
+++ hazelcast/src/main/java/com/hazelcast/internal/compatibility/serialization/impl/CompatibilitySerializationConstants.java
@@ -19,6 +19,7 @@ package com.hazelcast.internal.compatibility.serialization.impl;
 /**
  * Serialization constants for compatibility with compatibility (3.x) members
  */
+@SuppressWarnings("unused")
 public final class CompatibilitySerializationConstants {
 
     // WARNING: DON'T CHANGE VALUES!
