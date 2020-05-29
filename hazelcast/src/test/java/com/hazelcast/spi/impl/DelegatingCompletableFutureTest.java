ac91be5b2fe9da20163347ddbf8da27e48ee8fb7
Vassilis Bekiaris <vbekiaris@gmail.com>
2020-05-29 21:07:31 +0300

    Fixes interoperability issues with CompletableFuture
    Subclasses DelegatingCompletableFuture and
    AbstractInvocationFuture manage their own completion
    value state separately from superclass
    CompletableFuture. However CompletableFuture's
    methods make use of private CompletableFuture state
    resulting in broken interoperability.

--- hazelcast/src/test/java/com/hazelcast/spi/impl/DelegatingCompletableFutureTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/impl/DelegatingCompletableFutureTest.java
@@ -16,9 +16,9 @@
 
 package com.hazelcast.spi.impl;
 
+import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.internal.serialization.impl.DefaultSerializationServiceBuilder;
-import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
@@ -26,12 +26,19 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import static com.hazelcast.spi.impl.InternalCompletableFuture.completedExceptionally;
 import static com.hazelcast.spi.impl.InternalCompletableFuture.newCompletedFuture;
+import static com.hazelcast.test.HazelcastTestSupport.assertOpenEventually;
+import static com.hazelcast.test.HazelcastTestSupport.assertTrueEventually;
+import static com.hazelcast.test.HazelcastTestSupport.sleepSeconds;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
@@ -110,5 +117,106 @@ public class DelegatingCompletableFutureTest {
     public void test_isDone() {
         Future future = new DelegatingCompletableFuture(null, newCompletedFuture("value"));
         assertTrue(future.isDone());
+    }
+
+    @Test
+    public void test_actionsTrigger_whenAlreadyCompletedFuture() {
+        CountDownLatch latch = new CountDownLatch(1);
+        CompletableFuture<String> future =
+                new DelegatingCompletableFuture(null, newCompletedFuture("value"));
+        future.thenRun(latch::countDown);
+        assertOpenEventually(latch);
+    }
+
+    @Test
+    public void testInteroperability1() {
+        // given f1 (JDK CompletableFuture), is completed asynchronously after 3 seconds
+        //       f2 (DelegatingCompletableFuture wrapping f1) returned from thenCompose
+        // then  f2 is completed eventually
+        CompletableFuture f1 = CompletableFuture.runAsync(() -> sleepSeconds(2));
+        CompletableFuture f2 = CompletableFuture.completedFuture(null).thenCompose(v -> {
+            return new DelegatingCompletableFuture<>(serializationService, f1);
+        });
+
+        assertTrueEventually(() -> assertTrue(f2.isDone() && !f2.isCompletedExceptionally()));
+    }
+
+    @Test
+    public void testInteroperability2() {
+        // given f1 (JDK CompletableFuture), is already completed
+        //       f2 (DelegatingCompletableFuture wrapping f1) returned from thenCompose
+        // then  f2 is completed eventually
+        CompletableFuture f1 = CompletableFuture.completedFuture(null);
+        CompletableFuture f2 = CompletableFuture.completedFuture(null).thenCompose(v ->
+                new DelegatingCompletableFuture<>(serializationService, f1)
+        );
+
+        assertTrueEventually(() -> assertTrue(f2.isDone() && !f2.isCompletedExceptionally()));
+    }
+
+    @Test
+    public void testInteroperability3() {
+        // given f1 (DelegatingCompletableFuture wrapping completed future)
+        //       f2 (JDK CompletableFuture) returned from thenCompose
+        // then  f2 is completed eventually
+        CompletableFuture f1 = new DelegatingCompletableFuture<>(serializationService, completedFuture(null));
+        CompletableFuture f2 = f1.thenCompose(v -> CompletableFuture.runAsync(() -> sleepSeconds(2)));
+
+        assertTrueEventually(() -> assertTrue(f2.isDone() && !f2.isCompletedExceptionally()));
+    }
+
+    @Test
+    public void testInteroperability_withValueDeserializationInFunction() {
+        // given f1 (DelegatingCompletableFuture wrapping completed future with Data as value)
+        //       f2 (JDK CompletableFuture) returned from thenCompose
+        // then  argument in compose function is deserialized
+        //       f2 is completed eventually
+        String value = "test";
+        Data valueData = serializationService.toData(value);
+        CompletableFuture f1 = new DelegatingCompletableFuture<>(serializationService, completedFuture(valueData));
+        CompletableFuture f2 = f1.thenCompose(v -> {
+            assertEquals(value, v);
+            return CompletableFuture.runAsync(() -> sleepSeconds(2));
+        });
+
+        assertTrueEventually(() -> assertTrue(f2.isDone() && !f2.isCompletedExceptionally()));
+        assertEquals(value, f1.join());
+    }
+
+    @Test
+    public void testInteroperability_withValueDeserializationInCompletedReturnedFuture() {
+        // given f1 (DelegatingCompletableFuture wrapping completed future)
+        //       f2 (JDK CompletableFuture) returned from thenCompose
+        // then  f2 is completed eventually
+        //       completion value of f2 is deserialized
+        String value = "test";
+        Data valueData = serializationService.toData(value);
+        CompletableFuture f1 = completedFuture(null);
+        CompletableFuture<String> f2 = f1.thenCompose(v ->
+            new DelegatingCompletableFuture<String>(serializationService, completedFuture(valueData))
+        );
+
+        assertTrueEventually(() -> assertTrue(f2.isDone() && !f2.isCompletedExceptionally()));
+        assertEquals(value, f2.join());
+    }
+
+    @Test
+    public void testInteroperability_withValueDeserializationInReturnedFuture() {
+        // given f1 (DelegatingCompletableFuture wrapping completed future)
+        //       f2 (JDK CompletableFuture) returned from thenCompose
+        // then  f2 is completed eventually
+        //       completion value of f2 is deserialized
+        String value = "test";
+        Data valueData = serializationService.toData(value);
+        CompletableFuture f1 = completedFuture(null);
+        CompletableFuture<String> f2 = f1.thenCompose(v ->
+            new DelegatingCompletableFuture<String>(serializationService, supplyAsync(() -> {
+                sleepSeconds(2);
+                return valueData;
+            }))
+        );
+
+        assertTrueEventually(() -> assertTrue(f2.isDone() && !f2.isCompletedExceptionally()));
+        assertEquals(value, f2.join());
     }
 }
