6119a370e5e65457775ce2b2ae8b55a0e8421c7b
chanmol1999 <chanmol1999@gmail.com>
2020-06-02 20:38:59 +0530

    integration continue

--- hazelcast/src/test/java/com/hazelcast/internal/util/phonehome/IntegrationTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/util/phonehome/IntegrationTest.java
@@ -1,25 +1,31 @@
 package com.hazelcast.internal.util.phonehome;
 
+import com.github.tomakehurst.wiremock.extension.Extension;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.impl.Node;
+import com.hazelcast.spi.impl.operationservice.impl.responses.Response;
 import com.hazelcast.test.HazelcastTestSupport;
 import org.junit.Rule;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.Test;
 import wiremock.org.apache.http.HttpResponse;
+import wiremock.org.apache.http.client.methods.CloseableHttpResponse;
 import wiremock.org.apache.http.client.methods.HttpGet;
 import wiremock.org.apache.http.impl.client.CloseableHttpClient;
 import wiremock.org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static com.hazelcast.test.Accessors.getNode;
 import static org.junit.Assert.assertEquals;
 
@@ -27,29 +33,26 @@ public class IntegrationTest extends HazelcastTestSupport {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule();
+//    WireMockServer wireMockServer=new WireMockServer(wireMockConfig().extensions((Class<? extends Extension>) PhoneHome.class));
 
     @Test
     public void test() throws IOException {
         HazelcastInstance hz = createHazelcastInstance();
         Node node = getNode(hz);
-        PhoneHome phoneHome = new PhoneHome(node);
-        Map<String, String> parameters;
-        Map<String, String> map1 = hz.getMap("hazelcast");
-        Map<String, String> map2 = hz.getMap("phonehome");
-        parameters = phoneHome.phoneHome(true);
+
 
         stubFor(get(urlPathMatching("/ping/.*"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody(parameters.toString())));
+                        .withBody("content")));
+
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet request = new HttpGet("http://localhost:8080/ping/");
         HttpResponse httpResponse = httpClient.execute(request);
-        String stringResponse = convertHttpResponseToString(httpResponse);
-        assertEquals(stringResponse, parameters.toString());
 
+        verify(1,getRequestedFor(urlMatching("/ping/.*")));
 
     }
 
