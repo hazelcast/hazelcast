009dd8a9d48579fa4f09f5a57707433dcbc3d7d4
chanmol1999 <chanmol1999@gmail.com>
2020-06-03 21:08:17 +0530

    update

--- hazelcast/src/test/java/com/hazelcast/internal/util/phonehome/IntegrationTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/util/phonehome/IntegrationTest.java
@@ -1,24 +1,15 @@
 package com.hazelcast.internal.util.phonehome;
 
-import com.github.tomakehurst.wiremock.extension.Extension;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.impl.Node;
-import com.hazelcast.spi.impl.operationservice.impl.responses.Response;
 import com.hazelcast.test.HazelcastTestSupport;
-import org.apache.http.client.HttpClient;
-import org.junit.Before;
+
 import org.junit.Rule;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
+
 import org.junit.Test;
-import wiremock.com.google.common.annotations.VisibleForTesting;
+
 import wiremock.org.apache.http.HttpResponse;
-import wiremock.org.apache.http.client.methods.CloseableHttpResponse;
-import wiremock.org.apache.http.client.methods.HttpGet;
-import wiremock.org.apache.http.impl.client.CloseableHttpClient;
-import wiremock.org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,7 +31,7 @@ public class IntegrationTest extends HazelcastTestSupport {
         PhoneHome phoneHome = new PhoneHome(node, "http://localhost:8080/ping");
 
 
-        stubFor(get(urlPathMatching("/ping/.*"))
+        stubFor(get(urlPathEqualTo("/ping"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
