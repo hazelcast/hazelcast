6aeb3c5771aee36c1ceddadc97772e9642776d39
chanmol1999 <chanmol1999@gmail.com>
2020-06-04 14:40:14 +0530

    final update

--- hazelcast/src/test/java/com/hazelcast/internal/util/phonehome/PhoneHomeIntegrationTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/util/phonehome/PhoneHomeIntegrationTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2008-2020, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hazelcast.internal.util.phonehome;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
@@ -6,23 +21,20 @@ import com.hazelcast.instance.impl.Node;
 import com.hazelcast.test.HazelcastTestSupport;
 
 import org.junit.Rule;
-
 import org.junit.Test;
-
-import java.io.IOException;
 
 import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.hazelcast.test.Accessors.getNode;
 
-public class IntegrationTest extends HazelcastTestSupport {
+public class PhoneHomeIntegrationTest extends HazelcastTestSupport {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule();
 
     @Test()
-    public void test() throws IOException {
+    public void testMapCount() {
         HazelcastInstance hz = createHazelcastInstance();
         Node node = getNode(hz);
         PhoneHome phoneHome = new PhoneHome(node, "http://localhost:8080/ping");
@@ -31,9 +43,7 @@ public class IntegrationTest extends HazelcastTestSupport {
 
         stubFor(get(urlPathEqualTo("/ping"))
                 .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("content")));
+                        .withStatus(200)));
 
         phoneHome.phoneHome(false);
 
