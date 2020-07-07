df256c51dedfebe2854a9e7bb1982091c1b7a3a2
Jozsef Bartok <bjozsef@gmail.com>
2020-07-07 09:33:56 +0300

    Handle missed exception type in observer listener (#2389)

--- hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/observer/ObservableImpl.java
+++ hazelcast-jet-core/src/main/java/com/hazelcast/jet/impl/observer/ObservableImpl.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.jet.impl.observer;
 
+import com.hazelcast.client.HazelcastClientNotActiveException;
 import com.hazelcast.client.impl.clientside.HazelcastClientInstanceImpl;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.RingbufferConfig;
@@ -266,6 +267,10 @@ public class ObservableImpl<T> implements Observable<T> {
             } else if (t instanceof HazelcastInstanceNotActiveException) {
                 if (logger.isFinestEnabled()) {
                     logger.finest("Terminating message listener '" + id + "'. Reason: HazelcastInstance is shutting down");
+                }
+            } else if (t instanceof HazelcastClientNotActiveException) {
+                if (logger.isFinestEnabled()) {
+                    logger.finest("Terminating message listener '" + id + "'. Reason: HazelcastClient is shutting down");
                 }
             } else if (t instanceof DistributedObjectDestroyedException) {
                 if (logger.isFinestEnabled()) {
