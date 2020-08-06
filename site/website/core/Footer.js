341946275dd802ad6fc79a42bc7ced270ee0544e
Can Gencer <cgencer@gmail.com>
2020-08-06 09:54:49 +0200

    Update footer

--- site/website/core/Footer.js
+++ site/website/core/Footer.js
@@ -20,6 +20,7 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
+          <div>
           <a href={this.props.config.baseUrl} className="nav-home">
             {this.props.config.footerIcon && (
               <img
@@ -30,6 +31,10 @@ class Footer extends React.Component {
               />
             )}
           </a>
+          <a className="star-icon" href={this.props.config.repoUrl} target="_blank">
+              <img src="https://questdb.io/img/githubIcon.svg"></img>Give us a Star!
+            </a>
+          </div>
           <div>
             <h5>Docs</h5>
             <a href={this.docUrl('get-started/intro')}>Get Started</a>
@@ -71,16 +76,6 @@ class Footer extends React.Component {
             <a href="https://github.com/hazelcast/hazelcast-jet/issues">Issue Tracker</a>
             <a href="http://hazelcast.com/company/careers/">Work at Hazelcast</a>
             <a href={this.pageUrl('license')}>License</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
           </div>
         </section>
         <section className="copyright">{this.props.config.copyright}</section>
