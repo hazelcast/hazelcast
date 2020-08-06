ac274daf6161e2a597e3544345caa9b6361ef651
devozerov <ppozerov@gmail.com>
2020-08-06 13:54:47 +0300

    Added equals to TableField

--- hazelcast/src/main/java/com/hazelcast/sql/impl/schema/map/MapTableField.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/schema/map/MapTableField.java
@@ -42,23 +42,24 @@ public class MapTableField extends TableField {
         if (this == o) {
             return true;
         }
-
         if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
             return false;
         }
 
         MapTableField field = (MapTableField) o;
 
-        return name.equals(field.name) && type.equals(field.type) && path.equals(field.path) && hidden == field.hidden;
+        return path.equals(field.path);
     }
 
     @Override
     public int hashCode() {
-        int result = name.hashCode();
+        int result = super.hashCode();
 
-        result = 31 * result + type.hashCode();
         result = 31 * result + path.hashCode();
-        result = 31 * result + Boolean.hashCode(hidden);
 
         return result;
     }
