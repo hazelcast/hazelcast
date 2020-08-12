234715cc251116f98264b83366b20729a51f29c4
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2020-08-12 14:52:07 +0300

    Implement auto-config (#48)
    * Implement auto-config
    
    * Add documentation
    
    * Update src/main/java/com/hazelcast/azure/AzureDiscoveryStrategyFactory.java
    
    Co-authored-by: Rafał Leszko <rafal@hazelcast.com>
    
    * Update what we check for in "/etc/resolv.conf"
    
    We used to check a name server ip "168.63.129.16", but it seems this is not present in every Azure VM.
    
    Co-authored-by: Rafał Leszko <rafal@hazelcast.com>

--- src/main/java/com/hazelcast/azure/AzureDiscoveryStrategyFactory.java
+++ src/main/java/com/hazelcast/azure/AzureDiscoveryStrategyFactory.java
@@ -16,11 +16,18 @@
 package com.hazelcast.azure;
 
 import com.hazelcast.config.properties.PropertyDefinition;
+import com.hazelcast.internal.nio.IOUtil;
 import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
 import com.hazelcast.spi.discovery.DiscoveryNode;
 import com.hazelcast.spi.discovery.DiscoveryStrategy;
 import com.hazelcast.spi.discovery.DiscoveryStrategyFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -30,6 +37,8 @@ import java.util.Map;
  * Factory class which returns {@link AzureDiscoveryStrategy} to Discovery SPI
  */
 public class AzureDiscoveryStrategyFactory implements DiscoveryStrategyFactory {
+
+    private static final ILogger LOGGER = Logger.getLogger(AzureDiscoveryStrategyFactory.class);
 
     @Override
     public Class<? extends DiscoveryStrategy> getDiscoveryStrategyType() {
@@ -49,5 +58,57 @@ public class AzureDiscoveryStrategyFactory implements DiscoveryStrategyFactory {
             result.add(property.getDefinition());
         }
         return result;
+    }
+
+    /**
+     * Checks if Hazelcast is running on Azure.
+     * <p>
+     * To check if Hazelcast is running on Azure, we first check whether "internal.cloudapp.net" is present in the file
+     * "/etc/resolv.conf". Such an approach is not officially documented but seems like a good enough heuristic to detect an
+     * Azure Compute VM Instance. Since it's not the official method, we still need to make an API call to a local, non-routable
+     * address http://169.254.169.254/metadata/instance.
+     *
+     * @return true if running on Azure Instance
+     * @see https://docs.microsoft.com/en-us/azure/virtual-machines/linux/instance-metadata-service#metadata-apis
+     */
+    @Override
+    public boolean isAutoDetectionApplicable() {
+        return azureDnsServerConfigured() && azureInstanceMetadataAvailable();
+    }
+
+    private static boolean azureDnsServerConfigured() {
+        return readFileContents("/etc/resolv.conf").contains("internal.cloudapp.net");
+    }
+
+    static String readFileContents(String fileName) {
+        InputStream is = null;
+        try {
+            File file = new File(fileName);
+            byte[] data = new byte[(int) file.length()];
+            is = new FileInputStream(file);
+            is.read(data);
+            return new String(data, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not get " + fileName, e);
+        } finally {
+            IOUtil.closeResource(is);
+        }
+    }
+
+    private static boolean azureInstanceMetadataAvailable() {
+        return isEndpointAvailable("http://169.254.169.254/metadata/instance?api-version=2020-06-01");
+    }
+
+
+    static boolean isEndpointAvailable(String url) {
+        return !RestClient.create(url)
+                .withHeader("Metadata", "True")
+                .get()
+                .isEmpty();
+    }
+
+    @Override
+    public DiscoveryStrategyLevel discoveryStrategyLevel() {
+        return DiscoveryStrategyLevel.CLOUD_VM;
     }
 }
