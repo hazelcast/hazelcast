bf3084683fb804ea7f35e31e648687515b49b196
Ahmet Mircik <ahmetmircik@gmail.com>
2020-09-03 22:24:15 +0300

    Shutdown executors properly

--- hazelcast/src/main/java/com/hazelcast/spi/impl/executionservice/impl/ExecutionServiceImpl.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/executionservice/impl/ExecutionServiceImpl.java
@@ -57,6 +57,7 @@ import static com.hazelcast.internal.metrics.MetricDescriptorConstants.EXECUTOR_
 import static com.hazelcast.internal.metrics.MetricDescriptorConstants.EXECUTOR_PREFIX_SCHEDULED_INTERNAL;
 import static com.hazelcast.internal.metrics.MetricTarget.MANAGEMENT_CENTER;
 import static com.hazelcast.internal.util.ThreadUtil.createThreadPoolName;
+import static java.lang.String.format;
 import static java.lang.Thread.currentThread;
 
 @SuppressWarnings({"checkstyle:classfanoutcomplexity", "checkstyle:methodcount"})
@@ -67,7 +68,7 @@ public final class ExecutionServiceImpl implements ExecutionService {
     private static final long INITIAL_DELAY = 1000;
     private static final long PERIOD = 100;
     private static final int BEGIN_INDEX = 3;
-    private static final long AWAIT_TIME = 3;
+    private static final long AWAIT_TERMINATION_SECONDS = 3;
     private static final int POOL_MULTIPLIER = 2;
     private static final int QUEUE_MULTIPLIER = 100000;
     private static final int ASYNC_QUEUE_CAPACITY = 100000;
@@ -319,23 +320,37 @@ public final class ExecutionServiceImpl implements ExecutionService {
         for (ExecutorService executorService : scheduleDurableExecutors.values()) {
             executorService.shutdown();
         }
-        scheduledExecutorService.shutdownNow();
-        cachedExecutorService.shutdown();
-        try {
-            scheduledExecutorService.awaitTermination(AWAIT_TIME, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            currentThread().interrupt();
-            logger.finest(e);
-        }
-        try {
-            cachedExecutorService.awaitTermination(AWAIT_TIME, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            currentThread().interrupt();
-            logger.finest(e);
-        }
+
+        shutdownAndAwaitTermination(scheduledExecutorService,
+                cachedExecutorService);
+
         executors.clear();
         durableExecutors.clear();
         scheduleDurableExecutors.clear();
+    }
+
+    private void shutdownAndAwaitTermination(ExecutorService... executors) {
+        for (ExecutorService es : executors) {
+            es.shutdown();
+        }
+
+        long awaitNanos = TimeUnit.SECONDS.toNanos(AWAIT_TERMINATION_SECONDS) / 2;
+        for (ExecutorService es : executors) {
+            try {
+                if (!es.awaitTermination(awaitNanos, TimeUnit.NANOSECONDS)) {
+                    logger.finest(format("%s: Not all tasks completed promptly!", es));
+                    es.shutdownNow();
+                    if (!es.awaitTermination(awaitNanos, TimeUnit.NANOSECONDS)) {
+                        logger.finest(format("%s did not terminate!", es));
+                    }
+                }
+            } catch (InterruptedException ie) {
+                es.shutdownNow();
+                currentThread().interrupt();
+
+                logger.finest(ie);
+            }
+        }
     }
 
     @Override
