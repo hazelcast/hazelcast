363ee0df5144853a8f084f757622ec4d1f237601
lprimak <lenny@flowlogix.com>
2020-09-09 13:06:30 -0500

    revert MockJoiner changes, made tests fail spuriously

--- hazelcast/src/test/java/com/hazelcast/test/mocknetwork/MockJoiner.java
+++ hazelcast/src/test/java/com/hazelcast/test/mocknetwork/MockJoiner.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.concurrent.TimeUnit;
 
 class MockJoiner extends AbstractJoiner {
 
@@ -83,7 +82,12 @@ class MockJoiner extends AbstractJoiner {
             if (!shouldRetry()) {
                 break;
             }
-            clusterService.blockOnJoin(TimeUnit.MILLISECONDS.toNanos(500));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
         }
     }
 
