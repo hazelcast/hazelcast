b1ce13f8d7956b80c13ae2fe6c4eb4aaada149cd
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2020-09-09 12:44:40 +0300

    Add Integration tests (#51)

---
+++ .github/terraform/scripts/start_azure_hazelcast_member.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+TAG_KEY=$1
+TAG_VALUE=$2
+
+sed -i -e "s/TAG_KEY/${TAG_KEY}/g" ${HOME}/hazelcast.yaml
+sed -i -e "s/TAG_VALUE/${TAG_VALUE}/g" ${HOME}/hazelcast.yaml
+
+CLASSPATH="${HOME}/jars/hazelcast.jar:${HOME}/jars/hazelcast-azure.jar:${HOME}/hazelcast.yaml"
+
+nohup java -cp ${CLASSPATH} -server com.hazelcast.core.server.HazelcastMemberStarter &>> ${HOME}/logs/hazelcast.logs &
+
+sleep 5
