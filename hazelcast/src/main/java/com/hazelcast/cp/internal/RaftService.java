33e1d778dfc8e876260d01a1f19fa939521e00ad
Mehmet Dogan <mehmet@dogan.io>
2020-10-05 13:27:56 +0300

    Reset missing members map during split-brain merge (#17634)
    `missingMembers` map tracks the missing members from the CP subsystem,
    and used to auto-remove a missing CP member after a timeout if it does
    not join meanwhile.
    
    It's updated when a new membership event is received, only if node is
    already joined the cluster. While a member is joining the cluster,
    `missingMembers` map is not touched. Because we don't expect any missing
    members at this point.
    But during a split-brain merge, there may be some registered missing
    members already. That's why these entries should be cleared before
    joining the cluster.

--- hazelcast/src/main/java/com/hazelcast/cp/internal/RaftService.java
+++ hazelcast/src/main/java/com/hazelcast/cp/internal/RaftService.java
@@ -152,6 +152,7 @@ public class RaftService implements ManagedService, SnapshotAwareService<Metadat
 
     @Override
     public void reset() {
+        missingMembers.clear();
     }
 
     @Override
