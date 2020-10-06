766fa798e79155b90bf578bb4fee4b0b4dd1cb59
ihsan demir <ihsan@hazelcast.com>
2020-10-06 15:36:13 +0300

    Fixes to correctly decrement the permits in the session during `drainPermits` call. (#17639)

--- hazelcast/src/main/java/com/hazelcast/client/cp/internal/datastructures/semaphore/SessionAwareSemaphoreProxy.java
+++ hazelcast/src/main/java/com/hazelcast/client/cp/internal/datastructures/semaphore/SessionAwareSemaphoreProxy.java
@@ -200,7 +200,7 @@ public class SessionAwareSemaphoreProxy extends ClientProxy implements ISemaphor
                 HazelcastClientInstanceImpl client = getClient();
                 ClientMessage response = new ClientInvocation(client, request, objectName).invoke().joinInternal();
                 int count = SemaphoreDrainCodec.decodeResponse(response);
-                sessionManager.releaseSession(groupId, DRAIN_SESSION_ACQ_COUNT - count);
+                sessionManager.releaseSession(groupId, sessionId, DRAIN_SESSION_ACQ_COUNT - count);
                 return count;
             } catch (SessionExpiredException e) {
                 sessionManager.invalidateSession(this.groupId, sessionId);
