d921260aa5371e6931eb5c4d73047e48a8400848
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-10-28 15:15:28 +0100

    Extract FieldsUtil from sample resolver (#17781)
    The FieldsUtil will be also used by Jet. So far Jet contained analogous
    code with subtle differences.
    
    Also fix the issue when top-level field name clashes with a nested
    field, then the top-level field would override the nested field, but be
    hidden, even if it's the only field.

--- hazelcast/src/test/java/com/hazelcast/sql/impl/schema/map/PartitionedMapTableResolverTest.java
+++ hazelcast/src/test/java/com/hazelcast/sql/impl/schema/map/PartitionedMapTableResolverTest.java
@@ -220,9 +220,9 @@ public class PartitionedMapTableResolverTest extends MapSchemaTestSupport {
 
         // Check serializable maps. They all should have the same schema.
         MapTableField[] expectedFields = new MapTableField[] {
-            hiddenField(KEY, QueryDataType.OBJECT, true),
             field("field1", QueryDataType.INT, true),
             field("field2", QueryDataType.INT, true),
+            hiddenField(KEY, QueryDataType.OBJECT, true),
             field("field3", QueryDataType.INT, false),
             hiddenField(VALUE, QueryDataType.OBJECT, false)
         };
@@ -234,9 +234,9 @@ public class PartitionedMapTableResolverTest extends MapSchemaTestSupport {
         // Check portable in the OBJECT mode.
         checkFields(
             getExistingTable(tables, MAP_PORTABLE_OBJECT),
-            hiddenField(KEY, QueryDataType.OBJECT, true),
             field("portableField1", QueryDataType.INT, true),
             field("portableField2", QueryDataType.INT, true),
+            hiddenField(KEY, QueryDataType.OBJECT, true),
             field("portableField3", QueryDataType.INT, false),
             hiddenField(VALUE, QueryDataType.OBJECT, false)
         );
@@ -244,9 +244,9 @@ public class PartitionedMapTableResolverTest extends MapSchemaTestSupport {
         // Check portable in the BINARY mode.
         checkFields(
             getExistingTable(tables, MAP_PORTABLE_BINARY),
-            hiddenField(KEY, QueryDataType.OBJECT, true),
             field("portableField1", QueryDataType.INT, true),
             field("portableField2", QueryDataType.INT, true),
+            hiddenField(KEY, QueryDataType.OBJECT, true),
             field("portableField3", QueryDataType.INT, false),
             hiddenField(VALUE, QueryDataType.OBJECT, false)
         );
@@ -264,14 +264,7 @@ public class PartitionedMapTableResolverTest extends MapSchemaTestSupport {
             expectedFields = new MapTableField[0];
         }
 
-        assertEquals(expectedFields.length, table.getFieldCount());
-
-        for (int i = 0; i < expectedFields.length; i++) {
-            MapTableField field = table.getField(i);
-            MapTableField expectedField = expectedFields[i];
-
-            assertEquals(expectedField, field);
-        }
+        assertEquals(Arrays.asList(expectedFields), table.getFields());
     }
 
     @Test
