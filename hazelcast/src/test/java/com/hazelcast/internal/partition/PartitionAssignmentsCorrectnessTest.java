f0a9a025f7415c3b1a43deedc8a454931c31c617
Sergey Sitnikov <serge.sitnikov@gmail.com>
2020-10-28 16:45:40 +0700

    small config for PartitionAssignmentsCorrectnessTest

--- hazelcast/src/test/java/com/hazelcast/internal/partition/PartitionAssignmentsCorrectnessTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/partition/PartitionAssignmentsCorrectnessTest.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.internal.partition;
 
+import com.hazelcast.config.Config;
 import com.hazelcast.test.HazelcastParallelParametersRunnerFactory;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
@@ -40,4 +41,12 @@ public class PartitionAssignmentsCorrectnessTest extends AbstractPartitionAssign
                 {3, 4},
         });
     }
+
+    @Override
+    protected Config getConfig() {
+        // Partition count is overwritten back to PartitionCorrectnessTestSupport.partitionCount
+        // in PartitionCorrectnessTestSupport.getConfig(boolean, boolean).
+        return smallInstanceConfig();
+    }
+
 }
