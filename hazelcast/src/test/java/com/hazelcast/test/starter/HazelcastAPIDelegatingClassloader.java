b5609b9b0cec803e773ffe04dcbbf12bd1e121f7
Vassilis Bekiaris <vbekiaris@gmail.com>
2020-10-29 09:20:42 +0200

    Fixes for compatibility tests (#17760)
    * Do not reference CacheEntryEvents which do not matter
    
    In SimpleEntryListener for tests, we only care about
    counting the number of events. Referencing each
    CacheEntryEvent breaks compatibility tests due to
    current inability to reference CacheEntryEvents
    across classloaders. So instead of performing a
    for-each loop, we just use the iterator interface and
    increment the respective counters.
    
    * Moves agent implementation to hazelcast-build-utils
    When the FinalRemovalAgent is attached at runtime,
    it is possible that some classes may have already been
    loaded, resulting in random test failures in
    compatibility tests. The hazelcast-build-utils
    artifact declares the agent in its manifest
    so it can be used with java -javaagent on the
    command line.
    
    * Adds AbstractInvocationFuture#join to final methods
    for processing by FinalRemovalAgent.
    
    
    Co-authored-by: Sergey Sitnikov <serge.sitnikov@gmail.com>

--- hazelcast/src/test/java/com/hazelcast/test/starter/HazelcastAPIDelegatingClassloader.java
+++ hazelcast/src/test/java/com/hazelcast/test/starter/HazelcastAPIDelegatingClassloader.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashSet;
@@ -180,5 +181,10 @@ public class HazelcastAPIDelegatingClassloader extends URLClassLoader {
 
     private boolean checkResourceExcluded(String resourceName) {
         return (parent instanceof FilteringClassLoader) && ((FilteringClassLoader) parent).checkResourceExcluded(resourceName);
+    }
+
+    @Override
+    public String toString() {
+        return "HazelcastAPIDelegatingClassloader{urls = \"" + Arrays.toString(getURLs()) + "\"}";
     }
 }
