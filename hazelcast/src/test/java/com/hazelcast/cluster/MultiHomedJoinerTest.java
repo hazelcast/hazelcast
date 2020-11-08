2b4d3a172fde2a5cd881db5dbc479ced835eed86
lprimak <lenny@flowlogix.com>
2020-11-07 20:08:22 -0600

    now runs on non-multihomed hosts as well as multi-homed hosts added simulation test with localhost

--- hazelcast/src/test/java/com/hazelcast/cluster/MultiHomedJoinerTest.java
+++ hazelcast/src/test/java/com/hazelcast/cluster/MultiHomedJoinerTest.java
@@ -39,6 +39,7 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -62,7 +63,9 @@ public class MultiHomedJoinerTest extends HazelcastTestSupport {
     public static void init() throws SocketException {
         System.setProperty(HAZELCAST_TEST_USE_NETWORK, Boolean.TRUE.toString());
         for (NetworkInterface iface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
-            if (!iface.isLoopback() && !iface.isPointToPoint() && iface.isUp() && !iface.isVirtual()) {
+            if (!iface.isLoopback() && !iface.isPointToPoint() && iface.isUp() && !iface.isVirtual()
+                    && !iface.getName().equals("vboxnet0") && !iface.getName().equals("docker0")
+                    && !iface.getDisplayName().contains("Teredo")) {
                 iface.getInterfaceAddresses().stream().map(InterfaceAddress::getAddress)
                         .filter(addr -> addr instanceof Inet4Address).findFirst().map(InetAddress::getHostAddress)
                         .ifPresent(networks::add);
@@ -72,7 +75,7 @@ public class MultiHomedJoinerTest extends HazelcastTestSupport {
 
     @Before
     public void beforeRun() {
-        fact = createHazelcastInstanceFactory(3);
+        fact = createHazelcastInstanceFactory(isMultiHomed() ? 3 : 2);
     }
 
     @After
@@ -83,7 +86,8 @@ public class MultiHomedJoinerTest extends HazelcastTestSupport {
     Config getConfig(String hostname) {
         Config config = new Config();
         NetworkConfig networkConfig = config.getNetworkConfig();
-        networkConfig.setMemberAddressProviderConfig(new MemberAddressProviderConfig().setEnabled(true).setImplementation(new MemberAddressProviderImpl()));
+        networkConfig.setMemberAddressProviderConfig(new MemberAddressProviderConfig().setEnabled(true)
+                .setImplementation(new MemberAddressProviderImpl()));
         config.getNetworkConfig().getJoin().setMulticastConfig(new MulticastConfig().setEnabled(false));
 
         TcpIpConfig tcpIpConfig = new TcpIpConfig();
@@ -95,18 +99,39 @@ public class MultiHomedJoinerTest extends HazelcastTestSupport {
         return config;
     }
 
+    /**
+     * I believe that {@link #simulateMultiHomedWithLocalHost()} would be enough
+     * when the {@link #multiHomedConnectWithWildcardBind()} is fixed to be passing.
+     * At that time, this test can be removed and just the {@link #simulateMultiHomedWithLocalHost()}
+     * can be left for the future
+     * For now, this test makes sure that multi-homed setup actually gets tested before this test passes.
+     */
     @Test
     public void checkMultiHomed() {
         assertGreaterOrEquals("Not Multi-Homed", networks.size(), 2);
     }
 
-    @Test(timeout = 30 * 1000)
-    public void wildcardAddressTest() {
+    @Test(timeout = 15 * 1000)
+    public void multiHomedConnectWithWildcardBind() {
+        assumeTrue(isMultiHomed());
         HazelcastInstance hz1 = fact.newHazelcastInstance(getConfig(null));
         HazelcastInstance hz2 = fact.newHazelcastInstance(getConfig(networks.get(0)));
         HazelcastInstance hz3 = fact.newHazelcastInstance(getConfig(networks.get(1)));
         waitUntilClusterState(hz3, ClusterState.ACTIVE, 1);
         assertEquals("cluster not correct size", 3, hz1.getCluster().getMembers().size());
+    }
+
+    @Test(timeout = 15 * 1000)
+    public void simulateMultiHomedWithLocalHost() {
+        HazelcastInstance hz1 = fact.newHazelcastInstance(getConfig(null));
+        HazelcastInstance hz2 = fact.newHazelcastInstance(getConfig(networks.get(0)));
+        waitUntilClusterState(hz2, ClusterState.ACTIVE, 1);
+        assertEquals("cluster not correct size", 2, hz1.getCluster().getMembers().size());
+    }
+
+
+    private static boolean isMultiHomed() {
+        return networks.size() >= 2;
     }
 
     private static class MemberAddressProviderImpl implements MemberAddressProvider {
@@ -122,7 +147,7 @@ public class MultiHomedJoinerTest extends HazelcastTestSupport {
 
         @Override
         public InetSocketAddress getPublicAddress() {
-            return new InetSocketAddress(networks.get(0), 0);
+            return new InetSocketAddress(isMultiHomed() ? networks.get(0) : "127.0.0.1", 0);
         }
 
         @Override
