73927d725dce2aca696b4a6b05d1145ff42c9c4d
Grzegorz Gierlach <gierlachg@gmail.com>
2020-11-10 09:19:24 +0100

    Cleanup tests (#2646)

--- hazelcast-jet-sql/src/test/java/com/hazelcast/jet/sql/impl/SqlJobManagementTest.java
+++ hazelcast-jet-sql/src/test/java/com/hazelcast/jet/sql/impl/SqlJobManagementTest.java
@@ -22,9 +22,7 @@ import com.hazelcast.jet.SimpleTestInClusterSupport;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.sql.SqlService;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static com.hazelcast.jet.config.ProcessingGuarantee.EXACTLY_ONCE;
 import static com.hazelcast.jet.core.JobStatus.RUNNING;
@@ -40,9 +38,6 @@ public class SqlJobManagementTest extends SimpleTestInClusterSupport {
 
     private static SqlService sqlService;
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-
     @BeforeClass
     public static void beforeClass() {
         initialize(1, null);
@@ -54,23 +49,23 @@ public class SqlJobManagementTest extends SimpleTestInClusterSupport {
         sqlService.execute("CREATE MAPPING src TYPE TestStream");
         sqlService.execute(javaSerializableMapDdl("dest", Long.class, Long.class));
 
-        exception.expectMessage("You must use CREATE JOB statement for a streaming DML query");
-        sqlService.execute("SINK INTO dest SELECT v, v FROM src");
+        assertThatThrownBy(() -> sqlService.execute("SINK INTO dest SELECT v, v FROM src"))
+                .hasMessageContaining("You must use CREATE JOB statement for a streaming DML query");
     }
 
     @Test
     public void when_ddlStatementWithCreateJob_then_fail() {
-        exception.expectMessage("Encountered \"CREATE\" at line 1, column 19");
-        sqlService.execute("CREATE JOB job AS CREATE MAPPING src TYPE TestStream");
+        assertThatThrownBy(() -> sqlService.execute("CREATE JOB job AS CREATE MAPPING src TYPE TestStream"))
+                .hasMessageContaining("Encountered \"CREATE\" at line 1, column 19");
     }
 
     @Test
     public void when_dqlStatementWithCreateJob_then_fail() {
-        exception.expectMessage("Encountered \"SELECT\" at line 1, column 19." + System.lineSeparator() +
-                "Was expecting one of:" + System.lineSeparator() +
-                "    \"INSERT\" ..." + System.lineSeparator() +
-                "    \"SINK\" ...");
-        sqlService.execute("CREATE JOB job AS SELECT 42 FROM my_map");
+        assertThatThrownBy(() -> sqlService.execute("CREATE JOB job AS SELECT 42 FROM my_map"))
+                .hasMessageContaining("Encountered \"SELECT\" at line 1, column 19." + System.lineSeparator() +
+                        "Was expecting one of:" + System.lineSeparator() +
+                        "    \"INSERT\" ..." + System.lineSeparator() +
+                        "    \"SINK\" ...");
     }
 
     @Test
