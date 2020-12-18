0528eea65030ef2339ce92fbbaf2cc31ca703a25
Ahmet Mircik <ahmetmircik@gmail.com>
2020-12-18 14:27:25 +0300

    add new test

--- hazelcast/src/test/java/com/hazelcast/client/impl/spi/ClientHazelcastRunningInForkJoinPoolTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/impl/spi/ClientHazelcastRunningInForkJoinPoolTest.java
@@ -16,34 +16,94 @@
 
 package com.hazelcast.client.impl.spi;
 
-import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.test.TestHazelcastFactory;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.MapStoreConfig;
+import com.hazelcast.core.EntryAdapter;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spi.impl.HazelcastRunningInForkJoinPoolTest;
+import com.hazelcast.map.IMap;
+import com.hazelcast.map.MapStoreAdapter;
 import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+
 @RunWith(HazelcastParallelClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
-public class ClientHazelcastRunningInForkJoinPoolTest extends HazelcastRunningInForkJoinPoolTest {
+public class ClientHazelcastRunningInForkJoinPoolTest extends HazelcastTestSupport {
 
     private final TestHazelcastFactory hazelcastFactory = new TestHazelcastFactory();
 
-    @Override
-    public void setUp() throws Exception {
-        HazelcastInstance server = hazelcastFactory.newHazelcastInstance();
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.addNearCacheConfig(getNearCacheConfig());
-        HazelcastInstance hz = hazelcastFactory.newHazelcastClient(clientConfig);
-        this.hz = hz;
-    }
+    private HazelcastInstance client;
+    private HazelcastInstance server;
+    private IMap serverMap;
+    private IMap clientMap;
+    private String mapName = "loading-takes-ages";
+    private String innocentMap = "innocent";
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         hazelcastFactory.terminateAll();
+    }
+
+    @Before
+    public void setup() {
+        MapStoreConfig mapStoreConfig = new MapStoreConfig();
+        mapStoreConfig.setEnabled(true);
+        mapStoreConfig.setImplementation(new MapStoreAdapter() {
+            @Override
+            public Object load(Object key) {
+                sleepSeconds(1000);
+                return super.load(key);
+            }
+        });
+
+        MapConfig mapConfig = new MapConfig(
+                mapName);
+        mapConfig.setMapStoreConfig(mapStoreConfig);
+
+        Config config = getConfig().addMapConfig(mapConfig);
+
+        server = hazelcastFactory.newHazelcastInstance(config);
+        client = hazelcastFactory.newHazelcastClient();
+
+        serverMap = server.getMap(innocentMap);
+        clientMap = client.getMap(mapName);
+    }
+
+    @Test
+    public void slow_data_loading_does_not_block_entry_listener_addition() {
+        // 1. Let's simulate 1000 parallel tasks
+        // contend on loading 1 item from a database.
+        Collection<Future> tasks = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            tasks.add(ForkJoinPool.commonPool().submit(new ACallable()));
+        }
+
+        // 2. In parallel, adding a listener to a different
+        // map must not affected by loading phase at step 1.
+        serverMap.addEntryListener(new EntryAdapter<>(), true);
+    }
+
+    class ACallable implements Callable<Object> {
+
+        @Override
+        public Object call() throws Exception {
+            clientMap.get(1);
+            return null;
+        }
+
     }
 }
