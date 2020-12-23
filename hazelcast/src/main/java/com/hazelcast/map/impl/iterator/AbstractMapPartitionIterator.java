3941febb7223608df5fa95d4bb1a41e267075ece
Viliam Durina <viliam-durina@users.noreply.github.com>
2020-12-23 15:54:56 +0100

    Unrelated javadoc fixes

--- hazelcast/src/main/java/com/hazelcast/map/impl/iterator/AbstractMapPartitionIterator.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/iterator/AbstractMapPartitionIterator.java
@@ -22,6 +22,7 @@ import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.internal.serialization.SerializationService;
 import com.hazelcast.map.IMap;
 import com.hazelcast.map.impl.LazyMapEntry;
+import com.hazelcast.map.impl.recordstore.RecordStore;
 
 import java.util.Iterator;
 import java.util.List;
