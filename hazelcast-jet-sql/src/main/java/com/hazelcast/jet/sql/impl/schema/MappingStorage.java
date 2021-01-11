cc22f80605b87273e82fb367ee9aff50da76219e
Grzegorz Gierlach <gierlachg@gmail.com>
2021-01-11 13:38:13 +0100

    Wait for SQL mapping propagation to all members (#2790)

--- hazelcast-jet-sql/src/main/java/com/hazelcast/jet/sql/impl/schema/MappingStorage.java
+++ hazelcast-jet-sql/src/main/java/com/hazelcast/jet/sql/impl/schema/MappingStorage.java
@@ -16,27 +16,97 @@
 
 package com.hazelcast.jet.sql.impl.schema;
 
+import com.hazelcast.cluster.Address;
+import com.hazelcast.cluster.Member;
+import com.hazelcast.cluster.memberselector.MemberSelectors;
+import com.hazelcast.internal.serialization.Data;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.replicatedmap.impl.ReplicatedMapService;
+import com.hazelcast.replicatedmap.impl.operation.GetOperation;
 import com.hazelcast.spi.impl.NodeEngine;
+import com.hazelcast.spi.impl.operationservice.Operation;
+import com.hazelcast.spi.impl.operationservice.OperationService;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class MappingStorage {
 
     private static final String CATALOG_MAP_NAME = "__sql.catalog";
 
+    private static final int MAX_CHECK_ATTEMPTS = 3;
+    private static final long SLEEP_MILLIS = 100;
+
     private final NodeEngine nodeEngine;
+    private final ILogger logger;
 
     public MappingStorage(NodeEngine nodeEngine) {
         this.nodeEngine = nodeEngine;
+        this.logger = nodeEngine.getLogger(getClass());
     }
 
     void put(String name, Mapping mapping) {
         storage().put(name, mapping);
+        awaitMappingOnAllMembers(name, mapping);
     }
 
     boolean putIfAbsent(String name, Mapping mapping) {
-        return storage().putIfAbsent(name, mapping) == null;
+        Object previous = storage().putIfAbsent(name, mapping);
+        awaitMappingOnAllMembers(name, mapping);
+        return previous == null;
+    }
+
+    /**
+     * Temporary measure to ensure schema is propagated to all the members.
+     */
+    @SuppressWarnings("BusyWait")
+    private void awaitMappingOnAllMembers(String name, Mapping mapping) {
+        Data keyData = nodeEngine.getSerializationService().toData(name);
+        int keyPartitionId = nodeEngine.getPartitionService().getPartitionId(keyData);
+        OperationService operationService = nodeEngine.getOperationService();
+
+        Collection<Address> memberAddresses = getMemberAddresses();
+        for (int i = 0; i < MAX_CHECK_ATTEMPTS && !memberAddresses.isEmpty(); i++) {
+            List<CompletableFuture<Address>> futures = memberAddresses.stream()
+                    .map(memberAddress -> {
+                        Operation operation = new GetOperation(CATALOG_MAP_NAME, keyData)
+                                .setPartitionId(keyPartitionId)
+                                .setValidateTarget(false);
+                        return operationService
+                                .createInvocationBuilder(ReplicatedMapService.SERVICE_NAME, operation, memberAddress)
+                                .setTryCount(1)
+                                .invoke()
+                                .toCompletableFuture()
+                                .thenApply(result -> Objects.equals(mapping, result) ? memberAddress : null);
+                    }).collect(toList());
+            for (CompletableFuture<Address> future : futures) {
+                try {
+                    memberAddresses.remove(future.join());
+                } catch (Exception e) {
+                    logger.warning("Error occurred while trying to fetch mapping: " + e.getMessage(), e);
+                }
+            }
+            if (!memberAddresses.isEmpty()) {
+                try {
+                    Thread.sleep(SLEEP_MILLIS);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private Collection<Address> getMemberAddresses() {
+        return nodeEngine.getClusterService().getMembers(MemberSelectors.DATA_MEMBER_SELECTOR).stream()
+                         .filter(member -> !member.localMember() && !member.isLiteMember())
+                         .map(Member::getAddress)
+                         .collect(toSet());
     }
 
     Collection<Mapping> values() {
