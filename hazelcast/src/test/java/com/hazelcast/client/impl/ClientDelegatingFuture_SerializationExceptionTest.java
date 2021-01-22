f0587e935aaf20489ebd7e025368c9febe63f9ad
Vassilis Bekiaris <vbekiaris@gmail.com>
2021-01-22 13:35:23 +0200

    Add test annotations

--- hazelcast/src/test/java/com/hazelcast/client/impl/ClientDelegatingFuture_SerializationExceptionTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/impl/ClientDelegatingFuture_SerializationExceptionTest.java
@@ -27,10 +27,17 @@ import com.hazelcast.logging.ILogger;
 import com.hazelcast.spi.impl.DelegatingCompletableFuture_SerializationExceptionTest;
 import com.hazelcast.spi.impl.InternalCompletableFuture;
 import com.hazelcast.spi.impl.sequence.CallIdSequence;
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Before;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.mock;
 
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
 public class ClientDelegatingFuture_SerializationExceptionTest
         extends DelegatingCompletableFuture_SerializationExceptionTest {
 
