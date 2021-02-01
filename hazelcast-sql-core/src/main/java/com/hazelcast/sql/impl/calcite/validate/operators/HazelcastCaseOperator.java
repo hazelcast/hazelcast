7cfaaf5b5f5c31dd9d8cb55ae02775d84151371a
Alex Dukhno <alex.dukhno@icloud.com>
2021-02-01 14:21:35 +0200

    type inference

--- hazelcast-sql-core/src/main/java/com/hazelcast/sql/impl/calcite/validate/operators/HazelcastCaseOperator.java
+++ hazelcast-sql-core/src/main/java/com/hazelcast/sql/impl/calcite/validate/operators/HazelcastCaseOperator.java
@@ -16,7 +16,12 @@
 
 package com.hazelcast.sql.impl.calcite.validate.operators;
 
+import com.hazelcast.sql.impl.QueryException;
+import com.hazelcast.sql.impl.SqlErrorCode;
 import com.hazelcast.sql.impl.calcite.validate.HazelcastSqlCase;
+import com.hazelcast.sql.impl.calcite.validate.types.HazelcastTypeUtils;
+import com.hazelcast.sql.impl.type.QueryDataType;
+import com.hazelcast.sql.impl.type.QueryDataTypeFamily;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.sql.SqlBasicCall;
@@ -32,7 +37,6 @@ import org.apache.calcite.sql.SqlOperatorBinding;
 import org.apache.calcite.sql.SqlSyntax;
 import org.apache.calcite.sql.SqlUtil;
 import org.apache.calcite.sql.SqlWriter;
-import org.apache.calcite.sql.fun.SqlCase;
 import org.apache.calcite.sql.fun.SqlCaseOperator;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.calcite.sql.type.SqlOperandCountRanges;
@@ -41,7 +45,6 @@ import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.calcite.sql.validate.SqlValidator;
 import org.apache.calcite.sql.validate.SqlValidatorImpl;
 import org.apache.calcite.sql.validate.SqlValidatorScope;
-import org.apache.calcite.sql.validate.implicit.TypeCoercion;
 import org.apache.calcite.util.Litmus;
 import org.apache.calcite.util.Pair;
 
@@ -64,16 +67,19 @@ public final class HazelcastCaseOperator extends SqlOperator {
     public void validateCall(SqlCall call, SqlValidator validator, SqlValidatorScope scope, SqlValidatorScope operandScope) {
         System.out.println("HazelcastCaseOperator#validateCall");
 
-        final SqlCase sqlCase = (SqlCase) call;
+        final HazelcastSqlCase sqlCase = (HazelcastSqlCase) call;
+
         final SqlNodeList whenOperands = sqlCase.getWhenOperands();
-        final SqlNodeList thenOperands = sqlCase.getThenOperands();
-        final SqlNode elseOperand = sqlCase.getElseOperand();
         for (SqlNode operand : whenOperands) {
             operand.validateExpr(validator, operandScope);
         }
+
+        final SqlNodeList thenOperands = sqlCase.getThenOperands();
         for (SqlNode operand : thenOperands) {
             operand.validateExpr(validator, operandScope);
         }
+
+        final SqlNode elseOperand = sqlCase.getElseOperand();
         if (elseOperand != null) {
             elseOperand.validateExpr(validator, operandScope);
         }
@@ -101,9 +107,7 @@ public final class HazelcastCaseOperator extends SqlOperator {
         SqlNode elseOperand = callBinding.getCall().getOperandList().get(3);
         assert whenList.size() == thenList.size();
 
-        // checking that search conditions are ok...
         for (SqlNode node : whenList) {
-            // should throw validation error if something wrong...
             RelDataType type = callBinding.getValidator().deriveType(callBinding.getScope(), node);
             if (type.getSqlTypeName() != SqlTypeName.BOOLEAN) {
                 if (throwOnFailure) {
@@ -116,13 +120,13 @@ public final class HazelcastCaseOperator extends SqlOperator {
         boolean foundNotNull = false;
         for (SqlNode node : thenList) {
             RelDataType type = callBinding.getValidator().deriveType(callBinding.getScope(), node);
-            if (type.getSqlTypeName() == SqlTypeName.NULL) {
+            if (type.getSqlTypeName() != SqlTypeName.NULL) {
                 foundNotNull = true;
             }
         }
 
         RelDataType elseType = callBinding.getValidator().deriveType(callBinding.getScope(), elseOperand);
-        if (elseType.getSqlTypeName() == SqlTypeName.NULL) {
+        if (elseType.getSqlTypeName() != SqlTypeName.NULL) {
             foundNotNull = true;
         }
 
@@ -146,8 +150,7 @@ public final class HazelcastCaseOperator extends SqlOperator {
     public void unparse(SqlWriter writer, SqlCall call, int leftPrec, int rightPrec) {
         assert call.getOperandList().size() == 3;
 
-        final SqlWriter.Frame frame =
-                writer.startList(SqlWriter.FrameTypeEnum.CASE, "CASE", "END");
+        final SqlWriter.Frame frame = writer.startList(SqlWriter.FrameTypeEnum.CASE, "CASE", "END");
 
         SqlNodeList whenList = (SqlNodeList) call.getOperandList().get(0);
         SqlNodeList thenList = (SqlNodeList) call.getOperandList().get(1);
@@ -171,16 +174,18 @@ public final class HazelcastCaseOperator extends SqlOperator {
     }
 
     private static class CaseReturnTypeInference implements SqlReturnTypeInference {
-
         @Override
         @SuppressWarnings({"checkstyle:npathcomplexity", "checkstyle:cyclomaticcomplexity", "checkstyle:nestedifdepth"})
         public RelDataType inferReturnType(SqlOperatorBinding binding) {
-            System.out.println("ThreeOperandCaseReturnTypeInference#inferReturnType");
+            System.out.println("CaseReturnTypeInference#inferReturnType");
+
             // Copied from CalCite with small changes
             SqlCallBinding callBinding = (SqlCallBinding) binding;
             SqlCall sqlCall = callBinding.getCall();
+            SqlValidator validator = callBinding.getValidator();
+
             SqlNodeList thenList = (SqlNodeList) sqlCall.getOperandList().get(2);
-            ArrayList<SqlNode> nullList = new ArrayList<>();
+            List<SqlNode> nullList = new ArrayList<>();
             List<RelDataType> argTypes = new ArrayList<>();
 
             final SqlNodeList whenOperands = (SqlNodeList) sqlCall.getOperandList().get(1);
@@ -189,8 +194,7 @@ public final class HazelcastCaseOperator extends SqlOperator {
             final int size = thenList.getList().size();
             for (int i = 0; i < size; i++) {
                 SqlNode node = thenList.get(i);
-                RelDataType type = callBinding.getValidator().deriveType(
-                        callBinding.getScope(), node);
+                RelDataType type = validator.deriveType(callBinding.getScope(), node);
                 SqlNode operand = whenOperands.get(i);
                 if (operand.getKind() == SqlKind.IS_NOT_NULL && type.isNullable()) {
                     SqlBasicCall call = (SqlBasicCall) operand;
@@ -205,42 +209,63 @@ public final class HazelcastCaseOperator extends SqlOperator {
                 }
             }
 
+            List<SqlNode> allReturnNodes = new ArrayList<>(thenList.getList());
             SqlNode elseOp = sqlCall.getOperandList().get(3);
-            argTypes.add(
-                    callBinding.getValidator().deriveType(
-                            callBinding.getScope(), elseOp));
+            allReturnNodes.add(elseOp);
+            argTypes.add(validator.deriveType(callBinding.getScope(), elseOp));
             if (SqlUtil.isNullLiteral(elseOp, false)) {
                 nullList.add(elseOp);
             }
 
             RelDataType caseReturnType = typeFactory.leastRestrictive(argTypes);
             if (null == caseReturnType) {
-                boolean coerced = false;
-                if (callBinding.isTypeCoercionEnabled()) {
-                    TypeCoercion typeCoercion = callBinding.getValidator().getTypeCoercion();
-                    RelDataType commonType = typeCoercion.getWiderTypeFor(argTypes, true);
-                    // commonType is always with nullability as false, we do not consider the
-                    // nullability when deducing the common type. Use the deduced type
-                    // (with the correct nullability) in SqlValidator
-                    // instead of the commonType as the return type.
-                    if (null != commonType) {
-                        coerced = typeCoercion.caseWhenCoercion(callBinding);
-                        if (coerced) {
-                            caseReturnType = callBinding.getValidator()
-                                    .deriveType(callBinding.getScope(), callBinding.getCall());
-                        }
-                    }
+                RelDataType highType = argTypes.get(0);
+                for (int i = 1; i < argTypes.size(); i++) {
+                    highType = HazelcastTypeUtils.withHigherPrecedence(highType, argTypes.get(i));
                 }
-                if (!coerced) {
-                    throw callBinding.newValidationError(RESOURCE.illegalMixingOfTypes());
+
+                QueryDataType highHZType = HazelcastTypeUtils.toHazelcastType(highType.getSqlTypeName());
+
+                boolean canConvert = true;
+                for (int i = 0, argTypesSize = argTypes.size(); i < argTypesSize; i++) {
+                    RelDataType type = argTypes.get(i);
+                    QueryDataType hzType = HazelcastTypeUtils.toHazelcastType(type.getSqlTypeName());
+
+                    SqlNode sqlNode = allReturnNodes.get(i);
+                    canConvert &= bothParametersAreNumeric(highHZType, hzType)
+                            || bothOperandsAreTemporalAndLowOperandCanBeConvertedToHighOperand(highHZType, hzType)
+                            || highOperandIsTemporalAndLowOperandIsLiteralOfVarcharType(highHZType, hzType, sqlNode);
                 }
+
+                if (!canConvert) {
+                    throw QueryException.error(SqlErrorCode.GENERIC, "Cannot infer return type for CASE among " + argTypes);
+                }
+                caseReturnType = highType;
             }
-            final SqlValidatorImpl validator =
-                    (SqlValidatorImpl) callBinding.getValidator();
+
+            final SqlValidatorImpl sqlValidator = (SqlValidatorImpl) validator;
             for (SqlNode node : nullList) {
-                validator.setValidatedNodeType(node, caseReturnType);
+                sqlValidator.setValidatedNodeType(node, caseReturnType);
             }
             return caseReturnType;
+        }
+
+        private static boolean bothParametersAreNumeric(QueryDataType highHZType, QueryDataType lowHZType) {
+            return (highHZType.getTypeFamily().isNumeric() && lowHZType.getTypeFamily().isNumeric());
+        }
+
+        private static boolean bothOperandsAreTemporalAndLowOperandCanBeConvertedToHighOperand(QueryDataType highHZType,
+                                                                                               QueryDataType lowHZType) {
+            return highHZType.getTypeFamily().isTemporal()
+                    && lowHZType.getTypeFamily().isTemporal()
+                    && lowHZType.getConverter().canConvertTo(highHZType.getTypeFamily());
+        }
+
+        private static boolean highOperandIsTemporalAndLowOperandIsLiteralOfVarcharType(QueryDataType highHZType,
+                                                                                        QueryDataType lowHZType, SqlNode low) {
+            return highHZType.getTypeFamily().isTemporal()
+                    && lowHZType.getTypeFamily() == QueryDataTypeFamily.VARCHAR
+                    && low instanceof SqlLiteral;
         }
     }
 }
--- hazelcast-sql-core/src/test/java/com/hazelcast/sql/impl/expression/CaseOperationIntegrationTest.java
+++ hazelcast-sql-core/src/test/java/com/hazelcast/sql/impl/expression/CaseOperationIntegrationTest.java
@@ -21,7 +21,21 @@ import com.hazelcast.sql.impl.SqlDataSerializerHook;
 import com.hazelcast.sql.impl.SqlErrorCode;
 import com.hazelcast.sql.impl.expression.predicate.ComparisonMode;
 import com.hazelcast.sql.impl.expression.predicate.ComparisonPredicate;
+import com.hazelcast.sql.support.expressions.ExpressionBiValue;
+import com.hazelcast.sql.support.expressions.ExpressionType;
+import com.hazelcast.sql.support.expressions.ExpressionTypes;
 import org.junit.Test;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 import static com.hazelcast.sql.impl.type.QueryDataType.INT;
 import static com.hazelcast.sql.impl.type.QueryDataType.VARCHAR;
@@ -78,12 +92,12 @@ public class CaseOperationIntegrationTest extends ExpressionTestSupport {
 
         checkFailure0(
                 "select case this when 2 then 100 when 1 then 'a' end from map",
-                SqlErrorCode.GENERIC,
-                "Cannot compare this[INTEGER] with 'a'[VARCHAR]");
+                SqlErrorCode.PARSING,
+                "Cannot infer return type for CASE among [TINYINT, VARCHAR, NULL]");
         checkFailure0(
                 "select case when 1 = 1 then 1 when 2 = 2 then 1000000000 else 'some string' end from map",
-                SqlErrorCode.GENERIC,
-                "Cannot infer return type of case operator among [TINYINT, INTEGER, VARCHAR]");
+                SqlErrorCode.PARSING,
+                "Cannot infer return type for CASE among [TINYINT, INTEGER, VARCHAR]");
     }
 
     @Test
@@ -97,6 +111,197 @@ public class CaseOperationIntegrationTest extends ExpressionTestSupport {
         checkValue0("select case when ? IS NOT NULL then 100 end from map", SqlColumnType.TINYINT, (byte) 100, 1);
         checkValue0("select case ? when this then 100 end from map", SqlColumnType.TINYINT, (byte) 100, 1);
         checkValue0("select case when ? = this then 100 end from map", SqlColumnType.TINYINT, (byte) 100, 1);
+    }
+
+    @Test
+    public void numericWithString() {
+        String sql = "select case when 1 = 1 then field1 else field2 end from map";
+
+        putBiValue((byte) 1, "str", ExpressionTypes.BYTE, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [TINYINT, VARCHAR]");
+
+        putBiValue((short) 1, "str", ExpressionTypes.SHORT, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [SMALLINT, VARCHAR]");
+
+        putBiValue(1, "str", ExpressionTypes.INTEGER, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [INTEGER, VARCHAR]");
+
+        putBiValue(1L, "str", ExpressionTypes.LONG, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [BIGINT, VARCHAR]");
+
+        putBiValue(BigDecimal.ONE, "str", ExpressionTypes.BIG_DECIMAL, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [DECIMAL(38, 38), VARCHAR]");
+
+        putBiValue(BigInteger.ONE, "str", ExpressionTypes.BIG_INTEGER, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [DECIMAL(38, 38), VARCHAR]");
+
+        putBiValue(1.0, "str", ExpressionTypes.DOUBLE, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [DOUBLE, VARCHAR]");
+
+        putBiValue(1.0f, "str", ExpressionTypes.FLOAT, ExpressionTypes.STRING);
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [REAL, VARCHAR]");
+    }
+
+    @Test
+    public void parameterWithString() {
+        String sql = "select case when 1 = 1 then ? else this end from map";
+        put("str");
+
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but TINYINT was found", (byte) 1);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but SMALLINT was found", (short) 1);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but INTEGER was found", 1);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but BIGINT was found", 1L);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but DECIMAL was found", BigDecimal.ONE);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but DECIMAL was found", BigInteger.ONE);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but DOUBLE was found", 1.0);
+        checkFailure0(sql, SqlErrorCode.DATA_EXCEPTION, "Parameter at position 0 must be of VARCHAR type, but REAL was found", 1.0f);
+    }
+
+    @Test
+    public void dateTimeLiterals() {
+        String sql = "select case\n"
+                + "        when true\n"
+                + "            then this\n"
+                + "            else '%s'\n"
+                + "        end\n"
+                + "from map";
+
+        LocalDate date = LocalDate.now();
+        put(date);
+        checkValue0(String.format(sql, date.minusDays(1)), SqlColumnType.DATE, date);
+
+        LocalTime time = LocalTime.now();
+        put(time);
+        checkValue0(String.format(sql, time.minusHours(1)), SqlColumnType.TIME, time);
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        put(dateTime);
+        checkValue0(String.format(sql, dateTime.minusHours(1)), SqlColumnType.TIMESTAMP, dateTime);
+    }
+
+    @Test
+    public void date_time_typeConversion() {
+        String sql = "select case when 1 = 1 then field1 else field2 end from map";
+
+        putBiValue(
+                LocalDate.of(2020, 12, 30),
+                LocalTime.of(14, 2, 0),
+                ExpressionTypes.LOCAL_DATE, ExpressionTypes.LOCAL_TIME
+        );
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [DATE, TIME]");
+
+        putBiValue(
+                LocalTime.of(14, 2, 0),
+                LocalDate.of(2020, 12, 30),
+                ExpressionTypes.LOCAL_TIME, ExpressionTypes.LOCAL_DATE
+        );
+        checkFailure0(sql, SqlErrorCode.PARSING, "Cannot infer return type for CASE among [TIME, DATE]");
+
+        putBiValue(
+                LocalDateTime.of(2020, 12, 30, 14, 2, 0),
+                LocalTime.of(14, 2, 0),
+                ExpressionTypes.LOCAL_DATE_TIME, ExpressionTypes.LOCAL_TIME
+        );
+        checkValue0(sql, SqlColumnType.TIMESTAMP, LocalDateTime.of(2020, 12, 30, 14, 2, 0));
+
+        putBiValue(
+                LocalTime.of(14, 2, 0),
+                LocalDateTime.of(2020, 12, 30, 14, 2, 0),
+                ExpressionTypes.LOCAL_TIME, ExpressionTypes.LOCAL_DATE_TIME
+        );
+        checkValue0(sql, SqlColumnType.TIMESTAMP, LocalDateTime.of(LocalDate.now(), LocalTime.of(14, 2, 0)));
+
+        putBiValue(
+                LocalDate.of(2020, 12, 30),
+                LocalDateTime.of(2020, 12, 30, 14, 2, 0),
+                ExpressionTypes.LOCAL_DATE, ExpressionTypes.LOCAL_DATE_TIME
+        );
+        checkValue0(sql, SqlColumnType.TIMESTAMP, LocalDate.of(2020, 12, 30).atStartOfDay());
+
+        putBiValue(
+                LocalDateTime.of(2020, 12, 30, 14, 2, 0),
+                LocalDate.of(2020, 12, 30),
+                ExpressionTypes.LOCAL_DATE_TIME, ExpressionTypes.LOCAL_DATE
+        );
+        checkValue0(sql, SqlColumnType.TIMESTAMP, LocalDateTime.of(2020, 12, 30, 14, 2, 0));
+
+        putBiValue(
+                OffsetDateTime.of(LocalDateTime.of(2020, 12, 30, 14, 2, 0), ZoneOffset.UTC),
+                LocalTime.of(14, 2, 0),
+                ExpressionTypes.OFFSET_DATE_TIME, ExpressionTypes.LOCAL_TIME
+        );
+        checkValue0(
+                sql, SqlColumnType.TIMESTAMP_WITH_TIME_ZONE,
+                OffsetDateTime.of(LocalDateTime.of(2020, 12, 30, 14, 2, 0), ZoneOffset.UTC));
+
+        putBiValue(
+                LocalTime.of(14, 2, 0),
+                OffsetDateTime.of(LocalDateTime.of(2020, 12, 30, 14, 2, 0), ZoneOffset.UTC),
+                ExpressionTypes.LOCAL_TIME, ExpressionTypes.OFFSET_DATE_TIME
+        );
+        checkValue0(
+                sql, SqlColumnType.TIMESTAMP_WITH_TIME_ZONE,
+                ZonedDateTime.of(LocalDateTime.of(LocalDate.now(), LocalTime.of(14, 2, 0)),
+                        ZoneId.systemDefault()).toOffsetDateTime());
+
+        putBiValue(
+                LocalDateTime.of(2020, 12, 30, 14, 2, 0),
+                OffsetDateTime.of(LocalDateTime.of(2020, 12, 30, 14, 2, 0), ZoneOffset.UTC),
+                ExpressionTypes.LOCAL_DATE_TIME, ExpressionTypes.OFFSET_DATE_TIME
+        );
+        checkValue0(sql, SqlColumnType.TIMESTAMP, LocalDateTime.of(2020, 12, 30, 14, 2, 0));
+
+        putBiValue(
+                OffsetDateTime.of(LocalDateTime.of(2020, 12, 30, 14, 2, 0), ZoneOffset.UTC),
+                LocalDateTime.of(2020, 12, 30, 14, 2, 0),
+                ExpressionTypes.OFFSET_DATE_TIME, ExpressionTypes.LOCAL_DATE_TIME
+        );
+        checkValue0(
+                sql, SqlColumnType.TIMESTAMP_WITH_TIME_ZONE,
+                OffsetDateTime.of(LocalDateTime.of(2020, 12, 30, 14, 2, 0), ZoneOffset.UTC));
+    }
+
+    @Test
+    public void numericWithObject() {
+        String sql = "select case when 1 = 1 then field1 else field2 end from map";
+        SerializableDummy field2 = new SerializableDummy();
+
+        putBiValue((byte) 1, field2, ExpressionTypes.BYTE, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, (byte) 1);
+
+        putBiValue((short) 1, field2, ExpressionTypes.SHORT, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, (short) 1);
+
+        putBiValue(1, field2, ExpressionTypes.INTEGER, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, 1);
+
+        putBiValue(1L, field2, ExpressionTypes.LONG, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, 1L);
+
+        putBiValue(BigDecimal.ONE, field2, ExpressionTypes.BIG_DECIMAL, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, BigDecimal.ONE);
+
+        putBiValue(BigInteger.ONE, field2, ExpressionTypes.BIG_INTEGER, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, BigDecimal.ONE);
+
+        putBiValue(1.0, field2, ExpressionTypes.DOUBLE, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, 1.0);
+
+        putBiValue(1.0f, field2, ExpressionTypes.FLOAT, ExpressionTypes.OBJECT);
+        checkValue0(sql, SqlColumnType.OBJECT, 1.0f);
+    }
+
+    private static class SerializableDummy implements Serializable {
+    }
+
+    private void putBiValue(Object field1, Object field2, ExpressionType<?> type1, ExpressionType<?> type2) {
+        ExpressionBiValue value = ExpressionBiValue.createBiValue(
+                ExpressionBiValue.createBiClass(type1, type2),
+                field1,
+                field2
+        );
+
+        put(value);
     }
 
     @Test
