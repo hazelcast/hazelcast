dbfafeee60b6202c78dfcd7f2f375ef2b398be01
sancar <sancar@hazelcast.com>
2021-02-05 12:32:13 +0300

    Make NearcacheConfig equal before and after it is used
    HazelcastClientFailover checks equality of configs when switching clusters.
    If a config is not equals to itself after it is used,
    HazelcastClientFailover equality check fails where it should not.
    This test is to make sure `NearCacheConfigAccessor` does not break
    the equality of NearCacheConfig.
    
    fixes https://github.com/hazelcast/hazelcast/issues/17952
    
    (cherry picked from commit 8df8a6ebd1a43c42dd3ae86d223b0d3b5fd51606)

--- hazelcast/src/test/java/com/hazelcast/config/NearCacheConfigAccessorTest.java
+++ hazelcast/src/test/java/com/hazelcast/config/NearCacheConfigAccessorTest.java
@@ -24,6 +24,8 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(HazelcastParallelClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class NearCacheConfigAccessorTest extends HazelcastTestSupport {
@@ -36,5 +38,25 @@ public class NearCacheConfigAccessorTest extends HazelcastTestSupport {
     @Test
     public void testInitDefaultMaxSizeForOnHeapMaps_whenNull_thenDoNothing() {
         NearCacheConfigAccessor.initDefaultMaxSizeForOnHeapMaps(null);
+    }
+
+    @Test
+    public void testNearCacheConfigEqual_BeforeAndAfterDefaultSizeIsInitialized() {
+        NearCacheConfig config1 = new NearCacheConfig();
+        NearCacheConfig config2 = new NearCacheConfig();
+        NearCacheConfigAccessor.initDefaultMaxSizeForOnHeapMaps(config1);
+
+        assertEquals(config1, config2);
+    }
+
+    @Test
+    public void testEvictionConfigCopy_whenDefaultSizeIsInitialized() {
+        NearCacheConfig config1 = new NearCacheConfig();
+        NearCacheConfigAccessor.initDefaultMaxSizeForOnHeapMaps(config1);
+
+        EvictionConfig evictionConfig = config1.getEvictionConfig();
+        EvictionConfig evictionConfig2 = new EvictionConfig(evictionConfig);
+
+        assertEquals(evictionConfig.getSize(), evictionConfig2.getSize());
     }
 }
