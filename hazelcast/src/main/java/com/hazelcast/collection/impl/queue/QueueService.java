f6568c49cd1c2c9af0eca47699c70be039606a19
Ahmet Mircik <ahmetmircik@gmail.com>
2021-02-05 17:07:25 +0300

    Address Sergey reviews

--- hazelcast/src/main/java/com/hazelcast/collection/impl/queue/QueueService.java
+++ hazelcast/src/main/java/com/hazelcast/collection/impl/queue/QueueService.java
@@ -208,7 +208,6 @@ public class QueueService implements ManagedService, MigrationAwareService, Tran
             QueueContainer container = entry.getValue();
 
             if (partitionId == event.getPartitionId() && container.getConfig().getTotalBackupCount() >= event.getReplicaIndex()) {
-                container.prepareForReplication();
                 migrationData.put(name, container);
             }
         }
