d0ee94c2fb888ceb37a6ea7928044326da300a99
mdumandag <metin@hazelcast.com>
2021-02-11 16:36:00 +0300

    Optimize CopyOnWriteArrayList/Set serializers
    As reported on https://github.com/hazelcast/hazelcast/issues/18129,
    adding deserialized elements one by one to the CoW data structures
    causes too many array copies.
    
    Instead, elements are deserialized into a pre-allocated ArrayList
    first. Then, the expected CoW data structure is created from
    that ArrayList.

--- hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/defaultserializers/CopyOnWriteArraySetStreamSerializer.java
+++ hazelcast/src/main/java/com/hazelcast/internal/serialization/impl/defaultserializers/CopyOnWriteArraySetStreamSerializer.java
@@ -20,6 +20,7 @@ import com.hazelcast.internal.serialization.impl.SerializationConstants;
 import com.hazelcast.nio.ObjectDataInput;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
@@ -36,8 +37,9 @@ public class CopyOnWriteArraySetStreamSerializer<E> extends AbstractCollectionSt
     public CopyOnWriteArraySet<E> read(ObjectDataInput in) throws IOException {
         int size = in.readInt();
 
-        CopyOnWriteArraySet<E> collection = new CopyOnWriteArraySet<>();
+        ArrayList<E> collection = new ArrayList<>(size);
+        deserializeEntriesInto(in, size, collection);
 
-        return deserializeEntries(in, size, collection);
+        return new CopyOnWriteArraySet<>(collection);
     }
 }
