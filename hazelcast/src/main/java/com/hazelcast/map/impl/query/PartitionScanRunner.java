hazelcast/hazelcast

commit e891589de7ad482c5ad5f2d874fdf59da76b1b0c

Use cached deserialized values in SQL when possible (#18172)

hazelcast/src/main/java/com/hazelcast/map/impl/query/PartitionScanRunner.java
@@ -16,7 +16,6 @@
 
 package com.hazelcast.map.impl.query;
 
-import com.hazelcast.config.CacheDeserializedValues;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.internal.cluster.ClusterService;
 import com.hazelcast.internal.iteration.IterationPointer;
@@ -82,7 +81,7 @@ public class PartitionScanRunner {
         MapContainer mapContainer = mapServiceContext.getMapContainer(mapName);
         RecordStore<Record> recordStore = partitionContainer.getRecordStore(mapName);
         boolean nativeMemory = recordStore.getInMemoryFormat() == InMemoryFormat.NATIVE;
-        boolean useCachedValues = isUseCachedDeserializedValuesEnabled(mapContainer, partitionId);
+        boolean useCachedValues = mapContainer.isUseCachedDeserializedValuesEnabled(partitionId);
         Extractors extractors = mapServiceContext.getExtractors(mapName);
         Map.Entry<Integer, Map.Entry> nearestAnchorEntry =
                 pagingPredicate == null ? null : pagingPredicate.getNearestAnchorEntry();
@@ -164,18 +163,5 @@ public class PartitionScanRunner {
             }
         }
         return new QueryableEntriesSegment(resultList, pointers);
-    }
-
-    protected boolean isUseCachedDeserializedValuesEnabled(MapContainer mapContainer, int partitionId) {
-        CacheDeserializedValues cacheDeserializedValues = mapContainer.getMapConfig().getCacheDeserializedValues();
-        switch (cacheDeserializedValues) {
-            case NEVER:
-                return false;
-            case ALWAYS:
-                return true;
-            default:
-                //if index exists then cached value is already set -> let's use it
-                return mapContainer.getIndexes(partitionId).haveAtLeastOneIndex();
-        }
     }
 }

hazelcast-sql-core/src/test/java/com/hazelcast/sql/misc/SqlNoSerializationTest.java
@@ -1,0 +1,206 @@
+/*
+ * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.sql.misc;
+
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.IndexConfig;
+import com.hazelcast.config.IndexType;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import com.hazelcast.sql.SqlResult;
+import com.hazelcast.sql.SqlRow;
+import com.hazelcast.sql.SqlTestInstanceFactory;
+import com.hazelcast.sql.impl.SqlTestSupport;
+import com.hazelcast.test.HazelcastSerialParametersRunnerFactory;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test that ensures that there are no unexpected serializations when executing a query on an IMap with the
+ * default {@link InMemoryFormat#BINARY} format.
+ */
+@RunWith(Parameterized.class)
+@Parameterized.UseParametersRunnerFactory(HazelcastSerialParametersRunnerFactory.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class SqlNoSerializationTest extends SqlTestSupport {
+
+    private static final String MAP_NAME = "map";
+    private static final int KEY_COUNT = 100;
+
+    @Parameterized.Parameter
+    public boolean useIndex;
+
+    private final SqlTestInstanceFactory factory = SqlTestInstanceFactory.create();
+    private HazelcastInstance member;
+
+    private static volatile boolean failOnSerialization;
+
+    @Parameterized.Parameters(name = "useIndex:{0}")
+    public static Collection<Object> parameters() {
+        return Arrays.asList(true, false);
+    }
+
+    @Before
+    public void before() {
+        member = factory.newHazelcastInstance();
+        factory.newHazelcastInstance();
+
+        Map<Key, Value> localMap = new HashMap<>();
+
+        for (int i = 0; i < KEY_COUNT; i++) {
+            localMap.put(new Key(i), new Value(i));
+        }
+
+        IMap<Key, Value> map = member.getMap(MAP_NAME);
+
+        map.putAll(localMap);
+
+        // An index may change the behavior due to MapContainer.isUseCachedDeserializedValuesEnabled.
+        if (useIndex) {
+            map.addIndex(new IndexConfig().setType(IndexType.SORTED).addAttribute("val"));
+        }
+
+        failOnSerialization = true;
+    }
+
+    @After
+    public void after() {
+        failOnSerialization = false;
+
+        factory.shutdownAll();
+    }
+
+    @Test
+    public void testMapScan() {
+        check("SELECT __key, this FROM " + MAP_NAME, 100);
+    }
+
+    @Test
+    public void testIndexScan() {
+        check("SELECT __key, this FROM " + MAP_NAME + " WHERE val = 1", 1);
+    }
+
+    private void check(String sql, int expectedCount) {
+        try (SqlResult res = member.getSql().execute(sql)) {
+            int count = 0;
+
+            for (SqlRow row : res) {
+                Object key = row.getObject(0);
+                Object value = row.getObject(1);
+
+                assertTrue(key instanceof Key);
+                assertTrue(value instanceof Value);
+
+                count++;
+            }
+
+            assertEquals(expectedCount, count);
+        }
+    }
+
+    public static class Key implements Externalizable {
+
+        public int key;
+
+        public Key() {
+            // No-op
+        }
+
+        private Key(int key) {
+            this.key = key;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeInt(key);
+
+            if (failOnSerialization) {
+                throw new IOException("Key serialization must not happen.");
+            }
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            key = in.readInt();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Key that = (Key) o;
+
+            return key == that.key;
+        }
+
+        @Override
+        public int hashCode() {
+            return key;
+        }
+    }
+
+    public static class Value implements Externalizable {
+
+        public int val;
+
+        public Value() {
+            // No-op
+        }
+
+        private Value(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeInt(val);
+
+            if (failOnSerialization) {
+                throw new IOException("Value serialization must not happen.");
+            }
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            val = in.readInt();
+        }
+    }
+}

hazelcast/src/main/java/com/hazelcast/map/impl/MapContainer.java
@@ -469,4 +469,17 @@ public class MapContainer {
             return ss.toData(input, partitioningStrategy);
         }
     }
+
+    public boolean isUseCachedDeserializedValuesEnabled(int partitionId) {
+        CacheDeserializedValues cacheDeserializedValues = getMapConfig().getCacheDeserializedValues();
+        switch (cacheDeserializedValues) {
+            case NEVER:
+                return false;
+            case ALWAYS:
+                return true;
+            default:
+                //if index exists then cached value is already set -> let's use it
+                return getIndexes(partitionId).haveAtLeastOneIndex();
+        }
+    }
 }

hazelcast/src/main/java/com/hazelcast/query/impl/CachedQueryEntry.java
@@ -73,6 +73,11 @@ public class CachedQueryEntry<K, V> extends QueryableEntry<K, V> {
     }
 
     @Override
+    public Data getKeyData() {
+        return keyData;
+    }
+
+    @Override
     public V getValue() {
         if (valueObject == null) {
             valueObject = serializationService.toObject(valueData);
@@ -81,16 +86,44 @@ public class CachedQueryEntry<K, V> extends QueryableEntry<K, V> {
     }
 
     @Override
-    public Data getKeyData() {
-        return keyData;
-    }
-
-    @Override
     public Data getValueData() {
         if (valueData == null) {
             valueData = serializationService.toData(valueObject);
         }
         return valueData;
+    }
+
+    @Override
+    public K getKeyIfPresent() {
+        return keyObject != null ? keyObject : null;
+    }
+
+    @Override
+    public Data getKeyDataIfPresent() {
+        return keyData;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public V getValueIfPresent() {
+        if (valueObject != null) {
+            return valueObject;
+        }
+
+        Object possiblyNotData = record.getValue();
+
+        return possiblyNotData instanceof Data ? null : (V) possiblyNotData;
+    }
+
+    @Override
+    public Data getValueDataIfPresent() {
+        if (valueData != null) {
+            return valueData;
+        }
+
+        Object possiblyData = record.getValue();
+
+        return possiblyData instanceof Data ? (Data) possiblyData : null;
     }
 
     public Object getByPrioritizingDataValue() {

hazelcast/src/main/java/com/hazelcast/query/impl/QueryEntry.java
@@ -16,8 +16,8 @@
 
 package com.hazelcast.query.impl;
 
-import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.internal.serialization.Data;
+import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.query.impl.getters.Extractors;
 
 /**
@@ -70,18 +70,50 @@ public class QueryEntry extends QueryableEntry {
     }
 
     @Override
-    public Object getValue() {
-        return serializationService.toObject(value);
-    }
-
-    @Override
     public Data getKeyData() {
         return key;
     }
 
     @Override
+    public Object getValue() {
+        return serializationService.toObject(value);
+    }
+
+    @Override
     public Data getValueData() {
         return serializationService.toData(value);
+    }
+
+    @Override
+    public Object getKeyIfPresent() {
+        return null;
+    }
+
+    @Override
+    public Data getKeyDataIfPresent() {
+        return key;
+    }
+
+    @Override
+    public Object getValueIfPresent() {
+        if (!(value instanceof Data)) {
+            return value;
+        }
+
+        Object possiblyNotData = record.getValue();
+
+        return possiblyNotData instanceof Data ? null : possiblyNotData;
+    }
+
+    @Override
+    public Data getValueDataIfPresent() {
+        if (value instanceof Data) {
+            return (Data) value;
+        }
+
+        Object possiblyData = record.getValue();
+
+        return possiblyData instanceof Data ? (Data) possiblyData : null;
     }
 
     @Override

hazelcast/src/main/java/com/hazelcast/query/impl/QueryableEntry.java
@@ -46,7 +46,7 @@ public abstract class QueryableEntry<K, V> implements Extractable, Map.Entry<K, 
     protected InternalSerializationService serializationService;
     protected Extractors extractors;
 
-    private Record record;
+    protected Record record;
     private transient Metadata metadata;
 
     public Record getRecord() {
@@ -62,13 +62,15 @@ public abstract class QueryableEntry<K, V> implements Extractable, Map.Entry<K, 
         return extractAttributeValue(attributeName);
     }
 
-    public abstract V getValue();
-
     public abstract K getKey();
-
     public abstract Data getKeyData();
-
+    public abstract V getValue();
     public abstract Data getValueData();
+
+    public abstract K getKeyIfPresent();
+    public abstract Data getKeyDataIfPresent();
+    public abstract V getValueIfPresent();
+    public abstract Data getValueDataIfPresent();
 
     protected abstract Object getTargetObject(boolean key);
 

hazelcast/src/main/java/com/hazelcast/sql/impl/exec/scan/AbstractMapScanExec.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.sql.impl.exec.scan;
 
+import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.query.impl.getters.Extractors;
 import com.hazelcast.sql.impl.SqlErrorCode;
@@ -52,8 +53,7 @@ public abstract class AbstractMapScanExec extends AbstractExec {
     protected final List<QueryDataType> fieldTypes;
     protected final List<Integer> projects;
     protected final Expression<Boolean> filter;
-
-    private final InternalSerializationService serializationService;
+    protected final InternalSerializationService serializationService;
 
     private int migrationStamp;
     private KeyValueIterator recordIterator;
@@ -104,7 +104,12 @@ public abstract class AbstractMapScanExec extends AbstractExec {
         currentRows = null;
 
         while (recordIterator.tryAdvance()) {
-            Row row = prepareRow(recordIterator.getKey(), recordIterator.getValue());
+            Row row = prepareRow(
+                recordIterator.getKey(),
+                recordIterator.getKeyData(),
+                recordIterator.getValue(),
+                recordIterator.getValueData()
+            );
 
             if (row != null) {
                 if (currentRows == null) {
@@ -165,12 +170,14 @@ public abstract class AbstractMapScanExec extends AbstractExec {
      * 1) Check filter
      * 2) Extract projections
      *
-     * @param rawKey Key (data or object)
-     * @param rawValue Value (data or object)
+     * @param rawKey key as object, might be null
+     * @param rawKeyData key as data, might be null
+     * @param rawValue value as object, might be null
+     * @param rawValueData value as data, might be null
      * @return Row that is ready for processing by parent operators or {@code null} if the row hasn't passed the filter.
      */
-    protected Row prepareRow(Object rawKey, Object rawValue) {
-        row.setKeyValue(rawKey, rawValue);
+    protected Row prepareRow(Object rawKey, Data rawKeyData, Object rawValue, Data rawValueData) {
+        row.setKeyValue(rawKey, rawKeyData, rawValue, rawValueData);
 
         // Filter.
         if (filter != null && TernaryLogic.isNotTrue(filter.evalTop(row, ctx))) {

hazelcast/src/main/java/com/hazelcast/sql/impl/exec/scan/KeyValueIterator.java
@@ -16,6 +16,8 @@
 
 package com.hazelcast.sql.impl.exec.scan;
 
+import com.hazelcast.internal.serialization.Data;
+
 /**
  * Iterator over key/value pairs.
  */
@@ -36,13 +38,8 @@ public interface KeyValueIterator {
      */
     boolean done();
 
-    /**
-     * @return current key
-     */
     Object getKey();
-
-    /**
-     * @return current value
-     */
+    Data getKeyData();
     Object getValue();
+    Data getValueData();
 }

commit 55978647f9948120f05683cf60fe13896cf2a2fb

Fix compilation error in HD caused by #18172

hazelcast/src/main/java/com/hazelcast/map/impl/query/PartitionScanRunner.java
@@ -81,7 +81,7 @@ public class PartitionScanRunner {
         MapContainer mapContainer = mapServiceContext.getMapContainer(mapName);
         RecordStore<Record> recordStore = partitionContainer.getRecordStore(mapName);
         boolean nativeMemory = recordStore.getInMemoryFormat() == InMemoryFormat.NATIVE;
-        boolean useCachedValues = mapContainer.isUseCachedDeserializedValuesEnabled(partitionId);
+        boolean useCachedValues = isUseCachedDeserializedValuesEnabled(mapContainer, partitionId);
         Extractors extractors = mapServiceContext.getExtractors(mapName);
         Map.Entry<Integer, Map.Entry> nearestAnchorEntry =
                 pagingPredicate == null ? null : pagingPredicate.getNearestAnchorEntry();
@@ -163,5 +163,9 @@ public class PartitionScanRunner {
             }
         }
         return new QueryableEntriesSegment(resultList, pointers);
+    }
+
+    protected boolean isUseCachedDeserializedValuesEnabled(MapContainer mapContainer, int partitionId) {
+        return mapContainer.isUseCachedDeserializedValuesEnabled(partitionId);
     }
 }