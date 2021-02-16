1b4a6f65f176454c9fd5818bbb2b19dc2e18b46d
Frantisek Hartman <frant.hartm@gmail.com>
2021-02-16 23:49:15 +0100

    AFTER CORE: Update banner

--- hazelcast/src/main/java/com/hazelcast/jet/impl/NodeExtensionCommon.java
+++ hazelcast/src/main/java/com/hazelcast/jet/impl/NodeExtensionCommon.java
@@ -36,11 +36,12 @@ import static com.hazelcast.jet.impl.util.ExceptionUtil.rethrow;
 
 class NodeExtensionCommon {
     private static final String JET_LOGO =
-            "\to   o   o   o---o o---o o     o---o   o   o---o o-o-o        o o---o o-o-o\n" +
-            "\t|   |  / \\     /  |     |     |      / \\  |       |          | |       |\n" +
-            "\to---o o---o   o   o-o   |     o     o---o o---o   |          | o-o     |\n" +
-            "\t|   | |   |  /    |     |     |     |   |     |   |      \\   | |       |\n" +
-            "\to   o o   o o---o o---o o---o o---o o   o o---o   o       o--o o---o   o";
+            "\to  o   O  o---o o--o o      o-o   O   o-o  o-O-o     o--o  o      O  o-O-o o--o  o-o  o--o  o   o \n" +
+            "\t|  |  / \\    /  |    |     /     / \\ |       |       |   | |     / \\   |   |    o   o |   | |\\ /| \n" +
+            "\tO--O o---o -O-  O-o  |    O     o---o o-o    |       O--o  |    o---o  |   O-o  |   | O-Oo  | O | \n" +
+            "\t|  | |   | /    |    |     \\    |   |    |   |       |     |    |   |  |   |    o   o |  \\  |   | \n" +
+            "\to  o o   oo---o o--o O---o  o-o o   oo--o    o       o     O---oo   o  o   o     o-o  o   o o   o";
+
     private static final String COPYRIGHT_LINE = "Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.";
 
     private final Node node;
