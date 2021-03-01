a780a5d6b31da8f6e7aafb3418bb5a23037a5848
Alex Dukhno <alex.dukhno@icloud.com>
2021-03-01 13:22:36 +0200

    add test to cover map with index and without

--- hazelcast/src/test/java/com/hazelcast/map/impl/query/QueryBasicTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/query/QueryBasicTest.java
@@ -791,6 +791,37 @@ public class QueryBasicTest extends HazelcastTestSupport {
     }
 
     @Test
+    public void testLikePredicate_withAndWithoutIndexOnMap() {
+        TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(2);
+        HazelcastInstance instance = factory.newHazelcastInstance(getConfig());
+        factory.newHazelcastInstance(getConfig());
+        IMap<Integer, Employee> withIndex = instance.getMap("withIndex");
+        withIndex.addIndex(IndexType.SORTED, "name");
+        IMap<Integer, Employee> withoutIndex = instance.getMap("withoutIndex");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int employeeId = i * 3 + j;
+                String employeeName = String.format("name%d%d", i, j);
+                Employee employee = new Employee(employeeId, employeeName, "city" + i + j, employeeId, true, employeeId);
+                withIndex.put(employeeId, employee);
+                withoutIndex.put(employeeId, employee);
+            }
+        }
+
+        assertEquals(withIndex.size(), 9);
+        assertEquals(withoutIndex.size(), 9);
+
+        Predicate<Integer, Employee> predicate = Predicates.like("name", "name1%");
+        Collection<Employee> namesByIndex = withIndex.values(predicate);
+        Collection<Employee> namesMapLookup = withoutIndex.values(predicate);
+
+        assertEquals(namesMapLookup.size(), 3);
+        assertEquals(namesByIndex.size(), 3);
+        assertEquals(namesMapLookup, namesByIndex);
+    }
+
+    @Test
     public void testSqlQueryUsing__KeyField() {
         Config config = getConfig();
         TestHazelcastInstanceFactory factory = createHazelcastInstanceFactory(2);
