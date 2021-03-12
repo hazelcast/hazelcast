b4344f9ba6205181bf7760b09f68b7803cea142f
Grzegorz Gierlach <grzegorz.gierlach@hazelcast.com>
2021-03-12 13:56:03 +0100

    Propagate interrupts to test statements (#18393)

--- hazelcast/src/test/java/com/hazelcast/test/FailOnTimeoutStatement.java
+++ hazelcast/src/test/java/com/hazelcast/test/FailOnTimeoutStatement.java
@@ -82,7 +82,8 @@ public class FailOnTimeoutStatement extends Statement {
                 return task.get();
             }
         } catch (InterruptedException e) {
-            // caller will re-throw; no need to call Thread.interrupt()
+            // propagate interrupt
+            thread.interrupt();
             return e;
         } catch (ExecutionException e) {
             // test failed; have caller re-throw the exception thrown by the test
