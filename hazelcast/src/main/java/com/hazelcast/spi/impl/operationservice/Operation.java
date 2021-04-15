567bcc97371da413da478d9778cba286f6ddce09
Ahmet Mircik <ahmetmircik@gmail.com>
2021-04-15 12:39:14 +0300

    do not copy value if it is client call

--- hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/Operation.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/Operation.java
@@ -70,6 +70,7 @@ public abstract class Operation implements DataSerializable, Tenantable {
     static final int BITMASK_CALL_TIMEOUT_64_BIT = 1 << 5;
     static final int BITMASK_SERVICE_NAME_SET = 1 << 6;
     static final int BITMASK_CLIENT_CALL_ID_SET = 1 << 7;
+    static final int BITMASK_OP_SEND_BY_CLIENT = 1 << 8;
 
     private static final AtomicLongFieldUpdater<Operation> CALL_ID =
             AtomicLongFieldUpdater.newUpdater(Operation.class, "callId");
@@ -107,6 +108,14 @@ public abstract class Operation implements DataSerializable, Tenantable {
 
     public long getClientCallId() {
         return clientCallId;
+    }
+
+    public final void setOpSentByClient(boolean value) {
+        setFlag(value, BITMASK_OP_SEND_BY_CLIENT);
+    }
+
+    protected final boolean isOpSentByClient() {
+        return isFlagSet(BITMASK_OP_SEND_BY_CLIENT);
     }
 
     /**
