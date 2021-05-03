939c286b9c2c6b9e53a72296b01bd2dde3d2a539
Bence Erős <erosb@hazelcast.com>
2021-05-03 09:53:08 +0200

    reorganizing test structure to support client-only and member-only and common tests

--- hazelcast/src/test/java/com/hazelcast/config/AbstractYamlSchemaTest.java
+++ hazelcast/src/test/java/com/hazelcast/config/AbstractYamlSchemaTest.java
@@ -1,34 +1,13 @@
-/*
- * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hazelcast.config;
 
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
-import com.hazelcast.test.annotation.ParallelJVMTest;
-import com.hazelcast.test.annotation.QuickTest;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.ValidationException;
-import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
@@ -44,43 +23,35 @@ import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-@RunWith(Parameterized.class)
-@Category({QuickTest.class, ParallelJVMTest.class})
-public class YamlSchemaTest {
+public abstract class AbstractYamlSchemaTest {
 
-    private static final ILogger LOGGER = Logger.getLogger(YamlSchemaTest.class);
-
-    public static final Schema SCHEMA = SchemaLoader.builder()
-            .schemaJson(readJSONObject("/hazelcast-config-5.0.json"))
-            .build()
-            .load().build();
-    public static final String TESTCASE_ROOT_DIR = "com/hazelcast/config/yaml/testcases/";
-
-    private static JSONObject readJSONObject(String absPath) {
-        return new JSONObject(new JSONTokener(YamlSchemaTest.class.getResourceAsStream(absPath)));
+    private static final ILogger LOGGER = Logger.getLogger(AbstractYamlSchemaTest.class);
+    
+    static JSONObject readJSONObject(String absPath) {
+        return new JSONObject(new JSONTokener(MemberYamlSchemaTest.class.getResourceAsStream(absPath)));
     }
 
-    @Parameterized.Parameters(name = "{0}")
-    public static List<Object[]> buildTestcases() {
+    protected static List<Object[]> buildTestcases(String rootDir) {
         ConfigurationBuilder configuration = new ConfigurationBuilder()
                 .setUrls(ClasspathHelper.forJavaClassPath())
                 .setScanners(new ResourcesScanner());
         Reflections reflections = new Reflections(configuration);
         return reflections.getResources(Pattern.compile(".*\\.json")).stream()
-                .filter(e -> e.startsWith(TESTCASE_ROOT_DIR))
-                .map(path -> buildArgs(path))
+                .filter(e -> e.startsWith(rootDir))
+                .map(path -> buildArgs(rootDir, path))
                 .collect(toList());
     }
 
-    private static Object[] buildArgs(String path) {
+    private static Object[] buildArgs(String rootDir, String path) {
         JSONObject testcase = readJSONObject("/" + path);
         Object error = testcase.get("error");
         if (error == JSONObject.NULL) {
             error = null;
         }
-        String testName = path.substring(TESTCASE_ROOT_DIR.length(), path.length() - 5);
+        String testName = path.substring(rootDir.length(), path.length() - 5);
         return new Object[]{testName, testcase.getJSONObject("instance"), error};
     }
+
 
     private void sortCauses(JSONObject exc) {
         JSONArray causes = exc.optJSONArray("causingExceptions");
@@ -108,7 +79,8 @@ public class YamlSchemaTest {
     @Test
     public void runTest() {
         try {
-            SCHEMA.validate(input);
+            Schema schema = getSchema();
+            schema.validate(input);
             if (expectedValidationError != null) {
                 fail("did not throw exception");
             }
@@ -124,5 +96,7 @@ public class YamlSchemaTest {
             }
         }
     }
+
+    protected abstract Schema getSchema();
 
 }
---
+++ hazelcast/src/test/java/com/hazelcast/config/ClientYamlSchemaTest.java
@@ -0,0 +1,50 @@
+package com.hazelcast.config;
+
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaClient;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+@RunWith(Parameterized.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class ClientYamlSchemaTest
+        extends AbstractYamlSchemaTest {
+
+    public static final Schema SCHEMA = SchemaLoader.builder()
+            .schemaJson(readJSONObject("/hazelcast-config-5.0.json"))
+            .draftV6Support()
+            .schemaClient(SchemaClient.classPathAwareClient())
+            .build()
+            .load().build();
+
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> buildTestcases() {
+        return Stream.concat(
+                buildTestcases("com/hazelcast/config/yaml/testcases/client").stream(),
+                buildTestcases("com/hazelcast/config/yaml/testcases/common").stream().map(ClientYamlSchemaTest::changeInputRootElem)
+        ).collect(toList());
+    }
+
+    private static Object[] changeInputRootElem(Object[] args) {
+        JSONObject input = (JSONObject) args[1];
+        Object hazelcastValue = input.getJSONObject("hazelcast");
+        input.remove("hazelcast");
+        input.put("hazelcast-client", hazelcastValue);
+        return new Object[]{args[0], input, args[2]};
+    }
+
+    @Override
+    protected Schema getSchema() {
+        return SCHEMA;
+    }
+}
