a58ae9049e7cfc17305ea60d8c9c7b0fbb008a7c
sancar <sancar@hazelcast.com>
2021-05-04 11:11:08 +0300

    Remove client endpoint on connection remove (#18562)
    Client endpoints were not destroyed on connection remove but
    instead a periodic task was cleaning them up(every 10 seconds)
    This was causing some tests to take long.
    Added client connections to server connection manager so that
    connectionRemoved can be called and related endpoint is
    removed as soon as the connection is removed.
    
    While adding the test, I have realized that we were registering
    client connection to the server on the mock connection manager.
    This behaviour is different than actual one. This was hiding the
    bug on mock tests. Removed that registration. The actual
    registration is done while authentication.
    
    fixes #18318
    
    (cherry picked from commit 93b9c37365bcc41c3cd921c37cd16562107ed018)

--- hazelcast/src/test/java/com/hazelcast/client/listeners/AbstractListenersOnReconnectTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/listeners/AbstractListenersOnReconnectTest.java
@@ -76,6 +76,30 @@ public abstract class AbstractListenersOnReconnectTest extends ClientTestSupport
         factory.terminateAll();
     }
 
+    //-------------------------- testListenersWhenClientIsGone --------------------- //
+
+    @Test
+    public void testListenersWhenClientIsGone_smart() {
+        testListenersWhenClientIsGone(true);
+    }
+
+    @Test
+    public void testListenersWhenClientIsGone_unisocket() {
+        testListenersWhenClientIsGone(false);
+    }
+
+    private void testListenersWhenClientIsGone(boolean isSmartClient) {
+        factory.newInstances(null, 2);
+        ClientConfig clientConfig = createClientConfig(isSmartClient);
+        client = factory.newHazelcastClient(clientConfig);
+
+        setupListener();
+
+        client.shutdown();
+
+        validateRegistrationsOnMembers(factory, 0);
+    }
+
     //-------------------------- testListenersTerminateRandomNode --------------------- //
     @Test
     public void testListenersTerminateRandomNode_smart() {
@@ -279,7 +303,7 @@ public abstract class AbstractListenersOnReconnectTest extends ClientTestSupport
         assertOpenEventually(connectedLatch);
         setupListener();
 
-        validateRegistrationsOnMembers(factory);
+        validateRegistrationsOnMembers(factory, 1);
 
         for (HazelcastInstance instance : factory.getAllHazelcastInstances()) {
             instance.getLifecycleService().terminate();
@@ -304,13 +328,13 @@ public abstract class AbstractListenersOnReconnectTest extends ClientTestSupport
 
     private void validateRegistrationsAndListenerFunctionality() {
         assertClusterSizeEventually(clusterSize, client);
-        validateRegistrationsOnMembers(factory);
+        validateRegistrationsOnMembers(factory, 1);
         validateRegistrations(clusterSize, registrationId, getHazelcastClientInstanceImpl(client));
         validateListenerFunctionality();
         assertTrue(removeListener(registrationId));
     }
 
-    protected void validateRegistrationsOnMembers(final TestHazelcastFactory factory) {
+    protected void validateRegistrationsOnMembers(final TestHazelcastFactory factory, int expected) {
         assertTrueEventually(() -> {
             for (HazelcastInstance instance : factory.getAllHazelcastInstances()) {
                 NodeEngineImpl nodeEngineImpl = getNodeEngineImpl(instance);
@@ -319,7 +343,7 @@ public abstract class AbstractListenersOnReconnectTest extends ClientTestSupport
                 Member member = instance.getCluster().getLocalMember();
                 assertNotNull(member.toString(), serviceSegment);
                 ConcurrentMap registrationIdMap = serviceSegment.getRegistrationIdMap();
-                assertEquals(member.toString() + " Current registrations:" + registrationIdMap, 1,
+                assertEquals(member.toString() + " Current registrations:" + registrationIdMap, expected,
                         registrationIdMap.size());
                 ILogger logger = nodeEngineImpl.getLogger(AbstractListenersOnReconnectTest.class);
                 logger.warning("Current registrations at member " + member.toString() + ": " + registrationIdMap);
