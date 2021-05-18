24f28da640735994803b0aec4d11fd233f75b0b4
Viliam Durina <viliam-durina@users.noreply.github.com>
2021-05-18 10:12:03 +0200

    Fix the fix

--- hazelcast/src/main/java/com/hazelcast/jet/impl/connector/WriteMapP.java
+++ hazelcast/src/main/java/com/hazelcast/jet/impl/connector/WriteMapP.java
@@ -16,15 +16,22 @@
 
 package com.hazelcast.jet.impl.connector;
 
+import com.hazelcast.client.impl.proxy.ClientMapProxy;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.function.FunctionEx;
+import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.internal.serialization.SerializationService;
+import com.hazelcast.jet.JetException;
 import com.hazelcast.jet.config.EdgeConfig;
 import com.hazelcast.jet.core.Inbox;
 import com.hazelcast.jet.core.Outbox;
 import com.hazelcast.jet.core.Processor;
 import com.hazelcast.jet.impl.connector.HazelcastWriters.ArrayMap;
+import com.hazelcast.jet.impl.serialization.DelegatingSerializationService;
 import com.hazelcast.map.IMap;
+import com.hazelcast.map.impl.proxy.MapProxyImpl;
+import com.hazelcast.map.impl.proxy.NearCachedMapProxyImpl;
+import com.hazelcast.partition.PartitioningStrategy;
 
 import javax.annotation.Nonnull;
 import java.util.AbstractMap.SimpleEntry;
@@ -37,22 +44,25 @@ public final class WriteMapP<T, K, V> extends AsyncHazelcastWriterP {
     private static final int BUFFER_LIMIT = 1024;
 
     private final String mapName;
+    private final SerializationService serializationService;
     private final FunctionEx<? super T, ? extends K> toKeyFn;
     private final FunctionEx<? super T, ? extends V> toValueFn;
 
-    private ArrayMap<K, V> buffer;
-    private IMap<K, V> map;
+    private ArrayMap<Object, Object> buffer;
+    private IMap<Object, Object> map;
     private Consumer<T> addToBuffer;
 
     private WriteMapP(
             @Nonnull HazelcastInstance instance,
             int maxParallelAsyncOps,
             String mapName,
+            @Nonnull SerializationService serializationService,
             @Nonnull FunctionEx<? super T, ? extends K> toKeyFn,
             @Nonnull FunctionEx<? super T, ? extends V> toValueFn
     ) {
         super(instance, maxParallelAsyncOps);
         this.mapName = mapName;
+        this.serializationService = serializationService;
         this.toKeyFn = toKeyFn;
         this.toValueFn = toValueFn;
 
@@ -62,8 +72,43 @@ public final class WriteMapP<T, K, V> extends AsyncHazelcastWriterP {
     @Override
     public void init(@Nonnull Outbox outbox, @Nonnull Context context) {
         map = instance().getMap(mapName);
-        addToBuffer = item -> buffer.add(
-                new SimpleEntry<>(toKeyFn.apply(item), toValueFn.apply(item)));
+
+        boolean hasCustomSerializers = ((DelegatingSerializationService) serializationService).hasAddedSerializers();
+        boolean hasNearCache = map instanceof NearCachedMapProxyImpl;
+        if (hasNearCache && hasCustomSerializers) {
+            // To invalidate NearCache, the `putAll` method would need a deserialized key. It can't deserialize
+            // because it doesn't know the job-specific serializers.
+            // See https://github.com/hazelcast/hazelcast-jet/issues/3046
+            throw new JetException("Writing into IMap with both near cache and custom serializers not supported");
+        }
+
+        if (!hasCustomSerializers) {
+            addToBuffer = item -> buffer.add(new SimpleEntry<>(key(item), value(item)));
+        } else if (map instanceof MapProxyImpl) {
+            PartitioningStrategy<?> partitionStrategy = ((MapProxyImpl<K, V>) map).getPartitionStrategy();
+            addToBuffer = item -> {
+                Data key = serializationService.toData(key(item), partitionStrategy);
+                Data value = serializationService.toData(value(item));
+                buffer.add(new SimpleEntry<>(key, value));
+            };
+        } else if (map instanceof ClientMapProxy) {
+            // TODO: add strategy/unify after https://github.com/hazelcast/hazelcast/issues/13950 is fixed
+            addToBuffer = item -> {
+                Data key = serializationService.toData(key(item));
+                Data value = serializationService.toData(value(item));
+                buffer.add(new SimpleEntry<>(key, value));
+            };
+        } else {
+            throw new RuntimeException("Unexpected map class: " + map.getClass().getName());
+        }
+    }
+
+    private K key(T item) {
+        return toKeyFn.apply(item);
+    }
+
+    private V value(T item) {
+        return toValueFn.apply(item);
     }
 
     @Override
@@ -127,7 +172,7 @@ public final class WriteMapP<T, K, V> extends AsyncHazelcastWriterP {
 
         @Override
         protected Processor createProcessor(HazelcastInstance instance, SerializationService serializationService) {
-            return new WriteMapP<>(instance, maxParallelAsyncOps, mapName, toKeyFn, toValueFn);
+            return new WriteMapP<>(instance, maxParallelAsyncOps, mapName, serializationService, toKeyFn, toValueFn);
         }
     }
 }
