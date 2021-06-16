4b50c309e0422669a1b39ec8e24e65bc7dd894dd
ivan.yaschishin <ivan.yaschishin@hazelcast.com>
2021-06-16 14:57:31 +0300

    HZ-397 - added clarifying comment

--- hazelcast/src/main/java/com/hazelcast/sql/impl/expression/datetime/ToTimestampTzFunction.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/expression/datetime/ToTimestampTzFunction.java
@@ -31,9 +31,9 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 
 public final class ToTimestampTzFunction extends UniExpression<OffsetDateTime> implements IdentifiedDataSerializable {
-    private static final long YEAR_MILLISECONDS = 31536000000L;
-    private static final long YEAR_MICROSECONDS = YEAR_MILLISECONDS * 1000L;
-    private static final long YEAR_NANOSECONDS = YEAR_MICROSECONDS * 1000L;
+    private static final long MILLISECONDS_IN_YEAR = 31536000000L;
+    private static final long MICROSECONDS_IN_YEAR = MILLISECONDS_IN_YEAR * 1000L;
+    private static final long NANOSECONDS_IN_YEAR = MICROSECONDS_IN_YEAR * 1000L;
 
     public ToTimestampTzFunction() { }
 
@@ -73,12 +73,37 @@ public final class ToTimestampTzFunction extends UniExpression<OffsetDateTime> i
         return QueryDataType.TIMESTAMP_WITH_TZ_OFFSET_DATE_TIME;
     }
 
+    /**
+     * Returns TemporalUnit of the given unixTimestamp, using the magnitude of the value to determine most likely unit.
+     * - unixTimestamp < MILLISECONDS_IN_YEAR (365 * 86400 * 1000) - timestamp is in Seconds
+     * - unixTimestamp >= MILLISECONDS_IN_YEAR (365 * 86400 * 1000) - timestamp in in Milliseconds
+     * - unixTimestamp >= MICROSECONDS_IN_YEAR (365 * 86400 * 1000_000) - timestamp is in Microseconds
+     * - unixTimestamp >= NANOSECONDS_IN_YEAR (365 * 86400 * 1000_000_000) - timestamp is in Nanoseconds
+     * - unixTimestamp < 0 - timestamp is in Seconds
+     *
+     * Note that this also imposes limits on the possible max correctly interpreted timestamp value,
+     * that would be otherwise correctly interpreted as in Seconds/Milliseconds/Microseconds.
+     * For example if this function is called on a unixTimestamp of 31_536_000_001 which in seconds
+     * corresponds to datetime of 2969-05-03 00:00:01, this function would instead interpret it
+     * as 1971-01-01 00:00:00.001. This is also true for maximum date expressed in milliseconds and microseconds.
+     * Additionally this of course also imposes the min date possible to be interpreted in Milliseconds, Microseconds and
+     * Nanoseconds.
+     *
+     * Summary of Date limits (under which the interpretation of the timestamp's unit is correct):
+     * 1. Seconds:      1970-01-01 00:00:00Z   (0)                    - 2969-05-02 23:59:59Z (MILLISECONDS_IN_YEAR - 1)
+     * 2. Milliseconds: 1971-01-01 00:00:00.0Z (MILLISECONDS_IN_YEAR) - 2969-05-02 23:59:59.999Z (MICROSECONDS_IN_YEAR - 1)
+     * 3. Microseconds: 1971-01-01 00:00:00.0Z (MICROSECONDS_IN_YEAR) - 2969-05-02 23:59:59.999999Z (NANOSECONDS_IN_YEAR - 1)
+     * 4. Nanoseconds:  1971-01-01 00:00:00.0Z (NANOSECONDS_IN_YEAR)  - 2262-04-11 23:47:16.854775807Z (Long.MAX_VALUE of nanoseconds).
+     *
+     * @param unixTimestamp - input timestamp
+     * @return - determined {@link TemporalUnit} of the value, inferred based on the magnitude of the input unixTimestamp
+     */
     private TemporalUnit getChronoUnit(final long unixTimestamp) {
-        if (unixTimestamp < YEAR_MILLISECONDS) {
+        if (unixTimestamp < MILLISECONDS_IN_YEAR) {
             return ChronoUnit.SECONDS;
-        } else if (unixTimestamp < YEAR_MICROSECONDS) {
+        } else if (unixTimestamp < MICROSECONDS_IN_YEAR) {
             return ChronoUnit.MILLIS;
-        } else if (unixTimestamp < YEAR_NANOSECONDS) {
+        } else if (unixTimestamp < NANOSECONDS_IN_YEAR) {
             return ChronoUnit.MICROS;
         } else {
             return ChronoUnit.NANOS;
