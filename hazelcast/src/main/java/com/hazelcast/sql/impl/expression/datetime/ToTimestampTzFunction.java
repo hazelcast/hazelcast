41b1b37590aa033496b2b92fa8551a6d8fa57742
ivan.yaschishin <ivan.yaschishin@hazelcast.com>
2021-06-16 18:53:26 +0300

    HZ-397 - updated in-code doc

--- hazelcast/src/main/java/com/hazelcast/sql/impl/expression/datetime/ToTimestampTzFunction.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/expression/datetime/ToTimestampTzFunction.java
@@ -74,30 +74,47 @@ public final class ToTimestampTzFunction extends UniExpression<OffsetDateTime> i
     }
 
     /**
-     * Returns TemporalUnit of the given unixTimestamp, using the magnitude of the value to determine most likely unit.
-     * - unixTimestamp < MILLISECONDS_IN_YEAR (365 * 86400 * 1000) - timestamp is in Seconds
-     * - unixTimestamp >= MILLISECONDS_IN_YEAR (365 * 86400 * 1000) - timestamp in in Milliseconds
-     * - unixTimestamp >= MICROSECONDS_IN_YEAR (365 * 86400 * 1000_000) - timestamp is in Microseconds
-     * - unixTimestamp >= NANOSECONDS_IN_YEAR (365 * 86400 * 1000_000_000) - timestamp is in Nanoseconds
-     * - unixTimestamp < 0 - timestamp is in Seconds
+     * Returns TemporalUnit of the given unixTimestamp, using the
+     * magnitude of the value to determine most likely unit.
+     * <ol>
+     * <li>unixTimestamp < MILLISECONDS_IN_YEAR (365 * 86400 *
+     * 1000) - timestamp is in seconds</li>
+     * <li>unixTimestamp >= MILLISECONDS_IN_YEAR (365 * 86400 * 1000)
+     * - timestamp is in milliseconds</li>
+     * <li>unixTimestamp >= MICROSECONDS_IN_YEAR
+     * (365 * 86400 * 1000_000) - timestamp is in microseconds</li>
+     * <li>unixTimestamp >= NANOSECONDS_IN_YEAR (365 * 86400
+     * * 1000_000_000) - timestamp is in nanoseconds</li>
+     * <li>unixTimestamp < 0 - timestamp is in seconds</li>
+     * </ol>
      *
-     * Note that this also imposes limits on the possible max correctly interpreted timestamp value,
-     * that would be otherwise correctly interpreted as in Seconds/Milliseconds/Microseconds.
-     * For example if this function is called on a unixTimestamp of 31_536_000_001 which in seconds
-     * corresponds to datetime of 2969-05-03 00:00:01, this function would instead interpret it
-     * as 1971-01-01 00:00:00.001. This is also true for maximum date expressed in milliseconds and microseconds.
-     * Additionally this of course also imposes the min date possible to be interpreted in Milliseconds,
-     * Microseconds and Nanoseconds.
+     * <p>Note that this also imposes limits on the possible max
+     * correctly interpreted timestamp value, that would be otherwise
+     * correctly interpreted as in seconds/milliseconds/microseconds.
+     * For example if this function is called on a unixTimestamp of
+     * 31_536_000_001 which in seconds corresponds to datetime of
+     * 2969-05-03 00:00:01, this function would instead interpret it
+     * as 1971-01-01 00:00:00.001. This is also true for maximum date
+     * expressed in milliseconds and microseconds. This of course also
+     * imposes the min date possible to be interpreted in milliseconds,
+     * microseconds and nanoseconds. Additionally any value less
+     * than 0 is treated as seconds regardless of the magnitude.</p>
      *
-     * Summary of Date limits (under which the interpretation of the timestamp's unit is correct):
-     * 1. Seconds:      from 1970-01-01 00:00:00Z (0)
+     * <p>Summary of Date limits (under which the
+     * interpretation of the timestamp's unit is correct):</p>
+     * <pre>
+     * 1. negative
+     *    seconds:      from -999999999-01-01T00:00Z (-31557014135596800)
+     *                  to   1969-12-31 23:59:59Z (-1)
+     * 2. seconds:      from 1970-01-01 00:00:00Z (0)
      *                  to   2969-05-02 23:59:59Z (MILLISECONDS_IN_YEAR - 1)
-     * 2. Milliseconds: from 1971-01-01 00:00:00.0Z (MILLISECONDS_IN_YEAR)
+     * 3. milliseconds: from 1971-01-01 00:00:00.0Z (MILLISECONDS_IN_YEAR)
      *                  to   2969-05-02 23:59:59.999Z (MICROSECONDS_IN_YEAR - 1)
-     * 3. Microseconds: from 1971-01-01 00:00:00.0Z (MICROSECONDS_IN_YEAR)
+     * 4. microseconds: from 1971-01-01 00:00:00.0Z (MICROSECONDS_IN_YEAR)
      *                  to   2969-05-02 23:59:59.999999Z (NANOSECONDS_IN_YEAR - 1)
-     * 4. Nanoseconds:  from 1971-01-01 00:00:00.0Z (NANOSECONDS_IN_YEAR)
+     * 5. nanoseconds:  from 1971-01-01 00:00:00.0Z (NANOSECONDS_IN_YEAR)
      *                  to   2262-04-11 23:47:16.854775807Z (Long.MAX_VALUE of nanoseconds).
+     * </pre>
      *
      * @param unixTimestamp - input timestamp
      * @return - determined {@link TemporalUnit} of the value, inferred based on the magnitude
