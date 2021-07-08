dbdc833ec8f67c965db053e30ca56c3327abe7d9
Matko Medenjak <medenjak.matko@gmail.com>
2021-07-08 07:37:22 +0200

    Improvements from old IMDG scripts + removed IMDG scripts

--- hazelcast/src/main/resources/stop-all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM ******* This script kills java processes running HazelcastMemberStarter
-
-wmic process where "Name like '%%java%%' AND CommandLine like '%%HazelcastMemberStarter%%'" delete
