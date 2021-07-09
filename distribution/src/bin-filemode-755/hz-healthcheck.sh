8b698ac80b900dccf63e074f47ad9943ca09f66f
Matko Medenjak <medenjak.matko@gmail.com>
2021-07-09 15:24:21 +0200

    Revert change to cacert param

--- distribution/src/bin-filemode-755/hz-healthcheck.sh
+++ distribution/src/bin-filemode-755/hz-healthcheck.sh
@@ -8,7 +8,7 @@ print_help() {
     echo "  -d, --debug       : Prints curl error output."
     echo "HTTPs related (TLS enabled):"
     echo "      --https       : Uses HTTPs protocol for REST calls. (no parameter value expected)"
-    echo "      --ca-cert     : Defines trusted PEM-encoded certificate file path. It's used to verify member certificates."
+    echo "      --cacert      : Defines trusted PEM-encoded certificate file path. It's used to verify member certificates."
     echo "      --cert        : Defines PEM-encoded client certificate file path. Only needed when client certificate authentication is used."
     echo "      --key         : Defines PEM-encoded client private key file path. Only needed when client certificate authentication is used."
     echo "      --insecure    : Disables member certificate verification. (no parameter value expected)"
