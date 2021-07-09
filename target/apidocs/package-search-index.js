bba93d514e95a8cf11e39012cfb659a27eee66ea
Serge Bishyr <seriybg@gmail.com>
2021-07-09 12:31:08 +0300

    remove unneeded files

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hazelcast.kubernetes"}];updateSearchResults();
--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.hazelcast.kubernetes","l":"KubernetesProperties"}];updateSearchResults();
