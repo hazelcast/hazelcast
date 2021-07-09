4fc18e00423e958e54df1d5ff7decdbf986bcc17
Matko Medenjak <medenjak.matko@gmail.com>
2021-07-09 15:00:19 +0200

    Minor fix to bat script

--- distribution/src/bin-regular/hz-start.bat
+++ distribution/src/bin-regular/hz-start.bat
@@ -41,7 +41,7 @@ IF NOT "%JAVA_VERSION%" == "8" (
 
 set JAVA_OPTS=%JAVA_OPTS%^
  "-Dhazelcast.logging.type=log4j2"^
- "-Dlog4j.configurationFile=file:%HAZELCAST_HOME%\config\log4j2.properties"^
+ "-Dlog4j.configurationFile=file:%HAZELCAST_HOME%\config\log4j2.properties"
 
 set CLASSPATH="%HAZELCAST_HOME%\lib\*;%HAZELCAST_HOME%\bin\user-lib;%HAZELCAST_HOME%\bin\user-lib\*";%CLASSPATH%
 
