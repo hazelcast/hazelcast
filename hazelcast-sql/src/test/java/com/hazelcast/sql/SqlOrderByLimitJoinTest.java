220e31387e3eb23c3a5e5769fb49c91fd2121472
ivan.yaschishin <ivan.yaschishin@hazelcast.com>
2021-07-16 12:33:09 +0300

    Additional test

--- hazelcast-sql/src/test/java/com/hazelcast/sql/SqlOrderByLimitJoinTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/sql/SqlOrderByLimitJoinTest.java
@@ -20,13 +20,16 @@ import com.hazelcast.config.Config;
 import com.hazelcast.jet.config.JetConfig;
 import com.hazelcast.jet.sql.SqlTestSupport;
 import com.hazelcast.map.IMap;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -36,22 +39,41 @@ public class SqlOrderByLimitJoinTest extends SqlTestSupport {
         initialize(1, new Config().setJetConfig(new JetConfig().setEnabled(true)));
     }
 
-    @Test
-    public void when_orderByWithLimitUsedOnJoinedTable_limitWorks() {
+    @Before
+    public void before() {
         final IMap<Long, Long> t0 = instance().getMap("t0");
         final IMap<Long, String> t1 = instance().getMap("t1");
 
         for (long i = 0; i < 10; i++) {
-            t0.put(i, i * 1000);
+            t0.put(i, (i + 1));
             t1.put(i, "test-" + i);
         }
+    }
 
+    @Test
+    public void when_orderByWithLimitUsedOnJoinedTable_limitWorks() {
         final List<Map<String, Object>> results = execute("select abs(t0.this) as c1, t1.this as c2 "
                 + "from t0 join t1 on t0.__key = t1.__key "
-                + "order by t0.this "
+                + "order by t0.this desc "
                 + "limit 5");
 
+        final List<Object> c1s = results.stream().map(m -> m.get("c1")).collect(Collectors.toList());
+
         assertEquals(5, results.size());
+        assertEquals(Arrays.asList(10L, 9L, 8L, 7L, 6L), c1s);
+    }
+
+    @Test
+    public void when_orderByWithLimitAndOffsetUsedOnJoinedTable_limitAndOffsetWork() {
+        final List<Map<String, Object>> results = execute("select abs(t0.this) as c1, t1.this as c2 "
+                + "from t0 join t1 on t0.__key = t1.__key "
+                + "order by t0.this desc "
+                + "limit 5 offset 2");
+
+        final List<Object> c1s = results.stream().map(m -> m.get("c1")).collect(Collectors.toList());
+
+        assertEquals(5, results.size());
+        assertEquals(Arrays.asList(8L, 7L, 6L, 5L, 4L), c1s);
     }
 
     private List<Map<String, Object>> execute(String sql) {
