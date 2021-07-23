33ab9b348c54941a34cb9b15d243d4f4574f6e99
Serge Bishyr <sergii.bishyr@hazelcast.com>
2021-07-23 10:02:59 +0300

    clean up leftover SPI plugins config (#19160)

--- .github/terraform/aws/scripts/start_aws_hazelcast_member.sh
+++ .github/terraform/aws/scripts/start_aws_hazelcast_member.sh
@@ -11,7 +11,7 @@ sed -i -e "s/REGION/${REGION}/g" ${HOME}/hazelcast.yaml
 sed -i -e "s/TAG_KEY/${TAG_KEY}/g" ${HOME}/hazelcast.yaml
 sed -i -e "s/TAG_VALUE/${TAG_VALUE}/g" ${HOME}/hazelcast.yaml
 
-CLASSPATH="${HOME}/jars/hazelcast.jar:${HOME}/jars/hazelcast-aws.jar:${HOME}/hazelcast.yaml"
+CLASSPATH="${HOME}/jars/hazelcast.jar:${HOME}/hazelcast.yaml"
 
 nohup java -cp ${CLASSPATH} -server com.hazelcast.core.server.HazelcastMemberStarter >> ${HOME}/logs/hazelcast.stderr.log 2>> ${HOME}/logs/hazelcast.stdout.log &
 
