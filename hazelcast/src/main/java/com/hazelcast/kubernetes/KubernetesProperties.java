e83a1cd73802bffc0e75fbf85a4a61d427a6c520
Rafal Leszko <rafal@hazelcast.com>
2021-07-27 14:00:37 +0200

    Fix after code review

--- hazelcast/src/main/java/com/hazelcast/kubernetes/KubernetesProperties.java
+++ hazelcast/src/main/java/com/hazelcast/kubernetes/KubernetesProperties.java
@@ -103,7 +103,7 @@ public final class KubernetesProperties {
     public static final PropertyDefinition SERVICE_PER_POD_LABEL_NAME = property("service-per-pod-label-name", STRING);
     /**
      * <p>Configuration key: <code>service-per-pod-label-value</code></p>
-     * Defines the label name of the service used to expose one Hazelcast pod (for the external smart client use case).
+     * Defines the label value of the service used to expose one Hazelcast pod (for the external smart client use case).
      */
     public static final PropertyDefinition SERVICE_PER_POD_LABEL_VALUE = property("service-per-pod-label-value", STRING);
 
