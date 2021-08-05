2dd954433ffad22d5fb0fc3e9adbc062f34ac499
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2021-08-05 07:59:29 +0300

    Fix comp issue

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/index/JetSqlIndexCastTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/index/JetSqlIndexCastTest.java
@@ -195,7 +195,6 @@ public class JetSqlIndexCastTest extends OptimizerTestSupport {
                 MAP_NAME,
                 mapTableFields,
                 getPartitionedMapIndexes(getMapContainer(instance().getMap(MAP_NAME)), mapTableFields),
-                false,
                 1
         );
         Result optimizationResult = optimizePhysical(statement.getSql(), parameterTypes, table);
