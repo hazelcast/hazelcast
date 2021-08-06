bd0a5fe3444a08feb581992ed90aa3544337dc40
mdumandag <metin@hazelcast.com>
2021-08-06 09:46:34 +0300

    Add support for declarative compact serialization configuration
    This PR adds support for declarative configuration for the compact
    serialization format.
    
    For the XML configuration, the structure will look like
    
    ```xml
    <serialization>
    	<compact-serialization enabled="true">
    		<registered-classes>
    			<class>com.foo.Bar</class>
    			<class type-name="baz" serializer="com.foo.BazSerializer">com.foo.Baz</class>
    		</registered-classes>
    	</compact-serialization>
    </serialization>
    ```
    
    For YAML, the structure is as follows
    
    ```yaml
    serialization:
    	compact-serialization:
    		enabled: true
    		registered-classes:
    			- class: com.foo.Bar
    			- class: com.foo.Baz
    			  type-name: baz
    			  serializer: com.foo.BazSerializer
    ```
    
    After an internal discussion, we decided to remove support for
    registering reflective serialization with a custom type name and
    registering explicit serializer without a type name, and that
    changes are reflected in the declarative configuration as well.
    
    Since this is a BETA feature, I didn't make any changes to the
    XSDs. That's why one has to disable schema validation to be
    able to use declarative configuration. This limitation will be
    documented in the reference manual.

--- hazelcast/src/main/java/com/hazelcast/client/config/impl/YamlClientDomConfigProcessor.java
+++ hazelcast/src/main/java/com/hazelcast/client/config/impl/YamlClientDomConfigProcessor.java
@@ -21,6 +21,7 @@ import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.client.config.ClientSecurityConfig;
 import com.hazelcast.client.config.ClientUserCodeDeploymentConfig;
 import com.hazelcast.config.ClassFilter;
+import com.hazelcast.config.CompactSerializationConfig;
 import com.hazelcast.config.DiscoveryConfig;
 import com.hazelcast.config.GlobalSerializerConfig;
 import com.hazelcast.config.ListenerConfig;
@@ -107,6 +108,8 @@ public class YamlClientDomConfigProcessor extends ClientDomConfigProcessor {
                 fillGlobalSerializer(child, serializationConfig);
             } else if (matches("java-serialization-filter", name)) {
                 fillJavaSerializationFilter(child, serializationConfig);
+            } else if (matches("compact-serialization", name)) {
+                handleCompactSerialization(child, serializationConfig);
             }
         }
         return serializationConfig;
@@ -115,6 +118,30 @@ public class YamlClientDomConfigProcessor extends ClientDomConfigProcessor {
     @Override
     protected String parseCustomLoadBalancerClassName(Node node) {
         return getAttribute(node, "class-name");
+    }
+
+    @Override
+    protected void handleCompactSerialization(Node node, SerializationConfig serializationConfig) {
+        CompactSerializationConfig compactSerializationConfig = serializationConfig.getCompactSerializationConfig();
+        for (Node child : childElements(node)) {
+            String name = cleanNodeName(child);
+            if (matches("enabled", name)) {
+                boolean enabled = getBooleanValue(getTextContent(child));
+                compactSerializationConfig.setEnabled(enabled);
+            } else if (matches("registered-classes", name)) {
+                fillCompactSerializableClasses(child, compactSerializationConfig);
+            }
+        }
+    }
+
+    @Override
+    protected void fillCompactSerializableClasses(Node node, CompactSerializationConfig compactSerializationConfig) {
+        for (Node child : childElements(node)) {
+            String className = getAttribute(child, "class");
+            String typeName = getAttribute(child, "type-name");
+            String serializerClassName = getAttribute(child, "serializer");
+            registerCompactSerializableClass(compactSerializationConfig, className, typeName, serializerClassName);
+        }
     }
 
     private void fillGlobalSerializer(Node child, SerializationConfig serializationConfig) {
