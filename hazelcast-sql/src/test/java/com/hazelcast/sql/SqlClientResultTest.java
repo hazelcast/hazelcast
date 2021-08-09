0a23b5f56e7fc3e6659304bc4ce39a250494ccd2
Grzegorz Gierlach <grzegorz.gierlach@hazelcast.com>
2021-08-09 14:22:04 +0200

    Lazy SQL OBJECT deserialization

--- hazelcast-sql/src/test/java/com/hazelcast/sql/SqlClientResultTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/sql/SqlClientResultTest.java
@@ -17,7 +17,12 @@
 package com.hazelcast.sql;
 
 import com.hazelcast.client.test.TestHazelcastFactory;
+import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializableFactory;
+import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.sql.impl.SqlErrorCode;
 import com.hazelcast.sql.impl.SqlTestSupport;
 import com.hazelcast.test.HazelcastParallelClassRunner;
@@ -29,9 +34,11 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -45,16 +52,17 @@ public class SqlClientResultTest extends SqlTestSupport {
     private static final String SQL_BAD = "SELECT * FROM " + MAP_NAME + "_bad";
 
     private final TestHazelcastFactory factory = new TestHazelcastFactory();
+    private HazelcastInstance member;
     private HazelcastInstance client;
 
     @Before
     public void before() {
-        HazelcastInstance member = factory.newHazelcastInstance();
-        client = factory.newHazelcastClient();
+        Config config = smallInstanceConfig();
+        config.getSerializationConfig()
+                .addDataSerializableFactory(TestDataSerializableFactory.ID, new TestDataSerializableFactory());
+        member = factory.newHazelcastInstance(config);
 
-        Map<Integer, Integer> map = member.getMap(MAP_NAME);
-        map.put(0, 0);
-        map.put(1, 1);
+        client = factory.newHazelcastClient();
     }
 
     @After
@@ -64,6 +72,10 @@ public class SqlClientResultTest extends SqlTestSupport {
 
     @Test
     public void testQuery() {
+        Map<Integer, Integer> map = member.getMap(MAP_NAME);
+        map.put(0, 0);
+        map.put(1, 1);
+
         try (SqlResult result = execute(SQL_GOOD)) {
             assertEquals(2, result.getRowMetadata().getColumnCount());
 
@@ -81,6 +93,10 @@ public class SqlClientResultTest extends SqlTestSupport {
 
     @Test
     public void testBadQuery() {
+        Map<Integer, Integer> map = member.getMap(MAP_NAME);
+        map.put(0, 0);
+        map.put(1, 1);
+
         try (SqlResult result = execute(SQL_BAD)) {
             checkSqlException(result::iterator, SqlErrorCode.PARSING, "Object 'map_bad' not found");
             checkSqlException(result::getRowMetadata, SqlErrorCode.PARSING, "Object 'map_bad' not found");
@@ -91,6 +107,10 @@ public class SqlClientResultTest extends SqlTestSupport {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
     public void testCloseBetweenFetches() {
+        Map<Integer, Integer> map = member.getMap(MAP_NAME);
+        map.put(0, 0);
+        map.put(1, 1);
+
         try (SqlResult result = execute(SQL_GOOD)) {
             assertEquals(2, result.getRowMetadata().getColumnCount());
 
@@ -104,6 +124,21 @@ public class SqlClientResultTest extends SqlTestSupport {
 
             checkSqlException(iterator::hasNext, SqlErrorCode.CANCELLED_BY_USER, "Query was cancelled by the user");
             checkSqlException(iterator::next, SqlErrorCode.CANCELLED_BY_USER, "Query was cancelled by the user");
+        }
+    }
+
+    @Test
+    public void test_lazyDeserialization() {
+        Map<Integer, Person> map = member.getMap(MAP_NAME);
+        map.put(1, new Person("Alice", new Address()));
+
+        try (SqlResult result = execute("SELECT * FROM " + MAP_NAME)) {
+            SqlRow row = result.iterator().next();
+            assertEquals(1, (int) row.getObject("__key"));
+            assertEquals("Alice", row.getObject("name"));
+            assertThatThrownBy(() -> row.getObject("address"))
+                    .isInstanceOf(HazelcastSqlException.class)
+                    .hasMessageContaining("Failed to deserialize query result value");
         }
     }
 
@@ -122,5 +157,81 @@ public class SqlClientResultTest extends SqlTestSupport {
 
     private SqlResult execute(String sql) {
         return client.getSql().execute(new SqlStatement(sql).setCursorBufferSize(1));
+    }
+
+    private static final class TestDataSerializableFactory implements DataSerializableFactory {
+
+        private static final int ID = 1;
+
+        @Override
+        public IdentifiedDataSerializable create(int typeId) {
+            if (typeId == Person.ID) {
+                return new Person();
+            } else {
+                assert typeId == Address.ID;
+                return new Address();
+            }
+        }
+    }
+
+    public static final class Person implements IdentifiedDataSerializable {
+
+        private static final int ID = 1;
+
+        public String name;
+        public Address address;
+
+        private Person() {
+        }
+
+        private Person(String name, Address address) {
+            this.name = name;
+            this.address = address;
+        }
+
+        @Override
+        public int getFactoryId() {
+            return TestDataSerializableFactory.ID;
+        }
+
+        @Override
+        public int getClassId() {
+            return ID;
+        }
+
+        @Override
+        public void writeData(ObjectDataOutput out) throws IOException {
+            out.writeString(name);
+            out.writeObject(address);
+        }
+
+        @Override
+        public void readData(ObjectDataInput in) throws IOException {
+            name = in.readString();
+            address = in.readObject();
+        }
+    }
+
+    public static final class Address implements IdentifiedDataSerializable {
+
+        private static final int ID = 2;
+
+        @Override
+        public int getFactoryId() {
+            return TestDataSerializableFactory.ID;
+        }
+
+        @Override
+        public int getClassId() {
+            return ID;
+        }
+
+        @Override
+        public void writeData(ObjectDataOutput out) {
+        }
+
+        @Override
+        public void readData(ObjectDataInput in) {
+        }
     }
 }
