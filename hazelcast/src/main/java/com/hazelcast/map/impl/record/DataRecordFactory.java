55f03beebfb086418d80b8c9ddc1e43835e2b965
Ahmet Mircik <ahmetmircik@gmail.com>
2021-08-31 15:15:05 +0300

    Remove RU 4.1 compatibility code paths (#19405)

--- hazelcast/src/main/java/com/hazelcast/map/impl/record/DataRecordFactory.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/record/DataRecordFactory.java
@@ -52,14 +52,9 @@ public class DataRecordFactory implements RecordFactory<Data> {
         }
     }
 
-    @Override
-    public MapContainer geMapContainer() {
-        return mapContainer;
-    }
-
     private Record<Data> newCachedSimpleRecord(Data valueData, MapConfig mapConfig,
                                                boolean perEntryStatsEnabled, boolean hasEviction) {
-        if (perEntryStatsEnabled || isClusterV41()) {
+        if (perEntryStatsEnabled) {
             return new CachedDataRecordWithStats(valueData);
         }
 
@@ -84,7 +79,7 @@ public class DataRecordFactory implements RecordFactory<Data> {
 
     private Record<Data> newSimpleRecord(Data valueData, MapConfig mapConfig,
                                          boolean perEntryStatsEnabled, boolean hasEviction) {
-        if (perEntryStatsEnabled || isClusterV41()) {
+        if (perEntryStatsEnabled) {
             return new DataRecordWithStats(valueData);
         }
 
