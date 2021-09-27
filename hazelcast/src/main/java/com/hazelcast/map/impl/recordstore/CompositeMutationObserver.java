a87bcdf952e7b7fe67f61c50ab60e77c53d4a771
Ahmet Mircik <ahmetmircik@gmail.com>
2021-09-27 17:50:58 +0300

    refactor CompositeMutationObserver

--- hazelcast/src/main/java/com/hazelcast/map/impl/recordstore/CompositeMutationObserver.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/recordstore/CompositeMutationObserver.java
@@ -20,234 +20,90 @@ import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.map.impl.record.Record;
 
 import javax.annotation.Nonnull;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.hazelcast.internal.util.CollectionUtil.isEmpty;
-import static com.hazelcast.internal.util.ExceptionUtil.rethrow;
+import static java.util.Collections.EMPTY_LIST;
 
 class CompositeMutationObserver<R extends Record> implements MutationObserver<R> {
 
-    private Collection<MutationObserver<R>> mutationObservers;
+    private static final int DEFAULT_OBSERVER_COUNT = 4;
 
-    CompositeMutationObserver() {
+    private List<MutationObserver> mutationObservers = EMPTY_LIST;
+
+    public CompositeMutationObserver() {
     }
 
-    void add(MutationObserver<R> mutationObserver) {
-        if (mutationObservers == null) {
-            mutationObservers = new LinkedList<>();
+    public void add(MutationObserver<R> mutationObserver) {
+        if (mutationObservers == EMPTY_LIST) {
+            mutationObservers = new ArrayList<>(DEFAULT_OBSERVER_COUNT);
         }
         mutationObservers.add(mutationObserver);
     }
 
     @Override
     public void onClear() {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onClear();
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onClear();
         }
     }
 
     @Override
     public void onPutRecord(Data key, R record,
                             Object oldValue, boolean backup) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onPutRecord(key, record, oldValue, backup);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onPutRecord(key, record, oldValue, backup);
         }
     }
 
     @Override
     public void onReplicationPutRecord(@Nonnull Data key,
                                        @Nonnull R record, boolean populateIndex) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onReplicationPutRecord(key, record, populateIndex);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onReplicationPutRecord(key, record, populateIndex);
         }
     }
 
     @Override
     public void onUpdateRecord(@Nonnull Data key, @Nonnull R record,
                                Object oldValue, Object newValue, boolean backup) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onUpdateRecord(key, record, oldValue, newValue, backup);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onUpdateRecord(key, record, oldValue, newValue, backup);
         }
     }
 
     @Override
     public void onRemoveRecord(Data key, R record) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onRemoveRecord(key, record);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onRemoveRecord(key, record);
         }
     }
 
     @Override
     public void onEvictRecord(Data key, R record) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onEvictRecord(key, record);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onEvictRecord(key, record);
         }
     }
 
     @Override
     public void onLoadRecord(@Nonnull Data key, @Nonnull R record, boolean backup) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onLoadRecord(key, record, backup);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onLoadRecord(key, record, backup);
         }
     }
 
-
     @Override
     public void onDestroy(boolean isDuringShutdown, boolean internal) {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onDestroy(isDuringShutdown, internal);
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onDestroy(isDuringShutdown, internal);
         }
     }
 
     @Override
     public void onReset() {
-        if (isEmpty(mutationObservers)) {
-            return;
-        }
-
-        Throwable exception = null;
-
-        for (MutationObserver<R> mutationObserver : mutationObservers) {
-            try {
-                mutationObserver.onReset();
-            } catch (Throwable t) {
-                if (exception == null) {
-                    exception = t;
-                }
-            }
-        }
-
-        if (exception != null) {
-            rethrow(exception);
+        for (int i = 0; i < mutationObservers.size(); i++) {
+            mutationObservers.get(i).onReset();
         }
     }
 }
