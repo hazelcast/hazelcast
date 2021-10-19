0be23f4b0ba128e2eef12948b95ab853e75c460a
ufukyilmaz <ufukyilmaz333@gmail.com>
2021-10-19 18:16:19 +0300

    Update hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/NodeMulticastListener.java
    Co-authored-by: Ramiz Dündar <ramizdundar@gmail.com>

--- hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/NodeMulticastListener.java
+++ hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/NodeMulticastListener.java
@@ -124,7 +124,7 @@ public class NodeMulticastListener implements MulticastListener {
         }
 
         ConfigCheck theirConfig = joinMessage.getConfigCheck();
-        return ourConfig.isSameClusterName(theirConfig);
+        return ourConfig.hasSameClusterName(theirConfig);
     }
 
     private boolean isMessageToSelf(JoinMessage joinMessage) {
