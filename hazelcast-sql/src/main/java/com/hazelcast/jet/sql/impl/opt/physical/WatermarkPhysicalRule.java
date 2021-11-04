43dcc82d6bd3fc1a71223518670687ec46b879cc
Grzegorz Gierlach <grzegorz.gierlach@hazelcast.com>
2021-11-04 09:52:14 +0100

    Fail on invalid EXPLAIN-ordering statement

--- hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/opt/physical/WatermarkPhysicalRule.java
+++ hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/opt/physical/WatermarkPhysicalRule.java
@@ -16,34 +16,29 @@
 
 package com.hazelcast.jet.sql.impl.opt.physical;
 
-import com.hazelcast.jet.sql.impl.opt.OptUtils;
 import com.hazelcast.jet.sql.impl.opt.logical.WatermarkLogicalRel;
+import com.hazelcast.sql.impl.QueryException;
 import org.apache.calcite.plan.RelOptRule;
-import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rel.convert.ConverterRule;
+import org.apache.calcite.plan.RelOptRuleCall;
 
 import static com.hazelcast.jet.sql.impl.opt.Conventions.LOGICAL;
-import static com.hazelcast.jet.sql.impl.opt.Conventions.PHYSICAL;
 
-final class WatermarkPhysicalRule extends ConverterRule {
+final class WatermarkPhysicalRule extends RelOptRule {
 
     static final RelOptRule INSTANCE = new WatermarkPhysicalRule();
 
     private WatermarkPhysicalRule() {
         super(
-                WatermarkLogicalRel.class, LOGICAL, PHYSICAL,
+                operand(WatermarkLogicalRel.class, LOGICAL, node -> {
+                    // if we end up here it means watermarks were not pushed down into scan during logical phase
+                    throw QueryException.error("Ordering function cannot be applied to input table");
+                }, any()),
                 WatermarkPhysicalRule.class.getSimpleName()
         );
     }
 
     @Override
-    public RelNode convert(RelNode rel) {
-        WatermarkLogicalRel logicalWatermark = (WatermarkLogicalRel) rel;
-
-        return new WatermarkPhysicalRel(
-                logicalWatermark.getCluster(),
-                OptUtils.toPhysicalConvention(logicalWatermark.getTraitSet()),
-                OptUtils.toPhysicalInput(logicalWatermark.getInput())
-        );
+    public void onMatch(RelOptRuleCall call) {
+        throw new UnsupportedOperationException("Should never be called");
     }
 }
