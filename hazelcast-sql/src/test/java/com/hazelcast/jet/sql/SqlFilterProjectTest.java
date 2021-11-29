730d57c14401a6dbdc8ad611d5c2105f68a8b9b8
ivanthescientist <ivan.yaschishin@hazelcast.com>
2021-11-29 11:15:20 +0200

    small test addition

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/SqlFilterProjectTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/SqlFilterProjectTest.java
@@ -26,6 +26,8 @@ import com.hazelcast.sql.SqlService;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.Serializable;
+
 import static com.hazelcast.jet.core.TestUtil.createMap;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
@@ -690,5 +692,43 @@ public class SqlFilterProjectTest extends SqlTestSupport {
 
         createMapping("m", Integer.class, Integer.class);
         assertRowsAnyOrder("select this from m where __key=42", singletonList(new Row(43)));
+    }
+
+    @Test
+    public void test_selectByKey_keyNotSelectWithObject() {
+        IMap<Long, TestValue> map = instance().getMap("test");
+        map.put(1L, new TestValue(10L, "test-value"));
+
+        createMapping("test", Long.class, TestValue.class);
+        assertRowsAnyOrder("select name from test where __key = 1", singletonList(new Row("test-value")));
+    }
+
+    public static class TestValue implements Serializable {
+        private Long id;
+        private String name;
+
+        public TestValue() {
+        }
+
+        public TestValue(final Long id, final String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(final Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
     }
 }
