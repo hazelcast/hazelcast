e9ac1046268b39c00b6508050f8cfda4655a374c
Slava Sytnyk <67485185+slavagithub@users.noreply.github.com>
2021-12-07 12:13:43 +0200

    adding java.util open to common.sh

--- distribution/src/bin-regular/common.sh
+++ distribution/src/bin-regular/common.sh
@@ -41,6 +41,7 @@ if [ "$JAVA_VERSION" -ge "9" ]; then
         --add-exports java.base/jdk.internal.ref=ALL-UNNAMED \
         --add-opens java.base/java.lang=ALL-UNNAMED \
         --add-opens java.base/java.nio=ALL-UNNAMED \
+        --add-opens java.base/java.util=ALL-UNNAMED \
         --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
         --add-opens java.management/sun.management=ALL-UNNAMED \
         --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
