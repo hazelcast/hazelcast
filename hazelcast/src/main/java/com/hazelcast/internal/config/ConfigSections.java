eec24d5baca17b3aadd4223d70004180e194b080
Ramiz Dündar <ramizdundar@gmail.com>
2021-12-28 16:36:25 +0300

    Resolve merge leftovers

--- hazelcast/src/main/java/com/hazelcast/internal/config/ConfigSections.java
+++ hazelcast/src/main/java/com/hazelcast/internal/config/ConfigSections.java
@@ -67,7 +67,7 @@ public enum ConfigSections {
     INSTANCE_TRACKING("instance-tracking", false),
     SQL("sql", false),
     JET("jet", false),
-    LOCAL_DEVICE("device", true),
+    LOCAL_DEVICE("local-device", true),
     DYNAMIC_CONFIGURATION("dynamic-configuration", false);
 
     final boolean multipleOccurrence;
