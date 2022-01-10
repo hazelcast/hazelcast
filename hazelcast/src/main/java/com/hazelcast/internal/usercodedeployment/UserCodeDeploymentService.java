807bae40fca602adee87e2daada4232af0cf03cd
Frantisek Hartman <frant.hartm@gmail.com>
2022-01-10 21:28:43 +0100

    Update the UCD message, so it follows the same conventions as other messages

--- hazelcast/src/main/java/com/hazelcast/internal/usercodedeployment/UserCodeDeploymentService.java
+++ hazelcast/src/main/java/com/hazelcast/internal/usercodedeployment/UserCodeDeploymentService.java
@@ -70,12 +70,12 @@ public final class UserCodeDeploymentService implements ManagedService {
     public void defineClasses(List<Map.Entry<String, byte[]>> classDefinitions) {
         if (!enabled) {
             throw new IllegalStateException("User Code Deployment is not enabled. "
-                    + "To enable user code deployment, please do one of the following:\n"
+                    + "To enable User Code Deployment, please do one of the following:\n"
                     + "- Change member config using JAVA API: "
                     + "config.getUserCodeDeploymentConfig().setEnabled(true);\n"
-                    + "- Change XML/YAML configuration property: hazelcast.user-code-deployment.enabled to true\n"
+                    + "- Change XML/YAML configuration property: Set hazelcast.user-code-deployment.enabled to true\n"
                     + "- Add system property: -Dhz.user-code-deployment.enabled=true\n"
-                    + "- Add environment variable property: HZ_USERCODEDEPLOYMENT_ENABLED=true");
+                    + "- Add environment variable: HZ_USERCODEDEPLOYMENT_ENABLED=true");
         }
         locator.defineClassesFromClient(classDefinitions);
     }
