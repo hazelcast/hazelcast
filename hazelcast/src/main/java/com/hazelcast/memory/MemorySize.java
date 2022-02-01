9b607a620b8f9c6cb828b34fb3015bf6640df333
Farukhzhon Barotov <41333128+fbarotov@users.noreply.github.com>
2022-02-01 16:42:21 +0300

    restore methods used in ee

--- hazelcast/src/main/java/com/hazelcast/memory/MemorySize.java
+++ hazelcast/src/main/java/com/hazelcast/memory/MemorySize.java
@@ -34,4 +34,73 @@ public final class MemorySize extends Capacity {
     public MemorySize(long value, MemoryUnit unit) {
         super(value, unit);
     }
+
+    /**
+     * Parses string representation of a memory size value.
+     * Value may end with one of suffixes;
+     * <ul>
+     * <li>'k' or 'K' for 'kilo',</li>
+     * <li>'m' or 'M' for 'mega',</li>
+     * <li>'g' or 'G' for 'giga'.</li>
+     * </ul>
+     * <p>
+     * Default unit is bytes.
+     * <p>
+     * Examples:
+     * 12345, 12345m, 12345K, 123456G
+     *
+     * @deprecated since 5.1, use {@link Capacity#parse(String)} instead.
+     */
+    public static MemorySize parse(String value) {
+        return parse(value, MemoryUnit.BYTES);
+    }
+
+    /**
+     * Parses string representation of a memory size value.
+     * Value may end with one of suffixes;
+     * <ul>
+     * <li>'k' or 'K' for 'kilo',</li>
+     * <li>'m' or 'M' for 'mega',</li>
+     * <li>'g' or 'G' for 'giga'.</li>
+     * </ul>
+     * <p>
+     * Uses default unit if value does not contain unit information.
+     * <p>
+     * Examples:
+     * 12345, 12345m, 12345K, 123456G
+     *
+     * @deprecated since 5.1, use {@link Capacity#parse(String, MemoryUnit)} instead
+     */
+    public static MemorySize parse(String value, MemoryUnit defaultUnit) {
+        if (value == null || value.length() == 0) {
+            return new MemorySize(0, MemoryUnit.BYTES);
+        }
+
+        MemoryUnit unit = defaultUnit;
+        final char last = value.charAt(value.length() - 1);
+        if (!Character.isDigit(last)) {
+            value = value.substring(0, value.length() - 1);
+            switch (last) {
+                case 'g':
+                case 'G':
+                    unit = MemoryUnit.GIGABYTES;
+                    break;
+
+                case 'm':
+                case 'M':
+                    unit = MemoryUnit.MEGABYTES;
+                    break;
+
+                case 'k':
+                case 'K':
+                    unit = MemoryUnit.KILOBYTES;
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Could not determine memory unit of " + value + last);
+            }
+        }
+
+        return new MemorySize(Long.parseLong(value), unit);
+    }
 }
