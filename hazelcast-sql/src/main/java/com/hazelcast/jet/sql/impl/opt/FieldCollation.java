cc4f639e8eeb62cf8eea5932c99f3f1db09d8aff
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-02-08 09:54:50 +0100

    Fixing tests

--- hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/opt/FieldCollation.java
+++ hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/opt/FieldCollation.java
@@ -25,11 +25,12 @@ import org.apache.calcite.rel.RelFieldCollation.Direction;
 import org.apache.calcite.rel.RelFieldCollation.NullDirection;
 
 import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * Serializable equivalent of {@link RelFieldCollation}.
  */
-public class FieldCollation implements IdentifiedDataSerializable {
+public class FieldCollation implements Serializable, IdentifiedDataSerializable {
     private int index;
     private Direction direction;
     private NullDirection nullDirection;
