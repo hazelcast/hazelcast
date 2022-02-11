15c8fe461e96ee4a5f19096af78b88aed6706693
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-02-11 13:59:31 +0100

    Better test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/index/SqlIndexFromSearchFilterTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/index/SqlIndexFromSearchFilterTest.java
@@ -17,12 +17,13 @@
 package com.hazelcast.jet.sql.impl.connector.map.index;
 
 import com.hazelcast.config.IndexType;
-import com.hazelcast.jet.sql.impl.opt.OptimizerTestSupport;
+import com.hazelcast.jet.sql.impl.opt.logical.FullScanLogicalRel;
+import com.hazelcast.jet.sql.impl.opt.logical.SortLogicalRel;
 import com.hazelcast.jet.sql.impl.opt.physical.IndexScanMapPhysicalRel;
+import com.hazelcast.jet.sql.impl.opt.physical.SortPhysicalRel;
 import com.hazelcast.jet.sql.impl.schema.HazelcastTable;
 import com.hazelcast.jet.sql.impl.support.expressions.ExpressionBiValue;
 import com.hazelcast.map.IMap;
-import com.hazelcast.sql.SqlStatement;
 import com.hazelcast.sql.impl.extract.QueryPath;
 import com.hazelcast.sql.impl.schema.TableField;
 import com.hazelcast.sql.impl.schema.map.MapTableField;
@@ -41,9 +42,14 @@ import static com.hazelcast.jet.sql.impl.support.expressions.ExpressionBiValue.c
 import static com.hazelcast.jet.sql.impl.support.expressions.ExpressionTypes.INTEGER;
 import static com.hazelcast.sql.impl.schema.map.MapTableUtils.getPartitionedMapIndexes;
 import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class SqlIndexFromSearchFilterTest extends SqlIndexTestSupport {
+    public static final int MAP_SIZE = 1000;
+    public static final String F_1_INDEX = "f1_index";
+    public static final String F_2_INDEX = "f2_index";
+
     private String mapName;
 
     @BeforeClass
@@ -55,15 +61,14 @@ public class SqlIndexFromSearchFilterTest extends SqlIndexTestSupport {
     public void before() throws Exception {
         mapName = randomName();
 
-        String indexName = randomName();
-        String[] indexAttributes = new String[]{"field1"};
         Class<? extends ExpressionBiValue> valueClass = createBiClass(INTEGER, INTEGER);
         IMap<Integer, ? super ExpressionBiValue> map = instance().getMap(mapName);
 
         createMapping(mapName, int.class, valueClass);
-        createIndex(indexName, mapName, IndexType.SORTED, indexAttributes);
+        createIndex(F_1_INDEX, mapName, IndexType.SORTED, "field1");
+        createIndex(F_2_INDEX, mapName, IndexType.SORTED, "field2");
 
-        for (int i = 1; i <= 100; ++i) {
+        for (int i = 1; i <= MAP_SIZE; ++i) {
             map.put(i, createBiValue(valueClass, i, i, i));
         }
     }
@@ -71,9 +76,49 @@ public class SqlIndexFromSearchFilterTest extends SqlIndexTestSupport {
     @Test
     public void testSimpleRange() {
         String sql = "SELECT * FROM  \n" + mapName +
-                " WHERE field1 >= -1\n" +
-                " AND field1 <= 1 \n";
-        checkIndexUsage(new SqlStatement(sql));
+                " WHERE field1 >= 100\n" +
+                " AND field1 <= 10000 \n";
+        Result optimizePhysical = optimizePhysical(sql, parameterTypes(), table());
+
+        assertPlan(
+                optimizePhysical.getLogical(),
+                plan(
+                        planRow(0, FullScanLogicalRel.class)
+                )
+        );
+        assertPlan(
+                optimizePhysical.getPhysical(),
+                plan(
+                        planRow(0, IndexScanMapPhysicalRel.class)
+                )
+        );
+    }
+
+    @Test
+    public void testSimpleRangeWithOrderBy() {
+        String sql = "SELECT * FROM  \n" + mapName +
+                " WHERE field1 >= 100\n" +
+                " AND field1 <= 10000 \n" +
+                " ORDER BY field2 ASC LIMIT 20 OFFSET 0";
+        Result optimizePhysical = optimizePhysical(sql, parameterTypes(), table());
+
+        assertPlan(
+                optimizePhysical.getLogical(),
+                plan(
+                        planRow(0, SortLogicalRel.class),
+                        planRow(1, FullScanLogicalRel.class)
+                )
+        );
+        assertPlan(
+                optimizePhysical.getPhysical(),
+                plan(
+                        planRow(0, SortPhysicalRel.class),
+                        planRow(1, IndexScanMapPhysicalRel.class)
+                )
+        );
+        IndexScanMapPhysicalRel rel = (IndexScanMapPhysicalRel) optimizePhysical.getPhysical().getInput(0);
+
+        assertEquals(F_2_INDEX, rel.getIndex().getName());
     }
 
     @Test
@@ -82,11 +127,27 @@ public class SqlIndexFromSearchFilterTest extends SqlIndexTestSupport {
                 " WHERE field1 = -1\n" +
                 " OR field1 = 1 \n" +
                 " OR field1 = 3 \n";
-        checkIndexUsage(new SqlStatement(sql));
+        Result optimizePhysical = optimizePhysical(sql, parameterTypes(), table());
+
+        assertPlan(
+                optimizePhysical.getLogical(),
+                plan(
+                        planRow(0, FullScanLogicalRel.class)
+                )
+        );
+        assertPlan(
+                optimizePhysical.getPhysical(),
+                plan(
+                        planRow(0, IndexScanMapPhysicalRel.class)
+                )
+        );
     }
 
-    private void checkIndexUsage(SqlStatement statement) {
-        List<QueryDataType> parameterTypes = asList(QueryDataType.INT, QueryDataType.OBJECT, QueryDataType.INT);
+    private List<QueryDataType> parameterTypes() {
+        return asList(QueryDataType.INT, QueryDataType.OBJECT, QueryDataType.INT);
+    }
+
+    private HazelcastTable table() {
         List<TableField> mapTableFields = asList(
                 new MapTableField("__key", QueryDataType.INT, false, QueryPath.KEY_PATH),
                 new MapTableField("field1", INTEGER.getFieldConverterType(), false, new QueryPath("field1", false)),
@@ -96,13 +157,8 @@ public class SqlIndexFromSearchFilterTest extends SqlIndexTestSupport {
                 mapName,
                 mapTableFields,
                 getPartitionedMapIndexes(mapContainer(instance().getMap(mapName)), mapTableFields),
-                1
+                MAP_SIZE
         );
-        OptimizerTestSupport.Result optimizationResult = optimizePhysical(statement.getSql(), parameterTypes, table);
-
-        assertPlan(
-                optimizationResult.getPhysical(),
-                plan(planRow(0, IndexScanMapPhysicalRel.class))
-        );
+        return table;
     }
 }
