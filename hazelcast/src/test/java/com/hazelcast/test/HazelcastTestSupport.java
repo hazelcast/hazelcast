7eafe69511ee639591ac7ba3a41f3a9a8372609f
František Hartman <frant.hartm@gmail.com>
2022-02-11 09:39:33 +0100

    Add hazelcast-it module with integration tests to OS (#19698)
    * Add hazelcast-it module with integration tests to OS
    
    We already have hazelcast-it module with integration tests in EE.
    See https://github.com/hazelcast/hazelcast-enterprise/pull/4402.
    
    This EE module will be renamed to avoid the name clash - TODO PR.
    
    This module should contain integration tests, which expect a clean
    environment (e.g. classpath without many dependencies) etc..
    
    We add this module to OS as well to make it easier to write tests for OS
    PRs - we don't have to submit 2 PRs, one to OS, one to EE.
    
    Co-authored-by: Łukasz Dziedziul <l.dziedziul@gmail.com>

--- hazelcast/src/test/java/com/hazelcast/test/HazelcastTestSupport.java
+++ hazelcast/src/test/java/com/hazelcast/test/HazelcastTestSupport.java
@@ -38,6 +38,7 @@ import com.hazelcast.internal.partition.impl.PartitionServiceState;
 import com.hazelcast.internal.serialization.InternalSerializationService;
 import com.hazelcast.internal.serialization.SerializationService;
 import com.hazelcast.internal.server.ServerConnectionManager;
+import com.hazelcast.internal.util.OsHelper;
 import com.hazelcast.internal.util.UuidUtil;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
@@ -88,7 +89,6 @@ import java.util.function.BiConsumer;
 import static com.hazelcast.internal.partition.TestPartitionUtils.getPartitionServiceState;
 import static com.hazelcast.internal.util.ExceptionUtil.rethrow;
 import static com.hazelcast.internal.util.OsHelper.isLinux;
-import static com.hazelcast.internal.util.StringUtil.lowerCaseInternal;
 import static com.hazelcast.test.TestEnvironment.isRunningCompatibilityTest;
 import static java.lang.Integer.getInteger;
 import static java.lang.String.format;
@@ -1611,7 +1611,7 @@ public abstract class HazelcastTestSupport {
     }
 
     public static void assumeThatNoWindowsOS() {
-        assumeFalse(lowerCaseInternal(System.getProperty("os.name")).contains("windows"));
+        assumeFalse("Skipping on Windows", OsHelper.isWindows());
     }
 
     public static void assumeThatLinuxOS() {
