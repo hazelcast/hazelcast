601418753ed37c0b252d02a6a084f19d9ac63db1
Vassilis Bekiaris <vassilis@hazelcast.com>
2022-02-17 16:11:18 +0200

    Update copyright year
    Update copyright for 2022

--- extensions/cdc-debezium/src/main/java/com/hazelcast/jet/cdc/ChangeRecord.java
+++ extensions/cdc-debezium/src/main/java/com/hazelcast/jet/cdc/ChangeRecord.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import javax.annotation.Nonnull;
 /**
  * Information pertaining to a single data change event (insert, delete or
  * update), affecting a single database record.
- * <p>
+ * <p
  * Each event has a <em>key</em>, identifying the affected record, and a
  * <em>value</em>, describing the change to that record.
  * <p>
