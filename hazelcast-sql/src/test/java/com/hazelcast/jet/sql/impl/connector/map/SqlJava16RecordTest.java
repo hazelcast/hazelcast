bda70aca29de1ace795b699d41474f44813df23c
Viliam Durina <viliam@hazelcast.com>
2022-03-10 11:02:03 +0100

    WIP

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/SqlJava16RecordTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/SqlJava16RecordTest.java
@@ -6,6 +6,8 @@ import com.hazelcast.sql.SqlRow;
 import com.hazelcast.sql.SqlService;
 import org.junit.Test;
 
+import java.io.Serializable;
+
 public class SqlJava16RecordTest extends SqlTestSupport {
 
     @Test
@@ -14,12 +16,12 @@ public class SqlJava16RecordTest extends SqlTestSupport {
         createMapping(inst, "m", Integer.class, Person.class);
         SqlService sqlService = inst.getSql();
         sqlService.execute("insert into m values (1, 42, 'foo')");
+        inst.getMap("m").put(1, new Person("foo", 42));
         for (SqlRow r : sqlService.execute("select * from m")) {
             System.out.println(r);
         }
     }
 
-    public record Person (String name, int age) {
-        Record() { }
+    public record Person (String name, int age) implements Serializable {
     }
 }
