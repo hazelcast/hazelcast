788a77c6a15539818245471fed17d42f582f5253
Andrii Rodionov <andrii.rodionov@hazelcast.com>
2022-03-31 10:36:50 +0200

    The `binarySearch` method is used on unsorted arrays. (#21098)
    Added array sorting.

--- hazelcast/src/test/java/com/hazelcast/cp/internal/raft/impl/testing/LocalRaftGroup.java
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/raft/impl/testing/LocalRaftGroup.java
@@ -500,6 +500,7 @@ public class LocalRaftGroup {
     public void split(int... indexes) {
         assertThat(indexes.length, greaterThan(0));
         assertThat(indexes.length, lessThan(size()));
+        Arrays.sort(indexes);
 
         int runningMemberCount = 0;
         for (int i = 0; i < size(); i++) {
