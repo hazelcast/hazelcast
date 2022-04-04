b0a34abafb26bb6da96120cae0971a4af5eff961
Ahmet Mircik <ahmetmircik@gmail.com>
2022-04-04 14:06:53 +0300

    Nullify  object upon test finish to release associated heap

--- hazelcast/src/test/java/com/hazelcast/client/splitbrainprotection/cardinality/ClientCardinalityEstimatorSplitBrainProtectionWriteTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/splitbrainprotection/cardinality/ClientCardinalityEstimatorSplitBrainProtectionWriteTest.java
@@ -49,6 +49,8 @@ public class ClientCardinalityEstimatorSplitBrainProtectionWriteTest extends Car
         if (clients != null) {
             clients.terminateAll();
         }
+        clients = null;
+
         shutdownTestEnvironment();
     }
 
