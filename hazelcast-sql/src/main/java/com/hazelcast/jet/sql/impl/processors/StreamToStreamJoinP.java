615791c28be75ea2a70b26c9171dcfcf452c740e
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2022-04-15 16:46:02 +0300

    The first and naïve stream-to-stream JOIN processor prototype

--- hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/processors/StreamToStreamJoinP.java
+++ hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/processors/StreamToStreamJoinP.java
@@ -20,21 +20,21 @@ import com.hazelcast.function.ToLongFunctionEx;
 import com.hazelcast.internal.serialization.impl.DefaultSerializationServiceBuilder;
 import com.hazelcast.jet.core.AbstractProcessor;
 import com.hazelcast.jet.core.Watermark;
-import com.hazelcast.jet.impl.execution.WatermarkCoalescer;
 import com.hazelcast.jet.sql.impl.ExpressionUtil;
 import com.hazelcast.jet.sql.impl.JetJoinInfo;
 import com.hazelcast.sql.impl.expression.ExpressionEvalContext;
 import com.hazelcast.sql.impl.row.JetSqlRow;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.Queue;
 
 import static java.util.Collections.emptyList;
-import static java.util.Comparator.comparingLong;
+import static java.util.stream.Collectors.toList;
 
 public class StreamToStreamJoinP extends AbstractProcessor {
     // TODO: replace with ExpressionEvalContext.from(context) in init() method.
@@ -49,38 +49,45 @@ public class StreamToStreamJoinP extends AbstractProcessor {
      * </pre>
      */
     private final JetJoinInfo joinInfo;
-    private final ToLongFunctionEx<JetSqlRow> leftTimestampExtractor;
-    private final ToLongFunctionEx<JetSqlRow> rightTimestampExtractor;
-    private final Map<Byte, Long>[] postponeTimeMap;
+    private final Map<Byte, ToLongFunctionEx<JetSqlRow>> leftTimeExtractor;
+    private final Map<Byte, ToLongFunctionEx<JetSqlRow>> rightTimeExtractor;
+    private final Map<Byte, Map<Byte, Long>> postponeTimeMap;
 
-    private final PriorityQueue<JetSqlRow>[] buffer;
+    private final long[][] wmState;
+
+    private final List<JetSqlRow>[] buffer;
 
     private Iterator<JetSqlRow> pos;
 
     private JetSqlRow currItem;
     private JetSqlRow pendingOutput;
-    private final Watermark[] pendingWatermark;
-
-    private final List<Watermark> delayedWatermark = new ArrayList<>();
-
-    private final WatermarkCoalescer coalescer = WatermarkCoalescer.create(2);
+    private final Queue<Watermark> pendingWatermarks;
 
     public StreamToStreamJoinP(
             final JetJoinInfo joinInfo,
-            final ToLongFunctionEx<JetSqlRow> leftTimestampExtractor,
-            final ToLongFunctionEx<JetSqlRow> rightTimestampExtractor,
-            final Map<Byte, Long>[] postponeTimeMap
+            final Map<Byte, ToLongFunctionEx<JetSqlRow>> leftTimeExtractor,
+            final Map<Byte, ToLongFunctionEx<JetSqlRow>> rightTimeExtractor,
+            final Map<Byte, Map<Byte, Long>> postponeTimeMap
     ) {
+        this.buffer = new List[]{new LinkedList<>(), new LinkedList<>()};
+
         this.joinInfo = joinInfo;
-        this.leftTimestampExtractor = leftTimestampExtractor;
-        this.rightTimestampExtractor = rightTimestampExtractor;
+        this.leftTimeExtractor = leftTimeExtractor;
+        this.rightTimeExtractor = rightTimeExtractor;
         this.postponeTimeMap = postponeTimeMap;
 
-        buffer = new PriorityQueue[2];
-        buffer[0] = new PriorityQueue<>(comparingLong(leftTimestampExtractor));
-        buffer[1] = new PriorityQueue<>(comparingLong(rightTimestampExtractor));
+        this.wmState = new long[postponeTimeMap.size()][postponeTimeMap.size()];
+        for (byte i = 0; i < postponeTimeMap.size(); ++i) {
+            for (byte j = 0; j < postponeTimeMap.size(); ++j) {
+                if (postponeTimeMap.get(i).get(j) == null) {
+                    wmState[i][j] = Long.MAX_VALUE;
+                } else {
+                    wmState[i][j] = Long.MIN_VALUE;
+                }
+            }
+        }
 
-        pendingWatermark = new Watermark[2];
+        this.pendingWatermarks = new ArrayDeque<>();
     }
 
     @Override
@@ -95,7 +102,7 @@ public class StreamToStreamJoinP extends AbstractProcessor {
         //  having side input, traverse opposite buffer
         if (currItem == null) {
             currItem = (JetSqlRow) item;
-            buffer[ordinal].offer(currItem);
+            buffer[ordinal].add(currItem);
             pos = buffer[1 - ordinal].iterator();
         }
 
@@ -118,63 +125,84 @@ public class StreamToStreamJoinP extends AbstractProcessor {
         return false;
     }
 
-    /**
-     * ...
-     * 6. wm(d, 6)
-     * 7. wm(o, 6)
-     * 8. -> wm(d, 6)
-     * 9. -> wm(o, -4)
-     * ...
-     * <p>
-     * 6: We receive wm(d, 6). It means that we will receive no delivery items with d.time less than 6.
-     * We cannot emit any watermark after this, as we still can receive order with any time (order's WM is -inf),
-     * and when we receive order with t=-10, we must be able to join it with deliveries with t=-10..0.
-     * This also means that we cannot remove any delivery rows from buffer.
-     * <p>
-     * 7: We receive wm(o, 6). Now we can emit wm(o, -4) and wm(d, 6).
-     * The current value of watermarks for both keys is 6.
-     * It means be able to process orders with t=-4, because order with t=-4 will join with delivery with t=6.
-     * We still have them in the buffer and which we still can receive.
-     * From this follows that we can also remove orders older than -4 and deliveries older than 6 from the buffers.
-     */
     @Override
     public boolean tryProcessWatermark(int ordinal, @Nonnull Watermark watermark) {
-        if (pendingWatermark[ordinal] != null) {
-            if (tryEmit(pendingWatermark[ordinal])) {
-                pendingWatermark[ordinal] = null;
+        // if pending watermarks available - try to send them
+        if (!pendingWatermarks.isEmpty()) {
+            while (!pendingWatermarks.isEmpty()) {
+                Watermark wm = pendingWatermarks.peek();
+                if (!tryEmit(wm)) {
+                    return false;
+                } else {
+                    pendingWatermarks.remove();
+                }
             }
             return false;
         }
+        
+        // update wm state
+        offer(watermark);
 
-        long ts = watermark.timestamp();
-        long postponeTime = ts - postponeTimeMap[ordinal].get(watermark.key());
-        if (postponeTime != ts) {
-            watermark = new Watermark(postponeTime, watermark.key());
-        }
-        clearExpiredItemsInBuffer(
-                buffer[ordinal],
-                postponeTime,
-                ordinal == 0 ? leftTimestampExtractor : rightTimestampExtractor);
+        // try to clear buffers if possible
+        clearExpiredItemsInBuffer(ordinal, watermark);
 
-        if (!tryEmit(watermark)) {
-            pendingWatermark[ordinal] = watermark;
-            return false;
+        // We can't immediately emit current WM, as it would render items in left buffer late.
+        // Instead, we can emit WM with the minimum available time for this WM key.
+        long minimumItemTime = findMinimumBufferTime(ordinal, watermark);
+        if (minimumItemTime != Long.MAX_VALUE) {
+            Watermark wm = new Watermark(minimumItemTime, watermark.key());
+            if (!tryEmit(wm)) {
+                pendingWatermarks.offer(wm);
+            }
         }
+
         return true;
     }
 
-    private void clearExpiredItemsInBuffer(
-            PriorityQueue<JetSqlRow> buffer,
-            long timeLimit,
-            ToLongFunctionEx<JetSqlRow> tsExtractor) {
-        // since buffer is a priority queue, we may do just linear scan to first non-matched item.
-        while (!buffer.isEmpty()) {
-            long ts = tsExtractor.applyAsLong(buffer.peek());
-            if (ts <= timeLimit) {
-                buffer.poll();
+    private void offer(Watermark watermark) {
+        byte key = watermark.key();
+        final Map<Byte, Long> wmKeyMapping = postponeTimeMap.get(key);
+        for (byte i : wmKeyMapping.keySet()) {
+            if (wmState[i][key] != Long.MIN_VALUE) {
+                wmState[i][key] = Math.min(watermark.timestamp() - wmKeyMapping.get(i), wmState[i][key]);
             } else {
-                return;
+                wmState[i][key] = watermark.timestamp() - wmKeyMapping.get(i);
             }
         }
+    }
+
+    private long findMinimumBufferTime(int ordinal, Watermark watermark) {
+        byte key = watermark.key();
+        ToLongFunctionEx<JetSqlRow> extractor = ordinal == 0 ? leftTimeExtractor.get(key) : rightTimeExtractor.get(key);
+
+        long min = Long.MAX_VALUE;
+        for (JetSqlRow row : buffer[ordinal]) {
+            min = Math.min(min, extractor.applyAsLong(row));
+        }
+        return min;
+    }
+
+    private long findMinimumGroupTime(byte group) {
+        long min = Long.MAX_VALUE;
+        for (int i = 0; i < wmState[group].length; ++i) {
+            min = Math.min(min, wmState[group][i]);
+        }
+        return min;
+    }
+
+    private void clearExpiredItemsInBuffer(int ordinal, Watermark wm) {
+        final ToLongFunctionEx<JetSqlRow> tsExtractor = ordinal == 0
+                ? leftTimeExtractor.get(wm.key())
+                : rightTimeExtractor.get(wm.key());
+
+        long limit = findMinimumGroupTime(wm.key());
+        if (limit == Long.MAX_VALUE || limit == Long.MIN_VALUE) {
+            return;
+        }
+
+        buffer[ordinal] = buffer[ordinal]
+                .stream()
+                .filter(row -> tsExtractor.applyAsLong(row) >= limit)
+                .collect(toList());
     }
 }
--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/processors/StreamToStreamJoinPTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/processors/StreamToStreamJoinPTest.java
@@ -17,12 +17,14 @@
 package com.hazelcast.jet.sql.impl.processors;
 
 import com.hazelcast.function.SupplierEx;
+import com.hazelcast.function.ToLongFunctionEx;
 import com.hazelcast.jet.SimpleTestInClusterSupport;
 import com.hazelcast.jet.core.Processor;
 import com.hazelcast.jet.core.test.TestSupport;
 import com.hazelcast.jet.sql.impl.JetJoinInfo;
 import com.hazelcast.sql.impl.expression.ConstantExpression;
 import com.hazelcast.sql.impl.expression.Expression;
+import com.hazelcast.sql.impl.row.JetSqlRow;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
@@ -33,10 +35,12 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static com.hazelcast.jet.sql.SqlTestSupport.jetRow;
 import static com.hazelcast.sql.impl.type.QueryDataType.BOOLEAN;
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonMap;
 
 @Category({QuickTest.class, ParallelJVMTest.class})
 @RunWith(HazelcastSerialClassRunner.class)
@@ -44,15 +48,15 @@ public class StreamToStreamJoinPTest extends SimpleTestInClusterSupport {
     private static final Expression<Boolean> TRUE_PREDICATE =
             (Expression<Boolean>) ConstantExpression.create(true, BOOLEAN);
 
-    private HashMap<Byte, Long>[] postponeTimeMap;
+    private Map<Byte, ToLongFunctionEx<JetSqlRow>> leftExtractors = singletonMap((byte) 0, l -> l.getRow().get(0));
+    private Map<Byte, ToLongFunctionEx<JetSqlRow>> rightExtractors = singletonMap((byte) 1, r -> r.getRow().get(0));
+    private Map<Byte, Map<Byte, Long>> postponeTimeMap;
     private JetJoinInfo joinInfo;
 
     @Before
     public void before() {
         //noinspection unchecked
-        postponeTimeMap = new HashMap[2];
-        postponeTimeMap[0] = new HashMap<>();
-        postponeTimeMap[1] = new HashMap<>();
+        postponeTimeMap = new HashMap<>();
 
         joinInfo = new JetJoinInfo(
                 JoinRelType.INNER,
@@ -64,14 +68,14 @@ public class StreamToStreamJoinPTest extends SimpleTestInClusterSupport {
     }
 
     @Test
-    public void simpleTest() {
-        postponeTimeMap[0].put((byte) 0, 0L);
-        postponeTimeMap[1].put((byte) 1, 0L);
+    public void given_alwaysTrueCondition_when_singleWmKeyPerInput_then_successful() {
+        postponeTimeMap.put((byte) 0, singletonMap((byte) 0, 0L));
+        postponeTimeMap.put((byte) 1, singletonMap((byte) 1, 0L));
 
         SupplierEx<Processor> supplier = () -> new StreamToStreamJoinP(
                 joinInfo,
-                l -> l.getRow().get(0),
-                r -> r.getRow().get(0),
+                leftExtractors,
+                rightExtractors,
                 postponeTimeMap);
 
         TestSupport.verifyProcessor(supplier)
@@ -99,21 +103,29 @@ public class StreamToStreamJoinPTest extends SimpleTestInClusterSupport {
                                 // right <- 1
                                 jetRow(0L, 1L),
                                 jetRow(1L, 1L),
-                                wm((byte) 0, 1L),
-                                wm((byte) 1, 1L)
+                                wm((byte) 0, 0L),
+                                wm((byte) 1, 0L)
                         )
                 );
     }
 
     @Test
-    public void ordersDeliveriesTest() {
-        postponeTimeMap[0].put((byte) 0, 0L);
-        postponeTimeMap[1].put((byte) 1, 5L);
+    public void given_alwaysTrueCondition_when_twoWmKeysOnLeftAndSingleKeyWmRightInput_then_successful() {
+        // left ordinal
+        postponeTimeMap.put((byte) 0, singletonMap((byte) 0, 0L));
+        postponeTimeMap.put((byte) 1, mapOf((byte) 1, 0L, (byte) 2, 1L));
+        // right ordinal
+        postponeTimeMap.put((byte) 2, mapOf((byte) 1, 4L, (byte) 2, 0L));
+
+        leftExtractors = new HashMap<>();
+        leftExtractors.put((byte) 0, l -> l.getRow().get(0));
+        leftExtractors.put((byte) 1, l -> l.getRow().get(1));
+        rightExtractors = singletonMap((byte) 2, r -> r.getRow().get(0));
 
         SupplierEx<Processor> supplier = () -> new StreamToStreamJoinP(
                 joinInfo,
-                l -> l.getRow().get(0),
-                r -> r.getRow().get(0),
+                leftExtractors,
+                rightExtractors,
                 postponeTimeMap);
 
         TestSupport.verifyProcessor(supplier)
@@ -121,82 +133,41 @@ public class StreamToStreamJoinPTest extends SimpleTestInClusterSupport {
                 .disableProgressAssertion()
                 .inputs(asList(
                         asList(
-                                jetRow(5L),
-                                wm((byte) 0, 6L)
+                                jetRow(12L, 10L),
+                                jetRow(12L, 13L),
+                                wm((byte) 1, 12L),
+                                wm((byte) 0, 13L),
+                                wm((byte) 1, 13L)
                         ),
                         asList(
-                                jetRow(8L),
-                                wm((byte) 1, 8L)
+                                jetRow(9L),
+                                wm((byte) 2, 15L),
+                                jetRow(16L),
+                                jetRow(16L)
                         )
                 ))
                 .expectOutput(
                         asList(
-                                jetRow(5L, 8L),
-                                wm((byte) 0, 6L),
-                                wm((byte) 1, 3L)
+                                // left <- (12, 10)
+                                // right <- (9)
+                                jetRow(12L, 10L, 9L),
+                                // left <- (12, 13)
+                                jetRow(12L, 13L, 9L),
+                                // left <-  wm(1, 12)
+                                // right <- wm(2, 15)
+                                wm((byte) 2, 9L),
+                                wm((byte) 1, 13L), // new minimum for key=1 is 13, so emit the freshest WM.
+                                // right <- (21)
+                                jetRow(12L, 13L, 16L)
+                                // wm((byte) 0, 12L) // TODO: expected, but not reached ?
                         )
                 );
     }
 
-    @Test
-    public void testWatermarkCleanup() {
-        postponeTimeMap[0].put((byte) 0, 0L);
-        postponeTimeMap[1].put((byte) 1, 0L);
-
-        SupplierEx<Processor> supplier = () -> new StreamToStreamJoinP(
-                joinInfo,
-                l -> l.getRow().get(0),
-                r -> r.getRow().get(0),
-                postponeTimeMap);
-
-        TestSupport.verifyProcessor(supplier)
-                .disableSnapshots()
-                .disableProgressAssertion()
-                .inputs(asList(
-                        asList(
-                                jetRow(0L),
-                                jetRow(1L),
-                                wm((byte) 0, 1L),
-                                wm((byte) 0, 3L),
-                                jetRow(5L),
-                                jetRow(6L),
-                                wm((byte) 0, 6L)
-                        ),
-                        asList(
-                                jetRow(0L),
-                                jetRow(1L),
-                                wm((byte) 1, 1L),
-                                wm((byte) 1, 3L),
-                                jetRow(5L),
-                                jetRow(6L),
-                                wm((byte) 1, 6L)
-                        )
-                ))
-                .expectOutput(
-                        asList(
-                                // left <- 0
-                                // right <- 0
-                                jetRow(0L, 0L),
-                                // left <- 1
-                                jetRow(1L, 0L),
-                                // right <- 1
-                                jetRow(0L, 1L),
-                                jetRow(1L, 1L),
-                                wm((byte) 0, 1L),
-                                wm((byte) 1, 1L),
-                                wm((byte) 0, 3L),
-                                wm((byte) 1, 3L),
-                                // left <- 5
-                                // right <- 5
-                                jetRow(5L, 5L),
-                                // left <- 6
-                                jetRow(6L, 5L),
-                                // right <- 6
-                                jetRow(5L, 6L),
-                                jetRow(6L, 6L),
-                                wm((byte) 0, 6L),
-                                wm((byte) 1, 6L)
-                        )
-                );
+    static Map<Byte, Long> mapOf(Byte key1, Long value1, Byte key2, Long value2) {
+        Map<Byte, Long> map = new HashMap<>();
+        map.put(key1, value1);
+        map.put(key2, value2);
+        return map;
     }
 }
