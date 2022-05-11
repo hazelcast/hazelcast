e5eea19fac4aa52041ccb7cdf3a065ce1c4dedd6
Bence Erős <erosb@hazelcast.com>
2022-05-11 17:17:22 +0200

    checkpoint

---
+++ hazelcast/src/test/java/com/hazelcast/client/DummyClient.java
@@ -0,0 +1,146 @@
+package com.hazelcast.client;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.instance.impl.HazelcastInstanceFactory;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+
+public class DummyClient {
+
+    /**
+     * Logs: (infinite loop):
+     *
+     * May 10, 2022 9:43:19 AM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * WARNING: hz.client_1 [dev] [5.1-SNAPSHOT] Unable to get live cluster connection, retry in 22498 ms, attempt: 65, cluster connect timeout: INFINITE, max backoff: 30000 ms
+     */
+    @Test
+    public void addressNotFound() {
+        ClientConfig config = newClientConfig();
+        config.getNetworkConfig().addAddress("example.asdasd");
+        config.getNetworkConfig().setConnectionTimeout(5000);
+        HazelcastClient.newHazelcastClient(config);
+        // never fails, max backoff is 30sec, keeps retrying forever
+    }
+
+    /**
+     * Logs: (in infinite loop):
+     *
+     * May 10, 2022 9:31:10 AM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * INFO: hz.client_1 [dev] [5.1-SNAPSHOT] Trying to connect to [erosb.eu]:5703
+     * May 10, 2022 9:31:10 AM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * WARNING: hz.client_1 [dev] [5.1-SNAPSHOT] Exception during initial connection to [erosb.eu]:5703: com.hazelcast.core.HazelcastException: java.io.IOException: Connection refused to address erosb.eu/81.0.124.44:5703
+     * May 10, 2022 9:31:10 AM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * WARNING: hz.client_1 [dev] [5.1-SNAPSHOT] Unable to get live cluster connection, retry in 11369 ms, attempt: 51, cluster connect timeout: INFINITE, max backoff: 30000 ms
+     */
+    @Test
+    public void noMemberOnAddress() {
+        ClientConfig config = newClientConfig();
+        config.getNetworkConfig().addAddress("erosb.eu");
+        HazelcastClient.newHazelcastClient(config);
+    }
+
+    /**
+     * Logs:
+     *
+     * May 10, 2022 2:48:37 PM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * INFO: hz.client_1 [dev] [5.1-SNAPSHOT] Trying to connect to [localhost]:6000
+     * May 10, 2022 2:48:37 PM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * WARNING: hz.client_1 [dev] [5.1-SNAPSHOT] Exception during initial connection to [localhost]:6000: com.hazelcast.core.HazelcastException: java.io.IOException: Connection refused to address localhost/127.0.0.1:6000
+     */
+    @Test
+    public void portIsClosed() {
+        ClientConfig config = newClientConfig();
+        config.getNetworkConfig().addAddress("localhost:6000");
+        HazelcastClient.newHazelcastClient(config);
+    }
+
+    /**
+     * com.hazelcast.client.AuthenticationException: Authentication failed. The configured cluster name on the client (see ClientConfig.setClusterName()) does not match the one configured in the cluster or the credentials set in the Client security config could not be authenticated
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.checkAuthenticationResponse(TcpClientConnectionManager.java:990)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.onAuthenticated(TcpClientConnectionManager.java:893)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.getOrConnectToAddress(TcpClientConnectionManager.java:614)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.lambda$doConnectToCandidateCluster$3(TcpClientConnectionManager.java:504)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.connect(TcpClientConnectionManager.java:458)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.doConnectToCandidateCluster(TcpClientConnectionManager.java:504)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.doConnectToCluster(TcpClientConnectionManager.java:410)
+     * 	at com.hazelcast.client.impl.connection.tcp.TcpClientConnectionManager.connectToCluster(TcpClientConnectionManager.java:371)
+     * 	at com.hazelcast.client.impl.clientside.HazelcastClientInstanceImpl.start(HazelcastClientInstanceImpl.java:384)
+     * 	at com.hazelcast.client.HazelcastClient.constructHazelcastClient(HazelcastClient.java:460)
+     * 	at com.hazelcast.client.HazelcastClient.newHazelcastClientInternal(HazelcastClient.java:416)
+     * 	at com.hazelcast.client.HazelcastClient.newHazelcastClient(HazelcastClient.java:136)
+     * 	at com.hazelcast.client.DummyClient.clusterNameMismatch(DummyClient.java:69)
+     * 	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+     * 	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+     * 	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+     * 	at java.base/java.lang.reflect.Method.invoke(Method.java:566)
+     * 	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)
+     * 	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+     * 	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:56)
+     * 	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+     * 	at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+     * 	at org.junit.runners.BlockJUnit4ClassRunner$1.evaluate(BlockJUnit4ClassRunner.java:100)
+     * 	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:366)
+     * 	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:103)
+     * 	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:63)
+     * 	at org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)
+     * 	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)
+     * 	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)
+     * 	at org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)
+     * 	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)
+     * 	at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+     * 	at org.junit.runners.ParentRunner.run(ParentRunner.java:413)
+     * 	at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
+     * 	at com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:69)
+     * 	at com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:33)
+     * 	at com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:235)
+     * 	at com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:54)
+     *
+     * May 11, 2022 2:23:27 PM com.hazelcast.client.impl.connection.ClientConnectionManager
+     * WARNING: hz.client_1 [something-not-dev] [5.1-SNAPSHOT] Exception during initial connection to [127.0.0.1]:5701: com.hazelcast.client.AuthenticationException: Authentication failed. The configured cluster name on the client (see ClientConfig.setClusterName()) does not match the one configured in the cluster or the credentials set in the Client security config could not be authenticated
+     * May 11, 2022 2:23:27 PM com.hazelcast.internal.server.tcp.TcpServerConnection
+     */
+    @Test
+    public void clusterNameMismatch() {
+        Config memberConfig = new Config();
+        startMember(memberConfig);
+        ClientConfig config = newClientConfig();
+        config.setClusterName("something-not-dev");
+        HazelcastClient.newHazelcastClient(config);
+        HazelcastInstanceFactory.terminateAll();
+    }
+
+    /**
+     * Does not fail
+     */
+    @Test
+    public void connectsOnlyToSomeMembers() {
+        Config member1Config = new Config();
+        member1Config.getNetworkConfig().setPort(5701);
+        startMember(member1Config);
+        Config member2Config = new Config();
+        member2Config.getNetworkConfig().setPort(7001);
+        startMember(member2Config);
+        ClientConfig config = newClientConfig();
+        config.getNetworkConfig().setAddresses(asList("localhost:5701", "localhost:5702"));
+        HazelcastClient.newHazelcastClient(config);
+
+        HazelcastInstanceFactory.terminateAll();
+    }
+
+    private void startMember(Config config) {
+        new Thread(() ->
+            Hazelcast.newHazelcastInstance(config)
+        ).start();
+    }
+
+    @NotNull
+    private ClientConfig newClientConfig() {
+        ClientConfig config = new ClientConfig();
+        config.getConnectionStrategyConfig().getConnectionRetryConfig().setClusterConnectTimeoutMillis(10_000);
+        return config;
+    }
+}
