40730e1d0a64dcd5d5ebf48b609ef263b5d6814d
Josef Cacek <josef.cacek@gmail.com>
2022-05-16 13:43:17 +0200

    Wait for the decoder's protocol confirmation before sending the member data [HZ-1163] (#21391)

--- hazelcast/src/main/java/com/hazelcast/internal/server/tcp/SingleProtocolEncoder.java
+++ hazelcast/src/main/java/com/hazelcast/internal/server/tcp/SingleProtocolEncoder.java
@@ -22,6 +22,7 @@ import com.hazelcast.internal.nio.Protocols;
 
 import java.nio.ByteBuffer;
 
+import static com.hazelcast.internal.networking.HandlerStatus.BLOCKED;
 import static com.hazelcast.internal.networking.HandlerStatus.CLEAN;
 import static com.hazelcast.internal.networking.HandlerStatus.DIRTY;
 import static com.hazelcast.internal.nio.IOUtil.compactOrClear;
@@ -31,15 +32,11 @@ import static com.hazelcast.internal.util.JVMUtil.upcast;
 import static com.hazelcast.internal.util.StringUtil.stringToBytes;
 
 /**
- * Together with {@link SingleProtocolDecoder}, this encoder decoder pair is
- * used for checking correct protocol is used or not. {@link
- * SingleProtocolDecoder} checks if the correct protocol is received. If the
- * protocol is correct, both encoder and decoder swaps itself with the next
- * handler in the pipeline. If it isn't {@link SingleProtocolEncoder} throws
- * {@link ProtocolException} and {@link SingleProtocolDecoder} sends {@value
- * Protocols#UNEXPECTED_PROTOCOL}. Note that in client mode {@link
- * SingleProtocolEncoder} has no effect, and it swaps itself with the next
- * handler.
+ * Together with {@link SingleProtocolDecoder}, this encoder-decoder pair is used to check if correct protocol is used.
+ * {@link SingleProtocolDecoder} checks if the proper protocol is received. If the protocol is correct, both encoder and decoder
+ * are replaced by the next handlers in the pipeline. If it isn't the {@link SingleProtocolEncoder} sends
+ * {@link Protocols#UNEXPECTED_PROTOCOL} response and throws a {@link ProtocolException}. Note that in client mode the
+ * {@link SingleProtocolEncoder} allows blocking packet writes until the (member-)protocol is confirmed.
  */
 public class SingleProtocolEncoder extends OutboundHandler<Void, ByteBuffer> {
     private final OutboundHandler[] outboundHandlers;
@@ -66,27 +63,25 @@ public class SingleProtocolEncoder extends OutboundHandler<Void, ByteBuffer> {
         // sends anything and only swaps itself with the next encoder
         try {
             // First, decoder must receive the protocol
-            if (!isDecoderReceivedProtocol && !channel.isClientMode()) {
+            if (!isDecoderReceivedProtocol) {
+                return BLOCKED;
+            }
+            if (isDecoderVerifiedProtocol) {
+                // Set up the next encoder in the pipeline once the protocol is verified
+                setupNextEncoder();
                 return CLEAN;
             }
 
-            // Decoder didn't verify the protocol, protocol error should be sent
-            if (!isDecoderVerifiedProtocol && !channel.isClientMode()) {
+            // Decoder received protocol bytes, but verification failed. If we are server/acceptor, then respond with the
+            // UNEXPECTED_PROTOCOL response bytes.
+            if (!channel.isClientMode()) {
                 if (!sendProtocol()) {
                     return DIRTY;
                 }
-                // UNEXPECTED_PROTOCOL is sent (or at least in the socket
-                // buffer). We can now throw exception in the pipeline to close
-                // the channel.
-                throw new ProtocolException(exceptionMessage);
             }
-
-            if (isDecoderVerifiedProtocol || channel.isClientMode()) {
-                // Set up the next encoder in the pipeline if in client mode
-                setupNextEncoder();
-            }
-
-            return CLEAN;
+            // Either we are in the client mode or the UNEXPECTED_PROTOCOL is sent already (or at least placed into the
+            // destination buffer). We can now throw exception in the pipeline to close the channel.
+            throw new ProtocolException(exceptionMessage);
         } finally {
             upcast(dst).flip();
         }
