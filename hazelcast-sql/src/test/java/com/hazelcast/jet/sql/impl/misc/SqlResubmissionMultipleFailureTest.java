97fda71df872783897148339eed829e52caf1a4d
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-06-17 17:22:36 +0200

    Test fixes

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/misc/SqlResubmissionMultipleFailureTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/misc/SqlResubmissionMultipleFailureTest.java
@@ -24,68 +24,34 @@ import com.hazelcast.spi.properties.ClusterProperty;
 import com.hazelcast.sql.HazelcastSqlException;
 import com.hazelcast.sql.SqlStatement;
 import com.hazelcast.test.ClusterFailureTestSupport;
-import com.hazelcast.test.HazelcastParametrizedRunner;
-import com.hazelcast.test.HazelcastSerialParametersRunnerFactory;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.SlowTest;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(HazelcastParametrizedRunner.class)
-@Parameterized.UseParametersRunnerFactory(HazelcastSerialParametersRunnerFactory.class)
 @Category({SlowTest.class, ParallelJVMTest.class})
 public class SqlResubmissionMultipleFailureTest extends SqlResubmissionTestSupport {
     private static final int SLOW_MAP_SIZE = 100;
 
     private static final int INITIAL_CLUSTER_SIZE = 1;
-    private static final int INITIAL_FAILURE_SIZE = 20;
+    private static final int INITIAL_FAILURE_SIZE = 10;
     private static final Config SMALL_INSTANCE_CONFIG = smallInstanceConfig()
             .setProperty(ClusterProperty.PARTITION_COUNT.getName(), "111");
 
-    @Parameterized.Parameter(0)
-    public ClusterFailureTestSupport.MultipleFailingInstanceClusterFailure clusterFailure;
+    public ClusterFailureTestSupport.MultipleFailingInstanceClusterFailure clusterFailure =
+            new ClusterFailureTestSupport.NodeShutdownClusterMultipleFailure();
+    public ClientSqlResubmissionMode resubmissionMode = ClientSqlResubmissionMode.RETRY_ALL;
 
-    @Parameterized.Parameter(1)
-    public ClientSqlResubmissionMode resubmissionMode;
-
-    private HazelcastInstance client;
-
-    @Parameterized.Parameters(name = "clusterFailure:{0}, resubmissionMode:{1}")
-    public static Collection<Object[]> parameters() {
-        List<Object[]> res = new ArrayList<>();
-        List<ClusterFailureTestSupport.MultipleFailingInstanceClusterFailure> failures = Arrays.asList(
-                new ClusterFailureTestSupport.NodeShutdownClusterMultipleFailure()
-        );
-        for (ClientSqlResubmissionMode mode : ClientSqlResubmissionMode.values()) {
-            for (ClusterFailureTestSupport.MultipleFailingInstanceClusterFailure failure : failures) {
-                res.add(new Object[]{failure, mode});
-            }
-        }
-        return res;
-    }
-
-    @Before
-    public void initFailure() {
+    @Test
+    public void when_failingUpdate() throws InterruptedException {
         clusterFailure.initialize(INITIAL_CLUSTER_SIZE, INITIAL_FAILURE_SIZE, SMALL_INSTANCE_CONFIG);
         createMap(clusterFailure.getFailingInstance(), SLOW_MAP_NAME, SLOW_MAP_SIZE, SlowFieldAccessObject::new,
                 SlowFieldAccessObject.class);
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getSqlConfig().setSqlResubmissionMode(resubmissionMode);
-        client = clusterFailure.createClient(clientConfig);
-    }
-
-    @Test
-    public void when_failingUpdate() throws InterruptedException {
+        HazelcastInstance client = clusterFailure.createClient(clientConfig);
         SqlStatement statement = new SqlStatement("update " + SLOW_MAP_NAME + " set field = field + 1");
 
         Thread failingThread = new Thread(() -> {
