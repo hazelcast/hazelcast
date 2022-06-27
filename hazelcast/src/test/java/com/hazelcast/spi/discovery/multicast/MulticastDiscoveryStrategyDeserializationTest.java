0f58b54bb7e1bda7f49e8b6806cc38c2a87cf7d9
Callum Galbreath <callum.galbreath@phocassoftware.com>
2022-06-28 10:49:57 +1200

    * Revert change to MulticastDiscoveryStrategyDeserializationTest

--- hazelcast/src/test/java/com/hazelcast/spi/discovery/multicast/MulticastDiscoveryStrategyDeserializationTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/discovery/multicast/MulticastDiscoveryStrategyDeserializationTest.java
@@ -16,12 +16,6 @@
 
 package com.hazelcast.spi.discovery.multicast;
 
-import static com.hazelcast.test.HazelcastTestSupport.assertTrueEventually;
-import static com.hazelcast.test.HazelcastTestSupport.smallInstanceConfig;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import com.hazelcast.config.Config;
 import com.hazelcast.config.DiscoveryStrategyConfig;
 import com.hazelcast.config.JoinConfig;
@@ -30,17 +24,24 @@ import com.hazelcast.instance.impl.HazelcastInstanceFactory;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
 import example.serialization.TestDeserialized;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.util.concurrent.TimeUnit;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
+
+import static com.hazelcast.test.HazelcastTestSupport.assertTrueEventually;
+import static com.hazelcast.test.HazelcastTestSupport.smallInstanceConfig;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests if safe-serialization property works in {@link MulticastDiscoveryStrategy}.
@@ -79,6 +80,7 @@ public class MulticastDiscoveryStrategyDeserializationTest {
         assertNull(datagramsThreadException);
         HazelcastInstanceFactory.terminateAll();
         TestDeserialized.isDeserialized = false;
+
     }
 
     @Test
@@ -94,7 +96,7 @@ public class MulticastDiscoveryStrategyDeserializationTest {
     public void testDefaultPacketFormat() throws Exception {
         Config config = createConfig(null);
         Hazelcast.newHazelcastInstance(config);
-        assertTrueEventually(() -> assertTrue("Object was not deserialized", TestDeserialized.isDeserialized));
+        assertTrueEventually(() -> assertTrue("Object was not deserialized", TestDeserialized.isDeserialized), 15);
     }
 
     private Config createConfig(Boolean safeSerialization) {
