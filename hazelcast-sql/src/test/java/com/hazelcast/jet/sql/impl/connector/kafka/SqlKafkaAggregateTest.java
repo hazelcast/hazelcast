4607bf1934fec166ee7085305a6625d6d714a8e5
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2022-07-15 11:44:26 +0300

    Temporarily disable SQL Kafka test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/kafka/SqlKafkaAggregateTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/kafka/SqlKafkaAggregateTest.java
@@ -24,6 +24,7 @@ import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -59,6 +60,7 @@ public class SqlKafkaAggregateTest extends SqlTestSupport {
     }
 
     @Test
+    @Ignore("https://github.com/hazelcast/hazelcast/issues/21784")
     public void test_tumble() {
         String name = createRandomTopic();
         sqlService.execute("CREATE MAPPING " + name + ' '
@@ -93,6 +95,7 @@ public class SqlKafkaAggregateTest extends SqlTestSupport {
     }
 
     @Test
+    @Ignore("https://github.com/hazelcast/hazelcast/issues/21784")
     public void test_hop() {
         String name = createRandomTopic();
         sqlService.execute("CREATE MAPPING " + name + ' '
