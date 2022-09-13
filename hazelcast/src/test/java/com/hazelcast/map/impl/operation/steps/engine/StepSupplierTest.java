3a061ac7d83a1131ed8236e66fbd85ae55666c39
Ahmet Mircik <ahmetmircik@gmail.com>
2022-09-13 12:35:59 +0300

    Break flow when operation timed out (#22195)
    Break flow when operation timed out

--- hazelcast/src/test/java/com/hazelcast/map/impl/operation/steps/engine/StepSupplierTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/operation/steps/engine/StepSupplierTest.java
@@ -15,10 +15,16 @@
  */
 package com.hazelcast.map.impl.operation.steps.engine;
 
+import com.hazelcast.config.Config;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.internal.serialization.Data;
+import com.hazelcast.internal.util.Clock;
+import com.hazelcast.map.impl.MapService;
+import com.hazelcast.map.impl.MapServiceContext;
 import com.hazelcast.map.impl.operation.MapOperation;
 import com.hazelcast.map.impl.operation.SetOperation;
+import com.hazelcast.spi.impl.operationservice.OperationAccessor;
+import com.hazelcast.spi.impl.operationservice.impl.responses.CallTimeoutResponse;
 import com.hazelcast.test.Accessors;
 import com.hazelcast.test.HazelcastParallelClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
@@ -27,6 +33,9 @@ import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -68,5 +77,45 @@ public class StepSupplierTest extends HazelcastTestSupport {
         Runnable get1 = stepSupplier.get();
         Runnable get2 = stepSupplier.get();
         assertEquals(get1, get2);
+    }
+
+    @Test
+    public void step_supplier_ends_with_call_timeout_response_when_operation_timed_out() {
+        // create node with force offload
+        Config config = smallInstanceConfig();
+        config.setProperty(MapServiceContext.FORCE_OFFLOAD_ALL_OPERATIONS.getName(), "true");
+        HazelcastInstance node = createHazelcastInstance(config);
+
+        // create state to use for test verification
+        AtomicReference<Object> expectedResponse = new AtomicReference<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // create and call slow operation
+        Data data = Accessors.getSerializationService(node).toData("data");
+        int partitionId = Accessors.getPartitionService(node).getPartitionId(data);
+
+        MapOperation operation = new SetOperation("test-map", data, data) {
+            @Override
+            protected void innerBeforeRun() throws Exception {
+                super.innerBeforeRun();
+                sleepAtLeastSeconds(2);
+            }
+        };
+        operation.setNodeEngine(Accessors.getNodeEngineImpl(node));
+        operation.setPartitionId(partitionId);
+        operation.setServiceName(MapService.SERVICE_NAME);
+        operation.setOperationResponseHandler((op, response) -> {
+            expectedResponse.set(response);
+            latch.countDown();
+        });
+
+        // set op times out after 1 second
+        OperationAccessor.setCallTimeout(operation, 1000);
+        OperationAccessor.setInvocationTime(operation, Clock.currentTimeMillis());
+        Accessors.getOperationService(node).execute(operation);
+
+        // wait operation end
+        assertOpenEventually(latch);
+        assertInstanceOf(CallTimeoutResponse.class, expectedResponse.get());
     }
 }
