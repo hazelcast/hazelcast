962d8284fd3b31dcfa7dc9088932d86b88ea7965
arodionov <andrii.rodionov@hazelcast.com>
2022-09-15 11:31:36 +0200

    [22155] Fix incorrect CP group leader rebalancing

--- hazelcast/src/main/java/com/hazelcast/cp/internal/RaftGroupMembershipManager.java
+++ hazelcast/src/main/java/com/hazelcast/cp/internal/RaftGroupMembershipManager.java
@@ -674,7 +674,7 @@ class RaftGroupMembershipManager {
                         continue;
                     }
                     int k = g.size();
-                    if (k < min) {
+                    if (k <= min) {
                         min = k;
                         to = member;
                         groupId = group.id();
