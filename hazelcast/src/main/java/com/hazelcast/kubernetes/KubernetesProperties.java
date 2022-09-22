5dae82298e913121215d6f20dcbb6b65bca1bf8a
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2022-09-22 19:34:26 +0300

    [CN-360] Add multiple label support for Kubernetes Plugin service and pod label filters (#22277)
    Add multiple label support for service and pods
    
    User API for the Kubernetes Discovery Plugin stays the same. Now
    the users will be able to give comma separated lists for giving multiple
    labels for Kubernetes Plugin configs "service-label-name",
    "service-label-value" and "pod-label-name" and "pod-label-value"

--- hazelcast/src/main/java/com/hazelcast/kubernetes/KubernetesProperties.java
+++ hazelcast/src/main/java/com/hazelcast/kubernetes/KubernetesProperties.java
@@ -70,12 +70,12 @@ public final class KubernetesProperties {
     public static final PropertyDefinition SERVICE_NAME = property("service-name", STRING);
     /**
      * <p>Configuration key: <code>service-label-name</code></p>
-     * Defines the service label to lookup through the Service Discovery REST API of Kubernetes.
+     * Defines the comma separated service labels to lookup through the Service Discovery REST API of Kubernetes.
      */
     public static final PropertyDefinition SERVICE_LABEL_NAME = property("service-label-name", STRING);
     /**
      * <p>Configuration key: <code>service-label-value</code></p>
-     * Defines the service label value to lookup through the Service Discovery REST API of Kubernetes.
+     * Defines the comma separated service label values to lookup through the Service Discovery REST API of Kubernetes.
      */
     public static final PropertyDefinition SERVICE_LABEL_VALUE = property("service-label-value", STRING);
 
@@ -87,12 +87,12 @@ public final class KubernetesProperties {
 
     /**
      * <p>Configuration key: <code>pod-label-name</code></p>
-     * Defines the pod label to lookup through the Service Discovery REST API of Kubernetes.
+     * Defines the comma separated pod labels to lookup through the Service Discovery REST API of Kubernetes.
      */
     public static final PropertyDefinition POD_LABEL_NAME = property("pod-label-name", STRING);
     /**
      * <p>Configuration key: <code>pod-label-value</code></p>
-     * Defines the pod label value to lookup through the Service Discovery REST API of Kubernetes.
+     * Defines the comma separated pod label values to lookup through the Service Discovery REST API of Kubernetes.
      */
     public static final PropertyDefinition POD_LABEL_VALUE = property("pod-label-value", STRING);
 
