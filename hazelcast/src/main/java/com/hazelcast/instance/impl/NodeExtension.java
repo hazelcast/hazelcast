28480b5a750c9dc9679db68adf65846be40ea84c
Vassilis Bekiaris <vassilis@hazelcast.com>
2022-09-23 00:40:44 +0300

    Add javadoc for isReady method

--- hazelcast/src/main/java/com/hazelcast/instance/impl/NodeExtension.java
+++ hazelcast/src/main/java/com/hazelcast/instance/impl/NodeExtension.java
@@ -177,8 +177,9 @@ public interface NodeExtension {
     boolean isStartCompleted();
 
     /**
-     * todo
-     * @return
+     * Check whether this instance is ready to start accepting requests. Use this
+     * method as a readiness probe.
+     * @return {@code true} when this instance is ready to accept requests, {@code false} otherwise.
      */
     boolean isReady();
 
