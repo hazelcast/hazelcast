2434631bd545054aabbcfdb8de2e188283a66707
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-09-22 15:51:27 +0200

    [HZ-1482] Make LikeFunction thread-safe w/o synchronized (#22272)

--- hazelcast/src/test/java/com/hazelcast/sql/impl/expression/ConcurrentInitialSetCacheTest.java
+++ hazelcast/src/test/java/com/hazelcast/sql/impl/expression/ConcurrentInitialSetCacheTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hazelcast.sql.impl.expression;
 
 import com.hazelcast.function.ConsumerEx;
