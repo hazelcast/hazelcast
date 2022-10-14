2f95d152a3886caaddca3b19750162c486042379
semihbkgr <buyukgungorsemih@gmail.com>
2022-10-14 14:59:04 +0300

    resolve current region using metadata endpoint

--- hazelcast/src/test/java/com/hazelcast/aws/AwsClientConfiguratorTest.java
+++ hazelcast/src/test/java/com/hazelcast/aws/AwsClientConfiguratorTest.java
@@ -46,23 +46,6 @@ public class AwsClientConfiguratorTest {
     }
 
     @Test
-    public void resolveRegionEcsConfig() {
-        // given
-        String region = "us-east-1";
-        AwsConfig awsConfig = AwsConfig.builder().build();
-        AwsMetadataApi awsMetadataApi = mock(AwsMetadataApi.class);
-        Environment environment = mock(Environment.class);
-        given(environment.getAwsRegionOnEcs()).willReturn(region);
-        given(environment.isRunningOnEcs()).willReturn(true);
-
-        // when
-        String result = resolveRegion(awsConfig, awsMetadataApi, environment);
-
-        // then
-        assertEquals(region, result);
-    }
-
-    @Test
     public void resolveRegionEc2Metadata() {
         // given
         AwsConfig awsConfig = AwsConfig.builder().build();
@@ -73,6 +56,21 @@ public class AwsClientConfiguratorTest {
         // when
         String result = resolveRegion(awsConfig, awsMetadataApi, environment);
 
+        // then
+        assertEquals("us-east-1", result);
+    }
+
+    @Test
+    public void resolveRegionEcsMetadata() {
+        // given
+        AwsConfig awsConfig = AwsConfig.builder().build();
+        AwsMetadataApi awsMetadataApi = mock(AwsMetadataApi.class);
+        Environment environment = mock(Environment.class);
+        given(environment.isRunningOnEcs()).willReturn(true);
+        given(awsMetadataApi.availabilityZoneEcs()).willReturn("us-east-1a");
+
+        // when
+        String result = resolveRegion(awsConfig, awsMetadataApi, environment);
         // then
         assertEquals("us-east-1", result);
     }
