d060b8db7375378cac32942a5a60473342732e0f
Semih <semih.buyukgungor@hazelcast.com>
2022-10-28 13:19:23 +0300

    [CN-543] Fix EC2 API DescribeNetworkInterfaces filter (#22488)
    Check filter of (private ip list) DescribeNetworkInterfaces request
    before sending. Otherwise, it will return the network interfaces of all
    EC2 instances when the filter is empty.
    
    Checklist:
    
    - [x] Labels (`Team:`, `Type:`, `Source:`, `Module:`) and Milestone set
    - [x] Label Add to `Release Notes` or `Not Release Notes` content set
    - [x] Request reviewers, if possible
    - [x] Send backports/forwardports if a fix needs to be applied to
    past/future releases
    - [x] New public APIs have `@Nonnull/@Nullable` annotations
    - [x] New public APIs have `@since` tags in Javadoc

--- hazelcast/src/test/java/com/hazelcast/aws/AwsCredentialsProviderTest.java
+++ hazelcast/src/test/java/com/hazelcast/aws/AwsCredentialsProviderTest.java
@@ -20,7 +20,7 @@ import com.hazelcast.config.InvalidConfigurationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
