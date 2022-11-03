c5815bc09ad156ad8cd82b902cde701d38e295bb
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-11-03 14:55:29 +0100

    Fix serialization of MemberInfo

--- hazelcast/src/main/java/com/hazelcast/internal/cluster/MemberInfo.java
+++ hazelcast/src/main/java/com/hazelcast/internal/cluster/MemberInfo.java
@@ -24,7 +24,6 @@ import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.version.MemberVersion;
 
 import javax.annotation.Nullable;
