002014833b6c99628de7bd2a47cbb614d57bef8c
Ahmet Mircik <ahmetmircik@gmail.com>
2022-11-05 13:18:55 +0300

    Init cache partitions

--- hazelcast/src/test/java/com/hazelcast/query/impl/JsonIndexIntegrationTest.java
+++ hazelcast/src/test/java/com/hazelcast/query/impl/JsonIndexIntegrationTest.java
@@ -203,7 +203,7 @@ public class JsonIndexIntegrationTest extends HazelcastTestSupport {
         MapContainer mapContainer = mapServiceContext.getMapContainer(mapName);
 
         List<Index> result = new ArrayList<Index>();
-        for (int partitionId : mapServiceContext.getOrInitCachedMemberPartitions()) {
+        for (int partitionId : mapServiceContext.getCachedOwnedPartitions()) {
             Indexes indexes = mapContainer.getIndexes(partitionId);
 
             for (InternalIndex index : indexes.getIndexes()) {
