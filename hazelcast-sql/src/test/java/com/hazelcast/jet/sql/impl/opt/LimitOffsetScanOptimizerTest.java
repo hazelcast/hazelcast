6ef2b004bbe95894f0a480e63c04bf2acb205e39
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2022-11-16 14:16:37 +0200

    Add suggested Limit_Sort_Scan optimizer test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/opt/LimitOffsetScanOptimizerTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/opt/LimitOffsetScanOptimizerTest.java
@@ -17,8 +17,10 @@
 package com.hazelcast.jet.sql.impl.opt;
 
 import com.hazelcast.config.IndexType;
+import com.hazelcast.jet.sql.impl.opt.physical.FullScanPhysicalRel;
 import com.hazelcast.jet.sql.impl.opt.physical.IndexScanMapPhysicalRel;
 import com.hazelcast.jet.sql.impl.opt.physical.LimitPhysicalRel;
+import com.hazelcast.jet.sql.impl.opt.physical.SortPhysicalRel;
 import com.hazelcast.jet.sql.impl.schema.HazelcastTable;
 import com.hazelcast.map.IMap;
 import com.hazelcast.sql.impl.extract.QueryPath;
@@ -33,7 +35,7 @@ import java.util.List;
 import static com.hazelcast.sql.impl.schema.map.MapTableUtils.getPartitionedMapIndexes;
 import static java.util.Arrays.asList;
 
-public class LimitOffsetIndexScanTest extends OptimizerTestSupport {
+public class LimitOffsetScanOptimizerTest extends OptimizerTestSupport {
     @BeforeClass
     public static void beforeClass() throws Exception {
         initialize(1, null);
@@ -41,7 +43,7 @@ public class LimitOffsetIndexScanTest extends OptimizerTestSupport {
 
     // Test for https://github.com/hazelcast/hazelcast/issues/19223
     @Test
-    public void test() {
+    public void test_LimitAndIndexScan_arePresentInPlan() {
         String mapName = randomName();
         IMap<Integer, String> map = instance().getMap(mapName);
         map.addIndex(IndexType.SORTED, "this");
@@ -67,6 +69,37 @@ public class LimitOffsetIndexScanTest extends OptimizerTestSupport {
                 plan(
                         planRow(0, LimitPhysicalRel.class),
                         planRow(1, IndexScanMapPhysicalRel.class)
+                )
+        );
+    }
+
+    @Test
+    public void test_Limit_Sort_Scan_arePresentInPlan() {
+        String mapName = randomName();
+        IMap<Integer, String> map = instance().getMap(mapName);
+        for (int i = 0; i < 100; i++) {
+            map.put(i, "" + i);
+        }
+
+        List<QueryDataType> parameterTypes = asList(QueryDataType.INT, QueryDataType.VARCHAR);
+        List<TableField> mapTableFields = asList(
+                new MapTableField("__key", QueryDataType.INT, false, QueryPath.KEY_PATH),
+                new MapTableField("this", QueryDataType.VARCHAR, false, QueryPath.VALUE_PATH)
+        );
+        HazelcastTable table = partitionedTable(
+                mapName,
+                mapTableFields,
+                getPartitionedMapIndexes(mapContainer(map), mapTableFields),
+                map.size()
+        );
+
+        final String sql = "SELECT * FROM " + mapName + " ORDER BY this DESC LIMIT 5 OFFSET 3";
+
+        assertPlan(optimizePhysical(sql, parameterTypes, table).getPhysical(),
+                plan(
+                        planRow(0, LimitPhysicalRel.class),
+                        planRow(1, SortPhysicalRel.class),
+                        planRow(2, FullScanPhysicalRel.class)
                 )
         );
     }
