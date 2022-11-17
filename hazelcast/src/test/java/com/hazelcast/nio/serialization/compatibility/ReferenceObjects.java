d325883fc31c2aa6879716df844bde685a466775
Mehmet Tokgöz <mehmet.tokgoz@hazelcast.com>
2022-11-17 09:49:47 +0300

    refactor.

--- hazelcast/src/test/java/com/hazelcast/nio/serialization/compatibility/ReferenceObjects.java
+++ hazelcast/src/test/java/com/hazelcast/nio/serialization/compatibility/ReferenceObjects.java
@@ -174,17 +174,17 @@ class ReferenceObjects {
         localDates= new LocalDate[]{
                 LocalDate.of(2021, 6, 28),
                 LocalDate.of(1923, 4, 23),
-                LocalDate.of(1938, 11, 10),};
+                LocalDate.of(1938, 11, 10)};
         localTimes = new LocalTime[]{
                 LocalTime.of(9, 5, 10, 123456789),
                 LocalTime.of(18, 30, 55, 567891234),
                 LocalTime.of(15, 44, 39, 192837465)};
         localDateTimes= new LocalDateTime[]{
-                LocalDateTime.of(LocalDate.of(1938, 11, 10),LocalTime.of(9, 5, 10, 123456789)),
+                LocalDateTime.of(LocalDate.of(1938, 11, 10), LocalTime.of(9, 5, 10, 123456789)),
                 LocalDateTime.of(LocalDate.of(1923, 4, 23), LocalTime.of(15, 44, 39, 192837465)),
                 LocalDateTime.of(LocalDate.of(2021, 6, 28), LocalTime.of(18, 30, 55, 567891234))};
         offsetDateTimes= new OffsetDateTime[]{
-                OffsetDateTime.of(LocalDate.of(1938, 11, 10),LocalTime.of(9, 5, 10, 123456789),
+                OffsetDateTime.of(LocalDate.of(1938, 11, 10), LocalTime.of(9, 5, 10, 123456789),
                         ZoneOffset.ofHours(18)),
                 OffsetDateTime.of(LocalDateTime.of(LocalDate.of(1923, 4, 23), LocalTime.of(15, 44, 39, 192837465)),
                         ZoneOffset.ofHours(5)),
