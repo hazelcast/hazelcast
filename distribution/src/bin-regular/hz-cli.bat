b91927de5e2f747f106c0f14f1f268ecaf04a558
Stephan <stephane.gay@hazelcast.com>
2022-11-18 12:41:48 +0100

    Fix some hz-cli quirks on Windows

--- distribution/src/bin-regular/hz-cli.bat
+++ distribution/src/bin-regular/hz-cli.bat
@@ -6,7 +6,6 @@ if NOT DEFINED JAVA_HOME goto error
 set RUN_JAVA=%JAVA_HOME%\bin\java
 set HAZELCAST_HOME=%~dp0..
 set CLASSPATH="%HAZELCAST_HOME%\lib\*";%CLASSPATH%
-set JAVA_OPTS=%JAVA_OPTS% "-Dhazelcast.client.config=%HAZELCAST_HOME%\config\hazelcast-client.yaml"
 
 "%RUN_JAVA%" %JAVA_OPTS% -cp %CLASSPATH% com.hazelcast.client.console.HazelcastCommandLine %*
 goto endofscript
