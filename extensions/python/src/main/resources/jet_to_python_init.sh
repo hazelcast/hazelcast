89c26b45580d2c605bb895e5aef7926637f3b90b
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2022-11-22 11:04:15 +0100

    Revert "Update protobuf to 21.9 (#22826) [5.0.z] (#22881)" [5.0.z] (#22909)
    This reverts commit cec5ac16fdaaa25ed1d2391d221a65835639141f.
    
    protobuf 3.19.6 is not affected by CVE-2022-3509

--- extensions/python/src/main/resources/jet_to_python_init.sh
+++ extensions/python/src/main/resources/jet_to_python_init.sh
@@ -6,7 +6,7 @@ set -e -x
 python3 -m venv jet_to_python_env --system-site-packages
 source jet_to_python_env/bin/activate
 python3 -m pip install --upgrade pip
-python3 -m pip install protobuf==@python.protobuf.version@ grpcio==@grpc.version@
+python3 -m pip install protobuf==@protobuf.version@ grpcio==@grpc.version@
 [[ -f requirements.txt ]] && python3 -m pip install -r requirements.txt
 source ./jet_to_python_params.sh
 if [[ -f init.sh ]]; then
