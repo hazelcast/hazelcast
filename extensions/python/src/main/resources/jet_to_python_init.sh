641ca4b57e000b43902faf52d15119dbcca177ec
Frantisek Hartman <frant.hartm@gmail.com>
2022-11-22 13:24:46 +0100

    Use home directory instead of /tmp

--- extensions/python/src/main/resources/jet_to_python_init.sh
+++ extensions/python/src/main/resources/jet_to_python_init.sh
@@ -6,8 +6,8 @@ set -e -x
 python3 -m venv jet_to_python_env --system-site-packages
 source jet_to_python_env/bin/activate
 
-flock --exclusive /tmp/jet-pip.lock python3 -m pip install --upgrade pip
-flock --exclusive /tmp/jet-pip.lock python3 -m pip install protobuf==@protobuf.version@ grpcio==@grpc.version@
+flock --exclusive ~/.jet-pip.lock python3 -m pip install --upgrade pip
+flock --exclusive ~/.jet-pip.lock python3 -m pip install protobuf==@protobuf.version@ grpcio==@grpc.version@
 
 [[ -f requirements.txt ]] && python3 -m pip install -r requirements.txt
 source ./jet_to_python_params.sh
