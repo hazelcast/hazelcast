e64b3a33101e2efbbe4ad6012fd62fc977675de9
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2022-11-29 14:11:15 +0100

    Fix connection number exceeding max in WriteJdbcPTest (#22968)
    1. Max connection number increased
    2. After each test all remaining connections will be closed (in case of a bug in DS, not Hz).
    
    (cherry picked from commit 88ea27687d5c64a950abb93ec571aa274f7a243d)

--- hazelcast/src/test/java/com/hazelcast/jet/impl/connector/WriteJdbcPTest.java
+++ hazelcast/src/test/java/com/hazelcast/jet/impl/connector/WriteJdbcPTest.java
@@ -29,6 +29,8 @@ import com.hazelcast.jet.pipeline.test.TestSources;
 import com.hazelcast.jet.test.IgnoreInJenkinsOnWindows;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.SlowTest;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -44,6 +46,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.SQLNonTransientException;
 import java.sql.Statement;
@@ -72,13 +75,14 @@ public class WriteJdbcPTest extends SimpleTestInClusterSupport {
     private static final String DUMMY_DATA_STORE = "dummy-data-store";
 
     @ClassRule
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings({"rawtypes", "resource"})
     public static PostgreSQLContainer container = new PostgreSQLContainer<>("postgres:12.1")
-            .withCommand("postgres -c max_prepared_transactions=10");
+            .withCommand("postgres -c max_prepared_transactions=10 -c max_connections=500");
 
     private static final int PERSON_COUNT = 10;
 
-    private static AtomicInteger tableCounter = new AtomicInteger();
+    private static final AtomicInteger TABLE_COUNTER = new AtomicInteger();
+    private static HikariDataSource hikariDataSource;
     private String tableName;
 
     @BeforeClass
@@ -87,7 +91,7 @@ public class WriteJdbcPTest extends SimpleTestInClusterSupport {
     }
 
     @BeforeClass
-    public static void setupClass() throws SQLException {
+    public static void setupClass() {
         Config config = smallInstanceConfig();
         configureJdbcDataStore(JDBC_DATA_STORE, container.getJdbcUrl(), container.getUsername(), container.getPassword(), config);
         configureDummyDataStore(DUMMY_DATA_STORE, config);
@@ -96,11 +100,48 @@ public class WriteJdbcPTest extends SimpleTestInClusterSupport {
 
     @Before
     public void setup() throws SQLException {
-        tableName = "T" + tableCounter.incrementAndGet();
+        tableName = "T" + TABLE_COUNTER.incrementAndGet();
         logger.info("Will use table: " + tableName);
+        /* language=SQL */
+        executeSql("CREATE TABLE " + tableName + "(id int, name varchar(255))");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (hikariDataSource != null) {
+            hikariDataSource.close();
+        }
+
+        listRemainingConnections();
+        // kill any hanging connection
+        /* language=SQL */
+        executeSql("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid()");
+    }
+
+    private static void executeSql(String sql) throws SQLException {
         try (Connection connection = ((DataSource) createDataSource(false)).getConnection()) {
-            connection.createStatement()
-                    .execute("CREATE TABLE " + tableName + "(id int, name varchar(255))");
+            connection.createStatement().execute(sql);
+        }
+    }
+    private void listRemainingConnections() throws SQLException {
+        try (
+                Connection connection = ((DataSource) createDataSource(false)).getConnection();
+                ResultSet resultSet = connection.createStatement().executeQuery(
+                        "SELECT * FROM pg_stat_activity WHERE pid <> pg_backend_pid()")
+        ) {
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            List<String> connections = new ArrayList<>();
+            for (int i = 0; i < metaData.getColumnCount(); i++) {
+                connections.add(metaData.getColumnName(i) + "|");
+            }
+            while (resultSet.next()) {
+                for (int i = 0; i < metaData.getColumnCount(); i++) {
+                    connections.add(resultSet.getObject(i) + "|");
+                }
+            }
+            if (!connections.isEmpty()) {
+                logger.warning("Remaining connections: \n" + String.join("\n", connections));
+            }
         }
     }
 
