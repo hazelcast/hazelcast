9bd1b55e92e4d002623672be7c47b3c05801aa77
Isaac Sumner <isaac@hazelcast.com>
2022-12-07 13:26:23 -0500

    Add failing test

---
+++ hazelcast-sql/src/test/java/com/hazelcast/SqlFailingTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 Hazelcast Inc.
+ *
+ * Licensed under the Hazelcast Community License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://hazelcast.com/hazelcast-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast;
+
+import com.hazelcast.jet.sql.SqlTestSupport;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.fail;
+
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class SqlFailingTest extends SqlTestSupport {
+
+    @Test
+    public void testFailure() {
+        fail("Test fails!");
+    }
+}
