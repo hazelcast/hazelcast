352f64566e0f76da10b082f739174178d6a12ac5
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2022-12-07 10:33:27 +0200

    Simple snapshot trigger in stress test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/s2sjoin/SqlSTSJoinFaultToleranceStressTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/s2sjoin/SqlSTSJoinFaultToleranceStressTest.java
@@ -1,5 +1,7 @@
 package com.hazelcast.jet.sql.impl.s2sjoin;
 
+import com.hazelcast.jet.JetService;
+import com.hazelcast.jet.Job;
 import com.hazelcast.jet.kafka.impl.KafkaTestSupport;
 import com.hazelcast.jet.sql.SqlTestSupport;
 import com.hazelcast.jet.sql.impl.connector.kafka.KafkaSqlConnector;
@@ -11,6 +13,7 @@ import com.hazelcast.test.HazelcastSerialParametersRunnerFactory;
 import com.hazelcast.test.annotation.NightlyTest;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -32,12 +35,14 @@ import static org.junit.Assert.assertEquals;
 @Category({NightlyTest.class, ParallelJVMTest.class})
 public class SqlSTSJoinFaultToleranceStressTest extends SqlTestSupport {
     private static final int INITIAL_PARTITION_COUNT = 1;
+    private static final int EVENTS_COUNT = 20_000;
     private static KafkaTestSupport kafkaTestSupport;
 
     private SqlService sqlService;
     private String mapName;
     private String topicName;
     private IMap<Integer, String> map;
+    private AssertionError ex;
 
     private String query;
 
@@ -57,7 +62,7 @@ public class SqlSTSJoinFaultToleranceStressTest extends SqlTestSupport {
     }
 
     @AfterClass
-    public static void afterClass() throws Exception {
+    public static void afterClass() {
         kafkaTestSupport.shutdownKafkaCluster();
     }
 
@@ -92,9 +97,51 @@ public class SqlSTSJoinFaultToleranceStressTest extends SqlTestSupport {
 
     @Test
     public void stressTest() {
+        JobRestarter jobRestarter = new JobRestarter(instance().getJet());
+        jobRestarter.start();
+
         SqlResult result = sqlService.execute(query);
         assertEquals(0, result.updateCount());
-        assertTrueEventually(() -> assertEquals(300_000, map.size()));
+        assertTrueEventually(() -> assertEquals(EVENTS_COUNT, map.size()));
+        jobRestarter.finish();
+
+        try {
+            jobRestarter.join();
+        } catch (AssertionError e) {
+            Assert.fail(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static class JobRestarter extends Thread {
+        private final JetService jetService;
+        private volatile boolean finish;
+
+        JobRestarter(JetService jetService) {
+            this.jetService = jetService;
+        }
+
+        @Override
+        public void run() {
+            for (; ; ) {
+                try {
+                    if (finish) {
+                        return;
+                    }
+                    Thread.sleep(1000L);
+                    assert jetService.getJobs().size() == 1;
+                    Job job = jetService.getJobs().get(0);
+                    job.restart();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public void finish() {
+            this.finish = true;
+        }
     }
 
     private static String createRandomTopic() {
@@ -105,11 +152,10 @@ public class SqlSTSJoinFaultToleranceStressTest extends SqlTestSupport {
 
     private static void createTopicData(SqlService sqlService, String topicName) {
         StringBuilder queryBuilder = new StringBuilder("INSERT INTO " + topicName + " VALUES ");
-        int limit = 300_000;
-        for (int i = 1; i < limit; ++i) {
+        for (int i = 1; i < EVENTS_COUNT; ++i) {
             queryBuilder.append("(").append(i).append(", 'value-").append(i).append("'), ");
         }
-        queryBuilder.append("(").append(limit).append(", 'value-").append(limit).append("')");
+        queryBuilder.append("(").append(EVENTS_COUNT).append(", 'value-").append(EVENTS_COUNT).append("')");
 
         sqlService.execute(queryBuilder.toString());
     }
