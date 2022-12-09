304469587e7fee5956c8fbcf5afb7ccc769010cf
Ahmet Mircik <ahmetmircik@gmail.com>
2022-12-09 12:36:22 +0300

    Check node and cluster healt before running each Step

--- hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/OperationRunnerImpl.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationservice/impl/OperationRunnerImpl.java
@@ -230,6 +230,18 @@ public class OperationRunnerImpl extends OperationRunner implements StaticMetric
     }
 
     /**
+     * Like {@link #metWithPreconditions} but without timeout checks.
+     * <p>
+     * Includes checks related to node and cluster health
+     * before execution of an operation.
+     */
+    public void ensureNodeAndClusterHealth(Operation op) {
+        checkNodeState(op);
+        ensureNoPartitionProblems(op);
+        ensureNoSplitBrain(op);
+    }
+
+    /**
      * Runs the provided operation.
      *
      * @param op         the operation to execute
@@ -354,7 +366,7 @@ public class OperationRunnerImpl extends OperationRunner implements StaticMetric
         splitBrainProtectionService.ensureNoSplitBrain(op);
     }
 
-    private boolean timeout(Operation op) {
+    public boolean timeout(Operation op) {
         if (!operationService.isCallTimedOut(op)) {
             return false;
         }
