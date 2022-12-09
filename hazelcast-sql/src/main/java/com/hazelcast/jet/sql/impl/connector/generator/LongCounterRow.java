4a262f0ad3b4532555204ac32f8687d4d917c2a4
Krzysztof Jamróz <krzysztof.jamroz@hazelcast.com>
2022-12-09 16:34:12 +0100

    Fix checkstyle errors

--- hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/connector/generator/LongCounterRow.java
+++ hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/connector/generator/LongCounterRow.java
@@ -1,18 +1,19 @@
 /*
- * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ * Copyright 2021 Hazelcast Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Hazelcast Community License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http://hazelcast.com/hazelcast-community-license
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 package com.hazelcast.jet.sql.impl.connector.generator;
 
@@ -27,7 +28,7 @@ class LongCounterRow implements Row {
 
     private final long value;
 
-    public LongCounterRow(long value) {
+    LongCounterRow(long value) {
         this.value = value;
     }
 
@@ -46,8 +47,12 @@ class LongCounterRow implements Row {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         LongCounterRow that = (LongCounterRow) o;
         return value == that.value;
     }
