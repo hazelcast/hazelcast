3788612eb4df264da43491cb64cb0afe44e20fb4
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-12-20 14:41:10 +0100

    Rawtypes

---
+++ hazelcast-perf-tests/src/test/java/com/hazelcast/internal/perf/BenchmarkRunner.java
@@ -0,0 +1,14 @@
+package com.hazelcast.internal.perf;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .build();
+        new Runner(opt).run();
+    }
+}
