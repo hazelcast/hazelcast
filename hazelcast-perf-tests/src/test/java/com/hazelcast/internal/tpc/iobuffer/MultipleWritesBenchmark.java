f3f447b96847f8fc493cf56e686c47b5dd7b0d03
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-12-20 09:56:55 +0100

    Benchmarks

--- hazelcast-perf-tests/src/test/java/com/hazelcast/internal/tpc/iobuffer/MultipleWritesBenchmark.java
+++ hazelcast-perf-tests/src/test/java/com/hazelcast/internal/tpc/iobuffer/MultipleWritesBenchmark.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.internal.tpc.iobuffer;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -13,7 +29,6 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.profile.LinuxPerfProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -117,7 +132,6 @@ public class MultipleWritesBenchmark {
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(MultipleWritesBenchmark.class.getSimpleName())
-                .addProfiler(LinuxPerfProfiler.class)
                 .build();
 
         new Runner(opt).run();
