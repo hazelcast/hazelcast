8e4b23a349ca2bdee7fc5316714c78b62012872b
orcuncolak <orcun.colak@hazelcast.com>
2023-01-13 16:10:52 +0300

    updated copyright [HZ-1455]

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/codec/JetUploadJobMetaDataCodec.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/codec/JetUploadJobMetaDataCodec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import static com.hazelcast.client.impl.protocol.codec.builtin.FixedSizeTypesCod
 
 /**
  */
-@Generated("6814b5e0404aed3dd1ef51464959cf2f")
+@Generated("2a6487db39a9bbd8320968a72a4192b6")
 public final class JetUploadJobMetaDataCodec {
     //hex: 0xFE1000
     public static final int REQUEST_MESSAGE_TYPE = 16650240;
