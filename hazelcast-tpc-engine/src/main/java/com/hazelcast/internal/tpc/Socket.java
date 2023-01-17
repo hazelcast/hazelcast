3e06818716ca1584add18399456a07cf3d08093b
Peter Veentjer <alarmnummer@gmail.com>
2023-01-17 05:51:27 +0200

    Fixed race condition at closing with close listener

--- hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/Socket.java
+++ hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/Socket.java
@@ -18,7 +18,8 @@ import static com.hazelcast.internal.tpc.util.Preconditions.checkNotNull;
 public abstract class Socket implements Closeable {
 
     /**
-     * Allows for objects to be bound to this socket. Useful for the lookup of services and other dependencies.
+     * Allows for objects to be bound to this socket. Useful for the lookup of
+     * services and other dependencies.
      */
     @SuppressWarnings("checkstyle:VisibilityModifier")
     public final ConcurrentMap<?, ?> context = new ConcurrentHashMap<>();
@@ -29,37 +30,44 @@ public abstract class Socket implements Closeable {
     private volatile Throwable closeCause;
     private CloseListener closeListener;
     private Executor closeExecutor;
+    private boolean closeListenerChecked;
 
     /**
      * Configures the close listener.
      * <p/>
      * Can only be configured once.
      * <p/>
-     * Is threadsafe.
+     * This call is threadsafe.
      * <p/>
      * If the method is called when the socket already is closed, the close listener
-     * is called.
+     * is notified.
      *
-     * @param listener the CloseListener
-     * @param executor the executor used to execute the {@link CloseListener#close()} method.
+     * @param listener the close listener to set.
+     * @param executor the executor used to execute the close listener.
      * @throws NullPointerException  if listener or executor is null.
-     * @throws IllegalStateException if already set.
+     * @throws IllegalStateException if a close listener is already set.
      */
     public final void setCloseListener(CloseListener listener, Executor executor) {
         checkNotNull(executor, "executor");
         checkNotNull(listener, "listener");
 
         // Using lock to make sure that there is a matching listener/executor.
+        boolean closeListenerChecked;
         synchronized (this) {
-            if (closeExecutor != null) {
+            if (closeListener != null) {
                 throw new IllegalStateException("Can't reset the closeListener");
             }
+
             this.closeExecutor = executor;
             this.closeListener = listener;
+            closeListenerChecked = this.closeListenerChecked;
         }
 
-        // if the socket is already closed, we need to notify.
-        if (state.get() == State.CLOSED) {
+        if (closeListenerChecked) {
+            // the closing thread already checked the closeListener and therefor
+            // it hasn't seen the close listener that is now being set. So we need
+            // to notify the close listener ourselves to prevent omitting the
+            // notification.
             notifyCloseListener(listener, executor);
         }
     }
@@ -138,8 +146,12 @@ public abstract class Socket implements Closeable {
         CloseListener closeListener;
         Executor closeExecutor;
         synchronized (this) {
+            this.closeListenerChecked = true;
             closeListener = this.closeListener;
             closeExecutor = this.closeExecutor;
+            // this will signal to a different thread calling the setCloseListener that
+            // the socket is closed but the thread calling the close will not check any
+            // change to the closeListener after this point.
         }
 
         if (closeListener != null) {
