e6f3ceeeb5b92f31d9820517f06fa5825a8ada1b
Orçun Çolak <orcun.colak@hazelcast.com>
2023-01-19 17:16:40 +0300

    Fix the update parameter logic [HZ-1639]

--- extensions/mapstore/src/test/java/com/hazelcast/mapstore/JDBCParametersTest.java
+++ extensions/mapstore/src/test/java/com/hazelcast/mapstore/JDBCParametersTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JDBCParametersTest {
 
     @Test
-    public void testShiftParametersForUpdate() {
+    public void testShiftParametersForUpdateForPosition0() {
 
         JDBCParameters jDBCParameters = new JDBCParameters();
         jDBCParameters.setParams(new Object[] {1,2,3});
@@ -17,5 +17,19 @@ public class JDBCParametersTest {
         Object[] params = jDBCParameters.getParams();
 
         assertThat(params).isEqualTo(new Object[] {2,3,1});
+    }
+
+    @Test
+    public void testShiftParametersForUpdateForPosition1() {
+
+        JDBCParameters jDBCParameters = new JDBCParameters();
+        jDBCParameters.setParams(new Object[] {1,2,3});
+        jDBCParameters.setIdPos(1);
+
+        jDBCParameters.shiftParametersForUpdate();
+
+        Object[] params = jDBCParameters.getParams();
+
+        assertThat(params).isEqualTo(new Object[] {1,3,2});
     }
 }
