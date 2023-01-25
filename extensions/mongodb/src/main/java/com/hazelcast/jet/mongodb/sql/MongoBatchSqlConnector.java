0d001c047ce2947bc1a56c50e32631de38b06ec7
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-01-25 17:19:13 +0100

    MongoDB Batch sql connector in progress

---
+++ extensions/mongodb/src/main/java/com/hazelcast/jet/mongodb/sql/MongoBatchSqlConnector.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 Hazelcast Inc.
+ *
+ * Licensed under the Hazelcast Community License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://hazelcast.com/hazelcast-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.jet.mongodb.sql;
+
+import com.hazelcast.jet.sql.impl.connector.SqlConnector;
+import com.hazelcast.spi.impl.NodeEngine;
+import com.hazelcast.sql.impl.schema.MappingField;
+import com.hazelcast.sql.impl.schema.Table;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Batch-query MongoDB SQL Connector.
+ * TODO: add more
+ */
+public class MongoBatchSqlConnector implements SqlConnector {
+
+    private final FieldResolver fieldResolver = new FieldResolver();
+
+    @Override
+    public String typeName() {
+        return "MongoDB";
+    }
+
+    @Override
+    public boolean isStream() {
+        return false;
+    }
+
+    @Nonnull
+    @Override
+    public List<MappingField> resolveAndValidateFields(
+            @Nonnull NodeEngine nodeEngine,
+            @Nonnull Map<String, String> options,
+            @Nonnull List<MappingField> userFields,
+            @Nonnull String externalName
+    ) {
+        return fieldResolver.resolveFields(options, userFields);
+    }
+
+    @Nonnull
+    @Override
+    public Table createTable(@Nonnull NodeEngine nodeEngine, @Nonnull String schemaName, @Nonnull String mappingName,
+                             @Nonnull String externalName, @Nonnull Map<String, String> options,
+                             @Nonnull List<MappingField> resolvedFields) {
+        return null;
+    }
+}
