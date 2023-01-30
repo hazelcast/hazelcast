4a8a3730fca9f4547f42963861e0be313cc807a3
ivanthescientist <ivan.yaschishin@hazelcast.com>
2023-01-30 03:33:02 +0200

    basic implementation

---
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/type/AttributePartitioningStrategyTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2023 Hazelcast Inc.
+ *
+ * Licensed under the Hazelcast Community License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://hazelcast.com/hazelcast-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.sql.impl.type;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.PartitioningStrategyConfig;
+import com.hazelcast.core.HazelcastException;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.instance.impl.HazelcastInstanceProxy;
+import com.hazelcast.jet.sql.SqlJsonTestSupport;
+import com.hazelcast.map.IMap;
+import com.hazelcast.nio.serialization.ClassDefinition;
+import com.hazelcast.nio.serialization.ClassDefinitionBuilder;
+import com.hazelcast.nio.serialization.genericrecord.GenericRecord;
+import com.hazelcast.nio.serialization.genericrecord.GenericRecordBuilder;
+import com.hazelcast.partition.Partition;
+import com.hazelcast.partition.PartitioningStrategy;
+import com.hazelcast.partition.strategy.AttributePartitioningStrategy;
+import com.hazelcast.test.HazelcastSerialClassRunner;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.hazelcast.spi.properties.ClusterProperty.PARTITION_COUNT;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(HazelcastSerialClassRunner.class)
+public class AttributePartitioningStrategyTest extends SqlJsonTestSupport {
+    private static final ClassDefinition PORTABLE_CLASS = new ClassDefinitionBuilder(1, 1, 1)
+            .addLongField("id")
+            .addStringField("name")
+            .addStringField("org")
+            .build();
+
+    private static final PartitioningStrategy STRATEGY = new AttributePartitioningStrategy("org");
+    private static final String PARTITION_ATTRIBUTE = "1";
+    private static final Object[] PARTITION_KEY = new Object[]{PARTITION_ATTRIBUTE};
+
+    @BeforeClass
+    public static void beforeClass() {
+        Config config = smallInstanceConfig().setProperty(PARTITION_COUNT.getName(), "3");
+        config.getMapConfig("test").setPartitioningStrategyConfig(new PartitioningStrategyConfig()
+                .setPartitioningStrategy(STRATEGY));
+
+        initialize(3, config);
+    }
+
+    @Test
+    public void testJavaObject() {
+        final IMap<JavaKey, Long> map = instance().getMap("test");
+
+        for (long i = 0; i < 10000; i++) {
+            map.put(new JavaKey(i, "key#" + i, PARTITION_ATTRIBUTE), i);
+        }
+
+        final HazelcastInstance owner = getOwner(PARTITION_KEY);
+        final List<HazelcastInstance> nonOwners = getNonOwners(owner);
+
+        assertEquals(10000, owner.getMap("test").getLocalMapStats().getOwnedEntryCount());
+        assertEquals(0, nonOwners.get(0).getMap("test").getLocalMapStats().getOwnedEntryCount());
+        assertEquals(0, nonOwners.get(1).getMap("test").getLocalMapStats().getOwnedEntryCount());
+    }
+
+    @Test
+    public void testCompactObject() {
+        final IMap<GenericRecord, Long> map = instance().getMap("test");
+
+        for (long i = 0; i < 10000; i++) {
+            map.put(GenericRecordBuilder.compact("testType")
+                    .setInt64("id", i)
+                    .setString("name", "key#" + i)
+                    .setString("org", PARTITION_ATTRIBUTE)
+                    .build(), i);
+        }
+
+        final HazelcastInstance owner = getOwner(PARTITION_KEY);
+        final List<HazelcastInstance> nonOwners = getNonOwners(owner);
+
+        assertEquals(10000, owner.getMap("test").getLocalMapStats().getOwnedEntryCount());
+        assertEquals(0, nonOwners.get(0).getMap("test").getLocalMapStats().getOwnedEntryCount());
+        assertEquals(0, nonOwners.get(1).getMap("test").getLocalMapStats().getOwnedEntryCount());
+    }
+
+    @Test
+    public void testPortableObject() {
+        final IMap<GenericRecord, Long> map = instance().getMap("test");
+
+        for (long i = 0; i < 10000; i++) {
+            map.put(GenericRecordBuilder.portable(PORTABLE_CLASS)
+                    .setInt64("id", i)
+                    .setString("name", "key#" + i)
+                    .setString("org", PARTITION_ATTRIBUTE)
+                    .build(), i);
+        }
+
+        final HazelcastInstance owner = getOwner(PARTITION_KEY);
+        final List<HazelcastInstance> nonOwners = getNonOwners(owner);
+
+        assertEquals(10000, owner.getMap("test").getLocalMapStats().getOwnedEntryCount());
+        assertEquals(0, nonOwners.get(0).getMap("test").getLocalMapStats().getOwnedEntryCount());
+        assertEquals(0, nonOwners.get(1).getMap("test").getLocalMapStats().getOwnedEntryCount());
+    }
+
+    private HazelcastInstance getOwner(Object partitionKey) {
+        final Partition partition = instance().getPartitionService().getPartition(partitionKey);
+        if (partition == null) {
+            throw new HazelcastException("Can not find partition for key: " + partitionKey);
+        }
+
+        return Arrays.stream(instances())
+                .filter(instance -> ((HazelcastInstanceProxy) instance).getOriginal().node.address
+                        .equals(partition.getOwner().getAddress()))
+                .findFirst()
+                .orElseThrow(() -> new HazelcastException("Can not find partition owner"));
+    }
+
+    private List<HazelcastInstance> getNonOwners(final HazelcastInstance owner) {
+        return Arrays.stream(instances())
+                .filter(instance -> !instance.equals(owner))
+                .collect(Collectors.toList());
+    }
+
+    public static class JavaKey {
+        public Long id;
+        public String name;
+        public String org;
+
+        public JavaKey() {
+        }
+
+        public JavaKey(final Long id, final String name, final String org) {
+            this.id = id;
+            this.name = name;
+            this.org = org;
+        }
+    }
+}
---
+++ hazelcast/src/main/java/com/hazelcast/partition/strategy/AttributePartitioningStrategy.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.partition.strategy;
+
+import com.hazelcast.internal.serialization.impl.GenericRecordQueryReader;
+import com.hazelcast.internal.serialization.impl.InternalGenericRecord;
+import com.hazelcast.jet.impl.util.ReflectionUtils;
+import com.hazelcast.partition.PartitioningStrategy;
+
+import java.io.IOException;
+
+public class AttributePartitioningStrategy implements PartitioningStrategy {
+
+    private String[] attributes;
+
+    public AttributePartitioningStrategy(String... attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Object getPartitionKey(final Object key) {
+        final Object[] values = new Object[attributes.length];
+        if (key instanceof InternalGenericRecord) {
+            GenericRecordQueryReader reader = new GenericRecordQueryReader((InternalGenericRecord) key);
+            for (int i = 0; i < attributes.length; i++) {
+                final String attribute = attributes[i];
+                try {
+                    final Object value = reader.read(attribute);
+                    if (value == null) {
+                        return null;
+                    }
+                    values[i] = value;
+                } catch (IOException exception) {
+                    return null;
+                }
+            }
+        } else {
+            for (int i = 0; i < attributes.length; i++) {
+                final String attribute = attributes[i];
+                final Object value = ReflectionUtils.getFieldValue(attribute, key);
+                if (value == null) {
+                    return null;
+                }
+                values[i] = value;
+            }
+        }
+
+        return values;
+    }
+
+    public String[] getPartitioningAttributes() {
+        return attributes;
+    }
+}
