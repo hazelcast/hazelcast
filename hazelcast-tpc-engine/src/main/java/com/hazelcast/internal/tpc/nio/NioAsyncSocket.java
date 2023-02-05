hazelcast/hazelcast

commit 80ebcccf45def5c0c58fcf2faa564a2222c00759

[ALTO] Redesign of AsyncSocket.activate (#23577)
The idea behind AsyncSocket.activate was that you could create an
AsyncSocket on one reactor and then activate it on another. This is
useful on the serverside so you can run the accepted socket on a
different reactor than the reactor of the AsyncServerSocket that
accepted the socket.

This led to a bit of a weird design because you need to active with a
reactor you want to attach to and made it impossible to set fields of
the AsyncSocket on creation because you don't know the reactor at
construction time.

This PR solves that by always making the AsyncSocket on the appropriate
Reactor. And instead of listening to created AsyncSocket on the
AsyncServerSocket, you listen to AcceptRequest and then you create the
Socket on the appropriate reactor.

So we keep the flexibility and most fields of the AsyncSocket can now be
final.

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/nio/NioAsyncSocket.java
@@ -17,7 +17,6 @@
 package com.hazelcast.internal.tpc.nio;
 
 import com.hazelcast.internal.tpc.AsyncSocket;
-import com.hazelcast.internal.tpc.Reactor;
 import com.hazelcast.internal.tpc.iobuffer.IOBuffer;
 import jdk.net.ExtendedSocketOptions;
 import org.jctools.queues.MpmcArrayQueue;
@@ -29,7 +28,6 @@ import java.net.SocketAddress;
 import java.net.SocketOption;
 import java.nio.ByteBuffer;
 import java.nio.channels.CancelledKeyException;
-import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -41,7 +39,6 @@ import java.util.concurrent.atomic.AtomicReference;
 import static com.hazelcast.internal.tpc.util.BufferUtil.compactOrClear;
 import static com.hazelcast.internal.tpc.util.CloseUtil.closeQuietly;
 import static com.hazelcast.internal.tpc.util.ExceptionUtil.sneakyThrow;
-import static com.hazelcast.internal.tpc.util.Preconditions.checkInstanceOf;
 import static com.hazelcast.internal.tpc.util.Preconditions.checkNotNull;
 import static com.hazelcast.internal.tpc.util.Preconditions.checkPositive;
 import static com.hazelcast.internal.tpc.util.ReflectionUtil.findStaticFieldValue;
@@ -72,17 +69,6 @@ public final class NioAsyncSocket extends AsyncSocket {
     private static final AtomicBoolean TCP_KEEPIDLE_PRINTED = new AtomicBoolean();
     private static final AtomicBoolean TCP_KEEPINTERVAL_PRINTED = new AtomicBoolean();
 
-    /**
-     * Opens a stream based IPv4 NioAsyncSocket.
-     * <p/>
-     * To prevent coupling, it is better to use the {@link Reactor#openTcpAsyncSocket()}.
-     *
-     * @return the opened NioAsyncSocket.
-     */
-    public static NioAsyncSocket openTcpSocket() {
-        return new NioAsyncSocket();
-    }
-
     private int unflushedBufsCapacity = DEFAULT_UNFLUSHED_BUFS_CAPACITY;
 
     // concurrent
@@ -93,10 +79,11 @@ public final class NioAsyncSocket extends AsyncSocket {
 
     // immutable state
     private final SocketChannel socketChannel;
-    private NioReactor reactor;
-    private Thread eventloopThread;
-    private SelectionKey key;
-    private Selector selector;
+    private final NioReactor reactor;
+    private final Thread eventloopThread;
+    private final SelectionKey key;
+    private final Selector selector;
+
 
     // ======================================================
     // reading side of the socket.
@@ -109,24 +96,34 @@ public final class NioAsyncSocket extends AsyncSocket {
     private final IOVector ioVector = new IOVector();
     private boolean regularSchedule = true;
     private boolean writeThrough;
+    private boolean started;
+    private boolean connect;
 
-    private NioAsyncSocket() {
+    public NioAsyncSocket(NioReactor reactor) {
         try {
+            this.reactor = reactor;
+            this.eventloopThread = reactor.eventloopThread();
+            this.selector = reactor.selector;
             this.socketChannel = SocketChannel.open();
             this.socketChannel.configureBlocking(false);
             this.clientSide = true;
+            this.key = socketChannel.register(selector, 0, handler);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
 
-    NioAsyncSocket(SocketChannel socketChannel) {
+    public NioAsyncSocket(NioReactor reactor, NioAcceptRequest openRequest) {
         try {
-            this.socketChannel = socketChannel;
+            this.reactor = reactor;
+            this.eventloopThread = reactor.eventloopThread();
+            this.selector = reactor.selector;
+            this.socketChannel = openRequest.getSocketChannel();
             this.socketChannel.configureBlocking(false);
             this.localAddress = socketChannel.getLocalAddress();
             this.remoteAddress = socketChannel.getRemoteAddress();
             this.clientSide = false;
+            this.key = socketChannel.register(selector, 0, handler);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
@@ -393,17 +390,14 @@ public final class NioAsyncSocket extends AsyncSocket {
         return (key.interestOps() & OP_READ) != 0;
     }
 
-    @Override
-    public void activate(Reactor r) {
-        NioReactor reactor = checkInstanceOf(NioReactor.class, r, "reactor");
-
+    public void start() {
         if (Thread.currentThread() == reactor.eventloopThread()) {
-            activate0(reactor);
+            start0();
         } else {
             CompletableFuture future = new CompletableFuture();
             reactor.execute(() -> {
                 try {
-                    activate0(reactor);
+                    start0();
                     future.complete(null);
                 } catch (Throwable e) {
                     future.completeExceptionally(e);
@@ -414,24 +408,21 @@ public final class NioAsyncSocket extends AsyncSocket {
         }
     }
 
-    private void activate0(NioReactor reactor) {
-        if (this.reactor != null) {
-            throw new IllegalStateException(this + " already has been activated");
+    private void start0() {
+        if (started) {
+            throw new IllegalStateException(this + " is already started");
         }
+        started = true;
 
         if (readHandler == null) {
             throw new IllegalStateException("Can't activate " + this + ": readhandler isn't set");
         }
 
-        this.reactor = reactor;
-        this.selector = reactor.selector;
-        this.eventloopThread = reactor.eventloopThread();
         this.unflushedBufs = new MpmcArrayQueue<>(unflushedBufsCapacity);
         this.receiveBuffer = ByteBuffer.allocateDirect(getReceiveBufferSize());
-        try {
-            key = socketChannel.register(selector, clientSide ? 0 : OP_READ, handler);
-        } catch (ClosedChannelException e) {
-            throw new UncheckedIOException(e);
+        if (clientSide) {
+            // on the client side we immediately start reading.
+            key.interestOps(key.interestOps() | OP_READ);
         }
     }
 
@@ -459,9 +450,10 @@ public final class NioAsyncSocket extends AsyncSocket {
 
     private void connect0(SocketAddress address, CompletableFuture<Void> future) {
         try {
-            if (this.connectFuture != null) {
+            if (connect) {
                 throw new IllegalStateException(this + " is already trying to connect");
             }
+            connect = true;
 
             connectFuture = future;
             key.interestOps(key.interestOps() | OP_CONNECT);
@@ -485,8 +477,8 @@ public final class NioAsyncSocket extends AsyncSocket {
                 reactor.offer(handler);
             } else {
                 key.interestOps(key.interestOps() | OP_WRITE);
-
-                //todo: is this needed? The whole purpose of using the OP_WRITE is to wakeup the selector.
+                // we need to call the select wakeup because the interest set will only take
+                // effect after a select operation.
                 reactor.wakeup();
             }
         }
@@ -553,18 +545,8 @@ public final class NioAsyncSocket extends AsyncSocket {
 
     @Override
     protected void close0() throws IOException {
-        // First we need to obtain the key, because as soon as the
-        // socketChannel is closed, the key is deregistered.
-        // Can be called on any thread, so we need to make use of the
-        // synchronization of the socketChannel to obtain the key
-        SelectionKey key = socketChannel.keyFor(selector);
-
         closeQuietly(socketChannel);
-
-        if (key != null) {
-            key.cancel();
-        }
-
+        key.cancel();
         super.close0();
     }
 

hazelcast-tpc-engine/src/test/java/com/hazelcast/internal/tpc/AsyncSocketTest.java
@@ -54,7 +54,7 @@ public abstract class AsyncSocketTest {
         Reactor reactor = newReactor();
         AsyncSocket socket = reactor.openTcpAsyncSocket();
         socket.setReadHandler(mock(ReadHandler.class));
-        socket.activate(reactor);
+        socket.start();
 
         SocketAddress remoteAddress = socket.getRemoteAddress();
         assertNull(remoteAddress);
@@ -65,7 +65,7 @@ public abstract class AsyncSocketTest {
         Reactor reactor = newReactor();
         AsyncSocket socket = reactor.openTcpAsyncSocket();
         socket.setReadHandler(mock(ReadHandler.class));
-        socket.activate(reactor);
+        socket.start();
 
         SocketAddress localAddress = socket.getLocalAddress();
         System.out.println(localAddress);
@@ -151,12 +151,12 @@ public abstract class AsyncSocketTest {
 
         SocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5000);
         serverSocket.bind(serverAddress);
-        serverSocket.accept(asyncSocket -> {
+        serverSocket.accept(acceptRequest -> {
         });
 
         AsyncSocket clientSocket = reactor.openTcpAsyncSocket();
         clientSocket.setReadHandler(mock(ReadHandler.class));
-        clientSocket.activate(reactor);
+        clientSocket.start();
 
         CompletableFuture<Void> connect = clientSocket.connect(serverAddress);
 
@@ -181,7 +181,7 @@ public abstract class AsyncSocketTest {
         Reactor reactor = newReactor();
         AsyncSocket socket = reactor.openTcpAsyncSocket();
         socket.setReadHandler(mock(ReadHandler.class));
-        socket.activate(reactor);
+        socket.start();
 
         CompletableFuture<Void> future = socket.connect(new InetSocketAddress(50000));
 
@@ -209,25 +209,16 @@ public abstract class AsyncSocketTest {
         assertTrue(socket.isClosed());
     }
 
+
     @Test
-    public void test_activate_whenNull() {
+    public void test_start_whenAlreadyStarted() {
         Reactor reactor = newReactor();
+
         AsyncSocket socket = reactor.openTcpAsyncSocket();
         socket.setReadHandler(mock(ReadHandler.class));
 
-        assertThrows(NullPointerException.class, () -> socket.activate(null));
-    }
-
-    @Test
-    public void test_activate_whenAlreadyActivated() {
-        Reactor reactor1 = newReactor();
-        Reactor reactor2 = newReactor();
-
-        AsyncSocket socket = reactor1.openTcpAsyncSocket();
-        socket.setReadHandler(mock(ReadHandler.class));
-
-        socket.activate(reactor1);
-        assertThrows(CompletionException.class, () -> socket.activate(reactor2));
+        socket.start();
+        assertThrows(CompletionException.class, () -> socket.start());
     }
 
     @Test
@@ -235,7 +226,7 @@ public abstract class AsyncSocketTest {
         Reactor reactor = newReactor();
 
         AsyncSocket socket = reactor.openTcpAsyncSocket();
-        assertThrows(CompletionException.class, () -> socket.activate(reactor));
+        assertThrows(CompletionException.class, () -> socket.start());
     }
 
     @Test
@@ -247,7 +238,7 @@ public abstract class AsyncSocketTest {
 
         AsyncSocket socket = reactor.openTcpAsyncSocket();
         socket.setReadHandler(mock(ReadHandler.class));
-        socket.activate(reactor);
+        socket.start();
         socket.connect(serverAddress).join();
 
         assertTrue(socket.isReadable());

hazelcast-tpc-engine/src/test/java/com/hazelcast/internal/tpc/AsyncSocket_RpcTest.java
@@ -318,7 +318,7 @@ public abstract class AsyncSocket_RpcTest {
                 }
             }
         });
-        clientSocket.activate(clientReactor);
+        clientSocket.start();
         clientSocket.connect(serverAddress).join();
         return clientSocket;
     }
@@ -328,7 +328,8 @@ public abstract class AsyncSocket_RpcTest {
         serverSocket.setReceiveBufferSize(SOCKET_BUFFER_SIZE);
         serverSocket.bind(serverAddress);
 
-        serverSocket.accept(socket -> {
+        serverSocket.accept(acceptRequest -> {
+            AsyncSocket socket = serverReactor.openAsyncSocket(acceptRequest);
             socket.setTcpNoDelay(true);
             socket.setSendBufferSize(SOCKET_BUFFER_SIZE);
             socket.setReceiveBufferSize(serverSocket.getReceiveBufferSize());
@@ -370,7 +371,7 @@ public abstract class AsyncSocket_RpcTest {
                     }
                 }
             });
-            socket.activate(serverReactor);
+            socket.start();
         });
 
         return serverSocket;

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/AcceptRequest.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.internal.tpc;
+
+import java.util.function.Consumer;
+
+/**
+ * Contains an accept request when a socket connects to the {@link AsyncServerSocket}.
+ * For more information see {@link AsyncServerSocket#accept(Consumer)}.
+ * <p/>
+ * Currently it is just a dumb placeholder so that we can pass the appropriate resource
+ * (e.g. the accepted SocketChannel) to the constructor of the AsyncSocket.
+ */
+public interface AcceptRequest {
+}

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/AsyncServerSocket.java
@@ -184,11 +184,10 @@ public abstract class AsyncServerSocket extends Socket {
      * @param consumer a function that is called when a socket has connected.
      * @throws NullPointerException if consumer is null.
      */
-    public abstract void accept(Consumer<AsyncSocket> consumer);
+    public abstract void accept(Consumer<AcceptRequest> consumer);
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "[" + getLocalAddress() + "]";
     }
-
 }

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/AsyncSocket.java
@@ -31,7 +31,7 @@ import static com.hazelcast.internal.tpc.util.Preconditions.checkNotNull;
  * A 'client' Socket that is asynchronous. So reads and writes do not block,
  * but are executed on an {@link Reactor}.
  * <p/>
- * The socket should be configured and then activated before it is shared
+ * The socket should be configured and then started before it is shared
  * with other threads.
  */
 @SuppressWarnings({"checkstyle:MethodCount", "checkstyle:VisibilityModifier"})
@@ -107,8 +107,7 @@ public abstract class AsyncSocket extends Socket {
     /**
      * Gets the {@link Reactor} this {@link AsyncSocket} belongs to.
      *
-     * @return the {@link Reactor} this AsyncSocket belongs to or null if
-     * the AsyncSocket has not been activated yet.
+     * @return the {@link Reactor} this AsyncSocket belongs.
      */
     public abstract Reactor reactor();
 
@@ -277,7 +276,7 @@ public abstract class AsyncSocket extends Socket {
     public abstract int getSendBufferSize();
 
     /**
-     * Sets the read handler. Should be called before this AsyncSocket is activated.
+     * Sets the read handler. Should be called before this AsyncSocket is started.
      *
      * @param readHandler the ReadHandler
      * @throws NullPointerException if readHandler is null.
@@ -303,8 +302,6 @@ public abstract class AsyncSocket extends Socket {
      * This call can safely be made from any thread, but typically you want to call it from the
      * eventloop-thread. This call is blocking; this isn't an issue for the eventloop thread
      * because it is an instantaneous call. For any other thread this call is not cheap.
-     * <p/>
-     * This call should only be made after the socket is activated.
      *
      * @param readable the new readable status.
      * @throws RuntimeException if the readable status could not be set.
@@ -324,20 +321,14 @@ public abstract class AsyncSocket extends Socket {
     public abstract boolean isReadable();
 
     /**
-     * Activates an AsyncSocket by hooking it up to a Reactor.
-     * <p>
-     * This method should only be called once.
-     * <p>
-     * This method is not thread-safe.
-     * <p>
+     * Start the AsyncSocket. The Socket should be started only once.
+     * <p/>
      * Typically you do not want to share this AsyncSocket with other threads till this
      * method is called.
      *
-     * @param reactor the Reactor this AsyncSocket belongs to.
-     * @throws NullPointerException  if reactor is null.
-     * @throws IllegalStateException if this AsyncSocket is already activated.
+     * @throws RuntimeException if the Socket could not be started.
      */
-    public abstract void activate(Reactor reactor);
+    public abstract void start();
 
     /**
      * Ensures that any scheduled IOBuffers are flushed to the socket.

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/Reactor.java
@@ -162,21 +162,25 @@ public abstract class Reactor implements Executor {
     public abstract AsyncServerSocket openTcpAsyncServerSocket();
 
     /**
-     * Opens TCP/IP (stream) based async socket. The returned socket assumes IPv4. When support for
-     * IPv6 is added, a boolean 'ipv4' flag needs to be added.
-     * <p/>
-     * The opened AsyncSocket isn't tied to this Reactor. After it is opened, it needs to be assigned
-     * to a particular reactor by calling {@link AsyncSocket#activate(Reactor)}. The reason why
-     * this isn't done in 1 go, is that it could be that when the AsyncServerSocket accepts an
-     * AsyncSocket, we want to assign that AsyncSocket to a different Reactor. Otherwise if there
-     * would be 1 AsyncServerSocket, connected AsyncSockets can only run on top of the reactor of
-     * the AsyncServerSocket instead of being distributed over multiple reactors.
+     * Opens client-side TCP/IP (stream) based async socket. The returned socket assumes IPv4.
+     * When support for IPv6 is added, a boolean 'ipv4' flag needs to be added.
      * <p>
      * This method is thread-safe.
      *
      * @return the opened AsyncSocket.
      */
     public abstract AsyncSocket openTcpAsyncSocket();
+
+    /**
+     * Opens a server-side async socket. The AcceptRequest contains all the information to
+     * determine the type of socket.
+     * <p>
+     * This method is thread-safe.
+     *
+     * @return the opened AsyncSocket.
+     */
+
+    public abstract AsyncSocket openAsyncSocket(AcceptRequest request);
 
     /**
      * Creates the Eventloop run by this Reactor.

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/nio/NioAcceptRequest.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.internal.tpc.nio;
+
+import com.hazelcast.internal.tpc.AcceptRequest;
+
+import java.nio.channels.SocketChannel;
+
+public class NioAcceptRequest implements AcceptRequest {
+
+    private final SocketChannel socketChannel;
+
+    public NioAcceptRequest(SocketChannel socketChannel) {
+        this.socketChannel = socketChannel;
+    }
+
+    public SocketChannel getSocketChannel() {
+        return socketChannel;
+    }
+}

commit d3dabd00ef61d0aa5e0e3b4da685923a7f55fe3a

[ALTO] Fixed tests (#23587)
Fix some problems introduced by
https://github.com/hazelcast/hazelcast/pull/23577. Certain tests became
extremely slow.

hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/nio/NioAsyncSocket.java
@@ -420,8 +420,8 @@ public final class NioAsyncSocket extends AsyncSocket {
 
         this.unflushedBufs = new MpmcArrayQueue<>(unflushedBufsCapacity);
         this.receiveBuffer = ByteBuffer.allocateDirect(getReceiveBufferSize());
-        if (clientSide) {
-            // on the client side we immediately start reading.
+        if (!clientSide) {
+            // on the server side we immediately start reading.
             key.interestOps(key.interestOps() | OP_READ);
         }
     }
@@ -450,6 +450,10 @@ public final class NioAsyncSocket extends AsyncSocket {
 
     private void connect0(SocketAddress address, CompletableFuture<Void> future) {
         try {
+            if (!started) {
+                throw new IllegalStateException(this + " can't connect when socket not yet started");
+            }
+
             if (connect) {
                 throw new IllegalStateException(this + " is already trying to connect");
             }
@@ -459,7 +463,7 @@ public final class NioAsyncSocket extends AsyncSocket {
             key.interestOps(key.interestOps() | OP_CONNECT);
             socketChannel.connect(address);
         } catch (Throwable e) {
-            connectFuture.completeExceptionally(e);
+            future.completeExceptionally(e);
             throw sneakyThrow(e);
         }
     }

hazelcast-tpc-engine/src/test/java/com/hazelcast/internal/tpc/AsyncSocketTest.java
@@ -173,7 +173,7 @@ public abstract class AsyncSocketTest {
 
         SocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5000);
 
-        assertThrows(IllegalStateException.class, () -> socket.connect(serverAddress));
+        assertThrows(RuntimeException.class, () -> socket.connect(serverAddress).join());
     }
 
     @Test

hazelcast-tpc-engine/src/test/java/com/hazelcast/internal/tpc/AsyncSocket_RpcTest.java
@@ -318,8 +318,8 @@ public abstract class AsyncSocket_RpcTest {
                 }
             }
         });
-        clientSocket.connect(serverAddress).join();
         clientSocket.start();
+        clientSocket.connect(serverAddress).join();
         return clientSocket;
     }
 