921010b4a3ecbdd7b239334df204e0d59d5a9ae6
Krzysztof Jamróz <krzysztof.jamroz@hazelcast.com>
2023-02-13 18:14:31 +0100

    Remove unused import

--- hazelcast/src/test/java/com/hazelcast/jet/core/IndeterminateSnapshotTest.java
+++ hazelcast/src/test/java/com/hazelcast/jet/core/IndeterminateSnapshotTest.java
@@ -73,7 +73,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
