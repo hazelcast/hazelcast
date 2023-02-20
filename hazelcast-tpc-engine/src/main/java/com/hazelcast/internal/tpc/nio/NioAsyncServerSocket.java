15c114b9489d1fd0490a0af3e4ad7003f839ca92
Ramiz Dündar <ramizdundar@gmail.com>
2023-02-20 11:15:12 +0300

    Revert "Register server socket channel in eventloop"
    This reverts commit b5ef3cbbd2393e49c107dc7406ac75d8bc23e763.

--- hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/nio/NioAsyncServerSocket.java
+++ hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpc/nio/NioAsyncServerSocket.java
@@ -59,27 +59,9 @@ public final class NioAsyncServerSocket extends AsyncServerSocket {
             this.options = builder.options;
             this.eventloopThread = reactor.eventloopThread();
             this.serverSocketChannel = builder.serverSocketChannel;
-            this.key = register();
+            this.key = serverSocketChannel.register(reactor.selector, 0, new Handler());
         } catch (IOException e) {
             throw new UncheckedIOException(e);
-        }
-    }
-
-    private SelectionKey register() throws IOException {
-        if (Thread.currentThread() == eventloopThread) {
-            return serverSocketChannel.register(reactor.selector, 0, new Handler());
-        } else {
-            CompletableFuture<SelectionKey> future = new CompletableFuture<>();
-            reactor.execute(() -> {
-                try {
-                    future.complete(serverSocketChannel.register(reactor.selector, 0, new Handler()));
-                } catch (Throwable t) {
-                    future.completeExceptionally(t);
-                    throw sneakyThrow(t);
-                }
-            });
-
-            return future.join();
         }
     }
 
