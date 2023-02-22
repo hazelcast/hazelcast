3fb1ab20f0dfc66ecfbd2bcfb68a2e7814e055aa
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-02-22 16:50:00 +0200

    Checkstyle

--- hazelcast/src/main/java/com/hazelcast/sql/impl/expression/MockExpressionEvalContext.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/expression/MockExpressionEvalContext.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.sql.impl.expression;
 
 import com.hazelcast.internal.serialization.InternalSerializationService;
@@ -15,12 +31,12 @@ public class MockExpressionEvalContext implements ExpressionEvalContext {
 
     @Override
     public Object getArgument(int index) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("getArgument operation is not supported for Mock EEC");
     }
 
     @Override
     public List<Object> getArguments() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("getArgument operation is not supported for Mock EEC");
     }
 
     @Override
@@ -30,6 +46,6 @@ public class MockExpressionEvalContext implements ExpressionEvalContext {
 
     @Override
     public NodeEngine getNodeEngine() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("getNodeEngine operation is not supported for Mock EEC");
     }
 }
