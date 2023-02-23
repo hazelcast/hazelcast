ae2c4af33710546459f4f9a17ec37111b41ea11c
Vassilis Bekiaris <vbekiaris@gmail.com>
2023-02-23 10:28:42 +0200

    Update condition for running clear expired task [5.0.z] (#23653)
    - only reset singleRunPermit to false if previous CAS succeeded in
    setting it to true. Otherwise, the finally block resets it to false
     while another run is in progress.
    - do not start clear expired records task unless node is started.
    Currently not checking if node is started may result in a lot
    of unnecessary logging about
    operation failing due to failing checkNodeState checks in
    OperationRunnerImpl (in particular when recovering from persistence).
    
    (cherry picked from commit 2d1a336fdefe3dd2cc33e8cbfd7ced24640ceec3)
    Backport of #23110 to 5.0.z

--- hazelcast/src/main/java/com/hazelcast/spi/impl/NodeEngineImpl.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/NodeEngineImpl.java
@@ -397,6 +397,11 @@ public class NodeEngineImpl implements NodeEngine {
     }
 
     @Override
+    public boolean isStartCompleted() {
+        return node.getNodeExtension().isStartCompleted();
+    }
+
+    @Override
     public HazelcastInstance getHazelcastInstance() {
         return node.hazelcastInstance;
     }
