73862e2cff2495d598e58162bb6a1406d07462da
Orçun Çolak <90305879+orcunc@users.noreply.github.com>
2023-02-28 12:56:44 +0300

    Add HazelcastDataLink and support it in remoteMapJournal [HZ-1433]
    When map journal of a remote cluster is used within a Jet job, each job
    instance on a member creates a new Hazelcast client instance.
    
    This PR adds
    - DataLink for remote Hazelcast client
    - support for the DataLink in remoteMapJournal, allowing re-use of the
    Hazelcast client instance

---
+++ hazelcast/src/test/java/com/hazelcast/datalink/HazelcastDataLinkTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.datalink;
+
+import com.hazelcast.config.DataLinkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastException;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Collection;
+
+import static java.nio.file.Files.readAllBytes;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@RunWith(HazelcastSerialClassRunner.class)
+@Category({QuickTest.class})
+public class HazelcastDataLinkTest extends HazelcastTestSupport {
+
+    private static HazelcastInstance instance;
+    private HazelcastDataLink hazelcastDataLink;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        instance = Hazelcast.newHazelcastInstance();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        if (instance != null) {
+            instance.shutdown();
+            instance = null;
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (hazelcastDataLink != null) {
+            hazelcastDataLink.release();
+        }
+    }
+
+    @Test
+    public void list_resources_should_return_map() throws Exception {
+        IMap<Integer, String> map = instance.getMap("my_map");
+        map.put(42, "42");
+
+        DataLinkConfig dataLinkConfig = sharedDataLinkConfig();
+
+        hazelcastDataLink = new HazelcastDataLink(dataLinkConfig);
+        Collection<DataLinkResource> resources = hazelcastDataLink.listResources();
+
+        assertThat(resources).contains(new DataLinkResource("IMap", "my_map"));
+    }
+
+    @Test
+    public void list_resources_should_not_return_system_maps() throws Exception {
+        DataLinkConfig dataLinkConfig = sharedDataLinkConfig();
+
+        hazelcastDataLink = new HazelcastDataLink(dataLinkConfig);
+        Collection<DataLinkResource> resources = hazelcastDataLink.listResources();
+
+        assertThat(resources).isEmpty();
+    }
+
+    @Test
+    public void should_throw_with_missing_config() {
+        DataLinkConfig dataLinkConfig = new DataLinkConfig("data-link-name")
+                .setClassName(HazelcastDataLink.class.getName())
+                .setShared(true);
+
+        assertThatThrownBy(() -> hazelcastDataLink = new HazelcastDataLink(dataLinkConfig))
+                .isInstanceOf(HazelcastException.class)
+                .hasMessage("HazelcastDataLink with name 'data-link-name' "
+                        + "could not be created, provide either client_xml or client_yml property "
+                        + "with the client configuration.");
+    }
+
+    @Test
+    public void shared_client_should_return_same_instance() {
+        DataLinkConfig dataLinkConfig = sharedDataLinkConfig();
+        hazelcastDataLink = new HazelcastDataLink(dataLinkConfig);
+        HazelcastInstance c1 = hazelcastDataLink.getClient();
+        HazelcastInstance c2 = hazelcastDataLink.getClient();
+
+        try {
+            assertThat(c1).isSameAs(c2);
+        } finally {
+            c1.shutdown();
+            c2.shutdown();
+        }
+    }
+
+    @Test
+    public void non_shared_client_should_return_new_client_instance() {
+        DataLinkConfig dataLinkConfig = nonSharedDataLinkConfig();
+        hazelcastDataLink = new HazelcastDataLink(dataLinkConfig);
+        HazelcastInstance c1 = hazelcastDataLink.getClient();
+        HazelcastInstance c2 = hazelcastDataLink.getClient();
+
+        try {
+            assertThat(c1).isNotSameAs(c2);
+        } finally {
+            c1.shutdown();
+            c2.shutdown();
+        }
+    }
+
+    private static DataLinkConfig nonSharedDataLinkConfig() {
+        return sharedDataLinkConfig()
+                .setShared(false);
+    }
+    @Nonnull
+    private static DataLinkConfig sharedDataLinkConfig() {
+        DataLinkConfig dataLinkConfig = new DataLinkConfig("data-link-name")
+                .setClassName(HazelcastDataLink.class.getName())
+                .setShared(true);
+        try {
+            byte[] bytes = readAllBytes(Paths.get("src", "test", "resources", "hazelcast-client-test-external.xml"));
+            String xmlString = new String(bytes, StandardCharsets.UTF_8);
+            dataLinkConfig.setProperty(HazelcastDataLink.CLIENT_XML, xmlString);
+            return dataLinkConfig;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
