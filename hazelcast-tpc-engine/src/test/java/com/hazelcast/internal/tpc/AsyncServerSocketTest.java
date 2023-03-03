9a4b855af9790229d093f0cc7424ac37d85a24ea
Peter Veentjer <alarmnummer@gmail.com>
2023-03-03 11:21:01 +0200

    WIP

--- hazelcast-tpc-engine/src/test/java/com/hazelcast/internal/tpc/AsyncServerSocketTest.java
+++ hazelcast-tpc-engine/src/test/java/com/hazelcast/internal/tpc/AsyncServerSocketTest.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import static com.hazelcast.internal.tpc.TpcTestSupport.assertCompletesEventually;
 import static com.hazelcast.internal.tpc.TpcTestSupport.terminate;
 import static com.hazelcast.internal.tpc.TpcTestSupport.terminateAll;
 import static org.junit.Assert.assertEquals;
@@ -151,6 +152,36 @@ public abstract class AsyncServerSocketTest {
         assertThrows(UncheckedIOException.class, () -> socket.bind(local));
 
         socket.close();
+    }
+
+    @Test
+    public void test_connect() {
+        Reactor reactor = newReactor();
+        AsyncServerSocket serverSocket = reactor.newAsyncServerSocketBuilder()
+                .setAcceptConsumer(acceptRequest -> {
+                    AsyncSocket socket = reactor.newAsyncSocketBuilder(acceptRequest)
+                            .setReadHandler(new DevNullReadHandler())
+                            .build();
+                    socket.start();
+                })
+                .build();
+
+        SocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5000);
+        serverSocket.bind(serverAddress);
+        serverSocket.start();
+
+        int clients = 5;
+        for (int k = 0; k < clients; k++) {
+            AsyncSocket clientSocket = reactor.newAsyncSocketBuilder()
+                    .setReadHandler(new DevNullReadHandler())
+                    .build();
+            clientSocket.start();
+
+            CompletableFuture<Void> connect = clientSocket.connect(serverAddress);
+            assertCompletesEventually(connect);
+        }
+
+        assertEquals(clients, serverSocket.getAccepted());
     }
 
     @Test
