242e0a7e9132fdd46b75befb42ecf0e984844265
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2023-03-07 13:26:45 +0100

    Add integration test for the snapshotting

--- extensions/kafka-connect/src/test/java/com/hazelcast/jet/kafka/connect/KafkaConnectDatagenIntegrationTest.java
+++ extensions/kafka-connect/src/test/java/com/hazelcast/jet/kafka/connect/KafkaConnectDatagenIntegrationTest.java
@@ -17,9 +17,13 @@
 package com.hazelcast.jet.kafka.connect;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.EventJournalConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.jet.Job;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.core.JetTestSupport;
+import com.hazelcast.jet.impl.JobRepository;
 import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.StreamStage;
@@ -37,6 +41,8 @@ import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.CompletionException;
 
+import static com.hazelcast.jet.config.ProcessingGuarantee.AT_LEAST_ONCE;
+import static com.hazelcast.jet.core.JobStatus.RUNNING;
 import static com.hazelcast.test.OverridePropertyRule.set;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -126,5 +132,59 @@ public class KafkaConnectDatagenIntegrationTest extends JetTestSupport {
         }
     }
 
+
+    @Test
+    public void testSnapshotting() throws Exception {
+        int localParallelism = 3;
+        Properties randomProperties = new Properties();
+        randomProperties.setProperty("name", "datagen-connector");
+        randomProperties.setProperty("connector.class", "io.confluent.kafka.connect.datagen.DatagenConnector");
+        randomProperties.setProperty("tasks.max", Integer.toString(localParallelism));
+        randomProperties.setProperty("max.interval", "1");
+        randomProperties.setProperty("kafka.topic", "users");
+        randomProperties.setProperty("quickstart", "users");
+
+        Pipeline pipeline = Pipeline.create();
+        StreamStage<String> streamStage = pipeline.readFrom(KafkaConnectSources.connect(randomProperties))
+                .withoutTimestamps()
+                .setLocalParallelism(localParallelism)
+                .map(record -> Values.convertToString(record.valueSchema(), record.value()) + ", task.id: "
+                        + record.headers().lastWithName("task.id").value());
+        streamStage.writeTo(Sinks.logger());
+        streamStage
+                .writeTo(AssertionSinks.assertCollectedEventually(60,
+                        list -> assertEquals(localParallelism * ITEM_COUNT, list.size())));
+
+        JobConfig jobConfig = new JobConfig();
+        jobConfig.addJarsInZip(new URL(CONNECTOR_URL));
+        jobConfig.setProcessingGuarantee(AT_LEAST_ONCE);
+        jobConfig.setSnapshotIntervalMillis(10);
+
+        Config config = smallInstanceConfig();
+        config.addMapConfig(new MapConfig("*")
+                .setEventJournalConfig(new EventJournalConfig().setEnabled(true))
+                .setBackupCount(3)
+        );
+        config.getJetConfig().setResourceUploadEnabled(true);
+        HazelcastInstance[] hazelcastInstances = createHazelcastInstances(config, 3);
+        JobRepository jobRepository = new JobRepository(hazelcastInstances[0]);
+        Job job = hazelcastInstances[0].getJet().newJob(pipeline, jobConfig);
+        assertJobStatusEventually(job, RUNNING);
+
+        waitForFirstSnapshot(jobRepository, job.getId(), 30, false);
+
+        hazelcastInstances[1].shutdown();
+
+        assertTrueEventually(() -> assertEquals(2, hazelcastInstances[0].getCluster().getMembers().size()));
+
+        try {
+            job.join();
+            fail("Job should have completed with an AssertionCompletedException, but completed normally");
+        } catch (CompletionException e) {
+            String errorMsg = e.getCause().getMessage();
+            assertTrue("Job was expected to complete with AssertionCompletedException, but completed with: "
+                    + e.getCause(), errorMsg.contains(AssertionCompletedException.class.getName()));
+        }
+    }
 
 }
