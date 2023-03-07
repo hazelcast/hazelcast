87c678c777333cdf1fd89936e18a63d708b75e2d
ivanthescientist <ivan.yaschishin@hazelcast.com>
2023-03-07 15:57:10 +0200

    reverted executor test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/PlanExecutorTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/PlanExecutorTest.java
@@ -17,8 +17,6 @@
 package com.hazelcast.jet.sql.impl;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.internal.metrics.MetricDescriptor;
-import com.hazelcast.internal.metrics.MetricsRegistry;
 import com.hazelcast.internal.util.UuidUtil;
 import com.hazelcast.jet.JetService;
 import com.hazelcast.jet.Job;
@@ -40,6 +38,7 @@ import org.apache.calcite.rel.core.TableModify.Operation;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -51,7 +50,6 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.BDDMockito.given;
@@ -59,6 +57,8 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(JUnitParamsRunner.class)
 public class PlanExecutorTest {
+
+    @InjectMocks
     private PlanExecutor planExecutor;
 
     @Mock
@@ -69,12 +69,6 @@ public class PlanExecutorTest {
 
     @Mock
     private JetService jet;
-
-    @Mock()
-    private MetricsRegistry registry;
-
-    @Mock
-    private MetricDescriptor metricDescriptor;
 
     @Mock
     private Map<String, QueryResultProducer> resultConsumerRegistry;
@@ -89,9 +83,6 @@ public class PlanExecutorTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         given(job.getFuture()).willReturn(new CompletableFuture<>());
-        given(registry.newMetricDescriptor()).willReturn(metricDescriptor).getMock();
-        given(metricDescriptor.withTag(anyString(), anyString())).willReturn(metricDescriptor);
-        planExecutor = new PlanExecutor(catalog, hazelcastInstance, null);
     }
 
     @Test
