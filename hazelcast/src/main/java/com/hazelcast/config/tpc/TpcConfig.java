f58dd25f8f1a239c2ff0b16d4ff2c5d1fdcfc16f
Peter Veentjer <alarmnummer@gmail.com>
2023-03-14 11:47:35 +0200

    Update hazelcast/src/main/java/com/hazelcast/config/tpc/TpcConfig.java
    Co-authored-by: Metin Dumandag <metindumandag@gmail.com>

--- hazelcast/src/main/java/com/hazelcast/config/tpc/TpcConfig.java
+++ hazelcast/src/main/java/com/hazelcast/config/tpc/TpcConfig.java
@@ -45,7 +45,7 @@ public class TpcConfig {
     }
 
     /**
-     * Sets the enabled flag which defines Tpc is enabled or not. Can't
+     * Sets the enabled flag which defines TPC is enabled or not. Can't
      * return null.
      *
      * @param enabled a boolean to enable or disable tpc
