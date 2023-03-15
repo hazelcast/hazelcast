7718a297f585db404542c1bec7f59388e7163fdf
Peter Veentjer <alarmnummer@gmail.com>
2023-03-15 16:45:24 +0200

    WIP

--- hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpcengine/AsyncServerSocketBuilder.java
+++ hazelcast-tpc-engine/src/main/java/com/hazelcast/internal/tpcengine/AsyncServerSocketBuilder.java
@@ -66,7 +66,7 @@ public interface AsyncServerSocketBuilder {
      *
      * @param consumer the consumer
      * @return this
-     * @throws NullPointerException  if consumer is null.
+     * @throws NullPointerException  if consumer is <code>null</code>.
      * @throws IllegalStateException when build already has been called.
      */
     AsyncServerSocketBuilder setAcceptConsumer(Consumer<AcceptRequest> consumer);
