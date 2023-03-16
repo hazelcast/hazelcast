c6f0fe6ca933bc14c72a33a1e6c7e14cadea22a8
Viliam Durina <viliam-durina@users.noreply.github.com>
2023-03-16 07:56:26 +0100

    Fix issues in CREATE DATA LINK implementation (#23941)
    - race when creating/dropping
    - wrong field names in information_schema
    - `DataLink` class name used twice for different things
    - broadcasting the datalink change to all members
    - format datalink options as JSON in information_schema table
    - weird prefix for datalinks in SQL catalog
    - fix formatting broken in the original PR
    
    ---------
    
    Co-authored-by: Krzysztof Jamróz <krzysztof.jamroz@hazelcast.com>

--- hazelcast/src/main/java/com/hazelcast/sql/impl/schema/datalink/DataLinkCatalogEntry.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/schema/datalink/DataLinkCatalogEntry.java
@@ -28,15 +28,15 @@ import java.util.Objects;
 /**
  * SQL schema POJO class.
  */
-public class DataLink implements IdentifiedDataSerializable {
+public class DataLinkCatalogEntry implements IdentifiedDataSerializable {
     private String name;
     private String type;
     private Map<String, String> options;
 
-    public DataLink() {
+    public DataLinkCatalogEntry() {
     }
 
-    public DataLink(String name, String type, Map<String, String> options) {
+    public DataLinkCatalogEntry(String name, String type, Map<String, String> options) {
         this.name = name;
         this.type = type;
         this.options = options;
@@ -87,7 +87,7 @@ public class DataLink implements IdentifiedDataSerializable {
 
     @Override
     public int getClassId() {
-        return SqlDataSerializerHook.DATA_LINK;
+        return SqlDataSerializerHook.DATA_LINK_CATALOG_ENTRY;
     }
 
     @Override
@@ -98,8 +98,8 @@ public class DataLink implements IdentifiedDataSerializable {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        DataLink dataLink = (DataLink) o;
-        return name.equals(dataLink.name) && type.equals(dataLink.type) && options.equals(dataLink.options);
+        DataLinkCatalogEntry e = (DataLinkCatalogEntry) o;
+        return name.equals(e.name) && type.equals(e.type) && options.equals(e.options);
     }
 
     @Override
