4c5220dbaa4fedd29236950fa35e7bad73ec91cd
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-03-21 08:34:44 +0100

    Fix tests on Windows (#24026)
    Fixes #23650

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoSqlTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoSqlTest.java
@@ -25,19 +25,19 @@ import com.mongodb.client.MongoDatabase;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.testcontainers.containers.MongoDBContainer;
 
+import static com.hazelcast.test.DockerTestUtil.assumeDockerEnabled;
+
 @RunWith(HazelcastSerialClassRunner.class)
 @Category({QuickTest.class})
 public abstract class MongoSqlTest extends SqlTestSupport {
     private static final String TEST_MONGO_VERSION = System.getProperty("test.mongo.version", "6.0.3");
 
-    @ClassRule
     public static final MongoDBContainer mongoContainer
             = new MongoDBContainer("mongo:" + TEST_MONGO_VERSION);
 
@@ -52,6 +52,9 @@ public abstract class MongoSqlTest extends SqlTestSupport {
 
     @BeforeClass
     public static void beforeClass() throws InterruptedException {
+        assumeDockerEnabled();
+        mongoContainer.start();
+
         initialize(1, null);
         sqlService = instance().getSql();
         mongoClient = MongoClients.create(mongoContainer.getConnectionString());
@@ -63,6 +66,9 @@ public abstract class MongoSqlTest extends SqlTestSupport {
     public static void close() {
         if (mongoClient != null) {
             mongoClient.close();
+        }
+        if (mongoContainer != null) {
+            mongoContainer.stop();
         }
     }
 
