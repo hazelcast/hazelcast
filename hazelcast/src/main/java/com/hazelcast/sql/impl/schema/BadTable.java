8a14a4b29b3f415ab836178ac66f08f42026e5ae
Krzysztof Jamróz <krzysztof.jamroz@hazelcast.com>
2023-03-22 12:43:20 +0100

    Fix compilation error

--- hazelcast/src/main/java/com/hazelcast/sql/impl/schema/BadTable.java
+++ hazelcast/src/main/java/com/hazelcast/sql/impl/schema/BadTable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +47,7 @@ public final class BadTable extends Table {
     }
 
     private QueryException createException() {
-        return new QueryException(String.format("Mapping '%s' is invalid: %s", getSqlName(), cause),
+        return QueryException.error(String.format("Mapping '%s' is invalid: %s", getSqlName(), cause),
                 cause);
     }
 }
