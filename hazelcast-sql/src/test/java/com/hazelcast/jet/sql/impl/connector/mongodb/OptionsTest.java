4ff40da6f6623bb7fc09e7af45c2e9ca0f0df0e7
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-03-24 15:33:13 +0100

    Checkstyle

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/OptionsTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/OptionsTest.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright 2023 Hazelcast Inc.
+ *
+ * Licensed under the Hazelcast Community License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://hazelcast.com/hazelcast-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hazelcast.jet.sql.impl.connector.mongodb;
 
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.data.TemporalOffset;
 import org.bson.BsonTimestamp;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -11,10 +24,10 @@ import org.junit.runner.RunWith;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
+import static java.time.ZoneOffset.UTC;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 
@@ -26,13 +39,13 @@ public class OptionsTest {
     public void parses_milis_startAt() {
         // given
         long time = System.currentTimeMillis();
-        LocalDateTime timeDate = LocalDateTime.ofEpochSecond(time / 1000, 0, ZoneOffset.UTC);
+        LocalDateTime timeDate = LocalDateTime.ofEpochSecond(time / 1000, 0, UTC);
 
         // when
         BsonTimestamp startAt = Options.startAt(ImmutableMap.of(Options.START_AT_OPTION, String.valueOf(time)));
 
         // then
-        LocalDateTime instant = LocalDateTime.ofEpochSecond(startAt.getTime(), 0, ZoneOffset.UTC);
+        LocalDateTime instant = LocalDateTime.ofEpochSecond(startAt.getTime(), 0, UTC);
 
         assertThat(instant).isEqualToIgnoringNanos(timeDate);
     }
@@ -41,13 +54,13 @@ public class OptionsTest {
     public void parses_now_startAt() {
         // given
         long time = System.currentTimeMillis();
-        LocalDateTime timeDate = LocalDateTime.ofEpochSecond(time / 1000, 0, ZoneOffset.UTC);
+        LocalDateTime timeDate = LocalDateTime.ofEpochSecond(time / 1000, 0, UTC);
 
         // when
         BsonTimestamp startAt = Options.startAt(ImmutableMap.of(Options.START_AT_OPTION, "now"));
 
         // then
-        LocalDateTime instant = LocalDateTime.ofEpochSecond(startAt.getTime(), 0, ZoneOffset.UTC);
+        LocalDateTime instant = LocalDateTime.ofEpochSecond(startAt.getTime(), 0, UTC);
 
         assertThat(instant).isCloseTo(timeDate, within(1, ChronoUnit.SECONDS));
     }
@@ -56,14 +69,14 @@ public class OptionsTest {
     public void parses_dateTimeString_startAt() {
         // given
         long time = System.currentTimeMillis();
-        LocalDateTime timeDate = LocalDateTime.ofEpochSecond(time / 1000, 0, ZoneOffset.UTC);
+        LocalDateTime timeDate = LocalDateTime.ofEpochSecond(time / 1000, 0, UTC);
         String dateAsString = timeDate.format(DateTimeFormatter.ISO_DATE_TIME) + "Z";
 
         // when
         BsonTimestamp startAt = Options.startAt(ImmutableMap.of(Options.START_AT_OPTION, dateAsString));
 
         // then
-        LocalDateTime instant = LocalDateTime.ofEpochSecond(startAt.getTime(), 0, ZoneOffset.UTC);
+        LocalDateTime instant = LocalDateTime.ofEpochSecond(startAt.getTime(), 0, UTC);
         assertThat(instant).isEqualToIgnoringNanos(timeDate);
     }
 
