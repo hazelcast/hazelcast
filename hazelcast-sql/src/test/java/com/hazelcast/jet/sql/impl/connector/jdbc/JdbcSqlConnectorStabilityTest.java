cebff477532e8e52298d63012660175ac140eb72
Orçun Çolak <orcun.colak@hazelcast.com>
2023-04-11 15:12:20 +0300

    Unignore the test and chang the test suite to run only with h2 [HZ-1686]

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/jdbc/JdbcSqlConnectorStabilityTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/jdbc/JdbcSqlConnectorStabilityTest.java
@@ -18,82 +18,62 @@ package com.hazelcast.jet.sql.impl.connector.jdbc;
 
 import com.hazelcast.sql.HazelcastSqlException;
 import com.hazelcast.test.jdbc.H2DatabaseProvider;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 import static com.hazelcast.function.ConsumerEx.noop;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.util.Lists.newArrayList;
 
+/**
+ * This test should not be subclassed by a DB Provider that uses TestContainers underneath.
+ * Because when H2 is shut down, it will not re-start when we try to get a new JDBC connection
+ * <p>
+ * But TestContainers based DB providers such a MySQLDatabaseProvider
+ * will automatically re-start the container. So it is not possible to shut them down
+ */
 public class JdbcSqlConnectorStabilityTest extends JdbcSqlTestSupport {
-
-    private static final int ITEM_COUNT = 5;
-
-    private String tableName;
 
     @BeforeClass
     public static void beforeClass() {
         initialize(new H2DatabaseProvider());
     }
 
-    @Before
-    public void setUp() throws Exception {
-        tableName = randomTableName();
+    @Test
+    public void dataConnectionDownShouldTimeout() throws SQLException {
+        String tableName = "table1";
         createTable(tableName);
-        insertItems(tableName, ITEM_COUNT);
+        insertItems(tableName, 5);
 
         execute(
                 "CREATE MAPPING " + tableName + " ("
-                        + " id INT, "
-                        + " name VARCHAR "
-                        + ") "
-                        + "DATA CONNECTION " + TEST_DATABASE_REF
-        );
-    }
-
-    @Test
-    public void dataConnectionDownShouldTimeout() {
-
-        assertRowsAnyOrder(
-                "SELECT * FROM " + tableName,
-                newArrayList(
-                        new Row(0, "name-0"),
-                        new Row(1, "name-1"),
-                        new Row(2, "name-2"),
-                        new Row(3, "name-3"),
-                        new Row(4, "name-4")
-                )
+                + " id INT, "
+                + " name VARCHAR "
+                + ") "
+                + "DATA CONNECTION " + TEST_DATABASE_REF
         );
 
+        // Shut down H2 for the entire test suite
         databaseProvider.shutdown();
 
+        // We should not be able to access H2 anymore
         assertThatThrownBy(() -> {
-            instance()
-                    .getSql()
+            sqlService
                     .execute("SELECT * FROM " + tableName)
                     .forEach(noop());
-        }).isInstanceOf(HazelcastSqlException.class);
+        }).isInstanceOf(HazelcastSqlException.class)
+                .hasMessageContaining("Mapping 'table1' is invalid: com.hazelcast.core.HazelcastException: " +
+                                      "Could not determine dialect for data connection: testDatabaseRef");
     }
 
     @Test
-    @Ignore("https://github.com/hazelcast/hazelcast/issues/22651")
     public void dataConnectionDownShouldNotAffectUnrelatedQueries() {
-
-        assertRowsAnyOrder(
-                "SELECT * FROM " + tableName,
-                newArrayList(
-                        new Row(0, "name-0"),
-                        new Row(1, "name-1"),
-                        new Row(2, "name-2"),
-                        new Row(3, "name-3"),
-                        new Row(4, "name-4")
-                )
-        );
-
+        // Shut down H2 for the entire test suite
         databaseProvider.shutdown();
 
+        // Still we should be able to read from generated table
         assertRowsAnyOrder(
                 "SELECT * FROM TABLE(generate_series(0, 4))",
                 newArrayList(
@@ -105,5 +85,4 @@ public class JdbcSqlConnectorStabilityTest extends JdbcSqlTestSupport {
                 )
         );
     }
-
 }
