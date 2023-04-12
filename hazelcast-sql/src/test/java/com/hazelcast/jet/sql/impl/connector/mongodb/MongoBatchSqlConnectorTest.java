d7e3bf40ae5c48a34998717a49da013548c80e28
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-04-12 22:18:20 +0200

    Update hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoBatchSqlConnectorTest.java
    Co-authored-by: František Hartman <frant.hartm@gmail.com>

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoBatchSqlConnectorTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoBatchSqlConnectorTest.java
@@ -87,7 +87,7 @@ public class MongoBatchSqlConnectorTest extends MongoSqlTest {
         String mappingName = collectionName;
         String externalName = databaseName + "." + mappingName;
         execute("CREATE MAPPING " + mappingName + " external name " + externalName + " ("
-                + (includeIdInMapping ? " id VARCHAR external name _id, " : "")
+                + (includeIdInMapping ? " id VARCHAR EXTERNAL NAME _id, " : "")
                 + " firstName VARCHAR, "
                 + " lastName VARCHAR, "
                 + " jedi BOOLEAN "
