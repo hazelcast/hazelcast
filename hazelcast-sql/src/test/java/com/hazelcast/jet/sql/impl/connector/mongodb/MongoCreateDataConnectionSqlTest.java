c6b5a7351e82c6c0fb4d68cd9e94d4a770979029
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-04-24 18:14:41 +0200

    Mongo client connection won't cause CREATE DATA CONNECTION to fail

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoCreateDataConnectionSqlTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/mongodb/MongoCreateDataConnectionSqlTest.java
@@ -18,6 +18,7 @@ package com.hazelcast.jet.sql.impl.connector.mongodb;
 
 import com.hazelcast.dataconnection.DataConnection;
 import com.hazelcast.jet.mongodb.dataconnection.MongoDataConnection;
+import com.hazelcast.sql.HazelcastSqlException;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,9 +26,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MongoCreateDataConnectionSqlTest extends MongoSqlTest {
 
     @Test
-    public void test() {
+    public void createsConnection() {
         String dlName = randomName();
-        instance().getSql().execute("CREATE DATA CONNECTION " + dlName + " TYPE Mongo SHARED " + options());
+        instance().getSql().execute("CREATE DATA CONNECTION " + dlName + " TYPE Mongo SHARED " + options()).close();
 
 
         DataConnection dataConnection = getNodeEngineImpl(
@@ -35,5 +36,37 @@ public class MongoCreateDataConnectionSqlTest extends MongoSqlTest {
 
         assertThat(dataConnection).isNotNull();
         assertThat(dataConnection.getConfig().getType()).isEqualTo("Mongo");
+    }
+
+    @Test
+    public void createsConnectionEvenWhenUnreachable_shared() {
+        testCreatesConnectionEvenWhenUnreachable(true);
+    }
+    @Test
+    public void createsConnectionEvenWhenUnreachable_unshared() {
+        testCreatesConnectionEvenWhenUnreachable(false);
+    }
+
+    private void testCreatesConnectionEvenWhenUnreachable(boolean shared) {
+        String dataConnName = randomName();
+        String options = String.format("OPTIONS ('connectionString' = '%s', 'database' = 'fakeNonExisting') ",
+                "mongodb://non-existing-fake-address:1234/?connectTimeoutMS=200&socketTimeoutMS=200&serverSelectionTimeoutMS=200");
+
+        String sharedString = shared ? " SHARED " : " ";
+        instance().getSql().execute("CREATE DATA CONNECTION " + dataConnName + " TYPE Mongo " + sharedString + options)
+                .close();
+
+
+        DataConnection dataConnection = getNodeEngineImpl(
+                instance()).getDataConnectionService().getAndRetainDataConnection(dataConnName, MongoDataConnection.class);
+
+        assertThat(dataConnection).isNotNull();
+        assertThat(dataConnection.getConfig().getType()).isEqualTo("Mongo");
+
+        Exception e = assertThrows(HazelcastSqlException.class, () -> {
+            instance().getSql().execute("CREATE MAPPING test_" + shared + " data connection " + dataConnName)
+                      .close();
+        });
+        assertThat(e.getMessage()).contains("exception={com.mongodb.MongoSocketException: non-existing-fake-address}");
     }
 }
