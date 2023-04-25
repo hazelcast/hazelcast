c48bccdd4c8cdce5e21e17e8a68db16455b87c84
Łukasz Dziedziul <lukasz.dziedziul@hazelcast.com>
2023-04-25 13:22:16 +0200

    Revert "Fix MSSQLDatabaseProvider to work with lazy JdbcDataConnection"
    This reverts commit 938077279938a69e68c38d266401f69bb46e11ef.

--- hazelcast/src/test/java/com/hazelcast/test/jdbc/MSSQLDatabaseProvider.java
+++ hazelcast/src/test/java/com/hazelcast/test/jdbc/MSSQLDatabaseProvider.java
@@ -16,35 +16,26 @@
 
 package com.hazelcast.test.jdbc;
 
-import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.jdbc.ContainerDatabaseDriver;
 
 public class MSSQLDatabaseProvider implements TestDatabaseProvider {
-    public static final String TEST_MSSQL_VERSION = System.getProperty("test.mssql.version", "2017-CU12");
 
     private static final int LOGIN_TIMEOUT = 120;
 
-    private JdbcDatabaseContainer<?> container;
+    private String jdbcUrl;
 
     @Override
     public String createDatabase(String dbName) {
-        //noinspection resource
-        container = new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:" + TEST_MSSQL_VERSION)
-                .acceptLicense()
-                .withPassword("A_Str0ng_Required_Password")
-                .withUrlParam("user", "SA")
-                .withUrlParam("password", "A_Str0ng_Required_Password");
-        container.start();
-        String jdbcUrl = container.getJdbcUrl();
+        jdbcUrl = "jdbc:tc:sqlserver:2017-CU12:///" + dbName;
         waitForDb(jdbcUrl, LOGIN_TIMEOUT);
         return jdbcUrl;
     }
 
     @Override
     public void shutdown() {
-        if (container != null) {
-            container.stop();
-            container = null;
+        if (jdbcUrl != null) {
+            ContainerDatabaseDriver.killContainer(jdbcUrl);
+            jdbcUrl = null;
         }
     }
 }
