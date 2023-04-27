e1aa67683cdd50511521482fed96e7b51bf0793b
Orçun Çolak <orcun.colak@hazelcast.com>
2023-04-27 09:24:27 +0300

    Change MariaDBDatabaseProvider to use testcontainer instance Change MSSQLDatabaseProvider to accept licence in the code  [HZ-2252]

--- hazelcast/src/test/java/com/hazelcast/test/jdbc/MSSQLDatabaseProvider.java
+++ hazelcast/src/test/java/com/hazelcast/test/jdbc/MSSQLDatabaseProvider.java
@@ -29,8 +29,9 @@ public class MSSQLDatabaseProvider implements TestDatabaseProvider {
     @Override
     public String createDatabase(String dbName) {
         container = new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:" + TEST_MSSQLSERVER_VERSION);
+        container.acceptLicense();
         container.withUrlParam("user", container.getUsername())
-                 .withUrlParam("password", container.getPassword());
+                .withUrlParam("password", container.getPassword());
         container.start();
         String jdbcUrl = container.getJdbcUrl();
         waitForDb(jdbcUrl, LOGIN_TIMEOUT);
