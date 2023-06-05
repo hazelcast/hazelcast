326ede486ccf769d3eeaa745553748b4d1687712
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-06-05 17:17:04 +0300

    WIP

--- hazelcast/src/test/java/com/hazelcast/jet/impl/util/PMSTest.java
+++ hazelcast/src/test/java/com/hazelcast/jet/impl/util/PMSTest.java
@@ -15,9 +15,17 @@ import com.hazelcast.jet.core.Vertex;
 import com.hazelcast.jet.core.processor.Processors;
 import com.hazelcast.jet.datamodel.Tuple2;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.security.Permission;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -27,6 +35,7 @@ import static com.hazelcast.jet.core.ProcessorMetaSupplier.forceTotalParallelism
 import static com.hazelcast.jet.core.ProcessorMetaSupplier.memberPruningMetaSupplier;
 import static java.util.Collections.nCopies;
 import static java.util.Collections.singleton;
+import static org.junit.Assert.assertEquals;
 
 public class PMSTest extends SimpleTestInClusterSupport {
     @BeforeClass
@@ -37,10 +46,13 @@ public class PMSTest extends SimpleTestInClusterSupport {
     @Test
     public void test_simpleDag() {
         // Given
-        ProcessorMetaSupplier pmsGen = memberPruningMetaSupplier((ProcessorSupplier) count ->
-                IntStream.range(0, count)
-                        .mapToObj(GenP::new)
-                        .collect(Collectors.toList()));
+        int expectedTotalParallelism = 2;
+        ProcessorMetaSupplier pmsGen = new ValidatingMetaSupplier(
+                memberPruningMetaSupplier((ProcessorSupplier) count ->
+                        IntStream.range(0, count)
+                                .mapToObj(GenP::new)
+                                .collect(Collectors.toList())),
+                expectedTotalParallelism);
 
         ProcessorMetaSupplier pmsPrint = memberPruningMetaSupplier(
                 (ProcessorSupplier) count -> nCopies(count, new PrintP()));
@@ -57,6 +69,39 @@ public class PMSTest extends SimpleTestInClusterSupport {
         Job job = instance().getJet().newJob(dag, jobConfig);
         job.join();
 
+        // should print 0 and 1.
+        assertJobStatusEventually(job, JobStatus.COMPLETED);
+    }
+
+    @Test
+    public void test_simpleDag_takesTwoMembers() {
+        // Given
+        int expectedTotalParallelism = 4;
+        ProcessorMetaSupplier pmsGen = new ValidatingMetaSupplier(
+                memberPruningMetaSupplier((ProcessorSupplier) count ->
+                        IntStream.range(0, count)
+                                .mapToObj(GenP::new)
+                                .collect(Collectors.toList())),
+                expectedTotalParallelism);
+
+        ProcessorMetaSupplier pmsPrint = memberPruningMetaSupplier(
+                (ProcessorSupplier) count -> nCopies(count, new PrintP()));
+
+        DAG dag = new DAG();
+        Vertex generator = dag.newVertex("Generator", pmsGen);
+        Vertex printer = dag.newVertex("Printer", pmsPrint);
+        dag.edge(between(generator, printer));
+        dag.markAsPrunable();
+
+        JobConfig jobConfig = new JobConfig();
+        // TODO: there is an extremely high probability that partitions 1 and 2 will be on different members.
+        //  But we should not rely on probability -> rewrite it with assigning known partitions for diff members.
+        jobConfig.setArgument(KEY_REQUIRED_PARTITIONS, new HashSet<>(Arrays.asList(1, 2)));
+
+        Job job = instance().getJet().newJob(dag, jobConfig);
+        job.join();
+
+        // should print 0 and 1.
         assertJobStatusEventually(job, JobStatus.COMPLETED);
     }
 
@@ -151,7 +196,6 @@ public class PMSTest extends SimpleTestInClusterSupport {
     @Test
     public void test_memberPruningWithPMS() {
         // Given
-
         ProcessorMetaSupplier pmsGen = ProcessorMetaSupplier.of(2, (ProcessorSupplier) count ->
                 IntStream.range(0, count)
                         .mapToObj(GenP::new)
@@ -175,6 +219,69 @@ public class PMSTest extends SimpleTestInClusterSupport {
         job.join();
 
         assertJobStatusEventually(job, JobStatus.COMPLETED);
+    }
+
+    private static class ValidatingMetaSupplier implements ProcessorMetaSupplier {
+        private final ProcessorMetaSupplier wrappingPms;
+        private final int expectedTotalParallelism;
+
+
+        private ValidatingMetaSupplier(ProcessorMetaSupplier wrappingPms, int expectedTotalParallelism) {
+            this.wrappingPms = wrappingPms;
+            this.expectedTotalParallelism = expectedTotalParallelism;
+        }
+
+        @Override
+        public void init(@NotNull Context context) throws Exception {
+            assertEquals(expectedTotalParallelism, context.totalParallelism());
+        }
+
+        @Nullable
+        @Override
+        public Permission getRequiredPermission() {
+            return wrappingPms.getRequiredPermission();
+        }
+
+        @NotNull
+        @Override
+        public Map<String, String> getTags() {
+            return wrappingPms.getTags();
+        }
+
+        @Override
+        public int preferredLocalParallelism() {
+            return wrappingPms.preferredLocalParallelism();
+        }
+
+        @Override
+        public boolean initIsCooperative() {
+            return wrappingPms.initIsCooperative();
+        }
+
+        @Override
+        public Function<? super Address, ? extends ProcessorSupplier> get(@NotNull List<Address> addresses) {
+            return wrappingPms.get(addresses);
+        }
+
+        @Override
+        public boolean doesWorkWithoutInput() {
+            return wrappingPms.doesWorkWithoutInput();
+        }
+
+        @Override
+        public boolean closeIsCooperative() {
+            return wrappingPms.closeIsCooperative();
+        }
+
+        @Override
+        public void close(@Nullable Throwable error) throws Exception {
+            wrappingPms.close(error);
+        }
+
+        @Override
+        public boolean isReusable() {
+            return wrappingPms.isReusable();
+        }
     }
 
     private static class GenP extends AbstractProcessor {
