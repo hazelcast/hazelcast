9c75d3950d0091644d6ea3b4cd462b0108ea4557
Vassilis Bekiaris <vassilis@hazelcast.com>
2023-06-06 09:52:11 +0300

    Validate public address value
    when blank, fail fast with IllegalArgumementException

--- hazelcast/src/test/java/com/hazelcast/instance/impl/DefaultAddressPickerTest.java
+++ hazelcast/src/test/java/com/hazelcast/instance/impl/DefaultAddressPickerTest.java
@@ -295,9 +295,26 @@ public class DefaultAddressPickerTest {
         assertEquals(new Address(host, port), addressPicker.getPublicAddress(null));
     }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPublicAddress_whenBlankViaProperty() throws Exception {
+        config.setProperty("hazelcast.local.publicAddress", " ");
+
+        addressPicker = new DefaultAddressPicker(config, logger);
+        addressPicker.pickAddress();
+    }
+
     @Test(expected = UnknownHostException.class)
     public void testPublicAddress_withInvalidAddress() throws Exception {
         config.getNetworkConfig().setPublicAddress("invalid");
+
+        addressPicker = new DefaultAddressPicker(config, logger);
+        addressPicker.pickAddress();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPublicAddress_withBlankAddress() throws Exception {
+        config.getNetworkConfig().setPublicAddress(" ");
 
         addressPicker = new DefaultAddressPicker(config, logger);
         addressPicker.pickAddress();
