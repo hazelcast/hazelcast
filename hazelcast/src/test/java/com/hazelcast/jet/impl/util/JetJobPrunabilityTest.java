ef7632824c25ba3a2b5082f3182034bd9640a61a
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-06-22 21:31:38 +0300

    Fix tests

--- hazelcast/src/test/java/com/hazelcast/jet/impl/util/JetJobPrunabilityTest.java
+++ hazelcast/src/test/java/com/hazelcast/jet/impl/util/JetJobPrunabilityTest.java
@@ -27,35 +27,27 @@ import com.hazelcast.jet.core.Edge;
 import com.hazelcast.jet.core.JobStatus;
 import com.hazelcast.jet.core.ProcessorMetaSupplier;
 import com.hazelcast.jet.core.ProcessorSupplier;
+import com.hazelcast.jet.core.TestProcessors.CollectPerProcessorSink;
 import com.hazelcast.jet.core.Vertex;
 import com.hazelcast.jet.core.processor.Processors;
-import com.hazelcast.jet.datamodel.Tuple2;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.QuickTest;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Path;
 import java.security.Permission;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -63,18 +55,13 @@ import java.util.stream.IntStream;
 import static com.hazelcast.jet.config.JobConfigArguments.KEY_REQUIRED_PARTITIONS;
 import static com.hazelcast.jet.core.Edge.between;
 import static com.hazelcast.jet.core.ProcessorMetaSupplier.forceTotalParallelismOne;
-import static java.util.Collections.nCopies;
 import static java.util.Collections.singleton;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
 public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
-    private static final String OUTPUT_FILE = "output.txt";
-    private File output;
-    private Scanner scanner;
+    private CollectPerProcessorSink consumerPms;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -83,21 +70,7 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
 
     @Before
     public void setUp() throws Exception {
-        output = new File(OUTPUT_FILE);
-        assertTrue(output.createNewFile());
-        assertTrue(output.exists());
-        try {
-            scanner = new Scanner(Path.of(OUTPUT_FILE));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        File output = new File(OUTPUT_FILE);
-        assertTrue(output.exists());
-        assertTrue(output.delete());
+        consumerPms = new CollectPerProcessorSink();
     }
 
     @Test
@@ -111,12 +84,10 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
                                 .collect(Collectors.toList())),
                 expectedTotalParallelism);
 
-        ProcessorMetaSupplier pmsPrint = ProcessorMetaSupplier.of(
-                (ProcessorSupplier) count -> nCopies(count, new PrintP()));
 
         DAG dag = new DAG();
         Vertex generator = dag.newVertex("Generator", pmsGen);
-        Vertex printer = dag.newVertex("Printer", pmsPrint);
+        Vertex printer = dag.newVertex("Consumer", consumerPms);
         dag.edge(between(generator, printer));
 
         JobConfig jobConfig = new JobConfig();
@@ -127,10 +98,8 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
 
         // should print 0 and 1.
         assertJobStatusEventually(job, JobStatus.COMPLETED);
-
-        Assert.assertEquals("0", scanner.nextLine());
-        Assert.assertEquals("1", scanner.nextLine());
-        scanner.close();
+        List<List<Object>> lists = consumerPms.getLists();
+        assertContainsAll(lists.get(0), List.of(0, 1));
     }
 
     @Test
@@ -144,12 +113,9 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
                                 .collect(Collectors.toList())),
                 expectedTotalParallelism);
 
-        ProcessorMetaSupplier pmsPrint = ProcessorMetaSupplier.of(
-                (ProcessorSupplier) count -> nCopies(count, new PrintP()));
-
         DAG dag = new DAG();
         Vertex generator = dag.newVertex("Generator", pmsGen);
-        Vertex printer = dag.newVertex("Printer", pmsPrint);
+        Vertex printer = dag.newVertex("Printer", consumerPms);
         dag.edge(between(generator, printer));
 
         Map<Address, int[]> partitionAssignment = getPartitionAssignment(instance());
@@ -165,8 +131,8 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
 
         // should print 0 and 1.
         assertJobStatusEventually(job, JobStatus.COMPLETED);
-        Assert.assertEquals("0", scanner.nextLine());
-        Assert.assertEquals("1", scanner.nextLine());
+        List<List<Object>> lists = consumerPms.getLists();
+        assertContainsAll(lists.get(0), List.of(0, 1));
     }
 
     @Test
@@ -183,13 +149,10 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
                         Processors.aggregateP(AggregateOperations.counting())),
                 addr);
 
-        ProcessorMetaSupplier pmsPrint = ProcessorMetaSupplier.of(
-                (ProcessorSupplier) count -> nCopies(count, new PrintP()));
-
         DAG dag = new DAG();
         Vertex generator = dag.newVertex("Generator", pmsGen);
         Vertex aggregator = dag.newVertex("Aggregator", pmsAgg);
-        Vertex printer = dag.newVertex("Printer", pmsPrint);
+        Vertex printer = dag.newVertex("Printer", consumerPms);
 
         // generator -> aggregator
         dag.edge(between(generator, aggregator)
@@ -200,7 +163,6 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
         dag.edge(between(aggregator, printer).isolated());
 
 
-
         JobConfig jobConfig = new JobConfig();
         jobConfig.setArgument(KEY_REQUIRED_PARTITIONS, singleton(1));
 
@@ -209,7 +171,8 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
 
         // should print 2.
         assertJobStatusEventually(job, JobStatus.COMPLETED);
-        Assert.assertEquals("2", scanner.nextLine());
+        List<List<Object>> lists = consumerPms.getLists();
+        assertContainsAll(lists.get(0), List.of(2L));
     }
 
     @Test
@@ -225,23 +188,13 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
                         .mapToObj(GenP::new)
                         .collect(Collectors.toList()));
 
-        ProcessorMetaSupplier pmsJoin = new ValidatingMetaSupplier(
-                ProcessorMetaSupplier.of(
-                        (ProcessorSupplier) count -> nCopies(count, new JoinP())),
-                2);
-
-        ProcessorMetaSupplier pmsPrint = ProcessorMetaSupplier.of(
-                (ProcessorSupplier) count -> nCopies(count, new PrintP()));
-
         DAG dag = new DAG();
         Vertex generatorLeft = dag.newVertex("Generator-Left", pmsGen1);
         Vertex generatorRight = dag.newVertex("Generator-Right", pmsGen2);
-        Vertex joiner = dag.newVertex("Joiner", pmsJoin);
-        Vertex printer = dag.newVertex("Printer", pmsPrint);
+        Vertex consumer = dag.newVertex("Consumer", consumerPms);
 
-        dag.edge(Edge.from(generatorLeft).to(joiner, 0).isolated());
-        dag.edge(Edge.from(generatorRight).to(joiner, 1).distributed().broadcast());
-        dag.edge(between(joiner, printer).isolated());
+        dag.edge(Edge.from(generatorLeft).to(consumer, 0).isolated());
+        dag.edge(Edge.from(generatorRight).to(consumer, 1).distributed().broadcast());
 
         JobConfig jobConfig = new JobConfig();
         jobConfig.setArgument(KEY_REQUIRED_PARTITIONS, singleton(1));
@@ -250,14 +203,11 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
         job.join();
 
         assertJobStatusEventually(job, JobStatus.COMPLETED);
-        Set<String> expectedOutput = new HashSet<>(Arrays.asList("(0, 0)", "(1, 0)", "(0, 1)", "(1, 1)"));
-        try {
-            String nextLine = scanner.nextLine();
-            System.err.println(nextLine);
-            assertTrue(expectedOutput.remove(nextLine));
-        } catch (NoSuchElementException e) {
-            fail("Can't find any line in file");
-        }
+        List<List<Object>> lists = consumerPms.getLists();
+        Set<Object> containerList = new TreeSet<>();
+        containerList.addAll(lists.get(0));
+        containerList.addAll(lists.get(1));
+        assertEquals(containerList, Set.of(0, 1));
     }
 
     private static class ValidatingMetaSupplier implements ProcessorMetaSupplier {
@@ -328,67 +278,6 @@ public class JetJobPrunabilityTest extends SimpleTestInClusterSupport {
         @Override
         public boolean complete() {
             return tryEmit(item);
-        }
-
-        @Override
-        public boolean closeIsCooperative() {
-            return true;
-        }
-    }
-
-    private static class PrintP extends AbstractProcessor {
-        private FileWriter fileWriter;
-
-        {
-            try {
-                fileWriter = new FileWriter(OUTPUT_FILE);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        private PrintWriter printWriter = new PrintWriter(fileWriter);
-
-        @Override
-        protected boolean tryProcess0(@NotNull Object item) {
-            System.err.println(item);
-            printWriter.println(item);
-            printWriter.flush();
-            return complete();
-        }
-
-        @Override
-        public boolean closeIsCooperative() {
-            return true;
-        }
-    }
-
-    private static class JoinP extends AbstractProcessor {
-        protected Integer leftItem = null;
-        protected Integer rightItem = null;
-
-        @Override
-        protected boolean tryProcess0(@NotNull Object item) {
-            if (rightItem == null) {
-                leftItem = (Integer) item;
-                return true;
-            } else {
-                Integer rItem = rightItem;
-                rightItem = null;
-                return tryEmit(Tuple2.tuple2(item, rItem));
-            }
-        }
-
-        @Override
-        protected boolean tryProcess1(@NotNull Object item) {
-            if (leftItem == null) {
-                rightItem = (Integer) item;
-                return true;
-            } else {
-                Integer lItem = leftItem;
-                leftItem = null;
-                return tryEmit(Tuple2.tuple2(lItem, item));
-            }
         }
 
         @Override
