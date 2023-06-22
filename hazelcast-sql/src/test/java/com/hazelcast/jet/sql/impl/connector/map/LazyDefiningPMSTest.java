d9cd9f43e02ac67da5247a1676b5815c88525846
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-06-22 15:14:05 +0300

    Compilation error fix

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/LazyDefiningPMSTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/LazyDefiningPMSTest.java
@@ -61,7 +61,7 @@ public class LazyDefiningPMSTest extends SimpleTestInClusterSupport {
     public void setUp() throws Exception {
         NodeEngineImpl nodeEngine = getNodeEngineImpl(instance());
         partitionAssignment = ExecutionPlanBuilder.getPartitionAssignment(nodeEngine,
-                        Util.getMembersView(nodeEngine).getMembers())
+                        Util.getMembersView(nodeEngine).getMembers(), null)
                 .entrySet().stream().collect(toMap(en -> en.getKey().getAddress(), Entry::getValue));
         Address address = instance().getCluster().getLocalMember().getAddress();
         for (int i = 1; i < ITERATIONS; ++i) {
