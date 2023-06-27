109fe947ccb7d4bb9a437489348a3266d8e182a1
lprimak <lenny@flowlogix.com>
2023-06-26 22:14:35 -0500

    failover test

---
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/CPMemberFailoverTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.cp.internal;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.test.HazelcastParallelParametersRunnerFactory;
+import com.hazelcast.test.HazelcastParametrizedRunner;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
+import java.util.UUID;
+
+import static com.hazelcast.cp.internal.HazelcastRaftTestSupport.waitUntilCPDiscoveryCompleted;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(HazelcastParametrizedRunner.class)
+@UseParametersRunnerFactory(HazelcastParallelParametersRunnerFactory.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class CPMemberFailoverTest extends RaftSplitBrainTestSupport {
+    @Override
+    protected void onBeforeSplitBrainCreated(HazelcastInstance[] instances) {
+        waitUntilCPDiscoveryCompleted(instances);
+        assertEquals(5, instances[0].getCPSubsystem().getCPSubsystemManagementService()
+                .getCPMembers().toCompletableFuture().join().size());
+    }
+
+    @Override
+    protected void onAfterSplitBrainCreated(HazelcastInstance[] firstBrain, HazelcastInstance[] secondBrain) {
+        UUID uuid = secondBrain[0].getCPSubsystem().getLocalCPMember().getUuid();
+        firstBrain[0].getCPSubsystem().getCPSubsystemManagementService().removeCPMember(uuid).toCompletableFuture().join();
+    }
+
+    @Override
+    protected void onAfterSplitBrainHealed(HazelcastInstance[] instances) throws Exception {
+        assertEqualsEventually(() -> instances[0].getCPSubsystem().getCPSubsystemManagementService()
+                .getCPMembers().toCompletableFuture().join().size(), 5);
+    }
+}
