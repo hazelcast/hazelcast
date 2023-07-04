e7a275299875e08c586fbd9895b9b57087c2cdb5
Vassilis Bekiaris <vassilis@hazelcast.com>
2023-07-04 18:54:16 +0300

    DeleteMapMergePolicy cannot be resolved
    in EE WAN compatibility tests, because there is no
    EE tests artifact generated since 5.2.0. This class
    is necessary for checkErasingMapMergePolicy
    compatibility test. So it is included in OS tests
    artifact so it can be resolved reflectively
    in 3.12.8->5.3.1 migration tool compatibility tests

---
+++ hazelcast/src/test/java/com/hazelcast/wan/map/DeleteMapMergePolicy.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.wan.map;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.spi.merge.MergingValue;
+import com.hazelcast.spi.merge.SplitBrainMergePolicy;
+
+import java.io.IOException;
+
+/**
+ * Custom merge policy that ignores merging and existing entries and returns null
+ * that ends up with deletion of the related key.
+ * Used in the context of WAN compatibility tests.
+ */
+class DeleteMapMergePolicy implements SplitBrainMergePolicy<Object, MergingValue<Object>, Object> {
+
+    @Override
+    public Object merge(MergingValue<Object> mergingValue, MergingValue<Object> existingValue) {
+        return null;
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+    }
+}
