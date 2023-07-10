42d49cac6e4e043bcd66854a2fcac194fa7c86f9
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-07-10 17:09:09 +0300

    Checkstyle, specify Pms as non-reusable

--- hazelcast/src/main/java/com/hazelcast/jet/impl/connector/SpecificPartitionsImapReaderPms.java
+++ hazelcast/src/main/java/com/hazelcast/jet/impl/connector/SpecificPartitionsImapReaderPms.java
@@ -1,15 +1,15 @@
 /*
- * Copyright 2023 Hazelcast Inc.
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
  *
- * Licensed under the Hazelcast Community License (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://hazelcast.com/hazelcast-community-license
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -46,12 +46,12 @@ import static com.hazelcast.security.permission.ActionConstants.ACTION_READ;
  * A meta-supplier that will compute required partitions to scan,
  * if attribute partitioning strategy was applied.
  */
-public abstract class SpecificPartitionsImapReaderPms<F extends CompletableFuture, B, R> extends LocalProcessorMetaSupplier<F, B, R> {
+public abstract class SpecificPartitionsImapReaderPms<F extends CompletableFuture, B, R>
+        extends LocalProcessorMetaSupplier<F, B, R> {
     private transient int[] partitionsToScan;
     private transient Map<Address, int[]> partitionAssignment;
 
     private SpecificPartitionsImapReaderPms(
-            final String mapName,
             final BiFunctionEx<HazelcastInstance, InternalSerializationService, Reader<F, B, R>> readerSupplier) {
         super(readerSupplier);
     }
@@ -88,9 +88,14 @@ public abstract class SpecificPartitionsImapReaderPms<F extends CompletableFutur
         }
     }
 
+    @Override
+    public boolean isReusable() {
+        return false;
+    }
+
     // TODO: name it properly.
     public static ProcessorMetaSupplier mapReader(String mapName) {
-        return new SpecificPartitionsImapReaderPms<>(mapName, new LocalMapReaderFunction(mapName)) {
+        return new SpecificPartitionsImapReaderPms<>(new LocalMapReaderFunction(mapName)) {
             @Override
             public Permission getRequiredPermission() {
                 return new MapPermission(mapName, ACTION_CREATE, ACTION_READ);
