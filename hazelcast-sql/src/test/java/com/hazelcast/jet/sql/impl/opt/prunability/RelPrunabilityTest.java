7f2eac52c581253ea78f2b469656dc1e3f668828
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-07-14 17:44:51 +0300

    Cleanup, use attribute partitioning strategy in RelPrunabilityTest
    Signed-off-by: Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/opt/prunability/RelPrunabilityTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/opt/prunability/RelPrunabilityTest.java
@@ -34,7 +34,6 @@ import com.hazelcast.jet.sql.impl.validate.types.HazelcastTypeUtils;
 import com.hazelcast.map.IMap;
 import com.hazelcast.sql.impl.extract.QueryPath;
 import com.hazelcast.sql.impl.schema.TableField;
-import com.hazelcast.sql.impl.schema.map.MapTableField;
 import com.hazelcast.sql.impl.type.QueryDataType;
 import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.rel.type.RelDataType;
@@ -43,6 +42,7 @@ import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -52,8 +52,10 @@ import static com.hazelcast.sql.impl.extract.QueryPath.KEY;
 import static com.hazelcast.sql.impl.extract.QueryPath.VALUE;
 import static com.hazelcast.sql.impl.schema.map.MapTableUtils.getPartitionedMapIndexes;
 import static com.hazelcast.sql.impl.type.QueryDataType.BIGINT;
+import static com.hazelcast.sql.impl.type.QueryDataType.OBJECT;
 import static com.hazelcast.sql.impl.type.QueryDataType.VARCHAR;
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
@@ -64,8 +66,11 @@ public class RelPrunabilityTest extends OptimizerTestSupport {
             SqlTypeName.BIGINT,
             true);
 
+    private static final String MAP_NAME = "m";
+
     private HazelcastTable table;
     private HazelcastRelMetadataQuery query;
+    List<TableField> mapTableFields;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -74,55 +79,55 @@ public class RelPrunabilityTest extends OptimizerTestSupport {
 
     @Before
     public void before() throws Exception {
-        table = partitionedTable(
-                "m",
-                asList(
-                        mapField(KEY, BIGINT, QueryPath.KEY_PATH),
-                        mapField(VALUE, VARCHAR, QueryPath.VALUE_PATH)),
-                10);
+        this.mapTableFields = asList(
+                mapField(KEY, OBJECT, QueryPath.KEY_PATH),
+                mapField("comp0", BIGINT, QueryPath.create(QueryPath.KEY_PREFIX + "comp0")),
+                mapField("comp1", BIGINT, QueryPath.create(QueryPath.KEY_PREFIX + "comp1")),
+                mapField(VALUE, BIGINT, QueryPath.VALUE_PATH));
+
+        this.table = partitionedTable(
+                MAP_NAME,
+                mapTableFields,
+                emptyList(),
+                10,
+                singletonList("comp1"));
     }
 
     @Test
-    public void test_fullScan() {
-        PhysicalRel root = optimizePhysical(
-                "SELECT __key FROM m WHERE __key = 10 AND this IS NOT NULL",
-                asList(BIGINT, VARCHAR),
-                table
-        ).getPhysical();
+    public void test_fullScanWithStrategy() {
+        PhysicalRel root = optimizePhysical("SELECT * FROM m WHERE comp1 = 10", asList(BIGINT, BIGINT), table)
+                .getPhysical();
         assertPlan(root, plan(planRow(0, FullScanPhysicalRel.class)));
 
         query = HazelcastRelMetadataQuery.reuseOrCreate(RelMetadataQuery.instance());
         Map<String, List<Map<String, RexNode>>> prunability = query.extractPrunability(root);
         final RexLiteral expectedLiteral = HazelcastRexBuilder.INSTANCE.makeLiteral(10, REL_TYPE_BIGINT);
-        assertEquals(Map.of("m", singletonList(Map.of("__key", expectedLiteral))), prunability);
+        assertEquals(Map.of(MAP_NAME, singletonList(Map.of("comp1", expectedLiteral))), prunability);
     }
 
     @Test
+    @Ignore("Resolve later : There is no suitable accessor for 'comp1' on class 'java.lang.String'")
     public void test_indexScan_isNotSupported() {
         String mapName = randomName();
         String indexName = randomName();
-        IMap<Integer, String> map = instance().getMap(mapName);
-        createMapping(mapName, Integer.class, String.class);
-        createIndex(indexName, mapName, IndexType.HASH, "this");
+        IMap<CompoundKey, String> map = instance().getMap(mapName);
+        createMapping(mapName, CompoundKey.class, String.class);
+        createIndex(indexName, mapName, IndexType.HASH, "comp1");
         for (int i = 0; i < 100; ++i) {
-            map.put(i, "" + i);
+            map.put(new CompoundKey(i, i), "" + i);
         }
-
-        List<QueryDataType> parameterTypes = asList(QueryDataType.INT, VARCHAR);
-        List<TableField> mapTableFields = asList(
-                new MapTableField("__key", QueryDataType.INT, false, QueryPath.KEY_PATH),
-                new MapTableField("this", VARCHAR, false, QueryPath.VALUE_PATH));
 
         table = partitionedTable(
                 mapName,
                 mapTableFields,
                 getPartitionedMapIndexes(mapContainer(map), mapTableFields),
-                1 // we can place random number, doesn't matter in current case.
+                1, // we can place random number, doesn't matter in current case.
+                singletonList("comp1")
         );
 
         PhysicalRel root = optimizePhysical(
-                "SELECT __key FROM " + mapName + " WHERE this = '10'",
-                parameterTypes,
+                "SELECT * FROM " + mapName + " WHERE comp1 = 10",
+                asList(OBJECT, QueryDataType.INT, QueryDataType.INT, VARCHAR),
                 table
         ).getPhysical();
         assertPlan(root, plan(planRow(0, IndexScanMapPhysicalRel.class)));
@@ -133,14 +138,15 @@ public class RelPrunabilityTest extends OptimizerTestSupport {
 
         assertEquals(emptyMap(), prunability);
         // TODO: uncomment when IndexScan prunability is implemented
-        // assertEquals(Map.of("m", singletonList(Map.of("this", expectedLiteral))), prunability);
+        // assertEquals(Map.of(MAP_NAME, singletonList(Map.of("comp1", expectedLiteral))), prunability);
     }
 
 
     @Test
     public void test_aggAndCalc_areNotSupported() {
         PhysicalRel root = optimizePhysical(
-                "SELECT this, SUM(__key) FROM m WHERE __key = ? AND this = '10' GROUP BY __key, this",
+                "SELECT this, COUNT(__key) FROM m WHERE comp1 = ? AND comp0 = 10 " +
+                        "GROUP BY comp0, comp1, this",
                 asList(BIGINT, VARCHAR),
                 table
         ).getPhysical();
@@ -157,26 +163,25 @@ public class RelPrunabilityTest extends OptimizerTestSupport {
         assertEquals(emptyMap(), prunability);
         // TODO: uncomment when Aggregation prunability is implemented
         // final RexDynamicParam param = HazelcastRexBuilder.INSTANCE.makeDynamicParam(REL_TYPE_BIGINT, 0);
-        // assertEquals(Map.of("m", singletonList(Map.of("this", param))), prunability);
+        // assertEquals(Map.of(MAP_NAME, singletonList(Map.of("comp1", param))), prunability);
     }
 
     @Test
     public void test_joinAndCalc_areNotSupported() {
         PhysicalRel root = optimizePhysical(
-                "SELECT m1.__key, m2.__key FROM m AS m1 "
+                "SELECT * FROM m AS m1 "
                         + "JOIN m AS m2 "
-                        + "ON m1.__key = m2.__key "
-                        + "WHERE m1.__key = 10 AND m1.this IS NOT NULL " +
-                        "    AND m2.__key = 10 AND m2.this IS NOT NULL",
+                        + "ON m1.comp1 = m2.comp1 "
+                        + "WHERE m1.comp1 = 10 AND m1.this IS NOT NULL " +
+                        "    AND m2.comp1 = 10 AND m2.this IS NOT NULL",
                 asList(BIGINT, VARCHAR),
                 table
         ).getPhysical();
 
         assertPlan(root, plan(
-                planRow(0, CalcPhysicalRel.class),
-                planRow(1, JoinNestedLoopPhysicalRel.class),
-                planRow(2, FullScanPhysicalRel.class),
-                planRow(2, FullScanPhysicalRel.class)
+                planRow(0, JoinNestedLoopPhysicalRel.class),
+                planRow(1, FullScanPhysicalRel.class),
+                planRow(1, FullScanPhysicalRel.class)
         ));
 
         query = HazelcastRelMetadataQuery.reuseOrCreate(RelMetadataQuery.instance());
@@ -185,17 +190,17 @@ public class RelPrunabilityTest extends OptimizerTestSupport {
         // TODO: uncomment when Aggregation prunability is implemented
 //        final RexLiteral l = HazelcastRexBuilder.INSTANCE.makeLiteral(10, REL_TYPE_BIGINT);
 //        assertEquals(Map.of(
-//                        "m1", singletonList(Map.of("__key", l)),
-//                        "m2", singletonList(Map.of("__key", l))),
+//                        "m1", singletonList(Map.of("comp1", l)),
+//                        "m2", singletonList(Map.of("comp1", l))),
 //                prunability);
     }
 
     @Test
     public void test_union() {
         PhysicalRel root = optimizePhysical(
-                "(SELECT __key FROM m WHERE __key = 10 AND this IS NOT NULL)" +
+                "(SELECT * FROM m WHERE comp1 = 10 AND this IS NOT NULL)" +
                         " UNION ALL " +
-                        "(SELECT __key FROM m WHERE __key = 10 AND this IS NOT NULL)",
+                        "(SELECT * FROM m WHERE comp1 = 10 AND this IS NOT NULL)",
                 asList(BIGINT, VARCHAR),
                 table
         ).getPhysical();
@@ -210,7 +215,17 @@ public class RelPrunabilityTest extends OptimizerTestSupport {
         Map<String, List<Map<String, RexNode>>> prunability = query.extractPrunability(root);
         final RexLiteral l = HazelcastRexBuilder.INSTANCE.makeLiteral(10, REL_TYPE_BIGINT);
         assertEquals(
-                Map.of("m", asList(Map.of("__key", l), Map.of("__key", l))),
+                Map.of(MAP_NAME, asList(Map.of("comp1", l), Map.of("comp1", l))),
                 prunability);
+    }
+
+    static class CompoundKey {
+        public Integer comp0;
+        public Integer comp1;
+
+        CompoundKey(Integer comp0, Integer comp1) {
+            this.comp0 = comp0;
+            this.comp1 = comp1;
+        }
     }
 }
