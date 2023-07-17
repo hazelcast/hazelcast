9ebe8fd920bc27398651e1d8a95136f0d9eacb3a
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-07-17 22:09:56 +0200

    Give the possibility to control pool size

--- extensions/mongodb/src/main/java/com/hazelcast/jet/mongodb/dataconnection/MongoDataConnection.java
+++ extensions/mongodb/src/main/java/com/hazelcast/jet/mongodb/dataconnection/MongoDataConnection.java
@@ -21,6 +21,7 @@ import com.hazelcast.dataconnection.DataConnectionBase;
 import com.hazelcast.dataconnection.DataConnectionResource;
 import com.hazelcast.jet.impl.util.ConcurrentMemoizingSupplier;
 import com.hazelcast.spi.annotation.Beta;
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoClientSettings.Builder;
 import com.mongodb.MongoCredential;
@@ -28,6 +29,7 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.connection.ConnectionPoolSettings;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -78,31 +80,50 @@ public class MongoDataConnection extends DataConnectionBase {
      * Default value is {@code admin}.
      */
     public static final String AUTH_DB_PROPERTY = "authDb";
+    /**
+     * Name of the property holding the minimum size of Mongo Client connection pool.
+     * Default is 10.
+     */
+    public static final String CONNECTION_POOL_MIN = "connectionPoolMinSize";
+    /**
+     * Name of the property holding the maximum size of Mongo Client connection pool.
+     * Default is 0, which means unlimited.
+     */
+    public static final String CONNECTION_POOL_MAX = "connectionPoolMaxSize";
 
     private volatile ConcurrentMemoizingSupplier<MongoClient> mongoClientSup;
+    private final String name;
+    private final String connectionString;
     private final String databaseName;
     private final String username;
     private final String password;
     private final String host;
     private final String authDb;
+    private final int connectionPoolMinSize;
+    private final int connectionPoolMaxSize;
 
     /**
      * Creates a new data connection based on given config.
      */
+    @SuppressWarnings("DataFlowIssue")
     public MongoDataConnection(DataConnectionConfig config) {
         super(config);
+        this.name = config.getName();
+        this.connectionString = config.getProperty(CONNECTION_STRING_PROPERTY);
         this.databaseName = config.getProperty(DATABASE_PROPERTY);
         this.username = config.getProperty(USERNAME_PROPERTY);
         this.password = config.getProperty(PASSWORD_PROPERTY);
         this.host = config.getProperty(HOST_PROPERTY);
         this.authDb = config.getProperty(AUTH_DB_PROPERTY, "admin");
+        this.connectionPoolMinSize = Integer.parseInt(config.getProperty(CONNECTION_POOL_MIN, "10"));
+        this.connectionPoolMaxSize = Integer.parseInt(config.getProperty(CONNECTION_POOL_MAX, "0"));
 
         checkState(allSame((username == null), (password == null), (host == null)),
         "You have to provide connectionString property or combination of username, password and host");
 
         if (config.isShared()) {
             this.mongoClientSup = new ConcurrentMemoizingSupplier<>(
-                    () -> new CloseableMongoClient(createClient(config), this::release));
+                    () -> new CloseableMongoClient(createClient(), this::release));
         }
     }
 
@@ -119,23 +140,33 @@ public class MongoDataConnection extends DataConnectionBase {
         return true;
     }
 
-    private MongoClient createClient(DataConnectionConfig config) {
+    @SuppressWarnings("checkstyle:MagicNumber")
+    private MongoClient createClient() {
         try {
-            String connectionString = config.getProperty(CONNECTION_STRING_PROPERTY);
             if (connectionString != null) {
-                return MongoClients.create(connectionString);
+                Builder builder = MongoClientSettings.builder()
+                                                     .applyConnectionString(new ConnectionString(connectionString))
+                                                     .codecRegistry(defaultCodecRegistry())
+                                                     .applyToConnectionPoolSettings(this::connectionPoolSettings);
+                return MongoClients.create(builder.build());
             }
             ServerAddress serverAddress = new ServerAddress(host);
             MongoCredential credential = MongoCredential.createCredential(username, authDb, password.toCharArray());
             Builder builder = MongoClientSettings.builder()
                                                  .codecRegistry(defaultCodecRegistry())
+                                                 .applyToConnectionPoolSettings(this::connectionPoolSettings)
                                                  .applyToClusterSettings(s -> s.hosts(singletonList(serverAddress)))
                                                  .credential(credential);
             return MongoClients.create(builder.build());
         } catch (Exception e) {
-            throw new HazelcastException("Unable to create Mongo client for data connection '" + config.getName() + "'",
+            throw new HazelcastException("Unable to create Mongo client for data connection '" + name + "'",
                     e);
         }
+    }
+
+    private void connectionPoolSettings(ConnectionPoolSettings.Builder builder) {
+        builder.minSize(connectionPoolMinSize)
+               .maxSize(connectionPoolMaxSize);
     }
 
     /**
@@ -153,7 +184,7 @@ public class MongoDataConnection extends DataConnectionBase {
             checkState(supplier != null, "Mongo client should not be closed at this point");
             return supplier.get();
         } else {
-            MongoClient client = createClient(getConfig());
+            MongoClient client = createClient();
             return new CloseableMongoClient(client, client::close);
         }
     }
