a854877bea159176ff8fe41a41feb16919b1c0af
ivanthescientist <ivan.yaschishin@hazelcast.com>
2023-07-24 14:01:28 +0300

    checkstyle fix

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/model/OrderKey.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/map/model/OrderKey.java
@@ -62,8 +62,12 @@ public class OrderKey implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         OrderKey orderKey = (OrderKey) o;
         return orderId == orderKey.orderId;
     }
