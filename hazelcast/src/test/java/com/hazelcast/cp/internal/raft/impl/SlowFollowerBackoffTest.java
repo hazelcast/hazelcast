1aa793c480ab4d9c1900480e9279a966768e6aca
arodionov <andrii.rodionov@hazelcast.com>
2023-07-25 11:32:44 +0200

    Updated copyright headers

--- hazelcast/src/test/java/com/hazelcast/cp/internal/raft/impl/SlowFollowerBackoffTest.java
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/raft/impl/SlowFollowerBackoffTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ * Portions Copyright (c) 2020, MicroRaft.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- hazelcast/src/test/java/com/hazelcast/cp/internal/raft/impl/state/FollowerStateTest.java
+++ hazelcast/src/test/java/com/hazelcast/cp/internal/raft/impl/state/FollowerStateTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ * Original work Copyright (c) 2020, MicroRaft.
+ * Modified work Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
