e30859cd60107d37feef5563afb7f36815c871aa
James Holgate <james.holgate@hazelcast.com>
2023-08-03 17:15:17 +0100

    Simplify regression test, improve HZ instance creation

--- hazelcast/src/test/java/com/hazelcast/internal/cluster/impl/ClusterBatchedJoinTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/cluster/impl/ClusterBatchedJoinTest.java
@@ -16,14 +16,14 @@
 
 package com.hazelcast.internal.cluster.impl;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.cluster.Address;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
-import com.hazelcast.core.DistributedObject;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
+import com.hazelcast.internal.util.FutureUtil;
+import com.hazelcast.spi.impl.eventservice.EventRegistration;
+import com.hazelcast.spi.impl.proxyservice.impl.ProxyServiceImpl;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.QuickTest;
@@ -35,11 +35,13 @@ import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Set;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import static com.hazelcast.test.Accessors.getNodeEngineImpl;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(HazelcastSerialClassRunner.class)
@@ -47,7 +49,6 @@ import static org.junit.Assert.assertEquals;
 public class ClusterBatchedJoinTest extends HazelcastTestSupport {
 
     private final HazelcastInstance[] batchedMembers = new HazelcastInstance[10];
-    private final List<String> membersList = new ArrayList<>(10);
 
     @Before
     public void prepare() throws InterruptedException {
@@ -64,15 +65,14 @@ public class ClusterBatchedJoinTest extends HazelcastTestSupport {
         // Create a cluster that goes through a batched join, starting with a master instance (always index 0)
         batchedMembers[0] = Hazelcast.newHazelcastInstance(config);
         // Start 9 members simultaneously (so their join request is batched)
-        ExecutorService pool = Executors.newFixedThreadPool(9);
+        List<Future<?>> futures = new ArrayList<>();
         for (int k = 1; k < 10; k++) {
             int finalK = k;
-            pool.execute(() -> batchedMembers[finalK] = Hazelcast.newHazelcastInstance(config));
+            futures.add(spawn(() -> batchedMembers[finalK] = Hazelcast.newHazelcastInstance(config)));
         }
 
         // Wait for all member instances to be created
-        pool.shutdown();
-        pool.awaitTermination(30, TimeUnit.SECONDS);
+        FutureUtil.waitWithDeadline(futures, 30, TimeUnit.SECONDS);
 
         // Ensure cluster size is 10
         assertClusterSizeEventually(10, batchedMembers);
@@ -86,50 +86,31 @@ public class ClusterBatchedJoinTest extends HazelcastTestSupport {
         }
     }
 
+    /**
+     * <a href="https://hazelcast.atlassian.net/browse/HZ-2797">Related to HZ-2797</a>
+     */
     @Test
-    public void testDistributedObjectsAccurateAfterBatchedJoinHandling() {
-        // Create a client and connect to the cluster
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("myCluster").getNetworkConfig().setAddresses(membersList);
-        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-
-        try {
-            // Create 5 maps and populate them with some data
-            for (int i = 0; i < 5; i++) {
-                createAndPopulateMap(client, "map-" + i);
-            }
-
-            // Fetch distributed objects list and assert its size
-            Collection<DistributedObject> distributedObjects = client.getDistributedObjects();
-            assertEquals(5, distributedObjects.size());
-
-            // Delete 3 maps
-            for (int i = 0; i < 3; i++) {
-                client.getMap("map-" + i).destroy();
-            }
-
-            // Assert distributed objects size
-            distributedObjects = client.getDistributedObjects();
-            assertEquals(2, distributedObjects.size());
-
-            // Re-create 3 maps
-            for (int i = 0; i < 3; i++) {
-                createAndPopulateMap(client, "map-" + i);
-            }
-
-            // Assert distributed objects size
-            distributedObjects = client.getDistributedObjects();
-            assertEquals(5, distributedObjects.size());
-        } finally {
-            // Terminate client
-            client.getLifecycleService().terminate();
+    public void testProxyServiceRegistrationsPresentForAllMembers() {
+        // Gather a set of expected addresses to use in comparisons
+        Set<Address> expectedAddresses = new HashSet<>(batchedMembers.length);
+        for (HazelcastInstance member : batchedMembers) {
+            expectedAddresses.add(getNodeEngineImpl(member).getThisAddress());
         }
-    }
 
-    private void createAndPopulateMap(HazelcastInstance client, String mapName) {
-        IMap<Integer, String> map = client.getMap(mapName);
-        for (int j = 0; j < 10; j++) {
-            map.put(j, "test" + j);
+        // Iterate over all members of the cluster and assert that it has listeners registered for
+        //  every member of the cluster (including the local member itself)
+        Set<Address> localAddresses = new HashSet<>(batchedMembers.length);
+        for (int i = 0; i < 10; i++) {
+            HazelcastInstance member = batchedMembers[i];
+            Collection<EventRegistration> registrations = getNodeEngineImpl(member)
+                    .getEventService()
+                    .getRegistrations(ProxyServiceImpl.SERVICE_NAME, ProxyServiceImpl.SERVICE_NAME);
+
+            localAddresses.clear();
+            registrations.forEach(registration -> localAddresses.add(registration.getSubscriber()));
+
+            assertEquals(expectedAddresses.size(), localAddresses.size());
+            assertContainsAll(localAddresses, expectedAddresses);
         }
     }
 }
