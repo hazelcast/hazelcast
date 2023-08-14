f05c0eae6687767befa489980d678395958f530b
Peter Veentjer <alarmnummer@gmail.com>
2023-08-14 05:47:28 +0300

    Added some JMH benchmarks

---
+++ hazelcast-tpc-engine-parent/hazelcast-tpc-engine-jmh/src/main/java/com/hazelcast/ThreadLocalBenchmark.java
@@ -0,0 +1,29 @@
+package com.hazelcast;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1)
+@Warmup(iterations = 1)
+@Threads(value = 1)
+public class ThreadLocalBenchmark {
+
+    private static ThreadLocal<Integer> THREAD_LOCAL = ThreadLocal.withInitial(() -> 10);
+
+    @Benchmark
+    public Integer get() {
+        return THREAD_LOCAL.get();
+    }
+}
