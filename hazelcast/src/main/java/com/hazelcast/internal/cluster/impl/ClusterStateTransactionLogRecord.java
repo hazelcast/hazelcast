hazelcast/hazelcast

commit 35eabe912c995758ba972492d2d1ef906934b9e0

Cleanup code for rolling upgrade compatibility (#24595)
Remove code that was added to support rolling member upgrades from
previous versions which are not relevant for 5.4 development.

EE PR: https://github.com/hazelcast/hazelcast-enterprise/pull/6032

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/ClusterStateTransactionLogRecord.java
@@ -19,7 +19,6 @@ package com.hazelcast.internal.cluster.impl;
 import com.hazelcast.cluster.Address;
 import com.hazelcast.cluster.Cluster;
 import com.hazelcast.cluster.ClusterState;
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.cluster.impl.operations.CommitClusterStateOp;
 import com.hazelcast.internal.cluster.impl.operations.LockClusterStateOp;
 import com.hazelcast.internal.cluster.impl.operations.RollbackClusterStateOp;
@@ -27,7 +26,6 @@ import com.hazelcast.internal.util.Preconditions;
 import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.operationservice.Operation;
 import com.hazelcast.transaction.impl.TargetAwareTransactionLogRecord;
 
@@ -40,7 +38,7 @@ import java.util.UUID;
  * @see ClusterState
  * @see Cluster#changeClusterState(ClusterState, com.hazelcast.transaction.TransactionOptions)
  */
-public class ClusterStateTransactionLogRecord implements TargetAwareTransactionLogRecord, Versioned {
+public class ClusterStateTransactionLogRecord implements TargetAwareTransactionLogRecord {
 
     ClusterStateChange stateChange;
     Address initiator;
@@ -105,11 +103,7 @@ public class ClusterStateTransactionLogRecord implements TargetAwareTransactionL
         out.writeObject(target);
         UUIDSerializationUtil.writeUUID(out, txnId);
         out.writeLong(leaseTime);
-        if (out.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            out.writeLong(partitionStateStamp);
-        } else {
-            out.writeInt((int) partitionStateStamp);
-        }
+        out.writeLong(partitionStateStamp);
         out.writeBoolean(isTransient);
         out.writeInt(memberListVersion);
     }
@@ -121,11 +115,7 @@ public class ClusterStateTransactionLogRecord implements TargetAwareTransactionL
         target = in.readObject();
         txnId = UUIDSerializationUtil.readUUID(in);
         leaseTime = in.readLong();
-        if (in.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            partitionStateStamp = in.readLong();
-        } else {
-            partitionStateStamp = in.readInt();
-        }
+        partitionStateStamp = in.readLong();
         isTransient = in.readBoolean();
         memberListVersion = in.readInt();
     }

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/operations/HeartbeatOp.java
@@ -17,7 +17,6 @@
 package com.hazelcast.internal.cluster.impl.operations;
 
 import com.hazelcast.internal.cluster.MemberInfo;
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.cluster.impl.ClusterDataSerializerHook;
 import com.hazelcast.internal.cluster.impl.ClusterHeartbeatManager;
 import com.hazelcast.internal.cluster.impl.ClusterServiceImpl;
@@ -25,16 +24,14 @@ import com.hazelcast.internal.cluster.impl.MembersViewMetadata;
 import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.impl.Versioned;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.UUID;
 
 /** A heartbeat sent from one cluster member to another. The sent timestamp is the cluster clock time of the sending member */
-public final class HeartbeatOp extends AbstractClusterOperation implements Versioned {
+public final class HeartbeatOp extends AbstractClusterOperation {
 
     private MembersViewMetadata senderMembersViewMetadata;
     private UUID targetUuid;
@@ -70,11 +67,9 @@ public final class HeartbeatOp extends AbstractClusterOperation implements Versi
         out.writeObject(senderMembersViewMetadata);
         UUIDSerializationUtil.writeUUID(out, targetUuid);
         out.writeLong(timestamp);
-        if (out.getVersion().isGreaterThan(Versions.V4_0)) {
-            out.writeInt(suspectedMembers.size());
-            for (MemberInfo m : suspectedMembers) {
-                out.writeObject(m);
-            }
+        out.writeInt(suspectedMembers.size());
+        for (MemberInfo m : suspectedMembers) {
+            out.writeObject(m);
         }
     }
 
@@ -84,15 +79,11 @@ public final class HeartbeatOp extends AbstractClusterOperation implements Versi
         senderMembersViewMetadata = in.readObject();
         targetUuid = UUIDSerializationUtil.readUUID(in);
         timestamp = in.readLong();
-        if (in.getVersion().isGreaterThan(Versions.V4_0)) {
-            int suspectedMemberCount = in.readInt();
-            suspectedMembers = new HashSet<>(suspectedMemberCount);
-            for (int i = 0; i < suspectedMemberCount; i++) {
-                MemberInfo m = in.readObject();
-                suspectedMembers.add(m);
-            }
-        } else {
-            suspectedMembers = Collections.emptySet();
+        int suspectedMemberCount = in.readInt();
+        suspectedMembers = new HashSet<>(suspectedMemberCount);
+        for (int i = 0; i < suspectedMemberCount; i++) {
+            MemberInfo m = in.readObject();
+            suspectedMembers.add(m);
         }
     }
 

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/operations/LockClusterStateOp.java
@@ -19,7 +19,6 @@ package com.hazelcast.internal.cluster.impl.operations;
 import com.hazelcast.cluster.Address;
 import com.hazelcast.cluster.ClusterState;
 import com.hazelcast.core.MemberLeftException;
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.cluster.impl.ClusterDataSerializerHook;
 import com.hazelcast.internal.cluster.impl.ClusterServiceImpl;
 import com.hazelcast.internal.cluster.impl.ClusterStateChange;
@@ -28,7 +27,6 @@ import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.exception.TargetNotMemberException;
 import com.hazelcast.spi.impl.AllowedDuringPassiveState;
 import com.hazelcast.spi.impl.operationservice.ExceptionAction;
@@ -40,7 +38,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 public class LockClusterStateOp  extends Operation implements AllowedDuringPassiveState, UrgentSystemOperation,
-        IdentifiedDataSerializable, Versioned {
+        IdentifiedDataSerializable {
 
     private ClusterStateChange stateChange;
     private Address initiator;
@@ -114,11 +112,7 @@ public class LockClusterStateOp  extends Operation implements AllowedDuringPassi
         out.writeObject(initiator);
         UUIDSerializationUtil.writeUUID(out, txnId);
         out.writeLong(leaseTime);
-        if (out.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            out.writeLong(partitionStateStamp);
-        } else {
-            out.writeInt((int) partitionStateStamp);
-        }
+        out.writeLong(partitionStateStamp);
         out.writeInt(memberListVersion);
     }
 
@@ -129,11 +123,7 @@ public class LockClusterStateOp  extends Operation implements AllowedDuringPassi
         initiator = in.readObject();
         txnId = UUIDSerializationUtil.readUUID(in);
         leaseTime = in.readLong();
-        if (in.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            partitionStateStamp = in.readLong();
-        } else {
-            partitionStateStamp = in.readInt();
-        }
+        partitionStateStamp = in.readLong();
         memberListVersion = in.readInt();
     }
 

hazelcast/src/main/java/com/hazelcast/map/impl/operation/PutAllOperation.java
@@ -17,18 +17,16 @@
 package com.hazelcast.map.impl.operation;
 
 import com.hazelcast.core.EntryEventType;
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.map.IMap;
 import com.hazelcast.map.impl.MapDataSerializerHook;
 import com.hazelcast.map.impl.MapEntries;
-import com.hazelcast.map.impl.operation.steps.engine.Step;
 import com.hazelcast.map.impl.operation.steps.PutAllOpSteps;
 import com.hazelcast.map.impl.operation.steps.engine.State;
+import com.hazelcast.map.impl.operation.steps.engine.Step;
 import com.hazelcast.map.impl.record.Record;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.operationservice.BackupAwareOperation;
 import com.hazelcast.spi.impl.operationservice.MutatingOperation;
 import com.hazelcast.spi.impl.operationservice.Operation;
@@ -53,7 +51,7 @@ import static com.hazelcast.map.impl.record.Record.UNSET;
  */
 public class PutAllOperation extends MapOperation
         implements PartitionAwareOperation, BackupAwareOperation,
-        MutatingOperation, Versioned {
+        MutatingOperation {
 
     private transient int currentIndex;
     private MapEntries mapEntries;
@@ -261,20 +259,14 @@ public class PutAllOperation extends MapOperation
     protected void writeInternal(ObjectDataOutput out) throws IOException {
         super.writeInternal(out);
         out.writeObject(mapEntries);
-        if (out.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            out.writeBoolean(triggerMapLoader);
-        }
+        out.writeBoolean(triggerMapLoader);
     }
 
     @Override
     protected void readInternal(ObjectDataInput in) throws IOException {
         super.readInternal(in);
         mapEntries = in.readObject();
-        if (in.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            triggerMapLoader = in.readBoolean();
-        } else {
-            triggerMapLoader = true;
-        }
+        triggerMapLoader = in.readBoolean();
     }
 
     @Override

hazelcast/src/main/java/com/hazelcast/map/impl/operation/PutAllPartitionAwareOperationFactory.java
@@ -16,13 +16,11 @@
 
 package com.hazelcast.map.impl.operation;
 
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.map.IMap;
 import com.hazelcast.map.impl.MapDataSerializerHook;
 import com.hazelcast.map.impl.MapEntries;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.operationservice.Operation;
 import com.hazelcast.spi.impl.operationservice.impl.operations.PartitionAwareOperationFactory;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -36,7 +34,7 @@ import java.util.Map;
  * <p>
  * Used to reduce the number of remote invocations of an {@link IMap#putAll(Map)} or {@link IMap#setAll(Map)} call.
  */
-public class PutAllPartitionAwareOperationFactory extends PartitionAwareOperationFactory implements Versioned {
+public class PutAllPartitionAwareOperationFactory extends PartitionAwareOperationFactory {
 
     protected String name;
     protected MapEntries[] mapEntries;
@@ -71,9 +69,7 @@ public class PutAllPartitionAwareOperationFactory extends PartitionAwareOperatio
         for (MapEntries entry : mapEntries) {
             entry.writeData(out);
         }
-        if (out.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            out.writeBoolean(triggerMapLoader);
-        }
+        out.writeBoolean(triggerMapLoader);
     }
 
     @Override
@@ -86,11 +82,7 @@ public class PutAllPartitionAwareOperationFactory extends PartitionAwareOperatio
             entry.readData(in);
             mapEntries[i] = entry;
         }
-        if (in.getVersion().isGreaterOrEqual(Versions.V4_1)) {
-            triggerMapLoader = in.readBoolean();
-        } else {
-            triggerMapLoader = true;
-        }
+        triggerMapLoader = in.readBoolean();
     }
 
     @Override

hazelcast/src/main/java/com/hazelcast/map/impl/query/Query.java
@@ -17,7 +17,6 @@
 package com.hazelcast.map.impl.query;
 
 import com.hazelcast.aggregation.Aggregator;
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.serialization.SerializationService;
 import com.hazelcast.internal.util.IterationType;
 import com.hazelcast.internal.util.collection.PartitionIdSet;
@@ -25,7 +24,6 @@ import com.hazelcast.map.impl.MapDataSerializerHook;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.projection.Projection;
 import com.hazelcast.query.PagingPredicate;
 import com.hazelcast.query.Predicate;
@@ -38,7 +36,7 @@ import static com.hazelcast.internal.util.Preconditions.checkNotNull;
 /**
  * Object representing a Query together with all possible co-variants: like a predicate, iterationType, etc.
  */
-public class Query implements IdentifiedDataSerializable, Versioned {
+public class Query implements IdentifiedDataSerializable {
 
     private String mapName;
     private Predicate predicate;
@@ -145,9 +143,7 @@ public class Query implements IdentifiedDataSerializable, Versioned {
         out.writeByte(iterationType.getId());
         out.writeObject(aggregator);
         out.writeObject(projection);
-        if (out.getVersion().isGreaterOrEqual(Versions.V4_2)) {
-            out.writeObject(partitionIdSet);
-        }
+        out.writeObject(partitionIdSet);
     }
 
     @Override
@@ -157,9 +153,7 @@ public class Query implements IdentifiedDataSerializable, Versioned {
         this.iterationType = IterationType.getById(in.readByte());
         this.aggregator = in.readObject();
         this.projection = in.readObject();
-        if (in.getVersion().isGreaterOrEqual(Versions.V4_2)) {
-            this.partitionIdSet = in.readObject();
-        }
+        this.partitionIdSet = in.readObject();
     }
 
     public static final class QueryBuilder {

hazelcast-sql/src/main/java/com/hazelcast/sql/impl/schema/view/View.java
@@ -16,7 +16,6 @@
 
 package com.hazelcast.sql.impl.schema.view;
 
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.serialization.impl.SerializationUtil;
 import com.hazelcast.jet.sql.impl.parse.SqlCreateView;
 import com.hazelcast.nio.ObjectDataInput;
@@ -77,9 +76,6 @@ public class View implements Versioned, SqlCatalogObject {
     public void writeData(ObjectDataOutput out) throws IOException {
         out.writeString(name);
         out.writeString(query);
-        if (out.getVersion().isLessThan(Versions.V5_2)) {
-            out.writeBoolean(false);
-        }
         SerializationUtil.writeList(viewColumnNames, out);
         SerializationUtil.writeList(viewColumnTypes, out);
     }
@@ -88,9 +84,6 @@ public class View implements Versioned, SqlCatalogObject {
     public void readData(ObjectDataInput in) throws IOException {
         name = in.readString();
         query = in.readString();
-        if (in.getVersion().isLessThan(Versions.V5_2)) {
-            in.readBoolean();
-        }
         viewColumnNames = SerializationUtil.readList(in);
         viewColumnTypes = SerializationUtil.readList(in);
     }

hazelcast/src/main/java/com/hazelcast/config/IndexConfig.java
@@ -16,7 +16,6 @@
 
 package com.hazelcast.config;
 
-import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.internal.config.ConfigDataSerializerHook;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
@@ -257,11 +256,7 @@ public class IndexConfig implements IdentifiedDataSerializable, Versioned {
         out.writeInt(type.getId());
         writeNullableList(attributes, out);
         out.writeObject(bitmapIndexOptions);
-
-        // RU_COMPAT 5.1
-        if (out.getVersion().isGreaterOrEqual(Versions.V5_2)) {
-            out.writeObject(bTreeIndexConfig);
-        }
+        out.writeObject(bTreeIndexConfig);
     }
 
     @Override
@@ -270,11 +265,7 @@ public class IndexConfig implements IdentifiedDataSerializable, Versioned {
         type = IndexType.getById(in.readInt());
         attributes = readNullableList(in);
         bitmapIndexOptions = in.readObject();
-
-        // RU_COMPAT 5.1
-        if (in.getVersion().isGreaterOrEqual(Versions.V5_2)) {
-            bTreeIndexConfig = in.readObject();
-        }
+        bTreeIndexConfig = in.readObject();
     }
 
     @Override

commit cd601cd17dd60f61716440b59dd44cf801a893de

Restore implements Versioned where required (#25266)
Cleanup of rolling-upgrade code paths in #24595 is the cause of
compatibility regressions due to eagerly removing Versioned interface.
Proper code cleanup for rolling-upgrade conditional serde requires that
the `Versioned` interface is still implemented one version after the
conditional serde code is removed, to ensure that major.minor version
bytes are still written to the network output and previous-version serde
will find the proper `in.getVersion` to deserialize correctly.

Some fixes of issue caused by #24595 are included in #25263

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/ClusterStateTransactionLogRecord.java
@@ -26,6 +26,7 @@ import com.hazelcast.internal.util.Preconditions;
 import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.operationservice.Operation;
 import com.hazelcast.transaction.impl.TargetAwareTransactionLogRecord;
 
@@ -38,7 +39,8 @@ import java.util.UUID;
  * @see ClusterState
  * @see Cluster#changeClusterState(ClusterState, com.hazelcast.transaction.TransactionOptions)
  */
-public class ClusterStateTransactionLogRecord implements TargetAwareTransactionLogRecord {
+// RU_COMPAT_5_3 "implements Versioned" can be removed in 5.5
+public class ClusterStateTransactionLogRecord implements TargetAwareTransactionLogRecord, Versioned {
 
     ClusterStateChange stateChange;
     Address initiator;

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/operations/HeartbeatOp.java
@@ -24,6 +24,7 @@ import com.hazelcast.internal.cluster.impl.MembersViewMetadata;
 import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.impl.Versioned;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -31,7 +32,8 @@ import java.util.HashSet;
 import java.util.UUID;
 
 /** A heartbeat sent from one cluster member to another. The sent timestamp is the cluster clock time of the sending member */
-public final class HeartbeatOp extends AbstractClusterOperation {
+// RU_COMPAT_5_3 "implements Versioned" can be removed in 5.5
+public final class HeartbeatOp extends AbstractClusterOperation implements Versioned {
 
     private MembersViewMetadata senderMembersViewMetadata;
     private UUID targetUuid;

hazelcast/src/main/java/com/hazelcast/internal/cluster/impl/operations/LockClusterStateOp.java
@@ -27,6 +27,7 @@ import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
+import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.exception.TargetNotMemberException;
 import com.hazelcast.spi.impl.AllowedDuringPassiveState;
 import com.hazelcast.spi.impl.operationservice.ExceptionAction;
@@ -37,8 +38,9 @@ import com.hazelcast.transaction.TransactionException;
 import java.io.IOException;
 import java.util.UUID;
 
+// RU_COMPAT_5_3 "implements Versioned" can be removed in 5.5
 public class LockClusterStateOp  extends Operation implements AllowedDuringPassiveState, UrgentSystemOperation,
-        IdentifiedDataSerializable {
+        IdentifiedDataSerializable, Versioned {
 
     private ClusterStateChange stateChange;
     private Address initiator;

hazelcast/src/main/java/com/hazelcast/map/impl/operation/PutAllOperation.java
@@ -27,6 +27,7 @@ import com.hazelcast.map.impl.operation.steps.engine.Step;
 import com.hazelcast.map.impl.record.Record;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.operationservice.BackupAwareOperation;
 import com.hazelcast.spi.impl.operationservice.MutatingOperation;
 import com.hazelcast.spi.impl.operationservice.Operation;
@@ -49,9 +50,10 @@ import static com.hazelcast.map.impl.record.Record.UNSET;
  * Used to reduce the number of remote invocations
  * of an {@link IMap#putAll(Map)} or {@link IMap#setAll(Map)} call.
  */
+// RU_COMPAT_5_3 "implements Versioned" can be removed in 5.5
 public class PutAllOperation extends MapOperation
         implements PartitionAwareOperation, BackupAwareOperation,
-        MutatingOperation {
+        MutatingOperation, Versioned {
 
     private transient int currentIndex;
     private MapEntries mapEntries;

hazelcast/src/main/java/com/hazelcast/map/impl/operation/PutAllPartitionAwareOperationFactory.java
@@ -21,6 +21,7 @@ import com.hazelcast.map.impl.MapDataSerializerHook;
 import com.hazelcast.map.impl.MapEntries;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.operationservice.Operation;
 import com.hazelcast.spi.impl.operationservice.impl.operations.PartitionAwareOperationFactory;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -34,7 +35,8 @@ import java.util.Map;
  * <p>
  * Used to reduce the number of remote invocations of an {@link IMap#putAll(Map)} or {@link IMap#setAll(Map)} call.
  */
-public class PutAllPartitionAwareOperationFactory extends PartitionAwareOperationFactory {
+// RU_COMPAT_5_3 "implements Versioned" can be removed in 5.5
+public class PutAllPartitionAwareOperationFactory extends PartitionAwareOperationFactory implements Versioned {
 
     protected String name;
     protected MapEntries[] mapEntries;

hazelcast/src/main/java/com/hazelcast/map/impl/query/Query.java
@@ -24,6 +24,7 @@ import com.hazelcast.map.impl.MapDataSerializerHook;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
+import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.projection.Projection;
 import com.hazelcast.query.PagingPredicate;
 import com.hazelcast.query.Predicate;
@@ -36,7 +37,8 @@ import static com.hazelcast.internal.util.Preconditions.checkNotNull;
 /**
  * Object representing a Query together with all possible co-variants: like a predicate, iterationType, etc.
  */
-public class Query implements IdentifiedDataSerializable {
+// RU_COMPAT_5_3 "implements Versioned" can be removed in 5.5
+public class Query implements IdentifiedDataSerializable, Versioned {
 
     private String mapName;
     private Predicate predicate;