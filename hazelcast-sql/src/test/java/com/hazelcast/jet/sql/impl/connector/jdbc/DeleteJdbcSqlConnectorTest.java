43fad3c92d307b66a2d4649164ba87413cf2ea53
Ediz Civan <ediz.civan@hazelcast.com>
2023-08-23 09:06:37 +0300

    removed unused import

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/jdbc/DeleteJdbcSqlConnectorTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/jdbc/DeleteJdbcSqlConnectorTest.java
@@ -21,8 +21,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.util.Lists.newArrayList;
 
 public class DeleteJdbcSqlConnectorTest extends JdbcSqlTestSupport {
