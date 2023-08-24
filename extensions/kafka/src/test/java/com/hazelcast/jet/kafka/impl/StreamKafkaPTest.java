8022abd13786ec12e6ce796ff1b29d05b8f4a08e
Orçun Çolak <orcun.colak@hazelcast.com>
2023-08-24 13:59:48 +0300

    Change the test to send second batch of messages synchronously [HZ-2779]

--- extensions/kafka/src/test/java/com/hazelcast/jet/kafka/impl/StreamKafkaPTest.java
+++ extensions/kafka/src/test/java/com/hazelcast/jet/kafka/impl/StreamKafkaPTest.java
@@ -324,6 +324,7 @@ public class StreamKafkaPTest extends SimpleTestInClusterSupport {
             Properties kafkaProperties
     ) {
         String sinkListName = randomName();
+        // Send messages synchronously
         for (int i = 0; i < messageCount; i++) {
                 kafkaTestSupport.produceSync(topic1Name, i, String.valueOf(i));
         }
@@ -336,10 +337,14 @@ public class StreamKafkaPTest extends SimpleTestInClusterSupport {
         long oldExecutionId = assertJobRunningEventually(instance(), job, null);
         assertTrueEventually(() -> assertEquals(expectedCountBeforeRestart, instance().getList(sinkListName).size()));
 
+        // This will create a new KafkaConsumer, and consumer will resume
+        // according to value of "auto.offset.reset" configuration
+        // In this test, the value is set to "earliest", so it will start from the beginning
         job.restart();
 
+        // Send twice as many messages synchronously
         for (int i = messageCount; i < messageCount * 2; i++) {
-            kafkaTestSupport.produce(topic1Name, i, String.valueOf(i));
+            kafkaTestSupport.produceSync(topic1Name, i, String.valueOf(i));
         }
         assertJobRunningEventually(instance(), job, oldExecutionId);
         assertTrueEventually(() -> assertEquals(expectedCountAfterRestart, instance().getList(sinkListName).size()));
