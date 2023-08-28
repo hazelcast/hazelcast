992aae3cca01bee630d61594222f2fce270367f3
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-08-28 14:34:33 +0200

    Cleanup

--- extensions/mongodb/src/test/resources/runDocker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#  -v ./localhost-key.pem:/data/ssl/key.pem \
-#  -v ./localhost-cert.pem:/data/ssl/cert.pem \
-docker run -it --rm --name tls-mongo \
-  -v $(pwd)/certs/localhost.pem:/data/ssl/key.pem \
-  -v $(pwd)/tlsMongo.yaml:/etc/mongo/mongod.conf \
-  mongo:7 --config /etc/mongo/mongod.conf
