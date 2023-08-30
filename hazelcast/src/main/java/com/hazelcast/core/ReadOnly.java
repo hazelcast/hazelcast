fa61e99a0a1b120e2b38afd5644f943bce833f6e
Jack <jack.green@hazelcast.com>
2023-08-30 16:26:15 +0100

    Javadoc

--- hazelcast/src/main/java/com/hazelcast/core/ReadOnly.java
+++ hazelcast/src/main/java/com/hazelcast/core/ReadOnly.java
@@ -20,15 +20,15 @@ import com.hazelcast.map.EntryProcessor;
 import com.hazelcast.map.IMap;
 
 /**
- * Allows notifying Hazelcast that the processing unit implementing this interface will not do any modifications.
- * This marker interface allows optimizing the processing to gain more performance.
- *
+ * Allows notifying Hazelcast that the processing unit implementing this interface will not do any modifications. This marker
+ * interface allows optimizing the processing to gain more performance.
+ * <p>
  * If the processing processing unit implementing this interface does a modification an exception will be thrown.
- *
+ * <p>
  * Currently supported in:
  * <ul>
  * <li>{@link EntryProcessor} passed to {@link IMap#executeOnKey(Object, EntryProcessor)}</li>
- * <li>{@link EntryProcessor} passed to {@link IMap#submitToKey(Object, EntryProcessor)} </li>
+ * <li>{@link EntryProcessor} passed to {@link IMap#submitToKey(Object, EntryProcessor)}</li>
  * </ul>
  *
  * @see Offloadable
