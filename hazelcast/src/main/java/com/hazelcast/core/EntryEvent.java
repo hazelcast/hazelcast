eaca13a1cbdbb2e537bb6f422737159eb543a4b0
Jack <jack.green@hazelcast.com>
2023-08-30 17:36:57 +0100

    Refactor

--- hazelcast/src/main/java/com/hazelcast/core/EntryEvent.java
+++ hazelcast/src/main/java/com/hazelcast/core/EntryEvent.java
@@ -85,10 +85,7 @@ public class EntryEvent<K, V> extends AbstractIMapEvent {
      * @throws IllegalArgumentException if source is {@code null}
      */
     public EntryEvent(Object source, Member member, int eventType, K key, V oldValue, V value, V mergingValue) {
-        super(source, member, eventType);
-        this.key = key;
-        this.oldValue = oldValue;
-        this.value = value;
+        this(source, member, eventType, key, oldValue, value);
         this.mergingValue = mergingValue;
     }
 
