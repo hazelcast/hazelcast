b5eab4cbf8aa05a8540e03cb34e0fbdaf778e1be
Jack <jack.green@hazelcast.com>
2023-08-31 17:31:59 +0100

    Update test

--- hazelcast/src/test/java/com/hazelcast/map/listener/EntryUpdatedListenerTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/listener/EntryUpdatedListenerTest.java
@@ -17,21 +17,24 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(HazelcastParallelClassRunner.class)
+// TODO Why is this a slow test? Why does it take 10 seconds?
 @Category({SlowTest.class, ParallelJVMTest.class})
 public class EntryUpdatedListenerTest extends HazelcastTestSupport {
     /**
      * <a href="https://hazelcast.atlassian.net/browse/HZ-2837">HZ-2837 - Field level mutation being taken by listener as old
-     * value but not being considered by interceptor - Strange Behavior</a>
+     * value but not being considered by interceptor - Strange Behaviour</a>
      */
     @Test
     public void testOldValues() throws InterruptedException, ExecutionException {
         final HazelcastInstance instance = createHazelcastInstanceFactory().newHazelcastInstance();
 
-        final CompletableFuture<Object> entryListenerOldValue = new CompletableFuture<>();
-        final CompletableFuture<Object> interceptorOldValue = new CompletableFuture<>();
-
         final IMap<Object, AtomicInteger> map = instance.getMap(randomMapName());
 
+        final CompletableFuture<Object> entryListenerOldValue = new CompletableFuture<>();
+        map.addLocalEntryListener(
+                (EntryUpdatedListener<Object, AtomicInteger>) event -> entryListenerOldValue.complete(event.getOldValue()));
+
+        final CompletableFuture<Object> interceptorOldValue = new CompletableFuture<>();
         map.addInterceptor(new MapInterceptor() {
             private static final long serialVersionUID = 1L;
 
@@ -67,19 +70,19 @@ public class EntryUpdatedListenerTest extends HazelcastTestSupport {
             }
         });
 
-        map.addLocalEntryListener(
-                (EntryUpdatedListener<Object, AtomicInteger>) event -> entryListenerOldValue.complete(event.getOldValue()));
-
         final Object key = Void.TYPE;
-
         final AtomicInteger initial = new AtomicInteger(1);
+
         map.set(key, initial);
 
         map.executeOnKey(key, entry -> {
-            // Mutate the value in the map directly
+            // Mutate the value in the map directly - expectation is that this will not be visible to anyone -
+            // https://hazelcast.atlassian.net/browse/HZ-2837?focusedCommentId=82402
             entry.getValue().set(Integer.MAX_VALUE);
-            // Then write the new value back
+
+            // Also, update the map entry with a new value
             entry.setValue(new AtomicInteger(2));
+
             return null;
         });
 
@@ -88,7 +91,7 @@ public class EntryUpdatedListenerTest extends HazelcastTestSupport {
                 initial, entryListenerOldValue.get());
 
         assertEqualsStringFormat(
-                "nitial value provided (%s) does not match old value observed by MapInterceptor.interceptPut (%s)",
-                entryListenerOldValue.get(), initial);
+                "Initial value provided (%s) does not match old value observed by MapInterceptor.interceptPut (%s)", initial,
+                interceptorOldValue.get());
     }
 }
