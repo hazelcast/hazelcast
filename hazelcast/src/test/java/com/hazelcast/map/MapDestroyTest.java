c8050ba2e70b96c2471455d188fcd612c678a8bb
Krzysztof Jamróz <krzysztof.jamroz@hazelcast.com>
2023-09-01 16:01:35 +0200

    Add tests for IMap destruction with index

--- hazelcast/src/test/java/com/hazelcast/map/MapDestroyTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/MapDestroyTest.java
@@ -17,6 +17,8 @@
 package com.hazelcast.map;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.IndexConfig;
+import com.hazelcast.config.IndexType;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.impl.Node;
 import com.hazelcast.jet.impl.JobRepository;
@@ -26,26 +28,42 @@ import com.hazelcast.map.impl.PartitionContainer;
 import com.hazelcast.spi.impl.NodeEngineImpl;
 import com.hazelcast.spi.properties.ClusterProperty;
 import com.hazelcast.test.AssertTask;
-import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.HazelcastParallelParametersRunnerFactory;
+import com.hazelcast.test.HazelcastParametrizedRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.TestHazelcastInstanceFactory;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
+import com.hazelcast.test.annotation.SlowTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.hazelcast.test.Accessors.getNode;
 import static com.hazelcast.test.Accessors.getNodeEngineImpl;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(HazelcastParallelClassRunner.class)
+
+@RunWith(HazelcastParametrizedRunner.class)
+@Parameterized.UseParametersRunnerFactory(HazelcastParallelParametersRunnerFactory.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class MapDestroyTest extends HazelcastTestSupport {
+    @Parameterized.Parameters(name = "sortedIndex:{0}")
+    public static Collection<Object[]> data() {
+        return asList(new Object[][]{
+                {null}, {true}, {false}
+        });
+    }
+
+    @Parameterized.Parameter
+    public Boolean sortedIndex;
 
     private HazelcastInstance instance1;
     private HazelcastInstance instance2;
@@ -67,15 +85,33 @@ public class MapDestroyTest extends HazelcastTestSupport {
 
     @Test
     public void destroyAllReplicasIncludingBackups() {
+        createFillAndDestroyMap();
+
+        assertAllPartitionContainersAreEmptyEventually(instance1);
+        assertAllPartitionContainersAreEmptyEventually(instance2);
+    }
+
+    @Test
+    @Category(SlowTest.class)
+    public void destroyRepeatedly() {
+        for (int rep = 0; rep < 1_000; ++rep) {
+            createFillAndDestroyMap();
+        }
+
+        assertAllPartitionContainersAreEmptyEventually(instance1);
+        assertAllPartitionContainersAreEmptyEventually(instance2);
+    }
+
+    protected void createFillAndDestroyMap() {
         IMap<Integer, Integer> map = instance1.getMap(randomMapName());
+        if (sortedIndex != null) {
+            map.addIndex(new IndexConfig(sortedIndex ? IndexType.SORTED : IndexType.HASH, "this").setName("idx"));
+        }
         for (int i = 0; i < 1000; i++) {
             map.put(i, i);
         }
 
         map.destroy();
-
-        assertAllPartitionContainersAreEmptyEventually(instance1);
-        assertAllPartitionContainersAreEmptyEventually(instance2);
     }
 
     private void assertAllPartitionContainersAreEmptyEventually(final HazelcastInstance instance) {
