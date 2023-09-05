a40242f0ef219b2e13b8ff76541951cc50fc8fa0
Orçun Çolak <90305879+orcunc@users.noreply.github.com>
2023-09-05 08:37:47 +0300

    Remove IAtomicLongMocks and dead code [HZ-3053] (#25361)

--- hazelcast/src/test/java/com/hazelcast/mock/MockUtil.java
+++ hazelcast/src/test/java/com/hazelcast/mock/MockUtil.java
@@ -16,60 +16,13 @@
 
 package com.hazelcast.mock;
 
-import org.mockito.AdditionalAnswers;
-import org.mockito.MockSettings;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-
-import java.lang.reflect.Method;
-
-import static org.mockito.Mockito.CALLS_REAL_METHODS;
-import static org.mockito.Mockito.withSettings;
-
 public class MockUtil {
-
-    @SuppressWarnings("unchecked")
-    /** Delegate calls to another object **/
-    public static <T> Answer<T> delegateTo(Object delegate) {
-        return (Answer<T>) new DelegatingAnswer(delegate);
-    }
-
-    /**
-     * Creates a Mockito spy which is Serializable
-     **/
-    public static <T> T serializableSpy(Class<T> clazz, T instance) {
-        MockSettings settings = withSettings().spiedInstance(instance).serializable().defaultAnswer(CALLS_REAL_METHODS);
-        return Mockito.mock(clazz, settings);
-    }
 
     public static void closeMocks(AutoCloseable closeable) {
         try {
             closeable.close();
         } catch (Exception ex) {
             throw new IllegalStateException("Failed to close Mockito mocks", ex);
-        }
-    }
-
-    /**
-     * Mockito Answer that delegates invocations to another object. Similar to
-     * {@link AdditionalAnswers#delegatesTo(Object)} but also supports objects
-     * of different Class.
-     **/
-    static class DelegatingAnswer implements Answer<Object> {
-
-        private Object delegated;
-
-        DelegatingAnswer(Object delegated) {
-            this.delegated = delegated;
-        }
-
-        @Override
-        public Object answer(InvocationOnMock inv) throws Throwable {
-            Method m = inv.getMethod();
-            Method rm = delegated.getClass().getMethod(m.getName(),
-                    m.getParameterTypes());
-            return rm.invoke(delegated, inv.getArguments());
         }
     }
 }
