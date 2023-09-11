b0536531682cbfe772465b408a1f7697f1f845aa
Jack <jack.green@hazelcast.com>
2023-09-11 13:21:47 +0100

    Ensure assertion failures fail the build Consolidate duplicate code between tests Use existing `assertGreaterThanOrEqualTo` method Annotate existing failing test as `NightlyTest`

--- hazelcast/src/test/java/com/hazelcast/internal/jmx/LocalStatsDelegateTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/jmx/LocalStatsDelegateTest.java
@@ -16,168 +16,133 @@
 
 package com.hazelcast.internal.jmx;
 
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
 import com.hazelcast.internal.jmx.suppliers.LocalMapStatsSupplier;
 import com.hazelcast.internal.jmx.suppliers.StatsSupplier;
+import com.hazelcast.internal.util.UuidUtil;
+import com.hazelcast.map.IMap;
 import com.hazelcast.map.LocalMapStats;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.NightlyTest;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
-import com.hazelcast.internal.util.UuidUtil;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class LocalStatsDelegateTest extends HazelcastTestSupport {
 
     private AtomicBoolean done;
-    private HazelcastInstance hz;
 
     @Before
     public void setUp() {
-        hz = createHazelcastInstance();
         done = new AtomicBoolean(false);
     }
 
     @Test
-    public void testMapStats_Created_OnlyOnce_InGivenInternal() throws InterruptedException {
-        final IMap<Object, Object> trial = hz.getMap("trial");
-
-        final StatsSupplier<LocalMapStats> statsSupplier = new LocalMapStatsSupplier(trial);
-        final LocalStatsDelegate localStatsDelegate = new LocalStatsDelegate<LocalMapStats>(statsSupplier, 60);
-
-        Thread mapStatsThread1 = new Thread(new MapStatsThread(localStatsDelegate, done, 100));
-        Thread mapStatsThread2 = new Thread(new MapStatsThread(localStatsDelegate, done, 90));
-
-        mapStatsThread1.start();
-        mapStatsThread2.start();
-
-        Thread mapPutThread = new Thread(new MapPutThread(trial, done));
-        mapPutThread.start();
-
-        sleepSeconds(30);
-
-        done.set(true);
-
-        mapStatsThread1.join();
-        mapStatsThread2.join();
-        mapPutThread.join();
+    @Category(NightlyTest.class)
+    public void testMapStats_Created_OnlyOnce_InGivenInternal() throws Exception {
+        test(false, 60, 100, 90, 30);
     }
 
     /**
-     * In this test, map entry count is incrementing steadily. Thus, we expect OwnedEntryCount
-     * to increase as well. Since the interval value for generating new map stats is 5 seconds,
-     * we assert for greater or equal.
+     * In this test, map entry count is incrementing steadily. Thus, we expect OwnedEntryCount to increase as well. Since the
+     * interval value for generating new map stats is 5 seconds, we assert for greater or equal.
      */
     @Test
     @Category(NightlyTest.class)
-    public void stressTest() throws InterruptedException {
-        final IMap<Object, Object> trial = hz.getMap("trial");
+    public void stressTest() throws Exception {
+        test(true, 5, 100, 80, 60);
+    }
+
+    private void test(final boolean stress, final int intervalSec, final int mapStatsThread1SleepMs,
+            final int mapStatsThread2SleepMs, final int sleepSeconds) throws Exception {
+        final IMap<Object, Object> trial = createHazelcastInstance().getMap("trial");
 
         final StatsSupplier<LocalMapStats> statsSupplier = new LocalMapStatsSupplier(trial);
-        final LocalStatsDelegate localStatsDelegate = new LocalStatsDelegate<LocalMapStats>(statsSupplier, 5);
+        final LocalStatsDelegate<LocalMapStats> localStatsDelegate = new LocalStatsDelegate<>(statsSupplier, intervalSec);
 
-        Thread mapStatsThread1 = new Thread(new MapStatsThread(localStatsDelegate, done, true, 100));
-        Thread mapStatsThread2 = new Thread(new MapStatsThread(localStatsDelegate, done, true, 80));
+        final ExecutorService executor = Executors.newFixedThreadPool(3);
 
-        mapStatsThread1.start();
-        mapStatsThread2.start();
+        final Future<Void> mapStatsThread1 = executor
+                .submit(new MapStatsThread(localStatsDelegate, stress, mapStatsThread1SleepMs));
+        final Future<Void> mapStatsThread2 = executor
+                .submit(new MapStatsThread(localStatsDelegate, stress, mapStatsThread2SleepMs));
 
-        Thread mapPutThread = new Thread(new MapPutThread(trial, done));
+        final Thread mapPutThread = new Thread(new MapPutThread(trial));
         mapPutThread.start();
 
-        sleepSeconds(60);
+        sleepSeconds(sleepSeconds);
 
         done.set(true);
 
-        mapStatsThread1.join();
-        mapStatsThread2.join();
+        mapStatsThread1.get();
+        mapStatsThread2.get();
         mapPutThread.join();
     }
 
-    private void assertGreaterThanOrEqualTo(long leftHand, long rightHand) {
-        boolean correct = false;
-
-        if (leftHand >= rightHand || leftHand == rightHand) {
-            correct = true;
-        }
-        assertTrue(leftHand + " is less than " + rightHand, correct);
-    }
-
     private class MapPutThread implements Runnable {
-        private IMap map;
-        private AtomicBoolean done;
+        private final IMap<Object, Object> map;
 
-        MapPutThread(IMap map, AtomicBoolean done) {
+        MapPutThread(final IMap<Object, Object> map) {
             this.map = map;
-            this.done = done;
         }
 
         @Override
         public void run() {
             while (!done.get()) {
-                String randomString = UuidUtil.newUnsecureUuidString();
+                final String randomString = UuidUtil.newUnsecureUuidString();
                 map.put(randomString, randomString);
                 sleepMillis(10);
             }
         }
     }
 
-    private class MapStatsThread implements Runnable {
+    private class MapStatsThread implements Callable<Void> {
 
-        private LocalStatsDelegate localStatsDelegate;
-        private AtomicBoolean done;
+        private final LocalStatsDelegate<LocalMapStats> localStatsDelegate;
 
-        private boolean stress;
+        private final boolean stress;
 
-        private int sleepMs;
+        private final int sleepMs;
 
-        MapStatsThread(LocalStatsDelegate localMapStatsDelegate, AtomicBoolean done, int sleepMs) {
-            this.localStatsDelegate = localMapStatsDelegate;
-            this.done = done;
-            this.sleepMs = sleepMs;
-        }
-
-        MapStatsThread(LocalStatsDelegate localMapStatsDelegate, AtomicBoolean done, boolean stress, int sleepMs) {
-            this.localStatsDelegate = localMapStatsDelegate;
-            this.done = done;
+        MapStatsThread(final LocalStatsDelegate<LocalMapStats> localStatsDelegate, final boolean stress, final int sleepMs) {
+            this.localStatsDelegate = localStatsDelegate;
             this.stress = stress;
             this.sleepMs = sleepMs;
         }
 
         @Override
-        public void run() {
-            LocalMapStats localMapStats = (LocalMapStats) localStatsDelegate.getLocalStats();
+        public Void call() {
+            LocalMapStats localMapStats = localStatsDelegate.getLocalStats();
             long previous = localMapStats.getOwnedEntryCount();
             long current = previous;
 
             while (!done.get()) {
                 if (stress) {
-                    assertGreaterThanOrEqualTo(current, previous);
+                    assertGreaterOrEquals("owned entry count", current, previous);
                 } else {
                     assertEquals(current, previous);
                 }
                 previous = current;
-                localMapStats = (LocalMapStats) localStatsDelegate.getLocalStats();
+                localMapStats = localStatsDelegate.getLocalStats();
                 current = localMapStats.getOwnedEntryCount();
-                try {
-                    TimeUnit.MILLISECONDS.sleep(sleepMs);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                sleepMillis(sleepMs);
             }
+
+            return null;
         }
     }
 }
