2c43ac7e7f51c3612174ef76e389bc92e4ec23d7
Jack <jack.green@hazelcast.com>
2023-09-25 21:53:01 +0100

    Add diag method

--- hazelcast/src/test/java/com/hazelcast/internal/metrics/impl/ProbeUtilsTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/metrics/impl/ProbeUtilsTest.java
@@ -20,9 +20,18 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
+import com.hazelcast.internal.metrics.Probe;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.HazelcastTestSupport;
 import com.hazelcast.test.annotation.QuickTest;
+import com.hazelcast.test.starter.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
@@ -31,5 +40,22 @@ public class ProbeUtilsTest extends HazelcastTestSupport {
     @Test
     public void testPrivateConstructor() {
         assertUtilityConstructor(ProbeUtils.class);
+    }
+
+    /** Prints the types that a @Probe has been attached to in the codebase, and where they can be found */
+    public static void main(final String[] args) {
+        final Multimap<String, String> probeTypeToClassesFoundIn = TreeMultimap.create();
+
+        ReflectionUtils.getReflectionsForTestPackage("com.hazelcast").getSubTypesOf(Object.class).forEach(clazz -> {
+            final Stream<Class<?>> methodTypes = Arrays.stream(clazz.getDeclaredMethods())
+                    .filter(method -> method.getAnnotation(Probe.class) != null).map(Method::getReturnType);
+            final Stream<Class<?>> fieldTypes = Arrays.stream(clazz.getDeclaredFields())
+                    .filter(field -> field.getAnnotation(Probe.class) != null).map(Field::getType);
+
+            Stream.concat(methodTypes, fieldTypes)
+                    .forEach(probeType -> probeTypeToClassesFoundIn.put(probeType.getName(), clazz.getName()));
+        });
+
+        probeTypeToClassesFoundIn.asMap().entrySet().forEach(System.out::println);
     }
 }
