00afd561a87ed46a8edfbd562ddab7e59cf7541a
Jack <jack.green@hazelcast.com>
2023-09-25 13:36:14 +0100

    Checkstyle

--- hazelcast/src/main/java/com/hazelcast/internal/metrics/impl/ProbeUtils.java
+++ hazelcast/src/main/java/com/hazelcast/internal/metrics/impl/ProbeUtils.java
@@ -24,7 +24,7 @@ import java.util.Collection;
 class ProbeUtils {
     private ProbeUtils() {
     }
-    
+
     static void flatten(final Class<?> clazz, final Collection<Class<?>> result) {
         result.add(clazz);
 
