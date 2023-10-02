9cf6fa972aeb1c1ce9c63e2c495e9314375247da
James Holgate <130981049+JamesHazelcast@users.noreply.github.com>
2023-10-02 13:48:49 +0100

    Checkstyle in DemoteResponseOperation

--- hazelcast/src/main/java/com/hazelcast/internal/partition/operation/DemoteResponseOperation.java
+++ hazelcast/src/main/java/com/hazelcast/internal/partition/operation/DemoteResponseOperation.java
@@ -26,7 +26,6 @@ import com.hazelcast.internal.util.UUIDSerializationUtil;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
-import com.hazelcast.nio.serialization.impl.Versioned;
 import com.hazelcast.spi.impl.NodeEngine;
 
 import java.io.IOException;
