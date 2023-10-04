dea65d4b4474bd2616557068f25908b311566750
Jack <jack.green@hazelcast.com>
2023-10-04 09:30:46 +0100

    Checkstyle

--- hazelcast/src/main/java/com/hazelcast/internal/metrics/impl/MethodHandleProbe.java
+++ hazelcast/src/main/java/com/hazelcast/internal/metrics/impl/MethodHandleProbe.java
@@ -147,7 +147,7 @@ abstract class MethodHandleProbe<S> implements ProbeFunction {
             }
         }
 
-        @SuppressWarnings({"checkstyle:npathcomplexity"})
+        @SuppressWarnings({"checkstyle:cyclomaticcomplexity"})
         @Override
         public long get(S source) throws Exception {
             switch (type) {
