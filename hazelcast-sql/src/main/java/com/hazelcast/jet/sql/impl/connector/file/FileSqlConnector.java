e959d445969b656814c0501adcf052ba025150c4
František Hartman <frant.hartm@gmail.com>
2023-10-04 13:12:18 +0200

    Improve permission checks in File connector [HZ-2991] (#25348)
    `SqlConnector#resolveAndValidateFields` may try to resolve field names
    from metadata and a sample. This is not covered by any permission check.
    
    Added `SqlConnector#permissionsForResolve` that returns permissions
    required to run the `resolveAndValidateFields`. Propagation of
    SqlSecurityContext was required for the actuall permission check.
    
    Added permission to ProcessorSupplier returned by
    ReadFilesP.MetaSupplier (previously this allowed constructing unsecured
    DAG):
    
    
    https://github.com/hazelcast/hazelcast/blob/ee7e96933748283a01428e306e1b886b06b8a5ba/hazelcast/src/main/java/com/hazelcast/jet/impl/connector/ReadFilesP.java#L179-L191
    
    EE PR https://github.com/hazelcast/hazelcast-enterprise/pull/6421 (adds
    security tests)

--- hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/connector/file/FileSqlConnector.java
+++ hazelcast-sql/src/main/java/com/hazelcast/jet/sql/impl/connector/file/FileSqlConnector.java
@@ -31,10 +31,14 @@ import com.hazelcast.sql.impl.schema.Table;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.security.Permission;
 import java.util.List;
 import java.util.Map;
 
 import static com.hazelcast.jet.core.Edge.between;
+import static com.hazelcast.security.permission.ActionConstants.ACTION_READ;
+import static com.hazelcast.security.permission.ConnectorPermission.file;
+import static java.util.Collections.singletonList;
 
 public class FileSqlConnector implements SqlConnector {
 
@@ -81,6 +85,12 @@ public class FileSqlConnector implements SqlConnector {
             @Nonnull List<MappingField> userFields
     ) {
         return METADATA_RESOLVERS.resolveAndValidateFields(userFields, options);
+    }
+
+    @Nonnull
+    @Override
+    public List<Permission> permissionsForResolve(SqlExternalResource resource, NodeEngine nodeEngine) {
+        return singletonList(file(resource.options().get(OPTION_PATH), ACTION_READ));
     }
 
     @Nonnull
