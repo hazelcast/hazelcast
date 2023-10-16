ffc5f96d6aab690b8354ec770422a106f36ab0bc
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-10-16 16:03:15 +0200

    Remove Mongo permissions [HZ-3428] (#25701)

--- extensions/mongodb/src/main/java/com/hazelcast/jet/mongodb/impl/DbCheckingPMetaSupplier.java
+++ extensions/mongodb/src/main/java/com/hazelcast/jet/mongodb/impl/DbCheckingPMetaSupplier.java
@@ -32,7 +32,6 @@ import com.mongodb.client.MongoDatabase;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.security.Permission;
 import java.util.List;
 import java.util.function.Function;
 
@@ -46,7 +45,6 @@ import static com.hazelcast.jet.mongodb.impl.MongoUtilities.checkDatabaseExists;
  */
 public class DbCheckingPMetaSupplier implements ProcessorMetaSupplier {
 
-    private final Permission requiredPermission;
     private final boolean shouldCheckOnEachCall;
     private ProcessorMetaSupplier standardForceOnePMS;
     private final boolean forceTotalParallelismOne;
@@ -60,8 +58,7 @@ public class DbCheckingPMetaSupplier implements ProcessorMetaSupplier {
     /**
      * Creates a new instance of this meta supplier.
      */
-    public DbCheckingPMetaSupplier(@Nullable Permission requiredPermission,
-                                   boolean shouldCheckOnEachCall,
+    public DbCheckingPMetaSupplier(boolean shouldCheckOnEachCall,
                                    boolean forceTotalParallelismOne,
                                    @Nullable String databaseName,
                                    @Nullable String collectionName,
@@ -70,7 +67,6 @@ public class DbCheckingPMetaSupplier implements ProcessorMetaSupplier {
                                    @Nonnull ProcessorSupplier processorSupplier,
                                    int preferredLocalParallelism
     ) {
-        this.requiredPermission = requiredPermission;
         this.shouldCheckOnEachCall = shouldCheckOnEachCall;
         this.forceTotalParallelismOne = forceTotalParallelismOne;
         this.databaseName = databaseName;
@@ -84,12 +80,6 @@ public class DbCheckingPMetaSupplier implements ProcessorMetaSupplier {
     @Override
     public int preferredLocalParallelism() {
         return preferredLocalParallelism;
-    }
-
-    @Nullable
-    @Override
-    public Permission getRequiredPermission() {
-        return requiredPermission;
     }
 
     @Override
