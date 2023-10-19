936a21a474f430b52613e1b87354036fb4036082
Gareth Johnston <gareth.johnston@hazelcast.com>
2023-10-19 10:04:09 +0100

    checkstyle

--- hazelcast/src/test/java/com/hazelcast/spi/impl/operationservice/OperationAccessorTest.java
+++ hazelcast/src/test/java/com/hazelcast/spi/impl/operationservice/OperationAccessorTest.java
@@ -34,7 +34,9 @@ import static com.hazelcast.spi.impl.operationservice.OperationAccessor.setCallI
 import static com.hazelcast.spi.impl.operationservice.OperationAccessor.setCallTimeout;
 import static com.hazelcast.spi.impl.operationservice.OperationAccessor.setCallerAddress;
 import static com.hazelcast.spi.impl.operationservice.OperationAccessor.setInvocationTime;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
