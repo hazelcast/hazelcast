582afc00a9225cd6f998b6925a83a1ca24446973
Burak Gök <burak.gok@hazelcast.com>
2023-11-03 04:12:31 +0300

    Improve information_schema test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/kafka/SqlAvroTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/connector/kafka/SqlAvroTest.java
@@ -25,8 +25,6 @@ import com.hazelcast.jet.datamodel.Tuple4;
 import com.hazelcast.jet.kafka.HazelcastKafkaAvroDeserializer;
 import com.hazelcast.jet.kafka.HazelcastKafkaAvroSerializer;
 import com.hazelcast.jet.sql.impl.connector.test.TestAllTypesSqlConnector;
-import com.hazelcast.sql.SqlResult;
-import com.hazelcast.sql.SqlRow;
 import com.hazelcast.sql.impl.type.QueryDataType;
 import com.hazelcast.sql.impl.type.QueryDataTypeFamily;
 import com.hazelcast.test.HazelcastParametrizedRunner;
@@ -60,6 +58,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -82,10 +81,8 @@ import static java.util.Arrays.copyOfRange;
 import static java.util.Collections.emptyMap;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assume.assumeFalse;
 import static org.junit.Assume.assumeTrue;
 
@@ -402,37 +399,6 @@ public class SqlAvroTest extends KafkaSqlTestSupport {
 
         assertThatThrownBy(() -> insertRecord(6, Long.MAX_VALUE)).hasMessageContaining(
                 "Not in union [\"null\",\"boolean\",\"int\"]: " + Long.MAX_VALUE + " (Long) (field=info)");
-    }
-
-    @Test
-    public void when_createAvroMapping_then_keyAndValueAreAvailableInInfoSchema() {
-        assumeFalse(useSchemaRegistry);
-        String name = createRandomTopic();
-        kafkaMapping(name, ID_SCHEMA, NAME_SCHEMA)
-                .fields("id BIGINT EXTERNAL NAME \"__key.id\"", "name VARCHAR")
-                .create();
-
-        String query = "SELECT * FROM information_schema.mappings";
-        try (SqlResult result = sqlService.execute(query)) {
-            SqlRow row = result.stream().findFirst().orElseThrow();
-            assertNotNull(row);
-
-            // Destructure the row: it must contain "catalog", "schema", "name", "external name", "type", "options"
-            assertEquals("hazelcast", row.getObject(0));
-            assertEquals("public", row.getObject(1));
-            assertEquals(name, row.getObject(2));
-            assertEquals('"' + name + '"', row.getObject(3));
-            assertEquals(KafkaSqlConnector.TYPE_NAME, row.getObject(4));
-
-            String options = row.getObject(5);
-            assertNotNull(options);
-            // Ensure that the options contain avro schema key and value
-            assertThat(options)
-                    .contains("keyFormat\":\"avro\"")
-                    .contains("valueFormat\":\"avro\"")
-                    .contains("keyAvroSchema\":")
-                    .contains("valueAvroSchema\":");
-        }
     }
 
     @Test
@@ -753,6 +719,26 @@ public class SqlAvroTest extends KafkaSqlTestSupport {
                 .create();
 
         insertAndAssertRecord("Alice", "Bob");
+    }
+
+    @Test
+    public void test_keyAndValueSchemaAvailableInInformationSchema() {
+        assumeFalse(useSchemaRegistry);
+        String name = createRandomTopic();
+        kafkaMapping(name, ID_SCHEMA, NAME_SCHEMA)
+                .fields("id BIGINT EXTERNAL NAME \"__key.id\"",
+                        "name VARCHAR")
+                .create();
+
+        String options = sqlService.execute("SELECT mapping_options FROM information_schema.mappings")
+                .iterator().next().getObject(0);
+        BiConsumer<String, Schema> assertContainsSchema = (option, schema) -> assertContains(
+                options,
+                String.format("\"%s\":\"%s\"", option, schema.toString().replace("\"", "\\\""))
+        );
+
+        assertContainsSchema.accept(OPTION_KEY_AVRO_SCHEMA, keySchema);
+        assertContainsSchema.accept(OPTION_VALUE_AVRO_SCHEMA, valueSchema);
     }
 
     @Test
