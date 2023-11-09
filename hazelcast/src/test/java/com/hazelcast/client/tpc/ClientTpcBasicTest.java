c590ef7b0445ab325950d7a92d186aece26ae8f2
Serkan Özel <serkan.ozel@hazelcast.com>
2023-11-09 12:57:25 +0300

    Fix the test

--- hazelcast/src/test/java/com/hazelcast/client/tpc/ClientTpcBasicTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/tpc/ClientTpcBasicTest.java
@@ -71,7 +71,7 @@ public class ClientTpcBasicTest extends ClientTestSupport {
     }
 
     @Test
-    public void testClientConnectsToSingleTpcPort_byDefault() {
+    public void testClientConnectsToAllTpcPorts_byDefault() {
         Config config = getMemberConfig();
         Hazelcast.newHazelcastInstance(config);
         Hazelcast.newHazelcastInstance(config);
@@ -89,7 +89,7 @@ public class ClientTpcBasicTest extends ClientTestSupport {
 
                 Channel[] tpcChannels = clientConnection.getTpcChannels();
                 assertNotNull(tpcChannels);
-                assertEquals(1, tpcChannels.length);
+                assertEquals(config.getTpcConfig().getEventloopCount(), tpcChannels.length);
 
                 for (Channel channel : tpcChannels) {
                     assertNotNull(channel);
