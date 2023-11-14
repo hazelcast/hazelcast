c7f7fc004a324db1f8c5adfb71568ee0b04d91a4
Burak Gök <burak.gok@hazelcast.com>
2023-11-14 20:07:41 +0300

    Update ReadOptimizedLruCacheTest

--- hazelcast-sql/src/test/java/com/hazelcast/sql/impl/client/ReadOptimizedLruCacheTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/sql/impl/client/ReadOptimizedLruCacheTest.java
@@ -19,40 +19,96 @@ package com.hazelcast.sql.impl.client;
 import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.util.Map.Entry;
+
 import static com.hazelcast.test.HazelcastTestSupport.sleepMillis;
-import static org.junit.Assert.assertEquals;
+import static java.util.Comparator.comparingLong;
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class ReadOptimizedLruCacheTest {
+    private ReadOptimizedLruCache<Integer, Integer> cache;
+
+    @Before
+    public void init() {
+        cache = new ReadOptimizedLruCache<>(2, 3);
+        put(1, 1);
+        put(2, 2);
+        put(3, 3);
+    }
 
     @Test
-    public void test() {
-        ReadOptimizedLruCache<Integer, Integer> c = new ReadOptimizedLruCache<>(2, 3);
-        c.put(42, 42);
-        sleepMillis(20); // a little sleep to ensure the lastUsed timestamps are different even on a very imprecise clock
-        c.put(43, 43);
-        sleepMillis(20);
-        c.put(44, 44);
-        assertEquals(3, c.cache.size());
-        sleepMillis(20);
-        c.put(45, 45);
-        assertEquals(2, c.cache.size());
-        assertEquals(Integer.valueOf(44), c.cache.get(44).value);
-        assertEquals(Integer.valueOf(45), c.cache.get(45).value);
+    public void test_cleanup() {
+        assertEntries(entry(1, 1), entry(2, 2), entry(3, 3));
+
+        put(4, 4);
+        assertEntries(entry(3, 3), entry(4, 4));
+    }
+
+    @Test
+    public void test_changeAccessTime() {
+        cache.get(1); // Access makes it the most recently used one
+        assertEntries(entry(2, 2), entry(3, 3), entry(1, 1));
+
+        put(4, 4);
+        assertEntries(entry(1, 1), entry(4, 4));
+    }
+
+    @Test
+    public void test_computeIfAbsent() {
+        cache.computeIfAbsent(1, k -> 1); // Access makes it the most recently used one
+        assertEntries(entry(2, 2), entry(3, 3), entry(1, 1));
+
+        cache.computeIfAbsent(4, k -> 4);
+        assertEntries(entry(1, 1), entry(4, 4));
 
         sleepMillis(20);
-        c.put(46, 46);
-        sleepMillis(20);
-        c.get(44); // access makes the value the least recently used one
+        cache.computeIfAbsent(2, k -> 2);
+        assertEntries(entry(1, 1), entry(4, 4), entry(2, 2));
+    }
 
-        c.put(47, 47);
-        assertEquals(2, c.cache.size());
-        assertEquals(Integer.valueOf(44), c.cache.get(44).value);
-        assertEquals(Integer.valueOf(47), c.cache.get(47).value);
+    @Test
+    public void when_nullValueIsProvided_then_reject() {
+        assertThatThrownBy(() -> put(1, null)).hasMessage("Null values are disallowed");
+        assertEntries(entry(1, 1), entry(2, 2), entry(3, 3));
+
+        assertThatCode(() -> cache.computeIfAbsent(1, k -> null)).doesNotThrowAnyException();
+        assertEntries(entry(2, 2), entry(3, 3), entry(1, 1));
+
+        assertThatThrownBy(() -> cache.computeIfAbsent(4, k -> null)).hasMessage("Null values are disallowed");
+        assertEntries(entry(2, 2), entry(3, 3), entry(1, 1));
+    }
+
+    @Test
+    public void test_remove_getOrDefault() {
+        cache.remove(2);
+        assertEntries(entry(1, 1), entry(3, 3));
+
+        assertThat(cache.get(2)).isNull();
+        assertThat(cache.getOrDefault(2, 2)).isEqualTo(2);
+        assertThat(cache.getOrDefault(4, 4)).isEqualTo(4);
+    }
+
+    private void put(Integer key, Integer value) {
+        // A little sleep to ensure the timestamps are different even on a very imprecise clock
+        sleepMillis(20);
+        cache.put(key, value);
+    }
+
+    @SafeVarargs
+    private void assertEntries(Entry<Integer, Integer>... entries) {
+        assertThat(cache.cache.entrySet().stream()
+                .sorted(comparingLong(e -> e.getValue().timestamp))
+                .map(e -> entry(e.getKey(), e.getValue().value))
+        ).containsExactly(entries);
     }
 }
