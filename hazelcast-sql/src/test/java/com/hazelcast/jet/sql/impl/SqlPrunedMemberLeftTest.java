ec2325c99164762ab5b19bd70b8b63658039b693
Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>
2023-11-27 17:10:32 +0200

    Enhance test; run pruned member shutdown before second page is fetched
    Signed-off-by: Sasha Syrotenko <oleksandr.syrotenko@hazelcast.com>

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/SqlPrunedMemberLeftTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/SqlPrunedMemberLeftTest.java
@@ -35,114 +35,90 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static com.hazelcast.jet.sql.SqlTestSupport.createMapping;
 import static com.hazelcast.jet.sql.impl.SqlEndToEndTestSupport.calculateExpectedPartitions;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class SqlPrunedMemberLeftTest extends JetTestSupport {
-    private static final int MAP_ENTRIES = 1_000;
+    // Page size is 4096, so we'll fetch 2 pages.
+    private static final int MAP_ENTRIES = 5_000;
+    private static final int CLUSTER_SIZE = 5;
+    private static final int CONSTANT = 2;
     private static final String MAP_NAME = "map";
 
-    private NodeEngine nodeEngine1;
-
-    private HazelcastInstance hz1;
-    private HazelcastInstance hz2;
-    private HazelcastInstance hz3;
-    private HazelcastInstance hz4;
-    private HazelcastInstance hz5;
-
+    private HazelcastInstance hz[];
+    private HazelcastInstance client;
+    private NodeEngine masterNodeEngine;
     private Map<Address, HazelcastInstance> addrToInstanceMap;
 
     @Before
     public void setUp() throws Exception {
-        hz1 = createHazelcastInstance(regularInstanceConfig());
-        nodeEngine1 = getNodeEngineImpl(hz1);
-        hz2 = createHazelcastInstance(regularInstanceConfig());
-        hz3 = createHazelcastInstance(regularInstanceConfig());
-        hz4 = createHazelcastInstance(regularInstanceConfig());
-        hz5 = createHazelcastInstance(regularInstanceConfig());
-
-        addrToInstanceMap = new HashMap<>();
-        addrToInstanceMap.put(hz1.getCluster().getLocalMember().getAddress(), hz1);
-        addrToInstanceMap.put(hz2.getCluster().getLocalMember().getAddress(), hz2);
-        addrToInstanceMap.put(hz3.getCluster().getLocalMember().getAddress(), hz3);
-        addrToInstanceMap.put(hz4.getCluster().getLocalMember().getAddress(), hz4);
-        addrToInstanceMap.put(hz5.getCluster().getLocalMember().getAddress(), hz5);
+        hz = new HazelcastInstance[CLUSTER_SIZE];
+        addrToInstanceMap = new LinkedHashMap<>();
+        for (int i = 0; i < CLUSTER_SIZE; ++i) {
+            hz[i] = createHazelcastInstance(regularInstanceConfig());
+            addrToInstanceMap.put(hz[i].getCluster().getLocalMember().getAddress(), hz[i]);
+        }
+        client = createHazelcastClient();
+        masterNodeEngine = getNodeEngineImpl(hz[0]);
 
         prepareMap();
-        createMapping(hz5, MAP_NAME, Pojo.class, Integer.class);
+        createMapping(client, MAP_NAME, Pojo.class, Integer.class);
     }
 
     @Test
     // https://hazelcast.atlassian.net/browse/HZ-2992
     public void test() {
         // Given
-        final int c = 2; // constant, assuming hz3 will be coordinator.
+        String query = "SELECT this FROM " + MAP_NAME + " WHERE f0 = " + CONSTANT;
 
-        Set<Integer> expected = IntStream.range(c * 10, (c + 1) * 10).boxed().collect(Collectors.toSet());
-        String query = "SELECT this FROM " + MAP_NAME + " WHERE f0 = " + c;
-
-        var instances = findPrunedAndNonPrunedInstances(c);
-        HazelcastInstance prunedInstanceToTerminate = instances.f0();
-        HazelcastInstance chosenCoordinator = instances.f1();
+        HazelcastInstance prunedInstanceToTerminate = findPrunedInstance();
 
         int cnt = 0;
 
-        for (SqlRow row : chosenCoordinator.getSql().execute(query)) {
-            // Read one entry and shutdown chosen member.
-            if (cnt == 1) {
+        for (SqlRow row : client.getSql().execute(query)) {
+            // Shutdown chosen member before we read anything.
+            if (cnt == 0) {
                 prunedInstanceToTerminate.shutdown();
             }
-            Integer value = row.getObject(0);
-            assertThat(expected).contains(value);
-            expected.remove(value);
-
             ++cnt;
         }
-        // Since we're filling map at {i/10, i, i} -> {i} way, we're expecting 10 entries.
-        assertTrue(expected.isEmpty());
-        assertEquals(10, cnt);
+        assertEquals(MAP_ENTRIES, cnt);
     }
 
-    private Tuple2<HazelcastInstance, HazelcastInstance> findPrunedAndNonPrunedInstances(int constant) {
+    private HazelcastInstance findPrunedInstance() {
         Tuple2<Set<Address>, Set<Integer>> expectedPartitionsAndParticipants =
-                calculateExpectedPartitions(nodeEngine1, getPartitionAssignment(hz1), false, 1, constant);
+                calculateExpectedPartitions(masterNodeEngine, getPartitionAssignment(hz[0]), false, 1, CONSTANT);
 
         assertNotNull(expectedPartitionsAndParticipants.f0());
         assertEquals(1, expectedPartitionsAndParticipants.f0().size());
 
         Set<Address> addresses = new HashSet<>(addrToInstanceMap.keySet());
         assertTrue(addresses.removeAll(expectedPartitionsAndParticipants.f0()));
-        assertEquals(4, addresses.size());
+        assertEquals(CLUSTER_SIZE - 1, addresses.size());
 
-        var prunedInstance = addrToInstanceMap.get(addresses.iterator().next());
-        var nonPrunedInstance = addrToInstanceMap.get(expectedPartitionsAndParticipants.f0().iterator().next());
-        assertNotSame(prunedInstance, nonPrunedInstance);
-        return Tuple2.tuple2(prunedInstance, nonPrunedInstance);
+        return addrToInstanceMap.get(addresses.iterator().next());
     }
 
     private void prepareMap() {
-        hz1.getConfig().addMapConfig(
-                new MapConfig(MAP_NAME)
-                        .setPartitioningAttributeConfigs(
-                                Collections.singletonList(new PartitioningAttributeConfig("f0"))));
+        MapConfig mapConfig = new MapConfig(MAP_NAME)
+                .setPartitioningAttributeConfigs(
+                        Collections.singletonList(new PartitioningAttributeConfig("f0")));
 
-        IMap<Pojo, Integer> map = hz1.getMap(MAP_NAME);
+        hz[0].getConfig().addMapConfig(mapConfig);
+
+        IMap<Pojo, Integer> map = hz[0].getMap(MAP_NAME);
         for (int i = 0; i < MAP_ENTRIES; ++i) {
-            map.put(new Pojo(i / 10, i, i), i);
+            map.put(new Pojo(CONSTANT, i, i), i);
         }
     }
 }
