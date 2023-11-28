e81d33f57b3bca9d3a6f9a08fe8f3e68e3d0428b
Krzysztof Jamróz <krzysztof.jamroz@hazelcast.com>
2023-11-28 15:07:00 +0100

    Add pruned ANALYZE test

--- hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/SqlPrunedMemberLeftTest.java
+++ hazelcast-sql/src/test/java/com/hazelcast/jet/sql/impl/SqlPrunedMemberLeftTest.java
@@ -21,36 +21,46 @@ import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.PartitioningAttributeConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.jet.core.JetTestSupport;
+import com.hazelcast.jet.core.JobStatus;
 import com.hazelcast.jet.datamodel.Tuple2;
 import com.hazelcast.jet.sql.impl.misc.Pojo;
 import com.hazelcast.map.IMap;
 import com.hazelcast.spi.impl.NodeEngine;
 import com.hazelcast.sql.SqlRow;
-import com.hazelcast.test.HazelcastSerialClassRunner;
+import com.hazelcast.sql.SqlStatement;
+import com.hazelcast.test.HazelcastParametrizedRunner;
+import com.hazelcast.test.HazelcastSerialParametersRunnerFactory;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import static com.hazelcast.jet.sql.SqlTestSupport.createMapping;
 import static com.hazelcast.jet.sql.impl.SqlEndToEndTestSupport.calculateExpectedPartitions;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(HazelcastSerialClassRunner.class)
+@RunWith(HazelcastParametrizedRunner.class)
+@Parameterized.UseParametersRunnerFactory(HazelcastSerialParametersRunnerFactory.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class SqlPrunedMemberLeftTest extends JetTestSupport {
     // Page size is 4096, so we'll fetch 2 pages.
-    private static final int MAP_ENTRIES = 5_000;
+    private static final int MAP_ENTRIES = (int) (SqlStatement.DEFAULT_CURSOR_BUFFER_SIZE * 1.5);
     private static final int CLUSTER_SIZE = 5;
     private static final int CONSTANT = 2;
     private static final String MAP_NAME = "map";
@@ -58,6 +68,25 @@ public class SqlPrunedMemberLeftTest extends JetTestSupport {
     private HazelcastInstance[] hz;
     private NodeEngine masterNodeEngine;
     private Map<Address, HazelcastInstance> addrToInstanceMap;
+
+    @Parameterized.Parameters(name = "analyze={0}, graceful={1}")
+    public static Collection<Object[]> parameters() {
+        List<Object[]> params = new ArrayList<>();
+        for (boolean analyze : asList(false, true)) {
+            for (boolean graceful : asList(false, true)) {
+                params.add(new Object[] { analyze, graceful });
+            }
+        }
+
+        return params;
+    }
+
+    @Parameterized.Parameter
+    public boolean analyze;
+
+    @Parameterized.Parameter(1)
+    public boolean graceful;
+
 
     @Before
     public void setUp() throws Exception {
@@ -77,7 +106,7 @@ public class SqlPrunedMemberLeftTest extends JetTestSupport {
     // https://hazelcast.atlassian.net/browse/HZ-2992
     public void test_queryDoesNotTerminateWhenPrunedMemberLeavesCluster() {
         // Given
-        String query = "SELECT * FROM " + MAP_NAME + " WHERE f0 = " + CONSTANT;
+        String query = (analyze ? "ANALYZE " : "") + "SELECT * FROM " + MAP_NAME + " WHERE f0 = " + CONSTANT;
         HazelcastInstance prunedInstanceToTerminate = findPrunedAndNonPrunedInstance().f0();
         HazelcastInstance nonPrunedInstance = findPrunedAndNonPrunedInstance().f1();
         int cnt = 0;
@@ -86,7 +115,16 @@ public class SqlPrunedMemberLeftTest extends JetTestSupport {
             // Shutdown chosen member after we read 1st entry.
             if (cnt == 0) {
                 // When
-                prunedInstanceToTerminate.getLifecycleService().terminate();
+                if (graceful) {
+                    prunedInstanceToTerminate.shutdown();
+                } else {
+                    prunedInstanceToTerminate.getLifecycleService().terminate();
+                }
+
+                // Then
+                assertThat(nonPrunedInstance.getJet().getJobs())
+                        .as("The query should be still running")
+                        .anyMatch(j -> j.getStatus() == JobStatus.RUNNING);
             }
             ++cnt;
         }
