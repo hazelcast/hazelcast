0e70dbb9fcf8ac85997eeea325f6aa5f45caab5a
Jack Green <jack.green@hazelcast.com>
2023-12-05 10:36:24 +0000

    Update comment

--- extensions/python/src/main/resources/jet_to_python_init.sh
+++ extensions/python/src/main/resources/jet_to_python_init.sh
@@ -7,9 +7,8 @@ python3 -m venv jet_to_python_env --system-site-packages
 source jet_to_python_env/bin/activate
 
 flock --exclusive ~/.jet-pip.lock python3 -m pip install --upgrade pip
-# grpcio versions do not always align with Java releases, so ensure versions are broadly (rather than exactly) in sync
-# https://github.com/hazelcast/hazelcast/pull/26097#issuecomment-1840378216
-flock --exclusive ~/.jet-pip.lock python3 -m pip install 'protobuf==@python.protobuf.version@' 'grpcio~=@grpc.version@'
+# grpcio versions do not always align with Java releases, so ensure compatible (rather than identical) version is used
+flock --exclusive ~/.jet-pip.lock python3 -m pip install protobuf==@python.protobuf.version@ grpcio~=@grpc.version@
 
 [[ -f requirements.txt ]] && python3 -m pip install -r requirements.txt
 source ./jet_to_python_params.sh
