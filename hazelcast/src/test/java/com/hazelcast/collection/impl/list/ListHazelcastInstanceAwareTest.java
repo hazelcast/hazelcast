6b33d659722f9f8f7e8ffeec7141548c9102d075
Jack <jack.green@hazelcast.com>
2024-01-03 15:45:37 +0100

    Add test coverage for `HazelcastInstanceAware` registration when listener configured directly on datastructure (#234)
    Import of https://github.com/hazelcast/hazelcast/pull/26078
    
    **Original PR description:**
    
    Test coverage for https://github.com/hazelcast/hazelcast/issues/26062
    
    Imported changes:
    
      - 2772751423aaeb7f351c6650151499b465e4609c Add collection fix
      - c62c34171e2d996a5c0b80a9129208366b0d6ce8 Share method
      - d160a10a58eb9ae4216aa9e9136d4ee28cc58427 Fix in queue, too
      - 6e15c3f4d712013a83f733bb61408c82c48771c1 Refactor
      - dc0758d4014ef1a2fe8172b86d07d41611409727 Fix multimaps
      - a2dec63a81f05b0e5d8fc57478c2d6e279a889b8 Topic fix
      - 8e4b9aef8fbb505f912bdffc1e933db5f853ff36 Checkstyle
      - d02c2bd5e27ee00c2d87a4465d00a3920aa3dcfa Revert `src/main` changes
      - ef48db468957b587d166e8b10ec72972ba28dcda Revert `src/main` changes
    
    Closes hazelcast/hazelcast#26078
    
    Co-authored-by: Jack <jack.green@hazelcast.com>
    GitOrigin-RevId: 88967a28c118d8e3d736e558331d0bd3d619f148

---
+++ hazelcast/src/test/java/com/hazelcast/collection/impl/list/ListHazelcastInstanceAwareTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.collection.impl.list;
+
+import com.hazelcast.collection.ItemEvent;
+import com.hazelcast.collection.ItemListener;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class ListHazelcastInstanceAwareTest extends HazelcastTestSupport {
+    private CompletableFuture<HazelcastInstance> hazelcastInstanceFuture;
+
+    @Before
+    public void setUp() {
+        hazelcastInstanceFuture = new CompletableFuture<>();
+    }
+
+    @Test
+    public void testInjected() {
+        createHazelcastInstance().getList(randomName()).addItemListener(new MyItemListener<>(), false);
+
+        assertTrueEventually(() -> assertNotNull(hazelcastInstanceFuture.get()));
+    }
+
+    private class MyItemListener<V> implements ItemListener<V>, HazelcastInstanceAware {
+        @Override
+        public void itemAdded(ItemEvent<V> item) {
+        }
+
+        @Override
+        public void itemRemoved(ItemEvent<V> item) {
+        }
+
+        @Override
+        public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+            if (hazelcastInstance != null) {
+                hazelcastInstanceFuture.complete(hazelcastInstance);
+            }
+        }
+    }
+}
---
+++ hazelcast/src/test/java/com/hazelcast/multimap/MultiMapHazelcastInstanceAwareTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.multimap;
+
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.core.EntryListener;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+import com.hazelcast.map.MapEvent;
+import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.HazelcastTestSupport;
+import com.hazelcast.test.annotation.ParallelJVMTest;
+import com.hazelcast.test.annotation.QuickTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(HazelcastParallelClassRunner.class)
+@Category({QuickTest.class, ParallelJVMTest.class})
+public class MultiMapHazelcastInstanceAwareTest extends HazelcastTestSupport {
+    private CompletableFuture<HazelcastInstance> hazelcastInstanceFuture;
+
+    @Before
+    public void setUp() {
+        hazelcastInstanceFuture = new CompletableFuture<>();
+    }
+
+    @Test
+    public void testInjected() {
+        createHazelcastInstance().getMultiMap(randomMapName()).addEntryListener(new MyEntryListener<>(), false);
+
+        assertTrueEventually(() -> assertNotNull(hazelcastInstanceFuture.get()));
+    }
+
+    private class MyEntryListener<K, V> implements EntryListener<K, V>, HazelcastInstanceAware {
+        @Override
+        public void entryAdded(EntryEvent<K, V> event) {
+        }
+
+        @Override
+        public void entryUpdated(EntryEvent<K, V> event) {
+        }
+
+        @Override
+        public void entryRemoved(EntryEvent<K, V> event) {
+        }
+
+        @Override
+        public void entryEvicted(EntryEvent<K, V> event) {
+        }
+
+        @Override
+        public void entryExpired(EntryEvent<K, V> event) {
+        }
+
+        @Override
+        public void mapCleared(MapEvent event) {
+        }
+
+        @Override
+        public void mapEvicted(MapEvent event) {
+        }
+
+        @Override
+        public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+            if (hazelcastInstance != null) {
+                hazelcastInstanceFuture.complete(hazelcastInstance);
+            }
+        }
+    }
+}
