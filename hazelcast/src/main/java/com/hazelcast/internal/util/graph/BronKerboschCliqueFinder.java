3033b03ff8f886a77c4e2c3828252cb9c06eb879
Jack Green <jack.green@hazelcast.com>
2024-01-10 13:22:58 +0000

    Make `BronKerboschCliqueFinder` copyright year consistent with rest of codebase (#362)
    Make the format of the copyright year consistent with the rest of the
    codebase.
    
    https://github.com/hazelcast/hazelcast-mono/pull/324#discussion_r1447151522
    
    GitOrigin-RevId: f34d71fe47ff1ce4e748d8f11a9d84e4864d726e

--- hazelcast/src/main/java/com/hazelcast/internal/util/graph/BronKerboschCliqueFinder.java
+++ hazelcast/src/main/java/com/hazelcast/internal/util/graph/BronKerboschCliqueFinder.java
@@ -1,6 +1,6 @@
 /*
 *  Original work Copyright (c) 2005-2021, by Ewgenij Proschak and Contributors.
- * Modified work Copyright (c) 2023, Hazelcast, Inc. All Rights Reserved.
+ * Modified work Copyright (c) 2020-2023, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
