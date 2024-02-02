2081cd5ae707aaf17d617dc452ae6dcb6ab44399
ihsan demir <ihsandemir@gmail.com>
2024-02-02 21:24:44 +0300

    Add the configurable Https SSL support for the REST API [API-2201] (#577)
    Added the configurable Https SSL support for the REST API.
    
    GitOrigin-RevId: cf45f6dfe884a74e9578b1dab1d6b7f12115c6c7

--- hazelcast/src/main/java/com/hazelcast/config/rest/RestConfig.java
+++ hazelcast/src/main/java/com/hazelcast/config/rest/RestConfig.java
@@ -18,6 +18,7 @@ package com.hazelcast.config.rest;
 
 import com.hazelcast.spi.annotation.Beta;
 
+import java.util.Arrays;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -28,6 +29,472 @@ import java.time.temporal.ChronoUnit;
  */
 @Beta
 public class RestConfig {
+    public static class Ssl {
+
+        private boolean enabled;
+
+        private ClientAuth clientAuth = ClientAuth.NONE;
+
+        private String[] ciphers;
+
+        private String[] enabledProtocols;
+
+        private String keyAlias;
+
+        private String keyPassword;
+
+        private String keyStore;
+
+        private String keyStorePassword;
+
+        private String keyStoreType;
+
+        private String keyStoreProvider;
+
+        private String trustStore;
+
+        private String trustStorePassword;
+
+        private String trustStoreType;
+
+        private String trustStoreProvider;
+
+        private String certificate;
+
+        private String certificatePrivateKey;
+
+        private String trustCertificate;
+
+        private String trustCertificatePrivateKey;
+
+        private String protocol = "TLS";
+
+        /**
+         * Return whether to enable SSL support.
+         *
+         * @return whether to enable SSL support
+         */
+        public boolean isEnabled() {
+            return this.enabled;
+        }
+
+        /**
+         * Set whether to enable SSL support.
+         *
+         * @param enabled whether to enable SSL support
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+
+        /**
+         * Return Whether client authentication is not wanted ("none"), wanted ("want") or
+         * needed ("need"). Requires a trust store.
+         *
+         * @return the {@link ClientAuth} to use
+         */
+        public ClientAuth getClientAuth() {
+            return this.clientAuth;
+        }
+
+        /**
+         * Set whether client authentication is not wanted ("none"), wanted ("want") or
+         * needed ("need"). Requires a trust store.
+         *
+         * @param clientAuth the authentication mode
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setClientAuth(ClientAuth clientAuth) {
+            this.clientAuth = clientAuth;
+            return this;
+        }
+
+        /**
+         * Return the supported SSL ciphers.
+         *
+         * @return the supported SSL ciphers
+         */
+        public String[] getCiphers() {
+            return this.ciphers;
+        }
+
+        /**
+         * Set the supported SSL ciphers.
+         *
+         * @param ciphers the supported SSL ciphers
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setCiphers(String[] ciphers) {
+            this.ciphers = ciphers;
+            return this;
+        }
+
+        /**
+         * Return the enabled SSL protocols.
+         *
+         * @return the enabled SSL protocols.
+         */
+        public String[] getEnabledProtocols() {
+            return this.enabledProtocols;
+        }
+
+        /**
+         * Set the enabled SSL protocols.
+         *
+         * @param enabledProtocols the enabled SSL protocols
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setEnabledProtocols(String[] enabledProtocols) {
+            this.enabledProtocols = enabledProtocols;
+            return this;
+        }
+
+        /**
+         * Return the alias that identifies the key in the key store.
+         *
+         * @return the key alias
+         */
+        public String getKeyAlias() {
+            return this.keyAlias;
+        }
+
+        /**
+         * Set the alias that identifies the key in the key store.
+         *
+         * @param keyAlias the key alias
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setKeyAlias(String keyAlias) {
+            this.keyAlias = keyAlias;
+            return this;
+        }
+
+        /**
+         * Return the password used to access the key in the key store.
+         *
+         * @return the key password
+         */
+        public String getKeyPassword() {
+            return this.keyPassword;
+        }
+
+        /**
+         * Set the password used to access the key in the key store.
+         *
+         * @param keyPassword the key password
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setKeyPassword(String keyPassword) {
+            this.keyPassword = keyPassword;
+            return this;
+        }
+
+        /**
+         * Return the path to the key store that holds the SSL certificate (typically a jks
+         * file).
+         *
+         * @return the path to the key store
+         */
+        public String getKeyStore() {
+            return this.keyStore;
+        }
+
+        /**
+         * Set the path to the key store that holds the SSL certificate (typically a jks
+         * file).
+         *
+         * @param keyStore the path to the key store
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setKeyStore(String keyStore) {
+            this.keyStore = keyStore;
+            return this;
+        }
+
+        /**
+         * Return the password used to access the key store.
+         *
+         * @return the key store password
+         */
+        public String getKeyStorePassword() {
+            return this.keyStorePassword;
+        }
+
+        /**
+         * Set the password used to access the key store.
+         *
+         * @param keyStorePassword the key store password
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setKeyStorePassword(String keyStorePassword) {
+            this.keyStorePassword = keyStorePassword;
+            return this;
+        }
+
+        /**
+         * Return the type of the key store.
+         *
+         * @return the key store type
+         */
+        public String getKeyStoreType() {
+            return this.keyStoreType;
+        }
+
+        /**
+         * Set the type of the key store.
+         *
+         * @param keyStoreType the key store type. Can be JKS or PKCS12.
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setKeyStoreType(String keyStoreType) {
+            this.keyStoreType = keyStoreType;
+            return this;
+        }
+
+        /**
+         * Return the provider for the key store.
+         *
+         * @return the key store provider
+         */
+        public String getKeyStoreProvider() {
+            return this.keyStoreProvider;
+        }
+
+        public Ssl setKeyStoreProvider(String keyStoreProvider) {
+            this.keyStoreProvider = keyStoreProvider;
+            return this;
+        }
+
+        /**
+         * Return the trust store that holds SSL certificates.
+         *
+         * @return the trust store
+         */
+        public String getTrustStore() {
+            return this.trustStore;
+        }
+
+        /**
+         * Set the trust store that holds SSL certificates.
+         *
+         * @param trustStore the trust store
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setTrustStore(String trustStore) {
+            this.trustStore = trustStore;
+            return this;
+        }
+
+        /**
+         * Return the password used to access the trust store.
+         *
+         * @return the trust store password
+         */
+        public String getTrustStorePassword() {
+            return this.trustStorePassword;
+        }
+
+        /**
+         * Set the password used to access the trust store.
+         *
+         * @param trustStorePassword the trust store password
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setTrustStorePassword(String trustStorePassword) {
+            this.trustStorePassword = trustStorePassword;
+            return this;
+        }
+
+        /**
+         * Return the type of the trust store.
+         *
+         * @return the trust store type
+         */
+        public String getTrustStoreType() {
+            return this.trustStoreType;
+        }
+
+        /**
+         * Set the type of the trust store.
+         *
+         * @param trustStoreType the trust store type. Can be JKS or PKCS12.
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setTrustStoreType(String trustStoreType) {
+            this.trustStoreType = trustStoreType;
+            return this;
+        }
+
+        /**
+         * Return the provider for the trust store.
+         *
+         * @return the trust store provider
+         */
+        public String getTrustStoreProvider() {
+            return this.trustStoreProvider;
+        }
+
+
+        /**
+         * Set the provider for the trust store.
+         *
+         * @param trustStoreProvider the trust store provider
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setTrustStoreProvider(String trustStoreProvider) {
+            this.trustStoreProvider = trustStoreProvider;
+            return this;
+        }
+
+        /**
+         * Return the location of the certificate in PEM format.
+         *
+         * @return the certificate location
+         */
+        public String getCertificate() {
+            return this.certificate;
+        }
+
+        /**
+         * Set the location of the certificate in PEM format.
+         *
+         * @param certificate the certificate location
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setCertificate(String certificate) {
+            this.certificate = certificate;
+            return this;
+        }
+
+        /**
+         * Return the location of the private key for the certificate in PEM format.
+         *
+         * @return the location of the certificate private key
+         */
+        public String getCertificatePrivateKey() {
+            return this.certificatePrivateKey;
+        }
+
+        /**
+         * Set the location of the private key for the certificate in PEM format.
+         *
+         * @param certificatePrivateKey the location of the certificate private key
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setCertificatePrivateKey(String certificatePrivateKey) {
+            this.certificatePrivateKey = certificatePrivateKey;
+            return this;
+        }
+
+        /**
+         * Return the location of the trust certificate authority chain in PEM format.
+         *
+         * @return the location of the trust certificate
+         */
+        public String getTrustCertificate() {
+            return this.trustCertificate;
+        }
+
+        /**
+         * Set the location of the trust certificate authority chain in PEM format.
+         *
+         * @param trustCertificate the location of the trust certificate
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setTrustCertificate(String trustCertificate) {
+            this.trustCertificate = trustCertificate;
+            return this;
+        }
+
+        /**
+         * Return the location of the private key for the trust certificate in PEM format.
+         *
+         * @return the location of the trust certificate private key
+         */
+        public String getTrustCertificatePrivateKey() {
+            return this.trustCertificatePrivateKey;
+        }
+
+        /**
+         * Set the location of the private key for the trust certificate in PEM format.
+         *
+         * @param trustCertificatePrivateKey the location of the trust certificate private key
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setTrustCertificatePrivateKey(String trustCertificatePrivateKey) {
+            this.trustCertificatePrivateKey = trustCertificatePrivateKey;
+            return this;
+        }
+
+        /**
+         * Return the SSL protocol to use.
+         *
+         * @return the SSL protocol
+         */
+        public String getProtocol() {
+            return this.protocol;
+        }
+
+        /**
+         * Set the SSL protocol to use.
+         *
+         * @param protocol the SSL protocol
+         * @return the {@link Ssl} to use
+         */
+        public Ssl setProtocol(String protocol) {
+            this.protocol = protocol;
+            return this;
+        }
+
+        /**
+         * Client authentication types.
+         */
+        public enum ClientAuth {
+
+            /**
+             * Client authentication is not wanted.
+             */
+            NONE,
+
+            /**
+             * Client authentication is wanted but not mandatory.
+             */
+            WANT,
+
+            /**
+             * Client authentication is needed and mandatory.
+             */
+            NEED
+
+        }
+
+        @Override
+        public String toString() {
+            return "Ssl{"
+                    + "enabled=" + enabled
+                    + ", clientAuth=" + clientAuth
+                    + ", ciphers=" + Arrays.toString(ciphers)
+                    + ", enabledProtocols=" + Arrays.toString(enabledProtocols)
+                    + ", keyAlias='" + keyAlias + '\''
+                    + ", keyPassword='" + keyPassword + '\''
+                    + ", keyStore='" + keyStore + '\''
+                    + ", keyStorePassword='" + keyStorePassword + '\''
+                    + ", keyStoreType='" + keyStoreType + '\''
+                    + ", keyStoreProvider='" + keyStoreProvider + '\''
+                    + ", trustStore='" + trustStore + '\''
+                    + ", trustStorePassword='" + trustStorePassword + '\''
+                    + ", trustStoreType='" + trustStoreType + '\''
+                    + ", trustStoreProvider='" + trustStoreProvider + '\''
+                    + ", certificate='" + certificate + '\''
+                    + ", certificatePrivateKey='" + certificatePrivateKey + '\''
+                    + ", trustCertificate='" + trustCertificate + '\''
+                    + ", trustCertificatePrivateKey='" + trustCertificatePrivateKey + '\''
+                    + ", protocol='" + protocol + '\''
+                    + '}';
+        }
+    }
 
     private static final int DEFAULT_PORT = 8443;
     private static final int DEFAULT_DURATION_MINUTES = 15;
@@ -52,6 +519,11 @@ public class RestConfig {
      * Duration for a token to remain valid.
      */
     private Duration tokenValidityDuration = DEFAULT_DURATION;
+
+    /**
+     * SSL configuration.
+     */
+    private Ssl ssl = new Ssl();
 
     /**
      * Default constructor for RestConfig.
@@ -137,6 +609,26 @@ public class RestConfig {
     }
 
     /**
+     * Gets the SSL configuration.
+     *
+     * @return the SSL configuration.
+     */
+    public Ssl getSsl() {
+        return ssl;
+    }
+
+    /**
+     * Sets the SSL configuration.
+     *
+     * @param ssl the new SSL configuration.
+     * @return the updated RestConfig.
+     */
+    public RestConfig setSsl(Ssl ssl) {
+        this.ssl = ssl;
+        return this;
+    }
+
+    /**
      * Returns a string representation of the RestConfig.
      *
      * @return a string representation of the RestConfig.
@@ -144,6 +636,6 @@ public class RestConfig {
     @Override
     public String toString() {
         return "RestConfig{enabled=" + enabled + ", port=" + port + ", securityRealm='" + securityRealm + '\''
-                + ", tokenValidityDuration=" + tokenValidityDuration + '}';
+                + ", tokenValidityDuration=" + tokenValidityDuration + ", ssl=" + ssl + '}';
     }
 }
