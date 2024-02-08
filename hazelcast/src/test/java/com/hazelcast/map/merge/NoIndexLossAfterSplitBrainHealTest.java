dfc73c00f6e34c06a5fb008d99f2310f4610d7e9
Jack Green <jack.green@hazelcast.com>
2024-02-08 08:33:23 +0000

    Add extra logging to `NoIndexLossAfterSplitBrainHealTest` to try to catch flakey test root cause [HZ-3851] (#700)
    `com.hazelcast.map.merge.NoIndexLossAfterSplitBrainHealTest` is
    [flakey](https://github.com/hazelcast/hazelcast/issues/25725), but is so
    intermittent I am unable to reproduce locally more than once.
    
    I've updated the test to:
    - Log more information
    - Ensure a randomised, non-overlapping `MAP_NAME` is used
    - Added extra assertions in logical steps between:
       - Does the map have `N` entries
       - Is the size of the result of predicate `Y`, `N`
    
    This is to try to give a smaller problem space if/when if it comes up
    again.
    
    Fixes: [HZ-3851](https://hazelcast.atlassian.net/browse/HZ-3851)
    
    [HZ-3851]:
    https://hazelcast.atlassian.net/browse/HZ-3851?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: James Holgate <130981049+JamesHazelcast@users.noreply.github.com>
    GitOrigin-RevId: bab236709af253e05aa22e35bdee3f373ee5f8ee

--- hazelcast/src/test/java/com/hazelcast/map/merge/NoIndexLossAfterSplitBrainHealTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/merge/NoIndexLossAfterSplitBrainHealTest.java
@@ -26,44 +26,48 @@ import com.hazelcast.internal.util.ThreadLocalRandomProvider;
 import com.hazelcast.map.IMap;
 import com.hazelcast.query.Predicate;
 import com.hazelcast.query.Predicates;
+import com.hazelcast.test.ChangeLoggingRule;
 import com.hazelcast.test.HazelcastParallelParametersRunnerFactory;
 import com.hazelcast.test.HazelcastParametrizedRunner;
 import com.hazelcast.test.SplitBrainTestSupport;
 import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
+import org.junit.ClassRule;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
 @RunWith(HazelcastParametrizedRunner.class)
 @UseParametersRunnerFactory(HazelcastParallelParametersRunnerFactory.class)
 @Category({QuickTest.class, ParallelJVMTest.class})
 public class NoIndexLossAfterSplitBrainHealTest extends SplitBrainTestSupport {
-
     private static final int ENTRY_COUNT = 10000;
     private static final int[] BRAINS = new int[]{2, 3};
-    private static final String MAP_NAME = "test";
-    private static final Predicate VALUE_IS_NOT_EQUAL_X = Predicates.not(Predicates.equal("value", "X"));
-    private static final Predicate VALUE_IS_ONE_OF_A_B_C_D = Predicates.in("value", "A", "B", "C", "D");
-    private static final List<String> POSSIBLE_VALUES = Arrays.asList("A", "B", "C", "D");
+    private static final String MAP_NAME = randomMapName();
+    private static final String[] POSSIBLE_VALUES = new String[] {"A", "B", "C", "D"};
+    /** Logically equivalent for the input dataset */
+    private static final Collection<Predicate<Object, TestObject>> TEST_PREDICATES =
+            List.of(Predicates.alwaysTrue(), Predicates.not(Predicates.equal("value", "X")), Predicates.in("value", POSSIBLE_VALUES));
+
+    // Trying to investigate flakey test https://github.com/hazelcast/hazelcast/issues/25725
+    @ClassRule
+    public static ChangeLoggingRule changeLoggingRule = new ChangeLoggingRule("log4j2-debug-map.xml");
 
     @Parameterized.Parameter
     public InMemoryFormat inMemoryFormat;
 
     @Parameterized.Parameters(name = "format:{0}")
-    public static Collection<Object[]> parameters() {
-        return Arrays.asList(new Object[][]{
-                {InMemoryFormat.BINARY},
-                {InMemoryFormat.OBJECT},
-        });
+    public static Collection<InMemoryFormat> parameters() {
+        return List.of(InMemoryFormat.BINARY, InMemoryFormat.OBJECT);
     }
 
     @Override
@@ -80,58 +84,55 @@ public class NoIndexLossAfterSplitBrainHealTest extends SplitBrainTestSupport {
 
     @Override
     protected void onBeforeSplitBrainCreated(HazelcastInstance[] instances) {
+        IMap<Object, TestObject> testMap = instances[0].getMap(MAP_NAME);
+
         for (long key = 0; key < ENTRY_COUNT; key++) {
-            String randomValue = POSSIBLE_VALUES.get(ThreadLocalRandomProvider.get().nextInt(POSSIBLE_VALUES.size()));
-            instances[0].getMap(MAP_NAME).put(key, new TestObject(key, randomValue));
+            String randomValue = POSSIBLE_VALUES[ThreadLocalRandomProvider.get()
+                    .nextInt(POSSIBLE_VALUES.length)];
+            testMap.put(key, new TestObject(key, randomValue));
         }
 
-        for (HazelcastInstance instance : instances) {
-            IMap testMap = instance.getMap(MAP_NAME);
-
-            assertSizeEventually(ENTRY_COUNT, testMap);
-            assertEquals(ENTRY_COUNT, testMap.keySet(VALUE_IS_NOT_EQUAL_X).size());
-            assertEquals(ENTRY_COUNT, testMap.keySet(VALUE_IS_ONE_OF_A_B_C_D).size());
-        }
+        test(instances);
     }
 
     @Override
     protected void onAfterSplitBrainHealed(HazelcastInstance[] instances) {
+        test(instances);
+    }
+
+    private static void test(HazelcastInstance[] instances) {
         for (HazelcastInstance instance : instances) {
-            IMap testMap = instance.getMap(MAP_NAME);
+            IMap<Object, TestObject> testMap = instance.getMap(MAP_NAME);
+
+            // com.hazelcast.map.merge.NoIndexLossAfterSplitBrainHealTest.testSplitBrain [HZ-3851] #25725
+            // The predicates are returning unexpected values, so iteratively step up through the logic chain to see where
+            // exactly it goes wrong
 
             assertSizeEventually(ENTRY_COUNT, testMap);
-            assertEquals(ENTRY_COUNT, testMap.keySet(VALUE_IS_NOT_EQUAL_X).size());
-            assertEquals(ENTRY_COUNT, testMap.keySet(VALUE_IS_ONE_OF_A_B_C_D).size());
+
+            assertEquals("Map size has been modified mid-assertion", ENTRY_COUNT, testMap.size());
+            assertThat(testMap.values()).as("The size of \"values()\" and the maps' \"size()\" should be the same")
+                    .hasSize(ENTRY_COUNT);
+            assertThat(testMap.keySet()).as("The size of \"keySet()\" and the maps' \"size()\" should be the same")
+                    .hasSize(ENTRY_COUNT);
+
+            TEST_PREDICATES.forEach(predicate -> assertThat(testMap.keySet(predicate)).as(() -> {
+                Predicate<Object, TestObject> inversedPredicate = Predicates.not(predicate);
+                Collection<Object> inversedPredicateResult = testMap.keySet(inversedPredicate);
+
+                return MessageFormat.format(
+                        """
+                                Previous assertion established the size of testMap is {0}.
+                                It''s therefore impossible for the predicate "{1}" (which is functionally equivalent to "true") to return anything other than that same result.
+                                The inverse of this predicate ("{2}") (which is functionally equivalent to "false") returned {3} results ({4}).
+                                """,
+                        ENTRY_COUNT, predicate, inversedPredicate, inversedPredicateResult.size(), inversedPredicateResult);
+            })
+                    .hasSize(ENTRY_COUNT));
         }
     }
 
-    private static class TestObject implements Serializable {
-
-        private Long id;
-        private String value;
-
-        TestObject() {
-        }
-
-        TestObject(Long id, String value) {
-            this.id = id;
-            this.value = value;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
+    private record TestObject(Long id, String value) implements Serializable {
+        private static final long serialVersionUID = 1L;
     }
 }
