983976e7be4d07b03379ee2ae0e0e4f0fdca5b2f
Jack Green <jack.green@hazelcast.com>
2024-02-23 11:06:37 +0000

    Use platform-specific line separators in `ReflectionUtils#getStackTrace(Thread, int)` (#893)
    `ReflectionUtils#getStackTrace(Thread, int)` is using a
    platform-specific line separator (`\n`) which is causing a [test failure
    on Windows](https://github.com/hazelcast/hazelcast/issues/26259).
    
    Strictly speaking the test is wrong as this is the way the methods'
    always worked, but it makes more sense to be platform agnostic by
    abstracting the line separator.
    
    Fixes https://github.com/hazelcast/hazelcast/issues/26259
    
    GitOrigin-RevId: d079e38943c537d4e1c1edee7a19e50ef344be67

--- hazelcast/src/main/java/com/hazelcast/jet/impl/util/ReflectionUtils.java
+++ hazelcast/src/main/java/com/hazelcast/jet/impl/util/ReflectionUtils.java
@@ -17,6 +17,7 @@
 package com.hazelcast.jet.impl.util;
 
 import com.hazelcast.internal.nio.ClassLoaderUtil;
+import com.hazelcast.internal.util.StringUtil;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
@@ -405,7 +406,7 @@ public final class ReflectionUtils {
     public static String getStackTrace(Thread thread, int skip) {
         return Arrays.stream(thread.getStackTrace()).skip(skip)
                 .map(Object::toString)
-                .collect(Collectors.joining("\n\t"));
+                .collect(Collectors.joining(StringUtil.LINE_SEPARATOR + "\t"));
     }
 
     public static final class Resources {
