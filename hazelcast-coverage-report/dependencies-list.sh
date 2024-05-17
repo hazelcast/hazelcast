c6d308e58131a9430c391a45d09fc53a35fde4f6
Jack Green <jack.green@hazelcast.com>
2024-05-17 09:56:32 +0100

    Migrate Jenkins test scripts to use Maven wrapper [HZ-4660] (#1962)
    Jobs should use the Maven wrapper where possible, so that the version of
    Maven is consistent and managed.
    
    ---------
    
    Co-authored-by: Patryk Dadas <patryk.dadas@hazelcast.com>
    GitOrigin-RevId: ae5ee4aaa485562b90e007a03552f6d2ca474366

--- hazelcast-coverage-report/dependencies-list.sh
+++ hazelcast-coverage-report/dependencies-list.sh
@@ -2,4 +2,4 @@
 #### IMPORTANT NOTE:
 # Output of this command may differ depending on used JDK version - it prints only active modules (not all modules).
 # Therefore, it is required to run that on the same JDK as sonar build.
-mvn -f .. -T 1 -B exec:exec -Dexec.executable=echo -Dexec.args='#Module-info:<dependency><groupId>${project.groupId}</groupId><artifactId>${project.artifactId}</artifactId><version>$${project.version}</version><type>pom</type></dependency>' | grep '#Module-info:' | cut -f2 -d':'
+"$(dirname "$0")"/../mvnw -f .. -T 1 -B exec:exec -Dexec.executable=echo -Dexec.args='#Module-info:<dependency><groupId>${project.groupId}</groupId><artifactId>${project.artifactId}</artifactId><version>$${project.version}</version><type>pom</type></dependency>' | grep '#Module-info:' | cut -f2 -d':'
