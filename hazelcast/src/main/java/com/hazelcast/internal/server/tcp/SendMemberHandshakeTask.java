ae2f9b836d4e6cfea60feca31bf9e90d01cf79fc
James Holgate <130981049+JamesHazelcast@users.noreply.github.com>
2024-06-26 13:33:31 +0100

    Exclude non-WAN address aliases during WAN MemberHandshake [SUP-432] (#2069)
    Only send WAN address aliases during a WAN handshake - this avoids the
    scenario encountered and described in SUP-432.
    
    This PR also addresses 2 minor issues found with WAN during testing: an
    improvement to logging, and catching an exception that otherwise breaks
    health checks.
    
    There is currently no regression test included as it has been difficult
    to recreate in our current Docker IT environment - given the P1 nature
    of the issue, it was decided that this PR would be raised with
    verification confirmed via the manual reproducer for now, with the
    regression test planned for after the P1 patch release.
    
    Fixes https://hazelcast.atlassian.net/browse/SUP-432
    Fixes https://hazelcast.atlassian.net/browse/ESC-30
    
    GitOrigin-RevId: 107753a4c19aab352162b57b11f9f3b0c0c42c98

--- hazelcast/src/main/java/com/hazelcast/internal/server/tcp/SendMemberHandshakeTask.java
+++ hazelcast/src/main/java/com/hazelcast/internal/server/tcp/SendMemberHandshakeTask.java
@@ -82,9 +82,18 @@ public class SendMemberHandshakeTask implements Runnable {
     }
 
     Map<ProtocolType, Collection<Address>> getConfiguredLocalAddresses() {
+        EndpointQualifier qualifier = connection.getConnectionManager().getEndpointQualifier();
+        boolean isWanHandshake = qualifier != null && qualifier.getType().equals(ProtocolType.WAN);
+
         Map<ProtocolType, Collection<Address>> addressMap = new HashMap<>();
         Map<EndpointQualifier, Address> addressesPerEndpointQualifier = serverContext.getThisAddresses();
         for (Map.Entry<EndpointQualifier, Address> addressEntry : addressesPerEndpointQualifier.entrySet()) {
+            if (isWanHandshake && !addressEntry.getKey().getType().equals(ProtocolType.WAN)) {
+                // When conducting a WAN handshake we should only share WAN address aliases; there is no
+                //  purpose for other aliases when WAN communicating, and sharing non-WAN aliases can lead
+                //  to an address clash between the clusters, creating chaos. See SUP-432.
+                continue;
+            }
             Collection<Address> addresses = addressMap.computeIfAbsent(addressEntry.getKey().getType(), k -> new ArrayList<>());
             addresses.add(addressEntry.getValue());
         }
