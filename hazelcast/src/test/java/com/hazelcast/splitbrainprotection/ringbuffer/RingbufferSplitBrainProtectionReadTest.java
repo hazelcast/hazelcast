e256220f06e68d4093eaafa11364826dcc69409a
Orçun Çolak <90305879+orcunc@users.noreply.github.com>
2024-07-03 13:24:08 +0300

    Remove unused ExpectedException.none() (#2518)
    GitOrigin-RevId: d77bb243ed940021495d9d85b7b9da52176fbd3f

--- hazelcast/src/test/java/com/hazelcast/splitbrainprotection/ringbuffer/RingbufferSplitBrainProtectionReadTest.java
+++ hazelcast/src/test/java/com/hazelcast/splitbrainprotection/ringbuffer/RingbufferSplitBrainProtectionReadTest.java
@@ -28,10 +28,8 @@ import com.hazelcast.test.annotation.ParallelJVMTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
@@ -52,8 +50,6 @@ public class RingbufferSplitBrainProtectionReadTest extends AbstractSplitBrainPr
     @Parameter
     public static SplitBrainProtectionOn splitBrainProtectionOn;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @BeforeClass
     public static void setUp() {
@@ -139,7 +135,7 @@ public class RingbufferSplitBrainProtectionReadTest extends AbstractSplitBrainPr
         }
     }
 
-    private static class Filter implements IFunction {
+    private static class Filter implements IFunction<Object, Object> {
         @Override
         public Object apply(Object input) {
             return true;
