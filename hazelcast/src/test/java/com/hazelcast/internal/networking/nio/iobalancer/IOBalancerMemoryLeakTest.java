72eba17e10ef319ceb3de1ff01d5638a837efb19
James Holgate <130981049+JamesHazelcast@users.noreply.github.com>
2024-08-07 09:44:30 +0100

    Fix flaky IOBalancerMemoryLeakTest [HZG-54] (#2869)
    This test was failing because the `HttpClient` used for sending REST
    endpoint requests was holding on to connections. The `HttpClient`
    documentation states that connections are cleaned up as the client
    object is GC'd, and so this test can be resolved by simply invoking a GC
    using `System.gc()` after all of our REST requests have been submitted.
    
    Fixes https://hazelcast.atlassian.net/browse/HZG-54
    Closes https://github.com/hazelcast/hazelcast/issues/9216
    
    GitOrigin-RevId: d34026794626a68e72ee610d6cdbc172dd9f6952

--- hazelcast/src/test/java/com/hazelcast/internal/networking/nio/iobalancer/IOBalancerMemoryLeakTest.java
+++ hazelcast/src/test/java/com/hazelcast/internal/networking/nio/iobalancer/IOBalancerMemoryLeakTest.java
@@ -45,15 +45,13 @@ import static org.junit.Assert.assertEquals;
 @Category(NightlyTest.class)
 public class IOBalancerMemoryLeakTest extends HazelcastTestSupport {
 
-    private static final long INCREASED_TIMEOUT_SECONDS = 10 * 60L;
-
     @Before
     @After
     public void killAllHazelcastInstances() {
         HazelcastInstanceFactory.terminateAll();
     }
 
-    @Test(timeout = 12 * 60 * 1000)
+    @Test
     public void testMemoryLeak_with_RestConnections() throws IOException {
         Config config = new Config();
         config.setClusterName(randomName());
@@ -65,13 +63,15 @@ public class IOBalancerMemoryLeakTest extends HazelcastTestSupport {
         for (int i = 0; i < 100; i++) {
             communicator.getClusterInfo();
         }
+        System.gc();
+
         final IOBalancer ioBalancer = getIoBalancer(instance);
         assertTrueEventually(() -> {
             int inPipelineSize = ioBalancer.getInLoadTracker().getPipelines().size();
             int outPipelineSize = ioBalancer.getOutLoadTracker().getPipelines().size();
             assertEquals(0, inPipelineSize);
             assertEquals(0, outPipelineSize);
-        }, INCREASED_TIMEOUT_SECONDS);
+        });
     }
 
     @Test
