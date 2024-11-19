d7296310de3b2f11d284d81560e7d0a82d6bca52
Serkan <serkan.ozel@hazelcast.com>
2024-11-19 13:13:37 +0300

    Modify ClientListTest#testListener to cover item removal [PA-560] (#3602)
    As it can be seen
    [here](https://sonarcloud.io/code?id=hazelcast-os&selected=hazelcast-os%3Ahazelcast%2Fsrc%2Fmain%2Fjava%2Fcom%2Fhazelcast%2Fclient%2Fimpl%2Fproxy%2FClientListProxy.java&line=350),
    the item remove item listener were not being tested. This should
    increase coverage. To be ported to the standalone client as well.
    
    Very similar to https://github.com/hazelcast/hazelcast-mono/pull/3593
    
    GitOrigin-RevId: c9351c909327ed6cfe3a98dff67c906d2bd5928c

--- hazelcast/src/test/java/com/hazelcast/client/collections/ClientListTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/collections/ClientListTest.java
@@ -38,7 +38,6 @@ import java.util.ListIterator;
 import java.util.Spliterator;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
@@ -222,15 +221,17 @@ public class ClientListTest extends HazelcastTestSupport {
     @Test
     public void testListener() throws Exception {
 
-        final CountDownLatch latch = new CountDownLatch(6);
+        final CountDownLatch addLatch = new CountDownLatch(6);
+        final CountDownLatch removeLatch = new CountDownLatch(1);
 
         ItemListener<String> listener = new ItemListener<>() {
 
             public void itemAdded(ItemEvent<String> itemEvent) {
-                latch.countDown();
+                addLatch.countDown();
             }
 
             public void itemRemoved(ItemEvent<String> item) {
+                removeLatch.countDown();
             }
         };
         UUID registrationId = list.addItemListener(listener, true);
@@ -240,8 +241,10 @@ public class ClientListTest extends HazelcastTestSupport {
                 list.add("item" + i);
             }
             list.add("done");
+            list.remove("done");
         }).start();
-        assertTrue(latch.await(20, TimeUnit.SECONDS));
+        assertOpenEventually(addLatch);
+        assertOpenEventually(removeLatch);
         list.removeItemListener(registrationId);
     }
 
