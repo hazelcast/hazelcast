1412267478b51fe32fa7670a9f97792510858a34
Jack Green <jack.green@hazelcast.com>
2025-05-13 11:00:20 +0100

    Make AWS `RegionValidatorTest` use dynamic region list (#4316)
    Extends coverage added in
    https://github.com/hazelcast/hazelcast-mono/pull/4301, addressing:
    > In the future we may want to use the AWS API to fetch valid region
    names, or use an explicit list of region names to check against
    
    Changes:
    - Uses a _dynamic_ source of current AWS regions, sourced from
    [`botocore`](https://github.com/boto/botocore/) (`The botocore package
    is the foundation for the AWS CLI {...} maintained and published by
    Amazon Web Services.`)
    - Converts to `ParameterizedTest`
       - less duplication
       - better communication of which cases have passed or failed:
    <img width="502" alt="image"
    src="https://github.com/user-attachments/assets/240daad8-3060-4ef9-9008-991bb3e2ecc6"
    />
    GitOrigin-RevId: 62858bd9be2d653b509a3f507451cd1e7e4ba88c

--- hazelcast/src/test/java/com/hazelcast/aws/RegionValidatorTest.java
+++ hazelcast/src/test/java/com/hazelcast/aws/RegionValidatorTest.java
@@ -16,71 +16,64 @@
 
 package com.hazelcast.aws;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Streams;
 import com.hazelcast.config.InvalidConfigurationException;
-import org.junit.Test;
-import org.junit.function.ThrowingRunnable;
+import com.hazelcast.test.annotation.SlowTest;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static com.hazelcast.test.HazelcastTestSupport.assertThrows;
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.net.URL;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RegionValidatorTest {
 
-    private static final String[] VALID_AWS_REGIONS_MAY_2025 = new String[] {
-            "af-south-1", "ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-south-2",
-            "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "ap-southeast-4", "ap-southeast-5", "ap-southeast-7",
-            "aws-cn-global", "aws-global", "aws-iso-b-global", "aws-iso-global", "aws-us-gov-global", "ca-central-1",
-            "ca-west-1", "cn-north-1", "cn-northwest-1", "eu-central-1", "eu-central-2", "eu-isoe-west-1", "eu-north-1",
-            "eu-south-1", "eu-south-2", "eu-west-1", "eu-west-2", "eu-west-3", "eusc-de-east-1", "il-central-1",
-            "me-central-1", "me-south-1", "mx-central-1", "sa-east-1", "us-east-1", "us-east-2", "us-gov-east-1",
-            "us-gov-west-1", "us-iso-east-1", "us-iso-west-1", "us-isob-east-1", "us-isof-east-1", "us-isof-south-1",
-            "us-west-1", "us-west-2"
-    };
+    private static Stream<String> getAWSRegions() throws IOException {
+        URL source = new URL("https://raw.githubusercontent.com/boto/botocore/develop/botocore/data/endpoints.json");
+        JsonNode tree = new ObjectMapper().readTree(source);
+        JsonNode partitions = tree.get("partitions");
+        Stream<JsonNode> regionArrays = partitions.valueStream().map(partition -> partition.get("regions"));
+        Stream<String> regionNames = regionArrays.flatMap(region -> Streams.stream(region.fieldNames()));
 
-    @Test
-    public void validateValidRegion() {
-        for (String region : VALID_AWS_REGIONS_MAY_2025) {
-            RegionValidator.validateRegion(region);
-        }
+        return regionNames;
     }
 
-    @Test
-    public void validateInvalidRegion() {
-        // given
-        String region = "us-wrong-1";
-        String expectedMessage = String.format("The provided region %s is not a valid AWS region.", region);
-
-        //when
-        ThrowingRunnable validateRegion = () -> RegionValidator.validateRegion(region);
-
-        //then
-        InvalidConfigurationException thrownEx = assertThrows(InvalidConfigurationException.class, validateRegion);
-        assertEquals(expectedMessage, thrownEx.getMessage());
+    /**
+     * Because this has an external dependencies, it's not sensible to check it on a per-PR basis as it could start failing due
+     * to unrelated changes
+     */
+    @SlowTest
+    @ParameterizedTest
+    @MethodSource("getAWSRegions")
+    void getAWSRegions(String region) {
+        assertDoesNotThrow(() -> RegionValidator.validateRegion(region));
     }
 
-    @Test
-    public void validateInvalidGovRegion() {
-        // given
-        String region = "us-gov-wrong-1";
-        String expectedMessage = String.format("The provided region %s is not a valid AWS region.", region);
+    private static Stream<Arguments> invalidRegions() {
+        return Stream.of(
+                Arguments.of(Named.of("Invalid Region", "us-wrong-1"), "The provided region %s is not a valid AWS region."),
+                Arguments.of(Named.of("Invalid Gov Region", "us-gov-wrong-1"),
+                        "The provided region %s is not a valid AWS region."),
+                Arguments.of(Named.of("Null Region", null), "The provided region is null."));
+    }
 
+    @ParameterizedTest
+    @MethodSource("invalidRegions")
+    void validateInvalidRegion(String region, String expectedMessage) {
         // when
-        ThrowingRunnable validateRegion = () -> RegionValidator.validateRegion(region);
+        Executable validateRegion = () -> RegionValidator.validateRegion(region);
 
         //then
-        InvalidConfigurationException thrownEx = assertThrows(InvalidConfigurationException.class, validateRegion);
-        assertEquals(expectedMessage, thrownEx.getMessage());
-    }
-
-    @Test
-    public void validateNullRegion() {
-        // given
-        String expectedMessage = "The provided region is null.";
-
-        // when
-        ThrowingRunnable validateRegion = () -> RegionValidator.validateRegion(null);
-
-        //then
-        InvalidConfigurationException thrownEx = assertThrows(InvalidConfigurationException.class, validateRegion);
-        assertEquals(expectedMessage, thrownEx.getMessage());
+        Exception thrownEx = assertThrows(InvalidConfigurationException.class, validateRegion);
+        assertEquals(String.format(expectedMessage, region), thrownEx.getMessage());
     }
 }
