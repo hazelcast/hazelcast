a4ec7b08362a0facdd70a4996097d1ad0467630d
James Holgate <130981049+JamesHazelcast@users.noreply.github.com>
2025-05-21 12:41:47 +0100

    Fix QueryCacheEventData not receiving Versioned input [HZG-385] (#4354)
    A customer reported to us that they were seeing some lost Continuous
    Query Cache (CQC) events, with accompanying NullPointerExceptions
    (NPEs). The NPE stack traces showed that while reading CQC event data,
    the `mapName` field was `null` in some cases.
    
    Introducing some extra assertions and running our nightly test builds
    revealed that `mapName` was indeed being found to be null in
    `DefaultQueryCacheEventData` objects when read from `BatchEventData`.
    
    After some investigation, I identified the cause: the `ObjectDataInput`
    passed to `DefaultQueryCacheEventData#readData` does not have its
    version defined, so it is using version `0.0`, which means the
    `RU_COMPAT_5_3` code branch in `DefaultQueryCacheEventData` was not
    being reached - thus `mapName` was not read, and was left as `null`.
    
    This happens because `BatchEventData` reads `QueryCacheEventData` within
    its own `readData` method, and since `BatchEventData` does not implement
    `Versioned`, this input stream did not have a version set.
    
    This PR resolves the issue by marking `BatchEventData` as `Versioned`,
    so that the event data it loads will also receive a versioned input
    stream. It has been noted that `DefaultQueryCacheEventData` is also
    annotated with `BinaryInterface`, and so should not have implemented
    `Versioned` in the first place - but after walking through the code
    paths, we believe this may be an overly cautious annotation, as client
    compatibility is handled through the protocol encoding. This will be
    further investigated for a `6.0.0` release, to either remove these
    annotations, or correct this `Versioned` behaviour.
    
    Test coverage for this is now handled in existing tests (such as
    `QueryCacheSimpleStressTest`) with the addition of an assertion in
    `BatchEventData#readData`.
    
    Fixes https://hazelcast.atlassian.net/browse/HZG-385
    
    GitOrigin-RevId: a1a9d45ee803264cc779c4d8107c1d4045588cb7

--- hazelcast/src/main/java/com/hazelcast/map/impl/querycache/event/BatchEventData.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/querycache/event/BatchEventData.java
@@ -16,12 +16,14 @@
 
 package com.hazelcast.map.impl.querycache.event;
 
+import com.hazelcast.internal.cluster.Versions;
 import com.hazelcast.map.impl.event.EventData;
 import com.hazelcast.map.impl.querycache.event.sequence.Sequenced;
 import com.hazelcast.cluster.Address;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.internal.serialization.BinaryInterface;
+import com.hazelcast.nio.serialization.impl.Versioned;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,11 +35,18 @@ import static com.hazelcast.internal.util.Preconditions.checkNotNull;
 
 /**
  * Holder for a collection of {@link QueryCacheEventData}.
+ * <p>
+ * The {@link Versioned} interface is used due to {@link DefaultQueryCacheEventData}
+ * requiring it, which is handled by the same {@link ObjectDataInput} as this class.
+ * The {@link DefaultQueryCacheEventData} class should not have implemented
+ * {@link Versioned} due to being annotated with {@link BinaryInterface}, so this
+ * needs to be corrected in the next major release. For now, this class will implement
+ * {@link Versioned} to avoid breaking serialization of the new field.
  *
  * @see QueryCacheEventData
  */
 @BinaryInterface
-public class BatchEventData implements Sequenced, EventData {
+public class BatchEventData implements Sequenced, EventData, Versioned {
 
     private String source;
     private Collection<QueryCacheEventData> events;
@@ -133,6 +142,8 @@ public class BatchEventData implements Sequenced, EventData {
             for (int i = 0; i < size; i++) {
                 QueryCacheEventData eventData = newQueryCacheEventDataBuilder(true).build();
                 eventData.readData(in);
+                assert in.getVersion().isLessThan(Versions.V5_4) || eventData.getMapName() != null
+                        : "Map name should not be null in version 5.4 and above";
 
                 events.add(eventData);
             }
