04aba9c70ebd4fe3b3dc0144f4efa549fc78ec59
Brian Jones <2412706+brianjones@users.noreply.github.com>
2026-01-26 18:38:57 +0000

    Update MapStoreWriteBehindSplitBrainTest to extend SplitBrainTestSupport

--- hazelcast/src/test/java/com/hazelcast/map/impl/mapstore/writebehind/MapStoreWriteBehindSplitBrainTest.java
+++ hazelcast/src/test/java/com/hazelcast/map/impl/mapstore/writebehind/MapStoreWriteBehindSplitBrainTest.java
@@ -16,144 +16,138 @@
 
 package com.hazelcast.map.impl.mapstore.writebehind;
 
-import com.hazelcast.cluster.MembershipEvent;
-import com.hazelcast.cluster.MembershipListener;
 import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.MapStoreConfig;
-import com.hazelcast.config.NetworkConfig;
-import com.hazelcast.core.Hazelcast;
+import com.hazelcast.config.MergePolicyConfig;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.LifecycleEvent;
-import com.hazelcast.core.LifecycleEvent.LifecycleState;
-import com.hazelcast.core.LifecycleListener;
-import com.hazelcast.instance.impl.HazelcastInstanceFactory;
 import com.hazelcast.map.IMap;
-import com.hazelcast.spi.properties.ClusterProperty;
-import com.hazelcast.test.HazelcastSerialClassRunner;
-import com.hazelcast.test.HazelcastTestSupport;
-import com.hazelcast.test.annotation.NightlyTest;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import com.hazelcast.spi.merge.DiscardMergePolicy;
+import com.hazelcast.spi.merge.HigherHitsMergePolicy;
+import com.hazelcast.spi.merge.LatestAccessMergePolicy;
+import com.hazelcast.spi.merge.LatestUpdateMergePolicy;
+import com.hazelcast.spi.merge.PassThroughMergePolicy;
+import com.hazelcast.spi.merge.PutIfAbsentMergePolicy;
+import com.hazelcast.test.HazelcastParallelParametersRunnerFactory;
+import com.hazelcast.test.HazelcastParametrizedRunner;
+import com.hazelcast.test.SplitBrainTestSupport;
+import com.hazelcast.test.annotation.SlowTest;
+
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Collection;
 
-import static com.hazelcast.cluster.ClusterState.ACTIVE;
+import javax.annotation.Nonnull;
+
+import static com.hazelcast.config.InMemoryFormat.BINARY;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(HazelcastSerialClassRunner.class)
-@Category(NightlyTest.class)
-public class MapStoreWriteBehindSplitBrainTest extends HazelcastTestSupport {
 
-    @Before
-    @After
-    public void killAllHazelcastInstances() {
-        HazelcastInstanceFactory.terminateAll();
+@RunWith(HazelcastParametrizedRunner.class)
+@UseParametersRunnerFactory(HazelcastParallelParametersRunnerFactory.class)
+@Category(SlowTest.class)
+public class MapStoreWriteBehindSplitBrainTest extends SplitBrainTestSupport {
+
+    private static final int[] BRAINS = new int[]{3, 3};
+
+    private static final int NUMBER_OF_ITEMS_TO_WRITE = 2000;
+
+    @Parameters(name = "format:{0}, mergePolicy:{1}")
+    public static Collection<Object[]> parameters() {
+        return asList(new Object[][]{
+                {BINARY, DiscardMergePolicy.class.getName()},
+                {BINARY, HigherHitsMergePolicy.class.getName()},
+                {BINARY, LatestAccessMergePolicy.class.getName()},
+                {BINARY, LatestUpdateMergePolicy.class.getName()},
+                {BINARY, PassThroughMergePolicy.class.getName()},
+                {BINARY, PutIfAbsentMergePolicy.class.getName()},
+        });
     }
 
-    @Test
-    public void testMapStoreThreadsAfterSplitBrain() throws InterruptedException {
-        String clusterName = generateRandomString(10);
-        Config config = smallInstanceConfigWithoutJetAndMetrics();
-        config.setProperty(ClusterProperty.MERGE_FIRST_RUN_DELAY_SECONDS.getName(), "10");
-        config.setProperty(ClusterProperty.MERGE_NEXT_RUN_DELAY_SECONDS.getName(), "10");
-        config.setClusterName(clusterName);
+    @Parameter
+    public InMemoryFormat inMemoryFormat;
 
-        NetworkConfig networkConfig = config.getNetworkConfig();
-        JoinConfig join = networkConfig.getJoin();
-        join.getMulticastConfig().setEnabled(false);
-        join.getTcpIpConfig().setEnabled(true);
-        join.getTcpIpConfig().addMember("127.0.0.1");
+    @Parameter(value = 1)
+    public String mergePolicyClassName;
 
-        final TrackingMapStore<Integer, Integer> mapStore = new TrackingMapStore<Integer, Integer>();
-        final MapStoreConfig mapStoreConfig = new MapStoreConfig();
-        final String mapName = randomMapName("default");
+    @SuppressWarnings("null")
+    protected @Nonnull String mapName = randomMapName("map-");
+
+    private @Nonnull TrackingMapStore<Integer, Integer> mapStore = new TrackingMapStore<Integer, Integer>();
+
+    private MergeLifecycleListener mergeLifecycleListener;
+
+    @Override
+    protected int[] brains() {
+        return BRAINS;
+    }
+
+    @Override
+    protected Config config() {
+        MergePolicyConfig mergePolicyConfig = new MergePolicyConfig()
+                .setPolicy(mergePolicyClassName)
+                .setBatchSize(10);
+
+        MapStoreConfig mapStoreConfig = new MapStoreConfig();
         mapStoreConfig
                 .setImplementation(mapStore)
                 .setWriteDelaySeconds(1)
                 .setWriteBatchSize(10)
                 .setWriteCoalescing(false);
+
+        Config config = super.config();
         config.getMapConfig(mapName)
+                .setInMemoryFormat(inMemoryFormat)
+                .setMergePolicyConfig(mergePolicyConfig)
                 .setBackupCount(1)
-                .setMapStoreConfig(mapStoreConfig);
+                .setMapStoreConfig(mapStoreConfig)
+                .setStatisticsEnabled(true)
+                .setPerEntryStatsEnabled(true);
 
-        HazelcastInstance h1 = Hazelcast.newHazelcastInstance(config);
-        HazelcastInstance h2 = Hazelcast.newHazelcastInstance(config);
-        HazelcastInstance h3 = Hazelcast.newHazelcastInstance(config);
+        return config;
+    }
 
-        assertClusterSize(3, h1, h3);
-        assertClusterSizeEventually(3, h2);
+    @Override
+    protected void onBeforeSplitBrainCreated(HazelcastInstance[] instances) throws InterruptedException {
+        writeToMapAndAssertWriteBehind(instances);
+    }
 
-        final CountDownLatch splitLatch = new CountDownLatch(2);
-        h3.getCluster().addMembershipListener(new MembershipListener() {
-            @Override
-            public void memberAdded(MembershipEvent membershipEvent) {
-            }
+    @Override
+    protected void onAfterSplitBrainCreated(HazelcastInstance[] firstBrain, HazelcastInstance[] secondBrain) {
+        @Nonnull MergeLifecycleListener mergeLifecycleListener = new MergeLifecycleListener(secondBrain.length);
+        for (HazelcastInstance instance : secondBrain) {
+            instance.getLifecycleService().addLifecycleListener(mergeLifecycleListener);
+        }
+        this.mergeLifecycleListener = mergeLifecycleListener;
+    }
 
-            @Override
-            public void memberRemoved(MembershipEvent membershipEvent) {
-                splitLatch.countDown();
-            }
+    @Override
+    protected void onAfterSplitBrainHealed(HazelcastInstance[] instances) throws InterruptedException {
+        // wait until merge completes
+        mergeLifecycleListener.await();
 
-        });
-
-        final CountDownLatch mergeLatch = new CountDownLatch(1);
-        h3.getLifecycleService().addLifecycleListener(new MergedEventLifeCycleListener(mergeLatch));
-
-        final int numberOfItems = 1024;
-        h1.getLoggingService().getLogger("mapStore").info("populating h1 " + h1.getName());
-        populateMap(h1.getMap(mapName), mapStore, numberOfItems);
-        assertTrueEventually("expected all entries to be written", () ->
-            assertTrue(mapStore.allExpectedWritesComplete()), 10);
-
-        closeConnectionBetween(h1, h3);
-        closeConnectionBetween(h2, h3);
-
-        assertTrue(splitLatch.await(10, TimeUnit.SECONDS));
-        assertClusterSizeEventually(2, h1, h2);
-        assertClusterSize(1, h3);
-
-        assertTrue(mergeLatch.await(30, TimeUnit.SECONDS));
-        assertClusterSizeEventually(3, h1, h2, h3);
-
-        assertClusterState(ACTIVE, h1, h2, h3);
-        h1.getMap(mapName).flush();
+        // wait until any flushing of maps following merge is complete
         assertTrueEventually("write behinds complete", () ->
             assertTrue(mapStore.lastWriteAtLeastMsAgo(2000)), 30);
 
-        // this will generate thread conflicts on h3
-        populateMap(h1.getMap(mapName), mapStore, numberOfItems);
-        h1.getMap(mapName).flush();
-
-        assertTrueEventually("expected all entries to be written", () ->
-            assertTrue(mapStore.allExpectedWritesComplete()), 30);
+        writeToMapAndAssertWriteBehind(instances);
     }
 
-    public static class MergedEventLifeCycleListener implements LifecycleListener {
-        private final CountDownLatch mergeLatch;
-
-        MergedEventLifeCycleListener(CountDownLatch mergeLatch) {
-            this.mergeLatch = mergeLatch;
-        }
-
-        public void stateChanged(LifecycleEvent event) {
-            if (event.getState() == LifecycleState.MERGED) {
-                mergeLatch.countDown();
-            }
-        }
-    }
-
-    private void populateMap(IMap<Integer, Integer> map,
-            TrackingMapStore<Integer, Integer> trackingMapStore,
-            int numberOfItems) throws InterruptedException {
-        for (int i = 0; i < numberOfItems; i++) {
-            trackingMapStore.expectWrite(i, i);
+    private void writeToMapAndAssertWriteBehind(HazelcastInstance[] instances) throws InterruptedException {
+        IMap<Integer, Integer> map = instances[0].getMap(mapName);
+        for (int i = 0; i < NUMBER_OF_ITEMS_TO_WRITE; i++) {
+            mapStore.expectWrite(i, i);
             map.put(i, i);
             Thread.sleep(3); // ensure writes happen across multiple write-behind invocations
         }
+        map.flush();
+
+        assertTrueEventually("expected all entries to be written", () ->
+            assertTrue(mapStore.allExpectedWritesComplete()), 30);
     }
 }
