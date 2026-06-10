f2d6ed7917b01961970b3a71026ec16be8bae34e
James Holgate <130981049+JamesHazelcast@users.noreply.github.com>
2026-06-10 14:59:01 +0100

    Improve Python version mismatch failure message [CTT-1221] (#6955)
    When mapUsingPython in Jet is used with an unsupported Python runtime
    (like `3.14` currently), it currently fails with a complex and unclear
    stacktrace.
    
    This PR simply adds a more explicit version check, which also checks
    minor version, so we can throw a more useful error to improve
    diagnostics for unsupported Python runtimes.
    
    Fixes https://hazelcast.atlassian.net/browse/CTT-1221 (removes ambiguous
    failures)
    
    GitOrigin-RevId: f8e43e916df2afcf4391bbb0110eefe825f27738

--- extensions/python/src/main/resources/jet_to_python_init.sh
+++ extensions/python/src/main/resources/jet_to_python_init.sh
@@ -3,6 +3,8 @@
 
 set -e -x
 
+PYTHON_RUNTIME_COMPATIBILITY_CHECK_FAILED_EXIT_CODE=86
+
 python3 -m venv jet_to_python_env --system-site-packages
 source jet_to_python_env/bin/activate
 
@@ -10,6 +12,28 @@ flock --exclusive ~/.jet-pip.lock python3 -m pip install --upgrade pip
 flock --exclusive ~/.jet-pip.lock python3 -m pip install protobuf==@python.protobuf.version@ grpcio==@grpc.version@
 
 [[ -f requirements.txt ]] && python3 -m pip install -r requirements.txt
+
+if ! python3 - <<'PY'
+import importlib.metadata
+import sys
+
+try:
+    import grpc
+    import google.protobuf
+    import jet_to_python_pb2
+    import jet_to_python_pb2_grpc
+except Exception:
+    print("Python runtime compatibility check failed")
+    print("Python executable:", sys.executable)
+    print("Python version:", sys.version)
+    print("protobuf version:", importlib.metadata.version("protobuf"))
+    print("grpcio version:", importlib.metadata.version("grpcio"))
+    raise
+PY
+then
+    exit $PYTHON_RUNTIME_COMPATIBILITY_CHECK_FAILED_EXIT_CODE
+fi
+
 source ./jet_to_python_params.sh
 if [[ -f init.sh ]]; then
     echo "Executing init.sh"
